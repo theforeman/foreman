8e4c0917c9d361f5558d1bf4708c1ea75788e86c
Ohad Levy <ohadlevy@gmail.com>
2009-07-21 22:59:43 +0800

    added a basic external nodes support

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -9,4 +9,20 @@ class HostsController < ApplicationController
     config.columns[:operatingsystem].form_ui  = :select
     columns[:architecture].label = "Arch"
   end
+
+  def externalNodes
+    if params.has_key? "fqdn"
+      fqdn = params.delete "fqdn"
+    else
+      head(:bad_request) and return
+    end
+    host = Host.find_by_name fqdn.split(".")[0]
+    param = {}
+    param[:puppetmaster] = host.puppetmaster
+    param[:longsitename] = host.domain.fullname
+    param[:hostmode] = host.environment
+    puppetclasses = []
+    puppetclasses << "common"
+    render :text => Hash['classes' => puppetclasses, 'parameters' => param].to_yaml and return
+  end
 end
