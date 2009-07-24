29eeadb8e514076a9ca9f213ca1c587b5c6182cd
Ohad Levy <ohadlevy@gmail.com>
2009-07-24 14:47:09 +0800

    using machine localtime to calculate the time ago

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -1,6 +1,6 @@
 module HostsHelper
   def last_compile_column(record)
-    time_ago_in_words record.last_compile unless record.last_compile.nil?
+    time_ago_in_words record.last_compile.getlocal unless record.last_compile.nil?
   end
 
 end
