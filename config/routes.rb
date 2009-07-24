7291f36de067c637483dc4d3635653090171d285
Ohad Levy <ohadlevy@gmail.com>
2009-07-24 16:55:03 +0800

    removed default route, added pkelly email address

--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+   map.root :controller => "hosts"
 
   # See how all your routes lay out with "rake routes"
 
