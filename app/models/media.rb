4eeab9169bb0232000893443797847568fc0d1b1
Ohad Levy <ohadlevy@gmail.com>
2009-08-01 22:39:53 +0800

    updated validation column name

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,8 +1,8 @@
 class Media < ActiveRecord::Base
   belongs_to :operatingsystem
   has_many :hosts
-  validates_uniqueness_of :name, :scope => :operatingsystem
-  validates_uniqueness_of :path, :scope => :operatingsystem
+  validates_uniqueness_of :name, :scope => :operatingsystem_id
+  validates_uniqueness_of :path, :scope => :operatingsystem_id
   validates_presence_of :name, :path
   validates_format_of :path, :with => /^((http|ftp):\/\/)|\w+:\/\w+/, 
     :message => "Url (http:// or ftp://) or a NFS share area allowed (e.g. server:/vol/dir)"
