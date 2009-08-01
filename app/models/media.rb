f049c0ab83dd5d354342b65c432d133951672df5
Ohad Levy <ohadlevy@gmail.com>
2009-08-01 21:40:44 +0800

    updated validations and error message for medias

--- app/models/media.rb
+++ app/models/media.rb
@@ -1,12 +1,12 @@
 class Media < ActiveRecord::Base
   belongs_to :operatingsystem
   has_many :hosts
-  validates_uniqueness_of :path
+  validates_uniqueness_of :name, :scope => :operatingsystem
+  validates_uniqueness_of :path, :scope => :operatingsystem
   validates_presence_of :name, :path
-  validates_format_of :path, :with => /^((http|ftp):\/\/)|\w+:\/\w+/, :message => "path must be a url (http:// or ftp://) or a NFS share (e.g. server:/vol/dir), it should not include the architecture direcoty at the end.
-  for example: http://mirror.nus.edu.sg/fedora/releases/11/Fedora"
+  validates_format_of :path, :with => /^((http|ftp):\/\/)|\w+:\/\w+/, 
+    :message => "Url (http:// or ftp://) or a NFS share area allowed (e.g. server:/vol/dir)"
 
   alias_attribute :os, :operatingsystem
   before_destroy :ensure_not_used
-
 end
