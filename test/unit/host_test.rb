bff742abe00a543bf619f7d0ee202649a805c3b1
Ohad Levy <ohadlevy@gmail.com>
2009-08-13 00:04:14 +0800

    added basic features and a basic host creation test

--- test/unit/host_test.rb
+++ test/unit/host_test.rb
@@ -8,20 +8,25 @@ class HostTest < ActiveSupport::TestCase
 
   test "should fix mac address" do
     host = Host.create :name => "myhost", :mac => "aabbccddeeff"
-    assert !host.save
     assert_equal "aa:bb:cc:dd:ee:ff", host.mac
   end
 
   test "should fix ip address if a leading zero is used" do
     host = Host.create :name => "myhost", :mac => "aabbccddeeff", :ip => "123.01.02.03"
-    assert !host.save
     assert_equal "123.1.2.3", host.ip
   end
 
   test "should add domain name to hostname" do
     host = Host.create :name => "myhost", :mac => "aabbccddeeff", :ip => "123.01.02.03",
       :domain => Domain.find_or_create_by_name("company.com")
-    assert !host.save
     assert_equal "myhost.company.com", host.name
   end
+
+  test "should be able to save host" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first
+    assert host.valid?
+  end
+
 end
