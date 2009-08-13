ff8ad96a8a4a609f83fa85d86d812a8744a2fe0c
Ohad Levy <ohadlevy@gmail.com>
2009-08-14 00:07:57 +0800

    added some basic nested views

--- app/controllers/environments_controller.rb
+++ app/controllers/environments_controller.rb
@@ -1,5 +1,7 @@
 class EnvironmentsController < ApplicationController
   active_scaffold :environment do |config|
     config.columns = %w{name}
+    config.nested.add_link("Hosts", [:hosts])
+    config.nested.add_link("Classes", [:puppetclasses])
   end
 end
