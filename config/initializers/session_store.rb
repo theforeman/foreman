383a612c370071bc56171dd3caa6d8de77d6c35c
Ohad Levy <ohadlevy@gmail.com>
2009-08-20 10:44:04 +0800

    fixed typos

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,10 +2,10 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_gini_session',
+  :key         => '_GNI_session',
   :secret      => '4fe1a103699cf36976181cb4d343e6cb27d7904113c9a2113862c53b51f12357f413c996e820e6e544464242915d643d32b698a9679bbfe63f6eedfd9bf67961'
 }
 
