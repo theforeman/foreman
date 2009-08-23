766e0f384d9cca88b9bba17f50fa38f3593b0b90
Ohad Levy <ohadlevy@gmail.com>
2009-08-23 23:03:38 +0800

    a simple host status indicator

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -6,5 +6,17 @@ module HostsHelper
       password_field_tag field_name, record.root_pass
   end
 
+# method that reformats the hostname column by adding the status icons
+  def name_column(record)
+    return image_tag("hosts/attention_required.png", :size => "18x18",
+                     :title => "Pending Installation") + " " + record.name  if record.build
+
+    os_fact=record.fact('kernel')[0]
+    os=os_fact.value unless os_fact.nil?
+    return image_tag("hosts/warning.png", :size => "18x18",
+                     :title => "No Inventory Data") + " " + record.name if os.nil?
+
+    return image_tag("hosts/#{os}.jpg", :size => "18x18", :title => os) + " " + record.name
+  end
 
 end
