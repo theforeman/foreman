d5f708c35743f55a1ade6023a2dff4087aa62cd9
Ohad Levy <ohadlevy@gmail.com>
2009-09-11 22:22:59 +0800

    fixes #6

--- config/initializers/puppet.rb
+++ config/initializers/puppet.rb
@@ -2,3 +2,7 @@ class Resource < Puppet::Rails::Resource
 end
 class SourceFile < Puppet::Rails::SourceFile
 end
+class ResourceTag < Puppet::Rails::ResourceTag
+end
+class ParamValue < Puppet::Rails::ParamValue
+end
