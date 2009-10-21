c5d4bcbe052640344527d63ace3d8b6ccfca3f89
Ohad Levy <ohadlevy@gmail.com>
2009-10-21 16:50:09 +0800

    add a base report test for reports which have a skip value with no messages

--- test/unit/report_test.rb
+++ test/unit/report_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  test "it should not change host report status when we have skipped reports but there are no log entries" do
+    yaml=(File.read(File.expand_path(File.dirname(__FILE__) + "/report-skipped.yml")))
+    r=Report.import yaml
+    assert r.host.puppet_status == 0
   end
+
 end
