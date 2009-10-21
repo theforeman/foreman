244bdd5ac1574533aba82db624c8951ff8e376d1
Ohad Levy <ohadlevy@gmail.com>
2009-10-21 16:53:17 +0800

    switched to rest helpers

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,5 +1,9 @@
 module ReportsHelper
 
+  def host_column(record)
+    link_to record.host.shortname, reports_host_path(record.host)
+  end
+
   def reported_at_column(record)
     if record.error?
       img = "hosts/warning"
@@ -8,7 +12,7 @@ module ReportsHelper
     else
       img = "true"
     end
-    image_tag("#{img}.png") + " " +
-      time_ago_in_words(record.reported_at.getlocal)
+    image_tag("#{img}.png", :size => "18x18") + " " +
+      link_to(time_ago_in_words(record.reported_at.getlocal), report_path(record))
   end
 end
