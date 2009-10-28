44c965001d82256391584c924bb83371941ad039
Ohad Levy <ohadlevy@gmail.com>
2009-10-28 14:43:03 +0800

    added missing index page and added db indexs - ref #30

--- db/migrate/20091022054108_add_status_to_report.rb
+++ db/migrate/20091022054108_add_status_to_report.rb
@@ -2,22 +2,25 @@ class AddStatusToReport < ActiveRecord::Migration
   def self.up
     add_column :reports, :status, :integer
     add_index :reports, :status
+    add_index :reports, :host_id
+    add_index :reports, :reported_at
 
     Report.reset_column_information
     say_with_time "updating Reports records - this may take a long time.. we have #{Report.count} reports to process hold on" do
-      Report.find_each do |r|
+      Report.find_each(:conditions => ["status is ?", nil]) do |r|
         begin
-          r.update_single_attribute(:status, Report.report_status(r.log)) if r.status.nil?
-          #say "updated #{r.host.shortname}: #{r.reported_at}"
+          r.update_single_attribute(:status, Report.report_status(r.log))
         rescue Exception => e
-          say "#{r.id}: #{e}"
+          say "#{r.id}: #{e} - ignoring this report"
         end
       end
     end
   end
 
   def self.down
-#    remove_index :reports, :status
+    remove_index :reports, :status
+    remove_index :reports, :host_id
+    remove_index :reports, :reported_at
     remove_column :reports, :status
   end
 end
