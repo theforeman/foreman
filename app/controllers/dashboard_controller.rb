4bdd27e1b1993fe1afe07434e64921f812287cb5
Ohad Levy <ohadlevy@gmail.com>
2009-11-09 11:49:39 +0800

    fixes #70 - shows hosts in error state in the dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   def index
     @total_hosts = Host.count
     @good_hosts = Host.count(:all, :conditions => @good_reports)
+    @bad_hosts = Host.count(:all, :conditions => @host_conditions)
     @out_of_sync_hosts = Host.count(:all, :conditions => @sync_conditions)
     @intersting_reports = Report.count(:all, :conditions => @report_conditions)
   end
@@ -14,6 +15,7 @@ class DashboardController < ApplicationController
     @sync_conditions = ["last_report < ? or last_report is ?", time, nil]
     @report_conditions = "status > 0"
     @good_reports = ["last_report > ? and puppet_status = ?", time, 0]
+    @host_conditions = ["puppet_status > ?", 0]
   end
 
 end
