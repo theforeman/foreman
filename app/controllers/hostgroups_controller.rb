1cd4a9b62d9f71dd8661cca622b64d9b4f0ac1f7
Ohad Levy <ohadlevy@gmail.com>
2009-11-25 11:45:17 +0800

    fixes #82

--- app/controllers/hostgroups_controller.rb
+++ app/controllers/hostgroups_controller.rb
@@ -4,6 +4,7 @@ class HostgroupsController < ApplicationController
     config.columns = [ :name, :puppetclasses, :group_parameters]
     config.columns[:name].description = "The name of the group"
     config.columns[:puppetclasses].form_ui  = :select
+    config.columns[:puppetclasses].options = { :draggable_lists => {}}
   end
 
 end
