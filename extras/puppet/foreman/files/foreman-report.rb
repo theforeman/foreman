f26126b907f807989c6f7a8350b0597051588362
Frank Sweetser <fs@wpi.edu>
2009-12-01 10:39:22 -0500

    Handle HTTPS report submissions

--- extras/puppet/foreman/files/foreman-report.rb
+++ extras/puppet/foreman/files/foreman-report.rb
@@ -15,9 +15,17 @@ Puppet::Reports.register_report(:foreman) do
 
     def process
       begin
-        Net::HTTP.post_form(URI.parse("#{$foreman_url}/reports/create?format=yml"), {'report'=> to_yaml})
+        uri = URI.parse($foreman_url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        if uri.scheme == 'https' then
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        end
+        req = Net::HTTP::Post.new("/reports/create?format=yml")
+        req.set_form_data({'report' => to_yaml})
+        response = http.request(req)
       rescue Exception => e
-        raise Puppet::Error, "Could not send report to Foreman: #{e}"
+        raise Puppet::Error, "Could not send report to Foreman at #{$foreman_url}/reports/create?format=yml: #{e}"
       end
     end
 end
