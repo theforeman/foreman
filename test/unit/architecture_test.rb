935612ab418c7f29cf983f8360dfd84f96419da2
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2009-12-30 15:56:44 -0200

    refactor

--- test/unit/architecture_test.rb
+++ test/unit/architecture_test.rb
@@ -2,12 +2,12 @@ require 'test_helper'
 
 class ArchitectureTest < ActiveSupport::TestCase
   test "should not save without a name" do
-    a = Architecture.new
-    assert !a.save
+    architecture = Architecture.new
+    assert !architecture.save
   end
 
   test "name should not contain white spaces" do
-    a = Architecture.new :name => "  "
-    assert !a.save
+    architecture = Architecture.new :name => "  "
+    assert !architecture.save
   end
 end
--- test/unit/operatingsystem_test.rb
+++ test/unit/operatingsystem_test.rb
@@ -2,43 +2,43 @@ require 'test_helper'
 
 class OperatingsystemTest < ActiveSupport::TestCase
   test "shouldn't save with blank attributes" do
-    o = Operatingsystem.new
-    assert !o.save
+    operating_system = Operatingsystem.new
+    assert !operating_system.save
   end
 
   test "name shouldn't contain white spaces" do
-    o = Operatingsystem.new :name => "  ", :major => 9
-    assert !o.save
+    operating_system = Operatingsystem.new :name => "  ", :major => 9
+    assert !operating_system.save
   end
 
   test "major should be numeric" do
-    o = Operatingsystem.new :name => "Ubuntu", :major => "nine"
-    assert !o.save
+    operating_system = Operatingsystem.new :name => "Ubuntu", :major => "nine"
+    assert !operating_system.save
   end
 
   test "minor should be numeric" do
-    o = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => "one"
-    assert !o.save
+    operating_system = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => "one"
+    assert !operating_system.save
   end
 
   test "to_label should print correctly" do
-    o = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
-    assert o.save
-    o = Operatingsystem.find_by_name "Ubuntu"
-    assert o.to_label == "Ubuntu 9.10"
+    operating_system = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
+    assert operating_system.save
+    operating_system = Operatingsystem.find_by_name "Ubuntu"
+    assert operating_system.to_label == "Ubuntu 9.10"
   end
 
   test "to_version should print correctly" do
-    o = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
-    assert o.save
-    o = Operatingsystem.find_by_name "Ubuntu"
-    assert o.to_version == "9-10"
+    operating_system = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
+    assert operating_system.save
+    operating_system = Operatingsystem.find_by_name "Ubuntu"
+    assert operating_system.to_version == "9-10"
   end
 
   test "fullname should print correctly" do
-    o = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
-    assert o.save
-    o = Operatingsystem.find_by_name "Ubuntu"
-    assert o.fullname == "Ubuntu_9-10"
+    operating_system = Operatingsystem.new :name => "Ubuntu", :major => 9, :minor => 10
+    assert operating_system.save
+    operating_system = Operatingsystem.find_by_name "Ubuntu"
+    assert operating_system.fullname == "Ubuntu_9-10"
   end
 end
