1e6a9256e3e79a617e2a10f5555d0fc5e28a3479
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-04 10:15:17 -0200

    improved tests and corrected validation

--- app/models/architecture.rb
+++ app/models/architecture.rb
@@ -3,7 +3,7 @@ class Architecture < ActiveRecord::Base
   has_and_belongs_to_many :operatingsystem
   validates_uniqueness_of :name
   before_destroy :ensure_not_used
-  validates_format_of :name, :with => /\S+/, :message => "can't be blank or contain white spaces."
+  validates_format_of :name, :with => /\A(\S+)\Z/, :message => "can't be blank or contain white spaces."
 
   def to_s
     name
--- test/unit/architecture_test.rb
+++ test/unit/architecture_test.rb
@@ -7,7 +7,17 @@ class ArchitectureTest < ActiveSupport::TestCase
   end
 
   test "name should not contain white spaces" do
-    architecture = Architecture.new :name => "  "
+    architecture = Architecture.new :name => " i38  6 "
+    assert !architecture.name.strip.tr(' ', '').empty?
     assert !architecture.save
+
+    architecture = Architecture.new :name => "   "
+    assert architecture.name.strip.tr(' ', '').empty?
+    assert !architecture.save
+  end
+
+  test "to_s retrives name" do
+    architecture = Architecture.new :name => "i386"
+    assert architecture.to_s == architecture.name
   end
 end
