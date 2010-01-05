84245643b6bb9c08b8a0caf08cc1e9028205b89e
Juan Manuel <jmperez@atlantis.(none)>
2010-01-05 14:37:13 -0300

    Added model test (in red).

--- test/unit/model_test.rb
+++ test/unit/model_test.rb
@@ -5,4 +5,25 @@ class ModelTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "should have name" do
+    m = Model.new
+    assert !m.save 
+  end
+  
+  test "name should be unique" do
+    m1 = Model.new :name => "pepe"
+    assert m1.save
+    m2 = Model.new :name => m1.name
+    assert !m2.save
+  end
+  
+  test "should not be used when destroyed" do
+    m = Model.create :name => "m1"
+    
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition", :model => m
+    assert !m.destroy
+  end
 end
