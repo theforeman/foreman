33dc2d1910910db36bb7c59dfdfea3efb25fac9a
Juan Manuel <jmperez@atlantis.(none)>
2010-01-06 11:56:23 -0300

    Added environment test. It's green, but it lacks some tests.

--- test/unit/environment_test.rb
+++ test/unit/environment_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class EnvironmentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "should have name" do
+    env = Environment.new 
+    assert !env.valid?
   end
+  
+  test "name should be unique" do
+    env = Environment.create :name => "foo"
+    env2 = Environment.new :name => env.name
+    assert !env2.valid?
+  end
+  
+  test "name should have no spaces" do
+    env = Environment.new :name => "f o o"
+    assert !env.valid?
+  end
+  
+  #TODO: Test the other methods of the class..
 end
