fa89b47952dbe63ee53b40f5e9b63164914ab7af
Juan Manuel <jmperez@atlantis.(none)>
2010-01-07 15:43:13 -0300

    Added first functional test.

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get settings" do
+    get :settings
+    assert_response :success
   end
+  
 end
