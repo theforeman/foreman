213312e9b406488e344e1532facd6ae2447cd5cb
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-12 10:33:34 -0200

    removed trailing spaces

--- test/unit/environment_test.rb
+++ test/unit/environment_test.rb
@@ -1,22 +1,21 @@
 require 'test_helper'
 
 class EnvironmentTest < ActiveSupport::TestCase
-  
   test "should have name" do
-    env = Environment.new 
+    env = Environment.new
     assert !env.valid?
   end
-  
+
   test "name should be unique" do
     env = Environment.create :name => "foo"
     env2 = Environment.new :name => env.name
     assert !env2.valid?
   end
-  
+
   test "name should have no spaces" do
     env = Environment.new :name => "f o o"
     assert !env.valid?
   end
-  
+
   #TODO: Test the other methods of the class..
 end
