6aba9fc2cad67d3687f2bcea566aeceab89053ef
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-15 15:13:58 -0200

    a bit of refactoring

--- test/functional/auth_sources_controller_test.rb
+++ test/functional/auth_sources_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class AuthSourcesControllerTest < ActionController::TestCase
+  test "ActiveScaffold should look for AuthSourceLdap model" do
+    assert_not_nil AuthSourcesController.active_scaffold_config
+    assert AuthSourcesController.active_scaffold_config.model == AuthSourceLdap
+  end
+
   test "should get index" do
     get :index
     assert_response :success
@@ -14,7 +19,7 @@ class AuthSourcesControllerTest < ActionController::TestCase
 
   test "should create auth_source" do
     assert_difference('AuthSourceLdap.count') do
-      post :create, { :commit => "Create", :record => {"name" => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"} }
+      post :create, { :commit => "Create", :record => {:name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"} }
     end
 
     assert_redirected_to auth_sources_path
@@ -38,7 +43,10 @@ class AuthSourcesControllerTest < ActionController::TestCase
   test "should update auth_source" do
     auth_source = AuthSourceLdap.new :name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"
     assert auth_source.save!
-    put :update, { :commit => "Update", :id => auth_source.id, :record => {"name" => "some_auth_source_but_different_name"} }
+
+    put :update, { :commit => "Update", :id => auth_source.id, :record => {:name => "some_auth_source_but_different_name"} }
+    auth_source = AuthSourceLdap.find_by_id(auth_source.id)
+    assert auth_source.name == "some_auth_source_but_different_name"
 
     assert_redirected_to auth_sources_path
   end
