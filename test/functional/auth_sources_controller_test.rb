98ee81dd383e828efa4c27290a6722b449f17f8a
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-15 11:12:56 -0200

    fixed create and update tests

--- test/functional/auth_sources_controller_test.rb
+++ test/functional/auth_sources_controller_test.rb
@@ -14,7 +14,7 @@ class AuthSourcesControllerTest < ActionController::TestCase
 
   test "should create auth_source" do
     assert_difference('AuthSource.count') do
-      post :create, :auth_source => { }
+      post :create, { :commit => "Create", :record => {"name" => "some_auth_source"} }
     end
 
     assert_redirected_to auth_source_path(assigns(:auth_source))
@@ -31,7 +31,10 @@ class AuthSourcesControllerTest < ActionController::TestCase
   end
 
   test "should update auth_source" do
-    put :update, :id => auth_sources(:one).to_param, :auth_source => { }
+    auth_source = AuthSourceLdap.new :name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"
+    assert auth_source.save!
+    put :update, { :commit => "Update", :id => auth_source.id, :record => {"name" => "some_auth_source_but_different_name"} }
+
     assert_redirected_to auth_source_path(assigns(:auth_source))
   end
 
