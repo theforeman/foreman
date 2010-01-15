ace3d2d4cc78c23d7011b1bfdbc2a3d46529865f
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-15 12:30:00 -0200

    fixed all tests, but it seems not to be a show action, that test is commented.

--- test/functional/auth_sources_controller_test.rb
+++ test/functional/auth_sources_controller_test.rb
@@ -4,7 +4,7 @@ class AuthSourcesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:auth_sources)
+    assert_not_nil assigns(:records)
   end
 
   test "should get new" do
@@ -13,20 +13,25 @@ class AuthSourcesControllerTest < ActionController::TestCase
   end
 
   test "should create auth_source" do
-    assert_difference('AuthSource.count') do
-      post :create, { :commit => "Create", :record => {"name" => "some_auth_source"} }
+    assert_difference('AuthSourceLdap.count') do
+      post :create, { :commit => "Create", :record => {"name" => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"} }
     end
 
-    assert_redirected_to auth_source_path(assigns(:auth_source))
+    assert_redirected_to auth_sources_path
   end
 
-  test "should show auth_source" do
-    get :show, :id => auth_sources(:one).to_param
-    assert_response :success
-  end
+  # TODO: check why there's no show action.
+  # test "should show auth_source" do
+    # auth_source = AuthSourceLdap.new :name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"
+    # assert auth_source.save!
+    # get :show, :id => auth_source.id
+    # assert_response :success
+  # end
 
   test "should get edit" do
-    get :edit, :id => auth_sources(:one).to_param
+    auth_source = AuthSourceLdap.new :name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"
+    assert auth_source.save!
+    get :edit, :id => auth_source.id
     assert_response :success
   end
 
@@ -35,12 +40,14 @@ class AuthSourcesControllerTest < ActionController::TestCase
     assert auth_source.save!
     put :update, { :commit => "Update", :id => auth_source.id, :record => {"name" => "some_auth_source_but_different_name"} }
 
-    assert_redirected_to auth_source_path(assigns(:auth_source))
+    assert_redirected_to auth_sources_path
   end
 
   test "should destroy auth_source" do
+    auth_source = AuthSourceLdap.new :name => "some_auth_source", :port => "3890", :attr_login => "Chuck_Norris", :host => "vurbia"
+    assert auth_source.save!
     assert_difference('AuthSource.count', -1) do
-      delete :destroy, :id => auth_sources(:one).to_param
+      delete :destroy, :id => auth_source.id
     end
 
     assert_redirected_to auth_sources_path
