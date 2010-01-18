a87949465019d98ba8a7566316684c96d3435728
José Luis Escalante <joeloui@ubuntuJoe.(none)>
2010-01-18 14:30:28 -0300

    added the domain_controller_test

--- test/functional/domain_controller_test.rb
+++ test/functional/domain_controller_test.rb
@@ -1,8 +1,59 @@
 require 'test_helper'
 
-class DomainControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class DomainsControllerTest < ActionController::TestCase
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:records)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create domain" do
+    assert_difference('Domain.count') do
+      post :create, { :commit => "Create", :record => { :name => "my_domain" } }
+    end
+
+    assert_redirected_to domains_path
+  end
+
+   test "should show domain" do
+    domain = Domain.create :name => "my_domain"
+    assert domain.save!
+
+    get :show, :id => domain.id
+    assert_response :success
+   end
+
+  test "should get edit" do
+    domain = Domain.create :name => "my_domain"
+    assert domain.save!
+    get :edit, :id => domain.id
+    assert_response :success
+  end
+
+  test "should update domain" do
+    domain = Domain.create :name => "my_domain"
+    assert domain.save!
+
+    put :update, { :commit => "Update", :id => domain.id, :record => {:name => "our_domain"} }
+    domain = Domain.find_by_id(domain.id)
+    assert domain.name == "our_domain"
+
+    assert_redirected_to domains_path
+  end
+
+  test "should destroy domain" do
+    domain = Domain.create :name => "my_domain"
+    assert domain.save!
+    assert_difference('Domain.count', -1) do
+      delete :destroy, :id => domain.id
+    end
+
+    assert_redirected_to domains_path
   end
 end
+
