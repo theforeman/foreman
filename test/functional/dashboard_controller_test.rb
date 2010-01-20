848a8560f66009196c0bbd3ecd9475a2a2ef06bd
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-20 14:19:44 -0200

    added dashboard_controller_tests on green

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -1,8 +1,26 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get index" do
+    get :index
+    assert_response :success
+  end
+
+  test "should get errors" do
+    get :errors
+    assert_response :success
+    assert_template :partial => "hosts/_minilist"
+  end
+
+  test "should get active" do
+    get :active
+    assert_response :success
+    assert_template :partial => "hosts/_minilist"
+  end
+
+  test "should get out of sync" do
+    get :OutOfSync
+    assert_response :success
+    assert_template :partial => "hosts/_minilist"
   end
 end
