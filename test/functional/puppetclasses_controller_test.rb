c65af316c0420452e51f59ff27b3cad745154b07
José Luis Escalante <joeloui@ubuntuJoe.(none)>
2010-01-27 17:10:49 -0300

    reports controller test is stand-by. puppetclasses controller test finished.

--- test/functional/puppetclasses_controller_test.rb
+++ test/functional/puppetclasses_controller_test.rb
@@ -1,8 +1,58 @@
 require 'test_helper'
 
 class PuppetclassesControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "ActiveScaffold should look for Puppetclass model" do
+    assert_not_nil PuppetclassesController.active_scaffold_config
+    assert PuppetclassesController.active_scaffold_config.model == Puppetclass
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:records)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create new puppetclass" do
+    assert_difference 'Puppetclass.count' do
+      post :create, { :commit => "Create", :record => {:name => "my_puppetclass"} }
+    end
+
+    assert_redirected_to puppetclasses_path
+  end
+
+  test "should get edit" do
+    puppetclass = Puppetclass.new :name => "my_puppetclass"
+    assert puppetclass.save!
+
+    get :edit, :id => puppetclass.id
+    assert_response :success
+  end
+
+  test "should update puppetclass" do
+    puppetclass = Puppetclass.new :name => "my_puppetclass"
+    assert puppetclass.save!
+
+    put :update, { :commit => "Update", :id => puppetclass.id, :record => {:name => "my_other_puppetclass"} }
+    puppetclass = Puppetclass.find_by_id(puppetclass.id)
+    assert puppetclass.name == "my_other_puppetclass"
+
+    assert_redirected_to puppetclasses_path
+  end
+
+  test "should destroy puppetclass" do
+    puppetclass = Puppetclass.new :name => "my_puppetclass"
+    assert puppetclass.save!
+
+    assert_difference('Puppetclass.count', -1) do
+      delete :destroy, :id => puppetclass.id
+    end
+
+    assert_redirected_to puppetclasses_path
   end
 end
+
--- test/functional/reports_controller_test.rb
+++ test/functional/reports_controller_test.rb
@@ -12,11 +12,6 @@ class ReportsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:records)
   end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
   test "should create report" do
     h = Host.create  :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
                       :domain => Domain.find_or_create_by_name("company.com"),
@@ -26,7 +21,7 @@ class ReportsControllerTest < ActionController::TestCase
                       :disk => "empty partition"
 
     p = Puppet::Transaction::Report.new
-    p.logs = "willWork"
+    p.logs << Logger.new("willWork")
     p.save
 
     d = Date.today
@@ -44,29 +39,6 @@ class ReportsControllerTest < ActionController::TestCase
 
     get :show, :id => @report.id
     assert_response :success
-  end
-
-  test "should get edit" do
-    create_a_report
-    assert @report.save!
-
-    get :edit, :id => @report.id
-    assert_response :success
-  end
-
-  test "should update report" do
-    create_a_report
-    assert @report.save!
-
-    q = Puppet::Transaction::Report.new
-    q.logs = "lolo"
-    q.save
-
-    put :update, { :commit => "Update", :id => @report.id, :record => {:log => q} }
-    report = Report.find_by_id(@report.id)
-    assert report.log == q
-
-    assert_redirected_to reports_path
   end
 
   test "should destroy report" do
@@ -89,7 +61,7 @@ class ReportsControllerTest < ActionController::TestCase
                       :disk => "empty partition"
 
     p = Puppet::Transaction::Report.new
-    p.logs = "willWork"
+    p.logs << Logger.new("lalala")
     p.save
 
     @report = Report.create :host => h, :log => p, :reported_at => Date.new
