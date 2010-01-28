ea15e86609cb379e85cc2e7321f34c3ab80ad37c
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-01-28 10:45:07 -0200

    removed trailing spaces in user_test and the initial "Replace this with..." comment

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -6,23 +6,23 @@ class UserTest < ActiveSupport::TestCase
     u = User.new :mail => "foo@bar.com"
     assert !u.save
   end
-  
+
   test "should have mail" do
     u = User.new :login => "foo"
-    assert !u.save  
+    assert !u.save
   end
-  
+
   test "login should be unique" do
     u = User.create :login => "foo", :mail => "foo@bar.com"
     u2 = User.new :login => u.login, :mail => u.mail
-    
+
     assert !u2.valid?
   end
-  
+
   test "mail should have format" do
     u = User.create :login => "foo", :mail => "bar"
     assert !u.valid?
   end
-  
+
   # TODO; Authentication should be tested too.
 end
