cd0603452a3a3d37177014741fb4da82b3fe3bc4
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-02-05 14:55:37 -0200

    added more tests to hosts_controller_test

--- test/functional/hosts_controller_test.rb
+++ test/functional/hosts_controller_test.rb
@@ -47,4 +47,95 @@ class HostsControllerTest < ActionController::TestCase
       delete :destroy, :id => host.id
     end
   end
+
+  test "externalNodes should render 404 when no params are given" do
+    get :externalNodes
+    assert_response :missing
+    assert_template :text => '404 Not Found'
+  end
+
+  test "externalNodes should render correctly when id is given" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+
+    get :externalNodes, :id => host.id
+    assert_response :success
+    assert_template :text => host.info.to_yaml.gsub("\n","<br>")
+  end
+
+  test "externalNodes should render correctly when name is given" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+
+    get :externalNodes, :name => host.name
+    assert_response :success
+    assert_template :text => host.info.to_yaml.gsub("\n","<br>")
+  end
+
+  test "externalNodes should render yml request correctly" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+
+    get :externalNodes, :id => host.id, :format => "yml"
+    assert_response :success
+    assert_template :text => host.info.to_yaml
+  end
+
+  test "when host is saved after setBuild, the flash should informe it" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+    stub(host).setBuild {true}
+    @request.env['HTTP_REFERER'] = hosts_path
+
+    get :setBuild, :id => host.id
+    assert_response :found
+    assert_redirected_to hosts_path
+    # TODO: test flash content according to host.setBuild
+  end
+
+  test "rrdreport should print error message if host has no last_report" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+
+    get :rrdreport, :id => host.id
+    assert_response :success
+    assert_template :text => "Sorry, no graphs for this host"
+  end
+
+  test "rrdreport should render graphics" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition",
+      :last_report => Date.today
+    SETTINGS[:rrd_report_url] = "/some/url"
+
+    get :rrdreport, :id => host.id
+    assert_response :success
+    assert_template :partial => "_rrdreport"
+  end
+
+  test "report should redirect to host's last report" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition",
+      :last_report => Date.today
+
+    get :report, :id => host.id
+    assert_response :found
+    assert_redirected_to :controller => "reports", :action => "show", :id => host.id
+  end
+
+  test "query in .yml format should return host.to_yml" do
+    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+
+    get :query, :format => "yml"
+    assert_template :text => host.to_yaml
+  end
 end
