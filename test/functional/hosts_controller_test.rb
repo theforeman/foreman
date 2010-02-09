d91160eaed349e7d4c613dd1bf7efa4b21ed1b0c
Lucas Tolchinsky <ltolchinsky@vurbiatechnologies.com>
2010-02-09 15:06:23 -0200

    refactored hosts_controller_test

--- test/functional/hosts_controller_test.rb
+++ test/functional/hosts_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class HostsControllerTest < ActionController::TestCase
+  setup :initialize_host
+
   test "should get index" do
     get :index
     assert_response :success
@@ -13,38 +15,35 @@ class HostsControllerTest < ActionController::TestCase
 
   test "should create new host" do
     assert_difference 'Host.count' do
-      post :create, { :commit => "Create", :record => {:name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => {:id => Domain.find_or_create_by_name("company.com").id.to_s}, :operatingsystem => {:id => Operatingsystem.first.id.to_s}, :architecture => {:id => Architecture.first.id.to_s}, :environment => {:id => Environment.first.id.to_s}, :disk => "empty partition"} }
+      post :create, { :commit => "Create",
+        :record => {:name => "myotherfullhost",
+          :mac => "aabbecddee00",
+          :ip => "123.05.02.25",
+          :domain => {:id => Domain.find_or_create_by_name("othercompany.com").id.to_s},
+          :operatingsystem => {:id => Operatingsystem.first.id.to_s},
+          :architecture => {:id => Architecture.first.id.to_s},
+          :environment => {:id => Environment.first.id.to_s},
+          :disk => "empty partition"
+        }
+      }
     end
   end
 
   test "should get edit" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
-    get :edit, :id => host.id
+    get :edit, :id => @host.id
     assert_response :success
   end
 
   test "should update host" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
+    put :update, { :commit => "Update", :id => @host.id, :record => {:disk => "ntfs"} }
+    @host = Host.find_by_id(@host.id)
 
-    put :update, { :commit => "Update", :id => host.id, :record => {:disk => "ntfs"} }
-    host2 = Host.find_by_id(host.id)
-
-    assert host2.disk == "ntfs"
+    assert @host.disk == "ntfs"
   end
 
-  test "should destroy architecture" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
+  test "should destroy host" do
     assert_difference('Host.count', -1) do
-      delete :destroy, :id => host.id
+      delete :destroy, :id => @host.id
     end
   end
 
@@ -55,44 +54,29 @@ class HostsControllerTest < ActionController::TestCase
   end
 
   test "externalNodes should render correctly when id is given" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
-    get :externalNodes, :id => host.id
+    get :externalNodes, :id => @host.id
     assert_response :success
-    assert_template :text => host.info.to_yaml.gsub("\n","<br>")
+    assert_template :text => @host.info.to_yaml.gsub("\n","<br>")
   end
 
   test "externalNodes should render correctly when name is given" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
-    get :externalNodes, :name => host.name
+    get :externalNodes, :name => @host.name
     assert_response :success
-    assert_template :text => host.info.to_yaml.gsub("\n","<br>")
+    assert_template :text => @host.info.to_yaml.gsub("\n","<br>")
   end
 
   test "externalNodes should render yml request correctly" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
-    get :externalNodes, :id => host.id, :format => "yml"
+    get :externalNodes, :id => @host.id, :format => "yml"
     assert_response :success
-    assert_template :text => host.info.to_yaml
+    assert_template :text => @host.info.to_yaml
   end
 
   test "when host is saved after setBuild, the flash should informe it" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-    mock(host).setBuild {true}
-    mock(Host).find(host.id.to_s) {host}
+    mock(@host).setBuild {true}
+    mock(Host).find(@host.id.to_s) {@host}
     @request.env['HTTP_REFERER'] = hosts_path
 
-    get :setBuild, {:id => host.id}
+    get :setBuild, {:id => @host.id}
     assert_response :found
     assert_redirected_to hosts_path
     assert_not_nil flash[:foreman_notice]
@@ -100,12 +84,9 @@ class HostsControllerTest < ActionController::TestCase
   end
 
   test "when host is not saved after setBuild, the flash should informe it" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
     @request.env['HTTP_REFERER'] = hosts_path
 
-    get :setBuild, :id => host.id
+    get :setBuild, :id => @host.id
     assert_response :found
     assert_redirected_to hosts_path
     assert_not_nil flash[:foreman_error]
@@ -113,44 +94,41 @@ class HostsControllerTest < ActionController::TestCase
   end
 
   test "rrdreport should print error message if host has no last_report" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
-    get :rrdreport, :id => host.id
+    get :rrdreport, :id => @host.id
     assert_response :success
     assert_template :text => "Sorry, no graphs for this host"
   end
 
   test "rrdreport should render graphics" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition",
-      :last_report => Date.today
+    @host.last_report = Date.today
+    assert @host.save!
     SETTINGS[:rrd_report_url] = "/some/url"
 
-    get :rrdreport, :id => host.id
+    get :rrdreport, :id => @host.id
     assert_response :success
     assert_template :partial => "_rrdreport"
   end
 
   test "report should redirect to host's last report" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition",
-      :last_report => Date.today
-
-    get :report, :id => host.id
+    get :report, :id => @host.id
     assert_response :found
-    assert_redirected_to :controller => "reports", :action => "show", :id => host.id
+    assert_redirected_to :controller => "reports", :action => "show", :id => @host.id
   end
 
   test "query in .yml format should return host.to_yml" do
-    host = Host.create :name => "myfullhost", :mac => "aabbecddeeff", :ip => "123.05.02.03",
-      :domain => Domain.find_or_create_by_name("company.com"), :operatingsystem => Operatingsystem.first,
-      :architecture => Architecture.first, :environment => Environment.first, :disk => "empty partition"
-
     get :query, :format => "yml"
-    assert_template :text => host.to_yaml
+    assert_template :text => @host.to_yaml
+  end
+
+  private
+  def initialize_host
+    @host = Host.create :name => "myfullhost",
+      :mac => "aabbecddeeff",
+      :ip => "123.05.02.03",
+      :domain => Domain.find_or_create_by_name("company.com"),
+      :operatingsystem => Operatingsystem.first,
+      :architecture => Architecture.first,
+      :environment => Environment.first,
+      :disk => "empty partition"
   end
 end
