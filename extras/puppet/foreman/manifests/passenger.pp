ca0df7e1e9a83aa076a33d7d17ae6d8c199da6cf
Ohad Levy <ohadlevy@gmail.com>
2010-02-19 13:10:50 +0200

    minor fixes to foreman module, added basic support for Ubuntu with passenger

--- extras/puppet/foreman/manifests/passenger.pp
+++ extras/puppet/foreman/manifests/passenger.pp
@@ -1,7 +1,11 @@
 class foreman::passenger {
   include apache2::passenger
 
-  file{"/etc/httpd/conf.d/foreman.conf":
+  file{"foreman_vhost":
+    path => $lsbdistid ? {
+      default => "/etc/httpd/conf.d/foreman.conf",
+      "Ubuntu" => "/etc/apache2/conf.d/foreman.conf"
+    },
     content => template("foreman/foreman-vhost.conf.erb"),
     mode => 644, notify => Exec["reload-apache2"],
   }
