040608d03829740e9e4e756d635ffe7f8d296053
Ohad Levy <ohadlevy@gmail.com>
2010-03-09 15:12:17 +0800

    fixes broken testfixtures/lookup_keys.yml

--- test/functional/lookup_keys_controller_test.rb
+++ test/functional/lookup_keys_controller_test.rb
@@ -4,7 +4,7 @@ class LookupKeysControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:lookup_keys)
+    assert_not_nil assigns(:lookup_key)
   end
 
   test "should get new" do
@@ -13,8 +13,8 @@ class LookupKeysControllerTest < ActionController::TestCase
   end
 
   test "should create lookup_keys" do
-    assert_difference('LookupKeys.count') do
-      post :create, :lookup_keys => { }
+    assert_difference('LookupKey.count') do
+      post :create, :lookup_key=>{"value_attributes"=>[{"priority"=>"x", "value"=>"v1"}, {"priority"=>"y", "value"=>"v2"}], "key"=>"test it"}
     end
 
     assert_redirected_to lookup_keys_path(assigns(:lookup_keys))
@@ -31,12 +31,12 @@ class LookupKeysControllerTest < ActionController::TestCase
   end
 
   test "should update lookup_keys" do
-    put :update, :id => lookup_keys(:one).to_param, :lookup_keys => { }
+    put :update, :id => lookup_keys(:one).to_param, :lookup_key => { :key => "test that" }
     assert_redirected_to lookup_keys_path(assigns(:lookup_keys))
   end
 
   test "should destroy lookup_keys" do
-    assert_difference('LookupKeys.count', -1) do
+    assert_difference('LookupKey.count', -1) do
       delete :destroy, :id => lookup_keys(:one).to_param
     end
 
