f87f1250565aef41b0eedf32474bc3c59a663277
Ohad Levy <ohadlevy@gmail.com>
2010-04-19 10:17:26 +0800

    added links to all facts and switched to url helpers

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ ActionController::Routing::Routes.draw do |map|
                 :member => {:report => :get, :reports => :get, :facts => :get},
                 :collection => { :show_search => :get},
                 :active_scaffold => true
+  map.dashboard '/dashboard', :controller => 'dashboard'
+  map.audit '/audit', :controller => 'audit'
+  map.statistics '/statistics', :controller => 'statistics'
+  map.settings '/settings', :controller => 'home', :action => 'settings'
   map.connect   "/reports/expire_reports", :controller => "reports", :action => "expire_reports"
   map.connect   "/reports/expire_good_reports", :controller => "reports", :action => "expire_good_reports"
   map.resources :reports, :active_scaffold => true
@@ -23,7 +27,8 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :hostgroups, :active_scaffold => true
   map.resources :common_parameters
   map.resources :environments, :active_scaffold => true
-  map.resources :subnets,          :active_scaffold => true
+  map.resources :subnets, :active_scaffold => true
+  map.resources :fact_values, :active_scaffold => true
   map.resources :ptables
   map.resources :auth_source_ldaps
   map.login '/login', :controller => 'users', :action => 'login'
