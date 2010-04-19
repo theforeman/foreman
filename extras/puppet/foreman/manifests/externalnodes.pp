3a3a042ea698542c9667d66f599945913edefa44
Ohad Levy <ohadlevy@gmail.com>
2010-04-19 14:37:37 +0800

    removing puppet module from Foreman repo, as it now has its own repo - http://github.com/ohadlevy/puppet-foreman

--- extras/puppet/foreman/manifests/externalnodes.pp
+++
@@ -1,8 +0,0 @@
-class foreman::externalnodes {
-  file{"/etc/puppet/node.rb":
-    source => "puppet:///foreman/external_node.rb",
-    mode   => 555,
-    owner  => "puppet", group => "puppet",
-  }
-  
-}
