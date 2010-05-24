9f7fadef4ba7e611e63b02ef3a356bf47823ac7f
Jochen Schalanda <jochen.schalanda@detasad.com.sa>
2010-05-24 08:37:05 +0000

    Fixes #266 - Migrate the (NFS-) paths of existing Installation Media to URI format.

---
+++ db/migrate/20100524080302_migrate_installation_media_uri.rb
@@ -0,0 +1,15 @@
+class MigrateInstallationMediaUri < ActiveRecord::Migration
+  def self.up
+    Media.all.each { |medium|
+      matches = /^([^:]+):(\/.+)/.match(medium.path)
+
+      if matches.size == 3 and ![ 'http', 'https', 'ftp', 'ftps', 'nfs' ].include?(matches[1])
+        medium.path = 'nfs://' + matches[1] + matches[2]
+        medium.save
+      end
+    }
+  end
+
+  def self.down
+  end
+end
