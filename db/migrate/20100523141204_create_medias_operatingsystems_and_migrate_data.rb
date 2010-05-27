0bba6aae5598b368fd99a3c33f20d602e78261a5
Ohad Levy <ohadlevy@gmail.com>
2010-05-27 10:41:19 +0800

    migration might fail if medium does not have an opeartingsystem - refs #236

--- db/migrate/20100523141204_create_medias_operatingsystems_and_migrate_data.rb
+++ db/migrate/20100523141204_create_medias_operatingsystems_and_migrate_data.rb
@@ -1,16 +1,21 @@
 class CreateMediasOperatingsystemsAndMigrateData < ActiveRecord::Migration
   def self.up
-    create_table :medias_operatingsystems , :id => false do |t|
-      t.references :media, :null => false
-      t.references :operatingsystem, :null => false
-    end
 
     media_hash = Hash.new
     Media.all.each do |medium|
       unless medium.operatingsystem_id.nil?
-        os = Operatingsystem.find(medium.operatingsystem_id)
-        media_hash[os] = medium
+        if Operatingsystem.exists?(medium.operatingsystem_id)
+          os = Operatingsystem.find(medium.operatingsystem_id)
+          media_hash[os] = medium
+        else
+          say "skipped #{medium}"
+        end
       end
+    end
+
+    create_table :medias_operatingsystems , :id => false do |t|
+      t.references :media, :null => false
+      t.references :operatingsystem, :null => false
     end
 
     media_hash.keys.each { |os| os.medias << media_hash[os] }
