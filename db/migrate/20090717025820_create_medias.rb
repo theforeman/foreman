32119972fa20e37a5d0da2278e6e3a48004d0176
Ohad Levy <ohadlevy@gmail.com>
2010-05-28 00:29:33 +0800

    fixes #280 - sample data should include newer operating systems

--- db/migrate/20090717025820_create_medias.rb
+++ db/migrate/20090717025820_create_medias.rb
@@ -6,8 +6,11 @@ class CreateMedias < ActiveRecord::Migration
       t.references :operatingsystem
       t.timestamps
     end
-    Media.create :name => "CentOS 5 mirror", :path => "http://mirror.averse.net/centos/5.3/os/$arch"
-    Media.create :name => "Fedora 11 Mirror", :path => "http://mirror.nus.edu.sg/fedora/releases/11/Fedora/$arch/os/"
+    Media.create :name => "CentOS mirror", :path => "http://mirror.averse.net/centos/$major.$minor/os/$arch"
+    Media.create :name => "Fedora Mirror", :path => "http://mirror.nus.edu.sg/fedora/releases/$major/Fedora/$arch/os/"
+    Media.create :name => "RedHat Beta", :path => "http://ftp.redhat.com/pub/redhat/rhel/beta/$major/$arch/os"
+    Media.create :name => "Ubuntu Mirror", :path => "http://sg.archive.ubuntu.com"
+
   end
 
   def self.down
