b0e2071ac54b3a08f1f8a6872819e3d61b994e69
Ohad Levy <ohadlevy@gmail.com>
2010-05-28 22:54:08 +0800

    minor formating changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@ module ApplicationHelper
   end
 
   def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+    f.hidden_field(:_destroy) + link_to_function(image_tag("false.png", :title => "remove"), "remove_fields(this)")
   end
 
   def link_to_add_fields(name, f, association, partial = nil)
