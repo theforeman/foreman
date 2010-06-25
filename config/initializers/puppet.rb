5cd149934cd6cf09d98e3dbd5de8c1bcae85cf65
Ohad Levy <ohadlevy@gmail.com>
2010-06-25 14:15:59 +0800

    fixes #329 - trying to recover from serialization side effects

--- config/initializers/puppet.rb
+++ config/initializers/puppet.rb
@@ -6,3 +6,12 @@ class ResourceTag < Puppet::Rails::ResourceTag
 end
 class ParamValue < Puppet::Rails::ParamValue
 end
+
+# workaround for puppet bug http://projects.reductivelabs.com/issues/3949
+if Facter.puppetversion == "0.25.5"
+  begin
+    require 'RRDTool'
+  rescue
+    nil
+  end
+end
