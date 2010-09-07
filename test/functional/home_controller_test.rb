d3156019b320e96f7840a66b9f5d3c755638eb64
Paul Kelly <pikelly@blueyonder.co.uk>
2010-09-07 07:25:42 +0100

    Fixes #294 Convert settings to a dropdown

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,4 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get settings" do
-    get :settings, {}, set_session_user
-    assert_response :success
-  end
 end
