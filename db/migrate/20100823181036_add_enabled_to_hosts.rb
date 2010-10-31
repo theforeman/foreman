83940766ad6212352905366531f2e3844f26c3d7
Jochen Schalanda <jochen@schalanda.name>
2010-10-31 14:07:58 +0000

    Fixes #421 - change literal 1 to true in database migration AddEnabledToHosts.

--- db/migrate/20100823181036_add_enabled_to_hosts.rb
+++ db/migrate/20100823181036_add_enabled_to_hosts.rb
@@ -1,6 +1,6 @@
 class AddEnabledToHosts < ActiveRecord::Migration
   def self.up
-    add_column :hosts, :enabled, :boolean, :default => 1
+    add_column :hosts, :enabled, :boolean, :default => true
   end
 
   def self.down
