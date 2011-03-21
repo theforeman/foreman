78bda5491f12e06e27b824618c86b65ce5b4cfa9
Ohad Levy <ohadlevy@gmail.com>
2011-03-21 15:39:28 +0200

    fixes #495 - ensure we handle the correct index if there is more than one

--- db/migrate/20110321070954_revert_face_names_and_values_to_text_records.rb
+++ db/migrate/20110321070954_revert_face_names_and_values_to_text_records.rb
@@ -1,14 +1,13 @@
 class RevertFaceNamesAndValuesToTextRecords < ActiveRecord::Migration
   def self.up
     if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
-      index = ActiveRecord::Base.connection.indexes("fact_names").empty?
+      index = ActiveRecord::Base.connection.indexes("fact_names").map(&:columns).flatten.include?("name") rescue false
 
-      remove_index "fact_names", ActiveRecord::Base.connection.indexes("fact_names").map(&:columns).to_s if index
+      remove_index :fact_names, :name if index
 
       execute "ALTER TABLE fact_names MODIFY name text COLLATE utf8_bin NOT NULL;"
       execute "ALTER TABLE fact_values MODIFY value text COLLATE utf8_bin NOT NULL;"
-
-      add_index "fact_names", "name" if index
+      execute "CREATE INDEX `index_fact_names_on_name` ON `fact_names` (name(255))" if index
     end
   end
 
