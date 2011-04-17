e5179374285b44cc1b2d2bbb8adaf7d9545d6387
Amos Benari <abenari@redhat.com>
2011-04-17 04:21:00 -0400

    Improved usability of the search suggestion list - fixed #850 when writing a long query such as "a=b and c=" it will gray-out the completed part (a=b) this is done to help the user focus on the part that he is writing.
    Signed-off-by: Amos Benari <abenari@redhat.com>

--- lib/foreman/controller/auto_complete_search.rb
+++ lib/foreman/controller/auto_complete_search.rb
@@ -4,7 +4,9 @@ module Foreman::Controller::AutoCompleteSearch
       @items = eval(controller_name.singularize.camelize).complete_for(params[:search])
       @items = @items.map do |item|
         category = (['and','or','not','has'].include?(item.to_s.sub(/^.*\s+/,''))) ? 'Operators' : ''
-        {:label => item, :category => category}
+        part = item.to_s.sub(/^.*\b(and|or)\b/i) {|match| match.sub(/^.*\s+/,'')}
+        completed = item.to_s.chomp(part)
+        {:completed => completed, :part => part, :label => item, :category => category}
       end
     rescue ScopedSearch::QueryNotSupported => e
       @items = [{:error =>e.to_s}]
