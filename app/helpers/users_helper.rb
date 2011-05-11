26df2a7e068b098b064b0a1708cece0c51718b50
Paul Kelly <paul.ian.kelly@googlemail.com>
2011-05-11 11:17:50 +0200

    Fixes #458 - Long host list in edit user
    Signed-off-by: Paul Kelly <paul.ian.kelly@googlemail.com>

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,23 @@ module UsersHelper
   def auth_source_column record
     record.auth_source.to_label if record.auth_source
   end
+
+  def contracted_host_list user
+    content_tag(:span, :id => "contracted_host_list", :style => "display:inline;") do
+      link_to_function("#{user.hosts[0..20].join(", ")}#{"..." if user.hosts.size > 20}") do |page|
+        page[:contracted_host_list].hide
+        page[:expanded_host_list].show
+      end
+    end
+  end
+
+  def expanded_host_list user
+    content_tag(:span, :id => "expanded_host_list", :style => "display:none;") do
+      link_to_function(user.hosts.to_sentence) do |page|
+        page[:contracted_host_list].show
+        page[:expanded_host_list].hide
+      end
+    end
+  end
+
 end
