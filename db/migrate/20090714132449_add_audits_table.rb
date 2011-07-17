473f33e9e881c7a8d5fc2fe5370c630ab91c9d19
Ohad Levy <ohadlevy@gmail.com>
2011-07-17 18:54:42 +0300

    fixes #1049 - Ensures that all audit db fields are in place prior to first audit

--- db/migrate/20090714132449_add_audits_table.rb
+++ db/migrate/20090714132449_add_audits_table.rb
@@ -9,12 +9,16 @@ class AddAuditsTable < ActiveRecord::Migration
       t.column :action, :string
       t.column :changes, :text
       t.column :version, :integer, :default => 0
+      t.column :comment, :string
+      t.column :auditable_parent_id, :integer
+      t.column :auditable_parent_type, :string
       t.column :created_at, :datetime
     end
-    
+
     add_index :audits, [:auditable_id, :auditable_type], :name => 'auditable_index'
     add_index :audits, [:user_id, :user_type], :name => 'user_index'
-    add_index :audits, :created_at  
+    add_index :audits, :created_at
+    add_index :audits, [:auditable_parent_id, :auditable_parent_type], :name => 'auditable_parent_index'
   end
 
   def self.down
--- db/migrate/20110712064120_update_audits_table.rb
+++ db/migrate/20110712064120_update_audits_table.rb
@@ -1,9 +1,11 @@
 class UpdateAuditsTable < ActiveRecord::Migration
   def self.up
-    add_column :audits, :comment, :string
-    add_column :audits, :auditable_parent_id, :integer
-    add_column :audits, :auditable_parent_type, :string
-    add_index :audits, [:auditable_parent_id, :auditable_parent_type], :name => 'auditable_parent_index'
+    unless Audit.column_names.include?("comment")
+      add_column :audits, :comment, :string
+      add_column :audits, :auditable_parent_id, :integer
+      add_column :audits, :auditable_parent_type, :string
+      add_index :audits, [:auditable_parent_id, :auditable_parent_type], :name => 'auditable_parent_index'
+    end
   end
 
   def self.down
