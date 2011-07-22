0e0394777fbedb3c4c723a86b4c2747ec64642db
Paul Kelly <paul.ian.kelly@googlemail.com>
2011-07-22 12:16:18 +0200

    Fixes 1052 - Error when selecting medium
    Signed-off-by: Paul Kelly <paul.ian.kelly@googlemail.com>

--- app/helpers/hosts_and_hostgroups_helper.rb
+++ app/helpers/hosts_and_hostgroups_helper.rb
@@ -52,7 +52,7 @@ module HostsAndHostgroupsHelper
   def image_file_entry item
     # If the host has an explicit image_path then use that
     # Else use the default based upon the host's medium and operatingsystem
-    value = item.image_file || item.default_image_file
+    value = item.use_image and item.image_file || item.default_image_file
     text_field :item, :image_file, :value => value, :disabled => !item.use_image,
                  :id => type + "_image_file", :name => type + "[image_file]", :class => "span-14 last"
   end
