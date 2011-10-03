dc11280f19740d7a6510c9c8450f3b78e794a2ee
Ohad Levy <ohadlevy@gmail.com>
2011-10-03 14:56:01 +0200

    fixes #1199 -  hosts count in puppetclass page always shows 0
    Note that this fix still ignores classes assgined via hostgroups.

--- app/helpers/puppetclasses_helper.rb
+++ app/helpers/puppetclasses_helper.rb
@@ -9,4 +9,12 @@ module PuppetclassesHelper
       page.insert_html(:after, "puppetdoc",link_to(:back))
     end
   end
+
+  def host_counter klass
+    # workaround for sqlite bug
+    # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/4544-rails3-activerecord-sqlite3-lost-column-type-when-using-views#ticket-4544-2
+    @counter[klass.id.to_s] || @counter[klass.id.to_i] || 0
+  rescue
+    "N/A"
+  end
 end
