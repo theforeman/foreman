6166c80262c4d7d1ab4c3832713bb02d0cf59d7f
Ohad Levy <ohadlevy@gmail.com>
2011-10-03 16:24:03 +0200

    fixes #1063 - can't deploy default pxe menu

--- app/models/operatingsystem.rb
+++ app/models/operatingsystem.rb
@@ -45,7 +45,7 @@ class Operatingsystem < ActiveRecord::Base
 
 
   class Jail < Safemode::Jail
-    allow :name, :media_url, :major, :minor, :family, :to_s, :epel, :==, :release_name, :kernel, :initrd
+    allow :name, :media_url, :major, :minor, :family, :to_s, :epel, :==, :release_name, :kernel, :initrd, :pxe_type
   end
 
   # As Rails loads an object it casts it to the class in the 'type' field. If we ensure that the type and
