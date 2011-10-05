297fca2419790991ae1feeda4788f83f9cf490f1
Ohad Levy <ohadlevy@gmail.com>
2011-10-05 14:47:57 +0200

    refs #1211 - ENC url is name, not id.

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -16,7 +16,7 @@ class HostsController < ApplicationController
     :multiple_enable, :multiple_disable, :submit_multiple_disable, :submit_multiple_enable, :update_multiple_hostgroup,
     :update_multiple_environment, :submit_multiple_build, :submit_multiple_destroy]
   before_filter :find_by_name, :only => %w[show edit update destroy puppetrun setBuild cancelBuild report
-    reports facts storeconfig_klasses clone externalNodes pxe_config toggle_manage]
+    reports facts storeconfig_klasses clone pxe_config toggle_manage]
 
   filter_parameter_logging :root_pass
   helper :hosts, :reports
