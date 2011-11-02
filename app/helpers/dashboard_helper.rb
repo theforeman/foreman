df098e26312c301ca1669f612dec20037d4d2711
Amos Benari <abenari@redhat.com>
2011-11-02 10:41:47 +0200

    set consistent color array for the dashboard pie.

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -15,11 +15,11 @@ module DashboardHelper
   end
 
   def render_overview report, options = {}
-    data = {:Active =>    report[:active_hosts],
-            :Error  =>    report[:bad_hosts],
-            :OK     =>    report[:ok_hosts],
-            :'Out of sync' => report[:out_of_sync_hosts],
-            :'No report'   => report[:reports_missing]}
+    data = [[:Active,    report[:active_hosts]],
+            [:Error, report[:bad_hosts]],
+            [:OK, report[:ok_hosts]],
+            [:'Out of sync', report[:out_of_sync_hosts]],
+            [:'No report', report[:reports_missing]]]
     pie_chart 'overview', 'Puppet Clients Activity Overview', data, options
   end
 
