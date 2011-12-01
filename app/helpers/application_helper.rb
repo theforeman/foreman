fdac5d155427bb337cb6e38e859cd4e808f0c923
Amos Benari <abenari@redhat.com>
2011-12-01 14:12:48 +0200

    fixes #1304 re issue the fix as it seemed to get only half merged

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -63,7 +63,7 @@ module ApplicationHelper
 
   def link_to_add_puppetclass klass, type
     options = klass.name.size > 28 ? {:'data-original-title'=>klass.name, :rel=>'twipsy'} : {}
-    content_tag(:span, truncate(klass.name, :length => 28), :'data-original-title'=>klass.name, :rel=>'twipsy').html_safe +
+    content_tag(:span, truncate(klass.name, :length => 28), options).html_safe +
     link_to_function("", "add_puppet_class(this)", :'data-class-id'=>klass.id,
                      :'data-original-title'=>"Click to add #{klass}", :rel=>'twipsy',
                      :class=>"ui-icon ui-icon-plus")
