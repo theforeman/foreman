94a25eb7973c6e87501bd5fbce911c8215b03e68
Ohad Levy <ohadlevy@gmail.com>
2012-03-25 13:28:55 +0200

    hard coding vnc display for now, as we don't support spice display just yet.

--- lib/foreman/model/ovirt.rb
+++ lib/foreman/model/ovirt.rb
@@ -67,6 +67,7 @@ module Foreman::Model
     def create_vm args = {}
       #ovirt doesn't accept '.' in vm name.
       args[:name] = args[:name].parameterize
+      args[:display] = 'VNC'
       vm          = super args
       begin
         set_interfaces(vm, args[:interfaces_attributes])
