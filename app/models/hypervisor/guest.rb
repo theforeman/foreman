dbbbd0a7c7753e8b00dd15ec8ed63b9d978f603a
Ohad Levy <ohadlevy@gmail.com>
2012-04-26 20:26:50 +0300

    make sure foreman doesnt blow up when running without libvirt / virt gems

--- app/models/hypervisor/guest.rb
+++ app/models/hypervisor/guest.rb
@@ -1,24 +1,24 @@
-require 'virt'
-require 'virt/guest'
+module Virt
 
-class Virt::Guest
+  class Guest
 
-  def self.all
-    Virt.connection.host.guests.values.flatten.sort
+    def self.all
+      Virt.connection.host.guests.values.flatten.sort
+    end
+
+    def self.find(name)
+      Virt.connection.host.find_guest_by_name name
+    rescue
+      raise ActiveRecord::RecordNotFound
+    end
+
+    def to_param
+      to_s
+    end
+
+    def as_json opts
+      {to_s => {:memory => memory, :vcpu => vcpu, :running => running?, :volume => {:size => volume.size, :pool => volume.pool.name}}}
+    end
+
   end
-
-  def self.find(name)
-    Virt.connection.host.find_guest_by_name name
-  rescue
-    raise ActiveRecord::RecordNotFound
-  end
-
-  def to_param
-    to_s
-  end
-
-  def as_json opts
-    {to_s => {:memory => memory, :vcpu => vcpu, :running => running?, :volume => {:size => volume.size, :pool => volume.pool.name}}}
-  end
-
 end
