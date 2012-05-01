9915949c09313af64966441c9571e77d678f4ecc
Amos Benari <abenari@redhat.com>
2012-05-01 16:27:11 +0300

    added ace code editor to the provisioning template edit page.

---
+++ public/javascripts/config_template.js
@@ -0,0 +1,35 @@
+
+function snippet_changed(item){
+  var checked = !!$(item).attr('checked');
+  $('#kind_selector').toggle(!checked);
+  $('#snippet_message').toggle(checked);
+  $('#association').toggle(!checked);
+}
+$(function() {
+  var text_area = $(".template_text");
+  text_area.parent().prepend("<div id='editor1'></div>");
+
+  $("#editor1")
+      .css("position","relative")
+      .height(text_area.height())
+      .width(text_area.width());
+  text_area.hide();
+
+  var editor = ace.edit("editor1");
+  editor.setTheme("ace/theme/dawn");
+
+  var RubyMode = require("ace/mode/ruby").Mode;
+  var session = editor.getSession();
+  session.setMode(new RubyMode());
+//  session.setUseWrapMode(true);
+
+  var content = text_area.text();
+  session.setValue(content);
+  session.on('change', function(){
+    text_area.text(session.getValue());
+  });
+
+  $(".template_file").on("change", function(){
+      if ($(".template_file").val() != ""){ editor.setReadOnly(true); }
+    })
+})
