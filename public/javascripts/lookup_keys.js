ade838d980ca8def9dcb3875306a7084ba9a5229
Amos Benari <abenari@redhat.com>
2012-06-05 13:35:25 +0300

    smart vars looks sharper.

--- public/javascripts/lookup_keys.js
+++ public/javascripts/lookup_keys.js
@@ -1,35 +1,35 @@
 //on load
 $(function() {
   //select the first tab
-  $('.nav-pills li a span').hide();
+  $('.smart-var-tabs li a span').hide();
   select_first_tab();
   //make the remove variable button visible only on the active pill
-  $('.nav-pills li a').on('click',function(){ show_delete_button(this);});
+  $('.smart-var-tabs li a').on('click',function(){ show_delete_button(this);});
   //remove variable click event
-  $('.nav-pills li a span').on('click',function(){ remove_node(this);});
+  $('.smart-var-tabs li a span').on('click',function(){ remove_node(this);});
 })
 
 function select_first_tab(){
-  if ($('.nav-pills li').size() > 1){
+  if ($('.smart-var-tabs li').size() > 1){
     $('.tab-content .fields').first().addClass('active');
-    $('.nav-pills li').first().addClass('active');
+    $('.smart-var-tabs li').first().addClass('active');
   }
-  $('.nav-pills li.active a span').show('highlight',5  );
+  $('.smart-var-tabs li.active a span').show('highlight',5  );
 }
 
 function show_delete_button(item){
-  $('.nav-pills li a span:visible').hide();
+  $('.smart-var-tabs li a span:visible').hide();
   $(item).children("span").show('highlight',5);
-  if($(item).hasClass('label-success') && ($('.nav-pills li').size()>1)){
+  if($(item).hasClass('label-success') && ($('.smart-var-tabs li').size()>1)){
     select_first_tab();
   }
 }
 
 function remove_node(item){
   $($(item).parent("a").attr("href")).children('.btn-danger').click();
-  var pills = $('.nav-pills li a');
+  var pills = $('.smart-var-tabs li a');
   if (pills.size() > 1){pills.first().click();}
-  $('.nav-pills li.active a').click();
+  $('.smart-var-tabs li.active a').click();
 }
 
 function add_child_node(item) {
@@ -68,11 +68,11 @@ function add_child_node(item) {
     content     = content.replace(regexp, "new_" + new_id);
     var field   = '';
     if (assoc == 'lookup_keys') {
-      $('.nav-pills .active, .pill-content .active').removeClass('active');
+      $('.smart-var-tabs .active, .smart-var-content .active').removeClass('active');
       var pill = "<li class='active'><a onclick='show_delete_button(this);' data-toggle='pill' href='#new_" + new_id + "' id='pill_new_" + new_id + "'>new<span onclick='remove_node(this);' class='label label-important fr'>&times;</span></a></li>"
-      $('.nav-pills').prepend(pill);
-      field = $('.pill-content').prepend($(content).addClass('active'));
-      $('.nav-pills li.active a').show('highlight', 500);
+      $('.smart-var-tabs').prepend(pill);
+      field = $('.smart-var-content').prepend($(content).addClass('active'));
+      $('.smart-var-tabs li.active a').show('highlight', 500);
     } else {
       field = $(content).insertBefore($(item));
     }
