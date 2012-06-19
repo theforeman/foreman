74ff4db86f31281bb76d93cf31023c8e4b382300
Ohad Levy <ohadlevy@gmail.com>
2012-06-19 11:27:27 +0300

    [CI] typo in ci script

--- extras/ci/setup.sh
+++ extras/ci/setup.sh
@@ -4,7 +4,7 @@ APP_ROOT=`pwd`
 
 # setup basic settings file
 sed -e 's/:login: false/:login: true/' $APP_ROOT/config/settings.yaml.example > $APP_ROOT/config/settings.yaml
-cp $APP_ROOT/config/database.yml.example > $APP_ROOT/config/database.yml
+cp $APP_ROOT/config/database.yml.example $APP_ROOT/config/database.yml
 
 # install runtime C libs that are required:
 sudo apt-get install -y libvirt-dev
