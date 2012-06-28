7e3f8fb5796cfad05d92909cecf0d04816d688c1
Roberto <roberto_moral@cable.comcast.com>
2012-06-28 18:40:35 -0400

    Fixed migrations for mysql2

--- db/migrate/20110321070954_revert_face_names_and_values_to_text_records.rb
+++ db/migrate/20110321070954_revert_face_names_and_values_to_text_records.rb
@@ -1,12 +1,12 @@
 class RevertFaceNamesAndValuesToTextRecords < ActiveRecord::Migration
   def self.up
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql" or ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
       execute "ALTER TABLE fact_values MODIFY value text COLLATE utf8_bin NOT NULL;"
     end
   end
 
   def self.down
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql" or ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
       execute %{ALTER TABLE fact_values MODIFY value varchar(255) COLLATE utf8_bin NOT NULL}
     end
   end
