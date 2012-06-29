ec032ca7416326c1e89cd08218a2661c7c97da83
Roberto <roberto_moral@cable.comcast.com>
2012-06-29 07:26:45 -0400

    Added mysql2 conditionals to the rest of the migrations where mysql is used

--- db/migrate/20090915030726_change_report_field_type_to_text.rb
+++ db/migrate/20090915030726_change_report_field_type_to_text.rb
@@ -1,12 +1,12 @@
 class ChangeReportFieldTypeToText < ActiveRecord::Migration
   def self.up
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql" or ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2" 
       execute "ALTER TABLE reports MODIFY log text;"
     end
   end
 
   def self.down
-    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql"
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql" ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
       change_column :reports, :log, :text, :limit => 50.kilobytes, :null => false
     end
   end
