2807589bd5fc7f5d5d9cf00606181fa9b9136971
Corey Osman <corey@logicminds.biz>
2012-06-30 16:41:32 -0700

    split api routes to separate routes file

---
+++ config/routes/api/v2.rb
@@ -0,0 +1,11 @@
+# config/routes/api/v1.rb
+
+Rails.application.routes.draw do |map|
+
+#  namespace :api, :defaults => {:format => 'json'} do
+#    scope :module => :v2, :constraints => ApiConstraints.new(:version => 2, :default => false) do
+#      resources :bookmarks, :except => [:new, :edit]
+#    end
+#  end
+
+end
