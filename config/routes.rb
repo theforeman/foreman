a4b8e85ff0180d87737b087c01525ba03fff0dc4
Corey Osman <corey@logicminds.biz>
2012-06-30 16:41:32 -0700

    split api routes to separate routes file

--- config/routes.rb
+++ config/routes.rb
@@ -278,27 +278,6 @@ Foreman::Application.routes.draw do
 
   resources :tasks, :only => [:show]
 
-  #### API Namespace from here downwards ####
-
-  restapi
-
-  namespace :api, :defaults => {:format => 'json'} do
-    scope :module => :v1, :constraints => ApiConstraints.new(:version => 1, :default => true) do
-      resources :bookmarks, :except => [:new, :edit]
-      resources :architectures, :except => [:new, :edit]
-      resources :operatingsystems, :except => [:new, :edit] do
-        member do
-          get 'bootfiles'
-        end
-      end
-
-      match '/', :to => 'home#index'
-      match 'status', :to => 'home#status', :as => "status"
-    end
-#    scope module: :v2, constraints: ApiConstraints.new(version: 2, default: true) do
-#      resources :bookmarks
-#    end
-  end
 
  #Keep this line the last route
   match '*a', :to => 'errors#routing'
