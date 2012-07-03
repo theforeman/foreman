8cf00cea776f46b5490da73116dc8f14fe040324
Ohad Levy <ohadlevy@gmail.com>
2012-07-03 16:20:36 +0300

    avoid breaking mysql migration from an empty db

--- db/migrate/20120623002052_add_ok_hosts_book_mark.rb
+++ db/migrate/20120623002052_add_ok_hosts_book_mark.rb
@@ -1,4 +1,6 @@
 class AddOkHostsBookMark < ActiveRecord::Migration
+  class Bookmark < ActiveRecord::Base; end
+
   def self.up
      Bookmark.find_or_create_by_name_and_query_and_controller :name => "ok hosts", :query => 'last_report > "35 minutes ago" and status.enabled = true and status.applied = 0 and status.failed = 0 and status.pending = 0', :controller=> "hosts", :public => true
   end
