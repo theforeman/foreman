6147006ebc084cace2541566681b450926775c3b
Ohad Levy <ohadlevy@gmail.com>
2012-07-04 22:49:19 +0300

    fixes parameters whitespace validations

--- test/unit/common_parameter_test.rb
+++ test/unit/common_parameter_test.rb
@@ -10,48 +10,36 @@ class CommonParameterTest < ActiveSupport::TestCase
     assert !parameter.save
   end
 
-  test "name can't contain trailing spaces" do
+  test "name can't contain white spaces" do
     parameter = CommonParameter.new :name => "   a new     param    ", :value => "some_value"
-    assert !parameter.name.strip.squeeze(" ").empty?
     assert !parameter.save
 
-    parameter.name.strip!.squeeze!(" ")
+    parameter.name.gsub!(/\s+/,'_')
     assert parameter.save
   end
 
   test "value can't be blank" do
-    parameter = CommonParameter.new :name => "some parameter", :value => "   "
+    parameter = CommonParameter.new :name => "some_parameter", :value => "   "
     assert parameter.value.strip.empty?
     assert !parameter.save
   end
 
   test "value can't be empty" do
-    parameter = CommonParameter.new :name => "some parameter", :value => ""
+    parameter = CommonParameter.new :name => "some_parameter", :value => ""
     assert parameter.value.strip.empty?
     assert !parameter.save
   end
 
-  test "value can't contain trailing spaces" do
-    parameter = CommonParameter.new :name => "some parameter", :value => "   some crazy      value    "
-    assert !parameter.value.strip.squeeze(" ").empty?
-    assert !parameter.save
-
-    parameter.value.strip!.squeeze!(" ").empty?
-    assert parameter.save
-  end
-
   test "value can contain spaces and unusual characters" do
-    parameter = CommonParameter.new :name => "some parameter", :value => "   some crazy \"\'&<*%£# value"
-    assert !parameter.value.strip.squeeze(" ").empty?
+    parameter = CommonParameter.new :name => "some_parameter", :value => "   some crazy \"\'&<*%# value"
     assert parameter.save
 
-    parameter.value.strip!.squeeze!(" ").empty?
-    assert parameter.save
+    assert_equal parameter.value, "some crazy \"\'&<*%# value"
   end
 
   test "duplicate names cannot exist" do
-    parameter1 = CommonParameter.create :name => "some parameter", :value => "value"
-    parameter2 = CommonParameter.create :name => "some parameter", :value => "value"
+    parameter1 = CommonParameter.create :name => "some_parameter", :value => "value"
+    parameter2 = CommonParameter.create :name => "some_parameter", :value => "value"
     assert !parameter2.valid?
     assert  parameter2.errors.full_messages[0] == "Name has already been taken"
   end
