b304788d433a4d6fa1c049675d0113d80cc45373
Amos Benari <abenari@redhat.com>
2012-07-05 12:52:41 +0300

    fixes #1729 safer migration code.

--- db/migrate/20120624081510_add_auditable_name_and_associated_name_to_audit.rb
+++ db/migrate/20120624081510_add_auditable_name_and_associated_name_to_audit.rb
@@ -1,8 +1,8 @@
 class AddAuditableNameAndAssociatedNameToAudit < ActiveRecord::Migration
   def self.up
-    add_column :audits, :auditable_name, :string
-    add_column :audits, :associated_name, :string
-    add_index :audits, :id
+    add_column :audits, :auditable_name, :string unless column_exists? :audits, :auditable_name
+    add_column :audits, :associated_name, :string unless column_exists? :audits, :associated_name
+    add_index :audits, :id unless index_exists? :audits, :id
     Audit.reset_column_information
     say "About to review all audits, this may take a while..."
     Audit.includes(:user, :auditable, :associated).find_in_batches do |audits|
@@ -10,10 +10,10 @@ class AddAuditableNameAndAssociatedNameToAudit < ActiveRecord::Migration
         attr = {}
         auditable_name  ||= audit.auditable.try(:to_label)  rescue nil
         associated_name ||= audit.associated.try(:to_label) rescue nil
-        attr[:auditable_name] = "'#{auditable_name}'"  if auditable_name
-        attr[:associated_name]= "'#{associated_name}'" if associated_name
+        attr[:auditable_name] = auditable_name  if auditable_name
+        attr[:associated_name]= associated_name if associated_name
         if audit.username.empty? and audit.user
-          username = "'#{audit.user.to_label}'" rescue nil
+          username = audit.user.to_label rescue nil
           attr[:username] = username if !username.empty?
         end
         audit.update_multiple_attribute(attr) if attr.length > 0
