1b795429d8295f7f9aa20c2d08edf65b0c2ec012
Ohad Levy <ohadlevy@gmail.com>
2012-07-18 14:36:00 +0300

    fixes #1764 changing per_page setting would update only after a restart
    this commit changes it back to an area in the code which gets evaluated
    everytime instead of upon startup only

--- config/initializers/foreman.rb
+++ config/initializers/foreman.rb
@@ -5,5 +5,3 @@ Foreman::DefaultSettings::Loader.load
 
 # We load the default settings for the roles if they are not already present
 Foreman::DefaultData::Loader.load(false)
-
-WillPaginate.per_page = Setting.entries_per_page rescue 20
