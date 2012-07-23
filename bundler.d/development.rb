981a95082583910355873a3c675bb7c748227f21
Martin Bačovský <mbacovsk@redhat.com>
2012-07-23 11:27:14 +0200

    api v1 - restapi renamed to apipie

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -3,4 +3,7 @@ group :development do
   gem "ruby-debug", :platforms => :ruby_18
   gem "ruby-debug19", :platforms => :ruby_19
   gem 'redcarpet'
+  gem 'single_test'
+  gem 'pry'
+  gem "term-ansicolor"
 end
