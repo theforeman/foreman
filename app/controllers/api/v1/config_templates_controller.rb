5fd4c4c64ae771c93fe97ff5404995c7814ad074
Pavel Pokorný <pajkycz@gmail.com>
2012-08-14 17:16:30 +0200

    config templates - move build_pxe_default into model

--- app/controllers/api/v1/config_templates_controller.rb
+++ app/controllers/api/v1/config_templates_controller.rb
@@ -59,76 +59,21 @@ module Api
 
       api :GET, "/config_templates/build_pxe_default", "Change the default PXE menu on all configured TFTP servers"
       def build_pxe_default
-        if (proxies = Subnet.all.map(&:tftp).uniq.compact).empty?
-          error_msg = "No TFTP proxies defined, can't continue"
-        end
-
-        if (default_template = ConfigTemplate.find_by_name("PXE Default File")).nil?
-          error_msg = "Could not find a Configuration Template with the name \"PXE Default File\", please create one."
-        else
-          begin
-            @profiles = pxe_default_combos
-            menu = render_safe(default_template.template, [:default_template_url], {:profiles => @profiles})
-          rescue => e
-            error_msg = "failed to process template: #{e}"
-          end
-        end
-
-        unless error_msg.empty?
-          render :json => error_msg, :status => :unprocessable_entity and return
-        end
-
-        error_msgs = []
-        proxies.each do |proxy|
-          begin
-            tftp = ProxyAPI::TFTP.new(:url => proxy.url)
-            tftp.create_default({:menu => menu})
-
-            @profiles.each do |combo|
-              combo[:hostgroup].operatingsystem.pxe_files(combo[:hostgroup].medium, combo[:hostgroup].architecture).each do |bootfile_info|
-                for prefix, path in bootfile_info do
-                  tftp.fetch_boot_file(:prefix => prefix.to_s, :path => path)
-                end
-              end
-            end
-          rescue Exception => exc
-            error_msgs << "#{proxy}: #{exc.message}"
-          end
-        end
-
-        if error_msgs.empty?
-          head :status => :ok
-        else
-          msg = "There was an error creating the PXE Default file: #{error_msgs.join(",")}"
-          render :json => msg, :status => 500
-        end
+        status, msg = ConfigTemplate.build_pxe_default(self)
+        render :json => msg, :status => status
       end
 
       private
-
-      # get a list of all hostgroup, template combinations that a pxemenu will be
-      # generated for
-      def pxe_default_combos
-        combos = []
-        ConfigTemplate.joins(:template_kind).where("template_kinds.name" => "provision").each do |template|
-          template.template_combinations.each do |combination|
-            hostgroup = combination.hostgroup
-            if hostgroup and hostgroup.operatingsystem and hostgroup.architecture and hostgroup.medium
-              combos << {:hostgroup => hostgroup, :template => template}
-            end
-          end
-        end
-        combos
-      end
-
-      def default_template_url(template, hostgroup)
-        url_for :only_path => false, :action => :template, :controller => :unattended, :id => template.name, :hostgroup => hostgroup.name
-      end
 
       # convert the file upload into a simple string to save in our db.
       def handle_template_upload
         return unless params[:config_template] and (t=params[:config_template][:template])
         params[:config_template][:template] = t.read if t.respond_to?(:read)
+      end
+
+      def default_template_url template, hostgroup
+        url_for :only_path => false, :action => :template, :controller => :unattended,
+          :id => template.name, :hostgroup => hostgroup.name
       end
 
     end
