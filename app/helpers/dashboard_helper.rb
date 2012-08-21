e1863f846c966cb9d524de10f54d5b24e7df2234
cyberkov <admin@cyberkov.at>
2012-08-21 11:39:59 +0200

    changed dashboard display of interval to use time_ago_in_words

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -27,7 +27,7 @@ module DashboardHelper
 
   def render_run_distribution data, options = {}
     bar_chart "run_distribution",
-              "Run Distribution in the last #{Setting[:puppet_interval]} minutes",
+              "Run Distribution in the last #{time_ago_in_words((Setting[:puppet_interval]).minutes.ago)}",
               "Number Of Clients",
               data[:labels],
               data[:counter],
