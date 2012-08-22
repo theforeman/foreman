3b47fae222e55f5d8c1a1dabdb1b530a809a3921
Sam Kottler <sam@kottlerdevelopment.com>
2012-08-22 08:06:00 -0400

    Fixed some fixtures and added tenant tests

---
+++ test/unit/tenant_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class LocationTest < ActiveSupport::TestCase
+  test 'it should not save without an empty name' do
+    tenant = Tenant.new
+    assert !tenant.save
+  end
+
+  test 'it should not save with a blank name' do
+    tenant = Tenant.new
+    tenant.name = " "
+    assert !tenant.save
+  end
+
+  test 'it should not save another tenant with the same name' do
+    tenant = Tenant.new
+    tenant.name = "tenant1"
+    assert tenant.save
+
+    second_tenant = Tenant.new
+    second_tenant.name = "tenant1"
+    assert !second_tenant.save
+  end
+
+  test 'it should show the name for to_s' do
+    tenant = Tenant.new :name => "tenant1"
+    assert tenant.to_s == "tenant1"
+  end
+end
