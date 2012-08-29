9282e4fb87c054fda693edee8e6ff67ad6a8e635
Sam Kottler <sam@kottlerdevelopment.com>
2012-08-29 17:02:48 -0400

    Cleaned up some view cruft that should have been a helper all along

--- app/helpers/taxonomy_helper.rb
+++ app/helpers/taxonomy_helper.rb
@@ -6,4 +6,8 @@ module TaxonomyHelper
   def show_tenant_tab?
     SETTINGS[:tenants_enabled] && (User.current.admin? || authorized_keys(:tenants, :index))
   end
+
+  def show_taxonomy_tabs?
+    SETTINGS[:locations_enabled] or SETINGS[:tenants_enabled]
+  end
 end
