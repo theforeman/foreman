39a3e2befd137f15ea69a0773fefdb651c5e055c
Sam Kottler <sam@kottlerdevelopment.com>
2012-08-29 17:09:31 -0400

    Taxonomy helper typo fix

--- app/helpers/taxonomy_helper.rb
+++ app/helpers/taxonomy_helper.rb
@@ -8,6 +8,6 @@ module TaxonomyHelper
   end
 
   def show_taxonomy_tabs?
-    SETTINGS[:locations_enabled] or SETINGS[:tenants_enabled]
+    SETTINGS[:locations_enabled] or SETTINGS[:tenants_enabled]
   end
 end
