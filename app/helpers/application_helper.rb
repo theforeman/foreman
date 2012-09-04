2ae562d725c3896881068800f3d99e07dde1468f
Sam Kottler <shk@redhat.com>
2012-09-03 23:27:27 -0400

    Only show the help button if the welcome template exists

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -159,7 +159,7 @@ module ApplicationHelper
   end
 
   def help_path
-    link_to "Help", :action => "welcome"
+    link_to "Help", :action => "welcome" if File.exists?("#{RAILS_ROOT}/app/views/#{params[:controller]}/welcome.html.erb")
   end
 
   def method_path method
