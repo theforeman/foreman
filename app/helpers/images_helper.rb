f3d48c8d8702288e87a4b5753e690ee21b93653d
Daniel Lobato García <elobatocs@gmail.com>
2012-09-27 17:04:42 +0200

    Image form shows a dropdown with images for Openstack rather than a blank text box

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,2 +1,8 @@
 module ImagesHelper
+  def openstack_list_of_images
+    hash = {}
+    ComputeResource.first.vms.first.connection.list_images.body['images'].each { |image| hash[image['id']] = image['name']}   
+
+    hash
+  end
 end
