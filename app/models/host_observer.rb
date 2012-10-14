24a1db18f3a7fba57b1ae33c0c10095ade186a01
Ohad Levy <ohadlevy@gmail.com>
2012-10-14 16:51:30 +0200

    refs #1892 - ensure that when unattended / orchestation is disabled this code does not executed.

--- app/models/host_observer.rb
+++ app/models/host_observer.rb
@@ -4,12 +4,13 @@ class HostObserver < ActiveRecord::Observer
   # this has to happen post validation and before the orchesration queue is starting to 
   # process, as the token value is required within the tftp config file manipulations
   def after_validation(host)
+    return unless SETTINGS[:unattended]
     # new server in build mode
     if host.new_record? and host.build?
       host.set_token
     end
     # existing server change build mode
-    if host.old and host.build? != host.old.build?
+    if host.respond_to?(:old) and host.old and host.build? != host.old.build?
       host.build? ? host.set_token : host.expire_tokens
     end
   end
