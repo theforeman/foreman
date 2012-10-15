ab4a5ff999fb28c860de0cfa30f35ff15920e7b4
Joseph Mitchell Magen <jmagen@redhat.com>
2012-10-15 13:31:18 +0200

    change default order to fact_name

--- app/models/fact_value.rb
+++ app/models/fact_value.rb
@@ -4,8 +4,7 @@ class FactValue < Puppet::Rails::FactValue
   has_many :hostgroup, :through => :host
 
   scoped_search :on => :value, :in_key=> :fact_name, :on_key=> :name, :rename => :facts, :complete_value => true
-  scoped_search :on => :value, :default_order => true
-  scoped_search :in => :fact_name, :on => :name, :complete_value => true, :alias => "fact"
+  scoped_search :in => :fact_name, :on => :name, :complete_value => true, :alias => "fact", :default_order => true
   scoped_search :in => :host, :on => :name, :rename => :host, :complete_value => true
   scoped_search :in => :hostgroup, :on => :name, :rename => :"host.hostgroup", :complete_value => true
 
