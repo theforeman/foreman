7683267bd44cfbf87ffe28c3dc3e8fcc836795ab
Sam Kottler <shk@redhat.com>
2012-10-15 13:38:00 -0400

    Removed settings verification that was cruft from old orgs code

--- config/boot.rb
+++ config/boot.rb
@@ -6,11 +6,6 @@ SETTINGS = YAML.load_file("#{root}/config/settings.yaml")
 SETTINGS[:version]    = File.read(root + "/VERSION").chomp rescue ("N/A")
 SETTINGS[:unattended] = SETTINGS[:unattended].nil? || SETTINGS[:unattended]
 SETTINGS[:login]    ||= SETTINGS[:ldap]
-SETTINGS[:orgs_enabled] ||= SETTINGS[:multi_org] || SETTINGS[:single_org]
-if SETTINGS[:multi_org] && SETTINGS[:single_org]
-  warn "Cannot have both multi_org and single_org set to true.  Change setting in setting.yaml"
-  exit(1)
-end
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
