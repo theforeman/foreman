b7d818480421d61373b6c36ed6e6d0310b638a26
Joseph Mitchell Magen <jmagen@redhat.com>
2012-10-21 16:50:59 +0200

    api test for show environment

--- app/controllers/api/v1/environments_controller.rb
+++ app/controllers/api/v1/environments_controller.rb
@@ -13,6 +13,7 @@ module Api
       api :GET, "/environments/:id/", "Show an environment."
       param :id, String, :required => true
       def show
+        #@environment is returned by before_filter :find_resource
       end
 
       api :POST, "/environments/", "Create an environment."
--- test/functional/api/v1/environments_controller_test.rb
+++ test/functional/api/v1/environments_controller_test.rb
@@ -19,6 +19,8 @@ class Api::V1::EnvironmentsControllerTest < ActionController::TestCase
       get :show, {:id => environments(:production).to_param}
     end
     assert_response :success
+    env = ActiveSupport::JSON.decode(@response.body)
+    assert !env.empty?
   end
 
   test "should create environment" do
