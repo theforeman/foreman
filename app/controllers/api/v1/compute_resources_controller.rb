9a2f37d13df977c2b0fd1586b413df242e22073c
Joseph Mitchell Magen <jmagen@redhat.com>
2012-10-30 17:00:42 +0200

    api controller and test for compute_resource

--- app/controllers/api/v1/compute_resources_controller.rb
+++ app/controllers/api/v1/compute_resources_controller.rb
@@ -1,7 +1,7 @@
 module Api
   module V1
     class ComputeResourcesController < V1::BaseController
-      before_filter :find_resource, :only => :show
+      before_filter :find_resource, :only => [:show, :update, :destroy]
 
       api :GET, "/compute_resources/", "List all compute resources."
       param :search, String, :desc => "filter results"
@@ -14,6 +14,34 @@ module Api
       api :GET, "/compute_resources/:id/", "Show an compute resource."
       param :id, :identifier, :required => true
       def show
+      end
+
+      api :POST, "/compute_resources/", "Create a compute resource."
+      param :compute_resource, Hash, :required => true do
+        param :name, String, :required => true
+        param :provider, String, :required => true
+        param :url, String, :required => true
+      end
+      def create
+        @compute_resource = ComputeResource.new(params[:compute_resource])
+        process_response @compute_resource.save
+      end
+
+      api :PUT, "/compute_resources/:id/", "Update a compute resource."
+      param :id, String, :required => true
+      param :compute_resource, Hash, :required => true do
+        param :name, String, :allow_nil => true
+        param :provider, String, :allow_nil => true
+        param :url, String, :allow_nil => true
+      end
+      def update
+        process_response @compute_resource.update_attributes(params[:compute_resource])
+      end
+
+      api :DELETE, "/compute_resources/:id/", "Delete a compute resource."
+      param :id, String, :required => true
+      def destroy
+        process_response @compute_resource.destroy
       end
 
     end
--- test/functional/api/v1/compute_resources_controller_test.rb
+++ test/functional/api/v1/compute_resources_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class Api::V1::ComputeResourcesControllerTest < ActionController::TestCase
 
+  valid_attrs = {:name => 'special_compute', :provider => 'EC2', :url => 'eu-west1'}
+
   test "should get index" do
     as_user :admin do
       get :index, {}
@@ -19,6 +21,33 @@ class Api::V1::ComputeResourcesControllerTest < ActionController::TestCase
     assert_response :success
     show_response = ActiveSupport::JSON.decode(@response.body)
     assert !show_response.empty?
+  end
+
+  test "should create compute_resource" do
+    as_user :admin do
+      assert_difference('ComputeResource.count') do
+        post :create, {:bookmark => valid_attrs}
+      end
+    end
+    assert_response :success
+  end
+
+  test "should destroy compute_resource" do
+    as_user :admin do
+      assert_difference('ComputeResource.count', -1) do
+        delete :destroy, {:id => compute_resources(:one).to_param}
+      end
+    end
+    assert_response :success
+  end
+
+  test "should not destroy compute_resource that is in use with hosts" do
+    as_user :admin do
+      assert_difference('ComputeResource.count', 0) do
+        delete :destroy, {:id => compute_resources(:one).to_param}
+      end
+    end
+    assert_response :unprocessable_entity
   end
 
 end
