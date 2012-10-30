43392c7158e49e087fa680fd53052f2dc91d9b8e
Joseph Mitchell Magen <jmagen@redhat.com>
2012-10-30 16:40:57 +0200

    added create, update, destroy tests

--- app/controllers/api/v1/auth_source_ldaps_controller.rb
+++ app/controllers/api/v1/auth_source_ldaps_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AuthSourceLdapsController < V1::BaseController
       
-      before_filter :find_resource, :only => :show
+      before_filter :find_resource, :only => %w{show update destroy}
 
       api :GET, "/auth_source_ldaps/", "List all authsource ldaps"
       def index
@@ -13,6 +13,32 @@ module Api
       param :id, :identifier, :required => true
       def show
       end
+
+      api :POST, "/auth_source_ldaps/", "Create an auth_source_ldap."
+      param :auth_source_ldap, Hash, :required => true do
+        param :name, String, :required => true
+      end
+      def create
+        @auth_source_ldap = AuthSourceLdap.new(params[:auth_source_ldap])
+        process_response @auth_source_ldap.save
+      end
+
+      api :PUT, "/auth_source_ldaps/:id/", "Update an auth_source_ldap."
+      param :id, String, :required => true
+      param :auth_source_ldap, Hash, :required => true do
+        param :name, String
+      end
+      def update
+        process_response @auth_source_ldap.update_attributes(params[:auth_source_ldap])
+      end
+
+      api :DELETE, "/auth_source_ldaps/:id/", "Delete an auth_source_ldap."
+      param :id, String, :required => true
+      def destroy
+        process_response @auth_source_ldap.destroy
+      end
+
+
 
     end
   end
--- test/functional/api/v1/auth_source_ldaps_controller_test.rb
+++ test/functional/api/v1/auth_source_ldaps_controller_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class Api::V1::AuthSourceLdapsControllerTest < ActionController::TestCase
 
+  valid_attrs = {:name => 'ldap2', :host => 'ldap2'}
+
   test "should get index" do
     as_user :admin do
       get :index, {}
@@ -19,6 +21,31 @@ class Api::V1::AuthSourceLdapsControllerTest < ActionController::TestCase
     assert_response :success
     show_response = ActiveSupport::JSON.decode(@response.body)
     assert !show_response.empty?
+  end
+
+  test "should create auth_source_ldap" do
+    as_user :admin do
+      assert_difference('AuthSourceLdap.count', 1) do
+        post :create, {:auth_source_ldap => valid_attrs }
+      end
+    end
+    assert_response :success
+  end
+
+  test "should update auth_source_ldap" do
+    as_user :admin do
+      put :update, {:id => auth_sources(:one).to_param, :auth_source_ldap => {} }
+    end
+    assert_response :success
+  end
+
+  test "should destroy auth_source_ldap" do
+    as_user :admin do
+      assert_difference('AuthSourceLdap.count', -1) do
+        delete :destroy, {:id => auth_sources(:one).to_param}
+      end
+    end
+    assert_response :success
   end
 
 end
