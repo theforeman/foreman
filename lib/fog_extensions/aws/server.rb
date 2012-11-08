f3bda7b4b5ffb3017c9b5c2da2b2f4a9b6a8fd8f
Romain Vrignaud <romain@yakaz.com>
2012-11-08 14:17:12 +0100

    fixes #1876 fix display of instance name and dns for AWS Use instance number when instances name is not set. Use private DNS entry when public dns is not available

--- lib/fog_extensions/aws/server.rb
+++ lib/fog_extensions/aws/server.rb
@@ -2,11 +2,15 @@ module FogExtensions
   module AWS
     module Server
       def to_s
-        tags["Name"]
+        tags["Name"] || identity
       end
 
       def name
         to_s
+      end
+
+      def dns
+         dns_name || private_dns_name
       end
 
     end
