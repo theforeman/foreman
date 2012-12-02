c02c4b1393698a2fe3bfbe02f32e8c862f4c5dc5
Ohad Levy <ohadlevy@gmail.com>
2012-12-02 10:28:35 +0200

    fog 1.8 has been released - updating gem file

--- bundler.d/fog.rb
+++ bundler.d/fog.rb
@@ -1,5 +1,3 @@
 group :fog do
-  #  We should change it to official fog 1.8 once releases
-  #  gem 'fog', '>= 1.8'
-  gem 'fog', :git => "https://github.com/ohadlevy/fog.git", :branch => 'vmware'
+  gem 'fog', '>= 1.8'
 end
