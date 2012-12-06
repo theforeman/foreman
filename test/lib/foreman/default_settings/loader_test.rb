0b4b2129f86efefc5ef159f6f00960b34da98ccf
Dominic Cleal <dcleal@redhat.com>
2012-12-06 13:28:04 +0000

    fixes #1995 - enable param class ENC by default on Puppet 2.6.5+

--- test/lib/foreman/default_settings/loader_test.rb
+++ test/lib/foreman/default_settings/loader_test.rb
@@ -12,5 +12,26 @@ class DefaultSettingsLoaderTest < ActiveSupport::TestCase
     Foreman::DefaultSettings::Loader.load
     assert_equal '/var/lib/puppet/mycert.pem', Setting.find_by_name('ssl_certificate').value
   end
+
+  test "should disable param class ENC support on Puppet < 2.6.5" do
+    PuppetSetting.any_instance.stubs(:get).returns({})
+    Facter.expects(:puppetversion).returns('2.6.4')
+    Foreman::DefaultSettings::Loader.load
+    assert_equal false, Setting.find_by_name('Parametrized_Classes_in_ENC').value
+  end
+
+  test "should enable param class ENC support on Puppet >= 2.6.5" do
+    PuppetSetting.any_instance.stubs(:get).returns({})
+    Facter.expects(:puppetversion).returns('2.6.5')
+    Foreman::DefaultSettings::Loader.load
+    assert Setting.find_by_name('Parametrized_Classes_in_ENC').value
+  end
+
+  test "should handle -rc version numbers" do
+    PuppetSetting.any_instance.stubs(:get).returns({})
+    Facter.expects(:puppetversion).returns('3.0.2-rc1')
+    Foreman::DefaultSettings::Loader.load
+    assert_equal true, Setting.find_by_name('Parametrized_Classes_in_ENC').value
+  end
 end
 
