f528f1589a36c01fed6b031936c5d958da3bee92
Joseph Mitchell Magen <jmagen@redhat.com>
2012-12-10 14:15:33 +0200

    added compute resource actions and tests

--- test/functional/api/v1/compute_resources_controller_test.rb
+++ test/functional/api/v1/compute_resources_controller_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class Api::V1::ComputeResourcesControllerTest < ActionController::TestCase
 
-  valid_attrs = { :name => 'special_compute', :provider => 'EC2', :url => 'eu-west1', :user => 'user@example.com', :password => 'secret' }
+  def setup
+    Fog.mock!
+  end
+
+  def teardown
+    Fog.unmock!
+  end
+
+  valid_attrs = { :name => 'special_compute', :provider => 'EC2', :region => 'eu-west-1', :user => 'user@example.com', :password => 'secret' }
 
   test "should get index" do
     get :index, { }
@@ -17,6 +25,26 @@ class Api::V1::ComputeResourcesControllerTest < ActionController::TestCase
     assert_response :success
     show_response = ActiveSupport::JSON.decode(@response.body)
     assert !show_response.empty?
+  end
+
+  test "should create valid compute resource" do
+    post :create, { :compute_resource => valid_attrs }
+    assert_response :success
+    show_response = ActiveSupport::JSON.decode(@response.body)
+    assert !show_response.empty?
+  end
+
+  test "should update compute resource" do
+    put :update, { :id => compute_resources(:mycompute).to_param, :compute_resource => { :description => "new_description" } }
+    assert_equal "new_description", ComputeResource.find_by_name('mycompute').description
+    assert_response :success
+  end
+
+  test "should destroy compute resource" do
+    assert_difference('ComputeResource.count', -1) do
+      delete :destroy, { :id => compute_resources(:yourcompute).id }
+    end
+    assert_response :success
   end
 
 end
