568634452617a42f0b54d6f6699d5087cae75ec9
Dominic Cleal <dcleal@redhat.com>
2012-12-20 12:26:32 +0000

    fixes #2063 - pin minitest to 3.5+ and mocha 0.12.x
    Mocha 0.13.x and minitest 4.x will require Rails 3.2.

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,13 +1,9 @@
 group :test do
-  if RUBY_VERSION >= '1.9.3'
-    gem 'mocha', :require => false
-  else
-    gem 'mocha', '< 0.13.0', :require => false
-  end
+  gem 'mocha', '< 0.13.0', :require => false
   gem 'shoulda', "=3.0.1"
   gem 'rr'
   gem 'rake'
   gem 'single_test'
   gem 'ci_reporter', '>= 1.6.3'
-  gem "minitest", :platforms => :ruby_19
+  gem 'minitest', '~> 3.5', :platforms => :ruby_19
 end
