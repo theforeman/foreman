596d5f09c61a39168b12e12800bb83dc62085d9c
Joseph Mitchell Magen <jmagen@redhat.com>
2012-12-24 16:15:23 +0200

    fixes #1991 expire topbar cache on user when restart server

--- config/initializers/foreman.rb
+++ config/initializers/foreman.rb
@@ -5,3 +5,12 @@ Foreman::DefaultSettings::Loader.load
 
 # We load the default settings for the roles if they are not already present
 Foreman::DefaultData::Loader.load(false)
+
+# clear our users topbar cache
+begin
+  User.unscoped.pluck(:id).each do |id|
+    Rails.cache.delete("views/tabs_and_title_records-#{id}")
+  end
+rescue => e
+  Rails.logger.warn e
+end
