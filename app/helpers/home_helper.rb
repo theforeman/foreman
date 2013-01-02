700ed6b9f11b348191023979ec440d2b616bada8
Amos Benari <abenari@redhat.com>
2013-01-02 12:54:06 +0200

    reorg in the user and settings top menus.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -5,49 +5,42 @@ module HomeHelper
   end
 
   def setting_options
-    choices = [
-      [:group, "Configuration"],
-      ['Environments',           :environments],
-      ['Global Parameters',      :common_parameters],
-      ['Host Groups',            :hostgroups],
-      ['Puppet Classes',         :puppetclasses],
-      ['Smart Variables',        :lookup_keys],
-      ['Smart Proxies',          :smart_proxies]
-    ]
+    configuration_group =
+        [['Environments',           :environments],
+        ['Global Parameters',      :common_parameters],
+        ['Host Groups',            :hostgroups],
+        ['Puppet Classes',         :puppetclasses],
+        ['Smart Variables',        :lookup_keys],
+        ['Smart Proxies',          :smart_proxies]]
+    choices = [ [:group, "Configuration", configuration_group]]
+
+    if SETTINGS[:unattended]
+      provisioning_group =
+          [['Architectures',          :architectures],
+          ['Compute Resources',      :compute_resources],
+          ['Domains',                :domains],
+          ['Hardware Models',        :models],
+          ['Installation Media',     :media],
+          ['Operating Systems',      :operatingsystems],
+          ['Partition Tables',       :ptables],
+          ['Provisioning Templates', :config_templates],
+          ['Subnets',                :subnets]]
+      choices += [[:divider], [:group, "Provisioning", provisioning_group]]
+    end
 
     if (SETTINGS[:organizations_enabled] or SETTINGS[:locations_enabled])
-      choices += [[:divider], [:group, "Taxonomies"]]
+      choices += [[:divider]]
       choices += [ ['Locations', :locations] ]         if SETTINGS[:locations_enabled]
       choices += [ ['Organizations', :organizations] ] if SETTINGS[:organizations_enabled]
     end
 
-
-    if SETTINGS[:unattended]
-
-      choices += [
-        [:divider], [:group, "Provisioning"],
-        ['Architectures',          :architectures],
-        ['Compute Resources',      :compute_resources],
-        ['Domains',                :domains],
-        ['Hardware Models',        :models],
-        ['Installation Media',     :media],
-        ['Operating Systems',      :operatingsystems],
-        ['Partition Tables',       :ptables],
-        ['Provisioning Templates', :config_templates],
-        ['Subnets',                :subnets]
-      ]
-    end
-
-    choices += [
-      [:divider], [:group, "Users"],
-      ['LDAP Authentication',    :auth_source_ldaps],
+    users_group =
+      [['LDAP Authentication',    :auth_source_ldaps],
       ['Users',                  :users],
-      ['User Groups',            :usergroups],
-    ] if SETTINGS[:login]
+      ['User Groups',            :usergroups]]
+    users_group += [['Roles',     :roles]] if User.current && User.current.admin?
 
-    choices += [
-      ['Roles',                  :roles]
-    ] if SETTINGS[:login] && User.current && User.current.admin?
+    choices += [[:divider], [:group, "Users", users_group] ] if SETTINGS[:login]
 
     choices += [
       [:divider],
@@ -97,5 +90,24 @@ module HomeHelper
       url = eval("#{kontroller}_url")
       authorized_for(kontroller, action) ? [name, url] : nil
     end.compact.sort
+  end
+
+  def user_header
+    summary = content_tag(:span, "#{User.current.to_label}  ", :class=>'text-label')
+    summary += content_tag(:span, Organization.current.to_label, :class=>'boxed-label') if Organization.current
+    summary += content_tag(:span, Location.current.to_label, :class=>'boxed-label') if Location.current
+    summary += gravatar_image_tag(User.current.mail, :class=>'gravatar', :alt=>'Change your avatar at gravatar.com') + content_tag(:span, "", :class=>'caret')
+    link_to(summary.html_safe, "#", :class => "dropdown-toggle", :'data-toggle'=>"dropdown")
+  end
+
+  def gravatar_image_tag(email, html_options = {})
+    default_image = "/images/user.jpg"
+    html_options.merge!(:onerror=>"this.src='#{default_image}'")
+    image_tag(gravatar_url(email, default_image), html_options)
+  end
+
+  def gravatar_url(email, default_image)
+    return default_image if email.blank?
+    "#{request.protocol}//secure.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}?d=mm&s=30"
   end
 end
