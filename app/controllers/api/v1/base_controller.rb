b56f35df309e912d23453b443663a43088012c58
Ivan Necas <inecas@redhat.com>
2013-01-15 09:25:38 +0100

    Using Apipie versioning features and Maruku for markdown
    Apipie switched from Redcarpet to Maruku to avoid crashes of the C
    Extension. No further need of compiling anything to get markdown support.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,6 +2,11 @@ module Api
   module V1
     class BaseController < Api::BaseController
       include Api::Version1
+
+      resource_description do
+        api_version "v1"
+        api_version "v2"
+      end
     end
   end
 end
