fea8c95923bd4f4015c6ff18757da91428a6a21a
Joseph Mitchell Magen <jmagen@redhat.com>
2013-01-20 11:03:09 +0200

    fixes #2136 hyperlink host properties

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -180,17 +180,23 @@ module HostsHelper
 
   def overview_fields host
     fields = [
-      ["Domain", host.domain],
+      ["Domain", (link_to(host.domain, hosts_path(:search => "domain = #{host.domain}")) if host.domain)],
       ["IP Address", host.ip],
       ["MAC Address", host.mac],
-      ["Puppet Environment", host.environment],
-      ["Host Architecture", host.arch],
-      ["Operating System", host.os],
-      ["Host Group", host.hostgroup],
+      ["Puppet Environment", (link_to(host.environment, hosts_path(:search => "environment = #{host.environment}")) if host.environment)],
+      ["Host Architecture", (link_to(host.arch, hosts_path(:search => "architecture = #{host.arch}")) if host.arch)],
+      ["Operating System", (link_to(host.os, hosts_path(:search => "os = #{host.os.name}")) if host.os)],
+      ["Host Group", (link_to(host.hostgroup, hosts_path(:search => "hostgroup = #{host.hostgroup}")) if host.hostgroup)],
     ]
-    fields += [["Location", host.location.try(:name)]] if SETTINGS[:locations_enabled]
-    fields += [["Organization",host.organization.try(:name)]] if SETTINGS[:organizations_enabled]
-    fields += [["Owner", host.owner]] if SETTINGS[:login]
+    fields += [["Location", (link_to(host.location.name, hosts_path(:search => "location = #{host.location}")) if host.location)]] if SETTINGS[:locations_enabled]
+    fields += [["Organization", (link_to(host.organization.name, hosts_path(:search => "organization = #{host.organization}")) if host.organization)]] if SETTINGS[:organizations_enabled]
+    if SETTINGS[:login]
+      if host.owner_type == "User"
+        fields += [["Owner", (link_to(host.owner, hosts_path(:search => "user.login = #{host.owner.login}")) if host.owner)]]
+      else
+        fields += [["Owner", host.owner]]
+      end
+    end
     fields += [["Certificate Name", host.certname]] if Setting[:use_uuid_for_certificates]
     fields
   end
