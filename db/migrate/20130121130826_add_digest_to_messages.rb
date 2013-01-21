be21218d8a8bd6724f877536c4878fdb1244aae4
Amos Benari <abenari@redhat.com>
2013-01-21 15:42:56 +0200

    fixes #2126 Large report messages won't fit in PostgreSQL's index_messages_on_value

---
+++ db/migrate/20130121130826_add_digest_to_messages.rb
@@ -0,0 +1,18 @@
+class AddDigestToMessages < ActiveRecord::Migration
+  def self.up
+    if ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql" or ActiveRecord::Base.connection.instance_values["config"][:adapter] == "mysql2"
+      execute "DROP INDEX value ON messages" if index_exists?(:messages, :value, :name => 'value')
+    else
+      remove_index(:messages, :value) if index_exists?(:messages, :value)
+    end
+    add_column :messages, :digest, :string
+    Message.find_each {|m| m.update_attribute(:digest, Digest::SHA1.hexdigest(m.value)) }
+    add_index :messages, :digest
+  end
+
+  def self.down
+    remove_index :messages, :digest
+    remove_column :messages, :digest
+    add_index :messages, :value
+  end
+end
