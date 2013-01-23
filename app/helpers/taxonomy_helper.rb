6aaab7380e0959ea1e2fbead4d7c3515838db706
Amos Benari <abenari@redhat.com>
2013-01-23 16:34:18 +0200

    new org wizard.

--- app/helpers/taxonomy_helper.rb
+++ app/helpers/taxonomy_helper.rb
@@ -49,6 +49,23 @@ module TaxonomyHelper
     controller_name.humanize.titleize
   end
 
+  def wizard_header(current, *args)
+    content_tag(:ul,:class=>"wizard") do
+      step=1
+      content = nil
+      args.each do |arg|
+        step_content = content_tag(:li,(content_tag(:span,step,:class=>"badge" +" #{'badge-inverse' if step==current}")+arg).html_safe, :class=>"#{'active' if step==current}")
+        step == 1 ? content = step_content : content += step_content
+        step += 1
+      end
+      content
+    end
+  end
 
+  def option_button text, href, options
+    field(nil, "", options) do
+      link_to(text, href, options)
+    end
+  end
 
 end
