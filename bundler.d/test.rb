491042508950fd259f56baa10535ecd4743df507
Dominic Cleal <dcleal@redhat.com>
2013-02-09 13:16:23 +0000

    fixes #2367 - Ruby 2.0 support
    * require bundler 1.3, preventing ruby-debug19 getting installed on Ruby 2
    * update 1.9 gems to install on 2.0 too
    * don't use empty arrays in Hash constructor, causes ArgumentError

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,6 +1,6 @@
 group :test do
   gem 'mocha', :require => false
-  gem 'minitest', '~> 3.5', :platforms => :ruby_19
+  gem 'minitest', '~> 3.5', :platforms => [:ruby_19, :ruby_20]
   gem 'single_test'
   gem 'shoulda', "3.0.1"
   gem 'rr'
@@ -11,5 +11,5 @@ group :test do
   gem 'launchy'
   gem 'spork'
   gem 'spork-testunit'
-  gem 'simplecov', :platforms => :ruby_19
+  gem 'simplecov', :platforms => [:ruby_19, :ruby_20]
 end
