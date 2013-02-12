a416fe999f53453ef54a5d76cbdc290760cf06ac
Joseph Mitchell Magen <jmagen@redhat.com>
2013-02-12 17:07:44 +0200

    wrap parameters and group assets gems

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -2,12 +2,12 @@
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
 
-## Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-#ActiveSupport.on_load(:action_controller) do
-#  wrap_parameters :format => [:json]
-#end
-#
-## Disable root element in JSON by default.
-#ActiveSupport.on_load(:active_record) do
-#  self.include_root_in_json = false
-##end
+# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
+ActiveSupport.on_load(:action_controller) do
+ wrap_parameters :format => [:json]
+end
+
+# Disable root element in JSON by default.
+ActiveSupport.on_load(:active_record) do
+ self.include_root_in_json = true
+end
