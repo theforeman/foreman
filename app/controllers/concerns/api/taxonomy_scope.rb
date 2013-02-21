d789576eddd0b9a86db9532a47c339a7aa862ada
Joseph Mitchell Magen <jmagen@redhat.com>
2013-02-21 17:25:41 +0200

    removed InstanceMethods from mixin concerns

--- app/controllers/concerns/api/taxonomy_scope.rb
+++ app/controllers/concerns/api/taxonomy_scope.rb
@@ -7,11 +7,9 @@ module Api
       before_filter :set_taxonomy_scope
     end
 
-    module InstanceMethods
-      def set_taxonomy_scope
-        Location.current ||= @location = Location.find_by_id(params[:location_id]) if SETTINGS[:locations_enabled]
-        Organization.current ||= @organization = Organization.find_by_id(params[:organization_id]) if SETTINGS[:organizations_enabled]
-      end
+    def set_taxonomy_scope
+      Location.current ||= @location = Location.find_by_id(params[:location_id]) if SETTINGS[:locations_enabled]
+      Organization.current ||= @organization = Organization.find_by_id(params[:organization_id]) if SETTINGS[:organizations_enabled]
     end
 
   end
