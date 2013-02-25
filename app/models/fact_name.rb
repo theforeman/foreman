16b4506c7f3e1ed6eab6cc459f551f16622a252c
Joseph Mitchell Magen <jmagen@redhat.com>
2013-02-25 10:36:37 +0200

    added :through for fact_name.rb and added Foreman::STI on base.rb

--- app/models/fact_name.rb
+++ app/models/fact_name.rb
@@ -3,7 +3,7 @@ class FactName < ActiveRecord::Base
   has_many :fact_values, :dependent => :destroy
   has_many :user_facts
   has_many :users, :through => :user_facts
-  has_many_hosts
+  has_many_hosts :through => :fact_values
 
   scope :no_timestamp_fact, :conditions => ["fact_names.name <> ?",:_timestamp]
   scope :timestamp_facts,   :conditions => ["fact_names.name = ?", :_timestamp]
--- app/models/host/base.rb
+++ app/models/host/base.rb
@@ -2,6 +2,7 @@ require 'facts_importer'
 
 module Host
   class Base < ActiveRecord::Base
+    include Foreman::STI
     set_table_name = "hosts"
   end
 end
