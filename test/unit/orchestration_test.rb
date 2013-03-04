3b9133bc51d47959d68cbded131fab0c544c12b4
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-04 16:25:14 +0200

    fixes #2278 orchestration test failure

--- test/unit/orchestration_test.rb
+++ test/unit/orchestration_test.rb
@@ -5,8 +5,4 @@ class OrchestrationTest < ActiveSupport::TestCase
     h = Host.new
     assert_respond_to h, :queue
   end
-
-  def test_action_queue_should_be_an_array
-    assert_kind_of Array, Host.get_external_actions
-  end
 end
