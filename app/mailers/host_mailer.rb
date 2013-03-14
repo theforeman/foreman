0d716f30c6d9fd21689e5aad6fa247ec370f3d74
Daniel Lobato <elobatocs@gmail.com>
2013-03-14 17:20:07 +0100

    New mail rake task sends foreman reports to each user
    HostMailer contained a typo
    
    Hostgroup and environment are included in report, plus various styling fixes
    
    Changed deliver to avoid 3.0 deprecation
    
    Fixed typo and mail information to support 3.2 action mailer syntax
    
    Friendly message if foreman_url is not set

--- app/mailers/host_mailer.rb
+++ app/mailers/host_mailer.rb
@@ -12,9 +12,7 @@ class HostMailer < ActionMailer::Base
     # options our host list if required
     filter = []
 
-    if (@url = Setting[:foreman_url]).empty?
-      raise ::Foreman::Exception.new N_("':foreman_url:' entry in Foreman configuration file, see http://theforeman.org/projects/foreman/wiki/Mail_Notifications")
-    end
+    set_url
 
     if options[:env]
       hosts = envhosts = options[:env].hosts
@@ -40,14 +38,7 @@ class HostMailer < ActionMailer::Base
     raise ::Foreman::Exception.new(N_("unable to find recipients")) if email.empty?
     time = options[:time] || 1.day.ago
     host_data = Report.summarise(time, hosts.all).sort
-    total_metrics = {"failed"=>0, "restarted"=>0, "skipped"=>0, "applied"=>0, "failed_restarts"=>0}
-    host_data.flatten.delete_if { |x| true unless x.is_a?(Hash) }.each do |data_hash|
-      total_metrics["failed"]          += data_hash[:metrics]["failed"]
-      total_metrics["restarted"]       += data_hash[:metrics]["restarted"]
-      total_metrics["skipped"]         += data_hash[:metrics]["skipped"]
-      total_metrics["applied"]         += data_hash[:metrics]["applied"]
-      total_metrics["failed_restarts"] += data_hash[:metrics]["failed_restarts"]
-    end
+    total_metrics = load_metrics(host_data)
     total = 0 ; total_metrics.values.each { |v| total += v }
     subject = _("Summary Puppet report from Foreman - F:%{failed} R:%{restarted} S:%{skipped} A:%{applied} FR:%{failed_restarts} T:%{total}") % {
       :failed => total_metrics["failed"],
@@ -62,16 +53,58 @@ class HostMailer < ActionMailer::Base
     @out_of_sync = hosts.out_of_sync
     @disabled = hosts.alerts_disabled
     @filter = filter
-    mail(:to => email, :subject => subject)
+    mail(:to   => email,
+         :from => Setting["email_reply_address"],
+         :subject => subject,
+         :date => Time.now )
   end
 
   def error_state(report)
     host = report.host
-    email = host.owner.recipients if SETTINGS[:login] and not host.owner.nil?
+    email = host.owner.recipients if SETTINGS[:login] && host.owner.present?
     email = Setting[:administrator]   if email.empty?
     raise ::Foreman::Exception.new(N_("unable to find recipients")) if email.empty?
     @report = report
     @host = host
     mail(:to => email, :subject => (_("Puppet error on %s") % host.to_label))
   end
+
+  def failed_runs(user, options = {})
+    set_url
+    time = options[:time] || 1.day.ago
+    host_data = Report.summarise(time, user.hosts).sort
+    total_metrics = load_metrics(host_data)
+    total = 0 ; total_metrics.values.each { |v| total += v }
+    @hosts = host_data.sort_by { |h| h[1][:metrics]['failed'] }.reverse
+    @timerange = time
+    @out_of_sync = Host.out_of_sync.select { |h| h.owner == user }
+    @disabled = Host.alerts_disabled.select { |h| h.owner == user }
+    mail(:to   => user.mail,
+         :from => Setting["email_reply_address"],
+         :subject => _("Summary Puppet report from Foreman - F:#{total_metrics["failed"]} R:#{total_metrics["restarted"]} S:#{total_metrics["skipped"]} A:#{total_metrics["applied"]} FR:#{total_metrics["failed_restarts"]} T:#{total}"),
+         :date => Time.now )
+  end
+
+  def set_url
+    if (@url = Setting[:foreman_url]).empty?
+      raise ":foreman_url is not set, please configure in the Foreman Web UI (More -> Settings -> General)"
+    end
+  end
+
+
+  def load_metrics(host_data)
+    total_metrics = {"failed"=>0, "restarted"=>0, "skipped"=>0, "applied"=>0, "failed_restarts"=>0}
+
+    host_data.flatten.delete_if { |x| true unless x.is_a?(Hash) }.each do |data_hash|
+      total_metrics["failed"]          += data_hash[:metrics]["failed"]
+      total_metrics["restarted"]       += data_hash[:metrics]["restarted"]
+      total_metrics["skipped"]         += data_hash[:metrics]["skipped"]
+      total_metrics["applied"]         += data_hash[:metrics]["applied"]
+      total_metrics["failed_restarts"] += data_hash[:metrics]["failed_restarts"]
+    end
+
+    total_metrics
+  end
+
 end
+
