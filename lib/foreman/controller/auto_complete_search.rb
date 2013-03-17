b67b508a68341df8fedf610fb4ab4837ba25aef3
Ohad Levy <ohadlevy@gmail.com>
2013-03-17 21:42:37 +0200

    fixes #2332 workaround for broken search due to STI changes

--- lib/foreman/controller/auto_complete_search.rb
+++ lib/foreman/controller/auto_complete_search.rb
@@ -1,7 +1,7 @@
 module Foreman::Controller::AutoCompleteSearch
   def auto_complete_search
     begin
-      @items = eval(controller_name.singularize.camelize).complete_for(params[:search])
+      @items = controller_name.singularize.camelize.constantize.complete_for(params[:search])
       @items = @items.map do |item|
         category = (['and','or','not','has'].include?(item.to_s.sub(/^.*\s+/,''))) ? 'Operators' : ''
         part = item.to_s.sub(/^.*\b(and|or)\b/i) {|match| match.sub(/^.*\s+/,'')}
