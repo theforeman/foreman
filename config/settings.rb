c71be0bcee657ff0861fbe01636aa120e6a4d5f4
Greg Sutcliffe <gsutclif@redhat.com>
2013-03-26 12:32:28 +0000

    Add basic support for plugin config to live in separate files

--- config/settings.rb
+++ config/settings.rb
@@ -6,3 +6,8 @@ SETTINGS[:unattended] = SETTINGS[:unattended].nil? || SETTINGS[:unattended]
 SETTINGS[:login]    ||= SETTINGS[:ldap]
 SETTINGS[:puppetconfdir] ||= '/etc/puppet'
 SETTINGS[:puppetvardir]  ||= '/var/lib/puppet'
+
+# Load plugin config, if any
+Dir["#{root}/config/settings.plugins.d/*.yaml"].each do |f|
+  SETTINGS.merge! YAML.load_file f
+end
