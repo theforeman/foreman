da953c39956c2967e5abdf75f856f359363eafa2
Lukas Zapletal <lzap+git@redhat.com>
2013-03-28 13:13:09 +0100

    i18n - adding mark_translated option

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -13,10 +13,16 @@ FastGettext.add_text_domain 'foreman', :path => locale_dir, :type => locale_type
 FastGettext.default_available_locales = ['en'] + Dir.entries(locale_dir).reject {|d| d =~ /(^\.|pot$)/ }
 FastGettext.default_text_domain = 'foreman'
 
-module FastGettext::Translation
-  alias :old_ :_
+# When mark_translated setting is set, we will wrap all translated strings
+# which is useful when translating code base.
+if SETTINGS[:mark_translated]
+  module FastGettext
+    module Translation
+      alias :old_ :_
 
-  def _(key)
-    "X" + old_(key) + "X"
+      def _(key)
+        "X" + old_(key) + "X"
+      end
+    end
   end
 end
