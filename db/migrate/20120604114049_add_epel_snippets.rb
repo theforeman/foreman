cb623c5ebf0ba1d007cd2ea29d113c75bf9e6dc4
Dominic Cleal <dcleal@redhat.com>
2013-03-29 20:28:53 +0000

    fixes #2358 - remove duplicate epel/HTTP proxy snippets

--- db/migrate/20120604114049_add_epel_snippets.rb
+++ db/migrate/20120604114049_add_epel_snippets.rb
@@ -1,7 +1,7 @@
 class AddEpelSnippets < ActiveRecord::Migration
   class ConfigTemplate < ActiveRecord::Base; end
   def self.up
-    ConfigTemplate.create( :name => "epel", :snippet => true, :template => File.read("#{Rails.root}/app/views/unattended/snippets/_epel.erb"))
+    ConfigTemplate.find_or_create_by_name( :name => "epel", :snippet => true, :template => File.read("#{Rails.root}/app/views/unattended/snippets/_epel.erb"))
   end
 
   def self.down
