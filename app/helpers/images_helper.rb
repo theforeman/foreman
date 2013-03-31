c3e3727314600e67b6275058132a23e0abda97e8
Joseph Mitchell Magen <jmagen@redhat.com>
2013-03-31 13:02:52 +0300

    fixes #2362 ImagesHelper - not showing field if no images

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,10 +1,9 @@
 module ImagesHelper
   def image_field f
-    if @compute_resource.capabilities.include?(:image)
-      images = @compute_resource.available_images
-      if images.any?
-        return select_f(f, :uuid, images.to_a.sort! { |a, b| a.name.downcase <=> b.name.downcase }, :id, :name, {}, :label => 'Image')
-      end
+    return unless @compute_resource.capabilities.include?(:image)
+    images = @compute_resource.available_images
+    if images.any?
+      return select_f(f, :uuid, images.to_a.sort! { |a, b| a.name.downcase <=> b.name.downcase }, :id, :name, {}, :label => 'Image')
     else
       text_f f, :uuid, :label => "Image ID", :help_inline => "Image ID as provided by the compute resource, e.g. ami-.."
     end
