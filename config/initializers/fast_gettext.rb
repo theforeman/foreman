23c30e3458820629fddb7b7a5dd942c4c53040f1
Lukas Zapletal <lzap+git@redhat.com>
2013-04-02 09:45:13 +0200

    renaming gettext domain to 'foreman' and adding Makefile

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -2,7 +2,13 @@ require 'fast_gettext'
 require 'gettext_i18n_rails'
 
 locale_dir = File.join(File.dirname(__FILE__), '..', '..', 'locale')
+if Rails.env.development?
+  # no need to generate MO files for development mode
+  locale_type = :po
+else
+  locale_type = :mo
+end
 
-FastGettext.add_text_domain 'app', :path => locale_dir, :type => :po
-FastGettext.default_available_locales = ['en','ja'] #all you want to allow
-FastGettext.default_text_domain = 'app'
+FastGettext.add_text_domain 'foreman', :path => locale_dir, :type => locale_type
+FastGettext.default_available_locales = ['en'] + Dir.entries(locale_dir).reject {|d| d =~ /(^\.|pot$)/ }
+FastGettext.default_text_domain = 'foreman'
