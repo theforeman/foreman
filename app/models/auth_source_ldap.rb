30ff1cbc5a323dc31c2e1106b99d0b8e48b002ff
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-02 16:58:11 +0300

    fixes #2371 - removed iconv - causing deprecation notice - block in require: iconv will be deprecated in the future, use String#encode instead

--- app/models/auth_source_ldap.rb
+++ app/models/auth_source_ldap.rb
@@ -16,7 +16,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 require 'net/ldap'
-require 'iconv'
 
 class AuthSourceLdap < AuthSource
   validates_presence_of :host, :port
