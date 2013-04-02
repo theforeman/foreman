80422362c0e223ff3821afd427e610a6dbfd6acc
Joseph Mitchell Magen <jmagen@redhat.com>
2013-04-02 16:48:09 +0300

    fixes #2366 removes duplicate gems in bundler/test.rb

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,16 +1,11 @@
 group :test do
-  gem 'capybara'
-  gem 'database_cleaner'
-  gem 'launchy'
   gem 'mocha', :require => false
   gem 'minitest', '~> 3.5', :platforms => :ruby_19
   gem 'single_test'
   gem 'shoulda', "3.0.1"
   gem 'rr'
   gem 'rake'
-  gem 'single_test'
   gem 'ci_reporter', '>= 1.6.3', :require => false
-  gem 'minitest', '~> 3.5', :platforms => :ruby_19
   gem 'capybara'
   gem 'database_cleaner'
   gem 'launchy'
