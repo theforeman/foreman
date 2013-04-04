2a9617f3f3e43930328bb39a558dd0e5452f66a3
Ohad Levy <ohadlevy@gmail.com>
2013-04-04 12:23:09 +0300

    adds JS lint

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -7,6 +7,7 @@ group :development do
   gem 'pry'
   gem "term-ansicolor"
 #  gem 'rack-mini-profiler'
+  gem 'jslint_on_rails'
 
   # for generating i18n files
   gem 'gettext', '>= 1.9.3', :require => false
