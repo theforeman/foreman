b102d12bfed509b353bab297b9ddc9d14b572bb2
Amos Benari <abenari@redhat.com>
2013-04-10 13:29:36 +0300

    changed default pie chart colors.

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -4,6 +4,7 @@ $.fn.flot_pie = function(){
     var target = $(el);
 
     $.plot(target, target.data('series'), {
+      colors: ['#4572A7','#AA4643','#89A54E','#80699B','#3D96AE','#DB843D','#92A8CD','#A47D7C','#B5CA92'],
       series: {
         pie: options.pie || {
           show: true,
