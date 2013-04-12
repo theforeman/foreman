32bf6e02a03f6d9dc38e58c8e084cd4977688d7d
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-04-12 19:03:45 +0100

    fixes #2428 - a workaround for an issue with SETTINGS[:libvirt] not being properly initialized in rpm environments

--- config/boot.rb
+++ config/boot.rb
@@ -3,21 +3,4 @@ require 'yaml'
 require File.expand_path('../../config/settings', __FILE__)
 
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
-if File.exists?(ENV['BUNDLE_GEMFILE'])
-  require 'bundler/setup'
-
-  begin
-    if SETTINGS[:unattended]
-      Bundler.setup(:unattended)
-      Bundler.setup(:libvirt)
-      require 'libvirt'
-      SETTINGS[:libvirt] = true
-    else
-      SETTINGS[:libvirt] = false
-    end
-  rescue LoadError
-    print "Libvirt bindings are missing - hypervisor management is disabled"
-    SETTINGS[:libvirt] = false
-  end
-end
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
