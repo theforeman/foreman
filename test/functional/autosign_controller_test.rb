86db4a2f12863c3d56503ec7b6dc50940d6a131f
Dominic Cleal <dcleal@redhat.com>
2013-05-06 18:26:18 +0100

    fixes #2463 - move test files to follow controller move

--- test/functional/autosign_controller_test.rb
+++ test/functional/autosign_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class SmartProxies::PuppetcaControllerTest < ActionController::TestCase
+class AutosignControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
