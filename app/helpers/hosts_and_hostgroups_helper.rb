3cda1a7360cb4f27b7dff8db6717cdc91e1ca904
Dominic Cleal <dcleal@redhat.com>
2013-05-10 10:09:16 +0100

    fixes #2453 - fix aggressive truncation of hostgroups < 26 chars

--- app/helpers/hosts_and_hostgroups_helper.rb
+++ app/helpers/hosts_and_hostgroups_helper.rb
@@ -3,7 +3,7 @@ module HostsAndHostgroupsHelper
     return if hostgroup.blank?
     options = (hostgroup.label.to_s.size > max_length) ? {:'data-original-title'=> hostgroup.label, :rel=>'twipsy'} : {}
     nesting = hostgroup.label.to_s.gsub(/[^\/]+\/?$/, "")
-    nesting = truncate(nesting, :length => max_length - hostgroup.label.to_s.size) if nesting.to_s.size > 0
+    nesting = truncate(nesting, :length => max_length - hostgroup.name.to_s.size) if nesting.to_s.size > 0
     name =  truncate(hostgroup.name, :length => max_length - nesting.to_s.size)
     link_to_if_authorized(
         content_tag(:span,
