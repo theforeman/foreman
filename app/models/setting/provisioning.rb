710dfa86835e7b8b430cf59646a7888746c3b0bc
Ryan Davies <ryan@ryandavies.co.nz>
2013-05-21 18:41:14 +1200

    fixes #2562 libvirt domain listen address, add libvirt_default_console_address setting

--- app/models/setting/provisioning.rb
+++ app/models/setting/provisioning.rb
@@ -17,7 +17,8 @@ class Setting::Provisioning < Setting
         self.set('ignore_puppet_facts_for_provisioning', N_("Does not update ipaddress and MAC values from Puppet facts"), false),
         self.set('query_local_nameservers', N_("Should Foreman query the locally configured name server or the SOA/NS authorities"), false),
         self.set('remote_addr', N_("If Foreman is running behind Passenger or a remote loadbalancer, the ip should be set here"), "127.0.0.1"),
-        self.set('token_duration', N_("Time in minutes installation tokens should be valid for, 0 to disable"), 0)
+        self.set('token_duration', N_("Time in minutes installation tokens should be valid for, 0 to disable"), 0),
+        self.set('libvirt_default_console_address', N_("The IP address that should be used for the console listen address when provisioning new virtual machines via Libvirt"), "0.0.0.0")
       ].each { |s| self.create s.update(:category => "Setting::Provisioning")}
     end
 
