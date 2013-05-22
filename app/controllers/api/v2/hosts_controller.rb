8054c66e8279d29aad041f422100e759a36f571e
Joseph Mitchell Magen <jmagen@redhat.com>
2013-05-22 16:46:22 +0300

    fixes #2539 - added API puppetrun access permission and test

--- app/controllers/api/v2/hosts_controller.rb
+++ app/controllers/api/v2/hosts_controller.rb
@@ -2,13 +2,13 @@ module Api
   module V2
     class HostsController < V1::HostsController
       include Api::Version2
-      include Api::TaxonomyScope
 
-      before_filter :find_resource, :only => %w{show update destroy status puppetrun}
+      before_filter :find_resource, :only => :puppetrun
 
       api :GET, "/hosts/:id/puppetrun", "Force a puppet run on the agent."
 
       def puppetrun
+        return deny_access unless Setting[:puppetrun]
         process_response @host.puppetrun!
       end
 
