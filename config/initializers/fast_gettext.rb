40b31d27c0b45a7bc4d22411cd3dcc3551b3ab0f
Tomas Strachota <tstrachota@redhat.com>
2013-05-23 14:57:57 +0200

    fixes #2536 - loading locale .mo files in production mode
    - initializer script fixed to look for *.mo files
    - Makefile updated to place *.mo into /locale/*/LC_MESSAGES
      (rake gettext:pack places them there as well)
    - *.mo files added to foreman.spec

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,12 +1,18 @@
 require 'fast_gettext'
 require 'gettext_i18n_rails'
 
-locale_dir = File.join(File.dirname(__FILE__), '..', '..', 'locale')
+locale_dir = File.join(Rails.root, 'locale')
+locale_domain = 'foreman'
+
 if Rails.env.development?
   # no need to generate MO files for development mode
   locale_type = :po
+  locale_search_path = File.join(locale_dir, "*", "#{locale_domain}.#{locale_type}")
+  locale_search_re   = Regexp.new(".*/([^/]*)/#{locale_domain}.#{locale_type}")
 else
   locale_type = :mo
+  locale_search_path = File.join(locale_dir, "*", "LC_MESSAGES", "#{locale_domain}.#{locale_type}")
+  locale_search_re   = Regexp.new(".*/([^/]*)/LC_MESSAGES/#{locale_domain}.#{locale_type}")
 end
 
 begin
@@ -14,21 +20,20 @@ begin
     # in test mode we do not support i18n
     FastGettext.default_available_locales = ['en']
   else
-    FastGettext.default_available_locales =
-      Dir.glob("#{Rails.root}/locale/*/foreman.#{locale_type}").collect {|x| x.split('/')[-2]}
+    FastGettext.default_available_locales = Dir.glob(locale_search_path).collect {|f| locale_search_re.match(f)[1] }
   end
 rescue Exception => e
-  Rails.log.warn "Unable to set available locales: #{e}"
+  Rails.logger.warn "Unable to set available locales: #{e}"
   FastGettext.default_available_locales = ['en']
 end
 
 # initialize fast gettext
-FastGettext.add_text_domain 'foreman',
+FastGettext.add_text_domain locale_domain,
   :path => locale_dir,
   :type => locale_type,
   :ignore_fuzzy => true,
   :report_warning => false
-FastGettext.default_text_domain = 'foreman'
+FastGettext.default_text_domain = locale_domain
 
 # create list of human-readable locale names
 FastGettext.class.class_eval { attr_accessor :human_available_locales }
