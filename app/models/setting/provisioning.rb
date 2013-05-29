503af43dee30f67da1eb8c46ae4428926155297b
Ryan Davies <ryan@ryandavies.co.nz>
2013-05-29 19:25:16 +1200

    fixes #2562 for libvirt domain listen address Removed Migration in favour of settings helper

--- app/models/setting/provisioning.rb
+++ app/models/setting/provisioning.rb
@@ -18,6 +18,7 @@ class Setting::Provisioning < Setting
         self.set('query_local_nameservers', N_("Should Foreman query the locally configured name server or the SOA/NS authorities"), false),
         self.set('remote_addr', N_("If Foreman is running behind Passenger or a remote loadbalancer, the ip should be set here"), "127.0.0.1"),
         self.set('token_duration', N_("Time in minutes installation tokens should be valid for, 0 to disable"), 0)
+        self.set('default_console_address', N_("The IP address that should be used for the console listen address when provisioning new virtual machines via Libvirt"), "0")
       ].each { |s| self.create s.update(:category => "Setting::Provisioning")}
     end
 
