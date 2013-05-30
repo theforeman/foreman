deb8c4437ba380cfac6575b8574178cb743bf558
Ohad Levy <ohadlevy@gmail.com>
2013-05-30 15:03:03 +0300

    fixes #1951 - support for libvirt named networks
    based on Dominic initial pull request 308
    (cherry picked from commit c5c84034b1f6dec25c14340861a48175b49e682a)

--- app/helpers/compute_resources_vms_helper.rb
+++ app/helpers/compute_resources_vms_helper.rb
@@ -42,4 +42,13 @@ module ComputeResourcesVmsHelper
     options
   end
 
+  def libvirt_networks(compute)
+    networks   = compute.networks
+    interfaces = compute.interfaces
+    select     = []
+    select << [_('Physical (Bridge)'), :bridge]
+    select << [_('Virtual (NAT)'), :network] if networks.any?
+    select
+  end
+
 end
