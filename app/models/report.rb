8e0d7ee3010a468e30bcf597578580334247423e
Amos Benari <abenari@redhat.com>
2013-06-03 15:47:25 +0300

    fixes #2604 searching for hosts with digits can lead to unexpected results (cherry picked from commit 273b110af4c3f9416ec237f4880ce0406d5f795c)

--- app/models/report.rb
+++ app/models/report.rb
@@ -15,7 +15,7 @@ class Report < ActiveRecord::Base
   scoped_search :in => :messages, :on => :value,                         :rename => :log
   scoped_search :in => :sources,  :on => :value,                         :rename => :resource
 
-  scoped_search :on => :reported_at, :complete_value => true, :default_order => :desc,    :rename => :reported
+  scoped_search :on => :reported_at, :complete_value => true, :default_order => :desc,    :rename => :reported, :only_explicit => true
   scoped_search :on => :status, :offset => 0, :word_size => 4*BIT_NUM, :complete_value => {:true => true, :false => false}, :rename => :eventful
 
   scoped_search :on => :status, :offset => METRIC.index("applied"),         :word_size => BIT_NUM, :rename => :applied
