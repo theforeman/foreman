5b5af86ecd6f6b147aeb4dd4ceadd890dc9f2699
Greg Sutcliffe <gsutclif@redhat.com>
2013-06-10 16:31:03 +0100

    Fixes #2644 - Refactor Host find_by_name to use several ways to find a host, and refactor my_hosts scope to Host::Base, allowing us to remove unnecessary permissions check from find_by_name

--- app/models/host/base.rb
+++ app/models/host/base.rb
@@ -14,6 +14,43 @@ module Host
     validates_uniqueness_of :name
     validate :is_name_downcased?
 
+    scope :my_hosts, lambda {
+      user                 = User.current
+      return { :conditions => "" } if user.admin? # Admin can see all hosts
+
+      owner_conditions             = sanitize_sql_for_conditions(["((hosts.owner_id in (?) AND hosts.owner_type = 'Usergroup') OR (hosts.owner_id = ? AND hosts.owner_type = 'User'))", user.my_usergroups.map(&:id), user.id])
+      domain_conditions            = sanitize_sql_for_conditions([" (hosts.domain_id in (?))",dms = (user.domain_ids)])
+      compute_resource_conditions  = sanitize_sql_for_conditions([" (hosts.compute_resource_id in (?))",(crs = user.compute_resource_ids)])
+      hostgroup_conditions         = sanitize_sql_for_conditions([" (hosts.hostgroup_id in (?))",(hgs = user.hostgroup_ids)])
+      organization_conditions      = sanitize_sql_for_conditions([" (hosts.organization_id in (?))",orgs = (user.organization_ids)])
+      location_conditions          = sanitize_sql_for_conditions([" (hosts.location_id in (?))",locs = (user.location_ids)])
+
+      fact_conditions = ""
+      for user_fact in (ufs = user.user_facts)
+        fact_conditions += sanitize_sql_for_conditions ["(hosts.id = fact_values.host_id and fact_values.fact_name_id = ? and fact_values.value #{user_fact.operator} ?)", user_fact.fact_name_id, user_fact.criteria]
+        fact_conditions = user_fact.andor == "and" ? "(#{fact_conditions}) and " : "#{fact_conditions} or  "
+      end
+      if (match = fact_conditions.match(/^(.*).....$/))
+        fact_conditions = "(#{match[1]})"
+      end
+
+      conditions = ""
+      if user.filtering?
+        conditions  = "#{owner_conditions}"                                                                                                                                 if     user.filter_on_owner
+        (conditions = (user.domains_andor           == "and") ? "(#{conditions}) and #{domain_conditions} "           : "#{conditions} or #{domain_conditions} ")           unless dms.empty?
+        (conditions = (user.compute_resources_andor == "and") ? "(#{conditions}) and #{compute_resource_conditions} " : "#{conditions} or #{compute_resource_conditions} ") unless crs.empty?
+        (conditions = (user.hostgroups_andor        == "and") ? "(#{conditions}) and #{hostgroup_conditions} "        : "#{conditions} or #{hostgroup_conditions} ")        unless hgs.empty?
+        (conditions = (user.facts_andor             == "and") ? "(#{conditions}) and #{fact_conditions} "             : "#{conditions} or #{fact_conditions} ")             unless ufs.empty?
+        (conditions = (user.organizations_andor     == "and") ? "(#{conditions}) and #{organization_conditions} "     : "#{conditions} or #{organization_conditions} ")     unless orgs.empty?
+        (conditions = (user.locations_andor         == "and") ? "(#{conditions}) and #{location_conditions} "         : "#{conditions} or #{location_conditions} ")         unless locs.empty?
+        conditions.sub!(/\s*\(\)\s*/, "")
+        conditions.sub!(/^(?:\(\))?\s?(?:and|or)\s*/, "")
+        conditions.sub!(/\(\s*(?:or|and)\s*\(/, "((")
+      end
+
+      { :joins => ufs.empty? ? nil : :fact_values,
+        :conditions => conditions }
+    }
     def self.attributes_protected_by_default
       super - [ inheritance_column ]
     end
