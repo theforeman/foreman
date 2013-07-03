719624779584455d1a492fccd8d61a2729b942c1
Greg Sutcliffe <gsutclif@redhat.com>
2013-07-03 18:09:50 +0100

    Fixes #2576 - Add optional update of Host.ip from built request

--- app/models/setting/provisioning.rb
+++ app/models/setting/provisioning.rb
@@ -18,7 +18,8 @@ class Setting::Provisioning < Setting
         self.set('query_local_nameservers', N_("Should Foreman query the locally configured name server or the SOA/NS authorities"), false),
         self.set('remote_addr', N_("If Foreman is running behind Passenger or a remote load balancer, the IP should be set here. This is a regular expression, so it can support several load balancers, i.e: (10.0.0.1|127.0.0.1)"), "127.0.0.1"),
         self.set('token_duration', N_("Time in minutes installation tokens should be valid for, 0 to disable"), 0),
-        self.set('libvirt_default_console_address', N_("The IP address that should be used for the console listen address when provisioning new virtual machines via Libvirt"), "0.0.0.0")
+        self.set('libvirt_default_console_address', N_("The IP address that should be used for the console listen address when provisioning new virtual machines via Libvirt"), "0.0.0.0"),
+        self.set('update_ip_from_built_request', N_("Should we use the originating IP of the built request to update the host's IP?"), false)
       ].each { |s| self.create! s.update(:category => "Setting::Provisioning")}
     end
 
