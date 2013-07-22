241053f5712689a6292f139a60a7c13059e54426
Joseph Mitchell Magen <jmagen@redhat.com>
2013-07-22 11:26:40 +0300

    fixes 2812 - method lookup_values_attributes= not defined in Host::Managed (NameError)

--- test/unit/host_test.rb
+++ test/unit/host_test.rb
@@ -727,4 +727,14 @@ class HostTest < ActiveSupport::TestCase
     assert rundeck[h.name]['tags'].include?('ipaddress=10.0.19.33'), 'ipaddress fact missing'
   end
 
+  test "should accept lookup_values_attributes" do
+    h = hosts(:redhat)
+    puts h.lookup_values.inspect
+    as_admin do
+      assert_difference "LookupValue.count" do
+        assert h.update_attributes(:lookup_values_attributes => {"0" => {:lookup_key_id => lookup_keys(:one).id, :value => "8080" }})
+      end
+    end
+  end
+
 end
