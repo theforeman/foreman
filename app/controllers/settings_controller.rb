ed455d32a231e8e2d860af4f86eb5a163d62ac1b
Amos Benari <abenari@redhat.com>
2013-08-01 01:26:10 +0300

    fixes #2766 Setting categories not consistently loaded and shown in dev

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -2,6 +2,11 @@ class SettingsController < ApplicationController
   include Foreman::Controller::AutoCompleteSearch
   before_filter :require_admin
 
+  #This can happen in development when removing a plugin
+  rescue_from ActiveRecord::SubclassNotFound do |e|
+    render :text=> (e.to_s+"<br><b>run Setting.delete_all(:category=>'#{e.to_s.match(/\'(Setting::.*)\'\./)[1] rescue 'STI-Type'}') to recover.</b>").html_safe , :status=> 500
+  end
+
   def index
     @settings = Setting.live_descendants.search_for(params[:search])
     respond_to do |format|
