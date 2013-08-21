9b566a18d72133e3b2a7ea06ba093aa6c3ee33a4
Jeremy Kitchen <jeremy@nationbuilder.com>
2013-08-21 15:54:48 -0700

    unit tests for new legacy_puppet_hostname setting

--- test/unit/smart_proxy_test.rb
+++ test/unit/smart_proxy_test.rb
@@ -26,4 +26,22 @@ def test_should_not_be_modified_if_has_no_leading_slashes
     end
     assert_equal proxy.url, "http://some.proxy:4568"
   end
+
+  def test_should_honor_legacy_puppet_hostname_true_setting
+    Setting[:legacy_puppet_hostname] = true
+    proxy = SmartProxy.new
+    proxy.name = "test proxy"
+    proxy.url = "http://puppet.example.com:4568"
+
+    assert_equal proxy.to_s, "puppet"
+  end
+
+  def test_should_honor_legacy_puppet_hostname_false_setting
+    Setting[:legacy_puppet_hostname] = false
+    proxy = SmartProxy.new
+    proxy.name = "test proxy"
+    proxy.url = "http://puppet.example.com:4568"
+
+    assert_equal proxy.to_s, "puppet.example.com"
+  end
 end
