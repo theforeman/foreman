2164f95640dd7ca6a917aeec24ab16cbb3dac44e
Marek Hulan <mhulan@redhat.com>
2013-09-05 11:35:59 +0200

    Fixes #3043 - Support Signo on API level using BasicAuth

--- app/services/sso.rb
+++ app/services/sso.rb
@@ -1,5 +1,5 @@
 module SSO
-  METHODS = [Apache, Basic, Oauth, Signo]
+  METHODS = [Apache, SignoBasic, Basic, Oauth, Signo]
 
   def self.get_available(controller)
     all_methods = all.map { |method| method.new(controller) }
