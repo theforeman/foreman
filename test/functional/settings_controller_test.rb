62527f435e944384e434c2a013e7823d4372b46b
Joseph Mitchell Magen <jmagen@redhat.com>
2013-09-17 15:30:13 +0300

    fixes #3089 - Updating settings causes settings page to load again within the page

--- test/functional/settings_controller_test.rb
+++ test/functional/settings_controller_test.rb
@@ -8,8 +8,12 @@ class SettingsControllerTest < ActionController::TestCase
 
   def test_update_valid
     Setting::General.any_instance.stubs(:valid?).returns(true)
-    put :update, {:id => Setting::General.first, :setting => {}}, set_session_user
-    assert_redirected_to settings_url
+    new_value = 'root@another.com'
+    put :update, {:id => settings(:attributes1).to_param, :setting => {:value => new_value}, :format => :json}, set_session_user
+    assert :success
+    response = ActiveSupport::JSON.decode(@response.body)
+    assert !response.empty?
+    assert_equal new_value, response['setting']['value']
   end
 
   test "can render a new sti type setting" do
@@ -23,6 +27,12 @@ class SettingsControllerTest < ActionController::TestCase
     assert Setting.create(:name => "foo", :default => "bar", :description => "test foo", :category => "Setting::Invalid")
     get :index, {}, set_session_user
     assert_no_match /id='Invalid'/, @response.body
+  end
+
+  test "invalid inline edit of string on integer field" do
+    put :update, {:id => settings(:attributes16).to_param, :setting => {:value => '25aaaa'}, :format => :json}, set_session_user
+    assert_equal 'Value is not a number', assigns(:setting).errors.full_messages.first
+    assert :unprocessable_entity
   end
 
 end
