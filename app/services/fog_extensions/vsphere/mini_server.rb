6975551cec3bf12717ce7664c52354784f7776ba
Martin Matuska <martin@matuska.org>
2013-09-19 00:07:47 +0200

    fix #2093 vmware compute resources don't support folders store vSphere folder path string in MiniServer object add folder column to "Virtual Machines" view

--- app/services/fog_extensions/vsphere/mini_server.rb
+++ app/services/fog_extensions/vsphere/mini_server.rb
@@ -4,17 +4,22 @@ module FogExtensions
       attr_reader :name, :identity, :cpus, :memory, :ready
       alias_method :ready?, :ready
 
-      def initialize raw
+      def initialize (raw, path = nil)
         @raw      = raw
         @name     = raw.name
         @identity = raw.config.instanceUuid
         @cpus     = raw.config.hardware.numCPU
         @memory   = raw.config.hardware.memoryMB * 1024 * 1024
         @ready    = raw.runtime.powerState == "poweredOn"
+        @path     = path
       end
 
       def state
         raw.runtime.powerState
+      end
+
+      def path
+        @path
       end
 
       private
