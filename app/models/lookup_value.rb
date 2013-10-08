d8f976c551d328f187d8bdddc940c416bc0c280b
Joseph Magen <joseph@isratrade.co.il>
2013-10-08 11:26:58 +0300

    fixes #3206 - prevent any user including admin from creating lookup_value with fdqn= or hostgroup= if host/hostgroup does not exist

--- app/models/lookup_value.rb
+++ app/models/lookup_value.rb
@@ -6,7 +6,8 @@ class LookupValue < ActiveRecord::Base
   delegate :key, :to => :lookup_key
   before_validation :sanitize_match
   before_validation :validate_and_cast_value
-  validate :validate_list, :validate_regexp
+  validate :validate_list, :validate_regexp, :ensure_fqdn_exists, :ensure_hostgroup_exists
+
   attr_accessor :host_or_hostgroup
 
   serialize :value
@@ -53,6 +54,20 @@ class LookupValue < ActiveRecord::Base
   def validate_list
     return true unless (lookup_key.validator_type == 'list')
     errors.add(:value, _("%{value} is not one of %{rules}") % { :value => value, :rules => lookup_key.validator_rule }) and return false unless lookup_key.validator_rule.split(LookupKey::KEY_DELM).map(&:strip).include?(value)
+  end
+
+  def ensure_fqdn_exists
+    md = match.match(/^fqdn=(.*)/)
+    return true unless md
+    return true if Host.unscoped.find_by_name(md[1]) || host_or_hostgroup.try(:new_record?)
+    errors.add(:match, _("%{match} does not match an existing host") % { :match => match }) and return false
+  end
+
+  def ensure_hostgroup_exists
+    md = match.match(/^hostgroup=(.*)/)
+    return true unless md
+    return true if Hostgroup.unscoped.find_by_name(md[1]) || Hostgroup.unscoped.find_by_label(md[1]) || host_or_hostgroup.try(:new_record?)
+    errors.add(:match, _("%{match} does not match an existing hostgroup") % { :match => match }) and return false
   end
 
   private
