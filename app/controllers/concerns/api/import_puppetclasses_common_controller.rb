b1f7f970006835b144f6078ebb0bf5efcddb142d
Petr Chalupa <pchalupa@redhat.com>
2013-11-04 18:31:12 +0100

    fixes #3577 - do not treat `params[:dryrun]` with value `false` as true

--- app/controllers/concerns/api/import_puppetclasses_common_controller.rb
+++ app/controllers/concerns/api/import_puppetclasses_common_controller.rb
@@ -22,7 +22,10 @@ module Api::ImportPuppetclassesCommonController
 
     # DRYRUN - /import_puppetclasses?dryrun - do not run PuppetClassImporter
     rabl_template = @environment ? 'show' : 'index'
-    render("api/v1/import_puppetclasses/#{rabl_template}", :layout => "api/layouts/import_puppetclasses_layout") and return if (params.keys.include?('dryrun') && params['dryrun'] != 'false')
+    if params.key?('dryrun') && !['false', false].include?(params['dryrun'])
+      render("api/v1/import_puppetclasses/#{rabl_template}", :layout => "api/layouts/import_puppetclasses_layout")
+      return
+    end
 
     # RUN PuppetClassImporter
     if (errors = ::PuppetClassImporter.new.obsolete_and_new(@changed)).empty?
