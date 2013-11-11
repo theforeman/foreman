e9cb7fc3ef6e2bf66748991e51e9c0249953397d
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2013-11-11 16:48:02 +0000

    Feature #3622: smart proxy features are now being loaded dynamically

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -2,13 +2,9 @@ class Feature < ActiveRecord::Base
   has_and_belongs_to_many :smart_proxies
   validates :name, :presence => true
 
-  NAME_MAP = {
-    'tftp'     => 'TFTP',
-    'bmc'      => 'BMC',
-    'dns'      => 'DNS',
-    'dhcp'     => 'DHCP',
-    'puppetca' => 'Puppet CA',
-    'puppet'   => 'Puppet'
-  }
-
+  NAME_MAP =
+    Feature.all.inject({}) do |ret_val, feature|
+      ret_val[feature.name.downcase.gsub(" ", "")] = feature.name
+      ret_val
+    end
 end
