2de36329431c4c1a5266c49c537128da7e943aee
Joseph Magen <joseph@isratrade.co.il>
2013-11-20 13:32:27 +0200

    fixes #3674 - copy existing routes to legacy routes

---
+++ config/routes/api/legacy_v2.rb
@@ -0,0 +1,260 @@
+# config/routes/api/v2.rb
+Foreman::Application.routes.draw do
+
+  namespace :api, :defaults => {:format => 'json'} do
+
+    # new v2 routes that point to v2
+    scope :module => :v2, :constraints => ApiConstraints.new(:version => 2) do
+
+      resources :architectures, :except => [:new, :edit]
+
+      resources :audits, :only => [:index, :show]
+
+      resources :auth_source_ldaps, :except => [:new, :edit]
+
+      resources :bookmarks, :except => [:new, :edit]
+
+      resources :common_parameters, :except => [:new, :edit]
+
+      resources :config_templates, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+        collection do
+          get 'build_pxe_default'
+          get 'revision'
+        end
+        resources :template_combinations, :only => [:index, :create]
+      end
+
+      resources :dashboard, :only => [:index]
+
+      resources :environments, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+        resources :smart_proxies, :only => [] do
+          post :import_puppetclasses, :on => :member
+        end
+        resources :smart_class_parameters, :except => [:new, :edit, :create] do
+          resources :override_values, :except => [:new, :edit]
+        end
+        resources :puppetclasses, :except => [:new, :edit] do
+          resources :smart_class_parameters, :except => [:new, :edit, :create] do
+            resources :override_values, :except => [:new, :edit]
+          end
+        end
+      end
+
+      resources :fact_values, :only => [:index]
+
+      resources :system_groups, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+        resources :parameters, :except => [:new, :edit] do
+          collection do
+            delete '/', :to => :reset
+          end
+        end
+        resources :smart_variables, :except => [:new, :edit, :create] do
+          resources :override_values, :except => [:new, :edit]
+        end
+        resources :smart_class_parameters, :except => [:new, :edit, :create] do
+          resources :override_values, :except => [:new, :edit]
+        end
+        resources :puppetclasses, :except => [:new, :edit]
+        resources :system_group_classes, :path => :puppetclass_ids, :only => [:index, :create, :destroy]
+      end
+
+      resources :media, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+      end
+
+      resources :models, :except => [:new, :edit]
+
+      resources :operatingsystems, :except => [:new, :edit] do
+        get :bootfiles, :on => :member
+        resources :parameters, :except => [:new, :edit] do
+          collection do
+            delete '/', :to => :reset
+          end
+        end
+      end
+
+      resources :puppetclasses, :except => [:new, :edit] do
+        resources :smart_variables, :except => [:new, :edit] do
+          resources :override_values, :except => [:new, :edit]
+        end
+        resources :smart_class_parameters, :except => [:new, :edit, :create] do
+          resources :override_values, :except => [:new, :edit]
+        end
+        resources :environments, :only => [] do
+          resources :smart_class_parameters, :except => [:new, :edit, :create] do
+            resources :override_values, :except => [:new, :edit]
+          end
+        end
+        resources :system_groups, :only => [:index]
+      end
+
+      resources :ptables, :except => [:new, :edit]
+
+      resources :reports, :only => [:index, :show, :destroy] do
+        get :last, :on => :collection
+      end
+
+      resources :roles, :except => [:new, :edit]
+
+      resources :settings, :only => [:index, :show, :update]
+
+      resources :smart_proxies, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+        put :refresh, :on => :member
+        post :import_puppetclasses, :on => :member
+        resources :environments, :only => [] do
+          post :import_puppetclasses, :on => :member
+        end
+      end
+
+      resources :smart_variables, :except => [:new, :edit] do
+        resources :override_values, :except => [:new, :edit]
+      end
+
+      resources :smart_class_parameters, :except => [:new, :edit, :create] do
+        resources :override_values, :except => [:new, :edit]
+      end
+
+      resources :override_values, :only => [:update, :destroy]
+
+      resources :statistics, :only => [:index]
+
+      match '/', :to => 'home#index'
+      match 'status', :to => 'home#status', :as => "status"
+
+      resources :reports, :only => [:create]
+
+      resources :subnets, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+      end
+
+      resources :usergroups, :except => [:new, :edit]
+
+      resources :users, :except => [:new, :edit] do
+        (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+        (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+      end
+
+      resources :template_kinds, :only => [:index]
+
+      resources :template_combinations, :only => [:show, :destroy]
+
+      # add "constraint" that unconstrained and allows :id to have dot notation ex. sat.redhat.com
+      constraints(:id => /[^\/]+/) do
+        resources :compute_resources, :except => [:new, :edit] do
+          resources :images, :except => [:new, :edit]
+          get :available_images, :on => :member
+          (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+          (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+        end
+        resources :domains, :except => [:new, :edit] do
+          (resources :locations, :only => [:index, :show]) if SETTINGS[:locations_enabled]
+          (resources :organizations, :only => [:index, :show]) if SETTINGS[:organizations_enabled]
+          resources :parameters, :except => [:new, :edit] do
+            collection do
+              delete '/', :to => :reset
+            end
+          end
+        end
+        resources :systems, :except => [:new, :edit] do
+          get :status, :on => :member
+          get :puppetrun, :on => :member
+          put :boot, :on => :member
+          put :power, :on => :member
+          post :facts, :on => :collection
+          resources :audits        ,:only => :index
+          resources :facts         ,:only => :index, :controller => :fact_values
+          resources :system_classes, :path => :puppetclass_ids, :only => [:index, :create, :destroy]
+          resources :interfaces, :except => [:new, :edit]
+          resources :parameters, :except => [:new, :edit] do
+            collection do
+              delete '/', :to => :reset
+            end
+          end
+          resources :puppetclasses, :except => [:new, :edit]
+          resources :reports, :only => [:index, :show] do
+            get :last, :on => :collection
+          end
+          resources :smart_variables, :except => [:new, :edit, :create] do
+            resources :override_values, :except => [:new, :edit]
+          end
+          resources :smart_class_parameters, :except => [:new, :edit, :create] do
+            resources :override_values, :except => [:new, :edit]
+          end
+        end
+      end
+
+      if SETTINGS[:locations_enabled]
+        resources :locations, :except => [:new, :edit] do
+
+          # scoped by location
+          resources :domains, :only => [:index, :show]
+          resources :subnets, :only => [:index, :show]
+          resources :system_groups, :only => [:index, :show]
+          resources :environments, :only => [:index, :show]
+          resources :users, :only => [:index, :show]
+          resources :config_templates, :only => [:index, :show]
+          resources :compute_resources, :only => [:index, :show]
+          resources :media, :only => [:index, :show]
+          resources :smart_proxies, :only => [:index, :show]
+
+          # scoped by location AND organization
+          resources :organizations, :except => [:new, :edit] do
+            resources :domains, :only => [:index, :show]
+            resources :subnets, :only => [:index, :show]
+            resources :system_groups, :only => [:index, :show]
+            resources :environments, :only => [:index, :show]
+            resources :users, :only => [:index, :show]
+            resources :config_templates, :only => [:index, :show]
+            resources :compute_resources, :only => [:index, :show]
+            resources :media, :only => [:index, :show]
+            resources :smart_proxies, :only => [:index, :show]
+          end
+
+        end
+      end
+
+      if SETTINGS[:organizations_enabled]
+        resources :organizations, :except => [:new, :edit] do
+
+          # scoped by organization
+          resources :domains, :only => [:index, :show]
+          resources :subnets, :only => [:index, :show]
+          resources :system_groups, :only => [:index, :show]
+          resources :environments, :only => [:index, :show]
+          resources :users, :only => [:index, :show]
+          resources :config_templates, :only => [:index, :show]
+          resources :compute_resources, :only => [:index, :show]
+          resources :media, :only => [:index, :show]
+          resources :smart_proxies, :only => [:index, :show]
+
+          # scoped by location AND organization
+          resources :locations, :except => [:new, :edit] do
+            resources :domains, :only => [:index, :show]
+            resources :subnets, :only => [:index, :show]
+            resources :system_groups, :only => [:index, :show]
+            resources :environments, :only => [:index, :show]
+            resources :users, :only => [:index, :show]
+            resources :config_templates, :only => [:index, :show]
+            resources :compute_resources, :only => [:index, :show]
+            resources :media, :only => [:index, :show]
+            resources :smart_proxies, :only => [:index, :show]
+          end
+
+        end
+      end
+      get 'orchestration/(:id)/tasks', :to => 'tasks#index'
+
+    end
+    match '*other', :to => 'v2/home#route_error', :constraints => ApiConstraints.new(:version => 2)
+  end
+end
---
+++ config/routes/legacy_routes.rb
@@ -0,0 +1,345 @@
+require 'api_constraints'
+
+Foreman::Application.routes.draw do
+  #ENC requests goes here
+  match "node/:name" => 'systems#externalNodes', :constraints => { :name => /[^\.][\w\.-]+/ }
+
+  resources :reports, :only => [:index, :show, :destroy] do
+    collection do
+      get 'auto_complete_search'
+    end
+  end
+
+  match '(:controller)/help', :action => 'welcome', :as => "help"
+  constraints(:id => /[^\/]+/) do
+    resources :systems do
+      member do
+        get 'clone'
+        get 'storeconfig_klasses'
+        get 'externalNodes'
+        get 'setBuild'
+        get 'cancelBuild'
+        get 'puppetrun'
+        get 'pxe_config'
+        put 'toggle_manage'
+        post 'environment_selected'
+        put 'power'
+        get 'console'
+        get 'bmc'
+        put 'ipmi_boot'
+      end
+      collection do
+        get 'multiple_actions'
+        get 'multiple_parameters'
+        post 'update_multiple_parameters'
+        get 'select_multiple_system_group'
+        post 'update_multiple_system_group'
+        get 'select_multiple_environment'
+        post 'update_multiple_environment'
+        get 'multiple_puppetrun'
+        post 'update_multiple_puppetrun'
+        get 'multiple_destroy'
+        post 'submit_multiple_destroy'
+        get 'multiple_build'
+        post 'submit_multiple_build'
+        get 'reset_multiple'
+        get 'multiple_disable'
+        post 'submit_multiple_disable'
+        get 'multiple_enable'
+        post 'submit_multiple_enable'
+        get 'auto_complete_search'
+        get 'template_used'
+        get 'active'
+        get 'pending'
+        get 'out_of_sync'
+        get 'errors'
+        get 'disabled'
+        post 'current_parameters'
+        post 'puppetclass_parameters'
+        post 'process_system_group'
+        post 'process_taxonomy'
+        post 'system_group_or_environment_selected'
+        post 'architecture_selected'
+        post 'os_selected'
+        post 'domain_selected'
+        post 'use_image_selected'
+        post 'compute_resource_selected'
+        post 'medium_selected'
+        get  'select_multiple_organization'
+        post 'update_multiple_organization'
+        get  'select_multiple_location'
+        post 'update_multiple_location'
+      end
+
+      constraints(:system_id => /[^\/]+/) do
+        resources :reports       ,:only => [:index, :show]
+        resources :audits        ,:only => :index
+        resources :facts         ,:only => :index, :controller => :fact_values
+        resources :puppetclasses ,:only => :index
+      end
+    end
+
+
+    resources :bookmarks, :except => [:show]
+    resources :lookup_keys, :except => [:show, :new, :create] do
+      resources :lookup_values, :only => [:index, :create, :update, :destroy]
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+
+    resources :facts, :only => [:index, :show] do
+      constraints(:id => /[^\/]+/) do
+        resources :values, :only => :index, :controller => :fact_values, :as => "system_fact_values"
+      end
+    end
+
+  end
+
+  resources :settings, :only => [:index, :update] do
+    collection do
+      get 'auto_complete_search'
+    end
+  end
+  resources :common_parameters, :except => [:show] do
+    collection do
+      get 'auto_complete_search'
+    end
+  end
+  resources :environments, :except => [:show] do
+    collection do
+      get 'import_environments'
+      post 'obsolete_and_new'
+      get 'auto_complete_search'
+    end
+  end
+  resources :trends do
+    collection do
+      post 'count'
+    end
+  end
+
+  resources :system_groups, :except => [:show] do
+    member do
+      get 'nest'
+      get 'clone'
+    end
+    collection do
+      get 'auto_complete_search'
+      post 'environment_selected'
+      post 'architecture_selected'
+      post 'os_selected'
+      post 'domain_selected'
+      post 'use_image_selected'
+      post 'medium_selected'
+      post 'process_system_group'
+    end
+  end
+
+  resources :puppetclasses, :except => [:show] do
+    collection do
+      get 'import_environments'
+      post 'obsolete_and_new'
+      get 'auto_complete_search'
+    end
+    member do
+      post 'parameters'
+    end
+    constraints(:id => /[^\/]+/) do
+      resources :systems
+      resources :lookup_keys, :except => [:show, :new, :create]
+    end
+  end
+
+
+  resources :smart_proxies, :except => [:show] do
+    member do
+      post 'ping'
+      put 'refresh'
+    end
+    constraints(:id => /[^\/]+/) do
+      resources :puppetca, :only => [:index, :update, :destroy]
+      resources :autosign, :only => [:index, :new, :create, :destroy]
+    end
+  end
+
+  resources :fact_values, :only => [:index] do
+    collection do
+      get 'auto_complete_search'
+    end
+  end
+
+  resources :notices, :only => :destroy
+  resources :audits do
+    collection do
+      get 'auto_complete_search'
+    end
+  end
+
+  if SETTINGS[:login]
+    resources :usergroups, :except => [:show]
+    resources :users, :except => [:show] do
+      collection do
+        get 'login'
+        post 'login'
+        get 'logout'
+        get 'extlogin'
+        get 'extlogout'
+        get 'auth_source_selected'
+        get 'auto_complete_search'
+      end
+    end
+    resources :roles, :except => [:show] do
+      collection do
+        get 'report'
+        post 'report'
+        get 'auto_complete_search'
+      end
+    end
+
+    resources :auth_source_ldaps, :except => [:show]
+  end
+
+  if SETTINGS[:unattended]
+    resources :config_templates, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+        get 'build_pxe_default'
+        get 'revision'
+      end
+    end
+    constraints(:id => /[^\/]+/) do
+      resources :domains, :except => [:show] do
+        collection do
+          get 'auto_complete_search'
+        end
+      end
+    end
+
+    resources :operatingsystems, :except => [:show] do
+      member do
+        get 'bootfiles'
+      end
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+    resources :media, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+
+    resources :models, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+
+    resources :architectures, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+
+    resources :ptables, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+      end
+    end
+
+    constraints(:id => /[^\/]+/) do
+      resources :compute_resources do
+        member do
+          post 'hardware_profile_selected'
+          post 'cluster_selected'
+          post 'ping'
+          put 'associate'
+        end
+        constraints(:id => /[^\/]+/) do
+          resources :vms, :controller => "compute_resources_vms" do
+            member do
+              put 'power'
+              put 'pause'
+              put 'associate'
+              get 'console'
+            end
+          end
+        end
+        collection do
+          get  'auto_complete_search'
+          get 'provider_selected'
+          put  'test_connection'
+        end
+        resources :images, :except => [:show]
+      end
+    end
+
+    resources :subnets, :except => [:show] do
+      collection do
+        get 'auto_complete_search'
+        get 'import'
+        post 'create_multiple'
+        post 'freeip'
+      end
+    end
+
+    match 'unattended/template/:id/:system_group', :to => "unattended#template"
+  end
+
+  root :to => 'dashboard#index'
+  match 'dashboard', :to => 'dashboard#index', :as => "dashboard"
+  match 'dashboard/auto_complete_search', :to => 'systems#auto_complete_search', :as => "auto_complete_search_dashboards"
+  match 'statistics', :to => 'statistics#index', :as => "statistics"
+  match 'status', :to => 'home#status', :as => "status"
+
+  # match for all unattended scripts
+  match 'unattended/(:action/(:id(.format)))', :controller => 'unattended'
+
+  resources :tasks, :only => [:show]
+
+  if SETTINGS[:locations_enabled]
+    resources :locations, :except => [:show] do
+      resources :systems, :only => :index
+      member do
+        get 'select'
+        match "clone" => 'locations#clone_taxonomy'
+        post 'import_mismatches'
+        get 'step2'
+        get 'assign_systems'
+        post 'assign_all_systems'
+        put 'assign_selected_systems'
+      end
+      collection do
+        get 'auto_complete_search'
+        get 'clear'
+        get  'mismatches'
+        post 'import_mismatches'
+      end
+    end
+  end
+
+  if SETTINGS[:organizations_enabled]
+    resources :organizations, :except => [:show] do
+      member do
+        get 'select'
+        match "clone" => 'organizations#clone_taxonomy'
+        post 'import_mismatches'
+        get 'step2'
+        get 'assign_systems'
+        post 'assign_all_systems'
+        put 'assign_selected_systems'
+      end
+      collection do
+        get 'auto_complete_search'
+        get 'clear'
+        get  'mismatches'
+        post 'import_mismatches'
+      end
+    end
+  end
+
+  resources :about, :only => :index do
+  end
+
+end
