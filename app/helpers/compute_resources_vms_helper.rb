3d229f9ccbc78b961ba49adac6a9002c97c0d5bb
Martin Matuska <martin@matuska.org>
2013-11-20 17:56:25 +0100

    Move logic to compute_resources_vms_helper

--- app/helpers/compute_resources_vms_helper.rb
+++ app/helpers/compute_resources_vms_helper.rb
@@ -56,6 +56,12 @@ module ComputeResourcesVmsHelper
     select
   end
 
+  def vsphere_datastores(compute)
+    compute.datastores.map { |ds|
+      [ds.freespace && ds.capacity ? "#{ds.name} (#{_('free')}: #{number_to_human_size(ds.freespace)}, #{_('total')}: #{number_to_human_size(ds.capacity)})" : ds.name, ds.name]
+    }
+  end
+
   def available_actions(vm)
     case vm
     when Fog::Compute::OpenStack::Server
