08c8695e697e0d245737fe26b0c2ad4b91ca3f53
Greg Sutcliffe <gsutclif@redhat.com>
2013-11-28 12:17:15 +0000

    Fixes #3777 - Fix typo in Settings cleanup message

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -4,7 +4,7 @@ class SettingsController < ApplicationController
   #This can happen in development when removing a plugin
   rescue_from ActiveRecord::SubclassNotFound do |e|
     type = (e.to_s =~ /\'(Setting::.*)\'\./) ? $1 : 'STI-Type'
-    render :text => (e.to_s+"<br><b>run Setting.delete_all(:category=>'#{type}'}') to recover.</b>").html_safe, :status=> 500
+    render :text => (e.to_s+"<br><b>run Setting.delete_all(:category=>'#{type}') to recover.</b>").html_safe, :status=> 500
   end
 
   def index
