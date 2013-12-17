7d7abede537e26496c9f3e272e5fdb7372a45b64
Amos Benari <abenari@redhat.com>
2013-12-17 12:08:05 +0200

    fixed organizations and locations form

--- vendor/assets/javascripts/jquery.multi-select.js
+++ vendor/assets/javascripts/jquery.multi-select.js
@@ -488,10 +488,12 @@ $(function(){
 })
 
 function multiSelectOnLoad(){
-  $('select[multiple]').multiSelect({
-    disabledClass : 'disabled disabled_item',
-    selectableHeader: $("<div class='ms-header'>" + _('All items') + " <input placeholder='" + _('Filter') + "' class='ms-filter' type='text'><a href='#' title='" + _('Select All') + "' class='ms-select-all pull-right glyphicon glyphicon-plus icon-white'></a></div>"),
-    selectionHeader: $("<div class='ms-header'>" + _('Selected items') + "<a href='#' title='" + _('Deselect All') + "' class='ms-deselect-all pull-right glyphicon glyphicon-minus icon-white'></a></div>")
+  $('select[multiple]').each(function(i,item){
+    $(item).multiSelect({
+      disabledClass : 'disabled disabled_item',
+      selectableHeader: $("<div class='ms-header'>" + _('All items') + " <input placeholder='" + _('Filter') + "' class='ms-filter' type='text'><a href='#' title='" + _('Select All') + "' class='ms-select-all pull-right glyphicon glyphicon-plus icon-white'></a></div>"),
+      selectionHeader: $("<div class='ms-header'>" + _('Selected items') + "<a href='#' title='" + _('Deselect All') + "' class='ms-deselect-all pull-right glyphicon glyphicon-minus icon-white'></a></div>")
+    })
   });
 
   $('select[multiple]').each(function(i,item){
