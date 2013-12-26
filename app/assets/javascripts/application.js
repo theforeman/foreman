350ef0898efc9539ba3f09170cb7e42e3032b852
Amos Benari <abenari@redhat.com>
2013-12-26 13:46:37 +0200

    fixed inplace edit in settings page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 //= require vendor
 //= require about
 //= require jquery.extentions
+//= require settings
 
 $(document).on('ContentLoad', function(){onContentLoad()});
 
@@ -173,65 +174,6 @@ function template_info(div, url) {
                 }
               });
 }
-
-$(document).ready(function() {
-  var common_settings = {
-    method      : 'PUT',
-    indicator   : spinner_placeholder(),
-    tooltip     : _('Click to edit..'),
-    placeholder : _('Click to edit..'),
-    submitdata  : {authenticity_token: AUTH_TOKEN, format : "json"},
-    onedit      : function(data) { $(this).removeClass("editable"); },
-    callback    : function(value, settings) { $(this).addClass("editable"); },
-    onsuccess   :  function(data) {
-      var parsed = $.parseJSON(data);
-      var key = $(this).attr('name').split("[")[0];
-      var val = $(this).attr('data-field');
-
-      var editable_value = parsed[key][val];
-      if ($.isArray(editable_value))
-        editable_value = "[ "+editable_value.join(", ")+" ]";
-      else
-        editable_value = String(editable_value);
-
-      $(this).html(editable_value);
-    },
-    onerror     : function(settings, original, xhr) {
-      original.reset();
-      var error = $.parseJSON(xhr.responseText)["errors"];
-      $.jnotify(error, { type: "error", sticky: true });
-    }
-  };
-
-  $('.edit_textfield').each(function() {
-    var settings = {
-      type : 'text',
-      name : $(this).attr('name'),
-      width: '95%'
-    };
-    $(this).editable($(this).attr('data-url'), $.extend(common_settings, settings));
-  });
-
-  $('.edit_textarea').each(function() {
-    var settings = {
-      type : 'textarea',
-      name : $(this).attr('name'),
-      rows : 8,
-      cols : 36
-    };
-    $(this).editable($(this).attr('data-url'), $.extend(common_settings, settings));
-  });
-
-  $('.edit_select').each(function() {
-    var settings = {
-      type : 'select',
-      name : $(this).attr('name'),
-      data : $(this).attr('select_values'),
-      submit : 'Save'
-    };
-    $(this).editable($(this).attr('data-url'), $.extend(common_settings, settings));
-  });
-});
 
 //add bookmark dialog
 $(function() {
