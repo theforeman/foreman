3af332d82ceccc8c01aebca01995e2e42a2fa6bc
Romain Vrignaud <rvrignaud@gmail.com>
2014-01-05 19:08:25 +0100

    First support of chefproxy on Foreman
      * Related to theforeman/smart-proxy#112

--- db/seeds.rb
+++ db/seeds.rb
@@ -130,7 +130,7 @@ end
 end
 
 # Proxy features
-[ "TFTP", "DNS", "DHCP", "Puppet", "Puppet CA", "BMC" ].each do |input|
+[ "TFTP", "DNS", "DHCP", "Puppet", "Puppet CA", "BMC", "Chef Proxy" ].each do |input|
   f = Feature.find_or_create_by_name(input)
   raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
 end
