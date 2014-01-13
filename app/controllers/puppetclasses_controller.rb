99409d6a4d6a2ab386e9c23ad4485ad3eff71bc6
Marek Hulan <mhulan@redhat.com>
2014-01-13 17:28:45 +0100

    More reviews

--- app/controllers/puppetclasses_controller.rb
+++ app/controllers/puppetclasses_controller.rb
@@ -18,7 +18,7 @@ class PuppetclassesController < ApplicationController
     @host_counter = Host.group(:puppetclass_id).joins(:puppetclasses).where(:puppetclasses => {:id => @puppetclasses.collect(&:id)}).count
     @keys_counter = Puppetclass.joins(:class_params).select('distinct environment_classes.lookup_key_id').group(:name).count
     @authorizer = Authorizer.new(User.current, @puppetclasses)
-    @hostgroups_authorizer = Authorizer.new(User.current, @puppetclasses.map(&:hostgroups).compact.uniq)
+    @hostgroups_authorizer = Authorizer.new(User.current, HostgroupClass.find_all_by_puppetclass_id(@puppetclasses.map(&:id)).compact.uniq.map(&:hostgroup_id))
   end
 
   def new
