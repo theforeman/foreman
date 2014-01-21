9a3248b39a8443f30334e85592e7a79564f6572b
Dominic Cleal <dcleal@redhat.com>
2014-01-21 21:43:16 +0000

    fixes #4005 - ensure standalone Rabl.render works outside of a controller
    (cherry picked from commit 858708c466398ac5f42bb1bb0fd6c1e531e31385)

---
+++ test/unit/rabl_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+require 'ostruct'
+
+class RablTest < ActiveSupport::TestCase
+  test 'render of single template' do
+    rendered = Rabl.render(OpenStruct.new(:name => 'foo'),
+                           'api/v2/domains/show',
+                           :format => :json,
+                           :view_path => 'app/views')
+    loaded = JSON.load(rendered)
+    assert_equal Hash, loaded.class
+    assert_equal 'foo', loaded['name']
+  end
+
+  test 'render of collection template' do
+    rendered = Rabl.render([OpenStruct.new(:name => 'foo')],
+                           'api/v2/domains/index',
+                           :format => :json,
+                           :view_path => 'app/views')
+    loaded = JSON.load(rendered)
+    assert_equal Array, loaded.class
+    assert_equal Hash, loaded[0].class
+    assert_equal 'foo', loaded[0]['name']
+  end
+end
