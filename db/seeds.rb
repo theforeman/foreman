adce4ac3548b1599eb77e5422e494270cc3a0245
Dominic Cleal <dcleal@redhat.com>
2014-01-29 09:18:34 +0000

    templates - sync from community-templates
    (cherry picked from commit 6d755dd05f00a7f52b7bb3f791438b688ce54ba7)

--- db/seeds.rb
+++ db/seeds.rb
@@ -50,7 +50,8 @@ Ptable.without_auditing do
     { :name => 'Jumpstart mirrored', :os_family => 'Solaris', :source => 'jumpstart/disklayout_mirrored.erb' },
     { :name => 'Kickstart default', :os_family => 'Redhat', :source => 'kickstart/disklayout.erb' },
     { :name => 'Preseed default', :os_family => 'Debian', :source => 'preseed/disklayout.erb' },
-    { :name => 'Preseed custom LVM', :os_family => 'Debian', :source => 'preseed/disklayout_lvm.erb' }
+    { :name => 'Preseed custom LVM', :os_family => 'Debian', :source => 'preseed/disklayout_lvm.erb' },
+    { :name => 'Junos default fake', :os_family => 'Junos', :source => 'ztp/disklayout.erb' }
   ].each do |input|
     next if Ptable.find_by_name(input[:name])
     next if audit_modified? Ptable, input[:name]
@@ -87,6 +88,7 @@ ConfigTemplate.without_auditing do
     { :name => 'Preseed default iPXE', :source => 'preseed/iPXE.erb', :template_kind => kinds[:iPXE] },
     { :name => 'Preseed default user data', :source => 'preseed/userdata.erb', :template_kind => kinds[:user_data] },
     { :name => 'WAIK default PXELinux', :source => 'waik/PXELinux.erb', :template_kind => kinds[:PXELinux], :operatingsystems => os_windows },
+    { :name => "Junos default SLAX", :source => 'ztp/provision.erb', :template_kind => kinds[:provision], :operatingsystems => os_junos },
     { :name => "Junos default ZTP config", :source => 'ztp/ZTP.erb', :template_kind => kinds[:ZTP], :operatingsystems => os_junos },
     { :name => "Junos default finish", :source => 'ztp/finish.erb', :template_kind => kinds[:finish], :operatingsystems => os_junos },
     # snippets
