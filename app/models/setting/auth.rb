ec3a95fb86c2598780372a7fe87e05a7e96f74ae
Daniel Lobato <elobatocs@gmail.com>
2014-01-29 20:14:05 +0100

    fixes #4270 - setting descriptions updated for consistency

--- app/models/setting/auth.rb
+++ app/models/setting/auth.rb
@@ -8,10 +8,10 @@ class Setting::Auth < Setting
     fqdn = Facter.fqdn
     self.transaction do
       [
-        self.set('oauth_active', N_("Should foreman use OAuth for authorization in API"), false),
+        self.set('oauth_active', N_("Foreman will use OAuth for API authorization"), false),
         self.set('oauth_consumer_key', N_("OAuth consumer key"), 'katello'),
         self.set('oauth_consumer_secret', N_("OAuth consumer secret"), 'shhhh'),
-        self.set('oauth_map_users', N_("Should foreman map users by username in request-header"), true),
+        self.set('oauth_map_users', N_("Foreman will map users by username in request-header. If this is set to false, OAuth requests will have admin rights."), true),
         self.set('restrict_registered_puppetmasters', N_('Only known Smart Proxies with the Puppet feature can access fact/report importers and ENC output'), true),
         self.set('require_ssl_puppetmasters', N_('Client SSL certificates are used to identify Smart Proxies accessing fact/report importers and ENC output over HTTPS (:require_ssl should also be enabled)'), true),
         self.set('trusted_puppetmaster_hosts', N_('Hosts that will be trusted in addition to Smart Proxies for access to fact/report importers and ENC output'), []),
--- app/models/setting/provisioning.rb
+++ app/models/setting/provisioning.rb
@@ -12,19 +12,19 @@ class Setting::Provisioning < Setting
     self.transaction do
       [
         self.set('root_pass', N_("Default encrypted root password on provisioned hosts"), nil),
-        self.set('unattended_url', N_("The URL that hosts will retrieve templates from during build (normally http as many installers don't support https)"), "http://#{Facter.fqdn}"),
+        self.set('unattended_url', N_("URL hosts will retrieve templates from during build (normally http as many installers don't support https)"), "http://#{Facter.fqdn}"),
         self.set('safemode_render', N_("Enable safe mode config templates rendering (recommended)"), true),
         self.set('ssl_certificate', N_("SSL Certificate path that Foreman would use to communicate with its proxies"), ssl_cert),
         self.set('ssl_ca_file', N_( "SSL CA file that Foreman will use to communicate with its proxies"), ssl_ca_file),
         self.set('ssl_priv_key', N_("SSL Private Key file that Foreman will use to communicate with its proxies"), ssl_priv_key),
-        self.set('manage_puppetca', N_("Should Foreman automate certificate signing upon provisioning new host"), true),
-        self.set('ignore_puppet_facts_for_provisioning', N_("Does not update ipaddress and MAC values from Puppet facts"), false),
-        self.set('query_local_nameservers', N_("Should Foreman query the locally configured name server or the SOA/NS authorities"), false),
+        self.set('manage_puppetca', N_("Foreman will automate certificate signing upon provision of new host"), true),
+        self.set('ignore_puppet_facts_for_provisioning', N_("Stop updating IP address and MAC values from Puppet facts"), false),
+        self.set('query_local_nameservers', N_("Foreman will query the locally configured resolver instead of the SOA/NS authorities"), false),
         self.set('remote_addr', N_("If Foreman is running behind Passenger or a remote load balancer, the IP should be set here. This is a regular expression, so it can support several load balancers, i.e: (10.0.0.1|127.0.0.1)"), "127.0.0.1"),
         self.set('token_duration', N_("Time in minutes installation tokens should be valid for, 0 to disable"), 60),
         self.set('libvirt_default_console_address', N_("The IP address that should be used for the console listen address when provisioning new virtual machines via Libvirt"), "0.0.0.0"),
-        self.set('update_ip_from_built_request', N_("Should we use the originating IP of the built request to update the host's IP?"), false),
-        self.set('use_shortname_for_vms', N_("Should Foreman use the short hostname instead of the FQDN for creating new virtual machines"), false)
+        self.set('update_ip_from_built_request', N_("Foreman will update the host IP with the IP that made the built request"), false),
+        self.set('use_shortname_for_vms', N_("Foreman will use the short hostname instead of the FQDN for creating new virtual machines"), false)
       ].each { |s| self.create! s.update(:category => "Setting::Provisioning")}
     end
 
