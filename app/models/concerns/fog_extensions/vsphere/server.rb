50f80dcb09cc47f69aaca317240f517775cdcc1b
Martin Matuska <martin@matuska.org>
2014-02-06 17:27:38 +0100

    fixes #3496 vSphere: add support for selectable SCSI controller
    (cherry picked from commit 41ee835f2c872d09cd09af2179ef3bc2327e6b02)

--- app/models/concerns/fog_extensions/vsphere/server.rb
+++ app/models/concerns/fog_extensions/vsphere/server.rb
@@ -27,6 +27,15 @@ module FogExtensions
         _("%{cpus} CPUs and %{memory} MB memory") % {:cpus => cpus, :memory => memory_mb.to_i}
       end
 
+      def scsi_controller_type
+        return scsi_controller[:type] if scsi_controller.is_a?(Hash)
+        scsi_controller.type
+      end
+
+      def scsi_controller_type= type
+        scsi_controller[:type] = type
+      end
+
     end
   end
 end
