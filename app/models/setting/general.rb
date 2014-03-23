ebb19fcebcac73620bbdc79e88fc4cbb83ca3a63
marianitadn <maria.nita.dn@gmail.com>
2014-03-23 23:08:20 +0200

    fixes #3946 Set host URLs from local settings file, if no Facter configuration exists. Add default values for settings example.
        - Disable I18n default locales since is deprecated
        - Add default values in settings example for app config: domain and fqdn
        - Update settings for host configuration and provisioning

--- app/models/setting/general.rb
+++ app/models/setting/general.rb
@@ -4,21 +4,24 @@ class Setting::General < Setting
   def self.load_defaults
     # Check the table exists
     return unless super
-    protocol=SETTINGS[:require_ssl] ? 'https' : 'http'
+    protocol = SETTINGS[:require_ssl] ? 'https' : 'http'
+    domain = Facter.value(:domain) || SETTINGS[:domain]
+    administrator = "root@#{domain}"
+    foreman_url = "#{protocol}://#{Facter.value(:fqdn) || SETTINGS[:fqdn]}"
+    email_reply_address = "Foreman-noreply@#{domain}"
 
     self.transaction do
-      domain = Facter.value(:domain)
       [
-        self.set('administrator', N_("The default administrator email address"), "root@#{domain}"),
-        self.set('foreman_url', N_("URL where your Foreman instance is reachable (see also Provisioning > unattended_url)"), "#{protocol}://#{Facter.value(:fqdn)}"),
-        self.set('email_reply_address', N_("Email reply address for emails that Foreman is sending"), "Foreman-noreply@#{domain}"),
+        self.set('administrator', N_("The default administrator email address"), administrator),
+        self.set('foreman_url', N_("URL where your Foreman instance is reachable (see also Provisioning > unattended_url)"), foreman_url),
+        self.set('email_reply_address', N_("Email reply address for emails that Foreman is sending"), email_reply_address),
         self.set('entries_per_page', N_("Number of records shown per page in Foreman"), 20),
-        self.set('fix_db_cache', N_('Fix DB cache on next Foreman restart'),false),
-        self.set('authorize_login_delegation', N_("Authorize login delegation with REMOTE_USER environment variable"),false),
-        self.set('authorize_login_delegation_api', N_("Authorize login delegation with REMOTE_USER environment variable for API calls too"),false),
-        self.set('idle_timeout', N_("Log out idle users after a certain number of minutes"),60),
-        self.set('max_trend', N_("Max days for Trends graphs"),30),
-        self.set('use_gravatar', N_("Foreman will use gravatar to display user icons"),true)
+        self.set('fix_db_cache', N_('Fix DB cache on next Foreman restart'), false),
+        self.set('authorize_login_delegation', N_("Authorize login delegation with REMOTE_USER environment variable"), false),
+        self.set('authorize_login_delegation_api', N_("Authorize login delegation with REMOTE_USER environment variable for API calls too"), false),
+        self.set('idle_timeout', N_("Log out idle users after a certain number of minutes"), 60),
+        self.set('max_trend', N_("Max days for Trends graphs"), 30),
+        self.set('use_gravatar', N_("Foreman will use gravatar to display user icons"), true)
       ].each { |s| self.create! s.update(:category => "Setting::General")}
     end
 
--- app/models/setting/provisioning.rb
+++ app/models/setting/provisioning.rb
@@ -8,11 +8,12 @@ class Setting::Provisioning < Setting
     ssl_cert     = "#{SETTINGS[:puppetvardir]}/ssl/certs/#{Facter.value(:fqdn)}.pem"
     ssl_ca_file  = "#{SETTINGS[:puppetvardir]}/ssl/certs/ca.pem"
     ssl_priv_key = "#{SETTINGS[:puppetvardir]}/ssl/private_keys/#{Facter.value(:fqdn)}.pem"
+    unattended_url = "http://#{Facter.value(:fqdn) || SETTINGS[:fqdn]}"
 
     self.transaction do
       [
         self.set('root_pass', N_("Default encrypted root password on provisioned hosts"), nil),
-        self.set('unattended_url', N_("URL hosts will retrieve templates from during build (normally http as many installers don't support https)"), "http://#{Facter.value(:fqdn)}"),
+        self.set('unattended_url', N_("URL hosts will retrieve templates from during build (normally http as many installers don't support https)"), unattended_url),
         self.set('safemode_render', N_("Enable safe mode config templates rendering (recommended)"), true),
         self.set('ssl_certificate', N_("SSL Certificate path that Foreman would use to communicate with its proxies"), ssl_cert),
         self.set('ssl_ca_file', N_( "SSL CA file that Foreman will use to communicate with its proxies"), ssl_ca_file),
