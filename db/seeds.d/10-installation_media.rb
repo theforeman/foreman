7947b8d9a95e17686c9aeee68252b1032be16013
Michael Moll <kvedulv@kvedulv.de>
2014-04-02 20:53:22 +0200

    fixes #5072 - x86_86 -> amd64 for FreeBSD media

--- db/seeds.d/10-installation_media.rb
+++ db/seeds.d/10-installation_media.rb
@@ -6,6 +6,7 @@ Medium.without_auditing do
       { :name => "CentOS mirror", :os_family => "Redhat", :path => "http://mirror.centos.org/centos/$major.$minor/os/$arch" },
       { :name => "Debian mirror", :os_family => "Debian", :path => "http://ftp.debian.org/debian/" },
       { :name => "Fedora mirror", :os_family => "Redhat", :path => "http://dl.fedoraproject.org/pub/fedora/linux/releases/$major/Fedora/$arch/os/" },
+      { :name => "FreeBSD mirror", :os_family => "Freebsd", :path => "http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$major.$minor-RELEASE/" },
       { :name => "OpenSUSE mirror", :os_family => "Suse", :path => "http://download.opensuse.org/distribution/$major.$minor/repo/oss", :operatingsystems => os_suse },
       { :name => "Ubuntu mirror", :os_family => "Debian", :path => "http://archive.ubuntu.com/ubuntu/" }
   ].each do |input|
