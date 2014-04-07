e28ac593d4b53e7b6c306d4e2bd07dc5e4ec6de9
Martin Milata <mmilata@srck.net>
2014-04-07 17:27:01 +0200

    fixes #5090 - typo in bookmarks

--- db/seeds.d/15-bookmarks.rb
+++ db/seeds.d/15-bookmarks.rb
@@ -3,7 +3,7 @@ Bookmark.without_auditing do
   [
       { :name => "eventful", :query => "eventful = true", :controller=> "reports" },
       { :name => "active", :query => 'last_report > "35 minutes ago" and (status.applied > 0 or status.restarted > 0)', :controller=> "hosts" },
-      { :name => "out of sync", :query => 'last_report < "30 minutes ago" andstatus.enabled = true', :controller=> "hosts" },
+      { :name => "out of sync", :query => 'last_report < "30 minutes ago" and status.enabled = true', :controller=> "hosts" },
       { :name => "error", :query => 'last_report > "35 minutes ago" and (status.failed > 0 or status.failed_restarts > 0 or status.skipped > 0)', :controller=> "hosts" },
       { :name => "disabled", :query => 'status.enabled = false', :controller=> "hosts" },
       { :name => "ok hosts", :query => 'last_report > "35 minutes ago" and status.enabled = true and status.applied = 0 and status.failed = 0 and status.pending = 0', :controller=> "hosts" }
