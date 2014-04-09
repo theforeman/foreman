e8e6a960c44b2f4b6664b3227a667c2e958ca72b
Dominic Cleal <dcleal@redhat.com>
2014-04-09 18:09:54 +0100

    templates - sync from community-templates

--- db/seeds.d/08-partition_tables.rb
+++ db/seeds.d/08-partition_tables.rb
@@ -4,6 +4,7 @@ Ptable.without_auditing do
       { :name => 'AutoYaST entire SCSI disk', :os_family => 'Suse', :source => 'autoyast/disklayout_scsi.erb' },
       { :name => 'AutoYaST entire virtual disk', :os_family => 'Suse', :source => 'autoyast/disklayout_virtual.erb' },
       { :name => 'AutoYaST LVM', :os_family => 'Suse', :source => 'autoyast/disklayout_lvm.erb' },
+      { :name => 'FreeBSD', :os_family => 'Freebsd', :source => 'freebsd/disklayout_FreeBSD_mfsBSD.erb' },
       { :name => 'Jumpstart default', :os_family => 'Solaris', :source => 'jumpstart/disklayout.erb' },
       { :name => 'Jumpstart mirrored', :os_family => 'Solaris', :source => 'jumpstart/disklayout_mirrored.erb' },
       { :name => 'Kickstart default', :os_family => 'Redhat', :source => 'kickstart/disklayout.erb' },
