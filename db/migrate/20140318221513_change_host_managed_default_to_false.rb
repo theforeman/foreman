1058956ecc23f076c995ed1449654f6f8d78c410
Lukas Zapletal <lzap+git@redhat.com>
2014-04-09 10:29:33 +0200

    fixed #5120 - fixed migration for discovered hosts

--- db/migrate/20140318221513_change_host_managed_default_to_false.rb
+++ db/migrate/20140318221513_change_host_managed_default_to_false.rb
@@ -1,6 +1,6 @@
 class ChangeHostManagedDefaultToFalse < ActiveRecord::Migration
   def self.up
-    Host.unscoped.where(:managed => nil).update_all(:managed => false)
+    Host::Base.unscoped.where(:managed => nil).update_all(:managed => false)
     change_column :hosts, :managed, :boolean, :null => false, :default => false
   end
 
