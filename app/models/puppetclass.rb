d20aba2be53a788d057bd77c4ab18bfd45bd2115
Joseph Magen <jmagen@redhat.com>
2014-04-14 01:02:20 +0300

    fixes #5173 - sortable counters on list of config_groups for number of hosts, hostgroups and puppetclasses

--- app/models/puppetclass.rb
+++ app/models/puppetclass.rb
@@ -35,6 +35,7 @@ class Puppetclass < ActiveRecord::Base
   scoped_search :on => :lookup_keys_count, :rename => :variables_count        # Smart Variables
   scoped_search :in => :environments, :on => :name, :complete_value => :true, :rename => "environment"
   scoped_search :in => :hostgroups,   :on => :name, :complete_value => :true, :rename => "hostgroup"
+  scoped_search :in => :config_groups,   :on => :name, :complete_value => :true, :rename => "config_group"
   scoped_search :in => :hosts, :on => :name, :complete_value => :true, :rename => "host", :ext_method => :search_by_host, :only_explicit => true
   scoped_search :in => :class_params, :on => :key, :complete_value => :true, :only_explicit => true
 
