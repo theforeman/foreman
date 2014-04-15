5ae65cf1c578cdbc6f2ae6044718d3c30406597c
Stephen Benjamin <stephen@bitbin.de>
2014-04-15 12:29:53 +0200

    fixes #5202 - make config group counters default to 0

--- db/migrate/20140413123650_add_counters_to_config_groups.rb
+++ db/migrate/20140413123650_add_counters_to_config_groups.rb
@@ -1,7 +1,7 @@
 class AddCountersToConfigGroups < ActiveRecord::Migration
   def change
-    add_column :config_groups, :hosts_count, :integer
-    add_column :config_groups, :hostgroups_count, :integer
-    add_column :config_groups, :config_group_classes_count, :integer
+    add_column :config_groups, :hosts_count, :integer, :default => 0
+    add_column :config_groups, :hostgroups_count, :integer, :default => 0
+    add_column :config_groups, :config_group_classes_count, :integer, :default => 0
   end
 end
