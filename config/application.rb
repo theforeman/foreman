3fd648c7afaefb2eaff760a0e2fd872e0672d8cd
Achim Ledermüller <achim.ledermueller@netways.de>
2014-04-23 17:49:30 +0200

    one: validate fog is installed before using one api

--- config/application.rb
+++ config/application.rb
@@ -36,7 +36,7 @@ SETTINGS[:libvirt] = defined?(::Fog) && defined?(::Libvirt)
 SETTINGS[:ovirt] = defined?(::Fog) && defined?(::OVIRT)
 SETTINGS[:vmware] = defined?(::Fog) && defined?(::RbVmomi)
 SETTINGS[:gce] = defined?(::Fog) && defined?(::Google::APIClient::VERSION)
-SETTINGS[:one] = defined?(::OpenNebula::VERSION)
+SETTINGS[:one] = defined?(::Fog) && defined?(::OpenNebula::VERSION)
 SETTINGS[:openstack] = SETTINGS[:rackspace] = SETTINGS[:ec2] = !! defined?(::Fog)
 
 require File.expand_path('../../lib/foreman.rb', __FILE__)
