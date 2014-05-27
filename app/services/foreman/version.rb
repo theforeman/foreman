6f6c69702135c3c8674d807729deef4023639706
Joseph Magen <jmagen@redhat.com>
2014-05-27 14:54:16 +0300

    fixes #5912 - broken link into manual 'Useful template functions and macros'

--- app/services/foreman/version.rb
+++ app/services/foreman/version.rb
@@ -2,7 +2,7 @@ module Foreman
 
   # Simple struct for manipulation and comparing versions
   class Version
-    attr_reader :version, :major, :minor, :build, :tag, :short, :notag
+    attr_reader :version, :major, :minor, :build, :tag, :short, :notag, :prerelease
     alias :full :version
 
     def initialize givenversion=nil
@@ -14,6 +14,7 @@ module Foreman
       end
       @major, @minor, @build = @version.scan(/\d+/)
       @short = "#{@major}.#{@minor}"
+      @prerelease = @version.split('-')[1]
 
       if @version =~ /\A(.*)-([^-]+)\z/
         @notag = $1
@@ -27,6 +28,11 @@ module Foreman
     def to_s
       @version
     end
+
+    def short_latest_stable
+      @prerelease && @minor.to_i > 0 ? (@short.to_f - 0.1).to_s : @short
+    end
+
   end
 
 end
