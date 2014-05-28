bb0d37fc3afa585f889e8064c9a44e97465121a3
Dominic Cleal <dcleal@redhat.com>
2014-05-28 10:41:40 +0100

    fixes #5966 - pin execjs for Ruby 1.8 compatibility

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -1,6 +1,7 @@
 group :assets do
   gem 'sass-rails', '~> 3.2.3'
   gem 'uglifier', '>= 1.0.3'
+  gem 'execjs', '< 2.1.0'
   gem "jquery-rails", "2.0.3"
   gem 'jquery-ui-rails'
   gem "therubyracer", '0.11.3', :require => 'v8'
