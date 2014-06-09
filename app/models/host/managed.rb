11036a8d8fee4abb7e5b58200fbcf0a58e530dcd
Dmitri Dolguikh <dmitri@appliedlogic.ca>
2014-06-09 15:45:00 +0100

    fixes #5708: #nil? is now an allowed method inside of Host and Token jails
    (cherry picked from commit 3ff2bba4ef3987b131b49912fdd92bbf67f9c1ba)

--- app/models/host/managed.rb
+++ app/models/host/managed.rb
@@ -48,7 +48,7 @@ class Host::Managed < Host::Base
     allow :name, :diskLayout, :puppetmaster, :puppet_ca_server, :operatingsystem, :os, :environment, :ptable, :hostgroup, :location,
       :organization, :url_for_boot, :params, :info, :hostgroup, :compute_resource, :domain, :ip, :mac, :shortname, :architecture,
       :model, :certname, :capabilities, :provider, :subnet, :token, :location, :organization, :provision_method,
-      :image_build?, :pxe_build?, :otp, :realm, :param_true?, :param_false?
+      :image_build?, :pxe_build?, :otp, :realm, :param_true?, :param_false?, :nil?
   end
 
   attr_reader :cached_host_params
