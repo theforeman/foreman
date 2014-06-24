f31f7955571a8aac90209f236cbe3e3bcc187e7f
Joseph Magen <jmagen@redhat.com>
2014-06-24 17:46:14 +0300

    fixes #6355 - OS free text search shouldn't search associations
    (cherry picked from commit 366a0851c0bb0a8d760e21b9f90e9b65904b422d)

--- app/models/operatingsystem.rb
+++ app/models/operatingsystem.rb
@@ -43,10 +43,10 @@ class Operatingsystem < ActiveRecord::Base
   scoped_search :on => :hosts_count
   scoped_search :on => :hostgroups_count
 
-  scoped_search :in => :architectures,    :on => :name,  :complete_value => :true, :rename => "architecture"
-  scoped_search :in => :media,            :on => :name,  :complete_value => :true, :rename => "medium"
-  scoped_search :in => :config_templates, :on => :name,  :complete_value => :true, :rename => "template"
-  scoped_search :in => :os_parameters,    :on => :value, :on_key=> :name, :complete_value => true, :rename => :params
+  scoped_search :in => :architectures,    :on => :name,  :complete_value => :true, :rename => "architecture", :only_explicit => true
+  scoped_search :in => :media,            :on => :name,  :complete_value => :true, :rename => "medium", :only_explicit => true
+  scoped_search :in => :config_templates, :on => :name,  :complete_value => :true, :rename => "template", :only_explicit => true
+  scoped_search :in => :os_parameters,    :on => :value, :on_key=> :name, :complete_value => true, :rename => :params, :only_explicit => true
 
   FAMILIES = { 'Debian'    => %r{Debian|Ubuntu}i,
                'Redhat'    => %r{RedHat|Centos|Fedora|Scientific|SLC|OracleLinux}i,
