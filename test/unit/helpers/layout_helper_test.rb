bb988bca6e541e26474dedbb02f472bfec43f596
Joseph Magen <jmagen@redhat.com>
2014-06-26 11:11:31 +0300

    fixes #6400 - show * next to required fields on forms

--- test/unit/helpers/layout_helper_test.rb
+++ test/unit/helpers/layout_helper_test.rb
@@ -13,4 +13,12 @@ class LayoutHelperTest < ActionView::TestCase
     assert_not_include result, alert_close
   end
 
+  test "is_required?(f, attr) method returns true if attribute is required and false if not required" do
+    f = ActionView::Helpers::FormBuilder.new(:hostgroup, Hostgroup.new, @hostgroup, {}, {})
+    assert is_required?(f, :name)
+    assert is_required?(f, :title)
+    refute is_required?(f, :environment_id)
+    refute is_required?(f, :parent_id)
+  end
+
 end
