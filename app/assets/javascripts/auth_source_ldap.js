177911ad4488d7bea8ddfb18eaa861c79256d5ac
Ori Rabin <orabin@redhat.com>
2014-07-06 12:16:23 +0300

    changed variable value to a meaningful name

--- app/assets/javascripts/auth_source_ldap.js
+++ app/assets/javascripts/auth_source_ldap.js
@@ -1,15 +1,15 @@
 function change_ldap_port(item) {
   var port = $('#auth_source_ldap_port');
   if (port.length > 0) {
-    var value = port.val();
+    var port_value = port.val();
     var default_ports = port.data('default-ports');
-    if (value != null && default_ports != null && default_ports['ldap'] != null && default_ports['ldaps'] != null) {
+    if (port_value != null && default_ports != null && default_ports['ldap'] != null && default_ports['ldaps'] != null) {
       if ($(item).is(':checked')) {
-        if (value == default_ports['ldap']) {
+        if (port_value == default_ports['ldap']) {
           port.val(default_ports['ldaps']);
         }
       } else {
-        if (value == default_ports['ldaps']) {
+        if (port_value == default_ports['ldaps']) {
           port.val(default_ports['ldap']);
         }
       }
