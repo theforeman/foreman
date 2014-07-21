ff5239157a3640920d30d4cf423a055510ff7149
Dominic Cleal <dcleal@redhat.com>
2014-07-21 15:02:49 +0100

    fixes #6402 - use standard success/error handlers in UI controllers

--- app/controllers/concerns/foreman/controller/environments.rb
+++ app/controllers/concerns/foreman/controller/environments.rb
@@ -11,7 +11,7 @@ module Foreman::Controller::Environments
       @changed  = @importer.changes
     rescue => e
       if e.message =~ /puppet feature/i
-        error "We did not find a foreman proxy that can provide the information, ensure that you have at least one Proxy with the puppet feature turned on."
+        error _("No smart proxy was found to import environments from, ensure that at least one smart proxy is registered with the 'puppet' feature.")
         redirect_to :controller => controller_path and return
       else
         raise e
@@ -21,16 +21,16 @@ module Foreman::Controller::Environments
     if @changed["new"].size > 0 or @changed["obsolete"].size > 0 or @changed["updated"].size > 0
       render "common/_puppetclasses_or_envs_changed"
     else
-      notice "No changes to your environments detected"
+      notice _("No changes to your environments detected")
       redirect_to :controller => controller_path
     end
   end
 
   def obsolete_and_new
     if (errors = ::PuppetClassImporter.new.obsolete_and_new(params[:changed])).empty?
-      notice "Successfully updated environments and puppetclasses from the on-disk puppet installation"
+      notice _("Successfully updated environments and Puppet classes from the on-disk Puppet installation")
     else
-      error "Failed to update the environments and puppetclasses from the on-disk puppet installation<br/>" + errors.join("<br>")
+      error _("Failed to update environments and Puppet classes from the on-disk Puppet installation: %s") % errors.to_sentence
     end
     redirect_to :controller => controller_path
   end
