75885789c662a48ac258be4c14364271ca7da84c
Eric D. Helms <ericdhelms@gmail.com>
2014-08-01 21:42:19 +0100

    Fixes #6891: Taxonomies couldn't be seeded after initial run of seeds.
    On a clean run of seeds, a seed prior to the taxonomies sets User.current.
    On subsequent runs, errors are thrown since User.current is not thrown
    and taxonomy creation expects this.

--- db/seeds.d/05-taxonomies.rb
+++ db/seeds.d/05-taxonomies.rb
@@ -1,13 +1,17 @@
 # Create an initial organization if specified
 if SETTINGS[:organizations_enabled] && ENV['SEED_ORGANIZATION']
   Organization.without_auditing do
-    Organization.find_or_create_by_name(:name => ENV['SEED_ORGANIZATION'])
+    User.current = User.anonymous_admin
+    Organization.find_or_create_by_name!(:name => ENV['SEED_ORGANIZATION'])
+    User.current = nil
   end
 end
 
 # Create an initial location if specified
 if SETTINGS[:locations_enabled] && ENV['SEED_LOCATION']
   Location.without_auditing do
-    Location.find_or_create_by_name(:name => ENV['SEED_LOCATION'])
+    User.current = User.anonymous_admin
+    Location.find_or_create_by_name!(:name => ENV['SEED_LOCATION'])
+    User.current = nil
   end
 end
