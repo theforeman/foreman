8b01da843cffe421f5800f45538f9fcedecfe791
Ori Rabin <orabin@redhat.com>
2014-08-04 11:05:29 +0300

    Fixes #6794: Adds search parameter for template kinds

--- app/models/template_kind.rb
+++ app/models/template_kind.rb
@@ -3,4 +3,5 @@ class TemplateKind < ActiveRecord::Base
   has_many :config_templates, :inverse_of => :template_kind
   has_many :os_default_templates
   validates :name, :presence => true, :uniqueness => true
+  scoped_search :on => :name
 end
