12bb385dedbadf20294338008dd240684e05b026
Ori Rabin <orabin@redhat.com>
2014-08-04 11:05:29 +0300

    Fixes #6794: Adds search parameter for template kinds

--- app/controllers/api/v2/template_kinds_controller.rb
+++ app/controllers/api/v2/template_kinds_controller.rb
@@ -3,11 +3,13 @@ module Api
     class TemplateKindsController < V2::BaseController
 
       api :GET, "/template_kinds/", N_("List all template kinds")
+      param :search, String, :desc => N_("filter results"), :required => false
+      param :order, String, :desc => N_("sort results"), :required => false
       param :page, String, :desc => N_("paginate results")
       param :per_page, String, :desc => N_("number of entries per request")
 
       def index
-        @template_kinds = TemplateKind.paginate(paginate_options)
+        @template_kinds = TemplateKind.search_for(*search_options).paginate(paginate_options)
       end
     end
   end
