b4e4898632d6c9730345ce6b30c0a90060c7c8f1
Greg Sutcliffe <gsutclif@redhat.com>
2014-08-06 17:29:58 +0100

    Fixes #6962 - Use as_admin instead of find_by_login for usergroup tests

--- test/unit/usergroup_member_test.rb
+++ test/unit/usergroup_member_test.rb
@@ -2,10 +2,6 @@ require 'test_helper'
 
 class UsergroupMemberTest < ActiveSupport::TestCase
 
-  def setup
-    User.current = User.find_by_login "admin"
-  end
-
   test "remove membership of user in a group" do
     setup_admins_scenario
     assert_includes @superadmin_user.cached_user_roles.map(&:role), @semiadmin_role
@@ -234,12 +230,14 @@ class UsergroupMemberTest < ActiveSupport::TestCase
     @c.usergroups = [@d]
     @d.usergroups = [@e]
 
-    @user1 = FactoryGirl.create(:user, :login => 'um_user1')
-    @user2 = FactoryGirl.create(:user, :login => 'um_user2')
-    @user1.usergroups = [@d.reload] # @d cached #users already as []
-    @user2.usergroups = [@e]
-    @role = FactoryGirl.create(:role, :name => 'um_role')
-    @role_ur  = FactoryGirl.create :user_group_user_role, :owner => @a, :role => @role
+    as_admin do
+      @user1 = FactoryGirl.create(:user, :login => 'um_user1')
+      @user2 = FactoryGirl.create(:user, :login => 'um_user2')
+      @user1.usergroups = [@d.reload] # @d cached #users already as []
+      @user2.usergroups = [@e]
+      @role = FactoryGirl.create(:role, :name => 'um_role')
+      @role_ur  = FactoryGirl.create :user_group_user_role, :owner => @a, :role => @role
+    end
 
     assert_includes @user1.reload.cached_user_roles.map(&:role), @role
     assert_includes @user2.reload.cached_user_roles.map(&:role), @role
@@ -319,50 +317,54 @@ class UsergroupMemberTest < ActiveSupport::TestCase
   end
 
   def setup_redundant_scenario
-    @semiadmins = FactoryGirl.create :usergroup, :name => 'um_semiadmins'
-    @admins     = FactoryGirl.create :usergroup, :name => 'um_admins'
+    as_admin do
+      @semiadmins = FactoryGirl.create :usergroup, :name => 'um_semiadmins'
+      @admins     = FactoryGirl.create :usergroup, :name => 'um_admins'
 
-    @semiadmins.usergroups = [@admins]
+      @semiadmins.usergroups = [@admins]
 
-    @semiadmin_user = FactoryGirl.create(:user, :login => 'um_semiadmin1')
+      @semiadmin_user = FactoryGirl.create(:user, :login => 'um_semiadmin1')
 
-    @semiadmins.users = [@semiadmin_user]
-    @admins.users     = [@semiadmin_user]
+      @semiadmins.users = [@semiadmin_user]
+      @admins.users     = [@semiadmin_user]
 
-    @admin_role = FactoryGirl.create(:role, :name => 'um_admin')
+      @admin_role = FactoryGirl.create(:role, :name => 'um_admin')
 
-    @admin_ur = FactoryGirl.create :user_group_user_role, :owner => @admins, :role => @admin_role
+      @admin_ur = FactoryGirl.create :user_group_user_role, :owner => @admins, :role => @admin_role
+    end
   end
 
   def setup_admins_scenario
-    @semiadmins  = FactoryGirl.create :usergroup, :name => 'um_semiadmins'
-    @admins      = FactoryGirl.create :usergroup, :name => 'um_admins'
-    @superadmins = FactoryGirl.create :usergroup, :name => 'um_superadmins'
+    as_admin do
+      @semiadmins  = FactoryGirl.create :usergroup, :name => 'um_semiadmins'
+      @admins      = FactoryGirl.create :usergroup, :name => 'um_admins'
+      @superadmins = FactoryGirl.create :usergroup, :name => 'um_superadmins'
 
-    @semiadmins.usergroups = [@admins]
-    @admins.usergroups     = [@superadmins]
+      @semiadmins.usergroups = [@admins]
+      @admins.usergroups     = [@superadmins]
 
-    @semiadmin_user  = FactoryGirl.create(:user, :login => 'um_semiadmin1')
-    @admin_user      = FactoryGirl.create(:user, :login => 'um_admin1')
-    @superadmin_user = FactoryGirl.create(:user, :login => 'um_superadmin1')
+      @semiadmin_user  = FactoryGirl.create(:user, :login => 'um_semiadmin1')
+      @admin_user      = FactoryGirl.create(:user, :login => 'um_admin1')
+      @superadmin_user = FactoryGirl.create(:user, :login => 'um_superadmin1')
 
-    @semiadmins.users  = [@semiadmin_user]
-    @admins.users      = [@admin_user]
-    @superadmins.users = [@superadmin_user]
+      @semiadmins.users  = [@semiadmin_user]
+      @admins.users      = [@admin_user]
+      @superadmins.users = [@superadmin_user]
 
-    @semiadmin_role  = FactoryGirl.create(:role, :name => 'um_semiadmin')
-    @admin_role      = FactoryGirl.create(:role, :name => 'um_admin')
-    @superadmin_role = FactoryGirl.create(:role, :name => 'um_superadmin')
+      @semiadmin_role  = FactoryGirl.create(:role, :name => 'um_semiadmin')
+      @admin_role      = FactoryGirl.create(:role, :name => 'um_admin')
+      @superadmin_role = FactoryGirl.create(:role, :name => 'um_superadmin')
 
-    @semiadmin_ur  = FactoryGirl.create :user_group_user_role, :owner => @semiadmins, :role => @semiadmin_role
-    @admin_ur      = FactoryGirl.create :user_group_user_role, :owner => @admins, :role => @admin_role
-    @superadmin_ur = FactoryGirl.create :user_group_user_role, :owner => @superadmins, :role => @superadmin_role
+      @semiadmin_ur  = FactoryGirl.create :user_group_user_role, :owner => @semiadmins, :role => @semiadmin_role
+      @admin_ur      = FactoryGirl.create :user_group_user_role, :owner => @admins, :role => @admin_role
+      @superadmin_ur = FactoryGirl.create :user_group_user_role, :owner => @superadmins, :role => @superadmin_role
 
-    @role1 = FactoryGirl.create(:role)
-    @role2 = FactoryGirl.create(:role)
-    @role3 = FactoryGirl.create(:role)
-    @semiadmin_user.roles<< @role1
-    @admin_user.roles<< @role2
-    @superadmin_user.roles<< @role3
+      @role1 = FactoryGirl.create(:role)
+      @role2 = FactoryGirl.create(:role)
+      @role3 = FactoryGirl.create(:role)
+      @semiadmin_user.roles<< @role1
+      @admin_user.roles<< @role2
+      @superadmin_user.roles<< @role3
+    end
   end
 end
