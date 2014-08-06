ab866a3185b132555e1eed393f8de6c5a9a729c4
Ori Rabin <orabin@redhat.com>
2014-08-06 19:25:22 +0300

    Fixes #1592: making report::expire faster and without errors

--- test/factories/reports_related.rb
+++ test/factories/reports_related.rb
@@ -5,4 +5,41 @@ FactoryGirl.define do
     status 0
     metrics YAML.load("--- \n  time: \n    schedule: 0.00083\n    service: 0.149739\n    mailalias: 0.000283\n    cron: 0.000419\n    config_retrieval: 16.3637869358063\n    package: 0.003989\n    filebucket: 0.000171\n    file: 0.007025\n    exec: 0.000299\n  resources: \n    total: 33\n  changes: {}\n  events: \n    total: 0")
   end
+
+  trait :old_report do
+    after_build do |report|
+      report.created_at = 2.weeks.ago
+    end
+  end
+
+  trait :with_logs do
+    ignore do
+      log_count 30
+    end
+    after_create do |report,evaluator|
+      evaluator.log_count.times do
+      FactoryGirl.create(:log, :report => report)
+      end
+    end
+  end
+
+  factory :log do
+    report
+    level_id 1
+    after_build do |log|
+      log.message = FactoryGirl.create(:message)
+      log.source = FactoryGirl.create(:source)
+    end
+  end
+
+  factory :message do
+    sequence(:value) { |n| "message#{n}" }
+    sequence(:digest) { |n| Digest::SHA1.hexdigest("message#{n}") }
+  end
+
+  factory :source do
+    sequence(:value) { |n| "source#{n}" }
+    sequence(:digest) { |n| Digest::SHA1.hexdigest("source#{n}") }
+  end
+
 end
