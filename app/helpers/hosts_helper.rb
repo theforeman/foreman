f9616bd868dbf140a36636afe299897c95f5613b
Jason E. Rist <jrist@redhat.com>
2014-09-10 08:02:55 -0600

    Fixes #7403 - Some buttons not showing properly on host page

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -231,28 +231,32 @@ module HostsHelper
     title_actions(
         button_group(
             link_to_if_authorized(_("Edit"), hash_for_edit_host_path(:id => host).merge(:auth_object => host),
-                                    :title    => _("Edit your host")),
+                                    :title    => _("Edit your host"),
+                                    :class    => "btn btn-default"),
             if host.build
               link_to_if_authorized(_("Cancel build"), hash_for_cancelBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
                                     :disabled => host.can_be_built?,
-                                    :title    => _("Cancel build request for this host"))
+                                    :title    => _("Cancel build request for this host"),
+                                    :class    => "btn btn-default")
             else
               link_to_if_authorized(_("Build"), hash_for_setBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
                                     :disabled => !host.can_be_built?,
                                     :title    => _("Enable rebuild on next host boot"),
+                                    :class    => "btn btn-default",
                                     :confirm  => _("Rebuild %s on next reboot?\nThis would also delete all of its current facts and reports") % host)
             end
         ),
         if host.compute_resource_id || host.bmc_available?
           button_group(
-              link_to(_("Loading power state ..."), '#', :disabled => true, :id => :loading_power_state)
+              link_to(_("Loading power state ..."), '#', :disabled => true, :id => :loading_power_state, class: "btn btn-default")
           )
         end,
         button_group(
           if host.try(:puppet_proxy)
             link_to_if_authorized(_("Run puppet"), hash_for_puppetrun_host_path(:id => host).merge(:auth_object => host, :permission => 'puppetrun_hosts'),
                                   :disabled => !Setting[:puppetrun],
-                                  :title    => _("Trigger a puppetrun on a node; requires that puppet run is enabled"))
+                                  :title    => _("Trigger a puppetrun on a node; requires that puppet run is enabled"),
+                                  :class    => "btn btn-default")
           end
         ),
         button_group(
