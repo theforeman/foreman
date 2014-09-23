61750a4fcc0fdc046b90879abfd1b8560a82c569
karmab <karimboumedhel@gmail.com>
2014-09-23 18:11:42 +0200

    fixes #7608 - Override all puppetclass parameters in one click

--- app/helpers/puppetclasses_helper.rb
+++ app/helpers/puppetclasses_helper.rb
@@ -5,4 +5,8 @@ module PuppetclassesHelper
     klass = name.gsub('::', '/')
     "puppet/rdoc/#{environment}/classes/#{klass}.html"
   end
+
+  def overridden?(puppetclass)
+    puppetclass.class_params.present? && puppetclass.class_params.map(&:override).all?
+  end
 end
