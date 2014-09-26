c59d1081de4cedbbe43733564400031eff8e86ab
Dominic Cleal <dcleal@redhat.com>
2014-09-26 13:32:50 +0100

    refs #2127 - instantiate exception and pass i18n arg correctly

--- app/services/password_crypt.rb
+++ app/services/password_crypt.rb
@@ -2,7 +2,7 @@ class PasswordCrypt
   ALGORITHMS = {'MD5' => '$1$', 'SHA256' => '$5$', 'SHA512' => '$6$'}
 
   def self.passw_crypt(passwd, hash_alg = 'MD5')
-    raise Foreman::Exception N_("Unsupported password hash function '%s'") % hash_alg unless ALGORITHMS.has_key?(hash_alg)
+    raise Foreman::Exception.new(N_("Unsupported password hash function '%s'"), hash_alg) unless ALGORITHMS.has_key?(hash_alg)
     passwd.crypt("#{ALGORITHMS[hash_alg]}#{SecureRandom.base64(6)}")
   end
 
