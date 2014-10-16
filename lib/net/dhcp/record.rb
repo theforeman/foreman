c65b7011a142f2d65f2e2961d4d7cdcf4fd3d187
Lukas Zapletal <lzap+git@redhat.com>
2014-10-16 12:10:23 +0200

    Fixes #7971 - DHCP hostname commutative conflict fix

--- lib/net/dhcp/record.rb
+++ lib/net/dhcp/record.rb
@@ -51,7 +51,7 @@ module Net::DHCP
 
     def ==(other)
       return false unless other.present?
-      if attrs[:hostname].blank?
+      if other.attrs[:hostname].blank? || attrs[:hostname].blank?
         # If we're converting an 'ad-hoc' lease created by a host booting outside of Foreman's knowledge,
         # then :hostname will be blank on the incoming lease - if the ip/mac still match, then this
         # isn't a conflict
