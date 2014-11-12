d3faf043a74173be65ddef2c284d370a5ea2751f
Dominic Cleal <dcleal@redhat.com>
2014-11-12 16:58:04 +0000

    fixes #8366 - add parameter type to unique index

---
+++ db/migrate/20141112165600_add_type_to_parameter_index.rb
@@ -0,0 +1,12 @@
+class AddTypeToParameterIndex < ActiveRecord::Migration
+  def up
+    if index_exists? :parameters, [:reference_id, :name], :unique => true
+      remove_index :parameters, :column => [:reference_id, :name]
+      add_index :parameters, [:type, :reference_id, :name], :unique => true
+    end
+  end
+
+  def down
+    remove_index :parameters, :column => [:type, :reference_id, :name]
+  end
+end
