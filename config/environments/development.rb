55186b75c641104ff1850b397c01b237d4696f62
Lukas Zapletal <lzap+git@redhat.com>
2014-11-18 14:32:00 +0100

    Refs #8364 - removed javascript N+1 alerts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,6 @@ Foreman::Application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
     Bullet.rails_logger = true
