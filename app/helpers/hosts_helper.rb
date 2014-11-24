41d8cd7622fb59afb1d60906fa8559800191e2ae
ripcurld00d <ripcurld.github@gmail.com>
2014-11-24 15:39:14 +0000

    Fixes #2592 - No indication when waiting for host dashboard button action to complete

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -231,11 +231,11 @@ module HostsHelper
     title_actions(
         button_group(
             link_to_if_authorized(_("Edit"), hash_for_edit_host_path(:id => host).merge(:auth_object => host),
-                                    :title    => _("Edit your host")),
+                                    :title    => _("Edit your host"), :id => "edit-button"),
             if host.build
               link_to_if_authorized(_("Cancel build"), hash_for_cancelBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
                                     :disabled => host.can_be_built?,
-                                    :title    => _("Cancel build request for this host"))
+                                    :title    => _("Cancel build request for this host"), :id => "cancel-build-button")
             else
               link_to_if_authorized(_("Build"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts', :anchor => "review_before_build"),
                                     :disabled => !host.can_be_built?,
@@ -263,7 +263,7 @@ module HostsHelper
         ),
         button_group(
             link_to_if_authorized(_("Delete"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'destroy_hosts'),
-                                  :class => "btn btn-danger", :confirm => _('Are you sure?'), :method => :delete)
+                                  :class => "btn btn-danger", :id => "delete-button", :data => { :message => _("Are you sure?") }, :method => :delete)
         )
     )
   end
