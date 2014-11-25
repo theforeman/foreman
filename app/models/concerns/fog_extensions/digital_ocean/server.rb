3aa7d2c9a74ce8b6c478ca5a86d9bb6e4113a282
Tommy McNeely <tommy@lark-it.com>
2014-11-24 22:57:58 -0700

    fixes #8465 - fixes distribution.name nil class error
    * First it does the .to_i on the "id" which helps the "get" work
    * Second it does a ".try" on it so it can be nil quietly

--- app/models/concerns/fog_extensions/digital_ocean/server.rb
+++ app/models/concerns/fog_extensions/digital_ocean/server.rb
@@ -14,20 +14,21 @@ module FogExtensions
 
       def image
         requires :image_id
-        @image ||= service.images.get(image_id)
+        @image ||= service.images.get(image_id.to_i)
       end
 
       def region
         requires :region_id
-        @region ||= service.regions.get(region_id)
+        @region ||= service.regions.get(region_id.to_i)
       end
 
       def imageName
-        "#{image.distribution} #{image.name}"
+        distro = image.try(:distribution)
+        "#{distro} #{image.name}"
       end
 
       def ip_addresses
-        mage[public_ip_address, private_ip_address].flatten.select(&:present?)
+        [public_ip_address, private_ip_address].flatten.select(&:present?)
       end
 
     end
