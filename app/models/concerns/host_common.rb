83ea43d8bbb13237b98d1fec5af04a850b88509e
Tommy McNeely <tommy@lark-it.com>
2014-11-25 22:41:34 -0700

    fixes #8498 - fixes detection of encrypted root passwords
    A root password that started with "$" would not be hashed, and therefore
    would end up being stored in clear text and inserted in clear text to the
    /etc/shadow file, which just won't work. This adds a bit more to the match
    to ensure it starts with a $, then has a number, then another $, then
    any characters (salt), then another $, then some more characters. It has
    been tested both ways and seems to work well.
    
    ~tommy

--- app/models/concerns/host_common.rb
+++ app/models/concerns/host_common.rb
@@ -109,9 +109,9 @@ module HostCommon
   def crypt_root_pass
     unless root_pass.empty?
       unencrypted_pass = root_pass
-      self.root_pass = unencrypted_pass.starts_with?('$') ? unencrypted_pass :
+      self.root_pass = !!(unencrypted_pass.match('^\$\d+\$.+\$.+')) ? unencrypted_pass :
           (operatingsystem.nil? ? PasswordCrypt.passw_crypt(unencrypted_pass) : PasswordCrypt.passw_crypt(unencrypted_pass, operatingsystem.password_hash))
-      self.grub_pass = unencrypted_pass.starts_with?('$') ? unencrypted_pass : PasswordCrypt.grub2_passw_crypt(unencrypted_pass)
+      self.grub_pass = !!(unencrypted_pass.match('^\$\d+\$.+\$.+')) ? unencrypted_pass : PasswordCrypt.grub2_passw_crypt(unencrypted_pass)
     end
   end
 
