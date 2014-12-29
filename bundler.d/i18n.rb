d7947de33a7e2cc5d5a0bce7bc183b6462cd98ef
Michael Moll <kvedulv@kvedulv.de>
2014-12-29 04:51:27 +0100

    fixes #8791 - pin i18n gem
    (cherry picked from commit 40c46f1f8670e1ef834182fe53e670da8da42428)
    
    Conflicts:
    	Gemfile

--- bundler.d/i18n.rb
+++ bundler.d/i18n.rb
@@ -3,4 +3,5 @@ group :i18n do
   gem 'gettext_i18n_rails', '~> 0.10'
   gem 'gettext_i18n_rails_js', '>= 0.0.8'
   gem 'i18n_data', '>= 0.2.6', :require => 'i18n_data'
+  gem 'i18n', '~> 0.6.4'
 end
