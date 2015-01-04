886b2c469ccbf2216d899f1a85043c4140b6e9cd
ripcurld00d <ripcurld.github@gmail.com>
2015-01-04 22:16:48 +0200

    Fixes #8786 - Show override button after removing overridden parameter
    After a parameter is overridden in a host or host group, if one wants to
    revert the decision and return to the inherited parameter, it was
    necessary to reload the page after clicking on remove. This commit
    fixes that by making it show the override button again without reloading.
    
    (cherry picked from commit 90b00443a6fe27366d6a17572b8b0a41b4dcab97)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -120,7 +120,7 @@ function remove_fields(link) {
 
 function mark_params_override(){
   $('#inherited_parameters .override-param').removeClass('override-param');
-  $('#inherited_parameters span').show();
+  $('#inherited_parameters [data-tag=override]').show();
   $('#parameters').find('[id$=_name]:visible').each(function(){
     var param_name = $(this);
     $('#inherited_parameters').find('[id^=name_]').each(function(){
@@ -135,13 +135,13 @@ function mark_params_override(){
   $('#inherited_puppetclasses_parameters [data-tag=override]').show();
   $('#puppetclasses_parameters').find('[data-property=class]:visible').each(function(){
     var klass = $(this).val();
-    var name = $(this).siblings('[data-property=name]').val();
+    var name = $(this).closest('tr').find('[data-property=name]').val();
     $('#inherited_puppetclasses_parameters [id^="puppetclass_"][id*="_params\\["][id$="\\]"]').each(function(){
       var param = $(this);
       if (param.find('[data-property=class]').text() == klass && param.find('[data-property=name]').text() == name) {
         param.find('.error').removeClass('error');
         param.find('.warning').removeClass('warning');
-        param.addClass('override-param');
+        param.closest('tr').find('[data-property=name]').addClass('override-param');
         param.find('input, textarea').addClass('override-param');
         param.find('[data-tag=override]').hide();
       }
