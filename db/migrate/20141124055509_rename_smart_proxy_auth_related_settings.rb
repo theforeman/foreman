47f4ea06aaf4fa31b08b7dbcb4b069085e17bbac
Stephen Benjamin <stbenjam@redhat.com>
2015-01-14 15:23:32 +0100

    fixes #8949 - fix noop migration for smart proxy auth settings

--- db/migrate/20141124055509_rename_smart_proxy_auth_related_settings.rb
+++ db/migrate/20141124055509_rename_smart_proxy_auth_related_settings.rb
@@ -1,11 +1,5 @@
 class RenameSmartProxyAuthRelatedSettings < ActiveRecord::Migration
-  def self.up
-    execute "UPDATE settings SET name='restrict_registered_smart_proxies' WHERE name='restrict_registered_smart_proxies'"
-    execute "UPDATE settings SET name='require_ssl_smart_proxies' WHERE name='require_ssl_smart_proxies'"
-  end
-
-  def self.down
-    execute "UPDATE settings SET name='restrict_registered_smart_proxies' WHERE name='restrict_registered_smart_proxies'"
-    execute "UPDATE settings SET name='require_ssl_smart_proxies' WHERE name='require_ssl_smart_proxies'"
+  def change
+    # Removed - #8949
   end
 end
