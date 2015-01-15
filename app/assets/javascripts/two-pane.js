200538af3b4351ab0e38986722b78c3f66d443de
Ori Rabin <orabin@redhat.com>
2015-01-15 18:10:04 +0200

    Fixes #8970 - fixed second click on two-pane link

--- app/assets/javascripts/two-pane.js
+++ app/assets/javascripts/two-pane.js
@@ -7,7 +7,7 @@ $.fn.tab = function ( option ) {
   }
 }
 
-$(document).on('click', ".table-two-pane td[class$='display-two-pane'] a", function(e) {
+$(document).on('click', ".table-two-pane .display-two-pane a", function(e) {
   if ($('.table-two-pane').length) {
     e.preventDefault();
     two_pane_open(this);
