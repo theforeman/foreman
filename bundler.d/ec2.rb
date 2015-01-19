b37a262e04180c5d8488e6946e7badac8112b164
Daniel Lobato <elobatocs@gmail.com>
2015-01-19 12:40:46 +0100

    Fixes #8641 - Update fog to 1.27

---
+++ bundler.d/ec2.rb
@@ -0,0 +1,3 @@
+group :ec2 do
+  gem 'fog-aws', '~> 0.0.8'
+end
--- bundler.d/fog.rb
+++ bundler.d/fog.rb
@@ -1,5 +1,4 @@
 group :fog do
- gem 'fog', '1.25.0'
- gem 'fog-core', '1.25.0'
- gem 'unf', '>= 0.1.3'
+ gem 'fog', '1.27.0'
+ gem 'fog-core', '1.27.4'
 end
