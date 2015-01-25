7560679b9849b0fc5c168d5047bd3469d719b45a
Daniel Lobato <elobatocs@gmail.com>
2015-01-25 18:09:57 +0100

    Fixes #6884 - Installation media without $minor generate broken $version
    $version is generated with the major version and a dot, producing paths
    like foo.bar/x64/7. which don't exist. For CentOS at least we want to generate
    a path without the dot. The default values for these media have been changed too.

--- db/seeds.d/10-installation_media.rb
+++ db/seeds.d/10-installation_media.rb
@@ -3,11 +3,11 @@ os_suse = Operatingsystem.find_all_by_type "Suse" || Operatingsystem.where("name
 # Installation media: default mirrors
 Medium.without_auditing do
   [
-    { :name => "CentOS mirror", :os_family => "Redhat", :path => "http://mirror.centos.org/centos/$major.$minor/os/$arch" },
+    { :name => "CentOS mirror", :os_family => "Redhat", :path => "http://mirror.centos.org/centos/$version/os/$arch" },
     { :name => "Debian mirror", :os_family => "Debian", :path => "http://ftp.debian.org/debian" },
     { :name => "Fedora mirror", :os_family => "Redhat", :path => "http://dl.fedoraproject.org/pub/fedora/linux/releases/$major/Fedora/$arch/os/" },
-    { :name => "FreeBSD mirror", :os_family => "Freebsd", :path => "http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$major.$minor-RELEASE/" },
-    { :name => "OpenSUSE mirror", :os_family => "Suse", :path => "http://download.opensuse.org/distribution/$major.$minor/repo/oss", :operatingsystems => os_suse },
+    { :name => "FreeBSD mirror", :os_family => "Freebsd", :path => "http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$version-RELEASE/" },
+    { :name => "OpenSUSE mirror", :os_family => "Suse", :path => "http://download.opensuse.org/distribution/$version/repo/oss", :operatingsystems => os_suse },
     { :name => "Ubuntu mirror", :os_family => "Debian", :path => "http://archive.ubuntu.com/ubuntu" },
     { :name => "CoreOS mirror", :os_family => "Coreos", :path => "http://$release.release.core-os.net" }
   ].each do |input|
