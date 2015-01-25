9a89b0e6d43476d947c4e6bd09028e22219c9b3d
Daniel Lobato <elobatocs@gmail.com>
2015-01-25 18:09:57 +0100

    Fixes #6884 - Installation media without $minor generate broken $version
    $version is generated with the major version and a dot, producing paths
    like foo.bar/x64/7. which don't exist. For CentOS at least we want to generate
    a path without the dot. The default values for these media have been changed too.

--- test/unit/operatingsystem_test.rb
+++ test/unit/operatingsystem_test.rb
@@ -345,4 +345,10 @@ class OperatingsystemTest < ActiveSupport::TestCase
     assert_equal("#{operatingsystem.id}-applet-מערכתההפעלהשלי 4", operatingsystem.to_param)
   end
 
+  test 'interpolated $version does not include dots if only major is specified' do
+    operatingsystem = FactoryGirl.build(:operatingsystem, :name => 'foo', :major => '4')
+    result_path = operatingsystem.interpolate_medium_vars('http://foo.org/$version',
+                                                          'x64', operatingsystem)
+    assert result_path, 'http://foo.org/4'
+  end
 end
