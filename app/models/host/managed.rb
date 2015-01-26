e9672724ef29fc2db401e05aa89eacc2c06c51a7
Jiri Stransky <jistr@redhat.com>
2015-01-26 15:46:32 +0100

    Refs #9049 - fix log message formatting
    Missing "%s" in the commit that was supposed to fix #9049 caused the
    exception message not to be printed into the log.

--- app/models/host/managed.rb
+++ app/models/host/managed.rb
@@ -607,7 +607,7 @@ class Host::Managed < Host::Base
     ProxyAPI::Puppet.new({:url => puppet_proxy.url}).run fqdn
   rescue => e
     errors.add(:base, _("failed to execute puppetrun: %s") % e)
-    logger.warn "unable to execute puppet run: " % e
+    logger.warn "unable to execute puppet run: #{e}"
     logger.debug e.backtrace.join("\n")
     false
   end
