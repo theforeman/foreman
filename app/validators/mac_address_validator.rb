03e3186e56f8594cb03bec53e2e42666154425f4
Marek Hulan <mhulan@redhat.com>
2015-02-03 10:38:19 +0100

    Fixes #9195 - register MAC validation errors properly

--- app/validators/mac_address_validator.rb
+++ app/validators/mac_address_validator.rb
@@ -1,7 +1,14 @@
 class MacAddressValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless Net::Validations.valid_mac? value
-      record.errors[attribute] << (options[:message] || _("is not a valid MAC address"))
+    unless Net::Validations.valid_mac?(value)
+      make_invalid(record, attribute)
     end
+  rescue Net::Validations::Error
+    make_invalid(record, attribute)
+  end
+
+  def make_invalid(record, attribute)
+    record.errors[attribute] << (options[:message] || _("is not a valid MAC address"))
+    false
   end
 end
