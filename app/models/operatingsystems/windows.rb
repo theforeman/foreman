046b0099211aaf9ef55fab28c1774587534547f1
Dmitry Kireev <dmitry@kireev.co>
2015-02-09 14:51:17 -0800

    Fixing #9299

--- app/models/operatingsystems/windows.rb
+++ app/models/operatingsystems/windows.rb
@@ -1,16 +1,34 @@
 class Windows < Operatingsystem
-  PXEFILES = {:kernel => "startrom.0", :initrd => "boot.sdi"}
-
+  PXEFILES = {:kernel => "wimboot", :initrd => "bootmgr", :bcd => "bcd", :bootsdi => "boot.sdi", :bootwim => "boot.wim"}
+  
   def pxe_type
     "waik"
   end
 
-  def pxedir
-    "images"
+  def pxe_prefix(arch)
+    "boot/windows-#{arch}/".gsub(" ","-")
   end
 
-  def url_for_boot(file)
-    pxedir + "/" + PXEFILES[file]
+  def bootfile arch, type
+    pxe_prefix(arch) + eval("#{self.family}::PXEFILES[:#{type}]")
+  end
+
+  def boot_files_uri(medium, architecture, host = nil)
+    raise ::Foreman::Exception.new(N_("invalid medium for %s"), to_s) unless media.include?(medium)    
+
+    pxe_dir = ""
+
+    PXEFILES.values.collect do |img|    
+      if img =~ /boot.sdi/i || img =~ /bcd/i
+        pxe_dir = "boot"
+      elsif img =~ /boot.wim/i
+        pxe_dir = "sources"
+      else
+        pxe_dir = ""
+      end
+      
+      URI.parse("#{medium_vars_to_uri(medium.path, architecture.name, self)}/#{pxe_dir}/#{img}").normalize
+    end
   end
 
   def self.model_name
