7875f359ca99e588e6216818d344dad91e6156a2
Tom Caspy <tcaspy@gmail.com>
2015-02-10 17:37:03 +0200

    fixes #9240 - in case of hash with indifferent access, serialize as hash
    (cherry picked from commit 3059cea1c207718217f07b9cdc706bc516f5db1b)

--- app/models/compute_resources/foreman/model/vmware.rb
+++ app/models/compute_resources/foreman/model/vmware.rb
@@ -281,7 +281,7 @@ module Foreman::Model
     end
 
     def parse_args(args)
-      args = args.symbolize_keys
+      args = args.deep_symbolize_keys
 
       # convert rails nested_attributes into a plain, symbolized hash
       [:interfaces, :volumes].each do |collection|
@@ -333,7 +333,7 @@ module Foreman::Model
 
     def new_vm(args = {})
       args = parse_args args
-      opts = vm_instance_defaults.symbolize_keys.merge(args.symbolize_keys)
+      opts = vm_instance_defaults.symbolize_keys.merge(args.symbolize_keys).deep_symbolize_keys
       client.servers.new opts
     end
 
