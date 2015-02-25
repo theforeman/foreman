0cbe5d183611ec686b8c76011840ce9c0320d9c2
Johan Swensson <jswensso@redhat.com>
2015-02-25 11:31:45 +0100

    fixes #6044 - production.log should have timestamps in it
    (cherry picked from commit 70a602462261f6e9404c408e296618997c1e7ba5)

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,12 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+#Add timestamps and severity to environment.log
+class Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{timestamp.strftime('%Y-%m-%d %H:%M:%S')} [#{severity[0]}] #{msg}\n"
+  end
+end
+
 # Initialize the rails application
 Foreman::Application.initialize!
