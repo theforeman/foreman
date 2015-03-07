35011391d02883e2c141767db50a33e3abadf033
Daniel Lobato <elobatocs@gmail.com>
2015-03-07 12:55:51 +0200

    Fixes #9674 - Handle ProxyAPI exceptions on PuppetCA controller

--- app/controllers/puppetca_controller.rb
+++ app/controllers/puppetca_controller.rb
@@ -3,13 +3,7 @@ class PuppetcaController < ApplicationController
     @proxy = find_proxy
     # expire cache if forced
     Rails.cache.delete("ca_#{@proxy.id}") if params[:expire_cache] == "true"
-    certs = if params[:state].blank?
-              SmartProxies::PuppetCA.find_by_state(@proxy, "valid") + SmartProxies::PuppetCA.find_by_state(@proxy, "pending")
-            elsif params[:state] == "all"
-              SmartProxies::PuppetCA.all @proxy
-            else
-              SmartProxies::PuppetCA.find_by_state @proxy, params[:state]
-            end
+    certs = find_certs
     @certificates = certs.sort.paginate :page => params[:page], :per_page => Setting::General.entries_per_page
   end
 
@@ -17,9 +11,10 @@ class PuppetcaController < ApplicationController
     @proxy = find_proxy(:edit_smart_proxies_puppetca)
     cert = SmartProxies::PuppetCA.find(@proxy, params[:id])
     if cert.sign
-      process_success({ :success_redirect => smart_proxy_puppetca_index_path(@proxy, :state => params[:state]), :object_name => cert.to_s })
+      process_success(:success_redirect => smart_proxy_puppetca_index_path(@proxy, :state => params[:state]),
+                      :object_name => cert.to_s)
     else
-      process_error({ :redirect => smart_proxy_puppetca_index_path(@proxy) })
+      process_error(:redirect => smart_proxy_puppetca_index_path(@proxy))
     end
   end
 
@@ -37,5 +32,16 @@ class PuppetcaController < ApplicationController
 
   def find_proxy(permission = :view_smart_proxies_puppetca)
     SmartProxy.authorized(permission).find(params[:smart_proxy_id])
+  end
+
+  def find_certs
+    if params[:state].blank?
+      SmartProxies::PuppetCA.find_by_state(@proxy, "valid") +
+        SmartProxies::PuppetCA.find_by_state(@proxy, "pending")
+    elsif params[:state] == "all"
+      SmartProxies::PuppetCA.all @proxy
+    else
+      SmartProxies::PuppetCA.find_by_state @proxy, params[:state]
+    end
   end
 end
--- test/functional/puppetca_controller_test.rb
+++ test/functional/puppetca_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class PuppetcaControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test 'problems when signing certificate redirect to back' do
+    proxy = smart_proxies(:puppetmaster)
+    # Try set any random path in the referer to ensure redirect_to :back
+    @request.env['HTTP_REFERER'] = hosts_path
+    # This will try to find the certificate to no avail and will raise a ProxyException
+    post :update, { :smart_proxy_id => proxy.id, :id => 1 }, set_session_user
+    assert_redirected_to @request.env['HTTP_REFERER']
+    assert_match /ProxyAPI::ProxyException/, flash[:error]
   end
 end
