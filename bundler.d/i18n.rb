28d8a1506a4e97782040d52cc2564a4d2492ec3c
Dominic Cleal <dcleal@redhat.com>
2015-03-25 09:45:37 +0000

    fixes #9851 - pin gettext_i18n_rails_js to 0.x
    Cherry picked part of 51a8843830a66f00c2194e27b79b059c92759dcb

--- bundler.d/i18n.rb
+++ bundler.d/i18n.rb
@@ -1,7 +1,7 @@
 group :i18n do
   gem 'fast_gettext', '>=0.8.0'
   gem 'gettext_i18n_rails', '~> 0.10'
-  gem 'gettext_i18n_rails_js', '>= 0.0.8'
+  gem 'gettext_i18n_rails_js', '~> 0.0', '>= 0.0.8'
   gem 'i18n_data', '>= 0.2.6', :require => 'i18n_data'
   gem 'i18n', '~> 0.6.4'
 end
