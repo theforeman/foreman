e2980bb88992b7be517631dcd7bf952e5add79a9
Ondrej Prazak <oprazak@redhat.com>
2015-04-08 14:19:04 +0200

    Fixes #9439/BZ-1192552: 'white spaces' changed to 'spaces' in validation error message

--- app/validators/no_whitespace_validator.rb
+++ app/validators/no_whitespace_validator.rb
@@ -1,5 +1,5 @@
 class NoWhitespaceValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    record.errors.add(attribute, _("can't contain white spaces.")) if value =~ /\s/
+    record.errors.add(attribute, _("can't contain spaces.")) if value =~ /\s/
   end
 end
