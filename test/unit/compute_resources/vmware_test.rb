3aec1326d6a822a5da03f9c01aa40044303f3b3e
Chad Walstrom <chad.walstrom@govdelivery.com>
2015-04-08 09:09:29 -0500

    Fixes #10093 - VMware#create_vm calls clone_vm
    Apply '.with_indifferent_access' to the args passed in to create_vm,
    ensuring symbol test for args[:image_id] succeeds when args["image_id"]
    is present.
    
    (cherry picked from commit 140cfe262eac838e76eb27517bd0271beab9be15)

--- test/unit/compute_resources/vmware_test.rb
+++ test/unit/compute_resources/vmware_test.rb
@@ -45,6 +45,27 @@ class VmwareTest < ActiveSupport::TestCase
     assert_equal mock_vm, cr.new_vm(attrs_in)
   end
 
+  describe "#create_vm" do
+    setup do
+      @cr = FactoryGirl.build(:vmware_cr)
+      @cr.stubs(:test_connection)
+    end
+    test "calls clone_vm when image provisioning with symbol key" do
+      args = {:image_id =>"2" }
+      @cr.stubs(:parse_networks).returns(args)
+      @cr.expects(:clone_vm)
+      @cr.expects(:new_vm).times(0)
+      @cr.create_vm(args)
+    end
+    test "calls clone_vm when image provisioning with string key" do
+      args = {"image_id" =>"2" }
+      @cr.stubs(:parse_networks).returns(args)
+      @cr.expects(:clone_vm)
+      @cr.expects(:new_vm).times(0)
+      @cr.create_vm(args)
+    end
+  end
+
   test "#create_vm calls clone_vm when image provisioning" do
     attrs_in = HashWithIndifferentAccess.new("image_id"=>"2","cpus"=>"1", "interfaces_attributes"=>{"new_interfaces"=>{"type"=>"VirtualE1000", "network"=>"network-17", "_delete"=>""}, "0"=>{"type"=>"VirtualVmxnet3", "network"=>"network-17", "_delete"=>""}}, "volumes_attributes"=>{"new_volumes"=>{"size_gb"=>"10", "_delete"=>""}, "0"=>{"size_gb"=>"1", "_delete"=>""}})
     attrs_parsed = HashWithIndifferentAccess.new("image_id"=>"2","cpus"=>"1", "interfaces_attributes"=>{"new_interfaces"=>{"type"=>"VirtualE1000", "network"=>"Test network", "_delete"=>""}, "0"=>{"type"=>"VirtualVmxnet3", "network"=>"Test network", "_delete"=>""}}, "volumes_attributes"=>{"new_volumes"=>{"size_gb"=>"10", "_delete"=>""}, "0"=>{"size_gb"=>"1", "_delete"=>""}})
