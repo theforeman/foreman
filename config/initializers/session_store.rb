99ce0d0ecca98ef7a7e65de90a59b4de4de4bb11
Dominic Cleal <dcleal@redhat.com>
2015-05-14 15:39:25 +0100

    fixes #10510 - move secure flag to existing session configuration
    On a source installation with SSL, the session store is properly configured
    now and no longer continually resets user sessions.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Foreman::Application.config.session_store :active_record_store
+Foreman::Application.config.session_store :active_record_store, :secure => !!SETTINGS[:require_ssl]
