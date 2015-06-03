15055fe8f888647ca5cec704c6bcff3f088d8f72
Marek Hulan <mhulan@redhat.com>
2015-06-03 12:52:46 +0200

    Fixes #10642 - save the interface regardless of validation errors
    (cherry picked from commit 19d684ddf481c5ce8b30d234db28e8c336ccc272)

--- db/migrate/20150508124600_copy_unmanaged_hosts_to_interfaces.rb
+++ db/migrate/20150508124600_copy_unmanaged_hosts_to_interfaces.rb
@@ -19,9 +19,18 @@ class CopyUnmanagedHostsToInterfaces < ActiveRecord::Migration
     say "Migrating Unmanaged Host interfaces to standalone Interfaces"
 
     FakeHost.where(:managed => false).each do |host|
+      say "  ... migrating #{host.name}"
       nic = FakeNic.where(:host_id => host.id, :primary => true, :provision => true).first
       if nic.present?
         nic.name = host.name if nic.name.blank?
+        # we ignore validation errors on existing primary interface
+        unless nic.valid?
+          say "  Host #{host.name} has invalid interface with id `#{nic.id}` and identifier `#{nic.identifier}`, you have to fix it manually through API"
+          nic.errors.each { |a, m| say "    * #{a} #{m}" }
+          say "    following command is an example of how to modify the interface IP through API using curl"
+          say "      curl -X PUT -H 'Content-Type: application/json' --user 'admin:$adminpw' -d '{\"interface\":{\"ip\":\"$real_ip\"}}' 'https://$foreman_url/api/v2/hosts/#{host.name}/interfaces/#{nic.id}'"
+        end
+        nic.save(:validate => false)
       else
         nic = FakeNic.new
         nic.host_id = host.id
@@ -32,10 +41,11 @@ class CopyUnmanagedHostsToInterfaces < ActiveRecord::Migration
         nic.primary = true
         nic.provision = true
         nic.type = 'Nic::Managed'
+        nic.save!
       end
-      nic.save!
 
-      say "  Migrated #{nic.name}-#{nic.identifier} to nics"
+      say "  ... migrated #{nic.name}-#{nic.identifier} to nics"
+      say ''
     end
   end
 
