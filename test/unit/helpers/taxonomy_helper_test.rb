707d9bdda6bb95cdaf6f77281ee5a16e2b5bc555
Shlomi Zadok <shlomi@ben-hanna.com>
2015-06-28 16:05:06 +0300

    fixes #10868 - use classify for a Symbol instead of humanize

---
+++ test/unit/helpers/taxonomy_helper_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class TaxonomyHelperTest < ActionView::TestCase
+  include LayoutHelper
+  include TaxonomyHelper
+
+  describe "all_checkbox" do
+    setup do
+      User.current = users(:admin)
+    end
+
+    it "resource symbol should be constantized" do
+      form = form_for(taxonomies(:organization1)) do |f|
+        all_checkbox(f, :hostgroups)
+      end
+      html = Nokogiri::HTML(form)
+      assert_nothing_raised do
+        html.css("input[id='organization_ignore_types_hostgroup']")[0]["value"].constantize
+      end
+    end
+  end
+end
