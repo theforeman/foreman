33573dc6d1a46b859365b1dcffa204c92510044f
Shimon Shtein <sshtein@redhat.com>
2015-07-06 13:59:46 +0300

    Fixes #10438 - Removed trends:clean task, as it is no longer needed

--- test/lib/tasks/trends_test.rb
+++ test/lib/tasks/trends_test.rb
@@ -6,7 +6,6 @@ class TrendsTest < ActiveSupport::TestCase
     Rake.application.rake_require 'tasks/trends'
     Rake::Task.define_task(:environment)
     Rake::Task['trends:reduce'].reenable
-    Rake::Task['trends:clean'].reenable
 
     TrendCounter.unscoped.delete_all
     Trend.unscoped.delete_all
@@ -175,25 +174,6 @@ class TrendsTest < ActiveSupport::TestCase
     assert_equal os_point_dates[3], new_intervals[1].interval_start
     assert_equal os_point_dates[5], new_intervals[1].interval_end
     assert_equal os_point_dates[5], new_intervals[2].interval_start
-  end
-
-  test 'trends:clean' do
-    trend = FactoryGirl.create(:foreman_trends, :operating_system)
-    created_at = Time.now
-    args = [:trend_counter, :trend => trend, :created_at => created_at, :updated_at => created_at, :count => 1]
-    # create a legit counter
-    FactoryGirl.create(*args)
-
-    # now create some dupes (skipping validations)
-    2.times do
-      FactoryGirl.build(*args).save(validate: false)
-    end
-
-    assert_equal 3, TrendCounter.where(trend_id: trend.id).length
-
-    Rake.application.invoke_task 'trends:clean'
-
-    assert_equal 1, TrendCounter.where(trend_id: trend.id).length
   end
 
   private
