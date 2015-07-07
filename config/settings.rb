42f00572146b1059a689381f6bc5414542d26a91
Eric D. Helms <ericdhelms@gmail.com>
2015-07-07 19:54:05 -0400

    Fixes #11056: Configure base SETTINGS for test environment.
    This change introduces the idea of having a base configuration for
    SETTINGS within the test environment. This is captured in the
    settings.yaml.test file. To be able to detect the Rails environment
    in settings.rb, the ordering had to be changed whereby the Gemfile
    is loaded up via boot.rb and then Rails is required so that the
    Rails environment is available by the time settings.rb is processed.

--- config/settings.rb
+++ config/settings.rb
@@ -2,7 +2,9 @@ root = File.expand_path(File.dirname(__FILE__) + "/..")
 require 'yaml'
 require "#{root}/app/services/foreman/version"
 
-SETTINGS = YAML.load_file("#{root}/config/settings.yaml")
+settings_file = Rails.env.test? ? 'config/settings.yaml.test' : 'config/settings.yaml'
+
+SETTINGS = YAML.load_file("#{root}/#{settings_file}")
 SETTINGS[:version] = Foreman::Version.new
 SETTINGS[:unattended] = SETTINGS[:unattended].nil? || SETTINGS[:unattended]
 SETTINGS[:login]    ||= SETTINGS[:ldap]
