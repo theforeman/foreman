d6e3bd3e08efb65b52d56a397f2adcd331681ede
Dominic Cleal <dcleal@redhat.com>
2015-07-16 08:11:37 +0100

    fixes #11120 - migrate websockets setting when readonly in settings.yaml

--- db/migrate/20150312144232_migrate_websockets_setting.rb
+++ db/migrate/20150312144232_migrate_websockets_setting.rb
@@ -1,19 +1,37 @@
+class FakeSetting < ActiveRecord::Base
+  self.table_name = 'settings'
+
+  def default=(v)
+    write_attribute :default, v.to_yaml
+  end
+
+  def value=(v)
+    v = v.to_yaml unless v.nil?
+    write_attribute :value, v
+  end
+
+  def value
+    v = read_attribute(:value)
+    YAML.load(v) unless v.nil?
+  end
+end
+
 class MigrateWebsocketsSetting < ActiveRecord::Migration
   def up
-    return unless encrypt = Setting.find_by_name("websockets_encrypt")
+    return unless encrypt = FakeSetting.find_by_name("websockets_encrypt")
     encrypt.settings_type = "boolean"
     if encrypt.value == "auto"
       encrypt.value = (Setting[:websockets_ssl_key].nil? ||
                        Setting[:websockets_ssl_cert].nil?) ? false : true
-    else
+    elsif !encrypt.value.nil?
       encrypt.value = Foreman::Cast.to_bool(encrypt.value)
     end
-    encrypt.default = false
-    encrypt.save(:validate => false)
+    encrypt.default = !!SETTINGS[:require_ssl]
+    encrypt.save!
   end
 
   def down
     # delete and reset on next app server start
-    Setting.delete_by_name("websockets_encrypt")
+    FakeSetting.find_by_name("websockets_encrypt").delete
   end
 end
