3f8e6c33624f33acd65b371bf67549606397286e
imriz <imrizvik@gmail.com>
2015-07-19 14:27:03 +0300

    Fixes #11150 - Allow searching of facts as types other than string

--- app/models/concerns/scoped_search_extensions.rb
+++ app/models/concerns/scoped_search_extensions.rb
@@ -7,5 +7,19 @@ module ScopedSearchExtensions
       return value.tr_s('%*', '%') if (value =~ /%|\*/)
       "%#{value}%"
     end
+
+    def cast_facts(key, operator, value)
+      is_int = (value =~ /\A[-+]?\d+\z/ ) || (value.is_a?(Integer))
+      is_pg = ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'postgresql'
+      # Once Postgresql 8 support is removed (used in CentOS 6), this could be replaced to only keep the first form (working well with PG 9)
+      if (is_int && !is_pg)
+        casted = "CAST(fact_values.value AS DECIMAL) #{operator} #{value}"
+      elsif (is_int && is_pg)
+        casted = "fact_values.value ~ E'^\\\\d+$' AND CAST(fact_values.value AS DECIMAL) #{operator} #{value}"
+      else
+        casted = "fact_values.value #{operator} '#{value}'"
+      end
+      casted
+    end
   end
 end
