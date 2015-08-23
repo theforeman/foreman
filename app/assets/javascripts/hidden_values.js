04687d07c92ff57830472ba9b05f7e69c92e513d
Tomer Brisker <tbrisker@gmail.com>
2015-08-23 13:53:55 +0300

    Fixes #11450 - Improve hidden parameter handling

--- app/assets/javascripts/hidden_values.js
+++ app/assets/javascripts/hidden_values.js
@@ -1,22 +1,12 @@
-function turn_textarea_switch() {
-  $('.hidden_value_textarea_switch').each(function (index, switch_field) {
-    if ($(switch_field).attr('data-switch-active') != 'true') {
-      $(switch_field).change(function () {
-        var source = $(this).closest('tr').children('td.value').children()[0];
-        if (source === undefined) {
-          source = $(this).closest('div.form-group').parent().prev().children().children('div.col-md-4').children()[0];
-        }
-        if (this.checked) {
-          var target = '<input class="form-control" type="password" id="' + source.id + '" name="' + source.name + '" value ="' + source.value + '"></input>'
-        } else {
-          var target = '<textarea class="form-control" cols="40" id="' + source.id + '" name="' + source.name + '" placeholder="Value" rows="1">' + source.value + '</textarea>'
-        }
-        $(source).replaceWith(target);
-      });
-      this.value = '1';
-      $(this).attr('data-switch-active', 'true');
-    }
-  })
+function turn_textarea_switch(checkbox) {
+  var id = checkbox.id.replace(/hidden_value$/,"value");
+  var source = document.getElementById(id);
+  if (checkbox.checked) {
+    var target = '<input class="form-control" type="password" id="' + id + '" name="' + source.name + '" value ="' + source.value + '"></input>'
+  } else {
+    var target = '<textarea class="form-control" id="' + id + '" name="' + source.name + '" placeholder="Value" rows="1">' + source.value + '</textarea>'
+  }
+  $(source).replaceWith(target);
 }
 
 function hidden_value_control(){
@@ -34,12 +24,6 @@ function replace_value_control(link) {
   link.parent().find(".full-value").toggleClass("unhide pull-left");
   link.parent().parent().find('a.pull-left').toggleClass("hide");
 }
-
-// normal page load trigger
-$(document).ready(turn_textarea_switch);
-
-// two-pane ajax trigger
-$(document).ajaxComplete(turn_textarea_switch);
 
 // normal page load trigger
 $(document).ready(hidden_value_control);
