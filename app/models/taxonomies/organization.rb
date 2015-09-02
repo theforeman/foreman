422ade326b222b9f565bbaffee11fc47227572c1
Tomer Brisker <tbrisker@gmail.com>
2015-09-02 09:58:45 +0300

    Fixes #11650 - show global parameter value inherited from parent taxonomy

--- app/models/taxonomies/organization.rb
+++ app/models/taxonomies/organization.rb
@@ -19,20 +19,6 @@ class Organization < Taxonomy
     where(conditions)
   }
 
-  # returns self and parent parameters as a hash
-  def parameters(include_source = false)
-    hash = {}
-    ids = ancestor_ids
-    ids << id unless new_record? or self.frozen?
-    # need to pull out the organizations to ensure they are sorted first,
-    # otherwise we might be overwriting the hash in the wrong order.
-    orgs = ids.size == 1 ? [self] : Organization.sort_by_ancestry(Organization.includes(:organization_parameters).find(ids))
-    orgs.each do |org|
-      org.organization_parameters.each {|p| hash[p.name] = include_source ? {:value => p.value, :source => N_('organization').to_sym, :source_name => org.title} : p.value }
-    end
-    hash
-  end
-
   def dup
     new = super
     new.locations = locations
