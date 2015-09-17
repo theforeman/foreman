6542b50be1493aec02cb9d79792d6804af9d521d
Dominic Cleal <dcleal@redhat.com>
2015-09-17 15:56:25 +0100

    fixes #11835 - increase host_status.status field length to store metrics

--- db/migrate/20150612135546_create_host_status.rb
+++ db/migrate/20150612135546_create_host_status.rb
@@ -2,7 +2,7 @@ class CreateHostStatus < ActiveRecord::Migration
   def up
     create_table :host_status do |t|
       t.string :type
-      t.integer :status, :default => 0, :null => false
+      t.integer :status, :default => 0, :null => false, :limit => 5
       t.references :host, :null => false
       t.datetime :reported_at, :null => false
     end
@@ -18,7 +18,7 @@ class CreateHostStatus < ActiveRecord::Migration
   end
 
   def down
-    add_column :hosts, :puppet_status, :integer,  :null => false, :default => 0
+    add_column :hosts, :puppet_status, :bigint, :null => false, :default => 0
     remove_column :hosts, :global_status
     remove_foreign_key "host_status", :name => "host_status_hosts_host_id_fk"
     remove_index :host_status, :host_id
