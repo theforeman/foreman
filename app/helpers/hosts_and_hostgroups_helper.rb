027d208768c08e97ce3e000e3b24eb1f1b5cf75f
Tomer Brisker <tbrisker@gmail.com>
2015-10-06 10:13:27 +0300

    Fixes #8800 - Clip table cells only when needed
    For clipping to work, table must have class `table-fixed` and the cell
    have class `ellipsis`. Also column widths should be set in table head.
    Clipping will occur automagically with tooltip.

--- app/helpers/hosts_and_hostgroups_helper.rb
+++ app/helpers/hosts_and_hostgroups_helper.rb
@@ -4,7 +4,7 @@ module HostsAndHostgroupsHelper
   def model_name(host)
     name = host.try(:model)
     name = host.compute_resource.name if host.compute_resource
-    trunc_with_tooltip(name, 14)
+    name
   end
 
   def accessible_hostgroups
