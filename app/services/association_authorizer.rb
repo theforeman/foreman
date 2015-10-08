b6b977aaa0ca4ee4ff7cc59de234f1cbba54c5f4
Stephen Benjamin <stbenjam@redhat.com>
2015-10-08 19:59:12 -0400

    fixes #12118 - query model for alternative permission names

--- app/services/association_authorizer.rb
+++ app/services/association_authorizer.rb
@@ -1,20 +1,21 @@
 class AssociationAuthorizer
-  def self.authorized_associations(associations,  klass_name = nil, should_raise_exception = true)
+  def self.authorized_associations(associations,  klass_name = nil, should_raise_exception = true, action = 'view')
     if associations.included_modules.include?(Authorizable)
       associations_klass = associations
       if associations.respond_to?(:klass)
         associations_klass = associations.klass
       end
       klass_name ||= associations_klass
-      permission = view_permission_name(klass_name, should_raise_exception)
+      permission = permission_name(klass_name, action, should_raise_exception)
       associations.authorized(permission, associations_klass) if permission
     else
       associations
     end
   end
 
-  def self.view_permission_name(klass, should_raise_exception)
-    permission = "view_#{klass.to_s.underscore.pluralize}"
+  def self.permission_name(klass, permission, should_raise_exception)
+    suffix = klass.respond_to?(:permission_name) ? klass.permission_name : klass.to_s.underscore.pluralize
+    permission = "#{permission}_#{suffix}"
     if Permission.where(:name => permission).present?
       permission
     elsif should_raise_exception
