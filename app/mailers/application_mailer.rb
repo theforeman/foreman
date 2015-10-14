5d9cacc51685ba757f25961ac2f556ae841be50b
Stephen Benjamin <stbenjam@redhat.com>
2015-10-14 08:13:16 -0400

    fixes #12155 - register changes in reply address without restart

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,7 @@
 require 'uri'
 
 class ApplicationMailer < ActionMailer::Base
-  default :from => Setting[:email_reply_address] || "noreply@foreman.example.org"
+  default :from => -> { Setting[:email_reply_address] || "noreply@foreman.example.org" }
 
   def mail(headers = {}, &block)
     if headers.present?
@@ -12,34 +12,6 @@ class ApplicationMailer < ActionMailer::Base
   end
 
   private
-
-  class GroupMail
-    def initialize(emails)
-      Foreman::Deprecation.deprecation_warning("1.11", "GroupMail will be removed as mailers should not generate multiple messages, use MailNotification.deliver")
-      @emails = emails
-    end
-
-    def deliver
-      @emails.each do |email|
-        begin
-          email.deliver
-        rescue => e
-          Foreman::Logging.exception("Unable to send email notification", e)
-        end
-      end
-    end
-  end
-
-  def group_mail(users, options)
-    Foreman::Deprecation.deprecation_warning("1.11", "group_mail is replaced by MailNotification.deliver with :users in the options hash")
-    mails = users.map do |user|
-      @user = user
-      set_locale_for user
-      mail(options.merge(:to => user.mail)) unless user.mail.blank?
-    end
-
-    GroupMail.new(mails.compact)
-  end
 
   def set_locale_for(user)
     old_loc = FastGettext.locale
--- test/unit/application_mailer_test.rb
+++ test/unit/application_mailer_test.rb
@@ -10,12 +10,20 @@ class ApplicationMailerTest < ActiveSupport::TestCase
     ActionMailer::Base.deliveries.last
   end
 
-  test "foreman server header is set" do
+  test 'foreman server header is set' do
     assert_equal mail.header['X-Foreman-Server'].to_s, 'foreman.some.host.fqdn'
   end
 
-  test "foreman subject prefix is attached" do
+  test 'foreman subject prefix is attached' do
     Setting[:email_subject_prefix] = '[foreman-production]'
     assert_match /^\[foreman-production\]/, mail.subject
+  end
+
+  # The ActionMailer default is only evaluated at initialization so changes
+  # were only registered after a restart.  The from address is now a lambda.
+  test 'reply address evalulated at send time' do
+    new_from = 'foreman@widgets.example.com'
+    Setting[:email_reply_address] = new_from
+    assert_equal mail.from.first, new_from
   end
 end
