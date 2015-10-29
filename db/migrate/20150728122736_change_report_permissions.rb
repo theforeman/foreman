c3a34b7634f445fdd07acd7a8cb90411b36a01f7
Daniel Lobato <elobatocs@gmail.com>
2015-10-29 09:40:01 +0100

    Fixes #12344 - Use Rails 4 update_all syntax in migrations
    **Problem**
    Rails 3 syntax allows `update_all(old,new)`, however on Rails 4 the only
    way to accomplish that is `where(old).update_all(new)`. Reports STI
    contains a migration with this.
    
    **Solution**
    Change the migration to use the `where(old).update_all(new)` syntax

--- db/migrate/20150728122736_change_report_permissions.rb
+++ db/migrate/20150728122736_change_report_permissions.rb
@@ -3,22 +3,22 @@ class ChangeReportPermissions < ActiveRecord::Migration
   def up
     old_name = 'Report'
     new_name = 'ConfigReport'
-    Permission.update_all("resource_type = '#{new_name}'", "resource_type = '#{old_name}'")
+    Permission.where(:resource_type => new_name).update_all(:resource_type => old_name)
     PERMISSIONS.each do |from|
       to = from.sub('reports', 'config_reports')
       say "renaming permission #{from} to #{to}"
-      Permission.update_all("name = '#{to}'", "name = '#{from}'")
+      Permission.where(:name => to).update_all(:name => from)
     end
   end
 
   def down
     old_name = 'ConfigReport'
     new_name = 'Report'
-    Permission.update_all("resource_type = '#{new_name}'", "resource_type = '#{old_name}'")
+    Permission.where(:resource_type => new_name).update_all(:resource_type => old_name)
     PERMISSIONS.each do |from|
       to = from.sub('config_reports', 'reports')
       say "renaming permission #{from} to #{to}"
-      Permission.update_all("name = '#{to}'", "name = '#{from}'")
+      Permission.where(:name => to).update_all(:name => from)
     end
   end
 end
