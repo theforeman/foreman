eec49f635eda4adf05e4567fc904b048dd30ecc1
Dominic Cleal <dcleal@redhat.com>
2016-01-04 08:40:36 +0000

    refs #12714 - i18n extraction and interpolation fixes

--- app/services/proxy_status.rb
+++ app/services/proxy_status.rb
@@ -18,7 +18,7 @@ class ProxyStatus
 
   # TODO: extract to another status implementation
   def tftp_server
-    raise ::Foreman::Exception.new(N_('No TFTP feature for %s') % proxy.to_label) unless proxy.has_feature?('TFTP')
+    raise ::Foreman::Exception.new(N_('No TFTP feature for %s'), proxy.to_label) unless proxy.has_feature?('TFTP')
     Rails.cache.fetch("proxy_#{proxy.id}/tftp_server", :expires_in => cache_duration) do
       fetch_proxy_data do
         ProxyAPI::TFTP.new(:url => proxy.url).bootServer
