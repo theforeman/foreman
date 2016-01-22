65f3844fe72d6070b6ca46b89062076275e256c1
Daniel Lobato <elobatocs@gmail.com>
2016-01-22 23:31:34 +0100

    Fixes #13340 - Seed Fedora Atomic installation media
    Fedora Atomic now has a repository where you can get the isolinux stuff
    from -https://dl.fedoraproject.org/pub/alt/atomic/stable/Cloud_Atomic/x86_64/os/
    
    This allows us to perform fully unattended installations through
    foreman. Before, you would have to download the image and prepare your
    own local installation media to PXE boot it
    (http://blog.daniellobato.me/unattended-deployments-of-fedora-and-rhel-atomic-with-foreman/).

--- db/seeds.d/10-installation_media.rb
+++ db/seeds.d/10-installation_media.rb
@@ -6,6 +6,7 @@ Medium.without_auditing do
     { :name => "CentOS mirror", :os_family => "Redhat", :path => "http://mirror.centos.org/centos/$version/os/$arch" },
     { :name => "Debian mirror", :os_family => "Debian", :path => "http://ftp.debian.org/debian" },
     { :name => "Fedora mirror", :os_family => "Redhat", :path => "http://dl.fedoraproject.org/pub/fedora/linux/releases/$major/Server/$arch/os/" },
+    { :name => "Fedora Atomic mirror", :os_family => "Redhat", :path => "http://dl.fedoraproject.org/pub/alt/atomic/stable/Cloud_Atomic/$arch/os/" },
     { :name => "FreeBSD mirror", :os_family => "Freebsd", :path => "http://ftp.freebsd.org/pub/FreeBSD/releases/$arch/$version-RELEASE/" },
     { :name => "OpenSUSE mirror", :os_family => "Suse", :path => "http://download.opensuse.org/distribution/$version/repo/oss", :operatingsystems => os_suse },
     { :name => "Ubuntu mirror", :os_family => "Debian", :path => "http://archive.ubuntu.com/ubuntu" },
