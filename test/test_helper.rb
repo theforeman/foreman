d80b72b05ee8e29988ff45820c5aa9dc5adce3f0
Daniel Lobato <elobatocs@gmail.com>
2016-02-18 14:29:52 +0100

    Fixes #13293 - Make all ActiveRecord threads share the same connection
    Using transactional fixtures means "1 test" - "1 transaction" - the
    reason we moved to DatabaseCleaner is that the 'server thread'
    wouldn't be able to access the data in the 'test thread' as it is in a
    transaction.
    
    So we turned transactional_fixtures off, so data is
    available to the server thread, and decided to clean the db with
    DatabaseCleaner. That was very slow, and bugs were hard to find when
    some integration tests would fail every now and then.
    
    What we're doing here is to force ActiveRecord to share the same
    connection between the 'server thread' and the 'test thread'. It's fine
    because both don't request data from the db at the same time. Since data
    isn't saved to db but stored in a transaction, the speed boost is very
    noticeable too.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -257,17 +257,11 @@ Spork.prefork do
     end
   end
 
-  # Transactional fixtures do not work with Selenium tests, because Capybara
-  # uses a separate server thread, which the transactions would be hidden
-  # from. We hence use DatabaseCleaner to truncate our test database.
-  DatabaseCleaner.strategy = :truncation
-
   class ActionDispatch::IntegrationTest
     # Make the Capybara DSL available in all integration tests
     include Capybara::DSL
 
-    # Stop ActiveRecord from wrapping tests in transactions
-    self.use_transactional_fixtures = false
+    self.use_transactional_fixtures = true
 
     def assert_index_page(index_path,title_text,new_link_text = nil,has_search = true,has_pagination = true)
       visit index_path
@@ -373,7 +367,7 @@ Spork.each_run do
     setup :login_admin
 
     teardown do
-      DatabaseCleaner.clean       # Truncate the database
+      wait_for_ajax if Capybara.current_driver == Capybara.javascript_driver
       Capybara.reset_sessions!    # Forget the (simulated) browser state
       Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
     end
@@ -387,4 +381,14 @@ Spork.each_run do
       click_button "Login"
     end
   end
+
+  class ActiveRecord::Base
+    mattr_accessor :shared_connection
+    @@shared_connection = nil
+
+    def self.connection
+      @@shared_connection || ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
+    end
+  end
+  ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 end
