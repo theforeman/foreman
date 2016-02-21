9734f1f395b38a5b4920086bb287ea5857704df3
Shlomi Zadok <shlomi@ben-hanna.com>
2016-02-21 15:01:30 +0200

    Fixes #13519 - Alias lookupkey key to parameter

--- app/models/lookup_keys/puppetclass_lookup_key.rb
+++ app/models/lookup_keys/puppetclass_lookup_key.rb
@@ -3,7 +3,7 @@ class PuppetclassLookupKey < LookupKey
   has_many :environments, -> { uniq }, :through => :environment_classes
   has_many :param_classes, :through => :environment_classes, :source => :puppetclass
 
-  scoped_search :in => :param_classes, :on => :name, :rename => :puppetclass, :complete_value => true
+  scoped_search :in => :param_classes, :on => :name, :rename => :puppetclass, :alias => :puppetclass_name, :complete_value => true
 
   scope :smart_class_parameters_for_class, lambda { |puppetclass_ids, environment_id|
                                              joins(:environment_classes).where(:environment_classes => {:puppetclass_id => puppetclass_ids, :environment_id => environment_id})
