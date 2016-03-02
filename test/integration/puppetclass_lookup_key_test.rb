109ad47dcb245bb8d081d76b644dda450a6ffec2
Ori Rabin <orabin@redhat.com>
2016-03-02 10:52:55 +0200

    Fixes #13976 - override checkbox in smart class parameter doesn't uncheck other checkboxes

--- test/integration/puppetclass_lookup_key_test.rb
+++ test/integration/puppetclass_lookup_key_test.rb
@@ -35,5 +35,32 @@ class PuppetclassLookupKeyIntegrationTest < ActionDispatch::IntegrationTest
       page.find("#puppetclass_lookup_key_override").click
       assert page.find("#puppetclass_lookup_key_hidden_value:enabled")
     end
+
+    test "uncheck override" do
+      visit puppetclass_lookup_keys_path
+      within(:xpath, "//table") do
+        click_link "ssl"
+      end
+
+      page.find("#puppetclass_lookup_key_hidden_value").click
+
+      assert_submit_button(puppetclass_lookup_keys_path)
+      wait_for_ajax
+
+      within(:xpath, "//table") do
+        click_link "ssl"
+      end
+
+      page.find("#puppetclass_lookup_key_override").click
+
+      assert_submit_button(puppetclass_lookup_keys_path)
+      wait_for_ajax
+
+      within(:xpath, "//table") do
+        click_link "ssl"
+      end
+
+      assert page.find("#puppetclass_lookup_key_hidden_value").checked?
+    end
   end
 end
