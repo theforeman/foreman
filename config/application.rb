c7afbb86fde365746733e25d4964f4ec738e018a
Tom Caspy <tcaspy@gmail.com>
2016-03-07 11:24:36 +0200

    fixes #6792 - filtering template

--- config/application.rb
+++ config/application.rb
@@ -137,7 +137,7 @@ module Foreman
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :account_password, :facts, :root_pass, :value, :report, :password_confirmation, :secret]
+    config.filter_parameters += [:password, :account_password, :facts, :root_pass, :value, :report, :password_confirmation, :secret, :template]
 
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
