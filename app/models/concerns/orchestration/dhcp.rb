4baf7d4097c05d38365f85f74649efbd4b1ae73c
Marek Hulan <mhulan@redhat.com>
2016-03-09 09:01:45 +0100

    Fixes #14113 - run dhcp orchestration only on DHCP subnets

--- app/models/concerns/orchestration/dhcp.rb
+++ app/models/concerns/orchestration/dhcp.rb
@@ -12,7 +12,7 @@ module Orchestration::DHCP
     # host.managed? and managed? should always come first so that orchestration doesn't
     # even get tested for such objects
     (host.nil? || host.managed?) && managed? && hostname.present? && ip_available? && mac_available? &&
-        !subnet.nil? && subnet.dhcp? && SETTINGS[:unattended] && (!provision? || operatingsystem.present?)
+        !subnet.nil? && subnet.dhcp? && subnet.dhcp_boot_mode? && SETTINGS[:unattended] && (!provision? || operatingsystem.present?)
   end
 
   def dhcp_record
