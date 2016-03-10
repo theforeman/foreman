f0091ac8b2e8b53efb05514d78aa0545d2176a61
Dominic Cleal <dominic@cleal.org>
2016-03-10 08:33:56 +0000

    fixes #14133 - update to rubocop 0.39.0
    Replaces hirb-unicode with the maintained hirb-unicode-steakknife fork,
    due to conflicting requirements on unicode-display_width with the older
    hirb-unicode and new rubocop.
    
    Rake is unpinned due to the newer rubocop's compatibility with it, and
    parser unpinned as the newer rubocop+parser combination works. Custom
    rake test tasks have warnings disabled to match Rails behaviour and
    prevent lots of output.

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -1,6 +1,6 @@
 group :development do
   gem 'maruku', '~> 0.7'
-  gem 'rubocop', '0.35.1'
+  gem 'rubocop', '0.39.0'
 
   # for generating i18n files
   gem 'gettext', '>= 3.2.1', '< 4.0.0', :require => false
