d1be1d2e4dc50dd9fc4f527aa207b435cdbe1657
Ivan Nečas <inecas@redhat.com>
2016-03-11 14:14:59 +0100

    Fixes #6959 - set operating system for RHEV/ovirt VMs
    Backward compatible, but works best with https://github.com/abenari/rbovirt/pull/104

--- test/unit/compute_resources/ovirt_test.rb
+++ test/unit/compute_resources/ovirt_test.rb
@@ -24,4 +24,30 @@ class OvirtTest < ActiveSupport::TestCase
       assert_find_by_uuid_raises(ActiveRecord::RecordNotFound, cr)
     end
   end
+
+  describe "associating operating system" do
+    setup do
+      operating_systems_xml = Nokogiri::XML(File.read('test/fixtures/ovirt_operating_systems.xml'))
+      ovirt_oses = operating_systems_xml.xpath('/operating_systems/operating_system').map do |os|
+        OVIRT::OperatingSystem.new(self, os)
+      end
+      @compute_resource = FactoryGirl.build(:ovirt_cr).tap do |compute_resource|
+        compute_resource.stubs(:available_operating_systems).returns(ovirt_oses)
+      end
+      @host = FactoryGirl.build(:host, :mac => 'ca:d0:e6:32:16:97')
+    end
+
+    it 'maps operating system to ovirt operating systems' do
+      @compute_resource.determine_os_type(@host).must_equal "other_linux"
+
+      @host.operatingsystem = operatingsystems(:redhat)
+      @compute_resource.determine_os_type(@host).must_equal "rhel_6"
+
+      @host.architecture = architectures(:x86_64)
+      @compute_resource.determine_os_type(@host).must_equal "rhel_6x64"
+
+      @host.operatingsystem = operatingsystems(:ubuntu1210)
+      @compute_resource.determine_os_type(@host).must_equal "ubuntu_12_10"
+    end
+  end
 end
