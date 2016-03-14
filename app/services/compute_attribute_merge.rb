8cfc9d24536f4b9601f6d2724281c22104e87e1f
Dominic Cleal <dominic@cleal.org>
2016-03-14 11:52:21 +0000

    fixes #14179 - don't merge NIC compute attrs in ComputeProfileMerge
    When creating a host over the API and specifying interface compute
    attributes, the interface attributes were overwritten from the compute
    profile.  Example request:
    
        {"host":{"interfaces_attributes":{"0":{"primary":"true","compute_attributes":{"model":"e1000"}}}, ...}}
    
    ComputeProfileMerge stores :interfaces_attributes (name varies based on
    the CR type) from the profile in host.compute_attributes, while
    InterfaceMerge stores attributes in each interface.  When
    Orchestration::Compute#add_interfaces_to_compute_attrs runs to merge
    per-interface attributes into host.compute_attributes, the step was
    skipped as host.compute_attributes[:interfaces_attributes] already
    existed from ComputeProfileMerge.
    
    This change excludes interface attributes from being merged by
    ComputeProfileMerge, so InterfaceMerge is responsible for copying them
    into the interface.

--- app/services/compute_attribute_merge.rb
+++ app/services/compute_attribute_merge.rb
@@ -4,7 +4,11 @@ class ComputeAttributeMerge
 
     host.compute_attributes ||= {}.with_indifferent_access
 
-    vm_attrs = compute_attrs.vm_attrs
+    # Exclude interfaces compute attributes from merge, use InterfaceMerge
+    # instead for more precise merging of interfaces
+    interface_attrs_key = "#{compute_attrs.compute_resource.interfaces_attrs_name}_attributes"
+    vm_attrs = compute_attrs.vm_attrs.except(interface_attrs_key)
+
     vm_attrs = vm_attrs.deep_merge(host.compute_attributes)
 
     host.compute_attributes = vm_attrs
