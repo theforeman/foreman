240c24eb9ed54219944afab8431059c815a183e4
Tomer Brisker <tbrisker@gmail.com>
2016-03-21 17:12:11 +0200

    Fixes #14297 - Correctly calculate dashboard widget width

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -18,15 +18,16 @@ function auto_refresh(){
 }
 
 function start_gridster(){
+    var baseWidth = $(".gridster").width()/12 - 30;
     var gridster = $(".gridster>ul").gridster({
-        widget_margins: [10, 10],
-        widget_base_dimensions: [94, 340],
+        widget_margins: [15, 10],
+        widget_base_dimensions: [baseWidth, 360],
         max_size_x: 12,
         min_cols: 12,
         max_cols: 12,
         autogenerate_stylesheet: false
     }).data('gridster');
-
+    refreshCharts();
     $(".gridster>ul>li[data-hide='true']").each(function(i, widget) {
         $(widget).hide();
         gridster.remove_widget(widget);
