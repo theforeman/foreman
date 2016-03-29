dccf77508aa3945bd8a80fe6353769a11d79cd59
Ori Rabin <orabin@redhat.com>
2016-03-29 15:20:14 +0300

    Refs #12979 - adding btn-default class to leftover buttons

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -337,16 +337,16 @@ module HostsHelper
     title_actions(
         button_group(
             link_to_if_authorized(_("Edit"), hash_for_edit_host_path(:id => host).merge(:auth_object => host),
-                                    :title    => _("Edit your host"), :id => "edit-button"),
+                                    :title    => _("Edit your host"), :id => "edit-button", :class => 'btn btn-default'),
             if host.build
               link_to_if_authorized(_("Cancel build"), hash_for_cancelBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
                                     :disabled => host.can_be_built?,
-                                    :title    => _("Cancel build request for this host"), :id => "cancel-build-button")
+                                    :title    => _("Cancel build request for this host"), :id => "cancel-build-button", :class => 'btn btn-default')
             else
               link_to_if_authorized(_("Build"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts', :anchor => "review_before_build"),
                                     :disabled => !host.can_be_built?,
                                     :title    => _("Enable rebuild on next host boot"),
-                                    :class    => "btn",
+                                    :class    => "btn btn-default",
                                     :id       => "build-review",
                                     :data     => { :toggle => 'modal',
                                                    :target => '#review_before_build',
