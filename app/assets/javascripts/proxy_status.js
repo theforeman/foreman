ddbd8f72da0b3584e8810248996bf32e2b466741
Tomer Brisker <tbrisker@gmail.com>
2016-04-17 16:07:00 +0300

    Fixes #14679 - Fix tabbing of puppet CA in proxy
    The links were missing the `data-toggle="tab"` attribute, causing the
    tabbing to break when using under katello.

--- app/assets/javascripts/proxy_status.js
+++ app/assets/javascripts/proxy_status.js
@@ -1,5 +1,8 @@
 $(document).on('ContentLoad', function() {
   $('.nav-tabs a').on('shown.bs.tab', refreshCharts);
+  $('a[data-toggle="tab"]').on('click', function(e) {
+    history.pushState(null, null, $(this).attr('href'));
+  });
   showProxies();
   loadTFTP();
   setTab();
