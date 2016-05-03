c8654a57b33c005ee7337d277a228ced851aa697
Dominic Cleal <dominic@cleal.org>
2016-05-03 09:10:04 +0100

    fixes #15751 - replace spork with spring in development
    To make use of it, run bin/rails and bin/rake rather than plain
    rails or rake commands, and run `spring status` or `spring stop` to
    check and stop current background processes.
    
    Test log removal has moved to the logging framework as the file would
    not be recreated after being deleted in the test helper, since the
    Rails environment and logger are not reloaded on each `rake test`.

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -12,4 +12,5 @@ group :development do
 
   gem 'bullet'
   gem "parallel_tests"
+  gem 'spring', '~> 1.0'
 end
--- test/facet_test_helper.rb
+++ test/facet_test_helper.rb
@@ -1,14 +1,12 @@
-Spork.prefork do
-  unless ActiveRecord::Base.connection.table_exists?(:test_facets)
-    ActiveRecord::Base.connection.create_table :test_facets do |t|
-      # :id is created automatically
-      t.integer :host_id
-    end
+unless ActiveRecord::Base.connection.table_exists?(:test_facets)
+  ActiveRecord::Base.connection.create_table :test_facets do |t|
+    # :id is created automatically
+    t.integer :host_id
   end
-  unless ActiveRecord::Base.connection.table_exists?(:module_test_facets)
-    ActiveRecord::Base.connection.create_table :module_test_facets do |t|
-      # :id is created automatically
-      t.integer :host_id
-    end
+end
+unless ActiveRecord::Base.connection.table_exists?(:module_test_facets)
+  ActiveRecord::Base.connection.create_table :module_test_facets do |t|
+    # :id is created automatically
+    t.integer :host_id
   end
 end
