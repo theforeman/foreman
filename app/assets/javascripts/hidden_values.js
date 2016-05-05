8a5e18cf613c17ed9e3e23e409f78a92e5be2c05
Tomer Brisker <tbrisker@gmail.com>
2016-05-05 12:52:38 +0300

    Fixes #13741 - Prevent hidden parameters from erasing newline
    Switching the input from textarea to password leads to all newline
    characters being stripped from the value. This PR attempts to prevent
    this by changing the font of the textarea to a font that displays a
    bullet instead of every character, thereby preventing the need to
    change the input into a password field.

--- app/assets/javascripts/hidden_values.js
+++ app/assets/javascripts/hidden_values.js
@@ -1,27 +1,7 @@
-function turn_textarea_switch(checkbox) {
-  var target, session;
-  var id = checkbox.id.replace(/hidden_value$/, "value");
-  var source = document.getElementById(id);
-  var $editorContainer = $('.editor-container');
-
-  if (checkbox.checked) {
-    target = $('<input/>').attr({ type: 'password', id: id, name: source.name, value: $(source).val(), class: 'form-control'});
-    $editorContainer.find('.navbar').hide();
-    $editorContainer.find('.ace_editor').remove();
-    $(source).replaceWith(target);
-  } else if ($('.editor-container').length > 0) {
-    target = $('<textarea/>').attr({class: 'form-control editor_source hide', id: id, name: source.name, placeholder: 'Value', rows: 1, value: $(source).val()});
-    $editorContainer.find('.navbar').show();
-    $(source).replaceWith(target);
-
-    onEditorLoad();
-    session = Editor.getSession();
-    session.setValue($(source).val());
-  } else {
-    var target = $('<textarea/>').attr({class: 'form-control no-stretch', id: id, name: source.name, placeholder: 'Value', rows: 1, value: $(source).val()});
-    $(source).replaceWith(target);
-  }
+function turn_textarea_switch() {
+  $('.editor-container .ace_content').toggleClass('masked-input');
 }
+
 function hidden_value_control(){
   $(".toggle-hidden-value a").click(function(event){
     event.preventDefault();
