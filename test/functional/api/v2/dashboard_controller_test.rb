e2fcc3213464f709e53c228fab75b91e766e762d
Shlomi Zadok <shlomi@ben-hanna.com>
2016-05-08 12:03:42 +0300

    Fixes #14877 - Adds glossary to dashboard API

--- test/functional/api/v2/dashboard_controller_test.rb
+++ test/functional/api/v2/dashboard_controller_test.rb
@@ -7,4 +7,13 @@ class Api::V2::DashboardControllerTest < ActionController::TestCase
     dashboard = ActiveSupport::JSON.decode(@response.body)
     assert_operator(dashboard.length, :>, 0)
   end
+
+  test "should have glossary" do
+    get :index, { }
+    assert_response :success
+    dashboard = ActiveSupport::JSON.decode(@response.body)
+    assert(dashboard["glossary"], 'Should have glossary')
+    assert(dashboard["glossary"].is_a?(Hash), 'Glossary should have children')
+    assert_equal(17, dashboard["glossary"].keys.length)
+  end
 end
