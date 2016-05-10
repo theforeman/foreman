59e6ee202b794a0c9fcde447a36c4ab660a34d78
Dominic Cleal <dominic@cleal.org>
2016-05-10 09:45:31 +0100

    fixes #8884 - drop Gemfile support for Ruby 1.9.3

--- bundler.d/console.rb
+++ bundler.d/console.rb
@@ -1,9 +1,5 @@
 group :console do
-  if RUBY_VERSION.start_with? '1.9.'
-    gem 'wirb', '~> 1.0'
-  else
-    gem 'wirb', '>= 1.0', '< 3.0'
-  end
+  gem 'wirb', '>= 1.0', '< 3.0'
   gem 'hirb-unicode-steakknife', '~> 0.0.7', :require => 'hirb-unicode'
   gem 'awesome_print', '~> 1.0', :require => 'ap'
 end
