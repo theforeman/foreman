6d3f957bfef99042550eea857b19911b2f2e36c8
Lukas Zapletal <lzap+git@redhat.com>
2016-05-10 13:05:52 +0200

    Fixes #14980 - fixed unused_ip API subnet param

--- app/services/ipam/dhcp.rb
+++ app/services/ipam/dhcp.rb
@@ -5,7 +5,7 @@ module IPAM
       return unless subnet.dhcp?
       # we have DHCP proxy so asking it for free IP
       logger.debug "Asking #{dhcp.url} for free IP"
-      ip = dhcp_proxy.unused_ip(self, mac)["ip"]
+      ip = dhcp_proxy.unused_ip(subnet, mac)["ip"]
       logger.debug("Found #{ip}")
       ip
     end
