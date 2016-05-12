ba6cd8bea66f49099d21ed1dd8292ac96a91db28
Marek Hulan <mhulan@redhat.com>
2016-05-12 09:27:25 +0200

    Fixes #15020 - display only selected oVirt network

--- app/assets/javascripts/compute_resources/ovirt/nic_info.js
+++ app/assets/javascripts/compute_resources/ovirt/nic_info.js
@@ -1,3 +1,3 @@
 providerSpecificNICInfo = function(form) {
-  return form.find('select.ovirt_name').val() + ' @ ' + form.find('select.ovirt_network').text();
+  return form.find('select.ovirt_name').val() + ' @ ' + form.find('select.ovirt_network :selected').text();
 }
