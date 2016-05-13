theforeman/foreman

commit c3e0fed15667ef934a96c9eafddf7c094d56eab7

Fixes #12225 - invalidate topbar cache after restart

app/controllers/topbar_sweeper.rb
@@ -24,4 +24,8 @@ class TopbarSweeper < ActionController::Caching::Sweeper
   def self.expire_cache(controller)
     controller.expire_fragment(TopbarSweeper.fragment_name) if User.current
   end
+
+  def self.expire_cache_all_users
+    Rails.cache.delete_matched(/#{fragment_name('\d+')}/)
+  end
 end

config/initializers/expire_cache.rb
@@ -1,0 +1,6 @@
+# During restart, newly enabled or disabled plugins may modify topbar menu. For this reason,
+# cache is invalidated for all users. This will work for the default cache provider (files)
+# but might not work with other providers (memcached). On these installations, users need
+# to logout and re-login in order to update the top menu.
+
+TopbarSweeper.expire_cache_all_users

commit e1d5a1b9b79edf2de91a1f4693e5e3496c68d950

fixes #15040 - remove user fragment cache initializer
The initializer added in c3e0fed caused the foreman initializer to fail
as loaded the User model, which it assumes meant the table was present.
This isn't the case when db:migrate runs for the first time.

The new initializer has been removed so as to only keep the existing
cache expiry in the foreman initializer, which now checks for presence
of the table instead. The existing initializer is also compatible with
memcache, preventing an exception starting Rails.

app/controllers/topbar_sweeper.rb
@@ -26,6 +26,8 @@ class TopbarSweeper < ActionController::Caching::Sweeper
   end
 
   def self.expire_cache_all_users
-    Rails.cache.delete_matched(/#{fragment_name('\d+')}/)
+    User.unscoped.pluck(:id).each do |id|
+      Rails.cache.delete("views/tabs_and_title_records-#{id}")
+    end
   end
 end

config/initializers/expire_cache.rb
@@ -1,6 +1,0 @@
-# During restart, newly enabled or disabled plugins may modify topbar menu. For this reason,
-# cache is invalidated for all users. This will work for the default cache provider (files)
-# but might not work with other providers (memcached). On these installations, users need
-# to logout and re-login in order to update the top menu.
-
-TopbarSweeper.expire_cache_all_users

config/initializers/foreman.rb
@@ -32,10 +32,5 @@ Menu::Loader.load
 Dashboard::Loader.load
 
 # clear our users topbar cache
-# The User model may not be loaded or the table may not exist in all cases
-# where this initializer is called such as during initial migration of the database
-if defined?(User)
-  User.unscoped.pluck(:id).each do |id|
-    Rails.cache.delete("views/tabs_and_title_records-#{id}")
-  end
-end
+# The users table may not be exist during initial migration of the database
+TopbarSweeper.expire_cache_all_users if User.table_exists?