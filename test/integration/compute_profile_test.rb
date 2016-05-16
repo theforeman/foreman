1cd2ef49c41c23e83f199bb15ba1f37193784c56
Dominic Cleal <dominic@cleal.org>
2016-05-16 12:05:33 +0100

    refs #14482 - use correct 1.11 test helper

--- test/integration/compute_profile_test.rb
+++ test/integration/compute_profile_test.rb
@@ -47,4 +47,33 @@ class ComputeProfileIntegrationTest < ActionDispatch::IntegrationTest
     click_link("another-ec2 (eu-west-1-EC2)")
     assert page.has_selector?('h1', :text => 'New compute profile on another-ec2 (eu-west-1-EC2)'), "New compute profile on another-ec2 (eu-west-1-EC2) was expected in the <h1> tag, but was not found"
   end
+
+  describe 'js tests' do
+    setup do
+      @driver = Capybara.current_driver
+      Capybara.current_driver = Capybara.javascript_driver
+      login_admin
+    end
+
+    teardown do
+      Capybara.current_driver = @driver
+    end
+
+    test "edit compute attribute page" do
+      visit compute_profile_path(compute_profiles(:one))
+      # amazon123 exists in fixture compute_attributes.yml
+      click_link("amazon123 (eu-west-1-EC2)")
+
+      selected_profile = find("#s2id_compute_attribute_compute_profile_id .select2-chosen").text
+      assert_equal compute_profiles(:one).name, selected_profile
+
+      selected_compute = find("#s2id_compute_attribute_compute_resource_id .select2-chosen").text
+      assert_equal "amazon123 (eu-west-1-EC2)", selected_compute
+
+      click_button('Submit')
+      assert has_link?("amazon123 (eu-west-1-EC2)")
+      # two pane should close on save
+      assert page.has_no_selector?('div.two-pane-right')
+    end
+  end
 end
