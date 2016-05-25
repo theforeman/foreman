3e05c127986cca0d3b53341a93e16d0c55d0840e
Ivan Nečas <inecas@redhat.com>
2016-05-25 09:28:20 +0200

    Fixes #15174 - make sure to return 404 when taxonomy not found

--- app/controllers/concerns/api/taxonomy_scope.rb
+++ app/controllers/concerns/api/taxonomy_scope.rb
@@ -7,11 +7,19 @@ module Api
     end
 
     def set_taxonomy_scope
-      if SETTINGS[:locations_enabled]
+      if SETTINGS[:locations_enabled] && params[:location_id].present?
         Location.current ||= @location = Location.find_by_id(params[:location_id])
+        unless @location
+          not_found _("Location with id %{id} not found") % { :id => params[:location_id] }
+          return false
+        end
       end
-      if SETTINGS[:organizations_enabled]
+      if SETTINGS[:organizations_enabled] && params[:organization_id].present?
         Organization.current ||= @organization = Organization.find_by_id(params[:organization_id])
+        unless @organization
+          not_found _("Organization with id %{id} not found") % { :id => params[:organization_id] }
+          return false
+        end
       end
     end
   end
