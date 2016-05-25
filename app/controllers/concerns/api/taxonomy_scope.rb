c4cdec71aa9fabbf44310ed87196722ad46c62fc
Marek Hulan <mhulan@redhat.com>
2016-05-25 17:47:31 +0200

    Fixes #15182 - limit user taxonomies in API (CVE-2016-4451)
    (cherry picked from commit 1144040f444b4bf4aae81940a150b26b23b4623c)

--- app/controllers/concerns/api/taxonomy_scope.rb
+++ app/controllers/concerns/api/taxonomy_scope.rb
@@ -8,14 +8,14 @@ module Api
 
     def set_taxonomy_scope
       if SETTINGS[:locations_enabled] && params[:location_id].present?
-        Location.current ||= @location = Location.find_by_id(params[:location_id])
+        Location.current ||= @location = Location.my_locations.find_by_id(params[:location_id])
         unless @location
           not_found _("Location with id %{id} not found") % { :id => params[:location_id] }
           return false
         end
       end
       if SETTINGS[:organizations_enabled] && params[:organization_id].present?
-        Organization.current ||= @organization = Organization.find_by_id(params[:organization_id])
+        Organization.current ||= @organization = Organization.my_organizations.find_by_id(params[:organization_id])
         unless @organization
           not_found _("Organization with id %{id} not found") % { :id => params[:organization_id] }
           return false
