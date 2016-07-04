1ff7e25908f81912458a0fd85c421fb5feeb90a1
Daniel Lobato <elobatocs@gmail.com>
2016-07-04 16:00:14 +0200

    Fixes #15527 - api/v2/hosts is slow loading permissions
    The .eager_load call when called with [] generates a SELECT query that
    selects all host attributes. This can cause it to take a lot longer
    when there are large number of hosts involved. These attributes are
    irrelevant in this context (when they are relevant, they're put in the
    scope_components hash)
    
    With 5700 hosts, it takes about 50% less time to run this than to run
    it with eager_load.
    
    Chaining the call with .where makes the call near-instant.
    It selects the host from the list of hosts we can see using the db
    directly.

---
+++ app/services/authorizer_cache.rb
@@ -0,0 +1,14 @@
+module AuthorizerCache
+  def initialize_cache
+    @cache = HashWithIndifferentAccess.new do |h, k|
+      h[k] = HashWithIndifferentAccess.new
+    end
+  end
+
+  def collection_cache_lookup(subject, permission)
+    collection = @cache[subject.class.to_s][permission] ||=
+      find_collection(subject.class, :permission => permission)
+
+    collection.include?(subject)
+  end
+end
