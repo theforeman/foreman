bbb2ea1373f69e53f56b8d2532f525e0ba12d5c7
Michael Moll <kvedulv@kvedulv.de>
2016-07-16 01:50:57 +0200

    fixes #15709 - fix SuSE mediapath in PXELinux templates

--- test/unit/orchestration/tftp_test.rb
+++ test/unit/orchestration/tftp_test.rb
@@ -35,22 +35,39 @@ class TFTPOrchestrationTest < ActiveSupport::TestCase
                             :architecture => architectures(:x86_64))
       Setting[:unattended_url] = "http://ahost.com:3000"
 
-      template = h.send(:generate_pxe_template).split("~")
-      expected = File.open(Pathname.new(__FILE__).parent + "pxe_template").readlines.map(&:strip)
-      assert_equal template,expected
+      template = h.send(:generate_pxe_template).to_s.gsub! '~', "\n"
+      expected = <<-EXPECTED
+default linux
+label linux
+kernel boot/Redhat-6.1-x86_64-vmlinuz
+append initrd=boot/Redhat-6.1-x86_64-initrd.img ks=http://ahost.com:3000/unattended/kickstart ksdevice=bootif network kssendmac
+EXPECTED
+      assert_equal template,expected.strip
       assert h.build
     end
   end
 
   def test_generate_pxe_template_for_localboot
     if unattended?
-      h = FactoryGirl.create(:host)
+      h = FactoryGirl.create(:host, :managed)
       as_admin { h.update_attribute :operatingsystem, operatingsystems(:centos5_3) }
       assert !h.build
 
-      template = h.send(:generate_pxe_template).split("~")
-      expected = File.open(Pathname.new(__FILE__).parent + "pxe_local_template").readlines.map(&:strip)
-      assert_equal template,expected
+      template = h.send(:generate_pxe_template).to_s.gsub! '~', "\n"
+      expected = <<-EXPECTED
+DEFAULT menu
+PROMPT 0
+MENU TITLE PXE Menu
+TIMEOUT 200
+TOTALTIMEOUT 6000
+ONTIMEOUT local
+
+LABEL local
+MENU LABEL (local)
+MENU DEFAULT
+LOCALBOOT 0
+EXPECTED
+      assert_equal template,expected.strip
     end
   end
 
@@ -79,5 +96,24 @@ class TFTPOrchestrationTest < ActiveSupport::TestCase
     nic = FactoryGirl.build(:nic_managed)
     nic.expects(:setTFTP).never
     assert nic.rebuild_tftp
+  end
+
+  def test_generate_pxe_template_for_suse_build
+    if unattended?
+      h = FactoryGirl.build(:host, :managed, :build => true,
+                            :operatingsystem => operatingsystems(:opensuse),
+                            :architecture => architectures(:x86_64))
+      Setting[:unattended_url] = "http://ahost.com:3000"
+
+      template = h.send(:generate_pxe_template).to_s.gsub! '~', "\n"
+      expected = <<-EXPECTED
+DEFAULT linux
+LABEL linux
+KERNEL boot/OpenSuse-12.3-x86_64-linux
+APPEND initrd=boot/OpenSuse-12.3-x86_64-initrd ramdisk_size=65536 install=http://download.opensuse.org/distribution/12.3/repo/oss autoyast=http://ahost.com:3000/unattended/provision textmode=1
+EXPECTED
+      assert_equal template,expected.strip
+      assert h.build
+    end
   end
 end
