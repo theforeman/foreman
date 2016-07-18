f35e0e3d69c150148d85af20a9bc6fe40fdf30c6
Dominic Cleal <dominic@cleal.org>
2016-07-18 15:45:34 +0100

    Fixes #21097 - Update to Rails 5 configuration options

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,8 +18,8 @@ Foreman::Application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files   = true
-  config.static_cache_control = 'public, max-age=3600'
+  config.public_file_server.enabled = true
+  config.public_file_server.headers = {'Cache-Control' => 'public, max-age=3600'}
 
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
