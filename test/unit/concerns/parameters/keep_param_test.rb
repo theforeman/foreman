3fd795cbb281877ac1a008db9537b226739e20d7
Dominic Cleal <dominic@cleal.org>
2016-07-20 14:34:33 +0100

    fixes #18568 - replace deprecated AC::Parameters#update
    The #update method in Rails 5 is returning a HWIA rather than the
    ActionController::Parameters instance, causing keep_param to return the
    wrong object type. It is also deprecated in 5.0, so replace it with a
    simpler and supported assignment through #[]=.

--- test/unit/concerns/parameters/keep_param_test.rb
+++ test/unit/concerns/parameters/keep_param_test.rb
@@ -5,27 +5,28 @@ class KeepParamParametersTest < ActiveSupport::TestCase
 
   test "retains parameter within top-level hash" do
     params = ActionController::Parameters.new(:user => {:login => 'foo'})
-    returned = keep_param(params, 'user', :login) { {} }
+    returned = keep_param(params, 'user', :login) { params.permit(:another) }
+    assert_kind_of ActionController::Parameters, returned
     assert_equal 'foo', returned[:login]
   end
 
   test "retains multiple parameters" do
     params = ActionController::Parameters.new(:user => {:login => 'foo', :other => 'bar'})
-    returned = keep_param(params, 'user', :login, :other) { {} }
+    returned = keep_param(params, 'user', :login, :other) { params.permit(:another) }
     assert_equal 'foo', returned[:login]
     assert_equal 'bar', returned[:other]
   end
 
   test "ignores unknown parameters" do
     params = ActionController::Parameters.new(:user => {:login => 'foo'})
-    returned = keep_param(params, 'user', :login, :other) { {} }
+    returned = keep_param(params, 'user', :login, :other) { params.permit(:another) }
     assert_equal 'foo', returned[:login]
     refute returned.has_key?(:other)
   end
 
   test "doesn't modify input hash" do
     params = ActionController::Parameters.new(:user => {:login => 'foo'})
-    returned = keep_param(params, 'user', :login) { {:login => 'foo'} }
+    returned = keep_param(params, 'user', :login) { params.permit(:another) }
     assert_equal 'foo', returned[:login]
     assert_equal 'foo', params[:user][:login]
   end
