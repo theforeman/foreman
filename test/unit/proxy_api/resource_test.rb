fc4de75c5da340df426f8a0ff279737eff1103e6
Dominic Cleal <dominic@cleal.org>
2016-07-28 09:47:57 +0100

    fixes #10982 - configure RestClient log to 'proxy' logger
    Request/response information is sent to the proxy logger, showing the
    URLs called, headers, request body etc.
    
    The X-Request-ID header is now also set in the proxy requests to a
    random ID stored in the session, to prevent the user's real session ID
    being hijacked from the proxy request data. The ID should be correlated
    against the proxy logger.

---
+++ test/unit/proxy_api/resource_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class ProxyApiResourceTest < ActiveSupport::TestCase
+  setup do
+    ProxyAPI::Resource.any_instance.stubs(:url).returns('http://proxy.example.com')
+  end
+
+  test "connect_params includes x_request_id header" do
+    assert ProxyAPI::Resource.new({}).send(:connect_params)[:headers][:x_request_id].present?
+  end
+
+  test "connect_params sets x_request_id to logger safe session ID" do
+    begin
+      ::Logging.mdc['session_safe'] = 'test'
+      assert_equal 'test', ProxyAPI::Resource.new({}).send(:connect_params)[:headers][:x_request_id]
+    ensure
+      ::Logging.mdc.delete('session_safe')
+    end
+  end
+
+  test "with_logger sets RestClient.log" do
+    refute RestClient.log
+    ProxyAPI::Resource.new({}).send(:with_logger) do
+      assert_respond_to RestClient.log, :<<
+    end
+    refute RestClient.log
+  end
+
+  test "RestClientLogger#<< logs messages" do
+    logger = mock('logger')
+    logger.expects(:debug).with('test')
+    ProxyAPI::RestClientLogger.new(logger) << "test\n"
+  end
+end
