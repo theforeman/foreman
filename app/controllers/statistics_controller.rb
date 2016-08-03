4202625d3c2bfc4cf90f792c4246029743163c45
Ohad Levy <ohadlevy@gmail.com>
2016-08-03 22:13:31 +0300

    fixes #15919 - statistics page is loaded via AJAX
    Also:
    - refactor how statistics data is being called, now it should be trivial
    to add additional charts or consume other data from it. It should make
    it much easier to reuse statistics charts in other places
    (e.g. dashboard) or unify how fact charts are done.
    - remove unused JSON response from the statistics controller.

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,23 +1,22 @@
 class StatisticsController < ApplicationController
+  before_action :find_stat, :only => [:show]
+
   def index
-    @os_count    = Host.authorized(:view_hosts, Host).count_distribution :operatingsystem
-    @arch_count  = Host.authorized(:view_hosts, Host).count_distribution :architecture
-    @env_count   = Host.authorized(:view_hosts, Host).count_distribution :environment
-    @klass_count = Puppetclass.authorized(:view_puppetclasses).map{|pc| {:label => pc.to_label, :data => pc.hosts_count}}.select{|pc| pc[:data] > 0}
-    @cpu_count   = FactValue.authorized(:view_facts).my_facts.count_each "processorcount", :unit => Nn_('%s core', '%s cores')
-    @model_count = FactValue.authorized(:view_facts).my_facts.count_each "manufacturer"
-    @mem_size    = FactValue.authorized(:view_facts).my_facts.mem_average "memorysize"
-    @mem_free    = FactValue.authorized(:view_facts).my_facts.mem_average "memoryfree"
-    @swap_size   = FactValue.authorized(:view_facts).my_facts.mem_average "swapsize"
-    @swap_free   = FactValue.authorized(:view_facts).my_facts.mem_average "swapfree"
-    respond_to do |format|
-      format.html
-      format.json do
-        render :json => { :statistics => { :os_count    => @os_count, :arch_count => @arch_count,
-                                           :env_count   => @env_count, :klass_count => @klass_count, :cpu_count => @cpu_count,
-                                           :model_count => @model_count, :mem_size => @mem_size, :mem_free => @mem_free, :swap_size => @swap_size,
-                                           :swap_free   => @swap_free, :mem_totsize => @mem_totsize, :mem_totfree => @mem_totfree } }
-      end
-    end
+    @stats = charts
+  end
+
+  def show
+    render :show, :layout => false
+  end
+
+  private
+
+  def find_stat
+    @stat = charts.detect{|ch| ch.id.to_s == params[:id]}
+    @stat || not_found
+  end
+
+  def charts
+    ::Statistics.charts
   end
 end
--- test/integration/statistic_test.rb
+++ test/integration/statistic_test.rb
@@ -5,7 +5,7 @@ class StatisticIntegrationTest < ActionDispatch::IntegrationTest
     visit statistics_path
     assert page.has_selector?('h4', :text => "OS Distribution")
     assert page.has_selector?('h4', :text => "Architecture Distribution")
-    assert page.has_selector?('h4', :text => "Environments Distribution")
+    assert page.has_selector?('h4', :text => "Environment Distribution")
     assert page.has_selector?('h4', :text => "Number of CPUs")
     assert page.has_selector?('h4', :text => "Hardware")
     assert page.has_selector?('h4', :text => "Class Distribution")
