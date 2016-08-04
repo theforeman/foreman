812578aebba3b32e49d937fc5a0e0e03c5de6b29
Timo Goebel <timo.goebel@dm.de>
2016-08-04 11:02:53 +0200

    fixes #15977 - change enc output in a compatible way
    (cherry picked from commit b96e021026cdcc6918c61d246dbf5c7baabfc1b4)

--- app/models/host/managed.rb
+++ app/models/host/managed.rb
@@ -383,10 +383,12 @@ class Host::Managed < Host::Base
     param["realm"]        = realm.name unless realm.nil?
     param["hostgroup"]    = hostgroup.to_label unless hostgroup.nil?
     if SETTINGS[:locations_enabled]
-      param["location"] = location.title unless location.blank?
+      param["location"] = location.name unless location.blank?
+      param["location_title"] = location.title unless location.blank?
     end
     if SETTINGS[:organizations_enabled]
-      param["organization"] = organization.title unless organization.blank?
+      param["organization"] = organization.name unless organization.blank?
+      param["organization_title"] = organization.title unless organization.blank?
     end
     if SETTINGS[:unattended]
       param["root_pw"]      = root_pass unless (!operatingsystem.nil? && operatingsystem.password_hash == 'Base64')
