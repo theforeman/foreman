74a7418294a0a4e7931e95f32b0b39daf952eca8
Martin Bačovský <martin.bacovsky@gmail.com>
2016-08-04 07:22:10 -0400

    Fixes #12473 - Add admin flag to usergroup param description

--- app/controllers/api/v2/usergroups_controller.rb
+++ app/controllers/api/v2/usergroups_controller.rb
@@ -22,6 +22,7 @@ module Api
       def_param_group :usergroup do
         param :usergroup, Hash, :required => true, :action_aware => true do
           param :name, String, :required => true
+          param :admin, :bool, :required => false, :desc => N_("is an admin user group")
           param :user_ids, Array, :require => false
           param :usergroup_ids, Array, :require => false
           param :role_ids, Array, :require => false
