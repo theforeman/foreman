7a6d4dd0c154a15adbff21cb1e845b5bc45c230b
amirfefer <amirfefer@gmail.com>
2016-08-12 18:24:51 +0300

    Fixes #16075 - add authenticity token in setting params

--- app/assets/javascripts/settings.js
+++ app/assets/javascripts/settings.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
   $('.editable').editable({
+    params: {
+      authenticity_token: AUTH_TOKEN
+    },
     error: function(response) {
       return $.parseJSON(response.responseText).errors;
     }
