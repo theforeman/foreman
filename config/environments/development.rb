7c85192e56d36377f0ed9ba7b1c34ddac6545d94
Marek Hulan <mhulan@redhat.com>
2016-08-16 13:01:07 +0200

    Refs #12364 - webpack server can serve assets on https

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,5 @@ Foreman::Application.configure do
 
   #Allow disabling the webpack dev server from the settings
   config.webpack.dev_server.enabled = SETTINGS.fetch(:webpack_dev_server, true)
+  config.webpack.dev_server.https = SETTINGS.fetch(:webpack_dev_server_https, false)
 end
