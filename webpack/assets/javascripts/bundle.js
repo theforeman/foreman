ff51a9f3cce9626481cecc4634e80221884b93cb
Tomer Brisker <tbrisker@gmail.com>
2016-08-16 21:43:20 +0300

    Fixes #16120 - Allow including css in webpack assets
    This commit demonstrates using the css from the select2 module using the
    webpack css loaders. Images under 32k will be compiled into the css to
    reduce request counts.

--- webpack/assets/javascripts/bundle.js
+++ webpack/assets/javascripts/bundle.js
@@ -1,10 +1,10 @@
 require('expose?$!expose?jQuery!jquery');
 require('jquery-ujs');
 require('expose?_!lodash');
-require('select2');
 require('expose?jstz!jstz');
 require('jquery.cookie');
 require('./bundle_flot');
+require('./bundle_select2');
 window.tfm = {
   tools: require('./foreman_tools')
 }
