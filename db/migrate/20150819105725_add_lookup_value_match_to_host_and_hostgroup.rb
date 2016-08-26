4a3cb08b2847e27dd2256c24521834bd44e50296
Ori Rabin <orabin@redhat.com>
2016-08-26 14:41:13 +0300

    Fixes #14468 - fix db:migrate when unattended false on empty db

--- db/migrate/20150819105725_add_lookup_value_match_to_host_and_hostgroup.rb
+++ db/migrate/20150819105725_add_lookup_value_match_to_host_and_hostgroup.rb
@@ -8,17 +8,20 @@ class AddLookupValueMatchToHostAndHostgroup < ActiveRecord::Migration
     matchers = LookupValue.where("lookup_values.match like ?", "fqdn=%").uniq.pluck('lookup_values.match')
     fqdns = matchers.map{|m| m.gsub(/^fqdn=/, "")}
     hostnames = fqdns.map{|fqdn| fqdn.split(".").first}
+
     if SETTINGS[:unattended]
       hosts = Host::Managed.where(:nics => {:name => (fqdns + hostnames)}).joins(:primary_interface)
     else
-      fact_name_id = FactName.where(:name => "fqdn").first.id
-      hosts = Host::Managed.joins(:fact_values).where(:fact_values => {:fact_name_id => fact_name_id, :value => (fqdns + hostnames)})
+      fact_name_id = FactName.where(:name => "fqdn").first.try(:id)
+      hosts = Host::Managed.joins(:fact_values).where(:fact_values => {:fact_name_id => fact_name_id, :value => (fqdns + hostnames)}) if fact_name_id.present?
     end
 
-    hosts = hosts.readonly(false).uniq
+    if hosts.present?
+      hosts = hosts.readonly(false).uniq
 
-    hosts.each do |host|
-      host.update_attribute(:lookup_value_matcher, host.send(:lookup_value_match))
+      hosts.each do |host|
+        host.update_attribute(:lookup_value_matcher, host.send(:lookup_value_match))
+      end
     end
 
     Hostgroup.find_in_batches(:batch_size => 100) do |group|
