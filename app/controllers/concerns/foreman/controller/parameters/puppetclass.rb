9edd8859872805c4228d5b0a0dd6fce2ab694e99
Dominic Cleal <dominic@cleal.org>
2016-08-30 10:04:03 +0100

    fixes #16376 - use correct params filter for nested Puppet LKs

--- app/controllers/concerns/foreman/controller/parameters/puppetclass.rb
+++ app/controllers/concerns/foreman/controller/parameters/puppetclass.rb
@@ -7,7 +7,7 @@ module Foreman::Controller::Parameters::Puppetclass
     def puppetclass_params_filter
       Foreman::ParameterFilter.new(::Puppetclass).tap do |filter|
         filter.permit :name,
-          :class_params_attributes => [variable_lookup_key_params_filter],
+          :class_params_attributes => [puppetclass_lookup_key_params_filter],
           :hostgroup_ids => [], :hostgroup_names => [],
           :lookup_keys_attributes => [variable_lookup_key_params_filter],
           :smart_class_parameters => [puppetclass_lookup_key_params_filter],
