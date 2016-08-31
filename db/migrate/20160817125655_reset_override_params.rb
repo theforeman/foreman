d7f8cc79691f69cd4bddc818565cf70925162949
Ori Rabin <orabin@redhat.com>
2016-08-31 17:18:57 +0300

    Refs #15814 - Reset override params only for PuppetclassLookupKey

--- db/migrate/20160817125655_reset_override_params.rb
+++ db/migrate/20160817125655_reset_override_params.rb
@@ -1,9 +1,6 @@
 class ResetOverrideParams < ActiveRecord::Migration
-  class LookupKey < ::ActiveRecord::Base
-  end
-
   def up
-    LookupKey.where(override: false).update_all(
+    PuppetclassLookupKey.where(override: false).update_all(
       :merge_overrides => false,
       :avoid_duplicates => false,
       :merge_default => false
