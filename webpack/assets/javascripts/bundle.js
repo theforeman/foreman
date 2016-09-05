80963357e57ddfb6969b9de7e5ca4636e36b4182
Marek Hulan <mhulan@redhat.com>
2016-09-05 16:08:09 +0200

    Fixes #10395 - fine grain for CPU selectors

--- webpack/assets/javascripts/bundle.js
+++ webpack/assets/javascripts/bundle.js
@@ -10,5 +10,5 @@ require('./bundle_select2');
 require('./bundle_datatables');
 window.tfm = {
   tools: require('./foreman_tools'),
-  initByteSpinner: require('./jquery.ui.custom_spinners').initByteSpinner
+  numFields: require('./jquery.ui.custom_spinners')
 };
