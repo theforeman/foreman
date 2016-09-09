a467444a7c1bf2123a5d259869331c6a9a74bec5
Daniel Lobato <elobatocs@gmail.com>
2016-09-09 10:41:41 +0200

    Fixes #13748 - Add testing tools for code under webpack
    This commit adds jest as a tool to be used to test some of the code
    under webpack, supporting ES2015 via babel-polyfill.
    
    The purpose of this is just to give the tools for other developers
    to test their javascript code.
    
    By default, jest is automocking everything except for jQuery with this
    change

---
+++ webpack/assets/javascripts/__tests__/foreman_tools.test.js
@@ -0,0 +1,35 @@
+jest.unmock('../foreman_tools');
+const tools = require('../foreman_tools');
+
+describe('iconText', () => {
+  it('creates a label with the right icon class', () => {
+    expect(tools.iconText('plus', '', 'patternfly'))
+    .toBe('<span class="patternfly patternfly-plus"/>');
+  });
+
+  it('adds a bold text next to the label', () => {
+    expect(tools.iconText('plus', 'foo', 'patternfly'))
+    .toBe('<span class="patternfly patternfly-plus"/><strong>foo</strong>');
+  });
+});
+
+describe('activateDatatables', () => {
+  it('calls $.fn.DataTable when it finds a data-table=server', () => {
+    const $ = require('jquery');
+
+    // Used for rendering lists of VMs under compute resources
+    document.body.innerHTML =
+      '<div data-table=server data-source=http://example.foo>' +
+      'To be filled by a table' +
+        '</div>';
+    $.fn.DataTable = jest.fn();
+    tools.activateDatatables();
+    expect($.fn.DataTable).toBeCalledWith({
+      processing: true,
+      serverSide: true,
+      ordering: false,
+      ajax: $('[data-table=server]').data('source'),
+      dom: "<'row'<'col-md-6'f>r>t<'row'<'col-md-6'><'col-md-6'p>>"
+    });
+  });
+});
