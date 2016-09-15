20bae4410d1c767d5a38ec84d1b76b649f4671c4
Tomer Brisker <tbrisker@gmail.com>
2016-09-15 11:25:09 +0300

    Fixes #16564 - Disable FrozenStringLiteralComment cop
    Also fixes a failng cop in config/initializers/secure_headers.rb

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -1,11 +1,11 @@
 ::SecureHeaders::Configuration.default do |config|
   config.hsts = "max-age=#{20.years.to_i}; includeSubdomains"
   config.csp = {
-    :default_src => %w('self'),
-    :child_src   => %w('self'),
-    :connect_src => %w('self' ws: wss:),
-    :style_src   => %w('unsafe-inline' 'self'),
-    :script_src  => %w('unsafe-eval' 'unsafe-inline' 'self'),
-    :img_src     => %w('self' *.gravatar.com)
+    :default_src => ["'self'"],
+    :child_src   => ["'self'"],
+    :connect_src => ["'self'", 'ws:', 'wss:'],
+    :style_src   => ["'unsafe-inline'", "'self'"],
+    :script_src  => ["'unsafe-eval'", "'unsafe-inline'", "'self'"],
+    :img_src     => ["'self'", '*.gravatar.com']
   }
 end
