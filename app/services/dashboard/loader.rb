abb87b93a1778d6f326331876ae5e082e3dd60c0
Bryan Kearney <bkearney@redhat.com>
2016-09-23 17:28:53 -0400

    Fixes #10930 - Make widget names and titles consistent

--- app/services/dashboard/loader.rb
+++ app/services/dashboard/loader.rb
@@ -1,10 +1,10 @@
 module Dashboard
   module Loader
     # Default widgets that are displayed on the dashboard
-    DEFAULT_WIDGETS = [ {:template=>'status_widget',       :sizex=>8,:sizey=>1,:name=> N_('Status table')},
-                        {:template=>'status_chart_widget', :sizex=>4,:sizey=>1,:name=> N_('Status chart')},
-                        {:template=>'reports_widget',      :sizex=>6,:sizey=>1,:name=> N_('Report summary')},
-                        {:template=>'distribution_widget', :sizex=>6,:sizey=>1,:name=> N_('Distribution chart')}]
+    DEFAULT_WIDGETS = [ {:template=>'status_widget',       :sizex=>8,:sizey=>1,:name=> N_('Host Configuration Status')},
+                        {:template=>'status_chart_widget', :sizex=>4,:sizey=>1,:name=> N_('Host Configuration Chart')},
+                        {:template=>'reports_widget',      :sizex=>6,:sizey=>1,:name=> N_('Latest Events')},
+                        {:template=>'distribution_widget', :sizex=>6,:sizey=>1,:name=> N_('Run Distribution Chart')}]
     # Widget templates that are allowed on dashboard. Default widgets automatically allow their templates.
     ALLOWED_TEMPLATES = []
 
