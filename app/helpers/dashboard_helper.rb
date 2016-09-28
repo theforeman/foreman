1d551e514de6b34dcba7ce6e3e8c0bb171e55b6b
Tomer Brisker <tbrisker@gmail.com>
2016-09-28 18:14:01 +0300

    Fixes #16723 - Lazy load data for dashboard widgets

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -73,7 +73,7 @@ module DashboardHelper
       content_tag(:span, raw('&nbsp;'), :class=>'label', :style => "background-color:" + report_color[counter]) +
       raw('&nbsp;')+
       link_to(name, hosts_path(:search => search),:class=>"dashboard-links") +
-      content_tag(:h4,@report[counter])
+      content_tag(:h4, @data.report[counter])
     end
   end
 
