869540d65383bce41702c4facf4b871564f18594
Timo Goebel <mail@timogoebel.name>
2016-10-24 17:21:57 +0200

    fixes #17007 - normalize dns records before comparison

--- lib/net/dns/aaaa_record.rb
+++ lib/net/dns/aaaa_record.rb
@@ -1,9 +1,10 @@
 module Net
   module DNS
     class AAAARecord < DNS::ForwardRecord
-      def initialize(opts = { })
-        super opts
-        self.ip = Validations.validate_ip6! self.ip
+      def initialize(opts = {})
+        super(opts)
+        self.ip = Validations.normalize_ip6(self.ip)
+        Validations.validate_ip6!(self.ip)
         self.ipfamily = Socket::AF_INET6
         @type = "AAAA"
       end
