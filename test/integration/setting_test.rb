ba65983be18a0b82b3fe50a8fe5a31504116753f
Ondrej Prazak <oprazak@redhat.com>
2016-10-27 15:01:32 +0200

    Fixes #17120 - Allow custom name for setting category

--- test/integration/setting_test.rb
+++ test/integration/setting_test.rb
@@ -8,4 +8,17 @@ class SettingIntegrationTest < ActionDispatch::IntegrationTest
     assert page.has_link?("Provisioning", :href => "#Provisioning")
     assert page.has_link?("Auth", :href => "#Auth")
   end
+
+  test "humanized tab label for setting category" do
+    class Setting::Test < Setting
+      def self.humanized_category
+        "My Pretty Setting Label"
+      end
+    end
+
+    FactoryGirl.create(:setting, :settings_type => "boolean", :category => "Setting::Test", :name => 'test_setting', :default => false)
+
+    assert_index_page(settings_path,"Settings",false,true,false)
+    assert page.has_link?("My Pretty Setting Label", :href => "#Test")
+  end
 end
