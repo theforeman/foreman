93046b80ad54cfaecf37d14f701dda8613265b1a
Dominic Cleal <dominic@cleal.org>
2016-11-03 16:22:00 +0000

    fixes #17170 - use strong params in InterfacesController

---
+++ test/controllers/interfaces_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class InterfacesControllerTest < ActionController::TestCase
+  ['managed', 'bmc', 'bond', 'bridge'].each do |type|
+    test "#new with #{type} interface attributes should render form" do
+      nic = FactoryGirl.build(:"nic_#{type}")
+      xhr :get, :new, { :host => { :interfaces_attributes => { "0" => nic.attributes } } }, set_session_user
+      assert_response :success
+      assert_not_nil assigns(:interface)
+      assert_template 'nic/new'
+      assert_template "nic/#{type}s/_#{type}"
+    end
+  end
+end
