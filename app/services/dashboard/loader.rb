74b73527cc70f2e010896ca54050936aa8772924
Timo Goebel <mail@timogoebel.name>
2016-11-21 09:08:39 +0100

    fixes #17407 - add a new hosts dashboard widget
    (cherry picked from commit b907bcc44e547a1f32a76c7f680270922a4ffe40)

--- app/services/dashboard/loader.rb
+++ app/services/dashboard/loader.rb
@@ -4,7 +4,8 @@ module Dashboard
     DEFAULT_WIDGETS = [ {:template=>'status_widget',       :sizex=>8,:sizey=>1,:name=> N_('Host Configuration Status')},
                         {:template=>'status_chart_widget', :sizex=>4,:sizey=>1,:name=> N_('Host Configuration Chart')},
                         {:template=>'reports_widget',      :sizex=>6,:sizey=>1,:name=> N_('Latest Events')},
-                        {:template=>'distribution_widget', :sizex=>6,:sizey=>1,:name=> N_('Run Distribution Chart')}]
+                        {:template=>'distribution_widget', :sizex=>6,:sizey=>1,:name=> N_('Run Distribution Chart')},
+                        {:template=>'new_hosts_widget',    :sizex=>8,:sizey=>1,:name=> N_('New Hosts')}]
     # Widget templates that are allowed on dashboard. Default widgets automatically allow their templates.
     ALLOWED_TEMPLATES = []
 
