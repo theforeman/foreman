8d55252ef182cfa1b2023189d6abd459f3ea5803
Ivan Nečas <inecas@redhat.com>
2016-12-01 22:46:55 +0100

    Refs #15779 - move the foreman-tasks to own bundle group
    This makes it easier to achieve a minimal setup, that might be needed for
    things like plugins building etc.

--- bundler.d/foreman_tasks.rb
+++ bundler.d/foreman_tasks.rb
@@ -1,1 +1,3 @@
-gem 'foreman-tasks', '>= 0.8.5'
+group :foreman_tasks do
+  gem 'foreman-tasks', '>= 0.8.5'
+end
--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,11 @@ else
         end
       end
     end
+    begin
+      Bundler.require(:foreman_tasks)
+    rescue LoadError => e
+      warn "Could not load foreman tasks, async processing will not be available: #{e.message}"
+    end
   end
 end
 
