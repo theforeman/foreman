0ae52c29235a15050b4d5ac2bb0ae82d5293ac07
Jan Vansteenkiste <jan@vstone.eu>
2017-01-06 11:13:42 +0100

    Fixes #17951 - Better email validation
    Fix email validation until this is replaced by using the mail gem
    as is implemented in 1.14 and adds additinal tests for invalid
    email addresses.
    
    Validation is more relaxed than it should be but does not block valid
    addresses.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,6 +1,6 @@
 class EmailValidator < ActiveModel::EachValidator
   EMAIL_REGEXP = /\A(([\w!#\$%&\'\*\+\-\/=\?\^`\{\|\}~]+((\.\"[\w!#\$%&\'\*\+\-\/=\?\^`\{\|\}~\"\(\),:;<>@\[\\\] ]+(\.[\w!#\$%&\'\*\+\-\/=\?\^`\{\|\}~\"\(\),:;<>@\[\\\] ]+)*\")*\.[\w!#\$%&\'\*\+\-\/=\?\^`\{\|\}~]+)*)|(\"[\w !#\$%&\'\*\+\-\/=\?\^`\{\|\}~\"\(\),:;<>@\[\\\] ]+(\.[\w !#\$%&\'\*\+\-\/=\?\^`\{\|\}~\"\(\),:;<>@\[\\\] ]+)*\"))
-             @[a-z0-9]+((\.[a-z0-9]+)*|(\-[a-z0-9]+)*)*\z/ix
+             @[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})*\z/ix
   def validate_each(record, attribute, value)
     return if options[:allow_blank] && value.empty?
     record.errors.add(attribute, _("is too long (maximum is 254 characters)")) if value && value.length > 254
