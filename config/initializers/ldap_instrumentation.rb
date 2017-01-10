09a9a694ce6a4e6f76a2169d807dd018b73758f4
Daniel Lobato Garcia <me@daniellobato.me>
2017-01-10 09:17:03 +0000

    Fixes #17998 - Only print LDAP bind status if available
    The ldap logger tries to call payload[:bind].status but it might not be
    defined if the bind goes wrong in some LDAP sources.
    
    This causes a 500 instead of logging the status of the bind. We should
    only display payload[:bind].status if it's available.

--- config/initializers/ldap_instrumentation.rb
+++ config/initializers/ldap_instrumentation.rb
@@ -15,7 +15,13 @@ module Foreman
   end
 
   class NetLdapSubscriber < LdapSubscriber
-    define_log(:bind, 'op bind', YELLOW) { |payload| "result=#{payload[:bind].status}" }
+    define_log(:bind, 'op bind', YELLOW) do |payload|
+      if payload[:bind].try(:status)
+        "result=#{payload[:bind].status}"
+      else
+        "result=#{payload[:bind]}"
+      end
+    end
     define_log(:search, 'op search', YELLOW) { |payload| "filter=#{payload[:filter]}, base=#{payload[:base]}" }
   end
 
