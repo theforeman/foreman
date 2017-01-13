bec68fe2c04a2b6a714b53cfdcdc7abd00852a52
Timo Goebel <mail@timogoebel.name>
2017-01-13 16:27:05 +0100

    fixes #13810 - increase value range for id columns

---
+++ db/migrate/20170110113824_change_id_value_range.rb
@@ -0,0 +1,16 @@
+class ChangeIdValueRange < ActiveRecord::Migration
+  def self.up
+    if ['mysql', 'mysql2', 'postgresql'].include? ActiveRecord::Base.connection.instance_values['config'][:adapter]
+      change_column :logs, :id, :bigint
+      change_column :reports, :id, :bigint
+      change_column :fact_values, :id, :bigint
+    end
+  end
+  def self.down
+    if ['mysql', 'mysql2', 'postgresql'].include? ActiveRecord::Base.connection.instance_values['config'][:adapter]
+      change_column :logs, :id, :int
+      change_column :reports, :id, :int
+      change_column :fact_values, :id, :int
+    end
+  end
+end
