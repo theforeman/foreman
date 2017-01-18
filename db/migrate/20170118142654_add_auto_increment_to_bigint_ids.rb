0f38d367f09867411b61eee0addf3f1d5711357f
Timo Goebel <mail@timogoebel.name>
2017-01-18 15:40:01 +0100

    fixes #18130 - add autoincrement for bigint id columns

---
+++ db/migrate/20170118142654_add_auto_increment_to_bigint_ids.rb
@@ -0,0 +1,16 @@
+class AddAutoIncrementToBigintIds < ActiveRecord::Migration
+  def self.up
+    if ['mysql', 'mysql2'].include? ActiveRecord::Base.connection.instance_values['config'][:adapter]
+      change_column :logs, :id, 'SERIAL'
+      change_column :reports, :id, 'SERIAL'
+      change_column :fact_values, :id, 'SERIAL'
+    end
+  end
+  def self.down
+    if ['mysql', 'mysql2'].include? ActiveRecord::Base.connection.instance_values['config'][:adapter]
+      change_column :logs, :id, :bigint
+      change_column :reports, :id, :bigint
+      change_column :fact_values, :id, :bigint
+    end
+  end
+end
