c33ab7eef5cafacc2a28ea9bd69dee263a7eb868
Gail Steiger <gsteiger@netvision.net.il>
2017-02-01 11:50:03 +0200

    Fixes #18194, #18326 - minor UI bugs Notifications
    persist open group on navigation
    remove expand toggle indicator

--- webpack/assets/javascripts/react_app/components/notifications/NotificationDrawerTitle.js
+++ webpack/assets/javascripts/react_app/components/notifications/NotificationDrawerTitle.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const NotificationDrawerTitle = ({text}) => (
   <div className="drawer-pf-title">
-    <a className="drawer-pf-toggle-expand"></a>
     <h3 className="text-center">{text}</h3>
   </div>
 );
