87bf790b8b3c767ef9e1fff8d5aa57c5b1a2c5b2
Shlomi Zadok <shlomi@ben-hanna.com>
2017-02-20 17:58:07 +0200

    Fixes #18560 - Remove searchbar from 403, change 403 style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -176,7 +176,7 @@ module ApplicationHelper
   end
 
   def searchable?
-    return false if (SETTINGS[:login] && !User.current) || @welcome
+    return false if (SETTINGS[:login] && !User.current) || @welcome || @missing_permissions
     if (controller.action_name == "index") || (defined?(SEARCHABLE_ACTIONS) && (SEARCHABLE_ACTIONS.include?(controller.action_name)))
       controller.respond_to?(:auto_complete_search)
     end
