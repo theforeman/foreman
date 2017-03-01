790b0e29b685b1f110e6579e3c28558ba49d7c14
Dominic Cleal <dominic@cleal.org>
2017-03-01 16:05:27 +0000

    Pin patternfly-sass due to missing precompiled asset

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -1,7 +1,7 @@
 gem 'jquery-turbolinks', '~> 2.1'
 gem 'jquery-ui-rails', '< 5.0.0'
 gem 'autoprefixer-rails', '~> 5.2'
-gem 'patternfly-sass', '~> 3.9'
+gem 'patternfly-sass', '~> 3.15.0'
 gem 'gridster-rails', '~> 0.5'
 gem 'gettext_i18n_rails_js', '~> 1.0'
 gem 'execjs', '>= 1.4.0', '< 3.0'
