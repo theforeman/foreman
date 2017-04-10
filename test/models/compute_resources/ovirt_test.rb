theforeman/foreman

commit 4269abbd3c1fba6deb50b4b8ede2fecbbed11ddd

Fixes #10248 - cloning volume information for virtual machines

app/models/compute_resources/foreman/model/ovirt.rb
@@ -17,6 +17,12 @@ module Foreman::Model
       [:build, :image]
     end
 
+    def find_vm_by_uuid(uuid)
+      super
+    rescue OVIRT::OvirtException
+      raise(ActiveRecord::RecordNotFound)
+    end
+
     def supports_update?
       true
     end

test/unit/compute_resources/ovirt_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
+require 'unit/compute_resources/compute_resource_test_helpers'
 
 class OvirtTest < ActiveSupport::TestCase
+  include ComputeResourceTestHelpers
+
   test "#associated_host matches any NIC" do
     host = FactoryGirl.create(:host, :mac => 'ca:d0:e6:32:16:97')
     cr = FactoryGirl.build(:ovirt_cr)
@@ -8,5 +11,17 @@ class OvirtTest < ActiveSupport::TestCase
     iface2 = mock('iface2', :mac => 'ca:d0:e6:32:16:97')
     vm = mock('vm', :interfaces => [iface1, iface2])
     assert_equal host, as_admin { cr.associated_host(vm) }
+  end
+
+  describe "find_vm_by_uuid" do
+    it "raises RecordNotFound when the vm does not exist" do
+      cr = mock_cr_servers(Foreman::Model::Ovirt.new, empty_servers)
+      assert_find_by_uuid_raises(ActiveRecord::RecordNotFound, cr)
+    end
+
+    it "raises RecordNotFound when the compute raises retrieve error" do
+      cr = mock_cr_servers(Foreman::Model::Ovirt.new, servers_raising_exception(OVIRT::OvirtException.new('VM not found')))
+      assert_find_by_uuid_raises(ActiveRecord::RecordNotFound, cr)
+    end
   end
 end

app/assets/javascripts/compute_resources/ovirt/nic_info.js
@@ -1,3 +1,3 @@
 providerSpecificNICInfo = function(form) {
-  return form.find('.ovirt_name').val() + ' @ ' + form.find('.ovirt_network').val();
+  return form.find('.ovirt_name').val() + ' @ ' + form.find('.ovirt_network').text();
 }

app/models/compute_resource.rb
@@ -244,7 +244,17 @@ class ComputeResource < ActiveRecord::Base
 
   def vm_compute_attributes_for(uuid)
     vm = find_vm_by_uuid(uuid)
-    vm.attributes.reject{|k,v| k == :id }
+    vm_attrs = vm.attributes rescue {}
+    vm_attrs = vm_attrs.reject{|k,v| k == :id }
+
+    if vm.respond_to?(:volumes)
+      volumes = vm.volumes || []
+      vm_attrs[:volumes_attributes] = Hash[volumes.each_with_index.map { |volume, idx| [idx.to_s, volume.attributes] }]
+    end
+    vm_attrs
+  rescue ActiveRecord::RecordNotFound
+    logger.warn("VM with UUID '#{uuid}' not found on #{self}")
+    {}
   end
 
   def user_data_supported?

app/models/compute_resources/foreman/model/ec2.rb
@@ -28,7 +28,7 @@ module Foreman::Model
     end
 
     def find_vm_by_uuid(uuid)
-      client.servers.get(uuid)
+      super
     rescue Fog::Compute::AWS::Error
       raise(ActiveRecord::RecordNotFound)
     end

app/models/compute_resources/foreman/model/libvirt.rb
@@ -26,7 +26,7 @@ module Foreman::Model
     end
 
     def find_vm_by_uuid(uuid)
-      client.servers.get(uuid)
+      super
     rescue ::Libvirt::RetrieveError => e
       Foreman::Logging.exception("Failed retrieving libvirt vm by uuid #{ uuid }", e)
       raise ActiveRecord::RecordNotFound
@@ -148,6 +148,17 @@ module Foreman::Model
 
     def associated_host(vm)
       associate_by("mac", vm.mac)
+    end
+
+    def vm_compute_attributes_for(uuid)
+      vm_attrs = super
+      if vm_attrs[:memory_size].nil?
+        vm_attrs[:memory] = nil
+        logger.debug("Compute attributes for VM '#{uuid}' diddn't contain :memory_size")
+      else
+        vm_attrs[:memory] = vm_attrs[:memory_size]*1024 # value is returned in megabytes, we need bytes
+      end
+      vm_attrs
     end
 
     protected

app/models/compute_resources/foreman/model/rackspace.rb
@@ -16,7 +16,7 @@ module Foreman::Model
     end
 
     def find_vm_by_uuid(uuid)
-      client.servers.get(uuid)
+      super
     rescue Fog::Compute::Rackspace::Error
       raise(ActiveRecord::RecordNotFound)
     end

test/functional/api/v2/hosts_controller_test.rb
@@ -260,7 +260,7 @@ class Api::V2::HostsControllerTest < ActionController::TestCase
     get :vm_compute_attributes, { :id => host.to_param }
     assert_response :success
     data = JSON.parse(@response.body)
-    assert_equal data, "cpus" => 4
+    assert_equal data, "cpus" => 4, "memory" => nil
     ComputeResource.any_instance.unstub(:vm_compute_attributes_for)
   end
 

commit e409d711d1bd442e927242cc85a57d6edc27a7b0

Fixes #19236 - handle situation when host is destroyed oVirt
The issue was introduced in 4269abbd, where we replaced the
`OVIRT::OvirtException` error by `ActiveRecord::RecordNotFound` in
`find_vm_by_uuid method`, but we haven't updated the corresponding
`rescue` block in `destroy_vm`.

app/models/compute_resources/foreman/model/ovirt.rb
@@ -253,12 +253,8 @@ module Foreman::Model
     end
 
     def destroy_vm(uuid)
-      begin
-        find_vm_by_uuid(uuid).destroy
-      rescue OVIRT::OvirtException => e
-        #404 error are ignored on delete.
-        raise e unless e.message =~ /404/
-      end
+      find_vm_by_uuid(uuid).destroy
+    rescue ActiveRecord::RecordNotFound
       true
     end
 

test/models/compute_resources/ovirt_test.rb
@@ -13,6 +13,14 @@ class OvirtTest < ActiveSupport::TestCase
     assert_equal host, as_admin { cr.associated_host(vm) }
   end
 
+  describe "destroy_vm" do
+    it "handles situation when vm is not present" do
+      cr = mock_cr_servers(Foreman::Model::Ovirt.new, empty_servers)
+      cr.expects(:find_vm_by_uuid).raises(ActiveRecord::RecordNotFound)
+      assert cr.destroy_vm('abc')
+    end
+  end
+
   describe "find_vm_by_uuid" do
     it "raises RecordNotFound when the vm does not exist" do
       cr = mock_cr_servers(Foreman::Model::Ovirt.new, empty_servers)