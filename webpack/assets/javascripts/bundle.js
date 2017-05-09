66df46839e52670272780f2c95ebc1e0d7d091a9
Matan Werbner <mwerbner@redhat.com>
2017-05-09 11:50:41 +0300

    Fixes #19478 - migrate to webpack2

--- webpack/assets/javascripts/bundle.js
+++ webpack/assets/javascripts/bundle.js
@@ -1,10 +1,10 @@
-require('expose?$!expose?jQuery!jquery');
+require('expose-loader?$!expose-loader?jQuery!jquery');
 require('jquery-ujs');
-require('expose?_!lodash');
-require('expose?jstz!jstz');
-require('expose?ipaddr!ipaddr.js');
+require('expose-loader?_!lodash');
+require('expose-loader?jstz!jstz');
+require('expose-loader?ipaddr!ipaddr.js');
 require('jquery.cookie');
-require('expose?JsDiff!diff');
+require('expose-loader?JsDiff!diff');
 require('./bundle_flot');
 require('./bundle_multiselect');
 require('./bundle_select2');
