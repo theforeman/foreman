91f8ffb1fbd210057fbdc9c4613c9805f62ea0ac
Daniel Lobato Garcia <me@daniellobato.me>
2017-05-25 10:23:06 +0000

    Fixes #19631 - ComputeResource with KeyPair can be removed
    The concern for removing the compute resources with keypairs was trying
    to destroy the compute resource too early. Also, this deletion already
    happened because of the has_one :dependency => :destroy relation
    
    This fixes the problem and adds tests to ensure the relation works.

---
+++ test/models/concerns/key_pair_compute_resource_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class DummyComputeResource < ComputeResource
+  include KeyPairComputeResource
+  include Mocha::API
+
+  def client
+    @client ||= mock('client')
+  end
+end
+
+class KeyPairComputeResourceTest < ActiveSupport::TestCase
+  # We can't use 'Fog::Compute::AWS::KeyPair' model
+  # This class mocks it.
+  class FakeKeyPair
+    attr_reader :name, :fingerprint, :private_key
+
+    def initialize(name, fingerprint, private_key = nil)
+      @name = name
+      @fingerprint = fingerprint
+      @private_key = private_key
+    end
+  end
+
+  test "#get_compute_key_pairs" do
+    @key_pairs = []
+    3.times do |i|
+      @key_pairs << FakeKeyPair.new("foreman-#{i}", "13:01:73:0#{i}")
+    end
+
+    cr = DummyComputeResource.new
+    key_pair = FactoryGirl.build(:key_pair)
+    cr.key_pair = key_pair
+    DummyComputeResource.any_instance.stubs(:key_pairs).returns(@key_pairs)
+    assert_kind_of(ComputeResourceKeyPair, cr.get_compute_key_pairs.first)
+  end
+
+  test 'should remove the key pair on compute resource deletion' do
+    cr = DummyComputeResource.new
+    key_pair = FactoryGirl.build(:key_pair)
+    cr.key_pair = key_pair
+    mock_key_pairs = mock('mock_key_pairs')
+    fog_key_pair = mock('fog_key_pair')
+    cr.send(:client).expects(:key_pairs).returns(mock_key_pairs)
+    mock_key_pairs.expects(:get).with(key_pair.name).returns(fog_key_pair)
+    key_pair.expects(:destroy).once
+    fog_key_pair.expects(:destroy).once
+    assert cr.destroy!
+  end
+end
