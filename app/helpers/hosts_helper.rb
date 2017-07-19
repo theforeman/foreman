766421e697e46768210900e5b8fdd6afde4f1140
Tomer Brisker <tbrisker@gmail.com>
2017-07-19 12:48:00 +0300

    Fixes #20345 - add back button on host info page

--- app/helpers/hosts_helper.rb
+++ app/helpers/hosts_helper.rb
@@ -301,48 +301,51 @@ module HostsHelper
 
   def host_title_actions(host)
     title_actions(
-        button_group(
-            link_to_if_authorized(_("Edit"), hash_for_edit_host_path(:id => host).merge(:auth_object => host),
-                                    :title    => _("Edit this host"), :id => "edit-button", :class => 'btn btn-default'),
-            display_link_if_authorized(_("Clone"), hash_for_clone_host_path(:id => host).merge(:auth_object => host),
-                                    :title    => _("Clone this host"), :id => "clone-button", :class => 'btn btn-default'),
-            if host.build
-              link_to_if_authorized(_("Cancel build"), hash_for_cancelBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
-                                    :disabled => host.can_be_built?,
-                                    :title    => _("Cancel build request for this host"), :id => "cancel-build-button", :class => 'btn btn-default')
-            else
-              link_to_if_authorized(_("Build"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts', :anchor => "review_before_build"),
-                                    :disabled => !host.can_be_built?,
-                                    :title    => _("Enable rebuild on next host boot"),
-                                    :class    => "btn btn-default",
-                                    :id       => "build-review",
-                                    :data     => { :toggle => 'modal',
-                                                   :target => '#review_before_build',
-                                                   :url    => review_before_build_host_path(:id => host)
-                                    }
-              )
-            end
-        ),
-        if host.supports_power?
-          button_group(
-              link_to(_("Loading power state ..."), '#', :disabled => true, :class => 'btn btn-default', :id => :loading_power_state)
+      button_group(
+        link_to(_("Back"), hosts_path, :class => 'btn btn-default')
+      ),
+      button_group(
+        link_to_if_authorized(_("Edit"), hash_for_edit_host_path(:id => host).merge(:auth_object => host),
+                                :title    => _("Edit this host"), :id => "edit-button", :class => 'btn btn-default'),
+        display_link_if_authorized(_("Clone"), hash_for_clone_host_path(:id => host).merge(:auth_object => host),
+                                :title    => _("Clone this host"), :id => "clone-button", :class => 'btn btn-default'),
+        if host.build
+          link_to_if_authorized(_("Cancel build"), hash_for_cancelBuild_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts'),
+                                :disabled => host.can_be_built?,
+                                :title    => _("Cancel build request for this host"), :id => "cancel-build-button", :class => 'btn btn-default')
+        else
+          link_to_if_authorized(_("Build"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'build_hosts', :anchor => "review_before_build"),
+                                :disabled => !host.can_be_built?,
+                                :title    => _("Enable rebuild on next host boot"),
+                                :class    => "btn btn-default",
+                                :id       => "build-review",
+                                :data     => { :toggle => 'modal',
+                                               :target => '#review_before_build',
+                                               :url    => review_before_build_host_path(:id => host)
+                                }
           )
-        end,
+        end
+      ),
+      if host.supports_power?
         button_group(
-          if host.try(:puppet_proxy)
-            link_to_if_authorized(_("Run puppet"), hash_for_puppetrun_host_path(:id => host).merge(:auth_object => host, :permission => 'puppetrun_hosts'),
-                                  :disabled => !Setting[:puppetrun],
-                                  :class => 'btn btn-default',
-                                  :title    => _("Trigger a puppetrun on a node; requires that puppet run is enabled"))
-          end
-        ),
-        button_group(
-            link_to_if_authorized(_("Delete"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'destroy_hosts'),
-                                  :class => "btn btn-danger",
-                                  :id => "delete-button",
-                                  :data => { :message => delete_host_dialog(host) },
-                                  :method => :delete)
+            link_to(_("Loading power state ..."), '#', :disabled => true, :class => 'btn btn-default', :id => :loading_power_state)
         )
+      end,
+      button_group(
+        if host.try(:puppet_proxy)
+          link_to_if_authorized(_("Run puppet"), hash_for_puppetrun_host_path(:id => host).merge(:auth_object => host, :permission => 'puppetrun_hosts'),
+                                :disabled => !Setting[:puppetrun],
+                                :class => 'btn btn-default',
+                                :title    => _("Trigger a puppetrun on a node; requires that puppet run is enabled"))
+        end
+      ),
+      button_group(
+        link_to_if_authorized(_("Delete"), hash_for_host_path(:id => host).merge(:auth_object => host, :permission => 'destroy_hosts'),
+                              :class => "btn btn-danger",
+                              :id => "delete-button",
+                              :data => { :message => delete_host_dialog(host) },
+                              :method => :delete)
+      )
     )
   end
 
