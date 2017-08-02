708b47952ba7d6cc0a0da1dc6adf699fa66f02a7
Marek Hulan <mhulan@redhat.com>
2017-08-02 14:51:40 +0200

    Fixes #20483 - seed all permission for core roles

--- db/seeds.d/02-roles_list.rb
+++ db/seeds.d/02-roles_list.rb
@@ -1,3 +1,5 @@
+require (Rails.root + 'db/seeds.d/02-permissions_list.rb')
+
 class RolesList
   class << self
     def seeded_roles
@@ -38,38 +40,7 @@ class RolesList
     end
 
     def base_manage_permissions
-      [:create_architectures, :edit_architectures, :destroy_architectures,
-       :create_authenticators, :edit_authenticators, :destroy_authenticators,
-       :create_bookmarks, :edit_bookmarks, :destroy_bookmarks,
-       :create_compute_resources, :edit_compute_resources, :destroy_compute_resources,
-       :create_compute_resources_vms, :edit_compute_resources_vms, :destroy_compute_resources_vms, :power_compute_resources_vms, :console_compute_resources_vms,
-       :create_domains, :edit_domains, :destroy_domains,
-       :create_realms, :edit_realms, :destroy_realms,
-       :create_environments, :edit_environments, :destroy_environments, :import_environments,
-       :create_external_variables, :edit_external_variables, :destroy_external_variables,
-       :create_external_parameters, :edit_external_parameters, :destroy_external_parameters,
-       :create_hostgroups, :edit_hostgroups, :destroy_hostgroups,
-       :create_hosts, :edit_hosts, :destroy_hosts, :build_hosts, :power_hosts, :console_hosts, :ipmi_boot_hosts, :puppetrun_hosts,
-       :edit_classes,
-       :create_params, :edit_params, :destroy_params,
-       :create_images, :edit_images, :destroy_images,
-       :create_locations, :edit_locations, :destroy_locations, :assign_locations,
-       :create_media, :edit_media, :destroy_media,
-       :create_models, :edit_models, :destroy_models,
-       :create_operatingsystems, :edit_operatingsystems, :destroy_operatingsystems,
-       :create_provisioning_templates, :edit_provisioning_templates, :destroy_provisioning_templates, :deploy_provisioning_templates,
-       :create_ptables, :edit_ptables, :destroy_ptables,
-       :create_puppetclasses, :edit_puppetclasses, :destroy_puppetclasses, :import_puppetclasses,
-       :create_smart_proxies, :edit_smart_proxies, :destroy_smart_proxies,
-       :create_smart_proxies_autosign, :destroy_smart_proxies_autosign,
-       :edit_smart_proxies_puppetca, :destroy_smart_proxies_puppetca,
-       :create_ssh_keys, :destroy_ssh_keys,
-       :create_subnets, :edit_subnets, :destroy_subnets, :import_subnets,
-       :create_usergroups, :edit_usergroups, :destroy_usergroups,
-       :create_users, :edit_users, :destroy_users,
-       :destroy_config_reports, :upload_config_reports,
-       :upload_facts,
-       :create_trends, :edit_trends, :destroy_trends, :update_trends]
+      PermissionsList.permissions.reject { |resource, name| name.start_with?('view_') }.map { |p| p.last.to_sym } - manage_organizations_permissions
     end
 
     def manage_organizations_permissions
@@ -79,16 +50,7 @@ class RolesList
     end
 
     def view_permissions
-      [
-        :view_architectures, :view_authenticators, :view_bookmarks, :view_compute_resources, :view_compute_resources_vms, :view_compute_profiles,
-        :view_config_groups, :view_domains, :view_realms, :view_environments, :view_external_usergroups, :view_external_variables,
-        :view_external_parameters, :view_filters, :view_hostgroups, :view_keypairs,
-        :view_hosts, :view_params, :view_images, :view_locations, :view_media, :view_models, :view_operatingsystems,
-        :view_provisioning_templates, :view_ptables, :view_puppetclasses, :view_smart_proxies, :view_smart_proxies_autosign,
-        :view_smart_proxies_puppetca, :view_subnets, :view_organizations, :view_usergroups, :view_users, :view_config_reports,
-        :view_facts, :view_audit_logs, :view_statistics, :view_tasks, :view_trends, :view_plugins, :view_mail_notifications,
-        :access_dashboard, :view_roles, :view_ssh_keys
-      ]
+      PermissionsList.permissions.select { |resource, name| name.start_with?('view_') }.map { |p| p.last.to_sym }
     end
   end
 end
