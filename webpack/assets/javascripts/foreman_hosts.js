69eae7da911df09d68d2430257747cf54ca83a1e
Sebastian Gräßl <mail@bastilian.me>
2017-09-05 12:24:50 +0200

    Refs #19554 - Rewrite to use an Object and not Map
    Map is not yet fully supported in all browsers and
    would require additional dependencies to be added.
    
    Using a Object with lodash functions achieves the
    same and does not require any additional
    dependencies.

--- webpack/assets/javascripts/foreman_hosts.js
+++ webpack/assets/javascripts/foreman_hosts.js
@@ -10,16 +10,17 @@ let pluginEditAttributes = {
 export function registerPluginAttributes(componentType, attributes) {
   if (pluginEditAttributes[componentType] !== undefined) {
     pluginEditAttributes[componentType] = _.uniq(
-      pluginEditAttributes[componentType].concat(attributes));
+      pluginEditAttributes[componentType].concat(attributes)
+    );
   }
 }
 
 export function getAttributesToPost(componentType) {
   const defaultAttributes = {
-    'architecture': ['architecture_id', 'organization_id', 'location_id'],
-    'os': ['operatingsystem_id', 'organization_id', 'location_id'],
-    'medium': ['medium_id', 'operatingsystem_id', 'architecture_id'],
-    'image': ['medium_id', 'operatingsystem_id', 'architecture_id', 'model_id']
+    architecture: ['architecture_id', 'organization_id', 'location_id'],
+    os: ['operatingsystem_id', 'organization_id', 'location_id'],
+    medium: ['medium_id', 'operatingsystem_id', 'architecture_id'],
+    image: ['medium_id', 'operatingsystem_id', 'architecture_id', 'model_id']
   };
   let attrsToPost = defaultAttributes[componentType];
 
@@ -27,7 +28,7 @@ export function getAttributesToPost(componentType) {
     return [];
   }
   if (pluginEditAttributes[componentType] !== undefined) {
-      attrsToPost = attrsToPost.concat(pluginEditAttributes[componentType]);
+    attrsToPost = attrsToPost.concat(pluginEditAttributes[componentType]);
   }
   return _.uniq(attrsToPost);
 }
@@ -48,7 +49,7 @@ class PXECompatibilityCheck {
 
     supportedLoaders = this.__compatibleLoadersFunc(os);
     if (supportedLoaders != null) {
-      return (supportedLoaders.indexOf(pxeLoader) > -1);
+      return supportedLoaders.indexOf(pxeLoader) > -1;
     }
     return null;
   }
@@ -61,12 +62,11 @@ const GRUB_UEFI = 'Grub UEFI';
 const GRUB2_UEFI = 'Grub2 UEFI';
 const GRUB2_UEFI_SB = 'Grub2 UEFI SecureBoot';
 
-export let pxeCompatibility = new Map();
+export let pxeCompatibility = {};
 
 // Ubuntu 10.x or older and Grub1
 // Ubuntu 11.x or newer and Grub2
-pxeCompatibility.set('ubuntu',
-  new PXECompatibilityCheck(
+_.set(pxeCompatibility, 'ubuntu', new PXECompatibilityCheck(
     /ubuntu[^\d]*(\d+)(?:[.]\d+)?/,
     function (os) {
       if (os[1] <= '10') {
@@ -76,15 +76,13 @@ pxeCompatibility.set('ubuntu',
       }
       return null;
     }
-  )
-);
+));
 
 // RHEL 6.x and Grub1
 // RHEL 7.x and Grub2
-pxeCompatibility.set('rhel',
-  new PXECompatibilityCheck(
+_.set(pxeCompatibility, 'rhel', new PXECompatibilityCheck(
     /(?:red[ ]*hat|rhel|cent[ ]*os|scientific|oracle)[^\d]*(\d+)(?:[.]\d+)?/,
-    function (os) {
+    os => {
       if (os[1] === '6') {
         return [PXE_BIOS, GRUB_UEFI];
       } else if (os[1] >= '7') {
@@ -92,13 +90,11 @@ pxeCompatibility.set('rhel',
       }
       return null;
     }
-  )
-);
+));
 
 // Debian 2-6 and Grub1
 // Debian 7+ and Grub2
-pxeCompatibility.set('debian',
-  new PXECompatibilityCheck(
+_.set(pxeCompatibility, 'debian', new PXECompatibilityCheck(
     /debian[^\d]*(\d+)(?:[.]\d+)?/,
     function (os) {
       if (os[1] >= '2' && os[1] <= '6') {
@@ -108,21 +104,21 @@ pxeCompatibility.set('debian',
       }
       return null;
     }
-  )
-);
+));
 
 export function checkPXELoaderCompatibility(osTitle, pxeLoader) {
   if (pxeLoader === 'None' || pxeLoader === '') {
     return null;
   }
+  let compatible = null;
 
   osTitle = osTitle.toLowerCase();
-  for (let check of pxeCompatibility.values()) {
-    let compatible = check.isCompatible(osTitle, pxeLoader);
+  _.forEach(_.values(pxeCompatibility), (check) => {
+    let compatibleCheck = check.isCompatible(osTitle, pxeLoader);
 
-    if (compatible != null) {
-      return compatible;
+    if (compatibleCheck != null) {
+      compatible = compatibleCheck;
     }
-  }
-  return null;
+  });
+  return compatible;
 }
