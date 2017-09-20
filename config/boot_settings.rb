c96801e0b98bcbb44dce5351c68bb1ae2d2e82e1
Ewoud Kohl van Wijngaarden <github@kohlvanwijngaarden.nl>
2017-09-20 13:47:55 +0200

    Fixes #20969 - Allow setting the rails version via settings.yaml.dist

--- config/boot_settings.rb
+++ config/boot_settings.rb
@@ -5,6 +5,7 @@ dist_settings_file = File.expand_path('../settings.yaml.dist', __FILE__)
 SETTINGS = File.exist?(dist_settings_file) ? YAML.load(ERB.new(File.read(dist_settings_file)).result) || {} : {}
 
 settings_file = File.expand_path('../settings.yaml', __FILE__)
-SETTINGS[:rails] = YAML.load(ERB.new(File.read(settings_file)).result)[:rails]
+settings = YAML.load(ERB.new(File.read(settings_file)).result)
+SETTINGS[:rails] = settings[:rails] if settings[:rails]
 SETTINGS[:rails] ||= RUBY_VERSION < '2.3' ? '4.2' : '5.0'
 SETTINGS[:rails] = '%.1f' % SETTINGS[:rails] if SETTINGS[:rails].is_a?(Float) # unquoted YAML value
