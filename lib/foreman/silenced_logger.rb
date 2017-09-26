0126b62d6e0bb9fc893c56d7349f17d7e32ae60f
Lukas Zapletal <lzap+git@redhat.com>
2017-09-26 12:41:23 +0200

    Fixes #20559 - print rails log for test failures

--- lib/foreman/silenced_logger.rb
+++ lib/foreman/silenced_logger.rb
@@ -3,7 +3,7 @@
 # assets logging feature.
 module Foreman
   class SilencedLogger < SimpleDelegator
-    def silence(new_level = :error, &block)
+    def silence(new_level = Logger::ERROR, &block)
       old_level = self.level
       begin
         self.level = new_level
