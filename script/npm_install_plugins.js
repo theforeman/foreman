61cb10e2de7b9d1a036b19657b1049ac49ca4009
Ondrej Prazak <oprazak@redhat.com>
2017-10-03 11:55:04 +0200

    Fixes #21180 - Enable adding 3rd party libraries from plugins

---
+++ script/npm_install_plugins.js
@@ -0,0 +1,8 @@
+'use strict';
+/* eslint-disable no-var */
+var childProcess = require('child_process');
+var packageJsonDirs = require('./plugin_webpack_directories').packageJsonDirs;
+
+packageJsonDirs().forEach(function (pluginPath) {
+  childProcess.spawn('npm', ['i'], { env: process.env, cwd: pluginPath, stdio: 'inherit'});
+});
