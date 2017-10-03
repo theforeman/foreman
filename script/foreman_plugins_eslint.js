5316000ba8366f240a289f0dbab73ec348dfe72f
Ondrej Prazak <oprazak@redhat.com>
2017-10-03 11:55:04 +0200

    Fixes #21180 - Enable adding 3rd party libraries from plugins

--- script/foreman_plugins_eslint.js
+++ script/foreman_plugins_eslint.js
@@ -1,3 +1,3 @@
 #!/usr/bin/env node
 
-process.stdout.write(require('./plugin_webpack_directories').paths.join(' '));
+process.stdout.write(require('./plugin_webpack_directories').getPluginDirs().paths.join(' '));
--- script/plugin_webpack_directories.js
+++ script/plugin_webpack_directories.js
@@ -3,6 +3,7 @@
 
 var execSync = require('child_process').execSync;
 var path = require('path');
+var fs = require('fs');
 
 // If we get multiple lines, then the plugin_webpack_directories.rb script
 // has on the stdout more that just the JSON we want, so we use newline to split and check.
@@ -12,8 +13,52 @@ var sanitizeWebpackDirs = function (pluginDirs) {
   return splitDirs.length > 2 ? splitDirs[1] : pluginDirs;
 };
 
-var webpackDirs = execSync(path.join(__dirname, './plugin_webpack_directories.rb'), {
-  stdio: ['pipe', 'pipe', 'ignore']
-});
+// Get paths that have a specific file or folder
+var pluginPath = function (file) {
+  return function (pluginsObj) {
+    var paths = [];
 
-module.exports = JSON.parse(sanitizeWebpackDirs(webpackDirs));
+    pluginsObj.paths.forEach(function (entry) {
+      var filePath = path.join(path.dirname(entry), file);
+
+      if (fs.existsSync(filePath)) {
+        paths.push(filePath);
+      }
+    });
+    return paths;
+  };
+};
+
+// Create aliases for plugins so that their components are easily accessible.
+// Each alias points to /$path_to_plugin/webpack
+var aliasPlugins = function (pluginEntries) {
+  var aliases = {};
+
+  Object.keys(pluginEntries).forEach(function (key) {
+    aliases[key] = path.dirname(pluginEntries[key]);
+  });
+  return aliases;
+};
+
+var webpackedDirs = function () {
+  return execSync(path.join(__dirname, './plugin_webpack_directories.rb'), {
+    stdio: ['pipe', 'pipe', 'ignore']
+  });
+};
+
+var getPluginDirs = function () {
+  return JSON.parse(sanitizeWebpackDirs(webpackedDirs()));
+};
+
+var packageJsonDirs = function () {
+  return pluginPath('package.json')(getPluginDirs()).map(path.dirname);
+};
+
+module.exports = {
+  getPluginDirs: getPluginDirs,
+  pluginNodeModules: pluginPath('node_modules'),
+  aliasPlugins: aliasPlugins,
+  packageJsonDirs: packageJsonDirs,
+  sanitizeWebpackDirs: sanitizeWebpackDirs,
+  pluginPath: pluginPath
+};
