043910257f1974666c8b41d38afc5a5ec54d8d68
Adam Růžička <adamruzicka@users.noreply.github.com>
2017-10-11 15:56:57 +0200

    Fixes #18422 - Expose dynflow worker pool size in config

--- lib/foreman/dynflow/configuration.rb
+++ lib/foreman/dynflow/configuration.rb
@@ -1,6 +1,13 @@
 module Foreman
   module Dynflow
     class Configuration < ::Dynflow::Rails::Configuration
+      def initialize
+        super
+        self.pool_size = SETTINGS.fetch(:dynflow, {})
+                                 .fetch(:pool_size, pool_size)
+        self.db_pool_size = pool_size + 5
+      end
+
       # Action related info such as exceptions raised inside the actions' methods
       def action_logger
         Foreman::Logging.logger('background')
