59b1609ade31883dfadab4fbe736f5f3a1872e14
Amir Fefer <amirfefer@gmail.com>
2017-10-19 15:59:50 +0300

    Fixes #21392 - remove active class from secondary menu
    when taxonomies are not enabled,
    secondary menu gets active class, which marks it.

--- webpack/assets/javascripts/foreman_navigation.js
+++ webpack/assets/javascripts/foreman_navigation.js
@@ -2,12 +2,20 @@ import $ from 'jquery';
 
 function markActiveMenu() {
   const link = `[href='${window.location.pathname}']`;
-  const currentLoc = $('#location-dropdown .nav-item-iconic').text().trim();
-  const currentOrg = $('#organization-dropdown .nav-item-iconic').text().trim();
+  const currentLoc = $('#location-dropdown .nav-item-iconic').text();
+  const currentOrg = $('#organization-dropdown .nav-item-iconic').text();
 
-  $('.list-group-item.secondary-nav-item-pf').has(link).addClass('active');
-  $(`.nav-pf-secondary-nav .list-group-item:contains("${currentLoc}")`).addClass('active');
-  $(`.nav-pf-secondary-nav .list-group-item:contains("${currentOrg}")`).addClass('active');
+  $('.list-group-item.secondary-nav-item-pf')
+    .has(link)
+    .addClass('active');
+  currentLoc &&
+    $(
+      `.nav-pf-secondary-nav .list-group-item:contains("${currentLoc.trim()}")`
+    ).addClass('active');
+  currentOrg &&
+    $(
+      `.nav-pf-secondary-nav .list-group-item:contains("${currentOrg.trim()}")`
+    ).addClass('active');
 }
 
 export function init() {
@@ -26,6 +34,12 @@ export function init() {
 export function activate() {
   // a workaround to enable turbolinks works with pf vertical navigation
   $.fn.setupVerticalNavigation.self = undefined;
-  $(document).off('mouseenter.pf.tertiarynav.data-api', '.secondary-nav-item-pf');
-  $(document).off('mouseleave.pf.tertiarynav.data-api', '.secondary-nav-item-pf');
+  $(document).off(
+    'mouseenter.pf.tertiarynav.data-api',
+    '.secondary-nav-item-pf'
+  );
+  $(document).off(
+    'mouseleave.pf.tertiarynav.data-api',
+    '.secondary-nav-item-pf'
+  );
 }
