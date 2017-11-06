244a7f85779a3cdbd6c53b8f508d68a69e30c636
Marek Hulan <mhulan@redhat.com>
2017-11-06 18:39:09 +0100

    Fixes #21584 - set taxonomies for chart requests

--- app/services/statistics.rb
+++ app/services/statistics.rb
@@ -1,24 +1,24 @@
 module Statistics
-  def self.charts
+  def self.charts(org_id, loc_id)
     charts = [
-      CountHosts.new(:count_by => :operatingsystem, :title => ("OS Distribution"), :search =>"os_title=~VAL~"),
-      CountHosts.new(:count_by => :architecture, :title => _("Architecture Distribution"), :search => "facts.architecture=~VAL~"),
-      CountHosts.new(:count_by => :environment, :title => _("Environment Distribution"), :search => "environment=~VAL~"),
-      CountHosts.new(:count_by => :hostgroup, :title => _("Host Group Distribution"), :search => "hostgroup=~VAL~"),
-      CountHosts.new(:count_by => :compute_resource, :title => _("Compute Resource Distribution"), :search => "compute_resource=~VAL~"),
-      CountFacts.new(:count_by => :processorcount, :unit => Nn_('%s core', '%s cores'), :title => _("Number of CPUs"), :search => "facts.processorcount=~VAL1~"),
-      CountFacts.new(:count_by => :manufacturer, :title => _("Hardware"), :search => "facts.manufacturer~~VAL~"),
-      CountNumericalFactPair.new(:count_by => :memory, :title => _("Average Memory Usage")),
-      CountNumericalFactPair.new(:count_by => :swap, :title => _("Average Swap Usage")),
-      CountPuppetClasses.new(:id => :puppetclass, :title => _("Class Distribution"), :search => "class=~VAL1~")
+      CountHosts.new(:count_by => :operatingsystem, :title => ("OS Distribution"), :search =>"os_title=~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountHosts.new(:count_by => :architecture, :title => _("Architecture Distribution"), :search => "facts.architecture=~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountHosts.new(:count_by => :environment, :title => _("Environment Distribution"), :search => "environment=~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountHosts.new(:count_by => :hostgroup, :title => _("Host Group Distribution"), :search => "hostgroup=~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountHosts.new(:count_by => :compute_resource, :title => _("Compute Resource Distribution"), :search => "compute_resource=~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountFacts.new(:count_by => :processorcount, :unit => Nn_('%s core', '%s cores'), :title => _("Number of CPUs"), :search => "facts.processorcount=~VAL1~", :organization_id => org_id, :location_id => loc_id),
+      CountFacts.new(:count_by => :manufacturer, :title => _("Hardware"), :search => "facts.manufacturer~~VAL~", :organization_id => org_id, :location_id => loc_id),
+      CountNumericalFactPair.new(:count_by => :memory, :title => _("Average Memory Usage"), :organization_id => org_id, :location_id => loc_id),
+      CountNumericalFactPair.new(:count_by => :swap, :title => _("Average Swap Usage"), :organization_id => org_id, :location_id => loc_id),
+      CountPuppetClasses.new(:id => :puppetclass, :title => _("Class Distribution"), :search => "class=~VAL1~", :organization_id => org_id, :location_id => loc_id)
     ]
 
     if SETTINGS[:locations_enabled]
-      charts << CountHosts.new(:count_by => :location, :title => _("Location Distribution"), :search => "location=~VAL~")
+      charts << CountHosts.new(:count_by => :location, :title => _("Location Distribution"), :search => "location=~VAL~", :organization_id => org_id, :location_id => loc_id)
     end
 
     if SETTINGS[:organizations_enabled]
-      charts << CountHosts.new(:count_by => :organization, :title => _("Organization Distribution"), :search => "organization=~VAL~")
+      charts << CountHosts.new(:count_by => :organization, :title => _("Organization Distribution"), :search => "organization=~VAL~", :organization_id => org_id, :location_id => loc_id)
     end
 
     charts
--- app/services/statistics/base.rb
+++ app/services/statistics/base.rb
@@ -1,13 +1,15 @@
 module Statistics
   class Base
-    attr_reader :title, :count_by
+    attr_reader :title, :count_by, :url
 
     def initialize(options = {})
-      @id       = options[:id]
-      @title    = options[:title]
-      @search   = options[:search]
-      @url      = options[:url]
+      @id = options[:id]
+      @title = options[:title]
+      @search = options[:search]
       @count_by = options[:count_by]
+      @organization_id = options[:organization_id]
+      @location_id = options[:location_id]
+      @url = options[:url] || build_url
     end
 
     def calculate
@@ -18,16 +20,18 @@ module Statistics
       @id || count_by.to_s
     end
 
-    def url
-      @url || "statistics/#{id}"
-    end
-
     def search
       "/hosts?search=#{@search}"
     end
 
     def metadata
       {:id => id, :title => title, :url => url, :search => search}
+    end
+
+    private
+
+    def build_url
+      Rails.application.routes.url_helpers.url_for({ :controller => 'statistics', :action => 'show', :id => id, :only_path => true, :location_id => @location_id, :organization_id => @organization_id })
     end
   end
 end
