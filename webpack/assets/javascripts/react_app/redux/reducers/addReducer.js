56562f3fd2ae494294456aee00c1d71b87106b0c
Dan Seethaler <dan@seethalers.com>
2017-11-30 16:56:42 -0500

    refs #21137 - Cleanup register reducers
    Removes extra files from the initial PR #5014
    
    http://projects.theforeman.org/issues/21137

--- webpack/assets/javascripts/react_app/redux/reducers/addReducer.js
+++
@@ -1,9 +0,0 @@
-import store from '../index';
-import { combineReducersAsync } from './index';
-
-const asyncReducers = {};
-
-export default (name, asyncReducer) => {
-  asyncReducers[name] = asyncReducer;
-  store.replaceReducer(combineReducersAsync(asyncReducers));
-};
