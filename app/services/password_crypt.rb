133bba3f20853efe4add97225901fab8b76e167a
Dmitri Dolguikh <witlessbird@gmail.com>
2017-12-18 06:57:43 -0800

    Fixes #21875 - added support for sha512 grub passwords
    All new hosts provisioned using default and rhel
    	kickstarts will use sha512 grub passwords from
    	now on.

--- app/services/password_crypt.rb
+++ app/services/password_crypt.rb
@@ -1,7 +1,7 @@
 require 'base64'
 
 class PasswordCrypt
-  ALGORITHMS = {'SHA256' => '$5$', 'SHA512' => '$6$', 'MD5' => '$1$', 'Base64' => ''}
+  ALGORITHMS = {'SHA256' => '$5$', 'SHA512' => '$6$', 'Base64' => ''}
 
   def self.passw_crypt(passwd, hash_alg = 'SHA256')
     raise Foreman::Exception.new(N_("Unsupported password hash function '%s'"), hash_alg) unless ALGORITHMS.has_key?(hash_alg)
@@ -11,7 +11,7 @@ class PasswordCrypt
   end
 
   def self.grub2_passw_crypt(passw)
-    self.passw_crypt(passw, 'MD5')
+    self.passw_crypt(passw, 'SHA512')
   end
 
   def self.crypt_gnu_compatible?
