bbb05941def1110e7fda792ce12cfa2c5eebc5bd
Tomer Brisker <tbrisker@gmail.com>
2017-12-19 17:29:31 +0200

    Fixes #22033 - Update JS deprecations for 1.18

--- app/assets/javascripts/compute_resource.js
+++ app/assets/javascripts/compute_resource.js
@@ -1,14 +1,14 @@
 function providerSelected(item) {
-  tfm.tools.deprecate('providerSelected', 'tfm.computeResource.providerSelected');
+  tfm.tools.deprecate('providerSelected', 'tfm.computeResource.providerSelected', '1.18');
   tfm.computeResource.providerSelected(item);
 }
 
 function testConnection(item) {
-  tfm.tools.deprecate('testConnection', 'tfm.computeResource.testConnection');
+  tfm.tools.deprecate('testConnection', 'tfm.computeResource.testConnection', '1.18');
   tfm.computeResource.testConnection(item);
 }
 
 function capacity_edit(item) {
-  tfm.tools.deprecate('capacity_edit', 'tfm.computeResource.capacityEdit');
+  tfm.tools.deprecate('capacity_edit', 'tfm.computeResource.capacityEdit', '1.18');
   tfm.computeResource.capacityEdit(item);
 }
