d3c9abfd6e4ecde33254171571dc5bc63f119eff
selmison <selmison@gmail.com>
2018-01-29 15:52:06 -0200

    Fixes #15634 - RancherOS support

---
+++ app/models/operatingsystems/rancheros.rb
@@ -0,0 +1,27 @@
+class Rancheros < Operatingsystem
+  PXEFILES = {:kernel => 'vmlinuz', :initrd => 'initrd'}
+
+  def pxe_type
+    'rancheros'
+  end
+
+  def mediumpath(host)
+   ''
+  end
+
+  def url_for_boot(file)
+    PXEFILES[file]
+  end
+
+  def pxedir
+    ''
+  end
+
+  def boot_files_uri(medium, architecture, host = nil)
+    super(medium, architecture, host)
+  end
+
+  def display_family
+    'RancherOS'
+  end
+end
