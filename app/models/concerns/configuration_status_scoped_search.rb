22d4ca7dd158505ef1abf5e77af1d042459628b2
Tomer Brisker <tbrisker@gmail.com>
2018-02-19 11:50:37 +0200

    Fixes #22616 - Allow searching hosts by status as integer

--- app/models/concerns/configuration_status_scoped_search.rb
+++ app/models/concerns/configuration_status_scoped_search.rb
@@ -6,7 +6,6 @@ module ConfigurationStatusScopedSearch
       options[:offset] = ConfigReport::METRIC.index(status.to_s)
       options[:word_size] = ConfigReport::BIT_NUM
       options[:only_explicit] = true
-      options[:complete_value] = {:true => true, :false => false}
       scoped_search options
     end
   end
