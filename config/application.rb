a4d31ec0f74acd46103f40803be8f48894f9e79e
Ivan Nečas <inecas@redhat.com>
2018-03-13 21:47:34 +0100

    Fixes #22888 - remove code around dynflow db_pool_size
    Remove the code in favor of the Dynfow, where we can handle all the
    logic at one place.

--- config/application.rb
+++ config/application.rb
@@ -283,7 +283,6 @@ module Foreman
     config.after_initialize do
       dynflow = Rails.application.dynflow
       dynflow.eager_load_actions!
-      dynflow.config.increase_db_pool_size
 
       unless dynflow.config.lazy_initialization
         if defined?(PhusionPassenger)
