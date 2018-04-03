441232251177df66ea31b9428760668a7cb36032
Tomer Brisker <tbrisker@gmail.com>
2018-04-03 15:42:17 +0300

    Fixes #23098 - Allow searching on all audited models by type
    This automatically adds all the audited models in an after_initilize
    block so that models defined in plugins are also included. All the
    search related definitions for audit have been extracted to a seperate
    concern. Models using STI require special handling until audits are
    fixed to properly work with STI.

--- test/models/concerns/audit_extensions_test.rb
+++ test/models/concerns/audit_extensions_test.rb
@@ -14,17 +14,6 @@ class AuditExtensionsTest < ActiveSupport::TestCase
     assert_equal audit.username, @user.name
   end
 
-  test "host scoped search for audit works" do
-    resource = FactoryBot.create(:host, :managed, :with_auditing)
-    assert Audit.search_for("host = #{resource.name}").count > 0
-  end
-
-  test "host autocomplete works in audit search" do
-    FactoryBot.create(:host, :managed)
-    hosts = Audit.complete_for("host = ", {:controller => 'audits'})
-    assert hosts.count > 0
-  end
-
   test "audit's change is filtered when data is encrypted" do
     setting = settings(:attributes63)
     setting.expects(:encryption_key).at_least_once.returns('25d224dd383e92a7e0c82b8bf7c985e815f34cf5')
@@ -34,17 +23,6 @@ class AuditExtensionsTest < ActiveSupport::TestCase
     end
     a = Audit.where(auditable_type: 'Setting')
     assert_equal "[redacted]", a.last.audited_changes["value"][1]
-  end
-
-  test "search for type=lookupvalue in audit" do
-    key = lookup_keys(:three)
-    FactoryBot.create :lookup_value, :with_auditing, :lookup_key_id => key.id, :value => false, :match => "hostgroup=Common"
-    refute_empty Audit.search_for("type = override_value")
-  end
-
-  test "search for type=compute_resource in audit" do
-    FactoryBot.create(:ec2_cr, :with_auditing)
-    refute_empty Audit.search_for("type = compute_resource")
   end
 
   test "audited changes field can be greater then 65K bytes" do
