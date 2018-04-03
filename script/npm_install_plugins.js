210e2953618a5e5a65debf8b61e6c7e5a14b3b2d
Ondrej Prazak <oprazak@redhat.com>
2018-04-03 10:47:53 +0200

    Fixes #23097 - Show npm postinstall errors

--- script/npm_install_plugins.js
+++ script/npm_install_plugins.js
@@ -1,6 +1,6 @@
 const childProcess = require('child_process');
 const { packageJsonDirs } = require('./plugin_webpack_directories');
 
-packageJsonDirs().forEach((pluginPath) => {
+packageJsonDirs('pipe').forEach((pluginPath) => {
   childProcess.spawn('npm', ['i'], { env: process.env, cwd: pluginPath, stdio: 'inherit' });
 });
