75c651964e80372c2b647ee83770c85eac66fc8b
Adam Růžička <adamruzicka@users.noreply.github.com>
2018-04-12 11:32:38 -0700

    Fixes #23256 - Check if the delayed plans have expected interface (#5448)
    Not all delayed plan records have 'job_class' key in first element
    in serialized_args.

--- config/initializers/rss_notifications.rb
+++ config/initializers/rss_notifications.rb
@@ -4,7 +4,7 @@
   scheduled_job = pending_jobs.select do |job|
     delayed_plan = world.persistence.load_delayed_plan job.id
     next unless delayed_plan.present?
-    delayed_plan.to_hash[:serialized_args].first["job_class"] == 'CreateRssNotifications'
+    delayed_plan.to_hash[:serialized_args].first.try(:[], 'job_class') == 'CreateRssNotifications'
   end
 
   # Only create notifications if there isn't a scheduled job
