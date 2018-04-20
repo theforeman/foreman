00179523a6b7c143062f337f5adccc6c46b145e9
Marek Hulan <mhulan@redhat.com>
2018-04-20 09:28:32 +0200

    Fixes #22983, #22010 - ignore persistence authz for relations
    (cherry picked from commit 248d1f90ee2ee9f966180fc55de76daaa6ff18a3)

--- app/models/host_config_group.rb
+++ app/models/host_config_group.rb
@@ -6,4 +6,8 @@ class HostConfigGroup < ApplicationRecord
   belongs_to :config_group
 
   validates :host_id, :uniqueness => { :scope => [:config_group_id, :host_type] }
+
+  def check_permissions_after_save
+    true
+  end
 end
