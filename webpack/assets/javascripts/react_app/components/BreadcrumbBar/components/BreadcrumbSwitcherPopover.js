9c591616d51e6b0120fa5ea402dfbf4126a1fb5e
Amir Fefer <amirfefer@gmail.com>
2018-04-25 15:30:26 +0300

    Fixes #23391, #23251 - adjsut breadcrumbs swticher to pf design

--- webpack/assets/javascripts/react_app/components/BreadcrumbBar/components/BreadcrumbSwitcherPopover.js
+++ webpack/assets/javascripts/react_app/components/BreadcrumbBar/components/BreadcrumbSwitcherPopover.js
@@ -75,7 +75,7 @@ const BreadcrumbSwitcherPopover = ({
         </ListGroup>
         <Pager
           className="pager-sm"
-          messages={{ nextPage: __('Next'), previousPage: __('Previous') }}
+          messages={{ nextPage: '', previousPage: '' }}
           onNextPage={onNextPageClick}
           onPreviousPage={onPrevPageClick}
           disablePrevious={currentPage === 1}
