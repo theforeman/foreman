8e8b8b614bff2e0ebac8b5c36b5ec516332fcc7d
Michael Moll <kvedulv@kvedulv.de>
2018-05-02 01:45:37 +0200

    Refs #23474 - capybara 3.x compatible poltergeist version

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -11,7 +11,7 @@ group :test do
   gem 'launchy', '~> 2.4'
   gem 'factory_bot_rails', '~> 4.5', :require => false
   gem 'rubocop-checkstyle_formatter', '~> 0.2'
-  gem "poltergeist", :require => false
+  gem 'poltergeist', '>= 1.18.0', :require => false
   gem 'shoulda-matchers', '~> 3.0'
   gem 'shoulda-context', '~> 1.2'
   gem 'as_deprecation_tracker', '~> 1.4'
