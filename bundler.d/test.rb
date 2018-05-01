6252eea0dae56e5f370a719e3fd7a64dcdd7a1f6
Michael Moll <kvedulv@kvedulv.de>
2018-05-01 15:33:45 +0200

    Fixes #23469 - update show_me_the_cookies to 4.x

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -6,7 +6,7 @@ group :test do
   gem 'minitest-spec-rails', '~> 5.3'
   gem 'ci_reporter_minitest', :require => false
   gem 'capybara', '~> 2.5', :require => false
-  gem 'show_me_the_cookies', '~> 3.0', :require => false
+  gem 'show_me_the_cookies', '~> 4.0', :require => false
   gem 'database_cleaner', '~> 1.3', :require => false
   gem 'launchy', '~> 2.4'
   gem 'factory_bot_rails', '~> 4.5', :require => false
