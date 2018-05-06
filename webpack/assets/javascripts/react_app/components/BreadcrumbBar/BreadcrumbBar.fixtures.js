b4931b187984e34559d42004f4d263791ebd29a3
Avi Sharvit <asharvit@redhat.com>
2018-05-06 16:14:25 +0300

    Fixes #23521 - BreadcrumbBar nested name_field

--- webpack/assets/javascripts/react_app/components/BreadcrumbBar/BreadcrumbBar.fixtures.js
+++ webpack/assets/javascripts/react_app/components/BreadcrumbBar/BreadcrumbBar.fixtures.js
@@ -24,6 +24,12 @@ export const resource = {
   switcherItemUrl: 'some/url/:id',
 };
 
+export const resourceWithNestedFields = {
+  resourceUrl: 'some/url',
+  nameField: 'user.name',
+  switcherItemUrl: 'some/url/:id',
+};
+
 export const resourceList = [
   { id: '1', name: 'Host 1', url: '#' },
   { id: '2', name: 'Host 2', url: '#' },
@@ -45,6 +51,19 @@ export const serverResourceListResponse = {
       { id: '1', name: 'name-1' },
       { id: '2', name: 'name-2' },
       { id: '3', name: 'name-3' },
+    ],
+  },
+};
+
+export const serverResourceListWithNestedFieldsResponse = {
+  data: {
+    page: 1,
+    total: 3,
+    per_page: 2,
+    results: [
+      { id: '1', name: 'name-1', user: { name: 'username-1' } },
+      { id: '2', name: 'name-2', user: { name: 'username-2' } },
+      { id: '3', name: 'name-3', user: { name: 'username-3' } },
     ],
   },
 };
