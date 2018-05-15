c3b44f7bc47671713919168e675544469d91ab15
Amir Fefer <amirfefer@gmail.com>
2018-05-15 18:18:58 +0300

    Fixes #23602 - Remove statisticsHelper file (dead code)

--- app/helpers/statistics_helper.rb
+++
@@ -1,6 +0,0 @@
-module StatisticsHelper
-  def chart(stat)
-    options = {:class => 'statistics-pie small', :expandable => true, :border => 0, :show_title => true}
-    flot_pie_chart(stat.id, stat.title, stat.calculate, options.merge(:search => stat.search))
-  end
-end
