3f7f09d628add226f4f729ff3350509afb4aa8b0
Adam Ruzicka <aruzicka@redhat.com>
2018-05-17 10:43:43 +0200

    Fixes #23611 - Provide helper method to run "service" active jobs
    The root cause was we were triggering the active jobs too early,
    sometimes even before the db was migrated. This led to all sorts of
    errors. The provided helper method does not trigger the jobs when in
    any rake or in test environment. This helper should be reused in
    Katello as well.

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,4 +2,18 @@ class ApplicationJob < ActiveJob::Base
   def humanized_name
     self.class.name
   end
+
+  def self.spawn_if_missing(world)
+    unless Foreman.in_rake? || Rails.env.test?
+      pending_jobs = world.persistence.find_execution_plans(filters: { :state => 'scheduled' })
+      scheduled_job = pending_jobs.select do |job|
+        delayed_plan = world.persistence.load_delayed_plan job.id
+        next unless delayed_plan.present?
+        delayed_plan.to_hash[:serialized_args].first.try(:[], 'job_class') == self.to_s
+      end
+
+      # Only create notifications if there isn't a scheduled job
+      self.perform_later if scheduled_job.blank?
+    end
+  end
 end
