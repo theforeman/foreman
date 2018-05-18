f7b27912be99424f7c68bea87c30d9c8fcb20961
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2018-05-18 17:16:00 +0200

    Fixes #23645 - Make loading settings.yaml optional
    This makes the loading of config/settings.yaml optional. We do change
    the hardcoded default to default logins to true.

--- config/boot_settings.rb
+++ config/boot_settings.rb
@@ -5,7 +5,9 @@ dist_settings_file = File.expand_path('../settings.yaml.dist', __FILE__)
 SETTINGS = File.exist?(dist_settings_file) ? YAML.load(ERB.new(File.read(dist_settings_file)).result) || {} : {}
 
 settings_file = File.expand_path('../settings.yaml', __FILE__)
-settings = YAML.load(ERB.new(File.read(settings_file)).result)
-SETTINGS[:rails] = settings[:rails] if settings[:rails]
+if File.exist?(settings_file)
+  settings = YAML.load(ERB.new(File.read(settings_file)).result)
+  SETTINGS[:rails] = settings[:rails] if settings[:rails]
+end
 SETTINGS[:rails] ||= '5.1'
 SETTINGS[:rails] = '%.1f' % SETTINGS[:rails] if SETTINGS[:rails].is_a?(Float) # unquoted YAML value
