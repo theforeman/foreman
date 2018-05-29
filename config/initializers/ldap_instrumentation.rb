ae65b9d472f1fffd7c82c91f00c9df314d258067
Lukas Zapletal <lzap+git@redhat.com>
2018-05-29 15:06:42 +0200

    Fixes #23142,#23503 - telemetry improvements
    (cherry picked from commit c030096e0de5a026ec446069ecce188b57150667)

--- config/initializers/ldap_instrumentation.rb
+++ config/initializers/ldap_instrumentation.rb
@@ -1,12 +1,15 @@
 # Debug logging from net-ldap and ldap_fluff events sent via ActiveSupport::Notifications
 module Foreman
   class LdapSubscriber < ActiveSupport::LogSubscriber
+    include Foreman::TelemetryHelper
+
     def logger
       ::Foreman::Logging.logger('ldap')
     end
 
     def self.define_log(action, log_name, color)
       define_method(action) do |event|
+        telemetry_duration_histogram(:ldap_request_duration, event.duration)
         return unless logger.debug?
         name = '%s (%.1fms)' % [log_name, event.duration]
         debug "  #{color(name, color, true)}  [ #{yield(event.payload)} ]"
