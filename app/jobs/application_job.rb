0b83ccc569aa8f5b509d4772e68e2a250d45b061
Adam Růžička <adamruzicka@users.noreply.github.com>
2018-06-11 15:42:02 +0200

    Fixes #23611 - Provide helper method to run "service" active jobs
    The root cause was we were triggering the active jobs too early,
    sometimes even before the db was migrated. This led to all sorts of
    errors. The provided helper method does not trigger the jobs when in
    any rake or in test environment. This helper should be reused in
    Katello as well.
    
    (cherry picked from commit e5b4634fa2372e28d2792418305f4205477071de)

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,4 +2,19 @@ class ApplicationJob < ActiveJob::Base
   def humanized_name
     self.class.name
   end
+
+  def self.spawn_if_missing(world)
+    return if (Foreman.in_rake? && !Foreman.in_rake?('dynflow:executor')) || Rails.env.test?
+
+    pending_jobs = world.persistence.find_execution_plans(filters: { :state => 'scheduled' })
+    scheduled_job = pending_jobs.select do |job|
+      delayed_plan = world.persistence.load_delayed_plan job.id
+      next unless delayed_plan.present?
+      arg = delayed_plan.to_hash[:serialized_args].first
+      arg.is_a?(Hash) && arg['job_class'] == self.to_s
+    end
+
+    # Schedule the job only if it doesn't exit yet
+    self.perform_later if scheduled_job.blank?
+  end
 end
--- config/initializers/rss_notifications.rb
+++ config/initializers/rss_notifications.rb
@@ -1,12 +1,4 @@
 # First, we check if there's a job already enqueued for RSS notifications
 ::Foreman::Application.dynflow.config.on_init do |world|
-  pending_jobs = world.persistence.find_execution_plans(filters: { :state => 'scheduled' })
-  scheduled_job = pending_jobs.select do |job|
-    delayed_plan = world.persistence.load_delayed_plan job.id
-    next unless delayed_plan.present?
-    delayed_plan.to_hash[:serialized_args].first.try(:[], 'job_class') == 'CreateRssNotifications'
-  end
-
-  # Only create notifications if there isn't a scheduled job
-  CreateRssNotifications.perform_later if !Rails.env.test? && scheduled_job.blank?
+  CreateRssNotifications.spawn_if_missing(world)
 end
