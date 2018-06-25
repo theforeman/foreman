46cf18681603de397bf5c85cee488e52db8e5373
Amir Fefer <amirfefer@gmail.com>
2018-06-25 15:45:24 +0300

    Fixes #23991 - rephrase RSS setting description

--- app/models/setting/notification.rb
+++ app/models/setting/notification.rb
@@ -1,7 +1,7 @@
 class Setting::Notification < Setting
   def self.default_settings
     [
-      self.set('rss_enable', N_('Whether to get RSS notifications or not'), true, N_('RSS enable')),
+      self.set('rss_enable', N_('Whether to pull RSS notifications or not'), true, N_('RSS enable')),
       self.set('rss_url', N_('URL to fetch RSS notifications from'), 'https://theforeman.org/feed.xml', N_('RSS URL'))
     ]
   end
