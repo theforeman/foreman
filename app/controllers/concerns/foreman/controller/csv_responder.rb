02d5806e8f8cabf5265fb5dad609ac9f2710d16d
Partha Aji <paji@redhat.com>
2018-07-20 18:21:50 -0400

    Fixes #24338 - Can set filename for CSV response
    This commit adds the ability to set a custom filename as a part of the
    csv response

--- app/controllers/concerns/foreman/controller/csv_responder.rb
+++ app/controllers/concerns/foreman/controller/csv_responder.rb
@@ -1,10 +1,11 @@
 module Foreman::Controller::CsvResponder
   extend ActiveSupport::Concern
 
-  def csv_response(resources, columns = csv_columns, header = nil)
+  def csv_response(resources, columns = csv_columns, header = nil, filename = nil)
+    filename ||= "#{controller_name}-#{Date.today}.csv"
     headers["Cache-Control"] = "no-cache"
     headers["Content-Type"] = "text/csv; charset=utf-8"
-    headers["Content-Disposition"] = %(attachment; filename="#{controller_name}-#{Date.today}.csv")
+    headers["Content-Disposition"] = %(attachment; filename="#{filename}")
     self.response_body = CsvExporter.export(resources, columns, header)
   end
 
