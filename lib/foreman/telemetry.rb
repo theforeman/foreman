61ccfb5bd76b10f04d657bf1076f1e93c44c3fd7
Ivan Nečas <inecas@redhat.com>
2018-07-25 18:37:57 +0200

    Fixes #24387 - defined telemetry buckets in seconds
    Also, add more granular buckets, the scale inspired by
    https://github.com/prometheus/client_golang\
    /blob/1ba60c7d585/prometheus/histogram.go#L59

--- lib/foreman/telemetry.rb
+++ lib/foreman/telemetry.rb
@@ -7,7 +7,7 @@ module Foreman
     extend Forwardable
     attr_accessor :prefix, :sinks
 
-    DEFAULT_BUCKETS = [10, 50, 200, 1000, 15000].freeze
+    DEFAULT_BUCKETS = [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10].freeze
 
     def initialize
       @sinks = []
