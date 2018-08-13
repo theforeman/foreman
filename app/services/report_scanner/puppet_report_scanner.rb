3bcbda5fdfeace9d7219ef17ff7dfaff57353915
Marek Hulan <mhulan@redhat.com>
2018-08-13 15:38:47 +0200

    Fixes #24556 - correct puppet origin detection
    (cherry picked from commit 3af8b327bb67ebbfd21d6f3bfbacea2e8f85001f)

--- app/services/report_scanner/puppet_report_scanner.rb
+++ app/services/report_scanner/puppet_report_scanner.rb
@@ -9,8 +9,8 @@ module Foreman
       end
 
       def puppet_report?(logs)
-        first_log = logs.first
-        first_log && first_log['log'].fetch('sources', {}).fetch('source', '') =~ /Puppet/
+        log = logs.last
+        log && log['log'].fetch('sources', {}).fetch('source', '') =~ /Puppet/
       end
     end
   end
