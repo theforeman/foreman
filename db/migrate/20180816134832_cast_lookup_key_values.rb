9b1e1f0a7b49b246158f874bd7c3d788e1197960
Tomer Brisker <tbrisker@users.noreply.github.com>
2018-08-20 17:41:27 +0300

    Fixes #24640 - Cast all lookup keys and values (#5956)
    * Fixes #24640 - Cast all lookup keys and values
    
    db/migrate/20170112175131_migrate_template_to_parameters_macros.rb
    caused all LookupKey default values and LookupValue values to be saved
    as strings, because it updated the attributes without calling callbacks,
    causing cast_default_value or cast_value callbacks to be ignored.
    This causes any values not of string type to break.
    
    (cherry picked from commit e719d906764d500f862803b762bb1da8a506dfd0)

---
+++ db/migrate/20180816134832_cast_lookup_key_values.rb
@@ -0,0 +1,37 @@
+class CastLookupKeyValues < ActiveRecord::Migration[5.1]
+  def up
+    # Different LookupKey types handle casting a bit differently
+    PuppetclassLookupKey.unscoped.preload(:lookup_values).where(override: true).where.not(key_type: 'string').find_each do |key|
+      cast_key_and_values(key)
+    end
+
+    VariableLookupKey.unscoped.preload(:lookup_values).where.not(key_type: 'string').find_each do |key|
+      cast_key_and_values(key)
+    end
+  end
+
+  private
+
+  def cast_key_and_values(key)
+    fix_value(key, :default_value)
+    key.lookup_values.each do |lv|
+      fix_value(lv, :value)
+    end
+  end
+
+  def safemode
+    @box ||= Safemode::Box.new
+  end
+
+  def fix_value(obj, attribute)
+    return if obj.omit
+    value = obj.send(attribute)
+    return unless value.is_a? String
+    return if value.contains_erb?
+    fixed = safemode.eval(value)
+    obj.update_column(attribute, fixed)
+  rescue StandardError => e
+    puts "Error casting #{attribute} #{value} for #{obj.inspect} with error #{e.message}. Perhaps it is invalid?"
+    puts e.backtrace
+  end
+end
