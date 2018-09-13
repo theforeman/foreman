75d02d8ebf2f23999b1d8eda1e390ec52f00c01b
Stephen Benjamin <stephen@redhat.com>
2018-09-13 13:06:26 -0400

    fixes #24935 - validate existence of proxy assocations

--- app/validators/proxy_features_validator.rb
+++ app/validators/proxy_features_validator.rb
@@ -5,6 +5,10 @@ class ProxyFeaturesValidator < ActiveModel::EachValidator
   end
 
   def validate_each(record, attribute, value)
+    if !value && @options[:required]
+      record.errors.add("#{attribute}_id", _('was not found'))
+    end
+
     if value && !value.has_feature?(@options[:feature])
       if @options[:message].nil?
         message = _('does not have the %s feature') % @options[:feature]
