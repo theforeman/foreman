63fbccbf889224b4d5cc9cda9cb397eb5d9a5188
Martin Bačovský <mbacovsk@redhat.com>
2018-10-01 22:44:15 +0000

    Fixes #25099 - Add macros for easier report definition
    Add rows to a report with
      <%- report_row(
        'Column header 1': 'Value 1',
        'Column header 2': `Value 2`,
      ) -%>
    and render the report with
     <%= report_render -%>
    
    Each row of the report needs to have the same columns.
    report_render produces properly quoted valid CSV.
    Values are converted to a String using to_s.
    Enumerable values are comma separated and serialized.
    
    Experimental render to YAML is included (report_render(format: :yaml)
    Enumerable values are kept structured in this format.

--- lib/foreman/renderer/scope/report.rb
+++ lib/foreman/renderer/scope/report.rb
@@ -2,6 +2,65 @@ module Foreman
   module Renderer
     module Scope
       class Report < Foreman::Renderer::Scope::Base
+        def initialize(**args)
+          super
+          @report_data = []
+          @report_headers = []
+        end
+
+        def report_render(format: :csv)
+          case format
+          when :csv
+            report_render_csv
+          when :yaml
+            report_render_yaml
+          end
+        end
+
+        def report_row(row_data)
+          @report_headers = row_data.keys.map(&:to_s) if @report_headers.empty?
+          @report_data << row_data.values
+        end
+
+        def allowed_helpers
+          @allowed_helpers ||= super + [ :report_row, :report_render ]
+        end
+
+        private
+
+        def report_render_yaml
+          @report_data.map do |row|
+            valid_row = row.map { |cell| valid_yaml_type(cell) }
+            Hash[@report_headers.zip(valid_row)]
+          end.to_yaml
+        end
+
+        def report_render_csv
+          CSV.generate(headers: true, encoding: Encoding::UTF_8) do |csv|
+            csv << @report_headers
+            @report_data.each do |row|
+              csv << row.map { |cell| serialize_cell(cell) }
+            end
+          end
+        end
+
+        def serialize_cell(cell)
+          if cell.is_a?(Enumerable)
+            cell.map(&:to_s).join(',')
+          else
+            cell.to_s
+          end
+        end
+
+        def valid_yaml_type(cell)
+          if cell.is_a?(String) || [true, false].include?(cell) || cell.is_a?(Numeric) || cell.nil?
+            cell
+          elsif cell.is_a?(Enumerable)
+            cell.map { |item| valid_yaml_type(item) }
+          else
+            cell.to_s
+          end
+        end
       end
     end
   end
