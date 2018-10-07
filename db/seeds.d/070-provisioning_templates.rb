3eb9105e491f37fb7e02839a9a2500d79a01b26b
Michael Moll <kvedulv@kvedulv.de>
2018-10-07 18:12:24 +0200

    Refs #25123 - Fix Layout/EmptyLinesAroundBlockBody cop

--- db/seeds.d/070-provisioning_templates.rb
+++ db/seeds.d/070-provisioning_templates.rb
@@ -1,6 +1,5 @@
 # Provisioning templates
 ProvisioningTemplate.without_auditing do
-
   TemplateKind.default_template_labels.keys.each do |type|
     kind = TemplateKind.unscoped.find_by_name(type)
     kind ||= TemplateKind.unscoped.create(:name => type)
