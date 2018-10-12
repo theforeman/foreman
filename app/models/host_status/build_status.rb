dd8135582fef712686869af04ce0771e4adc8451
Akshay Mendhe <akshaymendhe786@gmail.com>
2018-10-12 22:21:51 +0000

    Fixes #23699 - Remediation Support for Host Status

--- app/models/host_status/build_status.rb
+++ app/models/host_status/build_status.rb
@@ -64,6 +64,21 @@ module HostStatus
     def build_errors?
       host && host.build_errors.present?
     end
+
+    def remediation_help_text
+      case to_status
+        when PENDING
+          N_("Installation haven't started yet or in progress")
+        when TOKEN_EXPIRED
+          N_("Build token is no longer valid, cancel build mode and enter it again to generate new token")
+        when BUILT
+          N_("OS installer reported end of installation and rebooted the system")
+        when BUILD_FAILED
+          N_("OS installer post script reported failure, check logs")
+        else
+          N_("The host was not scheduled for build yet")
+      end
+    end
   end
 end
 
