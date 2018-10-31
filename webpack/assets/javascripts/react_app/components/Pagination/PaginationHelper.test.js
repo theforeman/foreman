23466e474066f76edc8cadd499c492bee88de338
Boaz Shuster <boaz.shuster.github@gmail.com>
2018-10-31 23:20:35 +0000

    Fixes #25362 - Reset page to 1 when selecting per page
    Signed-off-by: Boaz Shuster <boaz.shuster.github@gmail.com>
    (cherry picked from commit 0635056046f00b08760f8f3a7eb9eba85afee023)

--- webpack/assets/javascripts/react_app/components/Pagination/PaginationHelper.test.js
+++ webpack/assets/javascripts/react_app/components/Pagination/PaginationHelper.test.js
@@ -8,4 +8,12 @@ describe('PaginationHelper', () => {
     changePerPage = changeQuery(uri, { per_page: 5 }, () => {});
     expect(changePerPage).toBe('http://localhost/?search=blue&per_page=5');
   });
+
+  it('should reset page to 1 when set the per_page param', () => {
+    const uri = getURI();
+    let changePerPage = changeQuery(uri, { page: 3 }, () => {});
+    expect(changePerPage).toBe('http://localhost/?page=3');
+    changePerPage = changeQuery(uri, { per_page: 5, page: 1 }, () => {});
+    expect(changePerPage).toBe('http://localhost/?page=1&per_page=5');
+  });
 });
