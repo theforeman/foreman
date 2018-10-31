bd4ebcdbefde976c421b35c80d39b2738d33b758
Lukas Zapletal <lzap+git@redhat.com>
2018-10-31 15:46:43 +0100

    Fixes #25356 - OS parameter to override PXE Loader

--- app/models/concerns/pxe_loader_suggestion.rb
+++ app/models/concerns/pxe_loader_suggestion.rb
@@ -1,6 +1,7 @@
 module PxeLoaderSuggestion
   extend ActiveSupport::Concern
   def suggest_default_pxe_loader
+    self.pxe_loader ||= self.try(:operatingsystem).try(:parameters).try(:find_by_name, "pxe-loader").try(:value)
     self.pxe_loader ||= self.try(:operatingsystem).try(:preferred_loader) if self.respond_to?(:pxe_loader)
   end
 end
