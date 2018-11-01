4107d4e160aa0cb726140ac23ac00a3c7e3d63eb
Ohad Levy <ohadlevy@gmail.com>
2018-11-01 10:47:34 +0200

    fixes #25363 - update jest and snapshots

--- webpack/assets/javascripts/react_app/components/Pagination/PaginationHelper.test.js
+++ webpack/assets/javascripts/react_app/components/Pagination/PaginationHelper.test.js
@@ -10,9 +10,9 @@ describe('PaginationHelper', () => {
   it('should not reset search when set the per_page param', () => {
     const uri = getURI();
     let changePerPage = changeQuery(uri, { search: 'blue' }, () => {});
-    expect(changePerPage).toBe('about:blank?search=blue');
+    expect(changePerPage).toBe('http://localhost/?search=blue');
     changePerPage = changeQuery(uri, { per_page: 5 }, () => {});
-    expect(changePerPage).toBe('about:blank?search=blue&per_page=5');
+    expect(changePerPage).toBe('http://localhost/?search=blue&per_page=5');
   });
 });
 
