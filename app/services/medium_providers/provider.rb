5f756152f9dcd0ca06b6f0da782178e3d30769f1
Stephen Benjamin <stephen@bitbin.de>
2018-11-02 04:36:36 -0400

    Fixes #25285 - medium provider can supply multiple repos

--- app/services/medium_providers/provider.rb
+++ app/services/medium_providers/provider.rb
@@ -6,6 +6,8 @@ module MediumProviders
   # provider.medium_uri
   # => #<URI::HTTP http://mirror.centos.org/centos/7/os/x86_64>
   class Provider
+    delegate :logger, :to => :Rails
+
     # Provides a friendly name of the provider in case of provider error.
     def self.friendly_name
       self.name
@@ -22,6 +24,21 @@ module MediumProviders
     # Returns URI of the installation medium for the current host.
     def medium_uri
       throw "medium_uri is not implemented for #{self.class.name}"
+    end
+
+    # A medium provider can optionally return an array of hashes for additional
+    # software repos to enable during installation, if the template supports
+    # it.  The default implemenation looks at host parameters. The hash keys:
+    #
+    #   name:    Repo name, no spaces
+    #   comment: Repo comment
+    #   url:     Repo URL
+    #   gpgkey:  GPG key URL
+    #   install: Install repo on system for after boot
+    #
+    def additional_media
+      return [] unless entity.respond_to?(:host_param) && (media = entity.host_param('additional_media'))
+      parse_media(media) || []
     end
 
     # Returns unique string representing current installation medium.
@@ -48,6 +65,26 @@ module MediumProviders
     end
 
     private
+
+    def parse_media(media)
+      media = JSON.parse(media)
+      if media.is_a?(Array)
+        media.reject { |medium| is_invalid_hash(medium) }
+      else
+        logger.error("Expected #{entity.name} additional_media parameter to be an array.")
+        []
+      end
+    rescue JSON::ParserError => e
+      Foreman::Logging.exception("JSON parsing error on #{entity.name}'s additional_media parameter.", e)
+      []
+    end
+
+    def is_invalid_hash(medium)
+      return false unless medium['name'].blank? || medium['url'].blank?
+      logger.error("Medium #{medium} missing name.") if medium['name'].blank?
+      logger.error("Medium #{medium} missing URL.") if medium['url'].blank?
+      true
+    end
 
     attr_reader :entity
   end
