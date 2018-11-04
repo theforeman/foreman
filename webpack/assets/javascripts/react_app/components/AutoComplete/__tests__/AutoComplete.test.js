5d9f5856d3e5eb2e93a44376ac9117595f524c41
laviro <1ronlavi@gmail.com>
2018-11-04 21:40:37 +0200

    fixes #25382 - update Autocomplete tests to work on latest Jest

--- webpack/assets/javascripts/react_app/components/AutoComplete/__tests__/AutoComplete.test.js
+++ webpack/assets/javascripts/react_app/components/AutoComplete/__tests__/AutoComplete.test.js
@@ -4,6 +4,7 @@ import AutoComplete from '../index';
 import { AutoCompleteProps } from '../AutoComplete.fixtures';
 import { testComponentSnapshotsWithFixtures } from '../../../common/testHelpers';
 import { KEYCODES } from '../AutoCompleteConstants';
+import { noop } from '../../../common/helpers';
 
 jest.mock('lodash/debounce', () => jest.fn(fn => fn));
 jest.mock('uuid/v1', () => jest.fn(fn => '1234'));
@@ -45,11 +46,11 @@ describe('AutoComplete', () => {
     it('pressing "forward-slash" should trigger focus', () => {
       const props = getProps();
       const component = mount(<AutoComplete {...props} />);
-      const typeahead = component.instance()._typeahead.current.getInstance();
+      const instance = component.instance();
+      const typeahead = instance._typeahead.current.getInstance();
       typeahead.focus = jest.fn();
       expect(typeahead.focus.mock.calls.length).toBe(0);
-      const event = new KeyboardEvent('keypress', { keyCode: KEYCODES.FWD_SLASH });
-      global.dispatchEvent(event);
+      instance.windowKeyPressHandler({ keyCode: KEYCODES.FWD_SLASH, preventDefault: noop });
       expect(typeahead.focus.mock.calls.length).toBe(1);
     });
 
