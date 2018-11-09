acd501031ab592db32e3be01039785e4f81f72b0
Avi Sharvit <asharvit@redhat.com>
2018-11-09 11:48:34 +0200

    Refs #25217 - Add prop-types to SearchInput

--- webpack/assets/javascripts/react_app/components/common/SearchInput/index.js
+++ webpack/assets/javascripts/react_app/components/common/SearchInput/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { DebounceInput } from 'react-debounce-input';
 import { Icon } from 'patternfly-react';
 import { translate as __ } from '../../../../react_app/common/I18n';
+import { noop } from '../../../common/helpers';
 import './searchInput.scss';
 
 class SearchInput extends React.Component {
@@ -34,10 +36,26 @@ class SearchInput extends React.Component {
           debounceTimeout={timeout}
           onChange={onSearchChange}
         />
-         <Icon type="fa" name="close" onClick={() => onClear()} />
+        <Icon type="fa" name="close" onClick={() => onClear()} />
       </div>
     );
   }
 }
+
+SearchInput.propTypes = {
+  focus: PropTypes.bool,
+  searchValue: PropTypes.string,
+  timeout: PropTypes.number,
+  onSearchChange: PropTypes.func,
+  onClear: PropTypes.func,
+};
+
+SearchInput.defaultProps = {
+  focus: false,
+  searchValue: '',
+  timeout: 300,
+  onSearchChange: noop,
+  onClear: noop,
+};
 
 export default SearchInput;
