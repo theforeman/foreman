47af0f6065b41720e09d2aad83eca894c4f56ffd
laviro <1ronlavi@gmail.com>
2018-11-11 17:17:38 +0200

    Fixes #24990 - Move Login Page into React.

---
+++ webpack/assets/javascripts/react_app/components/LoginPage/LoginPage.fixtures.js
@@ -0,0 +1,15 @@
+export const alerts = { error: 'some-error' };
+export const backgroundUrl = '/some-background';
+export const caption = 'some caption';
+export const logoSrc = '/some-logo';
+export const token = 'some token';
+export const version = 'some version';
+
+export const props = {
+  alerts,
+  backgroundUrl,
+  caption,
+  logoSrc,
+  token,
+  version,
+};
