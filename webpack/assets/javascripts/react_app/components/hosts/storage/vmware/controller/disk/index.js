e3eb262b13835222c6a1fcd2aa8cde4958b582c5
Avi Sharvit <asharvit@redhat.com>
2018-11-15 13:01:18 +0200

    Refs #25217 - Refactor hosts/vmware/disk component
    Adds prop-types, default-props and fix some code styling

--- webpack/assets/javascripts/react_app/components/hosts/storage/vmware/controller/disk/index.js
+++ webpack/assets/javascripts/react_app/components/hosts/storage/vmware/controller/disk/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable camelcase */
 import React from 'react';
 import { Button } from 'patternfly-react';
 import PropTypes from 'prop-types';
@@ -6,15 +5,14 @@ import Select from '../../../../../common/forms/Select';
 import Checkbox from '../../../../../common/forms/Checkbox';
 import NumericInput from '../../../../../common/forms/NumericInput';
 import { translate as __ } from '../../../../../../../react_app/common/I18n';
+import { noop } from '../../../../../../common/helpers';
 import './disk.scss';
 
 const Disk = ({
   removeDisk,
   updateDisk,
   name,
-  config: {
-    diskModeTypes, vmExists,
-  },
+  config: { diskModeTypes, vmExists },
   storagePod,
   datastore,
   sizeGb,
@@ -30,50 +28,48 @@ const Disk = ({
 }) => (
   <div className="disk-container">
     <div className="form-group">
-      <label className="col-md-2 control-label">
-        {__('Disk name')}
-      </label>
-      <div className="col-md-4">
-        {name}
-      </div>
+      <label className="col-md-2 control-label">{__('Disk name')}</label>
+      <div className="col-md-4">{name}</div>
       <div className="col-md-2">
-        {!vmExists &&
-        <Button className="close" onClick={removeDisk}>
-          <span aria-hidden="true">×</span>
-        </Button>}
+        {!vmExists && (
+          <Button className="close" onClick={removeDisk}>
+            <span aria-hidden="true">&times;</span>
+          </Button>
+        )}
       </div>
     </div>
-    {!datastore &&
-    <Select
-      label={__('Storage Pod')}
-      value={storagePod}
-      disabled={vmExists}
-      onChange={updateDisk.bind(this, 'storagePod')}
-      options={storagePods}
-      allowClear
-      key="storagePodsSelect"
-      status={storagePodsStatus}
-      errorMessage={storagePodsError}
-    />}
-    {!storagePod &&
-    <Select
-      disabled={vmExists}
-      label={__('Data store')}
-      value={datastore}
-      onChange={updateDisk.bind(this, 'datastore')}
-      options={datastores}
-      allowClear
-      key="datastoresSelect"
-      status={datastoresStatus}
-      errorMessage={datastoresError}
-    />
-    }
+    {!(datastore || datastore.length) && (
+      <Select
+        label={__('Storage Pod')}
+        value={storagePod}
+        disabled={vmExists}
+        onChange={newValues => updateDisk('storagePod', newValues)}
+        options={storagePods}
+        allowClear
+        key="storagePodsSelect"
+        status={storagePodsStatus}
+        errorMessage={storagePodsError}
+      />
+    )}
+    {!(storagePod && storagePod.length) && (
+      <Select
+        disabled={vmExists}
+        label={__('Data store')}
+        value={datastore}
+        onChange={newValues => updateDisk('datastore', newValues)}
+        options={datastores}
+        allowClear
+        key="datastoresSelect"
+        status={datastoresStatus}
+        errorMessage={datastoresError}
+      />
+    )}
 
     <Select
       label={__('Disk Mode')}
       value={mode}
       disabled={vmExists}
-      onChange={updateDisk.bind(this, 'mode')}
+      onChange={newValues => updateDisk('mode', newValues)}
       options={diskModeTypes}
     />
 
@@ -82,7 +78,7 @@ const Disk = ({
       minValue={1}
       format={v => `${v} GB`}
       className="text-vmware-size"
-      onChange={updateDisk.bind(this, 'sizeGb')}
+      onChange={newValues => updateDisk('sizeGb', newValues)}
       label={__('Size (GB)')}
     />
 
@@ -90,26 +86,24 @@ const Disk = ({
       label={__('Thin provision')}
       checked={thin}
       disabled={vmExists}
-      onChange={updateDisk.bind(this, 'thin')}
+      onChange={newValues => updateDisk('thin', newValues)}
     />
 
     <Checkbox
       label={__('Eager zero')}
       checked={eagerzero}
       disabled={vmExists}
-      onChange={updateDisk.bind(this, 'eagerzero')}
+      onChange={newValues => updateDisk('eagerzero', newValues)}
     />
   </div>
 );
 
 Disk.propTypes = {
-  removeDisk: PropTypes.func,
-  updateDisk: PropTypes.func,
-  name: PropTypes.string,
   config: PropTypes.shape({
     diskModeTypes: PropTypes.object,
     vmExists: PropTypes.bool,
-  }),
+  }).isRequired,
+  name: PropTypes.string,
   storagePod: PropTypes.string,
   datastore: PropTypes.string,
   sizeGb: PropTypes.number,
@@ -122,6 +116,26 @@ Disk.propTypes = {
   storagePods: PropTypes.object,
   storagePodsStatus: PropTypes.string,
   storagePodsError: PropTypes.string,
+  removeDisk: PropTypes.func,
+  updateDisk: PropTypes.func,
+};
+
+Disk.defaultProps = {
+  name: '',
+  storagePod: '',
+  datastore: '',
+  sizeGb: null,
+  thin: false,
+  eagerzero: false,
+  mode: '',
+  datastores: {},
+  datastoresStatus: undefined,
+  datastoresError: undefined,
+  storagePods: {},
+  storagePodsStatus: undefined,
+  storagePodsError: undefined,
+  removeDisk: noop,
+  updateDisk: noop,
 };
 
 export default Disk;
