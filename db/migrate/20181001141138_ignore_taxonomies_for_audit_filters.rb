f742d68943af35cf7aee37116bbcfc110c380661
Sebastian Gräßl <mail@bastilian.me>
2018-11-16 11:49:24 +0100

    Refs #24232 - Fix migration to ignore taxonomies for filters

--- db/migrate/20181001141138_ignore_taxonomies_for_audit_filters.rb
+++ db/migrate/20181001141138_ignore_taxonomies_for_audit_filters.rb
@@ -1,15 +1,5 @@
 class IgnoreTaxonomiesForAuditFilters < ActiveRecord::Migration[5.2]
   def up
-    unless (permission = Permission.find_by_name('view_audit_logs'))
-      permission.filters.uniq.each do |filter|
-        next if filter.role.locked?
-
-        filter.organizations = []
-        filter.locations = []
-        filter.override = false
-        filter.taxonomy_search = nil
-        filter.save!
-      end
-    end
+    # see db/migrate/20181116104823_fixed_ignore_taxonomies_for_audit_filters.rb
   end
 end
