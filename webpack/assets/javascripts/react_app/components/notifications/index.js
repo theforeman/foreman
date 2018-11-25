ba99da20f3c017a9d29a80f162ba2312489d9f19
Avi Sharvit <asharvit@redhat.com>
2018-11-25 18:36:07 +0200

    Refs #25217 - Refactor notifications prop-types
    * Adds snapshot testing for notifications toggle-icon

--- webpack/assets/javascripts/react_app/components/notifications/index.js
+++ webpack/assets/javascripts/react_app/components/notifications/index.js
@@ -1,18 +1,22 @@
 import onClickOutside from 'react-onclickoutside';
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { groupBy } from 'lodash';
 import { NotificationDrawerWrapper } from 'patternfly-react';
 import { translate as __ } from '../../../react_app/common/I18n';
 import * as NotificationActions from '../../redux/actions/notifications';
+import { noop } from '../../common/helpers';
 
 import './notifications.scss';
-import ToggleIcon from './toggleIcon';
-
+import ToggleIcon from './ToggleIcon/ToggleIcon';
 
 class notificationContainer extends React.Component {
   componentDidMount() {
-    const { startNotificationsPolling, data: { url } } = this.props;
+    const {
+      startNotificationsPolling,
+      data: { url },
+    } = this.props;
 
     startNotificationsPolling(url);
   }
@@ -59,27 +63,64 @@ class notificationContainer extends React.Component {
 
     return (
       <div>
-        <ToggleIcon hasUnreadMessages={hasUnreadMessages} onClick={toggleDrawer} />
-        {isReady &&
-          isDrawerOpen && (
-            <NotificationDrawerWrapper
-              panels={notificationGroups}
-              expandedPanel={expandedGroup}
-              togglePanel={expandGroup}
-              onNotificationAsRead={markAsRead}
-              onNotificationHide={clearNotification}
-              onMarkPanelAsRead={markGroupAsRead}
-              onMarkPanelAsClear={clearGroup}
-              onClickedLink={clickedLink}
-              toggleDrawerHide={toggleDrawer}
-              isExpandable={false}
-              translations={translations}
-            />
-          )}
+        <ToggleIcon
+          hasUnreadMessages={hasUnreadMessages}
+          onClick={toggleDrawer}
+        />
+        {isReady && isDrawerOpen && (
+          <NotificationDrawerWrapper
+            panels={notificationGroups}
+            expandedPanel={expandedGroup}
+            togglePanel={expandGroup}
+            onNotificationAsRead={markAsRead}
+            onNotificationHide={clearNotification}
+            onMarkPanelAsRead={markGroupAsRead}
+            onMarkPanelAsClear={clearGroup}
+            onClickedLink={clickedLink}
+            toggleDrawerHide={toggleDrawer}
+            isExpandable={false}
+            translations={translations}
+          />
+        )}
       </div>
     );
   }
 }
+
+notificationContainer.propTypes = {
+  data: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+  isDrawerOpen: PropTypes.bool,
+  isReady: PropTypes.bool,
+  notifications: PropTypes.object,
+  expandedGroup: PropTypes.string,
+  hasUnreadMessages: PropTypes.bool,
+  clickedLink: PropTypes.func,
+  startNotificationsPolling: PropTypes.func,
+  toggleDrawer: PropTypes.func,
+  expandGroup: PropTypes.func,
+  markAsRead: PropTypes.func,
+  markGroupAsRead: PropTypes.func,
+  clearNotification: PropTypes.func,
+  clearGroup: PropTypes.func,
+};
+
+notificationContainer.defaultProps = {
+  isDrawerOpen: false,
+  isReady: false,
+  notifications: {},
+  expandedGroup: null,
+  hasUnreadMessages: false,
+  clickedLink: noop,
+  startNotificationsPolling: noop,
+  toggleDrawer: noop,
+  expandGroup: noop,
+  markAsRead: noop,
+  markGroupAsRead: noop,
+  clearNotification: noop,
+  clearGroup: noop,
+};
 
 const mapStateToProps = (state) => {
   const {
@@ -100,4 +141,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, NotificationActions)(onClickOutside(notificationContainer));
+export default connect(
+  mapStateToProps,
+  NotificationActions,
+)(onClickOutside(notificationContainer));
