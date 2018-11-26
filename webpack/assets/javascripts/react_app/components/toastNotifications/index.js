df6f975a9b175438148e4e273cfa4e67684af415
Avi Sharvit <asharvit@redhat.com>
2018-11-26 11:52:56 +0200

    Refs #25217 - Refactor ToastNotifications prop-types

--- webpack/assets/javascripts/react_app/components/toastNotifications/index.js
+++ webpack/assets/javascripts/react_app/components/toastNotifications/index.js
@@ -1,33 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import { ToastNotificationList, ToastNotification, TimedToastNotification } from 'patternfly-react';
+import {
+  ToastNotificationList,
+  ToastNotification,
+  TimedToastNotification,
+} from 'patternfly-react';
 import AlertBody from '../common/Alert/AlertBody';
 import * as ToastActions from '../../redux/actions/toasts';
+import { noop } from '../../common/helpers';
 
-class ToastsList extends Component {
-  render() {
-    const { messages, deleteToast } = this.props;
+const ToastsList = ({ messages, deleteToast }) => {
+  const toastsList = Object.entries(messages)
+    .map(([key, message]) => ({ key, ...message }))
+    .map(({
+      key, link, message, sticky = false, ...toastProps
+    }) => {
+      const ToastComponent = sticky
+        ? ToastNotification
+        : TimedToastNotification;
 
-    const toastsList = Object.entries(messages)
-      .map(([key, message]) => ({ key, ...message }))
-      .map(({
-        key, link, message, sticky = false, ...toastProps
-      }) => {
-        const ToastComponent = sticky ? ToastNotification : TimedToastNotification;
+      return (
+        <ToastComponent
+          key={key}
+          onDismiss={() => deleteToast(key)}
+          {...toastProps}
+        >
+          <AlertBody link={link} message={message} />
+        </ToastComponent>
+      );
+    });
 
-        return (
-          <ToastComponent key={key} onDismiss={() => deleteToast(key)} {...toastProps}>
-            <AlertBody link={link} message={message} />
-          </ToastComponent>
-        );
-      });
+  return <ToastNotificationList>{toastsList}</ToastNotificationList>;
+};
 
-    return <ToastNotificationList>{toastsList}</ToastNotificationList>;
-  }
-}
+ToastsList.propTypes = {
+  messages: PropTypes.object.isRequired,
+  deleteToast: PropTypes.func,
+};
+
+ToastsList.defaultProps = {
+  deleteToast: noop,
+};
 
 const mapStateToProps = state => ({
   messages: state.toasts.messages,
 });
 
-export default connect(mapStateToProps, ToastActions)(ToastsList);
+export default connect(
+  mapStateToProps,
+  ToastActions,
+)(ToastsList);
