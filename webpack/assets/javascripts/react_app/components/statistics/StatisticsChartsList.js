ace8dfebb9bf374193587289767c1d3deb7f3768
Avi Sharvit <asharvit@redhat.com>
2018-11-26 11:34:02 +0200

    Refs #25217 - Refactor statistics prop-types

--- webpack/assets/javascripts/react_app/components/statistics/StatisticsChartsList.js
+++ webpack/assets/javascripts/react_app/components/statistics/StatisticsChartsList.js
@@ -7,6 +7,7 @@ import * as StatisticsChartActions from '../../redux/actions/statistics';
 import { STATUS } from '../../constants';
 import './StatisticsChartsListStyles.scss';
 import { translate as __ } from '../../../react_app/common/I18n';
+import { noop } from '../../common/helpers';
 
 const getStatusFromChart = (chart) => {
   if (chart.data) {
@@ -29,7 +30,7 @@ class StatisticsChartsList extends React.Component {
     const charts = Object.values(this.props.charts).map(chart => (
       <ChartBox
         key={chart.id}
-        type={'donut'}
+        type="donut"
         chart={chart}
         noDataMsg={__('No data available')}
         tip={__('Expand the chart')}
@@ -41,16 +42,30 @@ class StatisticsChartsList extends React.Component {
       />
     ));
 
-    return <div className="statistics-charts-list-root">{this.props.charts && charts}</div>;
+    return (
+      <div className="statistics-charts-list-root">
+        {this.props.charts && charts.length && charts}
+      </div>
+    );
   }
 }
 
 StatisticsChartsList.propTypes = {
   data: PropTypes.array.isRequired,
+  charts: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
+  getStatisticsData: PropTypes.func,
+};
+
+StatisticsChartsList.defaultProps = {
+  getStatisticsData: noop,
+  charts: [],
 };
 
 const mapStateToProps = state => ({
   charts: state.statistics.charts,
 });
 
-export default connect(mapStateToProps, StatisticsChartActions)(StatisticsChartsList);
+export default connect(
+  mapStateToProps,
+  StatisticsChartActions,
+)(StatisticsChartsList);
