c73ee1343a1262c09cca314936947f44112ad16a
kgaikwad <kavitagaikwad103@gmail.com>
2018-11-28 06:02:47 +0000

    Refs #24271 - extracted user check outside of authorizer

--- app/services/authorizer.rb
+++ app/services/authorizer.rb
@@ -25,9 +25,6 @@ class Authorizer
   end
 
   def find_collection(resource_class, options = {})
-    return resource_class.where('1=0') if user.nil?
-    return resource_class.where(nil) if user.admin?
-
     permission = options.delete :permission
     resource_class = Host if resource_class == Host::Base
 
