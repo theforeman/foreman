d0ac2fb394f2e89bb8efc1075bdf992b555470b7
Lukas Zapletal <lzap+git@redhat.com>
2018-11-30 11:22:26 +0100

    Fixes #25594 - issue warning during unused_ip call

--- app/services/ipam/dhcp.rb
+++ app/services/ipam/dhcp.rb
@@ -2,15 +2,21 @@ module IPAM
   class Dhcp < Base
     delegate :dhcp, :dhcp_proxy, :to => :subnet
     def suggest_ip
-      return unless subnet.dhcp?
+      unless subnet.dhcp?
+        message = N_("Unable to suggest IP: subnet %s don't have a DHCP proxy associated, a proxy is not in subnets organization or IPAM is not set to DHCP") % subnet.name
+        logger.info message
+        errors.add(:subnet, _(message))
+        return
+      end
       # we have DHCP proxy so asking it for free IP
       logger.debug "Asking #{dhcp.url} for free IP"
       ip = dhcp_proxy.unused_ip(subnet, mac)["ip"]
       logger.debug("Found #{ip}")
       ip
     rescue => e
-      logger.warn "Failed to fetch a free IP from proxy #{dhcp.name} (#{dhcp.url}): #{e}"
-      errors.add(:subnet, _('Failed to fetch a free IP from proxy %{proxy}: %{message}') % {:message => e, :proxy => "#{dhcp.name} (#{dhcp.url})"})
+      message = N_('Failed to fetch a free IP from proxy %{proxy}: %{message}') % {:message => e, :proxy => "#{dhcp.name} (#{dhcp.url})"}
+      logger.warn "#{message}: #{e}"
+      errors.add(:subnet, _(message))
       nil
     end
   end
