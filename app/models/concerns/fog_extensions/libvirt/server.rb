f3b54584a93ca0b4015b3aae78fe9fe20d96c417
Clemens Bergmann <c.bergmann@proventa.de>
2018-11-30 16:29:08 +0100

    fixes #2308 - add option to select cpumode in libvirt

--- app/models/concerns/fog_extensions/libvirt/server.rb
+++ app/models/concerns/fog_extensions/libvirt/server.rb
@@ -11,6 +11,14 @@ module FogExtensions
         name
       end
 
+      def cpu_mode
+        attributes[:cpu][:mode].nil? ? Setting[:libvirt_default_cpu_mode] : attributes[:cpu][:mode]
+      end
+
+      def cpu_mode=(cpumode)
+        attributes[:cpu][:mode]= cpumode == 'default' ? nil : cpumode
+      end
+
       def nics_attributes=(attrs)
       end
 
