989dc0bfebf87aa3522c03dbab62ab89cf22aec6
Ori Rabin <orrabin@gmail.com>
2018-12-02 15:07:52 +0200

    Fixes #25600 - Migrate Icon to patternfly-react
    Remove Icon from foreman
    Switch DocumentLinkContent to use patternfly-react Icon

--- webpack/assets/javascripts/react_app/components/common/DocumentationLink/DocumentationLink.stories.js
+++ webpack/assets/javascripts/react_app/components/common/DocumentationLink/DocumentationLink.stories.js
@@ -1,35 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import DocumentationLink, { DocumentLinkContent } from './index';
 import Story from '../../../../../../stories/components/Story';
+import DocumentationLink from './index';
 
-storiesOf('Components/DocumentationLink', module)
-  .add('Default', () => (
-    <Story>
-      <ul>
-        <DocumentationLink handleClick={action('Link was clicked')} href="#" />
-      </ul>
-    </Story>
-  ))
-  .add('DocumentLinkContent wrapped in a button', () => (
-    <Story>
-      <button className="btn btn-default">
-        <DocumentLinkContent />
-      </button>
-      <button className="btn btn-primary">
-        <DocumentLinkContent />
-      </button>
-      <button className="btn btn-warning">
-        <DocumentLinkContent />
-      </button>
-      <button className="btn btn-danger">
-        <DocumentLinkContent />
-      </button>
-    </Story>
-  ))
-  .add('DocumentLinkContent', () => (
-    <Story>
-      <DocumentLinkContent />
-    </Story>
-  ));
+storiesOf('Components/DocumentationLink', module).add('Default', () => (
+  <Story>
+    <ul>
+      <DocumentationLink handleClick={action('Link was clicked')} href="#" />
+    </ul>
+  </Story>
+));
