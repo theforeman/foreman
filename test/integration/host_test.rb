cd26bc78536b3b4e67445733d4cb7df39a9c0c24
Sebastian Ziebell <sebastian.ziebell@dm.de>
2018-12-12 16:04:16 +0100

    Fixes #20118 - display error instead of NilClass
    In some scenarios when editing a host in Foreman, the VM may not
    exist in VMware anymore, e.g. was deleted or uuid is missing.
    The host is handled as a new VM, but cannot be saved.
    
    In this case an error message should be shown.
    
    * display failure message in case `compute_attributes` are missing
    * add unit tests to check `setCompute`
    * add integration test to check alert is displayed

--- test/integration/host_test.rb
+++ test/integration/host_test.rb
@@ -67,6 +67,21 @@ class HostIntegrationTest < ActionDispatch::IntegrationTest
       click_link 'Parameters'
       assert_equal class_params.find("textarea").value, "a: c\n"
     end
+
+    test 'displays warning when vm not found by uuid' do
+      ComputeResource.any_instance.stubs(:find_vm_by_uuid).raises(ActiveRecord::RecordNotFound)
+      host = FactoryBot.create(:host, :with_hostgroup, :with_environment, :on_compute_resource, :managed)
+
+      visit edit_host_path(host)
+      assert page.has_link?('Operating System')
+      click_link 'Operating System'
+
+      alert_header = page.find('#compute_resource div.alert strong')
+      alert_body = page.find('#compute_resource div.alert span.text')
+
+      assert_equal "'#{host.name}' not found on '#{host.compute_resource}'", alert_header.text
+      assert_equal "'#{host.name}' could be deleted or '#{host.compute_resource}' is not responding.", alert_body.text
+    end
   end
 
   describe 'clone page' do
