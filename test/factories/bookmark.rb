f4ddc26aefae8644137260bea616854c255a0e8f
Michael Moll <kvedulv@kvedulv.de>
2018-12-18 03:12:59 +0100

    Fixes #25719 - Update Rubocop to 0.61.1

--- test/factories/bookmark.rb
+++ test/factories/bookmark.rb
@@ -2,7 +2,8 @@ FactoryBot.define do
   factory :bookmark do
     sequence(:name) { |n| "bookmark_#{n}" }
     query { "bar" }
-
+    # rubocop:disable Layout/EmptyLinesAroundAccessModifier
     public { false }
+    # rubocop:enable Layout/EmptyLinesAroundAccessModifier
   end
 end
