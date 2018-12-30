550999b9977a0544f0c2440f7a3133b4713e8d84
Avi Sharvit <asharvit@redhat.com>
2018-12-30 12:01:40 +0200

    Refs #25217 - fix charts eslint

--- webpack/assets/javascripts/react_app/components/common/charts/DonutChart/index.js
+++ webpack/assets/javascripts/react_app/components/common/charts/DonutChart/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { DonutChart as PfDonutChart } from 'patternfly-react';
 import { getDonutChartConfig } from '../../../../../services/charts/DonutChartService';
 import MessageBox from '../../MessageBox';
 import { translate as __ } from '../../../../../react_app/common/I18n';
+import { noop } from '../../../../common/helpers';
 
 const DonutChart = ({
   data,
   onclick,
-  config = 'regular',
-  noDataMsg = __('No data available'),
-  title = { type: 'percent' },
-  unloadData = false,
+  config,
+  noDataMsg,
+  title,
+  unloadData,
 }) => {
   const chartConfig = getDonutChartConfig({ data, config, onclick });
 
@@ -24,6 +26,24 @@ const DonutChart = ({
     );
   }
   return <MessageBox msg={noDataMsg} icontype="info" />;
+};
+
+DonutChart.propTypes = {
+  data: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  config: PropTypes.oneOf(['regular', 'medium', 'large']),
+  noDataMsg: PropTypes.string,
+  title: PropTypes.object,
+  unloadData: PropTypes.bool,
+  onclick: PropTypes.func,
+};
+
+DonutChart.defaultProps = {
+  data: undefined,
+  config: 'regular',
+  noDataMsg: __('No data available'),
+  title: { type: 'percent' },
+  unloadData: false,
+  onclick: noop,
 };
 
 export default DonutChart;
