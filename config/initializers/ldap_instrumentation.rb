56608248cb0448fd8ba2605789b51b7fc87bf3a4
Marek Hulan <mhulan@redhat.com>
2019-01-11 13:29:19 +0100

    Fixes #25840 - fix ldap logging
    (cherry picked from commit 2509afcd3762b26d2e903c8d8e0bb5bca49088d6)

--- config/initializers/ldap_instrumentation.rb
+++ config/initializers/ldap_instrumentation.rb
@@ -9,7 +9,7 @@ module Foreman
 
     def self.define_log(action, log_name, color)
       define_method(action) do |event|
-        telemetry_duration_histogram(:ldap_request_duration, event.duration)
+        telemetry_observe_histogram(:ldap_request_duration, event.duration)
         return unless logger.debug?
         name = '%s (%.1fms)' % [log_name, event.duration]
         debug "  #{color(name, color, true)}  [ #{yield(event.payload)} ]"
