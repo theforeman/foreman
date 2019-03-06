a569e240bff5414e0be92dff5690549461030fcb
Tomer Brisker <tbrisker@gmail.com>
2019-03-06 23:52:28 +0200

    Fixes #26260 - Mass assign discovered hosts to seeded taxonomies
    Otherwise, assignment triggers callbacks and audits for each discobered
    host, leading to very long seed time when upgrading with many discovered
    hosts and taxonomies disabled.
    Also, don't audit template assignments.

--- db/seeds.d/050-taxonomies.rb
+++ db/seeds.d/050-taxonomies.rb
@@ -1,5 +1,5 @@
 # some associations shouldn't be set or require special handling.
-skip_associations = [:associated_audits, :audits, :default_users, :hosts,
+skip_associations = [:associated_audits, :audits, :default_users, :hosts, :discovered_hosts,
                      :location_parameters, :organization_parameters,
                      :taxable_taxonomies, :reports ] + Template.descendants.map {|type| type.to_s.tableize.to_sym}
 
@@ -19,12 +19,14 @@ User.as_anonymous_admin do
 
       # Only default templates are assigned during taxonomy creation
       Template.where(default: false).each do |template|
-        template.send("#{taxonomy.to_s.parameterize.pluralize}=", [tax])
+        template.without_auditing do
+          template.send("#{taxonomy.to_s.parameterize.pluralize}=", [tax])
+        end
       end
 
       # Mass update when we can
       tax_id = "#{taxonomy.to_s.parameterize}_id"
-      Host::Managed.update_all(tax_id => tax.id)
+      Host::Base.update_all(tax_id => tax.id)
       User.update_all("default_#{tax_id}": tax.id)
 
       Setting[:"default_#{taxonomy.to_s.parameterize}"] = tax_name
