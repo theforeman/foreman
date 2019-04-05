0b167ac6bdd7be65ce632049306bc442a27dda3d
Gilad Lekner <giladlekner@Gilads-iMac.local>
2019-04-05 14:25:47 +0300

    Fixes #25335 - Create a React/Redux Editor

--- test/integration/ptable_js_test.rb
+++ test/integration/ptable_js_test.rb
@@ -1,7 +1,36 @@
 require 'integration_test_helper'
 
 class PtableJSTest < IntegrationTestWithJavascript
+  setup do
+    @ptable = FactoryBot.create(:ptable, :ubuntu, :name => 'ubuntu default')
+  end
+
   test "index page" do
     assert_index_page(ptables_path, "Partition Tables", "Create Partition Table")
+  end
+
+  test "edit page" do
+    visit ptables_path
+    click_link "ubuntu default"
+    fill_in "ptable_name", :with => "debian default"
+    find('#editor').click
+    find('.ace_content').send_keys "d-i partman-auto/disk string /dev/sda\nd-i"
+    sleep 1 # Wait for the editor onChange debounce
+    assert_submit_button(ptables_path)
+    assert page.has_link? 'debian default'
+  end
+
+  test "make sure that ptable names with slashes and dots work" do
+    visit ptables_path
+    click_link "ubuntu default"
+    fill_in "ptable_name", :with => "debian.default /dev/sda"
+    find('#editor').click
+    find('.ace_content').send_keys "d-i partman-auto/disk string /dev/sda\nd-i"
+    sleep 1 # Wait for the editor onChange debounce
+    assert_submit_button(ptables_path)
+
+    assert page.has_link? 'debian.default /dev/sda'
+    click_link "debian.default /dev/sda"
+    assert page.has_field?("ptable_name") # not 404
   end
 end
---
+++ webpack/assets/javascripts/react_app/components/Editor/EditorReducer.js
@@ -0,0 +1,155 @@
+import Immutable from 'seamless-immutable';
+
+import {
+  EDITOR_CHANGE_DIFF_VIEW,
+  EDITOR_CHANGE_SETTING,
+  EDITOR_CHANGE_TAB,
+  EDITOR_CHANGE_VALUE,
+  EDITOR_DISMISS_ERROR,
+  EDITOR_SHOW_ERROR,
+  EDITOR_EXEC_PREVIEW,
+  EDITOR_SHOW_LOADING,
+  EDITOR_HIDE_LOADING,
+  EDITOR_IMPORT_FILE,
+  EDITOR_INITIALIZE,
+  EDITOR_MODAL_TOGGLE,
+  EDITOR_REVERT_CHANGES,
+  EDITOR_TOGGLE_MASK,
+  EDITOR_TOGGLE_RENDER_VIEW,
+  EDITOR_HOST_SELECT_CLEAR,
+  EDITOR_HOST_SELECT_TOGGLE,
+  EDITOR_FETCH_HOST_PENDING,
+  EDITOR_HOST_SELECT_RESET,
+  EDITOR_FETCH_HOST_RESOLVED,
+  EDITOR_HOST_INITIAL_FETCH,
+  EDITOR_HOST_ARR,
+  EDITOR_HOST_FILTERED_ARR,
+} from './EditorConstants';
+
+const initialState = Immutable({
+  [EDITOR_HOST_ARR]: [],
+  [EDITOR_HOST_FILTERED_ARR]: [],
+  diffViewType: 'split',
+  editorName: 'editor',
+  errorText: '',
+  isFetchingHosts: false,
+  isLoading: false,
+  isMasked: false,
+  isMaximized: false,
+  isRendering: false,
+  isSearchingHosts: false,
+  isSelectOpen: false,
+  keyBinding: 'Default',
+  mode: 'Ruby',
+  previewResult: '',
+  renderedEditorValue: '',
+  readOnly: false,
+  searchQuery: '',
+  selectedHost: {
+    id: '',
+    name: '',
+  },
+  selectedView: 'input',
+  showError: false,
+  templateClass: '',
+  theme: 'Monokai',
+  value: '',
+});
+
+export default (state = initialState, action) => {
+  const { payload } = action;
+
+  switch (action.type) {
+    case EDITOR_INITIALIZE: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_REVERT_CHANGES: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_IMPORT_FILE: {
+      return state.set('value', payload.value);
+    }
+
+    case EDITOR_EXEC_PREVIEW: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_HOST_SELECT_CLEAR: {
+      return state.set('searchQuery', '').set('isSearchingHosts', false);
+    }
+
+    case EDITOR_MODAL_TOGGLE: {
+      return state.set('isMaximized', !state.isMaximized);
+    }
+
+    case EDITOR_CHANGE_DIFF_VIEW: {
+      return state.set('diffViewType', payload);
+    }
+
+    case EDITOR_CHANGE_VALUE: {
+      return state.set('value', payload);
+    }
+
+    case EDITOR_SHOW_ERROR: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_DISMISS_ERROR: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_CHANGE_TAB: {
+      return state.set('selectedView', payload);
+    }
+
+    case EDITOR_CHANGE_SETTING: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_TOGGLE_MASK: {
+      return state.set('isMasked', !state.isMasked);
+    }
+
+    case EDITOR_TOGGLE_RENDER_VIEW: {
+      return state.set('isRendering', !state.isRendering);
+    }
+
+    case EDITOR_SHOW_LOADING: {
+      return state.set('isLoading', true);
+    }
+
+    case EDITOR_HIDE_LOADING: {
+      return state.set('isLoading', false);
+    }
+
+    case EDITOR_FETCH_HOST_PENDING: {
+      return state.merge(payload);
+    }
+
+    case EDITOR_FETCH_HOST_RESOLVED: {
+      return state.set('isFetchingHosts', false).merge(payload);
+    }
+
+    case EDITOR_HOST_INITIAL_FETCH: {
+      return state.set('hosts', payload);
+    }
+
+    case EDITOR_HOST_SELECT_TOGGLE: {
+      return state
+        .set('isSelectOpen', !state.isSelectOpen)
+        .set('searchQuery', '');
+    }
+
+    case EDITOR_HOST_SELECT_RESET: {
+      return state
+        .set('searchQuery', '')
+        .set('isFetchingHosts', false)
+        .set('isSearchingHosts', false);
+    }
+
+    default:
+      return state;
+  }
+};
