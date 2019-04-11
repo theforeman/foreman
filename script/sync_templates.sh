01b4e8024adeeb8f72fa8ec90fc726bc69eba3ed
Matthias Dellweg <dellweg@atix.de>
2019-04-11 13:15:22 +0200

    Fixes #26590 - Rename template snippets
    Rename provisioning template snippets to not start with '_' as rails
    template partials. This will match better, what we see in
    community-templates, and will allow to do a directory comparison via diff.
    
    Script used:
    for i in $(ls *.erb); do mv $i ${i#_}; done

--- script/sync_templates.sh
+++ script/sync_templates.sh
@@ -13,15 +13,6 @@ trap "rm -rf $REPO" EXIT
 git clone -q -b $(git symbolic-ref -q HEAD --short) \
   https://github.com/theforeman/community-templates $REPO/ct
 
-# add underscore prefix to snippets
-if [ -d $REPO/ct/provisioning_templates/snippet/ ];
-then
-  for i in $REPO/ct/provisioning_templates/snippet/*;
-  do
-    mv $i $REPO/ct/provisioning_templates/snippet/_$(basename $i)
-  done
-fi
-
 # move into destination dir if run from Foreman root
 [ -d app/views/unattended ] && cd app/views/unattended
 
