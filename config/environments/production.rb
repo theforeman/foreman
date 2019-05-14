5a5d3c0d0539490289781bb6d00f6d22503ad82f
Ondrej Prazak <oprazak@redhat.com>
2019-05-14 13:19:49 +0200

    Fixes #26790 - Support ES6 in Uglifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,8 @@ Foreman::Application.configure do |app|
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # use support for ES6 syntax in uglifier
+  config.assets.js_compressor = Sprockets::UglifierCompressor.new(:harmony => true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
