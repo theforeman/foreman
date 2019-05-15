9a8d2cde08aae90f3e10de9bd743e6f362374006
Timo Goebel <mail@timogoebel.name>
2019-05-15 13:08:09 +0200

    fixes #26796 - coreos: url_for_boot

--- test/models/operatingsystems/coreos_test.rb
+++ test/models/operatingsystems/coreos_test.rb
@@ -1,19 +1,31 @@
 require 'test_helper'
 
 class CoreosTest < ActiveSupport::TestCase
+  let(:operatingsystem) { FactoryBot.create(:coreos) }
+  let(:architecture) { architectures(:x86_64) }
+  let(:medium) { FactoryBot.create(:medium, :coreos) }
+  let(:mock_entity) do
+    OpenStruct.new(
+      operatingsystem: operatingsystem,
+      architecture: architecture,
+      medium: medium
+    )
+  end
+  let(:medium_provider) { MediumProviders::Default.new(mock_entity) }
+
   describe '#mediumpath' do
     test 'generates medium path url' do
-      operatingsystem = FactoryBot.create(:coreos)
-      architecture = architectures(:x86_64)
-      medium = FactoryBot.create(:medium, :coreos)
-      mock_entity = OpenStruct.new(
-        operatingsystem: operatingsystem,
-        architecture: architecture,
-        medium: medium
-      )
+      assert_equal 'http://stable.release.core-os.net/amd64-usr/', operatingsystem.mediumpath(medium_provider)
+    end
+  end
 
-      provider = MediumProviders::Default.new(mock_entity)
-      assert_equal 'http://stable.release.core-os.net/amd64-usr/', operatingsystem.mediumpath(provider)
+  describe '#url_for_boot' do
+    test 'generates kernel url' do
+      assert_equal 'http://stable.release.core-os.net/amd64-usr/494.5.0/coreos_production_pxe.vmlinuz', operatingsystem.url_for_boot(medium_provider, :kernel)
+    end
+
+    test 'generates initrd url' do
+      assert_equal 'http://stable.release.core-os.net/amd64-usr/494.5.0/coreos_production_pxe_image.cpio.gz', operatingsystem.url_for_boot(medium_provider, :initrd)
     end
   end
 end
