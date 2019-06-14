3145e584321cfbc8ddde6078fd800594aa45fea0
Tomer Brisker <tbrisker@gmail.com>
2019-06-14 11:35:32 +0300

    Fixes #27052 - Convert ModelsTable to react hooks
    Also reordered the conditions to return earlier when possible.

--- webpack/assets/javascripts/react_app/components/ModelsTable/ModelsTable.js
+++ webpack/assets/javascripts/react_app/components/ModelsTable/ModelsTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Spinner } from 'patternfly-react';
 import PropTypes from 'prop-types';
 import { Table } from '../common/table';
@@ -8,42 +8,40 @@ import { translate as __ } from '../../common/I18n';
 import createModelsTableSchema from './ModelsTableSchema';
 import { getURIQuery } from '../../common/helpers';
 
-class ModelsTable extends React.Component {
-  componentDidMount() {
-    this.props.getTableItems(getURIQuery(window.location.href));
-  }
+const ModelsTable = ({
+  getTableItems,
+  sortBy,
+  sortOrder,
+  error,
+  status,
+  results,
+}) => {
+  useEffect(() => {
+    getTableItems(getURIQuery(window.location.href));
+  }, []);
 
-  render() {
-    const {
-      getTableItems,
-      sortBy,
-      sortOrder,
-      error,
-      status,
-      results,
-    } = this.props;
-
-    const renderTable =
-      status === STATUS.ERROR ? (
-        <MessageBox
-          key="models-table-error"
-          icontype="error-circle-o"
-          msg={__(`Could not receive data: ${error && error.message}`)}
-        />
-      ) : (
-        <Table
-          key="models-table"
-          columns={createModelsTableSchema(getTableItems, sortBy, sortOrder)}
-          rows={results}
-        />
-      );
-
-    if (results.length > 0) {
-      return renderTable;
-    }
+  if (results.length === 0) {
     return <Spinner size="lg" loading />;
   }
-}
+
+  if (status === STATUS.ERROR) {
+    return (
+      <MessageBox
+        key="models-table-error"
+        icontype="error-circle-o"
+        msg={__(`Could not receive data: ${error && error.message}`)}
+      />
+    );
+  }
+
+  return (
+    <Table
+      key="models-table"
+      columns={createModelsTableSchema(getTableItems, sortBy, sortOrder)}
+      rows={results}
+    />
+  );
+};
 
 ModelsTable.propTypes = {
   results: PropTypes.array.isRequired,
