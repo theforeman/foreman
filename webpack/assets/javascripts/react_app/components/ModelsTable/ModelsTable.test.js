d32e5d8aba7829479ca6bfeb3c64217114375ac9
Tomer Brisker <tbrisker@gmail.com>
2019-06-14 11:35:32 +0300

    Fixes #27052 - Convert ModelsTable to react hooks
    Also reordered the conditions to return earlier when possible.

--- webpack/assets/javascripts/react_app/components/ModelsTable/ModelsTable.test.js
+++ webpack/assets/javascripts/react_app/components/ModelsTable/ModelsTable.test.js
@@ -1,23 +1,29 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import ModelsTable from './ModelsTable';
 import { Table } from '../common/table';
 import MessageBox from '../common/MessageBox';
 
-const data = {
-  pagination: {
-    vieType: 'table',
-    perPageOptions: [1, 2, 3],
-    itemCount: 5,
-    perPage: 1,
+const results = [
+  {
+    info: null,
+    created_at: '2018-03-26 09:54:21 +0300',
+    updated_at: '2018-03-26 09:54:21 +0300',
+    vendor_class: null,
+    hardware_model: null,
+    id: 29,
+    name: 'X8SIL',
+    can_edit: true,
+    can_delete: true,
+    hosts_count: 1,
   },
-};
+];
 
 describe('ModelsTable', () => {
   it('render table on sucess', () => {
     const getModelItems = jest.fn().mockReturnValue([]);
-    const view = shallow(
-      <ModelsTable results={[{}]} getTableItems={getModelItems} data={data} />
+    const view = mount(
+      <ModelsTable results={results} getTableItems={getModelItems} />
     );
     expect(getModelItems.mock.calls).toHaveLength(1);
     expect(view.find(Table)).toHaveLength(1);
@@ -30,7 +36,6 @@ describe('ModelsTable', () => {
         results={[{}]}
         error={Error('some error message')}
         status="ERROR"
-        data={data}
       />
     );
     expect(view.find(MessageBox)).toHaveLength(1);
--- webpack/assets/javascripts/react_app/components/common/table/formatters/ellipsisCellFormatter.js
+++ webpack/assets/javascripts/react_app/components/common/table/formatters/ellipsisCellFormatter.js
@@ -3,4 +3,4 @@ import EllipsisWithTooltip from 'react-ellipsis-with-tooltip';
 import cellFormatter from './cellFormatter';
 
 export default value =>
-  cellFormatter(<EllipsisWithTooltip>{value}</EllipsisWithTooltip>);
+  cellFormatter(<EllipsisWithTooltip>{value || ''}</EllipsisWithTooltip>);
