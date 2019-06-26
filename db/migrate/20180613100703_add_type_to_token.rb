8e4b08538945afe6aaef5e22b39b53777af767bc
Tomer Brisker <tbrisker@gmail.com>
2019-06-26 13:07:37 +0300

    Fixes #24993 - Modify token value to text
    MySQL can't index fields longer than 767 bytes so we need to limit the
    index length. This is causing upgrade failures on MySQL, and users
    attempting to rerun migrations will fail on trying to recreate the
    existing type index, as there is no transaction support in MySQL for
    index creation so it isn't rolled back when the first migration fails.

--- db/migrate/20180613100703_add_type_to_token.rb
+++ db/migrate/20180613100703_add_type_to_token.rb
@@ -5,7 +5,10 @@ class AddTypeToToken < ActiveRecord::Migration[5.1]
     add_index :tokens, :host_id
     add_foreign_key :tokens, :hosts, :name => "tokens_host_id_fk" unless foreign_key_exists?(:tokens, { :name => "tokens_host_id_fk" })
     add_column :tokens, :type, :string, default: 'Token::Build', null: false, index: true
-    change_column :tokens, :value, :string, limit: 900
+    # mysql only allows indexing up to 767 bytes for text columns
+    remove_index :tokens, :value if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
+    change_column :tokens, :value, :text
+    add_index :tokens, :value, length: 767 if ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'mysql'
   end
 
   def down
