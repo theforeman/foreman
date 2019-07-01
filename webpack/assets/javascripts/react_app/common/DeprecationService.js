b259f71e227ccdaadd9b5c24be4d46e123111aff
Amir Fefer <amirfefer@gmail.com>
2019-07-01 15:33:06 +0300

    Fixes #27193 - remove hardcoded depraction version in js

--- webpack/assets/javascripts/react_app/common/DeprecationService.js
+++ webpack/assets/javascripts/react_app/common/DeprecationService.js
@@ -1,16 +1,11 @@
 /* eslint-disable no-console, max-len */
 
-export const deprecate = (oldMethod, newMethod, version = '1.22') =>
+export const deprecate = (oldMethod, newMethod, version) =>
   console.warn(
     `DEPRECATION WARNING: you are using deprecated ${oldMethod}, it will be removed in Foreman ${version}. Use ${newMethod} instead.`
   );
 
-export const deprecateObjectProperty = (
-  obj,
-  oldProp,
-  newProp,
-  version = '1.20'
-) => {
+export const deprecateObjectProperty = (obj, oldProp, newProp, version) => {
   const oldPropPointer = obj[oldProp];
 
   Object.defineProperty(obj, oldProp, {
