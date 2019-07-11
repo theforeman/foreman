7e9415db73841e05cbcf2fc6746806a6341464f8
Ondrej Prazak <oprazak@redhat.com>
2019-07-11 16:22:24 +0200

    Fixes #28016 - Add global slot for host(group) params

--- webpack/assets/javascripts/foreman_tools.js
+++ webpack/assets/javascripts/foreman_tools.js
@@ -3,6 +3,7 @@
 /* eslint-disable jquery/no-text */
 /* eslint-disable jquery/no-ajax */
 /* eslint-disable jquery/no-each */
+/* eslint-disable jquery/no-class */
 
 import $ from 'jquery';
 import { translate as __ } from './react_app/common/I18n';
@@ -116,3 +117,24 @@ export const setTab = () => {
     $(`.nav-tabs a[href="${urlHash}"]`).tab('show');
   }
 };
+
+export function highlightTabErrors() {
+  const errorFields = $('.tab-content .has-error');
+  errorFields.parents('.tab-pane').each(function fn() {
+    $(`a[href="#${this.id}"]`).addClass('tab-error');
+  });
+  $('.tab-error')
+    .first()
+    .click();
+  $('.nav-pills .tab-error')
+    .first()
+    .click();
+  errorFields
+    .first()
+    .find('.form-control')
+    .focus();
+
+  $('a[rel="popover"]').popover();
+  activateTooltips();
+  activateDatatables();
+}
