1245c20206b17cfaccd31084fb973a19ecbea540
Lukas Zapletal <lzap+git@redhat.com>
2019-07-29 12:20:17 +0200

    Fixes #27442 - deprecation mysql warning

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -1,1 +1,27 @@
 ActiveSupport::Deprecation.behavior = :silence if Rails.env.production?
+
+Foreman::Application.configure do |app|
+  config.after_initialize do
+    next if Foreman.in_rake?
+    next unless ActiveRecord::Base.connection.adapter_name.downcase.starts_with?('mysql')
+    blueprint = NotificationBlueprint.find_by_name('feature_deprecation')
+    next unless blueprint
+    message = UINotifications::StringParser.new(blueprint.message, {feature: 'MySQL as a backend database', version: '1.25'}).to_s
+    next if blueprint.notifications.where(message: message).any?
+    Notification.create!(
+      audience: Notification::AUDIENCE_ADMIN,
+      message: message,
+      notification_blueprint: blueprint,
+      initiator: User.anonymous_admin,
+      :actions => {
+        :links => [
+          {
+            :href => 'https://theforeman.org/2019/09/dropping-support-for-mysql.html',
+            :title => _('Further Information'),
+            :external => true,
+          },
+        ],
+      }
+    )
+  end
+end
