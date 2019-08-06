f71e2aada7618e1ebc3e9db7ad0fce27c05910ae
Tomer Brisker <tbrisker@gmail.com>
2019-08-06 16:22:44 +0300

    Refs #16818 - Add test and refactor url calculation

--- app/helpers/fact_values_helper.rb
+++ app/helpers/fact_values_helper.rb
@@ -12,7 +12,7 @@ module FactValuesHelper
       memo = memo.empty? ? current_name : memo + FactName::SEPARATOR + current_name
       content_tag(:li) do
         if value.compose && current_name == name.split(FactName::SEPARATOR).last
-          url = host_parent_fact_facts_path(:parent_fact => value_name, :host_id => params[:host_id] || value.host.name)
+          url = url_with_parent(value_name, params[:host_id] || value.host.name)
           link_to(icon_text('angle-down', '', :kind => 'fa', :title => _('Expand nested items')), url) + ' ' + create_fact_name_link(parent, current_name, params[:host_id], value, value_name, memo)
         else
           create_fact_name_link(parent, current_name, params[:host_id], value, value_name, memo)
@@ -24,9 +24,8 @@ module FactValuesHelper
   def create_fact_name_link(parent, current_name, host_id, value, value_name, memo)
     return current_name if parent.present? && h(parent.name) == memo
     if value_name != memo || value.compose
-      parameters = { :parent_fact => memo }
-      url = host_parent_fact_facts_path(parameters.merge({ :host_id => host_id || value.host.name }))
-      link_to(current_name, url.gsub('%2F', '/'),
+      url = url_with_parent(memo, host_id || value.host.name)
+      link_to(current_name, url,
               :title => _("Show all %s children fact values") % memo)
     else
       link_to(current_name, fact_values_path(:search => "name = #{value_name}"),
@@ -83,5 +82,12 @@ module FactValuesHelper
 
   def print_escape_warning(column)
     fact_contains_escaped_values
+  end
+
+  private
+
+  def url_with_parent(parent, host_id)
+    opts = { parent_fact: parent, host_id: host_id }
+    host_parent_fact_facts_path(opts).gsub('%2F', '/')
   end
 end
