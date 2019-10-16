bc37baca1aadefe96c55bf8a5100bffdd0173e97
Ondřej Ezr <oezr@redhat.com>
2019-10-16 03:28:57 +0200

    Refs #28064 - add sidekiq configs

--- bundler.d/dynflow_sidekiq.rb
+++ bundler.d/dynflow_sidekiq.rb
@@ -1,4 +1,4 @@
-group :sidekiq do
+group :dynflow_sidekiq do
   gem 'sidekiq', '~> 5.0'
   gem 'gitlab-sidekiq-fetcher', require: false
 end
--- extras/dynflow-sidekiq.rb
+++ extras/dynflow-sidekiq.rb
@@ -4,8 +4,11 @@ app_file = File.expand_path('./config/application', rails_root)
 require app_file
 
 rails_env_file = File.expand_path('./config/environment.rb', rails_root)
-
 require rails_env_file
+
+redis_url = SETTINGS.dig(:dynflow, :redis_url)
+Sidekiq.redis = { url: redis_url }
+
 if Sidekiq.options[:queues].include?("dynflow_orchestrator")
   ::Rails.application.dynflow.executor!
 elsif (Sidekiq.options[:queues] - ['dynflow_orchestrator']).any?
