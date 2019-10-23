46a3a56751a6ed465ef1bc77b023497fca3bc24d
Tomer Brisker <tbrisker@gmail.com>
2019-10-23 13:00:22 +0300

    Fixes #27740 - Allow creating parameters with lone taxonomies
    Parameter has a relation to taxonomies due to Location and
    Organization params.
    However, we shouldn't set location_id or organization_id on parameters
    even if only one taxonomy is present as they are not taxable.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -243,6 +243,8 @@ module Api
 
     def assign_lone_taxonomies
       return unless resource_class_for(resource_name)
+      # parameters aren't taxable but have a relation to taxonomies because of location and organization params
+      return if resource_name == 'parameter'
       Taxonomy.types.each do |taxonomy|
         tax_name = taxonomy.to_s.downcase
         if resource_class.reflections.has_key? tax_name.pluralize
