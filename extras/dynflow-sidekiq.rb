031166592df1e36932ce923981a6b1e1b3b7b311
Adam Ruzicka <aruzicka@redhat.com>
2019-10-25 15:22:11 +0200

    Fixes #28139 - Fix dynflow initialization with Sidekiq

--- extras/dynflow-sidekiq.rb
+++ extras/dynflow-sidekiq.rb
@@ -3,6 +3,8 @@ rails_root = Dir.pwd
 app_file = File.expand_path('./config/application', rails_root)
 require app_file
 
+::Rails.application.dynflow.config.lazy_initialization = true
+
 rails_env_file = File.expand_path('./config/environment.rb', rails_root)
 require rails_env_file
 
@@ -10,16 +12,15 @@ redis_url = SETTINGS.dig(:dynflow, :redis_url)
 Sidekiq.redis = { url: redis_url }
 
 if Sidekiq.options[:queues].include?("dynflow_orchestrator")
+  Sidekiq.options[:dynflow_executor] = true
   ::Rails.application.dynflow.executor!
 elsif (Sidekiq.options[:queues] - ['dynflow_orchestrator']).any?
   ::Rails.application.dynflow.config.remote = true
 end
 
-world_id = nil
 ::Rails.application.dynflow.config.on_init(false) do |world|
-  world_id = world.id
   Sidekiq.options[:dynflow_world] = world
 end
 
 ::Rails.application.dynflow.initialize!
-Rails.logger.info("Everything ready for world: #{world_id}")
+Rails.logger.info("Everything ready for world: #{::Rails.application.dynflow.world.id}")
