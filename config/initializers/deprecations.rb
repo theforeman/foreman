eb981c2e579159afba7fea638cb063457a302f58
Tomer Brisker <tbrisker@gmail.com>
2019-11-12 17:53:43 +0200

    Fixes #28248 - Notify about SQLite production usage deprecation

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -25,3 +25,30 @@ Foreman::Application.configure do |app|
     )
   end
 end
+
+Foreman::Application.configure do |app|
+  config.after_initialize do
+    next if Foreman.in_rake?
+    next unless Rails.env.production?
+    next unless ActiveRecord::Base.connection.adapter_name.downcase.starts_with?('sqlite')
+    blueprint = NotificationBlueprint.find_by_name('feature_deprecation')
+    next unless blueprint
+    message = UINotifications::StringParser.new(blueprint.message, {feature: 'SQLite as a production database', version: '1.25'}).to_s
+    next if blueprint.notifications.where(message: message).any?
+    Notification.create!(
+      audience: Notification::AUDIENCE_ADMIN,
+      message: message,
+      notification_blueprint: blueprint,
+      initiator: User.anonymous_admin,
+      :actions => {
+        :links => [
+          {
+            :href => 'https://community.theforeman.org/t/dropping-support-for-sqlite-as-production-database/16158',
+            :title => _('Further Information'),
+            :external => true,
+          },
+        ],
+      }
+    )
+  end
+end
