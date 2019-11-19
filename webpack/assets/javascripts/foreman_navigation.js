88eb8d4846c2806cde40253e8823e5f1f9a493cd
Amir Fefer <amirfefer@gmail.com>
2019-11-19 15:51:22 +0200

    Fixes #28303 - connect react router to redux

--- webpack/assets/javascripts/foreman_navigation.js
+++ webpack/assets/javascripts/foreman_navigation.js
@@ -1,6 +1,8 @@
 /* eslint-disable jquery/no-show */
 
 import $ from 'jquery';
+import URI from 'urijs';
+import { push } from 'connected-react-router';
 import store from './react_app/redux';
 import * as LayoutActions from './react_app/components/Layout/LayoutActions';
 import { deprecate } from './react_app/common/DeprecationService';
@@ -22,6 +24,16 @@ export const visit = url => {
 
 export const reloadPage = () => {
   window.location.reload();
+};
+
+/**
+ * Push a new url to foreman's react router
+ * @param {String} url - the base url i.e `/hosts`
+ * @param {Object} searchQuery - the query params, i.e {'per_page': 4, 'page': 2}
+ */
+export const pushUrl = (url, queryParams) => {
+  const urlWithQueries = new URI(url).search(queryParams).toString();
+  return store.dispatch(push(urlWithQueries));
 };
 
 export const showLoading = () => {
--- webpack/assets/javascripts/react_app/redux/middlewares/index.js
+++ webpack/assets/javascripts/react_app/redux/middlewares/index.js
@@ -1,2 +1,24 @@
-export { APIMiddleware } from '../API';
-export { IntervalMiddleware } from './IntervalMiddleware';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { routerMiddleware } from 'connected-react-router';
+import { APIMiddleware } from '../API';
+import { IntervalMiddleware } from './IntervalMiddleware';
+import history from '../../history';
+
+const logReduxToConsole = () => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const isLogger = process.env.REDUX_LOGGER;
+
+  if (!isProduction && !global.__testing__) {
+    if (isLogger === undefined || isLogger === true) return true;
+  }
+  return isProduction && isLogger;
+};
+
+export const middlewares = [
+  thunk,
+  IntervalMiddleware,
+  APIMiddleware,
+  routerMiddleware(history),
+  ...(logReduxToConsole() ? [createLogger()] : []),
+];
