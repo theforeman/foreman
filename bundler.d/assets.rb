2d5ab01b6e7e7aae268acff66794493e6870a48a
Ondřej Ezr <oezr@redhat.com>
2019-11-21 00:03:16 +0100

    Fixes #29522 - upgrade jquery-ui-rails

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -1,5 +1,5 @@
 group :assets do
-  gem 'jquery-ui-rails', '< 5.0.0'
+  gem 'jquery-ui-rails', '~> 6.0'
   gem 'patternfly-sass', '>= 3.32.1', '< 3.38.0'
   gem 'gettext_i18n_rails_js', '~> 1.0'
   gem 'execjs', '>= 1.4.0', '< 3.0'
