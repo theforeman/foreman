893f6e86fabfb6caafdc15cdd0aeaefe0a30dc43
laviro <1ronlavi@gmail.com>
2019-11-28 15:14:11 +0200

    Fixes #28379 - remove unexist export

--- webpack/assets/javascripts/react_app/redux/API/index.js
+++ webpack/assets/javascripts/react_app/redux/API/index.js
@@ -1,4 +1,4 @@
 export { actionTypeGenerator } from './APIActionTypeGenerator';
 export { API_OPERATIONS } from './APIConstants';
-export { get, APIMiddleware } from './APIMiddleware';
+export { APIMiddleware } from './APIMiddleware';
 export { default as API } from './API';
