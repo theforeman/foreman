6da60ab0ea86f5a01bcce7a2038b502c58e98d07
Evgeni Golov <evgeni@golov.de>
2019-12-03 15:32:58 +0100

    Fixes #28406 - pin mocha to < 1.10
    (cherry picked from commit bdf54d4c9fb46cab16b2ac4e6aec767cae568e8b)

--- bundler.d/test.rb
+++ bundler.d/test.rb
@@ -1,5 +1,5 @@
 group :test do
-  gem 'mocha', '~> 1.4'
+  gem 'mocha', '~> 1.4', '< 1.10'
   gem 'single_test', '~> 0.6'
   gem 'minitest', '~> 5.1', '< 5.11'
   gem 'minitest-retry', '~> 0.0', :require => false
