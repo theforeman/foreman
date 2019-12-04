15fed2f6e10daf20010d3f292c71b8457a8edaf3
Amir Fefer <amirfefer@gmail.com>
2019-12-04 19:02:53 +0200

    Fixes #28422 - use jest instead jest-cli

--- config/jest.config.js
+++ config/jest.config.js
@@ -18,11 +18,11 @@ module.exports = {
   unmockedModulePathPatterns: ['react', 'node_modules/'],
   moduleNameMapper: {
     '^.+\\.(png|gif|css|scss)$': 'identity-obj-proxy',
-    "^dnd-core$": "dnd-core/dist/cjs/index.js",
-    "^react-dnd$": "react-dnd/dist/cjs/index.js",
-    "^react-dnd-html5-backend$": "react-dnd-html5-backend/dist/cjs/index.js",
-    "^react-dnd-test-backend$": "react-dnd-test-backend/dist/cjs/index.js",
-    "^react-dnd-test-utils$": "react-dnd-test-utils/dist/cjs/index.js"
+    '^dnd-core$': 'dnd-core/dist/cjs/index.js',
+    '^react-dnd$': 'react-dnd/dist/cjs/index.js',
+    '^react-dnd-html5-backend$': 'react-dnd-html5-backend/dist/cjs/index.js',
+    '^react-dnd-test-backend$': 'react-dnd-test-backend/dist/cjs/index.js',
+    '^react-dnd-test-utils$': 'react-dnd-test-utils/dist/cjs/index.js',
   },
   globals: {
     __testing__: true,
@@ -31,7 +31,10 @@ module.exports = {
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
-  moduleDirectories: ['node_modules/@theforeman/vendor-core/node_modules', 'node_modules'],
+  moduleDirectories: [
+    'node_modules/@theforeman/vendor-core/node_modules',
+    'node_modules',
+  ],
   setupFiles: [
     'raf/polyfill',
     'jest-prop-type-error',
