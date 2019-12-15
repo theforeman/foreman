0b62e6debb5b8fc321eded600aeabf80e38033e8
laviro <1ronlavi@gmail.com>
2019-12-15 11:17:19 +0200

    Fixes #28507 - Move stored fixtures to snapshots

--- webpack/assets/javascripts/react_app/redux/actions/hosts/powerStatus/powerStatus.fixtures.js
+++ webpack/assets/javascripts/react_app/redux/actions/hosts/powerStatus/powerStatus.fixtures.js
@@ -1,47 +1,4 @@
-const mockSuccessRequests = [
-  {
-    path: '/hosts/:id/power',
-    searchRegex: '/hosts/1/power',
-    response: {
-      id: 1,
-      state: 'na',
-      title: 'N/A',
-    },
-  },
-];
-
 export const requestData = {
   failRequest: { id: 0, url: '/hosts/0/power' },
   successRequest: { id: 1, url: '/hosts/1/power' },
 };
-
-export const onFailureActions = [
-  { payload: { id: 0 }, type: 'HOST_POWER_STATUS_REQUEST' },
-  {
-    key: 'HOST_POWER_STATUS',
-    payload: { id: 0 },
-    type: 'API_GET',
-    url: '/hosts/0/power',
-  },
-  {
-    payload: {
-      error: new Error('Request failed with status code 500'),
-      id: 0,
-    },
-    type: 'HOST_POWER_STATUS_FAILURE',
-  },
-];
-
-export const onSuccessActions = [
-  { payload: { id: 1 }, type: 'HOST_POWER_STATUS_REQUEST' },
-  {
-    key: 'HOST_POWER_STATUS',
-    payload: { id: 1 },
-    type: 'API_GET',
-    url: '/hosts/1/power',
-  },
-  {
-    payload: mockSuccessRequests[0].response,
-    type: 'HOST_POWER_STATUS_SUCCESS',
-  },
-];
