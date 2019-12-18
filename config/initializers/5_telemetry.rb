b50324b4e899d1aa201496568757a4c905826aad
Ondřej Ezr <oezr@redhat.com>
2019-12-18 10:46:51 +0100

    Fixes #28506 - fix upgraded prometheus

--- config/initializers/5_telemetry.rb
+++ config/initializers/5_telemetry.rb
@@ -1,3 +1,11 @@
+# Set multiprocess-friendly data store
+require 'prometheus/client'
+require 'prometheus/client/data_stores/direct_file_store'
+PROMETHEUS_STORE_DIR = File.join(Rails.root, 'tmp', 'prometheus')
+FileUtils.mkdir_p(PROMETHEUS_STORE_DIR)
+Prometheus::Client.config.data_store =
+  Prometheus::Client::DataStores::DirectFileStore.new(dir: PROMETHEUS_STORE_DIR)
+
 # Foreman telemetry global setup.
 telemetry = Foreman::Telemetry.instance
 if SETTINGS[:telemetry] && (Rails.env.production? || Rails.env.development?)
--- lib/foreman/telemetry_sinks/prometheus_sink.rb
+++ lib/foreman/telemetry_sinks/prometheus_sink.rb
@@ -6,27 +6,27 @@ module Foreman::TelemetrySinks
     end
 
     def add_counter(name, description, instance_labels)
-      @prom.counter(name.to_sym, description)
+      @prom.counter(name.to_sym, docstring: description, labels: instance_labels)
     end
 
     def add_gauge(name, description, instance_labels)
-      @prom.gauge(name.to_sym, description)
+      @prom.gauge(name.to_sym, docstring: description, labels: instance_labels)
     end
 
     def add_histogram(name, description, instance_labels, buckets)
-      @prom.histogram(name.to_sym, description, {}, buckets)
+      @prom.histogram(name.to_sym, docstring: description, buckets: buckets, labels: instance_labels)
     end
 
     def increment_counter(name, value, tags)
-      @prom.get(name.to_sym).increment(tags, value)
+      @prom.get(name.to_sym).increment(by: value, labels: tags)
     end
 
     def set_gauge(name, value, tags)
-      @prom.get(name.to_sym).set(tags, value)
+      @prom.get(name.to_sym).set(value, labels: tags)
     end
 
     def observe_histogram(name, value, tags)
-      @prom.get(name.to_sym).observe(tags, value)
+      @prom.get(name.to_sym).observe(value, labels: tags)
     end
   end
 end
