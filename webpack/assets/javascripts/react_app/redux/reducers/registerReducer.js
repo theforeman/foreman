98105a044c28b709dd38fb6b8ac2c0f5296dd15b
Avi Sharvit <sharvita@gmail.com>
2020-01-23 11:24:06 +0200

    Fixes #28844 - protect the state from  duplication
    Make the redux state resiliant to code duplication
    by saving the state globally on the window object.

--- webpack/assets/javascripts/react_app/redux/reducers/registerReducer.js
+++ webpack/assets/javascripts/react_app/redux/reducers/registerReducer.js
@@ -1,9 +1,11 @@
 import store from '../index';
 import { combineReducersAsync } from './index';
 
-const asyncReducers = {};
+window.tfm_async_reducers = window.tfm_async_reducers || {};
 
 export default (name, asyncReducer) => {
+  const asyncReducers = window.tfm_async_reducers;
+
   asyncReducers[name] = asyncReducer;
   store.replaceReducer(combineReducersAsync(asyncReducers));
 };
