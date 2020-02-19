04627d1e0e06a88577fd388ace98f4f269a727fc
Ondřej Ezr <oezr@redhat.com>
2020-02-19 11:02:19 +0100

    Refs #29027 - session out of range adhere pg versions

--- db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
+++ db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
@@ -1,10 +1,14 @@
 class AlterSessionSequenceToCycle < ActiveRecord::Migration[5.2]
   def up
-    change_column :sessions, :id, :bigint
     if ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
-      ActiveRecord::Base.connection.execute(<<-SQL)
-        ALTER SEQUENCE sessions_id_seq MAXVALUE 9223372036854775807 CYCLE;
-      SQL
+      change_column :sessions, :id, :bigint
+      version = Gem::Version.new(ActiveRecord::Base.connection.select_value('SHOW server_version'))
+      if version < Gem::Version.new('10')
+        sql = "ALTER SEQUENCE sessions_id_seq MAXVALUE 9223372036854775807 CYCLE"
+      else
+        sql = "ALTER SEQUENCE sessions_id_seq AS bigint CYCLE"
+      end
+      ActiveRecord::Base.connection.execute(sql)
     end
   end
 
