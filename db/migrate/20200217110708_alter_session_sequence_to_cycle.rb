fe7072a93a8ed5ecb66b0531b216be1bff4e3062
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2020-02-20 15:50:24 +0100

    Refs #29027 - replace server_version with server_version_num

--- db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
+++ db/migrate/20200217110708_alter_session_sequence_to_cycle.rb
@@ -2,8 +2,8 @@ class AlterSessionSequenceToCycle < ActiveRecord::Migration[5.2]
   def up
     if ActiveRecord::Base.connection.adapter_name.downcase == 'postgresql'
       change_column :sessions, :id, :bigint
-      version = Gem::Version.new(ActiveRecord::Base.connection.select_value('SHOW server_version'))
-      if version < Gem::Version.new('10')
+      pg_version = ActiveRecord::Base.connection.select_value('SHOW server_version_num').to_i
+      if pg_version < 100000
         sql = "ALTER SEQUENCE sessions_id_seq MAXVALUE 9223372036854775807 CYCLE"
       else
         sql = "ALTER SEQUENCE sessions_id_seq AS bigint CYCLE"
