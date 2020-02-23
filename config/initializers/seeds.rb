ecf4b33e989e114381d1867d12c4963f1ec0d087
Tomer Brisker <tbrisker@gmail.com>
2020-02-23 15:50:06 +0200

    Fixes #28838, #28931 - Safer seeding on initialization
    Make sure that migartions have run before attempting to run seeds, to
    ensure we don't get into a wierd state of the database if the app is
    started before the db has been migrated.
    Also, catch and log errors during seeding to prevent passanger from
    failing to start if an error occurs.

--- config/initializers/seeds.rb
+++ config/initializers/seeds.rb
@@ -1,5 +1,10 @@
 return unless (ForemanInternal.table_exists? rescue(false)) && !Foreman.in_rake? && !Rails.env.test?
 
+if ActiveRecord::Base.connection.migration_context.needs_migration?
+  Rails.logger.warn("Migrations pending, skipping seeding. Please run `foreman-rake db:migrate` manually.")
+  return
+end
+
 Foreman::Application.configure do |app|
   config.after_initialize do
     seeder = ForemanSeeder.new
@@ -9,5 +14,8 @@ Foreman::Application.configure do |app|
     else
       Rails.logger.info("No new seed file updates found. Skipping")
     end
+  rescue StandardError => e
+    Rails.logger.error("Error while attempting to seed database, please run `foreman-rake db:seed` manually!")
+    Rails.logger.error(e.full_message)
   end
 end
