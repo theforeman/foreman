633f564017cec574933b46bcc585b7363d17f25e
Amir Fefer <amirfefer@gmail.com>
2020-03-23 17:55:30 +0200

    Fixes #27151 - fixing falsy 100% in charts

---
+++ webpack/assets/javascripts/react_app/components/common/charts/DonutChart/DonutChart.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import DonutChart from './';
+
+export default {
+  title: 'Components|Charts/DonutChart',
+  component: DonutChart,
+};
+
+export const donutChart = () => (
+  <DonutChart
+    data={[
+      ['Out of sync hosts', 1],
+      ['Hosts with no reports', 1000],
+    ]}
+  />
+);
