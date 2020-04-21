d5a8266c7ea0b1412c10742b520adff3defe3450
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2020-04-21 14:08:23 +0200

    Fixes #29584 - Preload Puma in production

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -13,6 +13,12 @@ threads ENV.fetch('FOREMAN_PUMA_THREADS_MIN', 0).to_i, ENV.fetch('FOREMAN_PUMA_T
 #
 workers ENV.fetch('FOREMAN_PUMA_WORKERS', 2).to_i
 
+# In clustered mode, Puma can "preload" your application. This loads all the
+# application code prior to forking. Preloading reduces total memory usage of
+# your application via an operating system feature called copy-on-write
+#
+preload_app!
+
 on_worker_boot do
   dynflow = ::Rails.application.dynflow
   dynflow.initialize! unless dynflow.config.lazy_initialization
