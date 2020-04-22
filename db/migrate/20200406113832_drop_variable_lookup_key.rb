5e91b2556aa19388480cf6b8bc5da242be6e9fc3
Ondřej Ezr <oezr@redhat.com>
2020-04-22 15:40:59 +0200

    Refs #28539 - smart variables migration direction

--- db/migrate/20200406113832_drop_variable_lookup_key.rb
+++ db/migrate/20200406113832_drop_variable_lookup_key.rb
@@ -1,5 +1,5 @@
 class DropVariableLookupKey < ActiveRecord::Migration[5.2]
-  def change
+  def up
     variables = LookupKey.where(:type => 'VariableLookupKey')
     LookupValue.where(lookup_key: variables).delete_all
     variables.delete_all
