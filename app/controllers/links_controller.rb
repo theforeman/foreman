b77f63461c8301e4fd1dcef799c0f01dd00d6bca
Ondřej Ezr <oezr@redhat.com>
2020-05-14 00:52:52 +0200

    Fixes #29796 - trends extraction warning

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -10,6 +10,8 @@ class LinksController < ApplicationController
     case type
     when 'manual'
       documentation_url(options['section'], options)
+    when 'plugin_manual'
+      plugin_documentation_url(options['name'])
     when 'feed'
       Setting['rss_url']
     when 'wiki'
@@ -25,6 +27,8 @@ class LinksController < ApplicationController
     end
   end
 
+  private
+
   def documentation_url(section = "", options = {})
     root_url = options[:root_url] || "https://theforeman.org/manuals/#{SETTINGS[:version].short}/index.html#"
     if section.empty?
@@ -32,6 +36,12 @@ class LinksController < ApplicationController
     else
       root_url + section
     end
+  end
+
+  def plugin_documentation_url(plugin_name, version: nil, options: {})
+    root_url = options[:root_url] || "https://theforeman.org/plugins"
+    path = version ? "#{plugin_name}/#{version}" : plugin_name
+    "#{root_url}/#{path}"
   end
 
   def wiki_url(section: '')
