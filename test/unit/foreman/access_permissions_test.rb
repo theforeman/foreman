140039fcc674c441c0285eac9429a3b492d58c38
Tomer Brisker <tbrisker@gmail.com>
2020-05-19 12:10:30 +0300

    Fixes #29872 - Only load used Rails frameworks

--- test/unit/foreman/access_permissions_test.rb
+++ test/unit/foreman/access_permissions_test.rb
@@ -49,21 +49,6 @@ class AccessPermissionsTest < ActiveSupport::TestCase
     "api/v2/table_preferences/index", "api/v2/table_preferences/show", "api/v2/table_preferences/create",
     "api/v2/table_preferences/update", "api/v2/table_preferences/destroy",
 
-    # skip Active Storage controllers
-    # https://github.com/rails/rails/issues/31228
-    "active_storage/blobs/show", "active_storage/direct_uploads/create", "active_storage/disk/show",
-    "active_storage/disk/update", "active_storage/previews/show", "active_storage/representations/show",
-    "active_storage/variants/show",
-
-    # skip Active Mailbox controllers
-    "action_mailbox/ingresses/mailgun/inbound_emails/create", "action_mailbox/ingresses/mandrill/inbound_emails/create",
-    "action_mailbox/ingresses/postmark/inbound_emails/create", "action_mailbox/ingresses/relay/inbound_emails/create",
-    "action_mailbox/ingresses/sendgrid/inbound_emails/create", "rails/conductor/action_mailbox/inbound_emails/create",
-    "rails/conductor/action_mailbox/inbound_emails/destroy", "rails/conductor/action_mailbox/inbound_emails/edit",
-    "rails/conductor/action_mailbox/inbound_emails/index", "rails/conductor/action_mailbox/inbound_emails/new",
-    "rails/conductor/action_mailbox/inbound_emails/show", "rails/conductor/action_mailbox/inbound_emails/update",
-    "rails/conductor/action_mailbox/reroutes/create",
-
     # graphql
     "api/graphql/execute",
 
