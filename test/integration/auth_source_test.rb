5a27fa8a3f7f21548e6a30e666bbab3017855b48
Rahul Bajaj <rahulrb0509@gmail.com>
2020-05-29 09:59:28 +0000

    Fixes #29958 - fix name convention for auth sources tab

--- test/integration/auth_source_test.rb
+++ test/integration/auth_source_test.rb
@@ -2,6 +2,6 @@ require 'integration_test_helper'
 
 class AuthSourcesIntegrationTest < ActionDispatch::IntegrationTest
   test "index page" do
-    assert_index_page(auth_sources_path, "Authentication sources", nil, false, true)
+    assert_index_page(auth_sources_path, "Authentication Sources", nil, false, true)
   end
 end
