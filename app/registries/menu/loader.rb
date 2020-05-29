5509543c0b2d326f794b49f895b54b96f2f70afb
Rahul Bajaj <rahulrb0509@gmail.com>
2020-05-29 09:59:28 +0000

    Fixes #29958 - fix name convention for auth sources tab

--- app/registries/menu/loader.rb
+++ app/registries/menu/loader.rb
@@ -25,7 +25,7 @@ module Menu
         menu.sub_menu :administer_menu,  :caption => N_('Administer'), :icon => 'fa fa-cog' do
           menu.item :locations,          :caption => N_('Locations') if SETTINGS[:locations_enabled]
           menu.item :organizations,      :caption => N_('Organizations') if SETTINGS[:organizations_enabled]
-          menu.item :auth_sources,       :caption => N_('Authentication sources')
+          menu.item :auth_sources,       :caption => N_('Authentication Sources')
           menu.item :users,              :caption => N_('Users')
           menu.item :usergroups,         :caption => N_('User Groups')
           menu.item :roles,              :caption => N_('Roles')
