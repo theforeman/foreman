bc7e319eaba68e8fcc3672ad21bf6f3ef066711c
Suraj Patil <patilsuraj767@gmail.com>
2020-06-07 13:55:45 +0000

    Refs #30034 - Fixed the linting issue

--- webpack/assets/javascripts/react_app/components/LoginPage/components/LoginPageCaption.js
+++ webpack/assets/javascripts/react_app/components/LoginPage/components/LoginPageCaption.js
@@ -1,13 +1,17 @@
 import React, { Fragment } from 'react';
+import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import { translate as __ } from '../../../common/I18n';
-import ReactMarkdown from 'react-markdown'
 
 const LoginPageCaption = ({ version, caption }) => (
   <Fragment>
     <h1 id="title">{__('Welcome')}</h1>
     {version && <p id="version">{`${__('Version')} ${version}`}</p>}
-    {caption && <p id="login_text"><ReactMarkdown source={caption} /></p>}
+    {caption && (
+      <p id="login_text">
+        <ReactMarkdown source={caption} />
+      </p>
+    )}
   </Fragment>
 );
 
