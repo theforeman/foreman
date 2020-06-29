c4f9af7cf6ee94de3b6fccb7d852bc782924503c
Oleh Fedorenko <ofedoren@redhat.com>
2020-06-29 15:48:36 +0000

    Fixes #29865 - Add DSL docs for inputs macros

--- lib/foreman/renderer/scope/macros/inputs.rb
+++ lib/foreman/renderer/scope/macros/inputs.rb
@@ -4,7 +4,19 @@ module Foreman
       module Macros
         module Inputs
           include Foreman::Renderer::Errors
+          extend ApipieDSL::Module
 
+          apipie :class, 'Macros related to template inputs' do
+            name 'Template Inputs'
+            sections only: %w[all reports provisioning jobs partition_tables]
+          end
+
+          apipie :method, 'Returns the value of template input' do
+            required :name, String, desc: 'name of the template input'
+            raises error: UndefinedInput, desc: 'when there is no input with such name defined for the current template'
+            returns Object, desc: 'The value of template input'
+            example 'input("Include Facts") #=> "yes"'
+          end
           def input(name)
             input = template.template_inputs&.find_by_name(name)
             if input
