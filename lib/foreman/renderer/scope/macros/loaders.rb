a48c85ff9503316bc3ed61f9d37c1a595376d0f2
Oleh Fedorenko <ofedoren@redhat.com>
2020-06-29 17:18:52 +0000

    Fixes #29864 - Add DSL docs for loaders

--- lib/foreman/renderer/scope/macros/loaders.rb
+++ lib/foreman/renderer/scope/macros/loaders.rb
@@ -4,6 +4,22 @@ module Foreman
       module Macros
         module Loaders
           include Foreman::Renderer::Errors
+          extend ApipieDSL::Class
+
+          apipie :class, desc: 'Loader macros to use within a template' do
+            name 'Loaders'
+            sections only: %w[all reports]
+          end
+
+          apipie :param_group, name: :load_resource_keywords do
+            keyword :search, String, desc: 'A search term to limit the resulting collection, using standard search syntax', default: ''
+            keyword :includes, Array, of: [String, Symbol], desc: 'An array of associations represented by strings or symbols, to be included in the SQL query. The list can be extended
+              from plugins and can not be fully documented here. Most used associations are :puppetclasses, :host_statuses, :fact_names, :interfaces, :domain, :subnet', default: nil
+            keyword :preload, Array, desc: 'Same as includes but using preload', default: nil
+            keyword :joins, Array, desc: 'An array of associations repesented by strings or symbols, to be included in the SQL query. The list can be extended from plugins and can not be
+              fully documented here. Joined associations can be then used in select keyword and will be loaded into instantiated object', default: nil
+            keyword :select, Array, desc: 'An array of columns (object attributes) to be loaded. By default, all columns are loaded, however you may specify subset of columns you need', default: nil
+          end
 
           LOADERS = [
             [:load_organizations, Organization, :view_organizations],
@@ -20,6 +36,22 @@ module Foreman
           ]
 
           LOADERS.each do |name, model, permission|
+            apipie :method, "Loads #{model} objects" do
+              desc "This macro returns a collection of #{model.to_s.pluralize} matching search criteria.
+                The collection is limited to what objects the current user is authorized to view by #{permission} permission. Also it's loaded in bulk
+                of 1 000 records."
+              param_group :load_resource_keywords
+              returns array_of: model, desc: 'The collection that can be iterated over using each_record'
+              example "<% #{name}.each_record do |#{model.to_s.downcase}| %>
+  <%= #{model.to_s.downcase}.id %>, <%= #{model.to_s.downcase}.#{model == User ? 'login' : 'name'} %>
+<% end %>", desc: "Prints #{model.to_s.downcase} id and #{model == User ? 'login' : 'name'} of each #{model.to_s.downcase}"
+              example_for :load_hosts, "<% load_hosts(search: 'domain = example.com', includes: [ :interfaces ]).each_record do |host| %>
+  <%= host.name %>, <%= host.interfaces.map { |i| i.mac }.join(', ') %>
+<% end %>", desc: 'Prints host name and MACs of each host'
+              example_for :load_users, "<% load_users(search: 'location = Europe', includes: :auth_source).each_record do |user| %>
+  <%= user.login %>, <%= user.last_login_on %>, <%= user_auth_source_name(user) %>
+<% end %>", desc: "Prints users in Europe, their login, when a user was logged on for the last time and the authentication source"
+            end
             define_method name do |search: '', includes: nil, preload: nil, joins: nil, select: nil, batch: 1_000, limit: nil|
               load_resource(klass: model, search: search, permission: permission, includes: includes, preload: preload, joins: joins, select: select, batch: batch, limit: limit)
             end
