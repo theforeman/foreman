082cabde90d38dbdeb341358f1619dd0ed46e1eb
Ondřej Ezr <oezr@redhat.com>
2020-07-04 15:09:36 +0200

    Refs #30098 - drop trends import

--- db/migrate/20150202094307_add_range_to_trend_counters.rb
+++ db/migrate/20150202094307_add_range_to_trend_counters.rb
@@ -2,7 +2,5 @@ class AddRangeToTrendCounters < ActiveRecord::Migration[4.2]
   def change
     add_column :trend_counters, :interval_start, :datetime
     add_column :trend_counters, :interval_end, :datetime
-
-    Rake::Task['trends:reduce'].invoke
   end
 end
