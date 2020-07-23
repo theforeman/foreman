d74749d4abe7057640d28ef4fa78506d5f1dfca1
Ondřej Ezr <oezr@redhat.com>
2020-07-23 09:27:19 +0200

    Fixes #30319 - Taxonomy scope for AuthSources#index

--- app/controllers/auth_sources_controller.rb
+++ app/controllers/auth_sources_controller.rb
@@ -2,10 +2,14 @@ class AuthSourcesController < ApplicationController
   def index
     @auth_sources = AuthSource.except_hidden
     @users = User.except_hidden.includes(:auth_source)
-    @auth_source_ldaps = resource_base_search_and_page.only_ldap
+    @auth_source_ldaps = resource_base_search_and_page
   end
 
   private
+
+  def model_of_controller
+    @model_of_controller ||= AuthSourceLdap
+  end
 
   def controller_permission
     'authenticators'
