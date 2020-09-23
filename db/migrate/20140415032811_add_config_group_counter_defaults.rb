272393b126d26a33547fe80963621dfd0b0ee240
Shira Maximov <shiramaximov@gmail.com>
2020-09-23 14:35:17 +0300

    Fixes #31095 - Remove config groups model
    Added to the plugin in https://github.com/theforeman/foreman_puppet_enc/pull/12

--- db/migrate/20140415032811_add_config_group_counter_defaults.rb
+++ db/migrate/20140415032811_add_config_group_counter_defaults.rb
@@ -3,17 +3,11 @@ class AddConfigGroupCounterDefaults < ActiveRecord::Migration[4.2]
     change_column :config_groups, :hosts_count, :integer, :default => 0
     change_column :config_groups, :hostgroups_count, :integer, :default => 0
     change_column :config_groups, :config_group_classes_count, :integer, :default => 0
-    ConfigGroup.unscoped.where(:hosts_count => nil).update_all(:hosts_count => 0)
-    ConfigGroup.unscoped.where(:hostgroups_count => nil).update_all(:hostgroups_count => 0)
-    ConfigGroup.unscoped.where(:config_group_classes_count => nil).update_all(:config_group_classes_count => 0)
   end
 
   def down
     change_column :config_groups, :hosts_count, :integer
     change_column :config_groups, :hostgroups_count, :integer
     change_column :config_groups, :config_group_classes_count, :integer
-    ConfigGroup.unscoped.where(:hosts_count => 0).update_all(:hosts_count => nil)
-    ConfigGroup.unscoped.where(:hostgroups_count => 0).update_all(:hostgroups_count => nil)
-    ConfigGroup.unscoped.where(:config_group_classes_count => 0).update_all(:config_group_classes_count => nil)
   end
 end
