5546d0013a66bf9985bdd940f6b85a5f67c61d70
Ondřej Ezr <oezr@redhat.com>
2020-09-28 08:18:26 +0200

    Refs #31007 - drop OverrideValues API
    Drops OverrideValues API tangled with Puppet, but leaves common partials,
    these can be used by all the plugins.
    Details of extraction in https://github.com/theforeman/foreman_puppet_enc/pull/32

--- app/controllers/api/v2/override_values_controller.rb
+++ app/controllers/api/v2/override_values_controller.rb
@@ -2,94 +2,37 @@ module Api
   module V2
     class OverrideValuesController < V2::BaseController
       include Api::Version2
-      include Api::V2::LookupKeysCommonController
-      include Foreman::Controller::Parameters::LookupValue
+      prepend_before_action :fail_and_inform_about_plugin
 
-      before_action :find_override_values
-      before_action :find_override_value, :only => [:show, :update, :destroy]
-      # override return_if_smart_mismatch in LookupKeysCommonController to add :create
-      before_action :return_if_smart_mismatch, :only => [:create, :show, :update, :destroy]
-      before_action :return_if_override_mismatch, :only => [:show, :update, :destroy]
-
-      before_action :cast_value, :only => [:create, :update]
+      resource_description do
+        desc 'This resource has been deprecated, to continue using it please install Foreman Puppet Enc plugin and use its API enpoints.'
+        deprecated true
+      end
 
       api :GET, "/smart_class_parameters/:smart_class_parameter_id/override_values", N_("List of override values for a specific smart class parameter")
-      param :smart_class_parameter_id, :identifier, :required => false
-      param :show_hidden, :bool, :desc => N_("Display hidden values")
-      param_group :pagination, ::Api::V2::BaseController
-
       def index
       end
 
       api :GET, "/smart_class_parameters/:smart_class_parameter_id/override_values/:id", N_("Show an override value for a specific smart class parameter")
-      param :smart_class_parameter_id, :identifier, :required => false
-      param :id, :identifier, :required => true
-      param :show_hidden, :bool, :desc => N_("Display hidden values")
-
       def show
       end
 
-      def_param_group :override_value do
-        param :override_value, Hash, :required => true, :action_aware => true do
-          param :match, String, :required => true, :desc => N_("Override match")
-          param :value, :any_type, :of => LookupKey::KEY_TYPES, :required => false, :desc => N_("Override value, required if omit is false")
-          param :omit, :bool, :required => false, :desc => N_("Foreman will not send this parameter in classification output")
-        end
-      end
-
       api :POST, "/smart_class_parameters/:smart_class_parameter_id/override_values", N_("Create an override value for a specific smart class parameter")
-      param :smart_class_parameter_id, :identifier, :required => false
-      param_group :override_value, :as => :create
-
       def create
-        @override_value = @smart_class_parameter.lookup_values.create!(lookup_value_params)
-        @smart_class_parameter.update_attribute(:override, true)
-        process_response @override_value
       end
 
       api :PUT, "/smart_class_parameters/:smart_class_parameter_id/override_values/:id", N_("Update an override value for a specific smart class parameter")
-      param :smart_class_parameter_id, :identifier, :required => false
-      param_group :override_value
-
       def update
-        @override_value.update!(lookup_value_params)
-        render 'api/v2/override_values/show'
       end
 
       api :DELETE, "/smart_class_parameters/:smart_class_parameter_id/override_values/:id", N_("Delete an override value for a specific smart class parameter")
-      param :smart_class_parameter_id, :identifier, :required => false
-      param :id, :identifier, :required => true
-
       def destroy
-        @override_value.destroy
-        render 'api/v2/override_values/show'
       end
 
       private
 
-      def find_override_values
-        if @smart_class_parameter
-          @override_values = @smart_class_parameter.lookup_values.paginate(paginate_options)
-          @total = @override_values.count
-        end
-      end
-
-      def find_override_value
-        @override_value = LookupValue.find_by_id(params[:id])
-        if @smart_class_parameter
-          @override_value ||= @smart_class_parameter.lookup_values.friendly.find(params[:id])
-        end
-      end
-
-      def return_if_override_mismatch
-        if (@override_values && @override_value && !@override_values.find_by_id(@override_value.id)) || (@override_values && !@override_value) || !@override_values
-          not_found "Override value not found by id '#{params[:id]}'"
-        end
-      end
-
-      # overwrite Api::BaseController
-      def resource_class
-        LookupValue
+      def fail_and_inform_about_plugin
+        render json: { message: _('To access SmartClassParameters API you need to install Foreman Puppet Enc plugin') }, status: :not_implemented
       end
     end
   end
