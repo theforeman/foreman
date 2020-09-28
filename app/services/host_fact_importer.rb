478fd1ed6e59728db2d5a1e7fdb21658b31f6793
Ondřej Ezr <oezr@redhat.com>
2020-09-28 23:15:54 +0200

    Fixes #30926 - move parse_facts to HostFactImporter
    Move `parse_facts` method to `HostFactImporter` to isolate fact population from Host class.
    Followup for bb84e41d391ba1b18dc9e2db8f0cbabf92a3b1e8

--- app/services/host_fact_importer.rb
+++ app/services/host_fact_importer.rb
@@ -35,12 +35,54 @@ class HostFactImporter
   end
 
   def parse_facts(facts, type, source_proxy)
+    time = facts[:_timestamp]
+    time = time.to_time if time.is_a?(String)
+    host.last_compile = time if time
+
+    # taxonomy must be set before populate_fields_from_facts call
+    set_host_taxonomies(facts)
+
     skipping_orchestration do
-      host.parse_facts facts, type, source_proxy
+      unless host.build?
+        parser = FactParser.parser_for(type).new(facts)
+
+        telemetry_duration_histogram(:importer_facts_import_duration, 1000, type: type) do
+          host.populate_fields_from_facts(parser, type, source_proxy)
+        end
+      end
+
+      # we are saving here with no validations, as we want this process to be as fast
+      # as possible, assuming we already have all the right settings in Foreman.
+      # If we don't (e.g. we never install the server via Foreman, we populate the fields from facts
+      # TODO: if it was installed by Foreman and there is a mismatch,
+      # we should probably send out an alert.
+      host.save(:validate => false)
     end
   end
 
   private
+
+  def set_host_taxonomies(facts)
+    ['location', 'organization'].each do |taxonomy|
+      taxonomy_class = taxonomy.classify.constantize
+      taxonomy_fact = Setting["#{taxonomy}_fact"]
+
+      if taxonomy_fact.present? && facts.key?(taxonomy_fact)
+        taxonomy_from_fact = taxonomy_class.find_by_title(facts[taxonomy_fact].to_s)
+      else
+        default_taxonomy = taxonomy_class.find_by_title(Setting["default_#{taxonomy}"])
+      end
+
+      if host.send(taxonomy).present?
+        # Change taxonomy to fact taxonomy if set, otherwise leave it as is
+        host.send("#{taxonomy}=", taxonomy_from_fact) unless taxonomy_from_fact.nil?
+      else
+        # No taxonomy was set, set to fact taxonomy or default taxonomy
+        host.send("#{taxonomy}=", (taxonomy_from_fact || default_taxonomy))
+      end
+      taxonomy_class.current = host.send(taxonomy)
+    end
+  end
 
   def skipping_orchestration
     if host.is_a?(Host::Managed)
