8149ab0bfe13c51a768240a31c2934debbe4ddbc
Ondřej Ezr <oezr@redhat.com>
2020-09-28 04:28:17 +0200

    Refs #31007 - drop SmartClassParameters API
    States the API deprecated and directs users to using a plugin.
    Details of extraction https://github.com/theforeman/foreman_puppet_enc/pull/30

--- app/controllers/api/v2/smart_class_parameters_controller.rb
+++ app/controllers/api/v2/smart_class_parameters_controller.rb
@@ -2,10 +2,12 @@ module Api
   module V2
     class SmartClassParametersController < V2::BaseController
       include Api::Version2
-      include Api::V2::LookupKeysCommonController
-      include Foreman::Controller::Parameters::PuppetclassLookupKey
+      prepend_before_action :fail_and_inform_about_plugin
 
-      alias_method :resource_scope, :smart_class_parameters_resource_scope
+      resource_description do
+        desc 'This resource has been deprecated, to continue using it please install Foreman Puppet Enc plugin and use its API enpoints.'
+        deprecated true
+      end
 
       api :GET, "/smart_class_parameters", N_("List all smart class parameters")
       api :GET, "/hosts/:host_id/smart_class_parameters", N_("List of smart class parameters for a specific host")
@@ -13,56 +15,20 @@ module Api
       api :GET, "/puppetclasses/:puppetclass_id/smart_class_parameters", N_("List of smart class parameters for a specific Puppet class")
       api :GET, "/environments/:environment_id/smart_class_parameters", N_("List of smart class parameters for a specific environment")
       api :GET, "/environments/:environment_id/puppetclasses/:puppetclass_id/smart_class_parameters", N_("List of smart class parameters for a specific environment/Puppet class combination")
-      param :host_id, :identifier, :required => false
-      param :hostgroup_id, :identifier, :required => false
-      param :puppetclass_id, :identifier, :required => false
-      param :environment_id, :identifier, :required => false
-      param :show_hidden, :bool, :desc => N_("Display hidden values")
-      param_group :search_and_pagination, ::Api::V2::BaseController
-      add_scoped_search_description_for(PuppetclassLookupKey)
-
       def index
       end
 
-      # no create action for API
-      # smart class parameters are imported by PuppetClassImporter
-
       api :GET, "/smart_class_parameters/:id/", N_("Show a smart class parameter")
-      param :id, :identifier, :required => true
-      param :show_hidden, :bool, :desc => N_("Display hidden values")
 
       def show
       end
 
       api :PUT, "/smart_class_parameters/:id", N_("Update a smart class parameter")
-      param :id, :identifier, :required => true
-      param :smart_class_parameter, Hash, :required => true do
-        # can't update parameter/key name for :parameter, String, :required => true
-        param :override, :bool, :desc => N_("Whether the smart class parameter value is managed by Foreman")
-        param :description, String, :desc => N_("Description of smart class")
-        param :default_value, :any_type, :of => LookupKey::KEY_TYPES, :desc => N_("Value to use when there is no match")
-        param :hidden_value, :bool, :desc => N_("When enabled the parameter is hidden in the UI")
-        param :omit, :bool, :desc => N_("Foreman will not send this parameter in classification output. Puppet will use the value defined in the Puppet manifest for this parameter")
-        param :path, String, :desc => N_("The order in which values are resolved")
-        param :validator_type, LookupKey::VALIDATOR_TYPES, :desc => N_("Types of validation values")
-        param :validator_rule, String, :desc => N_("Used to enforce certain values for the parameter values")
-        param :override_value_order, String, :desc => N_("The order in which values are resolved")
-        param :parameter_type, LookupKey::KEY_TYPES, :desc => N_("Types of variable values")
-        param :required, :bool, :desc => N_("If true, will raise an error if there is no default value and no matcher provide a value")
-        param :merge_overrides, :bool, :desc => N_("Merge all matching values (only array/hash type)")
-        param :merge_default, :bool, :desc => N_("Include default value when merging all matching values")
-        param :avoid_duplicates, :bool, :desc => N_("Remove duplicate values (only array type)")
-      end
-
       def update
-        # Note:  User must manually set :override => true. It is not automatically updated if optional input validator fields are updated.
-        @smart_class_parameter.update!(puppetclass_lookup_key_params)
-        render 'api/v2/smart_class_parameters/show'
       end
 
-      # overwrite Api::BaseController
-      def resource_class
-        LookupKey
+      def fail_and_inform_about_plugin
+        render json: { message: _('To access SmartClassParameters API you need to install Foreman Puppet Enc plugin') }, status: :not_implemented
       end
     end
   end
--- app/views/api/v2/smart_class_parameters/index.json.rabl
+++
@@ -1,3 +0,0 @@
-collection @smart_class_parameters
-
-extends "api/v2/smart_class_parameters/main"
