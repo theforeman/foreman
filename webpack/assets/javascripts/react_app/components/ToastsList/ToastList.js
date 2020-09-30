7d5ad5366e229b98b59ab8299a2463b310736585
MariaAga <mariaagaphonchev@gmail.com>
2020-09-30 15:02:44 +0300

    Fixes #30936 - Toast should have fallback type

--- webpack/assets/javascripts/react_app/components/ToastsList/ToastList.js
+++ webpack/assets/javascripts/react_app/components/ToastsList/ToastList.js
@@ -3,10 +3,30 @@ import {
   ToastNotificationList,
   ToastNotification,
   TimedToastNotification,
+  Alert,
 } from 'patternfly-react';
 import PropTypes from 'prop-types';
 import { noop } from '../../common/helpers';
 import AlertBody from '../common/Alert/AlertBody';
+
+const toastType = type => {
+  if (Alert.ALERT_TYPES.includes(type)) return type;
+  const message = `Toast notification type '${type}' is invalid. Please use one of the following types: ${Alert.ALERT_TYPES}`;
+  switch (type) {
+    case 'alert':
+      // eslint-disable-next-line no-console
+      console.warn(message);
+      return 'warning';
+    case 'notice':
+      // eslint-disable-next-line no-console
+      console.warn(message);
+      return 'info';
+    default:
+      // eslint-disable-next-line no-console
+      console.error(message);
+      return 'info';
+  }
+};
 
 const ToastsList = ({ messages, deleteToast }) => {
   const toastsList = Object.entries(messages)
@@ -15,12 +35,12 @@ const ToastsList = ({ messages, deleteToast }) => {
       const ToastComponent = sticky
         ? ToastNotification
         : TimedToastNotification;
-
       return (
         <ToastComponent
           key={key}
           onDismiss={() => deleteToast(key)}
           {...toastProps}
+          type={toastType(toastProps.type)}
         >
           <AlertBody link={link} message={message} />
         </ToastComponent>
