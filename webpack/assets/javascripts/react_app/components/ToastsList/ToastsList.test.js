f2f0b3d7189fecee7e9d49491a324e19bc13b53b
MariaAga <mariaagaphonchev@gmail.com>
2020-09-30 15:02:44 +0300

    Fixes #30936 - Toast should have fallback type

--- webpack/assets/javascripts/react_app/components/ToastsList/ToastsList.test.js
+++ webpack/assets/javascripts/react_app/components/ToastsList/ToastsList.test.js
@@ -8,6 +8,8 @@ import {
   singleMessageState,
   singleMessageWithLinkState,
   multipleMessagesState,
+  errorMessageState,
+  warnMessageState,
 } from './ToastList.fixtures';
 
 import ToastList from './';
@@ -39,4 +41,30 @@ describe('ToastList', () => {
       state: statesToTest[key],
     }))
     .forEach(testToastListRenderWithState);
+
+  it('Should show error for invalid type', () => {
+    const mockError = jest.fn();
+    // eslint-disable-next-line no-console
+    console.error = mockError;
+    const store = mockStore(errorMessageState);
+    mount(<ToastList store={store} />);
+    expect(mockError).toBeCalledWith(
+      "Toast notification type 'random' is invalid. Please use one of the following types: error,warning,success,info"
+    );
+    // eslint-disable-next-line no-console
+    console.error.mockRestore();
+  });
+
+  it('Should show warn for notice type', () => {
+    const mockWarn = jest.fn();
+    // eslint-disable-next-line no-console
+    console.warn = mockWarn;
+    const store = mockStore(warnMessageState);
+    mount(<ToastList store={store} />);
+    expect(mockWarn).toBeCalledWith(
+      "Toast notification type 'notice' is invalid. Please use one of the following types: error,warning,success,info"
+    );
+    // eslint-disable-next-line no-console
+    console.warn.mockRestore();
+  });
 });
