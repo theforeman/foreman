8c3d16e0a6dd890c88c79b4ab51dc8298f7fef64
Ondřej Ezr <oezr@redhat.com>
2020-10-13 21:15:02 +0200

    Fixes #31060 - drop Environments migrations
    Drops all migrations related to environments.
    Removes all the relations to the environments.
    Adds a migration to drop environments from Hostgroup and Host.
    
    Extracted in https://github.com/theforeman/foreman_puppet_enc/pull/41.

--- db/migrate/20150606065713_add_sti_to_lookup_keys.rb
+++ db/migrate/20150606065713_add_sti_to_lookup_keys.rb
@@ -2,18 +2,10 @@ class AddStiToLookupKeys < ActiveRecord::Migration[4.2]
   def up
     add_column :lookup_keys, :type, :string, limit: 255
     add_index :lookup_keys, :type
-
-    remove_foreign_key :environment_classes, :lookup_keys if foreign_key_exists?(:environment_classes, :lookup_keys)
-    rename_column :environment_classes, :lookup_key_id, :puppetclass_lookup_key_id
-    add_foreign_key :environment_classes, :lookup_keys, :column => :puppetclass_lookup_key_id, :name => "environment_classes_lookup_key_id_fk"
   end
 
   def down
     LookupKey.where(:type => "PuppetclassLookupKey").update_all(:is_param => true)
     remove_column :lookup_keys, :type
-
-    remove_foreign_key :environment_classes, :lookup_keys if foreign_key_exists?(:environment_classes, :lookup_keys)
-    rename_column :environment_classes, :puppetclass_lookup_key_id, :lookup_key_id
-    add_foreign_key :environment_classes, :lookup_keys
   end
 end
