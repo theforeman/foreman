fca200ae9057408e64745436896b92e6ef484b53
Rahul Bajaj <rahulrb0509@gmail.com>
2020-10-14 14:47:30 +0000

    Fixes #30417 - add pf4 emptystate component

--- webpack/assets/javascripts/react_app/components/common/EmptyState/EmptyStatePropTypes.js
+++ webpack/assets/javascripts/react_app/components/common/EmptyState/EmptyStatePropTypes.js
@@ -9,20 +9,21 @@ export const actionButtonPropTypes = {
 export const emptyStatePatternPropTypes = {
   icon: PropTypes.string.isRequired,
   header: PropTypes.string.isRequired,
+  documentation: PropTypes.oneOfType([
+    PropTypes.shape({
+      label: PropTypes.string,
+      buttonLabel: PropTypes.string,
+      url: PropTypes.string.isRequired,
+    }),
+    PropTypes.node,
+  ]),
   description: PropTypes.string.isRequired,
-  documentation: PropTypes.node,
   action: PropTypes.node,
   secondaryActions: PropTypes.node,
 };
 
 export const defaultEmptyStatePropTypes = {
   ...emptyStatePatternPropTypes,
-  icon: PropTypes.string,
-  documentation: PropTypes.shape({
-    label: PropTypes.string,
-    buttonLabel: PropTypes.string,
-    url: PropTypes.string.isRequired,
-  }),
   action: PropTypes.shape(actionButtonPropTypes),
   secondaryActions: PropTypes.arrayOf(PropTypes.shape(actionButtonPropTypes)),
 };
