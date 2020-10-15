b79fb6d3e723e8b3acfa0ed86f202577851517e8
Ondřej Ezr <oezr@redhat.com>
2020-10-16 01:05:11 +0200

    Refs #31059 - drop environments from nic
    Removes uses of Environment from Nic::Managed model

--- app/models/nic/managed.rb
+++ app/models/nic/managed.rb
@@ -25,8 +25,8 @@ module Nic
     # this ensures our orchestration works on both a host and a managed interface
     delegate :capabilities, :compute_resource, :operatingsystem, :provisioning_template, :jumpstart?, :build, :build?, :os, :arch,
       :image_build?, :pxe_build?, :pxe_build?, :token, :model, :to => :host
-    delegate :operatingsystem_id, :hostgroup_id, :environment_id,
-      :overwrite?, :skip_orchestration?, :skip_orchestration!, :to => :host, :allow_nil => true
+    delegate :operatingsystem_id, :hostgroup_id, :overwrite?,
+      :skip_orchestration?, :skip_orchestration!, :to => :host, :allow_nil => true
 
     attr_exportable :ip, :ip6, :mac, :name, :attrs, :virtual, :link, :identifier, :managed,
       :primary, :provision, :subnet, :subnet6,
