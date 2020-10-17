1a9e19924fba0675e3a7264885d108d00255f630
Ondřej Ezr <oezr@redhat.com>
2020-10-17 19:23:18 +0200

    Fixes #31058 - drop Environments views and helpers
    Drop templates, helpers and host action from multiple_actions menu.
    
    Extracted in https://github.com/theforeman/foreman_puppet_enc/pull/45

--- app/helpers/environments_helper.rb
+++
@@ -1,9 +0,0 @@
-module EnvironmentsHelper
-  include PuppetclassesAndEnvironmentsHelper
-
-  def environments_title_actions
-    title_actions import_proxy_select(hash_for_import_environments_environments_path),
-      button_group(new_link(_('Create Puppet Environment'))),
-      button_group(help_button)
-  end
-end
