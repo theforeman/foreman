16bbfde8147b2acddb0b480bcced98d4f6c660cb
Ondřej Ezr <oezr@redhat.com>
2020-10-18 23:47:11 +0200

    Fixes #31057 - drop Environment UI logic
    Drops UI controller and routes for Environment.
    
    Extracted in:
    - https://github.com/theforeman/foreman_puppet_enc/pull/46
    - https://github.com/theforeman/foreman_puppet_enc/pull/52

--- test/integration/environment_js_test.rb
+++
@@ -1,7 +0,0 @@
-require 'integration_test_helper'
-
-class EnvironmentJSTest < IntegrationTestWithJavascript
-  test "index page" do
-    assert_index_page(environments_path, "Environments", "Create Puppet Environment")
-  end
-end
