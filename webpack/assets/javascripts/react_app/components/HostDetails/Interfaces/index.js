ad2ff59966d9fee7ce4883adaef65b1888d07ab7
Amir Fefer <amirfefer@gmail.com>
2020-10-22 12:31:10 +0300

    Fixes #30987 - Add card template extension to host details page

--- webpack/assets/javascripts/react_app/components/HostDetails/Interfaces/index.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Interfaces/index.js
@@ -7,6 +7,7 @@ import {
   DataListItemCells,
   DataListCell,
   Card,
+  CardTitle,
   CardBody,
   Accordion,
   AccordionItem,
@@ -28,126 +29,128 @@ const InterfacesCard = ({ interfaces }) => {
   };
   return (
     <Card isHoverable>
-      <CardBody>{__('Interfaces')}</CardBody>
-      <Accordion asDefinitionList>
-        {!interfaces.length && (
-          <div style={{ marginLeft: '20px' }}>
-            <Skeleton count={3} width={200} />
-          </div>
-        )}
-        {interfaces.map(Interface => (
-          <AccordionItem key={Interface.id}>
-            <AccordionToggle
-              onClick={() => {
-                onToggle(Interface.identifier);
-              }}
-              isExpanded={ativeAccordion === Interface.identifier}
-              id={Interface.identifier}
-            >
-              <span>
-                {Interface.primary && <StarIcon />} {Interface.identifier}
-              </span>
-            </AccordionToggle>
-            <AccordionContent
-              id={Interface.identifier}
-              isHidden={ativeAccordion !== Interface.identifier}
-            >
-              <DataList isCompact aria-label="Interfaces">
-                <DataListItem aria-labelledby="ip">
-                  <DataListItemRow>
-                    <DataListItemCells
-                      dataListCells={[
-                        <DataListCell key={`${Interface.identifier}-ip`}>
-                          <span> {__('IP Address')}</span>
-                        </DataListCell>,
-                        <DataListCell
-                          key={`${Interface.identifier}-ip-content`}
-                        >
-                          {Interface.ip}
-                        </DataListCell>,
-                      ]}
-                    />
-                  </DataListItemRow>
-                </DataListItem>
-                <DataListItem aria-labelledby="ip6">
-                  <DataListItemRow>
-                    <DataListItemCells
-                      dataListCells={[
-                        <DataListCell key={`${Interface.identifier}-ip6`}>
-                          <span>{__('IP6 Address')}</span>
-                        </DataListCell>,
-                        <DataListCell
-                          key={`${Interface.identifier}-ip6-content`}
-                        >
-                          {Interface.ip6}
-                        </DataListCell>,
-                      ]}
-                    />
-                  </DataListItemRow>
-                </DataListItem>
-                <DataListItem aria-labelledby="fqdn">
-                  <DataListItemRow>
-                    <DataListItemCells
-                      dataListCells={[
-                        <DataListCell key={`${Interface.identifier}-fqdn`}>
-                          <span>{__('FQDN')}</span>
-                        </DataListCell>,
-                        <DataListCell
-                          key={`${Interface.identifier}-fqdn-content`}
-                        >
-                          {Interface.fqdn}
-                        </DataListCell>,
-                      ]}
-                    />
-                  </DataListItemRow>
-                </DataListItem>
-                <DataListItem aria-labelledby="type">
-                  <DataListItemRow>
-                    <DataListItemCells
-                      dataListCells={[
-                        <DataListCell
-                          isFilled
-                          key={`${Interface.identifier}-type`}
-                        >
-                          <span>{__('Type')}</span>
-                        </DataListCell>,
-                        <DataListCell
-                          isFilled
-                          alignRight
-                          key={`${Interface.identifier}-type-content`}
-                        >
-                          {Interface.type}
-                        </DataListCell>,
-                      ]}
-                    />
-                  </DataListItemRow>
-                </DataListItem>
-                <DataListItem aria-labelledby="mac">
-                  <DataListItemRow>
-                    <DataListItemCells
-                      dataListCells={[
-                        <DataListCell
-                          isFilled
-                          key={`${Interface.identifier}-mac`}
-                        >
-                          <span>MAC</span>
-                        </DataListCell>,
-                        <DataListCell
-                          isFilled
-                          alignRight
-                          key={`${Interface.identifier}-mac-content`}
-                        >
-                          {Interface.mac}
-                        </DataListCell>,
-                      ]}
-                    />
-                  </DataListItemRow>
-                </DataListItem>
-              </DataList>
-            </AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <CardTitle>{__('Interfaces')}</CardTitle>
+      <CardBody>
+        <Accordion asDefinitionList>
+          {!interfaces.length && (
+            <div style={{ marginLeft: '20px' }}>
+              <Skeleton count={3} width={200} />
+            </div>
+          )}
+          {interfaces.map(Interface => (
+            <AccordionItem key={Interface.id}>
+              <AccordionToggle
+                onClick={() => {
+                  onToggle(Interface.identifier);
+                }}
+                isExpanded={ativeAccordion === Interface.identifier}
+                id={Interface.identifier}
+              >
+                <span>
+                  {Interface.primary && <StarIcon />} {Interface.identifier}
+                </span>
+              </AccordionToggle>
+              <AccordionContent
+                id={Interface.identifier}
+                isHidden={ativeAccordion !== Interface.identifier}
+              >
+                <DataList isCompact aria-label="Interfaces">
+                  <DataListItem aria-labelledby="ip">
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell key={`${Interface.identifier}-ip`}>
+                            <span> {__('IP Address')}</span>
+                          </DataListCell>,
+                          <DataListCell
+                            key={`${Interface.identifier}-ip-content`}
+                          >
+                            {Interface.ip}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                  <DataListItem aria-labelledby="ip6">
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell key={`${Interface.identifier}-ip6`}>
+                            <span>{__('IP6 Address')}</span>
+                          </DataListCell>,
+                          <DataListCell
+                            key={`${Interface.identifier}-ip6-content`}
+                          >
+                            {Interface.ip6}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                  <DataListItem aria-labelledby="fqdn">
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell key={`${Interface.identifier}-fqdn`}>
+                            <span>{__('FQDN')}</span>
+                          </DataListCell>,
+                          <DataListCell
+                            key={`${Interface.identifier}-fqdn-content`}
+                          >
+                            {Interface.fqdn}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                  <DataListItem aria-labelledby="type">
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell
+                            isFilled
+                            key={`${Interface.identifier}-type`}
+                          >
+                            <span>{__('Type')}</span>
+                          </DataListCell>,
+                          <DataListCell
+                            isFilled
+                            alignRight
+                            key={`${Interface.identifier}-type-content`}
+                          >
+                            {Interface.type}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                  <DataListItem aria-labelledby="mac">
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell
+                            isFilled
+                            key={`${Interface.identifier}-mac`}
+                          >
+                            <span>MAC</span>
+                          </DataListCell>,
+                          <DataListCell
+                            isFilled
+                            alignRight
+                            key={`${Interface.identifier}-mac-content`}
+                          >
+                            {Interface.mac}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                </DataList>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </CardBody>
     </Card>
   );
 };
