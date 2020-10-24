7411fdbfbcd00aa5546607f5c796bc194859ce24
Ondřej Ezr <oezr@redhat.com>
2020-10-25 01:50:17 +0200

    Fixes #31160 - drop Puppet Classes Host(group) form tab
    Extracted in: https://github.com/theforeman/foreman_puppet_enc/pull/62.

--- app/helpers/puppet_related_helper.rb
+++ app/helpers/puppet_related_helper.rb
@@ -67,40 +67,6 @@ module PuppetRelatedHelper
     classes.where(:id => class_vars)
   end
 
-  def extraction_warning_puppet_classes_management
-    _("Puppet Classes management is being extracted to the foreman_puppet plugin and will be removed from core in 3.0")
-  end
-
-  def extraction_warning_config_groups
-    _("Config Groups are being extracted to the foreman_puppet plugin and will be removed from core in 3.0")
-  end
-
-  def extraction_warning_puppet_environments
-    _("Puppet Environments management is being extracted to the foreman_puppet plugin and will be removed from core in 3.0")
-  end
-
-  def extraction_warning_smart_class_parameters
-    _("Smart Class Parameters management is being extracted to the foreman_puppet plugin and will be removed from core in 3.0")
-  end
-
-  def extraction_warning_puppet_enc
-    _("Puppet ENC is being extracted to the foreman_puppet plugin and will be removed from core in 3.0")
-  end
-
-  def puppetclasses_tab(puppetclasses_receiver)
-    content_tag(:div, :class => "tab-pane", :id => "puppet_klasses") do
-      if @environment.present? ||
-          @hostgroup.present? && @hostgroup.environment.present?
-        alert(:class => "alert-info", :header => _("Notice"),
-              :text => extraction_warning_puppet_classes_management)
-        render "puppetclasses/class_selection", :obj => puppetclasses_receiver
-      else
-        alert(:class => "alert-info", :header => _("Notice"),
-              :text => _("Please select an environment first"))
-      end
-    end
-  end
-
   def puppet_actions
     actions = []
     if authorized_for(:controller => :hosts, :action => :edit)
--- config/initializers/puppet.rb
+++ config/initializers/puppet.rb
@@ -1,12 +1,5 @@
 # initialize puppet related pagelets
 Pagelets::Manager.with_key "hosts/_form" do |mgr|
-  mgr.add_pagelet :main_tabs,
-    :id => :puppet_klasses,
-    :name => _("Puppet Classes"),
-    :partial => "hosts/puppet/puppet_classes_tab",
-    :priority => 100,
-    :onlyif => proc { |host, context| context.instance_eval { accessible_resource(host, :smart_proxy, :name, association: :puppet_proxy).present? } }
-
   mgr.add_pagelet :main_tab_fields,
     :partial => "hosts/puppet/main_tab_fields",
     :priority => 100
