53e0f6c999ac20d4916f38559322e8b42b94cd39
Ron Lavi <1ronlavi@gmail.com>
2020-10-28 05:03:49 -0400

    Fixes #31097 - link to a tab in host details page

--- app/assets/javascripts/proxy_status.js
+++ app/assets/javascripts/proxy_status.js
@@ -12,10 +12,10 @@ $(document).on('ContentLoad', function() {
   });
   showProxies();
   loadTFTP();
-  setTab();
+  tfm.tools.setTab();
 });
 
-$(window).on('hashchange', setTab); //so buttons that link to an anchor can open that tab
+$(window).on('hashchange', tfm.tools.setTab); //so buttons that link to an anchor can open that tab
 
 function setItemStatus(item, response) {
   if (response.success && response.message && response.message.warning) {
@@ -119,17 +119,4 @@ function populateData(response, item) {
   item.find('.proxy-show-status').each(function() {
     setItemStatus($(this), response);
   });
-}
-
-// Make sure the correct tab is displayed when loading the page with an anchor,
-// even if the anchor is to a sub-tab.
-function setTab() {
-  var anchor = document.location.hash.split('?')[0];
-  if (anchor.length) {
-    var parent_tab = $(anchor).parents('.tab-pane');
-    if (parent_tab.exists()) {
-      $('.nav-tabs a[href="#' + parent_tab[0].id + '"]').tab('show');
-    }
-    $('.nav-tabs a[href="' + anchor + '"]').tab('show');
-  }
 }
