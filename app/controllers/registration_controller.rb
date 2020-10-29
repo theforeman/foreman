58b0b69e5bf31e65206c259597f81f9abc09ac74
Leos Stejskal <lstejska@redhat.com>
2020-10-29 19:32:37 +0100

    Fixes #31021 - Global Registration & Insights setup parameter (#8091)
    * Fixes #31021 - Global Registration & Insights setup parameter
    
    New parameter :setup_insights, allowing user to enable / disable
    installation of insights-client on the host.
    If :setup_insights is set, 'host_registration_insights' parameter
    for the host will be created (or updated, if it exists already).

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,4 +1,6 @@
 class RegistrationController < ApplicationController
+  include Foreman::Controller::Registration
+
   def new
     form_options
   end
@@ -15,6 +17,7 @@ class RegistrationController < ApplicationController
     @host_groups = Hostgroup.authorized(:view_hostgroups).select(:id, :name)
     @operating_systems = Operatingsystem.authorized(:view_operatingsystems).select(:id, :title)
     @smart_proxies = Feature.find_by(name: 'Registration')&.smart_proxies || []
+    @insights_param = CommonParameter.find_by(name: 'host_registration_insights')
   end
 
   def headers
@@ -33,8 +36,19 @@ class RegistrationController < ApplicationController
 
   def registration_args
     ignored = ['utf8', 'authenticity_token', 'commit', 'action', 'locale', 'controller', 'jwt_expiration']
-    params.except(*ignored)
-          .delete_if { |_, v| v.blank? }
-          .permit!
+    args = params.except(*ignored)
+    args[:setup_insights] = setup_insights_param if params['setup_insights'].to_s.present?
+
+    args.delete_if { |_, v| v.blank? }
+        .permit!
+  end
+
+  def setup_insights_param
+    organization = Organization.authorized(:view_organizations).find(params['organization_id']) if params['organization_id'].present?
+    location = Location.authorized(:view_locations).find(params['location_id']) if params['location_id'].present?
+    host_group = Hostgroup.authorized(:view_hostgroups).find(params['host_group_id']) if params["host_group_id"].present?
+    operatingsystem = Operatingsystem.authorized(:view_operatingsystems).find(params['operatingsystem_id']) if params["operatingsystem_id"].present?
+
+    global_setup_insights(organization, location, host_group, operatingsystem).to_s
   end
 end
--- test/controllers/registration_controller_test.rb
+++ test/controllers/registration_controller_test.rb
@@ -7,11 +7,60 @@ class RegistrationControllerTest < ActionController::TestCase
     assert_template :new
   end
 
-  test 'create' do
-    params = { organization: taxonomies(:organization1).id, location: taxonomies(:location1).id }
-    post :create, params: params, session: set_session_user
+  describe 'create' do
+    test 'create' do
+      params = { organization: taxonomies(:organization1).id, location: taxonomies(:location1).id }
+      post :create, params: params, session: set_session_user
 
-    assert_response :success
-    assert_template :create
+      assert_response :success
+      assert_template :create
+    end
+
+    context 'setup_insights_param' do
+      let(:params) { { organization: taxonomies(:organization1).id, location: taxonomies(:location1).id } }
+
+      before do
+        CommonParameter.where(name: 'host_registration_insights').destroy_all
+      end
+
+      test 'without param' do
+        post :create, params: params, session: set_session_user
+        refute assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = nil && setup_insights = true' do
+        post :create, params: params.merge(setup_insights: true), session: set_session_user
+        assert assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = nil && setup_insights = false' do
+        post :create, params: params.merge(setup_insights: false), session: set_session_user
+        assert assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = true && setup_insights = true' do
+        CommonParameter.create(name: 'host_registration_insights', key_type: 'boolean', value: true)
+        post :create, params: params.merge(setup_insights: true), session: set_session_user
+        refute assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = false && setup_insights = false' do
+        CommonParameter.create(name: 'host_registration_insights', key_type: 'boolean', value: false)
+        post :create, params: params.merge(setup_insights: false), session: set_session_user
+        refute assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = true && setup_insights = false' do
+        CommonParameter.create(name: 'host_registration_insights', key_type: 'boolean', value: true)
+        post :create, params: params.merge(setup_insights: false), session: set_session_user
+        assert assigns(:command).include?('setup_insights')
+      end
+
+      test 'when host_registration_insights = false && setup_insights = true' do
+        CommonParameter.create(name: 'host_registration_insights', key_type: 'boolean', value: false)
+        post :create, params: params.merge(setup_insights: true), session: set_session_user
+        assert assigns(:command).include?('setup_insights')
+      end
+    end
   end
 end
