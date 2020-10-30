6a54e5da049ade7a1d9ac3252ce704adb08da29d
Ondřej Ezr <oezr@redhat.com>
2020-10-30 14:21:41 +0100

    Refs #31094 - route config groups API to plugin

--- test/controllers/api/v2/smart_class_parameters_controller_test.rb
+++ test/controllers/api/v2/smart_class_parameters_controller_test.rb
@@ -4,27 +4,27 @@ class Api::V2::SmartClassParametersControllerTest < ActionController::TestCase
   [{}, { host_id: 123 }, { hostgroup_id: 123 }, { puppetclass_id: 123 },
    { environment_id: 123 }, { puppetclass_id: 123, environment_id: 124 }].each do |params|
     test "should not get index with #{params.inspect}" do
-      skip('Foreman Puppet Enc plugin is installed') if Foreman::Plugin.find(:foreman_puppet_enc)
+      skip('Foreman Puppet plugin is installed') if Foreman::Plugin.find(:foreman_puppet)
       get :index, params: params
       assert_response :not_implemented
       response = ActiveSupport::JSON.decode(@response.body)
-      assert_equal response['message'], 'To access SmartClassParameters API you need to install Foreman Puppet Enc plugin'
+      assert_equal response['message'], 'To access Smart Class Parameter API, you need to install the Foreman Puppet plugin'
     end
   end
 
   test "should not show" do
-    skip('Foreman Puppet Enc plugin is installed') if Foreman::Plugin.find(:foreman_puppet_enc)
+    skip('Foreman Puppet plugin is installed') if Foreman::Plugin.find(:foreman_puppet)
     get :show, params: { id: 123 }
     assert_response :not_implemented
     response = ActiveSupport::JSON.decode(@response.body)
-    assert_equal response['message'], 'To access SmartClassParameters API you need to install Foreman Puppet Enc plugin'
+    assert_equal response['message'], 'To access Smart Class Parameter API, you need to install the Foreman Puppet plugin'
   end
 
   test "should not update" do
-    skip('Foreman Puppet Enc plugin is installed') if Foreman::Plugin.find(:foreman_puppet_enc)
+    skip('Foreman Puppet plugin is installed') if Foreman::Plugin.find(:foreman_puppet)
     patch :update, params: { id: 123 }
     assert_response :not_implemented
     response = ActiveSupport::JSON.decode(@response.body)
-    assert_equal response['message'], 'To access SmartClassParameters API you need to install Foreman Puppet Enc plugin'
+    assert_equal response['message'], 'To access Smart Class Parameter API, you need to install the Foreman Puppet plugin'
   end
 end
