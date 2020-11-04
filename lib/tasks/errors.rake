theforeman/foreman

commit a7874f5722625e3a64c557a4d2d445c8b67d1548

Fixes #28384 - Allow hiding stacktraces in the UI

lib/tasks/errors.rake
@@ -1,0 +1,49 @@
+namespace :errors do
+  desc <<~END_DESC
+    Fetches log for given request id
+
+    This task is useful to get log for given request id from logs in log directory. The request_id is required.
+    You can also specify log file where request logs has been saved. Default option is 'production.log'.
+
+    Available attributes:
+      * request_id = Request id from 500 page
+      * log_file = specify another log file to lookup (default is 'production.log')
+
+    Examples:
+      # foreman-rake errors:fetch_log request_id=aabbccss5 [log_file=development.log]
+  END_DESC
+
+  task :fetch_log => :environment do
+    request_id = ENV['request_id']
+    logfile = ENV['log_file'] || 'production.log' # default file where we want to find is production.log
+    unless request_id
+      puts "Can't find log without request_id"
+      exit(1)
+    end
+    type = Foreman::Logging.config[:type]
+    unless type == 'file'
+      puts "This rake task only support log file, you're using #{type}"
+      puts "You can search the logs in #{type} for request_id #{request_id}"
+      exit(1)
+    end
+    file_path = File.join(Foreman::Logging.log_directory, logfile)
+    unless File.exist?(file_path)
+      puts "Can't find log file #{file_path}"
+      exit(1)
+    end
+    result = `grep "#{request_id}" "#{file_path}"`
+    if result.empty?
+      puts "Can't find log for request_id"
+      exit(1)
+    end
+    puts result
+    puts "\n"
+    puts "Foreman version: #{Foreman::VERSION.new.full}"
+    unless (plugins = Foreman::Plugin.all).empty?
+      puts "Plugins: "
+      plugins.each do |plugin|
+        puts " - #{plugin.name} #{plugin.version}"
+      end
+    end
+  end
+end

app/controllers/application_controller.rb
@@ -348,8 +348,10 @@ class ApplicationController < ActionController::Base
     if exception.try(:cause).is_a?(ActiveRecord::SubclassNotFound)
       sti_clean_up(exception.cause)
     else
-      Foreman::Logging.exception("Action failed", exception)
-      render :template => "common/500", :layout => !request.xhr?, :status => :internal_server_error, :locals => { :exception => exception}
+      ex_message = exception.message
+      Foreman::Logging.exception(ex_message, exception)
+      full_request_id = request.request_id
+      render :template => "common/500", :layout => !request.xhr?, :status => :internal_server_error, :locals => { :exception_message => ex_message, request_id: full_request_id.split('-').first, full_request_id: full_request_id}
     end
   end
 

app/views/common/500.html.erb
@@ -1,21 +1,21 @@
 <%= alert :header => _("Oops, we're sorry but something went wrong"), :class => 'alert-danger',
   :actions => link_to(_('Back'), main_app.root_path, :class => 'btn btn-default'),
-  :text => h(exception.message),
+  :text => h(exception_message),
   :close => false %>
 
 <div class="pull-right">
   <%= documentation_button("7.3GettingHelp")%>
 </div>
+
 <p id="message">
   <%= _("If you feel this is an error with Foreman itself, please open a new issue with") %>
-  <%= link_to _("Foreman ticketing system"), "http://theforeman.org/projects/foreman/issues", :rel => "external" %>,
-  <%= _("You would probably need to attach the") %>
-  <%= link_to_function(_("Full trace"), "$('#backtrace').toggle()")%>
-  <%= _(", relevant log entries, and it is highly recommended to also attach the foreman-debug output.") %>
+  <%= link_to _("Foreman ticketing system"), "https://projects.theforeman.org/projects/foreman/issues", :rel => "external" %>,
+  <% if Foreman::Logging.config[:type] == "file" %>
+    <%= _("Please include in your report the full error log that can be acquired by running: ") %>
+    <strong> foreman-rake errors:fetch_log request_id=<%= request_id %></strong>
+  <% else %>
+    <%= _('Search your logs for request-id') %> <%= request_id %>.
+    <%= _('When using external logging, search for request UUID') %> <%= full_request_id %>
+  <% end %>
+    <%= _(" and it is highly recommended to also attach the foreman-debug output.") %>
 </p>
-
-<div id="backtrace" class="alert alert-block alert-danger base in fade hide">
-  <em><%= exception.class %></em><br>
-  <strong><%= exception.message %></strong><br>
-  <%= exception.backtrace.join("<br>").html_safe %>
-</div>

config/logging.yaml
@@ -34,8 +34,9 @@
 #   values: true, false
 #
 # :layout:
-#   Formatter, one of 'pattern', 'multiline_pattern' or 'json'. The multiline pattern prepends
-#   '|' characters on all additional lines for better readability.
+#   Formatter, one of 'pattern', 'multiline_pattern', 'multiline_request_pattern' or 'json'. The multiline pattern prepends
+#   '|' characters on all additional lines for better readability. The multiline request pattern also adds request id to
+#   all additional line for better log searching.
 #
 # :json_items:
 #   Items to include in JSON. Only applies when 'json' layout is set. Possible items are
@@ -82,7 +83,7 @@
   :log_trace: false
   :level: info
   :type: file
-  :layout: multiline_pattern
+  :layout: multiline_request_pattern
   :json_items:
     - logger
     - timestamp

lib/foreman/logging.rb
@@ -86,7 +86,7 @@ module Foreman
       options.assert_valid_keys :level, :logger
       logger_name = options[:logger] || 'app'
       level = options[:level] || :warn
-      backtrace_level = options[:backtrace_level] || :debug
+      backtrace_level = options[:backtrace_level] || :info
       unless ::Logging::LEVELS.key?(level.to_s)
         raise "Unexpected log level #{level}, expected one of #{::Logging::LEVELS.keys}"
       end
@@ -219,6 +219,9 @@ module Foreman
       when 'multiline_pattern'
         pattern += "  Log trace: %F:%L method: %M\n" if @config[:log_trace]
         MultilinePatternLayout.new(:pattern => pattern, :color_scheme => colorize ? 'bright' : nil)
+      when 'multiline_request_pattern'
+        pattern += "  Log trace: %F:%L method: %M\n" if @config[:log_trace]
+        MultilineRequestPatternLayout.new(:pattern => pattern, :color_scheme => colorize ? 'bright' : nil)
       end
     end
 
@@ -251,6 +254,12 @@ module Foreman
       # all new lines will be indented
       def indent_lines(string)
         string.gsub("\n", "\n | ")
+      end
+    end
+
+    class MultilineRequestPatternLayout < MultilinePatternLayout
+      def indent_lines(string)
+        string.gsub("\n", "\n #{::Logging.mdc['request'].split('-').first} | ")
       end
     end
   end

commit c76475b48394dfff4d3a7c61bfc828f189ea03bb

Fixes #31262 - Warn about incorrect logging layout
a7874f5722625e3a64c557a4d2d445c8b67d1548 introduced a rake task to get
error reports, but this requires the multiline_request_pattern logging
layout. This warns users in case the layout is incorrect.

lib/tasks/errors.rake
@@ -26,6 +26,10 @@ namespace :errors do
       puts "You can search the logs in #{type} for request_id #{request_id}"
       exit(1)
     end
+    if Foreman::Logging.config[:layout] != 'multiline_request_pattern'
+      $STDERR.puts "Warning: Logging layout is not multiline_request_pattern."
+      $STDERR.puts "This output of this command can be incomplete."
+    end
     file_path = File.join(Foreman::Logging.log_directory, logfile)
     unless File.exist?(file_path)
       puts "Can't find log file #{file_path}"