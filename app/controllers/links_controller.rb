123f99f66905cb83f84a9da4bb1ce0e2ee99e055
Ron Lavi <1ronlavi@gmail.com>
2020-11-10 16:16:08 +0200

    Fixes #31290 - plugin_documentation_url params
    Enable to pass params to 'plugin_documentation_url' through links#show

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -11,7 +11,7 @@ class LinksController < ApplicationController
     when 'manual'
       documentation_url(options['section'], options)
     when 'plugin_manual'
-      plugin_documentation_url(options['name'])
+      plugin_documentation_url
     when 'feed'
       Setting['rss_url']
     when 'wiki'
@@ -29,15 +29,26 @@ class LinksController < ApplicationController
 
   private
 
+  def foreman_org_path(sub_path)
+    "https://theforeman.org/#{sub_path}"
+  end
+
   def documentation_url(section = "", options = {})
-    root_url = options[:root_url] || "https://theforeman.org/manuals/#{SETTINGS[:version].short}/index.html#"
+    root_url = options[:root_url] || foreman_org_path("manuals/#{SETTINGS[:version].short}/index.html#")
     root_url + (section || '')
   end
 
-  def plugin_documentation_url(plugin_name, version: nil, options: {})
-    root_url = options[:root_url] || "https://theforeman.org/plugins"
-    path = version ? "#{plugin_name}/#{version}" : plugin_name
-    "#{root_url}/#{path}"
+  def plugin_documentation_url
+    name, version, section, root_url = plugin_documentation_params.values_at(:name, :version, :section, :root_url)
+    path = root_url || foreman_org_path("plugins")
+    path += "/#{name}"
+    path += "/#{version}" unless version.nil?
+    path + (section || '')
+  end
+
+  def plugin_documentation_params
+    params.require(:name)
+    params.permit(:version, :section, :root_url, :name, :type)
   end
 
   def forum_url(post_path = nil, options: {})
