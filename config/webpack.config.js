7e400bb3501e1f718a7dab8454215305faf999ac
Amir Fefer <amirfefer@gmail.com>
2020-11-12 01:12:59 +0200

    Fixes #31277 - styling webpack config file

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -3,9 +3,10 @@
 
 var path = require('path');
 var webpack = require('webpack');
-var ForemanVendorPlugin = require('@theforeman/vendor').WebpackForemanVendorPlugin;
+var ForemanVendorPlugin = require('@theforeman/vendor')
+  .WebpackForemanVendorPlugin;
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var StatsWriterPlugin = require("webpack-stats-plugin").StatsWriterPlugin;
+var StatsWriterPlugin = require('webpack-stats-plugin').StatsWriterPlugin;
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CompressionPlugin = require('compression-webpack-plugin');
 var pluginUtils = require('../script/plugin_webpack_directories');
@@ -22,23 +23,25 @@ var args = argvParse({
   },
   host: {
     type: 'string',
-  }
-})
+  },
+});
 
 const supportedLocales = () => {
   const localeDir = path.join(__dirname, '..', 'locale');
 
   // Find all files in ./locale/*
-  const localesFiles = fs.readdirSync(localeDir)
+  const localesFiles = fs.readdirSync(localeDir);
 
   // Return only folders
-  return localesFiles.filter(f => fs.statSync(path.join(localeDir, f)).isDirectory());
-}
+  return localesFiles.filter(f =>
+    fs.statSync(path.join(localeDir, f)).isDirectory()
+  );
+};
 
 const supportedLanguages = () => {
   // Extract extract languages from the language tags (strip off dialects)
-  return [ ...new Set(supportedLocales().map(d => d.split('_')[0]))];
-}
+  return [...new Set(supportedLocales().map(d => d.split('_')[0]))];
+};
 
 const devServerConfig = () => {
   const result = require('dotenv').config();
@@ -49,8 +52,8 @@ const devServerConfig = () => {
   return {
     port: args.port || '3808',
     host: args.host || process.env.BIND || 'localhost',
-  }
-}
+  };
+};
 
 module.exports = env => {
   const devServer = devServerConfig();
@@ -60,28 +63,43 @@ module.exports = env => {
     process.env.RAILS_ENV === 'production' ||
     process.env.NODE_ENV === 'production';
 
-  var bundleEntry = path.join(__dirname, '..', 'webpack/assets/javascripts/bundle.js');
+  var bundleEntry = path.join(
+    __dirname,
+    '..',
+    'webpack/assets/javascripts/bundle.js'
+  );
 
   var plugins = pluginUtils.getPluginDirs('pipe');
 
-  var resolveModules = [  path.join(__dirname, '..', 'webpack'),
-                          path.join(__dirname, '..', 'node_modules'),
-                          'node_modules/',
-                       ].concat(pluginUtils.pluginNodeModules(plugins));
+  var resolveModules = [
+    path.join(__dirname, '..', 'webpack'),
+    path.join(__dirname, '..', 'node_modules'),
+    'node_modules/',
+  ].concat(pluginUtils.pluginNodeModules(plugins));
 
   if (env && env.pluginName !== undefined) {
     var pluginEntries = {};
     pluginEntries[env.pluginName] = plugins['entries'][env.pluginName];
     for (var entry of Object.keys(plugins['entries'])) {
-      if (entry.startsWith(env.pluginName + ":")) {
-          pluginEntries[entry] = plugins['entries'][entry]
+      if (entry.startsWith(env.pluginName + ':')) {
+        pluginEntries[entry] = plugins['entries'][entry];
       }
-    };
+    }
 
-    var outputPath = path.join(plugins['plugins'][env.pluginName]['root'], 'public', 'webpack');
-    var jsFilename = production ? env.pluginName + '/[name]-[chunkhash].js' : env.pluginName + '/[name].js';
-    var cssFilename = production ? env.pluginName + '/[name]-[chunkhash].css' : env.pluginName + '/[name].css';
-    var chunkFilename = production ? env.pluginName + '/[name]-[chunkhash].js' : env.pluginName + '/[name].js';
+    var outputPath = path.join(
+      plugins['plugins'][env.pluginName]['root'],
+      'public',
+      'webpack'
+    );
+    var jsFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].js'
+      : env.pluginName + '/[name].js';
+    var cssFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].css'
+      : env.pluginName + '/[name].css';
+    var chunkFilename = production
+      ? env.pluginName + '/[name]-[chunkhash].js'
+      : env.pluginName + '/[name].js';
     var manifestFilename = env.pluginName + '/manifest.json';
   } else {
     var pluginEntries = plugins['entries'];
@@ -100,7 +118,9 @@ module.exports = env => {
     pluginEntries
   );
 
-  const supportedLanguagesRE = new RegExp(`/(${supportedLanguages().join('|')})$`);
+  const supportedLanguagesRE = new RegExp(
+    `/(${supportedLanguages().join('|')})$`
+  );
 
   var config = {
     entry: entry,
@@ -112,18 +132,20 @@ module.exports = env => {
       path: outputPath,
       publicPath: '/webpack/',
       filename: jsFilename,
-      chunkFilename
+      chunkFilename,
     },
 
     resolve: {
       modules: resolveModules,
-      alias: Object.assign({
-        foremanReact:
-          path.join(__dirname,
-             '../webpack/assets/javascripts/react_app'),
+      alias: Object.assign(
+        {
+          foremanReact: path.join(
+            __dirname,
+            '../webpack/assets/javascripts/react_app'
+          ),
         },
         pluginUtils.aliasPlugins(pluginEntries)
-      )
+      ),
     },
 
     module: {
@@ -135,53 +157,60 @@ module.exports = env => {
           exclude: /node_modules(?!\/(@novnc|unidiff))/,
           loader: 'babel-loader',
           options: {
-            presets: [require.resolve('@theforeman/builder/babel')]
+            presets: [require.resolve('@theforeman/builder/babel')],
           },
         },
         {
           test: /\.css$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader',
-            use: 'css-loader'
-          })
+            use: 'css-loader',
+          }),
         },
         {
           test: /\.(png|gif|svg)$/,
-          use: 'url-loader?limit=32767'
+          use: 'url-loader?limit=32767',
         },
         {
           test: /\.scss$/,
           use: ExtractTextPlugin.extract({
             fallback: 'style-loader', // The backup style loader
-            use: production ? 'css-loader!sass-loader' : 'css-loader?sourceMap!sass-loader?sourceMap'
-          })
+            use: production
+              ? 'css-loader!sass-loader'
+              : 'css-loader?sourceMap!sass-loader?sourceMap',
+          }),
         },
         {
           test: /\.(graphql|gql)$/,
           exclude: /node_modules/,
           loader: 'graphql-tag/loader',
-        }
-      ]
+        },
+      ],
     },
 
     plugins: [
-      new ForemanVendorPlugin({ mode: production ? 'production' : 'development' }),
+      new ForemanVendorPlugin({
+        mode: production ? 'production' : 'development',
+      }),
       // must match config.webpack.manifest_filename
       new StatsWriterPlugin({
         filename: manifestFilename,
         fields: null,
-        transform: function (data, opts) {
+        transform: function(data, opts) {
           return JSON.stringify(
             {
               assetsByChunkName: data.assetsByChunkName,
               errors: data.errors,
-              warnings: data.warnings
-            }, null, 2 );
-        }
+              warnings: data.warnings,
+            },
+            null,
+            2
+          );
+        },
       }),
       new ExtractTextPlugin({
         filename: cssFilename,
-        allChunks: true
+        allChunks: true,
       }),
       new OptimizeCssAssetsPlugin({
         assetNameRegExp: /\.css$/g,
@@ -201,8 +230,8 @@ module.exports = env => {
         'process.env': {
           NODE_ENV: JSON.stringify(production ? 'production' : 'development'),
           NOTIFICATIONS_POLLING: process.env.NOTIFICATIONS_POLLING,
-          REDUX_LOGGER: process.env.REDUX_LOGGER
-        }
+          REDUX_LOGGER: process.env.REDUX_LOGGER,
+        },
       }),
       // limit locales from intl only to supported ones
       new webpack.ContextReplacementPlugin(
@@ -214,13 +243,15 @@ module.exports = env => {
         /react-intl\/locale-data/,
         supportedLanguagesRE
       ),
-    ]
+    ],
   };
 
-  config.plugins.push(new webpack.optimize.CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: Infinity,
-  }))
+  config.plugins.push(
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: Infinity,
+    })
+  );
 
   if (production) {
     config.plugins.push(
@@ -229,7 +260,7 @@ module.exports = env => {
         uglifyOptions: {
           compress: { warnings: false },
         },
-        sourceMap: true
+        sourceMap: true,
       }),
       new SimpleNamedModulesPlugin(),
       new webpack.optimize.ModuleConcatenationPlugin(),
@@ -254,4 +285,4 @@ module.exports = env => {
   }
 
   return config;
-}
+};
