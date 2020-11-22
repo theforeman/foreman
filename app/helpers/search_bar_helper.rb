a5bf8f54edf9ef0e353cf8dbee228c8eb9f924b3
Tomer Brisker <tbrisker@gmail.com>
2020-11-22 17:53:21 +0200

    Fixes #31368 - Simplify mount_search_bar method
    This method made a lot of preperations for customization, but in
    practice it is never used except for the default case in the main
    layout. Any more elaborate customization should be done in react and
    not in erb.

--- app/helpers/search_bar_helper.rb
+++ app/helpers/search_bar_helper.rb
@@ -1,31 +1,20 @@
 module SearchBarHelper
-  def mount_search_bar(
-    id,
-    controller: auto_complete_controller_name,
-    url: send("auto_complete_search_#{auto_complete_controller_name}_path"),
-    search_query: params[:search],
-    use_bookmarks: true,
-    use_key_shortcuts: true,
-    autocomplete_id: "searchBar"
-  )
-    bookmarks = {}
-    if use_bookmarks
-      bookmarks = {
-        url: main_app.api_bookmarks_path,
-        canCreate: authorizer.can?(:create_bookmarks),
-        documentationUrl: documentation_url("4.1.5Searching"),
-      }
-    end
-    react_component("SearchBar", {
-                      data: {
-                        controller: controller,
+  def mount_search_bar
+    bookmarks = {
+      url: main_app.api_bookmarks_path,
+      canCreate: authorizer.can?(:create_bookmarks),
+      documentationUrl: documentation_url("4.1.5Searching"),
+    }
+    url = send("auto_complete_search_#{auto_complete_controller_name}_path")
+    react_component("SearchBar", data: {
+                      controller: auto_complete_controller_name,
                       autocomplete: {
-                        id: autocomplete_id,
-                        searchQuery: search_query,
+                        id: 'searchBar',
+                        searchQuery: params[:search],
                         url: url,
-                        useKeyShortcuts: use_key_shortcuts,
+                        useKeyShortcuts: true,
                       },
                       bookmarks: bookmarks,
-                      }})
+                    })
   end
 end
