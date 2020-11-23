02d7719cbd7538109be96e29c0a3638eb5b857b5
Ondřej Ezr <oezr@redhat.com>
2020-11-23 13:13:06 +0100

    Fixes #31372 - improve facet relation hook
    This introduce more rigid relation registration.
    Instead of refreshing the relations every time we register new facet.

--- app/models/concerns/facets/managed_host_extensions.rb
+++ app/models/concerns/facets/managed_host_extensions.rb
@@ -3,16 +3,11 @@ require_dependency 'facets'
 module Facets
   module ManagedHostExtensions
     extend ActiveSupport::Concern
+    include Facets::BaseHostExtensions
 
     included do
-      include Facets::BaseHostExtensions
       configure_facet(:host, :host, :host_id) do |facet_config|
         include_in_clone facet_config.name
-      end
-      refresh_facet_relations
-
-      Facets.after_entry_created do |entry|
-        register_facet_relation(entry) if entry.has_host_configuration?
       end
     end
   end
