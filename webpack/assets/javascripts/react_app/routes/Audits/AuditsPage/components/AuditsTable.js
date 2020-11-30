cb951fef3cc448de68a071f20b07a6aadce29dc0
Tomer Brisker <tbrisker@gmail.com>
2020-11-30 20:27:38 +0200

    Refs #29292 - Remove useless pagination options from audit table
    The pagination component already gets the pagination options and doesn't
    need them to be passed in from the calling component.

--- webpack/assets/javascripts/react_app/routes/Audits/AuditsPage/components/AuditsTable.js
+++ webpack/assets/javascripts/react_app/routes/Audits/AuditsPage/components/AuditsTable.js
@@ -4,35 +4,30 @@ import { withRenderHandler } from '../../../../common/HOC';
 import AuditsList from '../../../../components/AuditsList';
 import AuditsLoadingPage from './AuditsLoadingPage';
 import Pagination from '../../../../components/Pagination/Pagination';
-import { usePaginationOptions } from '../../../../components/Pagination/PaginationHooks';
 
-const AuditsTable = ({ audits, page, itemCount, fetchAndPush }) => {
-  const perPageOptions = usePaginationOptions();
-  return (
-    <React.Fragment>
-      <div id="audit-list">
-        <AuditsList data={{ audits }} fetchAndPush={fetchAndPush} />
-      </div>
-      <div id="pagination">
-        <Pagination
-          data={{
-            itemCount,
-            viewType: 'table',
-            classNames: { pagination_classes: 'audits-pagination' },
-          }}
-          pagination={{
-            page,
-            perPageOptions,
-          }}
-          onPageSet={newPage => fetchAndPush({ page: newPage })}
-          onPerPageSelect={newPerPage =>
-            fetchAndPush({ perPage: newPerPage, page: 1 })
-          }
-        />
-      </div>
-    </React.Fragment>
-  );
-};
+const AuditsTable = ({ audits, page, itemCount, fetchAndPush }) => (
+  <React.Fragment>
+    <div id="audit-list">
+      <AuditsList data={{ audits }} fetchAndPush={fetchAndPush} />
+    </div>
+    <div id="pagination">
+      <Pagination
+        data={{
+          itemCount,
+          viewType: 'table',
+          classNames: { pagination_classes: 'audits-pagination' },
+        }}
+        pagination={{
+          page,
+        }}
+        onPageSet={newPage => fetchAndPush({ page: newPage })}
+        onPerPageSelect={newPerPage =>
+          fetchAndPush({ perPage: newPerPage, page: 1 })
+        }
+      />
+    </div>
+  </React.Fragment>
+);
 
 AuditsTable.propTypes = {
   audits: PropTypes.array.isRequired,
