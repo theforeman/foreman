theforeman/foreman

commit cd4b1820bd38e888500f2b2f31b65f22857ade5e

Fixes #29710 - Clean up .where conditions
Using methods such as `none`, `not`, `or`, or `all`, we can simplify
some `.where` calls in ways that weren't possible in some very old rails
versions.

app/models/puppetclass.rb
@@ -20,9 +20,8 @@ class Puppetclass < ApplicationRecord
   has_many_hosts :through => :host_classes, :dependent => :destroy
   has_many :config_group_classes
   has_many :config_groups, :through => :config_group_classes, :dependent => :destroy
-  # param classes
-  has_many :class_params, -> { where('environment_classes.puppetclass_lookup_key_id is NOT NULL').distinct },
-    :through => :environment_classes, :source => :puppetclass_lookup_key
+
+  has_many :class_params, :through => :environment_classes, :source => :puppetclass_lookup_key
   accepts_nested_attributes_for :class_params, :reject_if => ->(a) { a[:key].blank? }, :allow_destroy => true
 
   validates :name, :uniqueness => true, :presence => true, :no_whitespace => true

app/controllers/api/base_controller.rb
@@ -70,7 +70,7 @@ module Api
     def parent_scope
       parent_name, scope = parent_resource_details
 
-      return resource_class.where(nil) unless scope
+      return resource_class.all unless scope
 
       association = resource_class.reflect_on_all_associations.detect { |assoc| assoc.plural_name == parent_name.pluralize }
       # if couldn't find an association by name, try to find one by class

app/controllers/application_controller.rb
@@ -186,7 +186,7 @@ class ApplicationController < ActionController::Base
     @resource_base ||= if model_of_controller.respond_to?(:authorized)
                          model_of_controller.authorized(current_permission)
                        else
-                         model_of_controller.where(nil)
+                         model_of_controller.all
                        end
   end
 

app/controllers/filters_controller.rb
@@ -67,14 +67,6 @@ class FiltersController < ApplicationController
     @role = Role.find_by_id(role_id)
   end
 
-  def resource_base
-    @resource_base ||= if @role.present?
-                         Filter.authorized(current_permission)
-                       else
-                         Filter.where(nil).authorized(current_permission)
-                       end
-  end
-
   def role_id
     params[:role_id]
   end

app/controllers/hosts_controller.rb
@@ -363,7 +363,7 @@ class HostsController < ApplicationController
   # multiple host selection methods
 
   def multiple_parameters
-    @parameters = HostParameter.where(:reference_id => @hosts).select("distinct name")
+    @parameters = HostParameter.where(:reference_id => @hosts).distinct.select("name")
   end
 
   def update_multiple_parameters
@@ -713,7 +713,7 @@ class HostsController < ApplicationController
     # Lets search by name or id and make sure one of them exists first
     if params.key?(:host_names) || params.key?(:host_ids) || multiple_with_filter?
       @hosts = resource_base.search_for(params[:search]) if multiple_with_filter?
-      @hosts ||= resource_base.where("hosts.id IN (?) or hosts.name IN (?)", params[:host_ids], params[:host_names])
+      @hosts ||= resource_base.merge(Host.where(id: params[:host_ids]).or(Host.where(name: params[:host_names])))
       if @hosts.empty?
         error _('No hosts were found with that id, name or query filter')
         redirect_to(hosts_path)

app/helpers/common_parameters_helper.rb
@@ -84,7 +84,7 @@ module CommonParametersHelper
 
   def find_parameters_to_view(params_authorizer, parameters_by_type, user = User.current)
     return parameters_by_type.none if user.nil?
-    return parameters_by_type.where(nil) if user.admin?
+    return parameters_by_type.all if user.admin?
     params_authorizer.find_collection(parameters_by_type.klass, :permission => :view_params)
   end
 

app/helpers/smart_proxies_helper.rb
@@ -63,11 +63,11 @@ module SmartProxiesHelper
   end
 
   def services_tab_features(proxy)
-    proxy.features.where('features.name NOT IN (?)', TABBED_FEATURES).distinct.pluck("name").sort
+    proxy.features.where.not(name: TABBED_FEATURES).distinct.pluck("name").sort
   end
 
   def tabbed_features(proxy)
-    proxy.features.where('features.name IN (?)', TABBED_FEATURES).distinct.pluck("name").sort
+    proxy.features.where(name: TABBED_FEATURES).distinct.pluck("name").sort
   end
 
   def show_feature_version(feature)

app/helpers/taxonomy_helper.rb
@@ -135,11 +135,11 @@ module TaxonomyHelper
       association = resource.to_s.classify.constantize
     end
     return unless User.current.allowed_to?("view_#{resource}".to_sym)
-    ids = "#{association.where(nil).klass.to_s.underscore.singularize}_ids".to_sym
+    ids = "#{association.all.klass.to_s.underscore.singularize}_ids".to_sym
 
     content_tag(:div, :id => resource, :class => "tab-pane") do
       all_checkbox(f, resource) +
-      multiple_selects(f, association.where(nil).klass.to_s.underscore.pluralize.to_sym, association, taxonomy.selected_or_inherited_ids[ids],
+      multiple_selects(f, association.all.klass.to_s.underscore.pluralize.to_sym, association, taxonomy.selected_or_inherited_ids[ids],
         {:disabled => taxonomy.used_and_selected_or_inherited_ids[ids],
          :label => translated_label(resource, :select)},
         {'data-mismatches' => taxonomy.need_to_be_selected_ids[ids].to_json,

commit a77f5e01192438c1131af4e97b03723c958de653

Fixes #31538 - use `distinct` scope for class params association
Otherwise, classes with multiple environments load the paramters
multiple times. This is a regression caused by cleanup in cd4b1820.

app/models/puppetclass.rb
@@ -21,7 +21,7 @@ class Puppetclass < ApplicationRecord
   has_many :config_group_classes
   has_many :config_groups, :through => :config_group_classes, :dependent => :destroy
 
-  has_many :class_params, :through => :environment_classes, :source => :puppetclass_lookup_key
+  has_many :class_params, -> { distinct }, :through => :environment_classes, :source => :puppetclass_lookup_key
   accepts_nested_attributes_for :class_params, :reject_if => ->(a) { a[:key].blank? }, :allow_destroy => true
 
   validates :name, :uniqueness => true, :presence => true, :no_whitespace => true