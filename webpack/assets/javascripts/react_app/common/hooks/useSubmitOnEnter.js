8c821988486f368280aa4cc45c6a7d5ad8e7957b
John Mitsch <jomitsch@redhat.com>
2021-01-07 14:12:05 +0000

    Refs #30426 - Update hook check for listener

--- webpack/assets/javascripts/react_app/common/hooks/useSubmitOnEnter.js
+++ webpack/assets/javascripts/react_app/common/hooks/useSubmitOnEnter.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 
 /**
  * A custom hook that listens for enter on a ref and runs a function on enter keydown
@@ -6,18 +6,25 @@ import { useEffect, useCallback } from 'react';
  * @param  {function} onSubmit function to execute on enter keydown
  */
 export const useSubmitOnEnter = (ref, onSubmit) => {
-  const listener = useCallback(event => {
-    if (event.code === 'Enter' || event.code === 'NumpadEnter') onSubmit();
-  }, []);
+  const [listenerAdded, setListenerAdded] = useState(false);
 
   useEffect(() => {
+    const listener = event => {
+      if (event.code === 'Enter' || event.code === 'NumpadEnter') onSubmit();
+    };
+
     const refPointer = ref; // re-assigning so it's available on unmount
-    /* eslint-disable-next-line no-unused-expressions */
-    refPointer?.current?.addEventListener?.('keydown', listener);
+    // A check is added so we don't keep attaching the same event listener. The listener function
+    // is created in the hook, so the browser will attach multiple "new" event listeners.
+    if (!listenerAdded) {
+      /* eslint-disable-next-line no-unused-expressions */
+      refPointer?.current?.addEventListener?.('keydown', listener);
+      setListenerAdded(true);
+    }
 
     return () =>
       refPointer?.current?.removeEventListener?.('keydown', listener);
-  }, [ref, listener]);
+  }, [ref, listenerAdded, onSubmit]);
 };
 
 export default useSubmitOnEnter;
