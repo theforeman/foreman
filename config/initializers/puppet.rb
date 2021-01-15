c10f15f5092b35eaeb70acc7b2231597cbb1976f
Ondřej Ezr <oezr@redhat.com>
2021-01-15 14:52:05 +0100

    Fixes #31366 - drop Puppet parameter template input type
    Drops input type providing PuppetParameter.
    
    Extracted in: https://github.com/theforeman/foreman_puppet_enc/pull/90

--- config/initializers/puppet.rb
+++ config/initializers/puppet.rb
@@ -5,7 +5,3 @@ FactParser.register_fact_parser :puppet, PuppetFactParser, true
 # since it tries to alias_method_chain a method that is defined
 # in the class itself.
 Host::Managed.prepend PuppetHostExtensions
-
-Rails.application.config.to_prepare do
-  Foreman.input_types_registry.register(InputType::PuppetParameterInput)
-end
