325ec5e6e27f47b080c1efc3daf98172701bba82
Jonathon Turel <jturel@gmail.com>
2021-01-18 15:27:37 +0000

    Fixes #31669 - Register RegistrationFacet in application.rb

--- config/initializers/foreman_register.rb
+++ config/initializers/foreman_register.rb
@@ -5,7 +5,3 @@ Pagelets::Manager.with_key 'hosts/show' do |mgr|
     priority: 100,
     onlyif: proc { |host, ctx| host.managed? }
 end
-
-Facets.register(ForemanRegister::RegistrationFacet, :registration_facet) do
-  set_dependent_action :destroy
-end
