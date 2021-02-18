543d8fdb899129263168b5f0463557e66387b679
Leos Stejskal <lstejska@redhat.com>
2021-02-18 08:04:01 +0100

    Fixes #31885 - Registration - Simplified logic for rex & insights
    - New options in form for rex & insights: keep default value or override.
    - Show current value of rex & insights parameters in the default opts.

--- app/controllers/concerns/foreman/controller/registration.rb
+++ app/controllers/concerns/foreman/controller/registration.rb
@@ -18,7 +18,6 @@ module Foreman::Controller::Registration
     location = Location.authorized(:view_locations).find(params['location_id']) if params['location_id'].present?
     host_group = Hostgroup.authorized(:view_hostgroups).find(params['hostgroup_id']) if params["hostgroup_id"].present?
     operatingsystem = Operatingsystem.authorized(:view_operatingsystems).find(params['operatingsystem_id']) if params["operatingsystem_id"].present?
-    host_config_params = host_config_params(organization, location, host_group, operatingsystem)
 
     context = {
       user: User.current,
@@ -29,8 +28,8 @@ module Foreman::Controller::Registration
       operatingsystem: operatingsystem,
       url_host: registration_url.host,
       registration_url: registration_url,
-      setup_insights: global_setup_insights(host_config_params),
-      setup_remote_execution: global_setup_remote_execution(host_config_params),
+      setup_insights: ActiveRecord::Type::Boolean.new.deserialize(params['setup_insights']),
+      setup_remote_execution: ActiveRecord::Type::Boolean.new.deserialize(params['setup_remote_execution']),
     }
 
     params.permit(permitted)
@@ -91,28 +90,6 @@ module Foreman::Controller::Registration
 
     msg = N_('URL in :url parameter is missing a scheme, please set http:// or https://')
     fail Foreman::Exception.new(msg)
-  end
-
-  def host_config_params(organization, location, host_group, operatingsystem)
-    Host.new(organization: organization, location: location, hostgroup: host_group, operatingsystem: operatingsystem).params
-  end
-
-  def global_setup_insights(host_params)
-    return if params['setup_insights'].to_s.blank?
-
-    from_host = host_params['host_registration_insights']
-    from_request = ActiveRecord::Type::Boolean.new.deserialize(params['setup_insights'])
-
-    from_request if (from_request != from_host)
-  end
-
-  def global_setup_remote_execution(host_params)
-    return if params['setup_remote_execution'].to_s.blank?
-
-    from_host = host_params['host_registration_remote_execution']
-    from_request = ActiveRecord::Type::Boolean.new.deserialize(params['setup_remote_execution'])
-
-    from_request if (from_request != from_host)
   end
 
   def host_setup_insights
--- app/controllers/concerns/foreman/controller/registration_commands.rb
+++ app/controllers/concerns/foreman/controller/registration_commands.rb
@@ -10,9 +10,8 @@ module Foreman::Controller::RegistrationCommands
 
   def registration_args
     args = registration_params.except(*ignored_query_args)
-
-    args['setup_insights'] = setup_insights.to_s
-    args['setup_remote_execution'] = setup_remote_execution.to_s
+    args['setup_insights'] = registration_params['setup_insights']
+    args['setup_remote_execution'] = registration_params['setup_remote_execution']
 
     args.delete_if { |_, v| v.blank? }
         .permit!
@@ -27,33 +26,6 @@ module Foreman::Controller::RegistrationCommands
 
     proxy = SmartProxy.authorized(:view_smart_proxies).find(registration_params['smart_proxy_id'])
     "#{proxy.url}/register"
-  end
-
-  def host_config_params
-    organization = Organization.authorized(:view_organizations).find(registration_params['organization_id']) if registration_params['organization_id'].present?
-    location = Location.authorized(:view_locations).find(registration_params['location_id']) if registration_params['location_id'].present?
-    host_group = Hostgroup.authorized(:view_hostgroups).find(registration_params['hostgroup_id']) if registration_params["hostgroup_id"].present?
-    operatingsystem = Operatingsystem.authorized(:view_operatingsystems).find(registration_params['operatingsystem_id']) if registration_params["operatingsystem_id"].present?
-
-    Host.new(organization: organization, location: location, hostgroup: host_group, operatingsystem: operatingsystem).params
-  end
-
-  def setup_insights
-    return if registration_params['setup_insights'].to_s.blank?
-
-    from_host = host_config_params['host_registration_insights']
-    from_request = ActiveRecord::Type::Boolean.new.deserialize(registration_params['setup_insights'])
-
-    from_request if (from_request != from_host)
-  end
-
-  def setup_remote_execution
-    return if registration_params['setup_remote_execution'].to_s.blank?
-
-    from_host = host_config_params['host_registration_remote_execution']
-    from_request = ActiveRecord::Type::Boolean.new.deserialize(registration_params['setup_remote_execution'])
-
-    from_request if (from_request != from_host)
   end
 
   def command_headers
