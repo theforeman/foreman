a889a865e4f8fad52c239102f176c65318490c2c
Ondřej Ezr <oezr@redhat.com>
2021-03-13 23:55:17 +0100

    Fixes #32091 - drop Environment from Host overview
    Extracted in https://github.com/theforeman/foreman_puppet/pull/124

--- app/helpers/puppet_related_helper.rb
+++ app/helpers/puppet_related_helper.rb
@@ -1,7 +1,6 @@
 module PuppetRelatedHelper
   UI.register_host_description do
     multiple_actions_provider :puppet_actions
-    overview_fields_provider :puppet_host_overview_fields
   end
 
   def puppet_actions
@@ -10,19 +9,5 @@ module PuppetRelatedHelper
       actions << { :action => [_('Change Puppet CA'), select_multiple_puppet_ca_proxy_hosts_path], :priority => 1051 } if SmartProxy.unscoped.authorized.with_features("Puppet CA").exists?
     end
     actions
-  end
-
-  def puppet_host_overview_fields(host)
-    fields = []
-    if host.environment.present?
-      fields << {
-        :field => [
-          _("Puppet Environment"),
-          link_to(host.environment, hosts_path(:search => "environment = #{host.environment}")),
-        ],
-        :priority => 650,
-      }
-    end
-    fields
   end
 end
