aa7bb15db840fa1533ada951a4da6aecddeeecba
yifatmakias <ymakias@redhat.com>
2021-03-21 11:35:54 +0200

    Fixes #32148 - Add ansible check mode global parameter

--- db/seeds.d/180-common_parameters.rb
+++ db/seeds.d/180-common_parameters.rb
@@ -2,6 +2,7 @@ CommonParameter.without_auditing do
   params = [
     { name: "host_registration_insights", key_type: "boolean", value: false },
     { name: "host_registration_remote_execution", key_type: "boolean", value: true },
+    { name: "ansible_roles_check_mode", key_type: "boolean", value: false },
   ]
 
   params.each { |param| CommonParameter.find_or_create_by(param) }
