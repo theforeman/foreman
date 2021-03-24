3332bd9049ade6726159486c5b6f091b1fb7f27a
Lukas Zapletal <lzap+git@redhat.com>
2021-03-24 13:30:06 +0100

    Fixes #32193 - enable byebug in development

--- bundler.d/development.rb
+++ bundler.d/development.rb
@@ -7,12 +7,14 @@ group :development do
   # for generating foreign key migrations
   gem 'immigrant', '~> 0.1'
 
+  # debugging and REPL tools
+  gem 'byebug'
   gem 'pry'
   gem 'pry-rails'
   gem 'pry-byebug'
   gem 'pry-doc'
   gem 'pry-stack_explorer'
-  gem 'pry-remote'
+  gem 'pry-remote' # will not work when running via foreman
 
   gem 'rainbow', '>= 2.2.1'
 
