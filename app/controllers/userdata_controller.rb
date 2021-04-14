b1fe802d2e38c095a64c817ecf017778f3666e9b
Lukáš Zapletal <lzap+git@redhat.com>
2021-04-14 11:50:54 +0200

    Fixes #32266 - render_error call in userdata fixed (#8442)
    (cherry picked from commit e97aec2be2fdf6b1dcbbaa9b11455b962b145f10)

--- app/controllers/userdata_controller.rb
+++ app/controllers/userdata_controller.rb
@@ -38,7 +38,7 @@ class UserdataController < ApplicationController
     template ||= @host.provisioning_template(kind: 'user_data')
     unless template
       render_error(
-        :message => 'Unable to find user-data or cloud-init template for host %{host} running %{os}',
+        _('Unable to find user-data or cloud-init template for host %{host} running %{os}'),
         :status => :not_found,
         :host => @host.name,
         :os => @host.operatingsystem
@@ -64,7 +64,7 @@ class UserdataController < ApplicationController
     @host = Foreman::UnattendedInstallation::HostFinder.new(query_params: query_params).search
 
     return true if @host
-    render_error('Could not find host for request %{request_ip}',
+    render_error(_('Could not find host for request %{request_ip}'),
       :status => :not_found,
       :request_ip => request.remote_ip
     )
