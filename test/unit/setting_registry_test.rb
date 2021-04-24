00bbf55c5d6f2b84348ab2ba19fb5e82af6e8055
Ondřej Ezr <oezr@redhat.com>
2021-04-24 23:36:04 +0200

    Fixes #32415 - access only through SettingRegistry
    Update and read the settings through SettingRegistry.
    Adds a layer between controller and Model.
    SettingRegistry is proxying the value parsing method to the model.
    
    This lifts blocker for settings without DB record.

--- test/unit/setting_registry_test.rb
+++ test/unit/setting_registry_test.rb
@@ -14,40 +14,68 @@ class SettingRegistryTest < ActiveSupport::TestCase
     registry.load_values
   end
 
-  context 'with nil default' do
-    let(:default) { nil }
+  describe 'the value getter' do
+    context 'with nil default' do
+      let(:default) { nil }
 
-    it 'allows nil default value' do
-      assert_nil registry['foo']
+      it 'allows nil default value' do
+        assert_nil registry['foo']
+      end
+    end
+
+    it 'provides default if no value defined' do
+      assert_equal 5, registry['foo']
+      assert_equal 5, registry[:foo]
+    end
+
+    it 'saves the value on assignment' do
+      registry[:foo] = 3
+      assert Setting.find_by(name: 'foo').persisted?
+      assert_equal 3, registry['foo']
+    end
+
+    it 'returns updated value only after it is saved' do
+      setting.value = 3
+      assert_equal 5, registry['foo']
+
+      setting.save
+      registry.load_values
+      assert_equal 3, setting.value
+      assert_equal 3, registry['foo']
+    end
+
+    context 'with value' do
+      let(:setting_value) { 10 }
+
+      it 'retrieves the value' do
+        assert_equal setting_value, registry['foo']
+      end
     end
   end
 
-  it 'provides default if no value defined' do
-    assert_equal 5, registry['foo']
-    assert_equal 5, registry[:foo]
-  end
+  describe '#set_user_value' do
+    setup do
+      registry._add('test',
+        category: 'Setting::General',
+        default: default,
+        type: :integer,
+        full_name: 'Test Foo',
+        description: 'test update',
+        context: :test)
+    end
 
-  it 'saves the value on assignment' do
-    registry[:foo] = 3
-    assert Setting.find_by(name: 'foo').persisted?
-    assert_equal 3, registry['foo']
-  end
+    it 'initiates the DB model if none exists yet' do
+      model = registry.set_user_value('test', '10')
+      assert_not_nil model
+      assert model.valid?
+      assert model.save
+      assert_equal 10, model.reload.value
+    end
 
-  it 'returns updated value only after it is saved' do
-    setting.value = 3
-    assert_equal 5, registry['foo']
-
-    setting.save
-    registry.load_values
-    assert_equal 3, setting.value
-    assert_equal 3, registry['foo']
-  end
-
-  context 'with value' do
-    let(:setting_value) { 10 }
-
-    it 'retrieves the value' do
-      assert_equal setting_value, registry['foo']
+    it 'updates the DB model if already exists' do
+      model = Setting.create(registry.find('test').attributes.merge(value: setting_value))
+      registry.set_user_value('test', '10').save
+      assert_equal 10, model.reload.value
     end
   end
 
