80833ed9c08664a0781e150e23e6dd430ff16e03
aabramov <aabramov@redhat.com>
2021-04-25 11:58:39 +0300

    Fixes #21039 - Ensure dates are printed consistently

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,4 @@
 module UsersHelper
-  def last_login_on_column(record)
-    _("%s ago") % time_ago_in_words(record.last_login_on) if record.last_login_on
-  end
-
   def auth_source_column(record)
     record.auth_source&.to_label
   end
