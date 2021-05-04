038379d6caab8f34ef14fabd12ec09462985c573
Amir Fefer <amirfefer@gmail.com>
2021-05-04 17:45:49 +0300

    Fixes #32505 - fix storybook compile errors

--- webpack/assets/javascripts/react_app/components/hosts/storage/vmware/__tests__/StorageContainer.stories.js
+++ webpack/assets/javascripts/react_app/components/hosts/storage/vmware/__tests__/StorageContainer.stories.js
@@ -1,74 +1,62 @@
 import React from 'react';
-import Store from '../../../../../redux';
+import storeDecorator from '../../../../../../../../stories/storeDecorator';
 import StorageContainer from '../index';
 import * as VMWareData from './StorageContainer.fixtures';
-import { mockRequest } from '../../../../../mockRequests';
 import Story from '../../../../../../../../stories/components/Story';
-
-const initializeMocks = () => {
-  mockRequest({
-    url: '/api/v2/compute_resources/1/available_storage_domains',
-    response: VMWareData.storageDomainResponse,
-  });
-
-  mockRequest({
-    url: '/api/v2/compute_resources/1/available_storage_pods',
-    response: VMWareData.storagePodResponse,
-  });
-};
 
 export default {
   title: 'Page chunks/Host VMWare Storage',
+  decorators: [storeDecorator],
 };
 
 export const defaultStateForNewHost = () => {
-  initializeMocks();
   return (
     <Story>
-      <StorageContainer store={Store} data={VMWareData.state1} />
+      <StorageContainer data={VMWareData.state1} />
     </Story>
   );
 };
 
 defaultStateForNewHost.story = {
   name: 'default state for new host',
+  decorators: [storeDecorator],
 };
 
 export const multipleControllers = () => {
-  initializeMocks();
   return (
     <Story>
-      <StorageContainer store={Store} data={VMWareData.state2} />
+      <StorageContainer data={VMWareData.state2} />
     </Story>
   );
 };
 
 multipleControllers.story = {
   name: 'multiple controllers',
+  decorators: [storeDecorator],
 };
 
 export const onClone = () => {
-  initializeMocks();
   return (
     <Story>
-      <StorageContainer store={Store} data={VMWareData.clone} />
+      <StorageContainer data={VMWareData.clone} />
     </Story>
   );
 };
 
 onClone.story = {
   name: 'on clone',
+  decorators: [storeDecorator],
 };
 
 export const withoutAnyControllers = () => {
-  initializeMocks();
   return (
     <Story>
-      <StorageContainer store={Store} data={VMWareData.emptyState} />
+      <StorageContainer data={VMWareData.emptyState} />
     </Story>
   );
 };
 
 withoutAnyControllers.story = {
   name: 'without any controllers',
+  decorators: [storeDecorator],
 };
