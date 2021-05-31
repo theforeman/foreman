aafa21bb2a3755c2205ba48a10a5c29e3615222a
Ondřej Ezr <oezr@redhat.com>
2021-05-31 12:16:07 +0200

    Fixes #32673 - deduplicate records prior uniq index
    In 84cb565af244f56029471700bb313d46c45e5fcf we've introduced unique
    index in database, because there is a way in Rails to bypass uniqueness
    validation, if the records are being introduced at the same time.
    
    We forgot to deduplicate the records prior introducing this migration so
    some installations are failing to run the migration.
    
    (cherry picked from commit 122975ddbe035bdc2663cc0207c8d666dae2df4b)

--- db/migrate/20200625081552_add_unique_index_to_lookup_value.rb
+++ db/migrate/20200625081552_add_unique_index_to_lookup_value.rb
@@ -1,5 +1,13 @@
 class AddUniqueIndexToLookupValue < ActiveRecord::Migration[6.0]
-  def change
+  def up
+    LookupValue.where.not(id: LookupValue.group(:lookup_key_id, :match).select('MAX(id)')).each do |lookup_value|
+      say "Deleting duplicate override value for #{lookup_value.lookup_key.class.humanize_class_name} #{lookup_value.lookup_key.key} with matcher #{lookup_value.match} and value #{lookup_value.value}"
+      lookup_value.destroy
+    end
     add_index :lookup_values, [:lookup_key_id, :match], unique: true
+  end
+
+  def down
+    remove_index :lookup_values, column: [:lookup_key_id, :match], unique: true
   end
 end
