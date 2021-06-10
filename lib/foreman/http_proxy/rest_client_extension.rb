a41cabb4d0af472515c647a7c9d79b0bac417207
Adam Ruzicka <aruzicka@redhat.com>
2021-06-10 09:47:48 +0200

    Fixes #32192 - Override only the relevant parts

--- lib/foreman/http_proxy/rest_client_extension.rb
+++ lib/foreman/http_proxy/rest_client_extension.rb
@@ -12,18 +12,13 @@ module Foreman
       def net_http_object(hostname, port)
         p_uri = proxy_uri
 
-        if p_uri.nil?
-          # no proxy set
-          Net::HTTP.new(hostname, port)
-        elsif !p_uri
-          # proxy explicitly set to none
-          Net::HTTP.new(hostname, port, nil, nil, nil, nil)
-        else
-          proxy_pass = CGI.unescape(p_uri.password) if p_uri.password
-          proxy_user = CGI.unescape(p_uri.user) if p_uri.user
-          Net::HTTP.new(hostname, port,
-            p_uri.hostname, p_uri.port, proxy_user, proxy_pass)
-        end
+        # no proxy set or proxy explicitly set to none
+        return super unless p_uri
+
+        proxy_pass = CGI.unescape(p_uri.password) if p_uri.password
+        proxy_user = CGI.unescape(p_uri.user) if p_uri.user
+        Net::HTTP.new(hostname, port,
+          p_uri.hostname, p_uri.port, proxy_user, proxy_pass)
       end
     end
   end
