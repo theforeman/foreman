a6932c7be1ca507591b443e5b4c938607886d303
Amir Fefer <amirfefer@gmail.com>
2021-06-17 00:32:58 +0300

    Fixes #32820 - align recent audits card to the new design

--- webpack/assets/javascripts/react_app/components/HostDetails/Audits/index.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Audits/index.js
@@ -1,153 +1,97 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import Skeleton from 'react-loading-skeleton';
-import { ArrowIcon } from '@patternfly/react-icons';
-import ElipsisWithTooltip from 'react-ellipsis-with-tooltip';
+import React from 'react';
 import {
-  Card,
+  Bullseye,
   DataList,
+  Card,
+  CardActions,
+  CardHeader,
+  CardTitle,
+  CardBody,
   DataListItem,
   DataListItemRow,
   DataListItemCells,
   DataListCell,
-  CardBody,
-  CardTitle,
-  Accordion,
-  AccordionItem,
-  AccordionContent,
-  AccordionToggle,
+  Title,
 } from '@patternfly/react-core';
+import { Link } from 'react-router-dom';
+import URI from 'urijs';
 
-import { foremanUrl } from '../../../../foreman_tools';
+import { foremanUrl } from '../../../common/helpers';
 import { translate as __ } from '../../../common/I18n';
 import { useAPI } from '../../../common/hooks/API/APIHooks';
+import RelativeDateTime from '../../common/dates/RelativeDateTime';
+import SkeletonLoader from '../../common/SkeletonLoader';
+import { STATUS } from '../../../constants';
+
+const NUMBER_OF_RECORDS = 3;
+const BASE_URL = '/audits';
 
 const AuditCard = ({ hostName }) => {
-  const [activeAccordion, setActiveAccordion] = useState(0);
-  const onToggle = id => {
-    if (id === activeAccordion) {
-      setActiveAccordion('');
-    } else {
-      setActiveAccordion(id);
-    }
-  };
-
-  const url = hostName && foremanUrl(`/api/audits?search=host+%3D+${hostName}`);
+  const hostSearch = `host=${hostName}`;
+  const apiUrl = new URI({
+    path: foremanUrl(`/api/${BASE_URL}`),
+    query: { search: hostSearch, per_page: NUMBER_OF_RECORDS },
+  }).toString();
+  const uiUrl = new URI({
+    path: foremanUrl(BASE_URL),
+    query: { search: hostSearch },
+  }).toString();
   const {
-    response: { results },
-  } = useAPI('get', url);
+    response: { results: audits },
+    status = STATUS.PENDING,
+  } = useAPI('get', apiUrl);
   return (
     <Card isHoverable>
-      <CardTitle>
-        {__('Recent Audits')}{' '}
-        <a href={foremanUrl(`/audits?search=host+%3D+${hostName}`)}>
-          <ArrowIcon />
-        </a>
-      </CardTitle>
+      <CardHeader>
+        <CardTitle>{__('Recent Audits')}</CardTitle>
+        <CardActions>
+          <Link to={uiUrl}> {__('All Audits')}</Link>
+        </CardActions>
+      </CardHeader>
       <CardBody>
-        <Accordion asDefinitionList>
-          {!results && (
-            <div style={{ marginLeft: '20px' }}>
-              <Skeleton count={3} width={200} />
-            </div>
+        <SkeletonLoader
+          skeletonProps={{ count: NUMBER_OF_RECORDS }}
+          status={status}
+          emptyState={
+            <Bullseye>
+              <Title headingLevel="h4"> {__('No Results found')} </Title>
+            </Bullseye>
+          }
+        >
+          {audits && (
+            <DataList isCompact>
+              {audits.map(
+                ({ user_name: user, created_at: timestamp, action, id }) => (
+                  <DataListItem key={id}>
+                    <DataListItemRow>
+                      <DataListItemCells
+                        dataListCells={[
+                          <DataListCell key={`action-${id}`}>
+                            {action}
+                          </DataListCell>,
+                          <DataListCell key={`date-${id}`}>
+                            <RelativeDateTime date={timestamp} />
+                          </DataListCell>,
+                          <DataListCell key={`user-${id}`}>
+                            {user}
+                          </DataListCell>,
+                        ]}
+                      />
+                    </DataListItemRow>
+                  </DataListItem>
+                )
+              )}
+            </DataList>
           )}
-          {results?.map((audit, index) => {
-            if (index < 3)
-              return (
-                <AccordionItem key={index}>
-                  <AccordionToggle
-                    onClick={() => {
-                      onToggle(`${audit.request_uuid}-${index}`);
-                    }}
-                    isExpanded={
-                      activeAccordion === `${audit.request_uuid}-${index}`
-                    }
-                    id={`${audit.request_uuid}-${index}`}
-                  >
-                    <span>{`${audit.action} (by ${audit.user_name})`}</span>
-                  </AccordionToggle>
-                  <AccordionContent
-                    isHidden={
-                      activeAccordion !== `${audit.request_uuid}-${index}`
-                    }
-                  >
-                    <DataList
-                      style={{
-                        width: '350px',
-                        overflowX: 'hidden',
-                      }}
-                      isCompact
-                      aria-label="Audits"
-                    >
-                      {audit &&
-                        Object.entries(audit.audited_changes).map(
-                          ([key, value], i) => (
-                            <DataListItem key={`audit-${i}`} aria-labelledby="">
-                              <DataListItemRow>
-                                <DataListItemCells
-                                  dataListCells={[
-                                    <DataListCell key={key}>
-                                      <span> {key}</span>
-                                    </DataListCell>,
-                                    <DataListCell
-                                      key={`old-value-${i}`}
-                                      style={{
-                                        width: '350px',
-                                        overflowX: 'hidden',
-                                      }}
-                                    >
-                                      {value && (
-                                        <ElipsisWithTooltip>
-                                          <mark
-                                            style={{
-                                              backgroundColor: '#FFC0CB',
-                                            }}
-                                          >
-                                            {value[0]}
-                                          </mark>
-                                        </ElipsisWithTooltip>
-                                      )}
-                                    </DataListCell>,
-                                    <DataListCell
-                                      style={{
-                                        width: '350px',
-                                        overflowX: 'hidden',
-                                      }}
-                                      key={`new-value-${i}`}
-                                    >
-                                      {value && (
-                                        <mark
-                                          style={{
-                                            backgroundColor: '#90EE90',
-                                          }}
-                                        >
-                                          {value[1]}
-                                        </mark>
-                                      )}
-                                    </DataListCell>,
-                                  ]}
-                                />
-                              </DataListItemRow>
-                            </DataListItem>
-                          )
-                        )}
-                    </DataList>
-                  </AccordionContent>
-                </AccordionItem>
-              );
-            return null;
-          })}
-        </Accordion>
+        </SkeletonLoader>
       </CardBody>
     </Card>
   );
 };
 
 AuditCard.propTypes = {
-  hostName: PropTypes.string,
-};
-AuditCard.defaultProps = {
-  hostName: undefined,
+  hostName: PropTypes.string.isRequired,
 };
 
 export default AuditCard;
