b90b495b073175fb874bfd370b81dc5443fbfdc4
Lukas Zapletal <lzap+git@redhat.com>
2021-06-18 14:17:49 +0200

    Fixes #32837 - drop unwanted URI parsing from extension

--- lib/foreman/http_proxy/net_http_extension.rb
+++ lib/foreman/http_proxy/net_http_extension.rb
@@ -2,9 +2,8 @@ module Foreman
   module HttpProxy
     module NetHttpExtension
       def request(req, body = nil, &block)
-        host = URI.parse(@address).host
-        if proxy_http_request?(@proxy_address, host, @socket)
-          log_proxied_request("NetHttp", http_proxy, host)
+        if proxy_http_request?(@proxy_address, address, @socket)
+          log_proxied_request("NetHttp", http_proxy, address)
           @proxy_address = URI.parse(http_proxy)
           http_proxied_rescue do
             super(req, body, &block)
