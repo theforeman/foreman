9929a1393d6307c35f90e4c76ffed213b407969b
Leos Stejskal <lstejska@redhat.com>
2021-06-29 15:12:13 +0200

    Fixes #32756 - Toast notifications & PatternFly 4
    Use `Alert` & `AlertGroup` PF4 components for toast notifications

---
+++ webpack/assets/javascripts/react_app/components/ToastsList/ToastListHelpers.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+
+import { AlertVariant } from '@patternfly/react-core';
+
+export const toastType = type => {
+  if (type in AlertVariant) return type;
+
+  console.warn(
+    `Toast notification type '${type}' is invalid. Please use one of the following types: ${Object.values(
+      AlertVariant
+    )}`
+  );
+
+  const fallbackTypes = {
+    alert: AlertVariant.warning,
+    notice: AlertVariant.info,
+    error: AlertVariant.danger,
+  };
+
+  return fallbackTypes[type] || AlertVariant.default;
+};
+
+export const toastTitle = (message, type) => {
+  if (message.length <= 60) return message;
+  return defaultTitle(type);
+};
+
+const defaultTitle = type => {
+  switch (type) {
+    case 'danger':
+    case 'error':
+      return 'Error';
+    case 'warning':
+      return 'Warning';
+    case 'success':
+      return 'Success';
+    default:
+      return 'Info';
+  }
+};
