198040f68c03d2b05c28afb6f8e2653f290aec35
Leos Stejskal <lstejska@redhat.com>
2021-06-30 16:26:11 +0200

    Fixes #32918 - Host registration - Update packages
    New parameter `update_packages` for update of
    all packages on the host when registered

--- app/controllers/api/v2/registration_commands_controller.rb
+++ app/controllers/api/v2/registration_commands_controller.rb
@@ -16,6 +16,7 @@ module Api
         param :jwt_expiration, :number, desc: N_("Expiration of the authorization token (in hours)")
         param :insecure, :bool, desc: N_("Enable insecure argument for the initial curl")
         param :packages, String, desc: N_("Packages to install on the host when registered. Can be set by `host_packages` parameter. Example: `pkg1 pkg2`")
+        param :update_packages, :bool, desc: N_("Update all packages on the host")
         param :repo, String, desc: N_("Repository URL / details, for example for Debian OS family: 'deb http://deb.example.com/ buster 1.0', for Red Hat OS family: 'http://yum.theforeman.org/client/latest/el8/x86_64/'")
         param :repo_gpg_key_url, String, desc: N_("URL of the GPG key for the repository")
       end
