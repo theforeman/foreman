c915522ef393a314b6bab4394fc83952455234f6
Ron Lavi <1ronlavi@gmail.com>
2021-07-11 08:26:55 +0000

    Fixes #32979 - passing react routes from server
    Enable to pass a react route from the server
    which will render a component from the components registry
    and pass props from the server to it.

--- webpack/assets/javascripts/react_app/routes/index.js
+++ webpack/assets/javascripts/react_app/routes/index.js
@@ -4,29 +4,46 @@ import PropTypes from 'prop-types';
 import { routes } from './routes';
 import { renderRoute } from './RoutingService';
 import ForemanSwitch from './ForemanSwitcher';
+import componentRegistry from '../components/componentRegistry';
 
-const AppSwitcher = ({ children }) => (
-  <>
-    <ForemanSwitch>
-      {routes.map(({ render, path, ...routeProps }) => (
-        <Route
-          path={path}
-          key={path}
-          {...routeProps}
-          render={renderProps => renderRoute(render, renderProps)}
-        />
-      ))}
-    </ForemanSwitch>
-    {children}
-  </>
-);
+const AppSwitcher = ({ children, serverRoutes }) => {
+  const appRoutes = [
+    ...routes,
+    ...serverRoutes.map(({ path, props: serverProps, component }) => {
+      const Component = componentRegistry.registry[component]?.type;
+      return {
+        path,
+        render: renderProps =>
+          Component && <Component {...renderProps} {...serverProps} />,
+      };
+    }),
+  ];
+
+  return (
+    <>
+      <ForemanSwitch>
+        {appRoutes.map(({ render, path, ...routeProps }) => (
+          <Route
+            path={path}
+            key={path}
+            {...routeProps}
+            render={renderProps => renderRoute(render, renderProps)}
+          />
+        ))}
+      </ForemanSwitch>
+      {children}
+    </>
+  );
+};
 
 AppSwitcher.propTypes = {
   children: PropTypes.object,
+  serverRoutes: PropTypes.array,
 };
 
 AppSwitcher.defaultProps = {
   children: null,
+  serverRoutes: [],
 };
 
 export default AppSwitcher;
