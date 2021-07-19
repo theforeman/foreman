a49660efb2041211794dd9f1d4f67f0bf3b556d3
Alexander Olofsson <alexander.olofsson@liu.se>
2021-07-19 15:09:03 +0200

    Fixes #33055: Support ADFS-style JWTs
    This uses the keyfinder method support from the JWT gem to look for keys
    in both the standard 'kid' header as well as the non-standard 'x5t' one.

--- app/services/oidc_jwt_validate.rb
+++ app/services/oidc_jwt_validate.rb
@@ -12,8 +12,8 @@ class OidcJwtValidate
         verify_aud: true,
         iss: Setting['oidc_issuer'],
         verify_iss: true,
-        algorithms: [Setting['oidc_algorithm']],
-        jwks: jwks_loader }
+        algorithms: [Setting['oidc_algorithm']] },
+      &jwk_findkey
     ).first
   rescue JWT::DecodeError => e
     Foreman::Logging.exception('Failed to decode JWT', e)
@@ -21,6 +21,17 @@ class OidcJwtValidate
   end
 
   private
+
+  def jwk_findkey
+    finder = JWT::JWK::KeyFinder.new(jwks: jwks_loader)
+
+    # Support ADFS's use of the non-standard x5t header for holding key ID
+    lambda do |header|
+      key = finder.key_for(header['kid']) if header.key? 'kid'
+      key ||= finder.key_for(header['x5t']) if header.key? 'x5t'
+      key
+    end
+  end
 
   def jwks_loader(options = {})
     response = RestClient::Request.execute(
