dcd301548442c28b3b4223c3d2e3900ec2dc4d2c
Tomer Brisker <tbrisker@gmail.com>
2021-08-19 16:46:33 +0300

    Refs #33416 - Remove beforeRenderingCallback from renderRoute
    The only place that used it was the fallback route, which is now called
    directly.

--- webpack/assets/javascripts/react_app/routes/__test__/RoutingService.test.js
+++ webpack/assets/javascripts/react_app/routes/__test__/RoutingService.test.js
@@ -8,18 +8,10 @@ jest.unmock('../RoutingService');
 
 const props = { location: { pathname: '/test' } };
 const renderFn = () => <div> Test </div>;
-const callback = location => location.pathname === '/test';
-const falseCallback = () => false;
 
 describe('Routing Service', () => {
-  it('rendering a route without a callback', () => {
+  it('rendering a route', () => {
     expect(renderRoute(renderFn, props)).toMatchSnapshot();
-  });
-  it('rendering a route with a callback', () => {
-    expect(renderRoute(renderFn, props, callback)).toMatchSnapshot();
-  });
-  it('rendering a route with a falsy callback', () => {
-    expect(renderRoute(renderFn, props, falseCallback)).toMatchSnapshot();
   });
 });
 
