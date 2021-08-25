6b41f7d04ab49aecb255616ad1eee56c73556979
Evgeni Golov <evgeni@golov.de>
2021-08-25 11:36:43 +0200

    call npm install with --no-optional and --no-audit for plugins
    speedup all the things, please

--- script/npm_install_plugins.js
+++ script/npm_install_plugins.js
@@ -3,7 +3,7 @@ const childProcess = require('child_process');
 const { packageJsonDirs } = require('./plugin_webpack_directories');
 
 packageJsonDirs('pipe').forEach(pluginPath => {
-  childProcess.spawn('npm', ['i'], {
+  childProcess.spawn('npm', ['install', '--no-optional', '--no-audit'], {
     env: process.env,
     cwd: pluginPath,
     stdio: 'inherit',
