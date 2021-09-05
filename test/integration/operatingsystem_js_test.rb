bc5488657aff9eba04c676223492a733e8cd670c
Tomer Brisker <tbrisker@gmail.com>
2021-09-05 11:43:45 +0300

    Refs #32880 - Fix integration test

--- test/integration/operatingsystem_js_test.rb
+++ test/integration/operatingsystem_js_test.rb
@@ -6,9 +6,10 @@ class OperatingsystemJSTest < IntegrationTestWithJavascript
   end
 
   test "delete operating system" do
+    os = FactoryBot.create(:operatingsystem, name: 'aaa')
     visit operatingsystems_path
     first_row = find("table > tbody > tr:nth-child(1)")
-    assert first_row.find("td:nth-child(1)").text, "centos 5.3"
+    assert_equal os.title, first_row.find("td:nth-child(1)").text.strip
     assert has_no_css?("#app-confirm-modal")
 
     actions = first_row.find("td:nth-child(3) > div")
@@ -16,12 +17,16 @@ class OperatingsystemJSTest < IntegrationTestWithJavascript
     actions.find("ul > li > a.delete").click
 
     confirm_modal = page.find("#app-confirm-modal")
-    assert confirm_modal.find(".pf-c-modal-box__title-text").text, "Confirm"
-    assert confirm_modal.find(".pf-c-modal-box__body").text, "Delete centos 5.3?"
+    assert_equal "Confirm", confirm_modal.find(".pf-c-modal-box__title-text").text
+    assert_equal "Delete #{os.title}?", confirm_modal.find(".pf-c-modal-box__body").text
 
     confirm_button = confirm_modal.find("footer > button:nth-child(1)")
-    assert confirm_button.text, "Confirm"
+    assert_equal "Confirm", confirm_button.text
     confirm_button.click
     assert has_no_css?("#app-confirm-modal")
+
+    first_row = find("table > tbody > tr:nth-child(1)")
+    assert_not_equal os.title, first_row.find("td:nth-child(1)").text.strip
+    refute Operatingsystem.find_by_name(os.name).present?
   end
 end
