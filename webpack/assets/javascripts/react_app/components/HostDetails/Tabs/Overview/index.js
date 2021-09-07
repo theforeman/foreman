fb3d42db82d5593cdec6393023d715dd85ec57b8
Ondrej Prazak <oprazak@redhat.com>
2021-09-07 09:31:23 +0200

    Fixes #33423 - Keep gray bg on overview tab only

--- webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Overview/index.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Overview/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, GridItem } from '@patternfly/react-core';
 
 import AuditCard from '../../Audits';
@@ -9,25 +9,34 @@ import { STATUS } from '../../../../constants';
 import AggregateStatus from '../../Status/AggregateStatusCard';
 import './Details.css';
 
-const DetailsTab = ({ response, status, hostName }) => (
-  <div className="host-details-tab-item details-tab">
-    <Grid hasGutter>
-      <GridItem xl2={3} xl={4} md={6} lg={4} rowSpan={2}>
-        <DetailsCard {...response} status={status} />
-      </GridItem>
-      <GridItem xl2={3} xl={4} md={6} lg={4}>
-        <AggregateStatus
-          hostName={hostName}
-          permissions={response.permissions}
-        />
-      </GridItem>
-      <GridItem xl2={3} xl={4} md={6} lg={4}>
-        <AuditCard hostName={hostName} />
-      </GridItem>
-      <Slot hostDetails={response} id="details-cards" multi />
-    </Grid>
-  </div>
-);
+const DetailsTab = ({ response, status, hostName }) => {
+  useEffect(() => {
+    //  This is a workaround for adding gray background inspiring pf4 desgin
+    //  TODO: delete it when pf4 layout (Page copmponent) is implemented in foreman
+    document.body.classList.add('pf-gray-background');
+    return () => document.body.classList.remove('pf-gray-background');
+  }, []);
+
+  return (
+    <div className="host-details-tab-item details-tab">
+      <Grid hasGutter>
+        <GridItem xl2={3} xl={4} md={6} lg={4} rowSpan={2}>
+          <DetailsCard {...response} status={status} />
+        </GridItem>
+        <GridItem xl2={3} xl={4} md={6} lg={4}>
+          <AggregateStatus
+            hostName={hostName}
+            permissions={response.permissions}
+          />
+        </GridItem>
+        <GridItem xl2={3} xl={4} md={6} lg={4}>
+          <AuditCard hostName={hostName} />
+        </GridItem>
+        <Slot hostDetails={response} id="details-cards" multi />
+      </Grid>
+    </div>
+  );
+};
 
 DetailsTab.propTypes = {
   response: PropTypes.object,
