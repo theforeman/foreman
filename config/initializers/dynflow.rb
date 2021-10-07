7e588ce33ff395df46568163dbcc03ff91fa8971
Lukas Zapletal <lzap+git@redhat.com>
2021-10-07 14:04:29 +0200

    Fixes #33660 - ignore dynflow tables for dump
    Signed-off-by: Lukas Zapletal <lzap+git@redhat.com>

--- config/initializers/dynflow.rb
+++ config/initializers/dynflow.rb
@@ -1,2 +1,14 @@
 # Ignore Dynflow tables when schema-dumping. Dynflow tables are handled automatically by Dynflow.
-ActiveRecord::SchemaDumper.ignore_tables << '^dynflow_*'
+# Ruby dumper recognizes table names and regular expression (as string), PostgreSQL dumper only
+# recognizes table names as it passes this to pg_dump via arguments.
+#
+ActiveRecord::SchemaDumper.ignore_tables = [
+  'dynflow_actions',
+  'dynflow_coordinator_records',
+  'dynflow_delayed_plans',
+  'dynflow_envelopes',
+  'dynflow_execution_plans',
+  'dynflow_output_chunks',
+  'dynflow_schema_info',
+  'dynflow_steps',
+]
