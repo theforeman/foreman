c2e1e34f84ac4815bd6ef0ed681862f89b09b0b2
Nadja Heitmann <nadjah@atix.de>
2021-10-11 19:41:40 +0000

    Refs #33550 - Remove environment from template combinations api controller

--- app/controllers/api/v2/template_combinations_controller.rb
+++ app/controllers/api/v2/template_combinations_controller.rb
@@ -5,17 +5,14 @@ module Api
 
       before_action :find_optional_nested_object
       before_action :find_resource, :only => [:show, :update, :destroy]
-      before_action :deprecation_warning
 
       def_param_group :template_combination_identifiers do
         param :provisioning_template_id, String, :desc => N_("ID of config template")
         param :hostgroup_id, String, :desc => N_("ID of host group")
-        param :environment_id, String, :desc => N_("ID of environment - DEPRECATED will have no effect")
       end
 
       api :GET, "/provisioning_templates/:provisioning_template_id/template_combinations", N_("List template combination")
       api :GET, "/hostgroups/:hostgroup_id/template_combinations", N_("List template combination")
-      api :GET, "/environments/:environment_id/template_combinations", N_("DEPRECATED: List template combination")
       param_group :template_combination_identifiers
       def index
         @template_combinations = nested_obj.template_combinations
@@ -24,14 +21,12 @@ module Api
 
       def_param_group :template_combination do
         param :template_combination, Hash, :required => true, :action_aware => true do
-          param :environment_id, :number, :allow_nil => true, :desc => N_("environment id")
           param :hostgroup_id, :number, :allow_nil => true, :desc => N_("host group id")
         end
       end
 
       api :POST, "/provisioning_templates/:provisioning_template_id/template_combinations", N_("Add a template combination")
       api :POST, "/hostgroups/:hostgroup_id/template_combinations", N_("Add a template combination")
-      api :POST, "/environments/:environment_id/template_combinations", N_("DEPRECATED: Add a template combination")
       param_group :template_combination_identifiers
       param_group :template_combination, :as => :create
 
@@ -43,7 +38,6 @@ module Api
       api :GET, "/template_combinations/:id", N_("Show template combination")
       api :GET, "/provisioning_templates/:provisioning_template_id/template_combinations/:id", N_("Show template combination")
       api :GET, "/hostgroups/:hostgroup_id/template_combinations/:id", N_("Show template combination")
-      api :GET, "/environments/:environment_id/template_combinations/:id", N_("DEPRECATED: Show template combination")
       param_group :template_combination_identifiers
       param :id, :identifier, :required => true
       def show
@@ -51,7 +45,6 @@ module Api
 
       api :PUT, "/provisioning_templates/:provisioning_template_id/template_combinations/:id", N_("Update template combination")
       api :PUT, "/hostgroups/:hostgroup_id/template_combinations/:id", N_("Update template combination")
-      api :PUT, "/environments/:environment_id/template_combinations/:id", N_("DEPRECATED: Update template combination")
       param :id, :identifier, :required => true
       param_group :template_combination_identifiers
       param_group :template_combination
@@ -71,16 +64,6 @@ module Api
 
       def allowed_nested_id
         %w(hostgroup_id provisioning_template_id)
-      end
-
-      def deprecation_warning
-        if request.path =~ /^\/environments\//
-          if params[:action] == :index
-            Foreman::Deprecation.api_deprecation_warning('This endpoint will be extracted to ForemanPuppetEnc plugin')
-          else
-            Foreman::Deprecation.api_deprecation_warning('Please use /template_combinations endpoint directly')
-          end
-        end
       end
     end
   end
