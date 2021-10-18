92a6bd860c522b71111e3e2ec65c875fd5721810
Ondřej Ezr <oezr@redhat.com>
2021-10-18 19:35:57 +0200

    Fixes #33712 - console action responding in json

--- app/controllers/concerns/foreman/controller/console_common.rb
+++ app/controllers/concerns/foreman/controller/console_common.rb
@@ -1,14 +1,19 @@
 module Foreman::Controller::ConsoleCommon
   def console
-    render case @console[:type]
-             when 'spice'
-               'hosts/console/spice'
-             when 'vnc'
-               'hosts/console/vnc'
-             when 'vmrc'
-               'hosts/console/vmrc'
-             else
-               'hosts/console/log'
-           end
+    respond_to do |format|
+      format.html do
+        render case @console[:type]
+                 when 'spice'
+                   'hosts/console/spice'
+                 when 'vnc'
+                   'hosts/console/vnc'
+                 when 'vmrc'
+                   'hosts/console/vmrc'
+                 else
+                   'hosts/console/log'
+               end
+      end
+      format.json { render json: { host_id: @host&.to_param, console: @console } }
+    end
   end
 end
