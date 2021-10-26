576774e7acca44ab32f020c3dda0ba95b5b6892b
Tomer Brisker <tbrisker@gmail.com>
2021-10-26 16:49:17 +0300

    Fixes #33787 - Drop Accept Header handling for API version
    The API controller has been abusing the Accept header to define which
    version of the API should be used. This is not compliant with the HTTP
    spec[1] which specifies that this header should be used to indicate
    acceptable media types by the client.
    Any clients currently passing the header should not be affected as the
    default version of the API is v2. There are no plans currently to create
    API v3, and even if it will eventually be developed, users will be able
    to use the route to indicate which version they wish to use.
    
    [1] https://httpwg.org/specs/rfc7231.html#header.accept

--- app/services/api_constraints.rb
+++ app/services/api_constraints.rb
@@ -5,12 +5,10 @@ class ApiConstraints
   end
 
   def matches?(req)
-    route_match       = req.fullpath.match(%r{/api/v(\d+)}) if req.fullpath
-    header_match      = req.accept.match(%r{version=(\d+)}) if req.accept
+    route_match = req.fullpath.match(%r{/api/v(\d+)}) if req.fullpath
 
     return (@version.to_s == route_match[1]) if route_match
-    return (@version.to_s == header_match[1]) if header_match
-    # if version is not specified in route or header, then it returns true only if :default => true in routes file v1.rb or v2.rb
+    # if version is not specified in route, then it returns true only if :default => true in routes
     @default
   end
 end
