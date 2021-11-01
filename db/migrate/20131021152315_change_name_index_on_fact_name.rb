7accfbd455c3f1c3ccf0d185931952fc2b266462
Tomer Brisker <tbrisker@gmail.com>
2021-11-01 13:28:08 +0200

    Remove invalid mysql checks

--- db/migrate/20131021152315_change_name_index_on_fact_name.rb
+++ db/migrate/20131021152315_change_name_index_on_fact_name.rb
@@ -1,21 +1,11 @@
 class ChangeNameIndexOnFactName < ActiveRecord::Migration[4.2]
   def up
     remove_index :fact_names, :column => :name, :unique => true
-    options = if ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/mysql/).any?
-                { :unique => true, :length => 254 }
-              else
-                { :unique => true }
-              end
-    add_index :fact_names, [:name, :type], options
+    add_index :fact_names, [:name, :type], unique: true
   end
 
   def down
     remove_index :fact_names, [:name, :type]
-    options = if ActiveRecord::Base.connection.instance_values["config"][:adapter].grep(/mysql/).any?
-                { :unique => true, :length => 254 }
-              else
-                { :unique => true }
-              end
-    add_index :fact_names, :name, options
+    add_index :fact_names, :name, unique: true
   end
 end
