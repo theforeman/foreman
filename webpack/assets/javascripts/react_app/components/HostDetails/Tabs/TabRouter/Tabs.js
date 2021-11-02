cb60c17a91cc88b29a3778d61c8c92544d5bdd81
Ron Lavi <1ronlavi@gmail.com>
2021-11-02 10:55:15 +0200

    Refs #33805 - clear search on host details tab switch
    A tab component may update the url with search params
    which we need to clear when switching between tabs.

--- webpack/assets/javascripts/react_app/components/HostDetails/Tabs/TabRouter/Tabs.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Tabs/TabRouter/Tabs.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { push } from 'connected-react-router';
 import PropTypes from 'prop-types';
 
 const TabsWithHashHistory = ({ tabs }) => {
   const hashHistory = useHistory();
+  const dispatch = useDispatch();
   const onSelect = (evt, tab) => {
-    hashHistory.push(`/${tab}`);
+    const hash = `/${tab}`;
+    hashHistory.push(hash);
+    dispatch(push({ search: null, hash })); // A tab component may update the url with search params which we need to clear when switching between tabs.
   };
 
   return React.cloneElement(tabs, { onSelect });
