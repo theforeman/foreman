e307baabcd882062fbea6edc3cf93d8e66480c85
Tomer Brisker <tbrisker@gmail.com>
2021-11-11 17:32:47 +0200

    Refs #33909 - Remove useless User lookup
    There is no need to lookup the admin user and use it to update the
    http proxy fields.

--- db/migrate/20210114143800_nilify_empty_proxy_credentials.rb
+++ db/migrate/20210114143800_nilify_empty_proxy_credentials.rb
@@ -1,19 +1,11 @@
 class NilifyEmptyProxyCredentials < ActiveRecord::Migration[6.0]
   def up
-    if User.unscoped.find_by_login(User::ANONYMOUS_ADMIN).present?
-      User.as_anonymous_admin do
-        HttpProxy.where(username: "").update_all(username: nil)
-        HttpProxy.where(password: "").update_all(password: nil)
-      end
-    end
+    HttpProxy.unscoped.where(username: "").update_all(username: nil)
+    HttpProxy.unscoped.where(password: "").update_all(password: nil)
   end
 
   def down
-    if User.unscoped.find_by_login(User::ANONYMOUS_ADMIN).present?
-      User.as_anonymous_admin do
-        HttpProxy.where(username: nil).update_all(username: "")
-        HttpProxy.where(password: nil).update_all(password: "")
-      end
-    end
+    HttpProxy.unscoped.where(username: nil).update_all(username: "")
+    HttpProxy.unscoped.where(password: nil).update_all(password: "")
   end
 end
