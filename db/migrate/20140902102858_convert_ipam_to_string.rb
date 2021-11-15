0974fd4668e12eabf7737851e497a4e9a0352440
Tomer Brisker <tbrisker@gmail.com>
2021-11-15 11:21:53 +0200

    Refs #33909 - Squash imap column conversion

--- db/migrate/20140902102858_convert_ipam_to_string.rb
+++ db/migrate/20140902102858_convert_ipam_to_string.rb
@@ -1,15 +1,9 @@
 class ConvertIpamToString < ActiveRecord::Migration[4.2]
   def up
-    add_column :subnets, :ipam_tmp, :string, :default => IPAM::MODES[:dhcp], :null => false, :limit => 255
-
-    remove_column :subnets, :ipam
-    rename_column :subnets, :ipam_tmp, :ipam
+    change_column :subnets, :ipam, :string, :default => IPAM::MODES[:dhcp], :null => false, :limit => 255
   end
 
   def down
-    add_column :subnets, :ipam_tmp, :boolean, :default => true, :null => false
-
-    remove_column :subnets, :ipam
-    rename_column :subnets, :ipam_tmp, :ipam
+    change_column :subnets, :ipam, :boolean, :default => true, :null => false
   end
 end
