4d294c5e548137207065e5c8d20a15a4c5cfb867
Tomer Brisker <tbrisker@gmail.com>
2021-11-15 12:53:48 +0200

    Refs #33909 - Reset User column information
    While migrations shouldn't rely on any users existing in the DB, there
    are some that currently do. If they try to use User.find_by_lower_login
    they will currently error out since the method won't be defined yet.

--- db/migrate/20140325093623_add_lowerlogin_to_users.rb
+++ db/migrate/20140325093623_add_lowerlogin_to_users.rb
@@ -2,6 +2,10 @@ class AddLowerloginToUsers < ActiveRecord::Migration[4.2]
   def up
     add_column :users, :lower_login, :string, :limit => 255
     add_index  :users, :lower_login, :unique => true
+
+    # reset the column information in case any future migration relies on
+    # User.find_by_lower_login method
+    User.reset_column_information
   end
 
   def down
