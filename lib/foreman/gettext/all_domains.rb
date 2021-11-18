c7a0068f85395eaec38bc533c2b987df6d0ac677
Leos Stejskal <lstejska@redhat.com>
2021-11-18 13:31:00 +0100

    Fixes #33890 - Require Foreman::Gettext in initializers
    Fix deprecation warning the about initialization of auto-loaded constants

--- lib/foreman/gettext/all_domains.rb
+++ lib/foreman/gettext/all_domains.rb
@@ -1,20 +1,24 @@
 require 'fast_gettext'
 
 # include this module to translate in all domains by default
-module Foreman::Gettext::AllDomains
-  def _(key)
-    FastGettext::TranslationMultidomain.D_(key)
-  end
+module Foreman
+  module Gettext
+    module AllDomains
+      def _(key)
+        FastGettext::TranslationMultidomain.D_(key)
+      end
 
-  def n_(*keys)
-    FastGettext::TranslationMultidomain.Dn_(*keys)
-  end
+      def n_(*keys)
+        FastGettext::TranslationMultidomain.Dn_(*keys)
+      end
 
-  def s_(key, separator = nil)
-    FastGettext::TranslationMultidomain.Ds_(key, separator)
-  end
+      def s_(key, separator = nil)
+        FastGettext::TranslationMultidomain.Ds_(key, separator)
+      end
 
-  def ns_(*keys)
-    FastGettext::TranslationMultidomain.Dns_(*keys)
+      def ns_(*keys)
+        FastGettext::TranslationMultidomain.Dns_(*keys)
+      end
+    end
   end
 end
--- lib/foreman/gettext/debug.rb
+++ lib/foreman/gettext/debug.rb
@@ -1,43 +1,47 @@
 require 'fast_gettext'
 
 # include this module to see translations in the UI
-module Foreman::Gettext::Debug
-  DL = "\u00BB".encode("UTF-8") rescue '>'
-  DR = "\u00AB".encode("UTF-8") rescue '<'
+module Foreman
+  module Gettext
+    module Debug
+      DL = "\u00BB".encode("UTF-8") rescue '>'
+      DR = "\u00AB".encode("UTF-8") rescue '<'
 
-  # slightly modified copy of fast_gettext D_* method
-  def _(key)
-    FastGettext.translation_repositories.each_key do |domain|
-      result = FastGettext::TranslationMultidomain.d_(domain, key) { nil }
-      return DL + result.to_s + DR unless result.nil?
-    end
-    DL + key.to_s + DR
-  end
+      # slightly modified copy of fast_gettext D_* method
+      def _(key)
+        FastGettext.translation_repositories.each_key do |domain|
+          result = FastGettext::TranslationMultidomain.d_(domain, key) { nil }
+          return DL + result.to_s + DR unless result.nil?
+        end
+        DL + key.to_s + DR
+      end
 
-  # slightly modified copy of fast_gettext D_* method
-  def n_(*keys)
-    FastGettext.translation_repositories.each_key do |domain|
-      result = FastGettext::TranslationMultidomain.dn_(domain, *keys) { nil }
-      return DL + result.to_s + DR unless result.nil?
-    end
-    DL + keys[-3].split(keys[-2] || FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
-  end
+      # slightly modified copy of fast_gettext D_* method
+      def n_(*keys)
+        FastGettext.translation_repositories.each_key do |domain|
+          result = FastGettext::TranslationMultidomain.dn_(domain, *keys) { nil }
+          return DL + result.to_s + DR unless result.nil?
+        end
+        DL + keys[-3].split(keys[-2] || FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
+      end
 
-  # slightly modified copy of fast_gettext D_* method
-  def s_(key, separator = nil)
-    FastGettext.translation_repositories.each_key do |domain|
-      result = FastGettext::TranslationMultidomain.ds_(domain, key, separator) { nil }
-      return DL + result.to_s + DR unless result.nil?
-    end
-    DL + key.split(separator || FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
-  end
+      # slightly modified copy of fast_gettext D_* method
+      def s_(key, separator = nil)
+        FastGettext.translation_repositories.each_key do |domain|
+          result = FastGettext::TranslationMultidomain.ds_(domain, key, separator) { nil }
+          return DL + result.to_s + DR unless result.nil?
+        end
+        DL + key.split(separator || FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
+      end
 
-  # slightly modified copy of fast_gettext D_* method
-  def ns_(*keys)
-    FastGettext.translation_repositories.each_key do |domain|
-      result = FastGettext::TranslationMultidomain.dns_(domain, *keys) { nil }
-      return DL + result.to_s + DR unless result.nil?
+      # slightly modified copy of fast_gettext D_* method
+      def ns_(*keys)
+        FastGettext.translation_repositories.each_key do |domain|
+          result = FastGettext::TranslationMultidomain.dns_(domain, *keys) { nil }
+          return DL + result.to_s + DR unless result.nil?
+        end
+        DL + keys[-2].split(FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
+      end
     end
-    DL + keys[-2].split(FastGettext::NAMESPACE_SEPARATOR).last.to_s + DR
   end
 end
