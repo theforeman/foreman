aa8657bcb0b6689cf20391ce51230f74ef85cc7a
Ondřej Ezr <oezr@redhat.com>
2021-11-25 15:46:39 +0100

    Fixes #34006 - use rbac_registry for all plugin rbac
    Before this change we have executed all the changes to the Roles and Permissions right away in the plugins.
    This pushes all the changes into the rbac_registry and uses it to initialize the changes only after Rails are ready.

--- app/registries/foreman/plugin/rbac_registry.rb
+++ app/registries/foreman/plugin/rbac_registry.rb
@@ -2,12 +2,19 @@ module Foreman
   class Plugin
     class RbacRegistry
       attr_accessor :role_ids, :default_roles, :registered_permissions
+      attr_accessor :add_all_permissions_to_default_roles
+      attr_reader :modified_roles
+      attr_reader :added_resource_permissions
 
       def initialize(plugin_id)
         @plugin_id = plugin_id
         @role_ids = []
         @registered_permissions = []
         @default_roles = {}
+        @default_role_descriptions = {}
+        @modified_roles = {}
+        @added_resource_permissions = []
+        @add_all_permissions_to_default_roles = false
       end
 
       def registered_roles
@@ -16,6 +23,11 @@ module Foreman
 
       def register(name, options)
         @registered_permissions << [name, options]
+      end
+
+      def register_role(name, permissions, description = '')
+        default_roles[name] = permissions
+        @default_role_descriptions[name] = description
       end
 
       # needed for fixtures permissions.yml,
@@ -29,7 +41,47 @@ module Foreman
       end
 
       def setup!
-        # setup the plugin Rbac in DB
+        setup_permissions!
+        setup_roles!
+      end
+
+      def setup_permissions!
+        return false if Rails.env.test? || Foreman.in_setup_db_rake? || !permission_table_exists?
+        registered_permissions.each do |(name, options)|
+          Permission.where(:name => name).first_or_create(:resource_type => options[:resource_type])
+        end
+      end
+
+      def setup_roles!
+        return false if Foreman.in_setup_db_rake? || Rails.env.test? || !permission_table_exists? || User.unscoped.find_by_login(User::ANONYMOUS_ADMIN).nil?
+        Role.without_auditing do
+          Filter.without_auditing do
+            default_roles.each do |name, permissions|
+              Plugin::RoleLock.new(@plugin_id).register_role name, permissions, self, @default_role_descriptions[name] || ''
+            rescue PermissionMissingException => e
+              Rails.logger.warn(_("Could not create role '%{name}': %{message}") % {:name => name, :message => e.message})
+              return false if Foreman.in_rake?
+              Rails.logger.error(_('Cannot continue because some permissions were not found, please run rake db:seed and retry'))
+              raise e
+            end
+
+            next unless permission_table_exists?
+            rbac_support = Plugin::RbacSupport.new
+            rbac_support.add_all_permissions_to_default_roles(Permission.where(name: permission_names)) if add_all_permissions_to_default_roles
+            rbac_support.add_permissions_to_default_roles(modified_roles) if modified_roles.any?
+            added_resource_permissions.each do |(resources, opts)|
+              rbac_support.add_resource_permissions_to_default_roles resources, opts
+            end
+          end
+        end
+      end
+
+      private
+
+      def permission_table_exists?
+        exists = Permission.connection.table_exists?(Permission.table_name)
+        Rails.logger.debug("Not adding permissions from plugin #{@id} to default roles - permissions table not found") if !exists && !Rails.env.test?
+        exists
       end
     end
   end
