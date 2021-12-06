44432d50c4f35f73f9f7264b59212aeebb54116e
Tomer Brisker <tbrisker@gmail.com>
2021-12-06 18:01:50 +0200

    Refs #34093 - Minor authorizer clean ups
    - Use `.exists?` instead of `.where.any?` and `.where.present?`.
    `.any?` avoids triggering another sql query for loaded relations, but in
    this specific case the relation won't be loaded. `.present?` loads the
    relation to an array and checks if it is empty, which is obviously worse
    then checking in sql.
    - Use regular `Hash` instead of `HashWithIndefferentAccess`
    HWIA comes with a performance overhead since it needs to handle both
    string and symbol keys. Since the `@cache` is only used in one place, we
    can control the keys and use a regular Hash instead.
    
    (cherry picked from commit 92d40b237f0c8f1748d89aafd84b990ed650a3f8)

--- app/services/authorizer_cache.rb
+++ app/services/authorizer_cache.rb
@@ -1,12 +1,12 @@
 module AuthorizerCache
   def initialize_cache
-    @cache = HashWithIndifferentAccess.new do |h, k|
-      h[k] = HashWithIndifferentAccess.new
+    @cache = Hash.new do |h, k|
+      h[k] = {}
     end
   end
 
   def collection_cache_lookup(subject, permission)
-    collection = @cache[subject.class.to_s][permission] ||=
+    collection = @cache[subject.class.to_s][permission.to_s] ||=
       find_collection(subject.class, :permission => permission).pluck(:id)
 
     collection.include?(subject.id)
