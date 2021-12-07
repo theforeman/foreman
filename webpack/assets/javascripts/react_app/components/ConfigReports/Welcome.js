57a4506c6060dbf34e6b94e322c2316b1d50a808
MariaAga <mariaaga@redhat.com>
2021-12-07 13:54:11 +0100

    Fixes #33946 - remove dangerouslySetInnerHTML uses

--- webpack/assets/javascripts/react_app/components/ConfigReports/Welcome.js
+++ webpack/assets/javascripts/react_app/components/ConfigReports/Welcome.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 import { translate as __ } from '../../common/I18n';
 import EmptyState from '../common/EmptyState';
 import { getManualURL, getWikiURL } from '../../common/helpers';
 
 export const WelcomeConfigReports = () => {
-  const content = __(`If you wish to configure Puppet to forward its reports to Foreman, 
-  please follow <a href=${getManualURL(
-    '3.5.4PuppetReports'
-  )}>setting up reporting</a> and
-  <a href=${getWikiURL('Mail_Notifications')}>e-mail reporting</a>`);
   const description = (
     <>
       {__("You don't seem to have any reports.")}
-      <div dangerouslySetInnerHTML={{ __html: content }} />
+
+      <br />
+      <FormattedMessage
+        id="report-manual"
+        defaultMessage={__(
+          'If you wish to configure Puppet to forward its reports to Foreman, please follow {settingUpPuppetReports} and {emailReporting}'
+        )}
+        values={{
+          settingUpPuppetReports: (
+            <a href={getManualURL('3.5.4PuppetReports')}>
+              {__('setting up reporting')}
+            </a>
+          ),
+          emailReporting: (
+            <a href={getWikiURL('Mail_Notifications')}>
+              {__('e-mail reporting')}
+            </a>
+          ),
+        }}
+      />
+      <br />
     </>
   );
   return (
