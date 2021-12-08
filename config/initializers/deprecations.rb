20929c447f18ae5dff51d929eb7dc463b9de4b08
Tomer Brisker <tbrisker@gmail.com>
2021-12-08 19:02:27 +0200

    Refs #10413 - Deprecate setting unattended: false
    This setting is rarely used and complicates the code quite a lot. With
    the introduction of host registration, there is a path for any remaining
    users who had been using Foreman only for inventory to add their hosts.
    
    (cherry picked from commit 2481080e656f6e740036d8887301d22b4255c64e)

--- config/initializers/deprecations.rb
+++ config/initializers/deprecations.rb
@@ -1,1 +1,28 @@
 ActiveSupport::Deprecation.behavior = :silence if Rails.env.production?
+
+Foreman::Application.configure do |app|
+  config.after_initialize do
+    next if Foreman.in_rake?
+    next unless Rails.env.production?
+    next if SETTINGS[:unattended]
+    blueprint = NotificationBlueprint.find_by_name('feature_deprecation')
+    next unless blueprint
+    message = UINotifications::StringParser.new(blueprint.message, {feature: 'Setting ":unattended: false" in settings.yaml', version: '3.3'}).to_s
+    next if blueprint.notifications.where(message: message).any?
+    Notification.create!(
+      audience: Notification::AUDIENCE_ADMIN,
+      message: message,
+      notification_blueprint: blueprint,
+      initiator: User.anonymous_admin,
+      actions: {
+        links: [
+          {
+            href: 'https://community.theforeman.org/t/rfc-remove-unattended-setting/10035',
+            title: _('Further Information'),
+            external: true,
+          },
+        ],
+      }
+    )
+  end
+end
