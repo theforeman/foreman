e898750c50b16df1411f39befc19cab77158f9ff
Ron Lavi <1ronlavi@gmail.com>
2021-12-13 15:22:53 +0200

    Fixes #33378 - Make pages layout styles consistent

--- webpack/assets/javascripts/react_app/components/Layout/components/LayoutContainer.js
+++ webpack/assets/javascripts/react_app/components/Layout/components/LayoutContainer.js
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 const LayoutContainer = ({ isCollapsed, children }) => {
-  const classes = 'react-container container-fluid nav-pf-persistent-secondary';
+  const classes =
+    'react-container container-pf-nav-pf-vertical nav-pf-persistent-secondary';
 
   useEffect(() => {
     if (isCollapsed) document.body.classList.add('collapsed-nav');
