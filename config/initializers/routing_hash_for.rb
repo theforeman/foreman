a7f5b1b8fcc839dd572d198294ec1f193eb12475
Ondřej Ezr <oezr@redhat.com>
2022-01-03 16:03:16 +0100

    Refs #34500 - reimplement routing hash_for on Rails 6.1
    Rails 6.1 refactored the routing method responsible for definition of named routes.
    We are overriding this method and thus we need to refactor it as well.
    The switch for these is neccessary to support both Rails 6.0 and 6.1

--- config/initializers/routing_hash_for.rb
+++ config/initializers/routing_hash_for.rb
@@ -2,17 +2,55 @@ module ActionDispatch
   module Routing
     class RouteSet
       class NamedRouteCollection
-        def define_url_helper(mod, route, name, opts, route_key, url_strategy)
-          helper = UrlHelper.create(route, opts, route_key, url_strategy)
-          mod.module_eval do
-            define_method(name) do |*args|
-              options = nil
-              options = args.pop if args.last.is_a? Hash
+        if Gem::Version.new(SETTINGS[:rails]) >= Gem::Version.new('6.1')
+          def define_url_helper(mod, name, helper, url_strategy)
+            mod.define_method(name) do |*args|
+              last = args.last
+              options = \
+                case last
+                when Hash
+                  args.pop
+                when ActionController::Parameters
+                  args.pop.to_h
+                end
+              helper.call(self, name, args, options, url_strategy)
+            end
+
+            # because we heavily rely on the removed hash_for method in routes, we must add this monkey patch.
+            mod.define_method("hash_for_#{name}") do |*args|
+              inner_options = \
+                case args.last
+                when Hash
+                  args.pop
+                when ActionController::Parameters
+                  args.pop.to_h
+                end
+              opts = helper.instance_variable_get(:@options)
+              helper.send(:handle_positional_args,
+                {},
+                inner_options || {},
+                args,
+                opts.merge(:use_route => helper.route_name),
+                helper.instance_variable_get(:@segment_keys))
+            end
+          end
+        else
+          def define_url_helper(mod, route, name, opts, route_key, url_strategy)
+            helper = UrlHelper.create(route, opts, route_key, url_strategy)
+            mod.define_method(name) do |*args|
+              last = args.last
+              options = \
+                case last
+                when Hash
+                  args.pop
+                when ActionController::Parameters
+                  args.pop.to_h
+                end
               helper.call self, args, options
             end
 
             # because we heavily rely on the removed hash_for method in routes, we must add this monkey patch.
-            define_method("hash_for_#{name}") do |*args|
+            mod.define_method("hash_for_#{name}") do |*args|
               inner_options = nil
               inner_options = args.pop if args.last.is_a? Hash
               helper.send(:handle_positional_args,
