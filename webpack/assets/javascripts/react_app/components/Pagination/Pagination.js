d43f2c17a477ec0c385efeb66a6ba881f1fc035d
Ron Lavi <1ronlavi@gmail.com>
2022-03-09 11:39:53 +0200

    Fixes #34580 - Remove pagination deprecations

--- webpack/assets/javascripts/react_app/components/Pagination/Pagination.js
+++
@@ -1,9 +0,0 @@
-import { deprecate } from '../../common/DeprecationService';
-
-export default () => {
-  deprecate(
-    'PF3 Pagination',
-    'PF4 pagination from "react_app/components/Pagination/index.js"',
-    3.2
-  );
-};
--- webpack/assets/javascripts/react_app/components/Pagination/PaginationHooks.js
+++
@@ -1,9 +0,0 @@
-import { deprecate } from '../../common/DeprecationService';
-
-export const usePaginationOptions = () => {
-  deprecate(
-    'usePaginationOptions',
-    'PF4 pagination which is already shipped with those options',
-    3.2
-  );
-};
