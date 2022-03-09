059a824378c8a976fd9a704b5dc76a4c50d1c1ef
Ron Lavi <1ronlavi@gmail.com>
2022-03-09 11:39:53 +0200

    Fixes #34580 - Remove pagination deprecations

--- webpack/assets/javascripts/react_app/components/Pagination/PaginationWrapper.js
+++
@@ -1,9 +0,0 @@
-import { deprecate } from '../../common/DeprecationService';
-
-export default () => {
-  deprecate(
-    'PaginationWrapper',
-    'PF4 pagination from "react_app/components/Pagination/index.js"',
-    3.2
-  );
-};
