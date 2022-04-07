d1da93b40d9a2f721542fb6fd25125bd6172553e
Jeremy Lenz <jlenz@redhat.com>
2022-04-07 08:23:51 -0400

    Fixes #34745 - Error on API actions if no key is passed (#9179)

--- webpack/assets/javascripts/react_app/redux/API/APIMiddleware.js
+++ webpack/assets/javascripts/react_app/redux/API/APIMiddleware.js
@@ -3,6 +3,11 @@ import { isAPIAction } from './APIHelpers';
 
 export const APIMiddleware = store => next => action => {
   if (isAPIAction(action)) {
+    if (!action?.payload?.key) {
+      throw new Error(
+        `API action ${action?.type} was dispatched without a key. API action payloads must have a key. Did you confuse default and named exports?`
+      );
+    }
     apiRequest(action, store);
   }
   return next(action);
