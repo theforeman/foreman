944eb30acb3c070f1e82e459f1008310bcd10cec
nofaralfasi <nofaralfasi@gmail.com>
2022-05-18 16:28:34 +0300

    Refs #33672 - remove unnecessary timestamps from taxable_taxonomies table

---
+++ db/migrate/20220515130414_drop_unnecessary_timestamps.rb
@@ -0,0 +1,6 @@
+class DropUnnecessaryTimestamps < ActiveRecord::Migration[6.0]
+  def change
+    remove_column taxable_taxonomies, :created_at, :datetime
+    remove_column taxable_taxonomies, :updated_at, :datetime
+  end
+end
