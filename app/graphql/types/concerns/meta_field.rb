8b5a63f2414312454b111b7b937db0df10610fc5
Evgeni Golov <evgeni@golov.de>
2022-05-24 10:40:46 +0200

    Refs #32685 - properly resolve meta field by having a real proc

--- app/graphql/types/concerns/meta_field.rb
+++ app/graphql/types/concerns/meta_field.rb
@@ -4,12 +4,14 @@ module Types
       extend ActiveSupport::Concern
 
       included do
-        field :meta, ::Types::Meta, resolve: (proc do |object|
+        field :meta, ::Types::Meta
+
+        def meta
           {
             :can_edit => ::User.current.can?(object.permission_name(:edit), object),
             :can_destroy => ::User.current.can?(object.permission_name(:destroy), object),
           }
-        end)
+        end
       end
     end
   end
