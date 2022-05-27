c22eccfab8ca3d984608a89386415e276beae3d4
Jeremy Lenz <jlenz@redhat.com>
2022-05-27 15:13:27 -0400

    Fixes #34981 - Add 'Recent communication' card to overview

--- webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Overview/CardsRegistry.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Overview/CardsRegistry.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { addGlobalFill } from '../../../common/Fill/GlobalFill';
 import AuditCard from '../../Audits';
 import DetailsCard from '../../DetailsCard';
+import RecentCommunicationCard from '../../RecentCommunicationCard';
 import AggregateStatus from '../../Status/AggregateStatusCard';
 
 const cards = [
-  { key: '[core]-detail-card', Component: DetailsCard, weight: 4000 },
-  { key: '[core]-status-card', Component: AggregateStatus, weight: 3500 },
+  { key: '[core]-status-card', Component: AggregateStatus, weight: 4000 },
+  {
+    key: '[core]-recent-comms-card',
+    Component: RecentCommunicationCard,
+    weight: 3800,
+  },
+  { key: '[core]-detail-card', Component: DetailsCard, weight: 3400 },
   { key: '[core]-audit-card', Component: AuditCard, weight: 3000 },
 ];
 
