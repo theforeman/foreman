8b3c7c54d53a2747146dfb3819499912d5732806
Adam Ruzicka <a.ruzicka@outlook.com>
2022-06-03 11:23:24 +0200

    Fixes #35014 - Fix injection of global HTTP proxy into net/http
    We were passing wrong arguments to proxy_http_request?, causing it to
    always evaluate to false so no requests were proxied.

--- lib/foreman/http_proxy/net_http_extension.rb
+++ lib/foreman/http_proxy/net_http_extension.rb
@@ -2,9 +2,10 @@ module Foreman
   module HttpProxy
     module NetHttpExtension
       def request(req, body = nil, &block)
-        if proxy_http_request?(@proxy_address, address, @socket)
+        if proxy_http_request?(@proxy_address, address, req&.uri&.scheme)
           log_proxied_request("NetHttp", http_proxy, address)
-          @proxy_address = URI.parse(http_proxy)
+          @proxy_from_env = false
+          @proxy_address = http_proxy
           http_proxied_rescue do
             super(req, body, &block)
           end
