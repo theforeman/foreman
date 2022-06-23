cc8d9a67c1cf623eba89cace76f659d7cc29b3a7
Ron Lavi <1ronlavi@gmail.com>
2022-06-23 10:06:04 +0300

    Fixes #35083 - add power status timeout param
    When checking for the host's power status, the request stops after 3 seconds by default,
    Therefore in cases where it takes longer for the host to respond, the timeout fails the request.
    
    discussion: https://community.theforeman.org/t/bmc-timeout-setting-in-foreman-ui/29016

--- webpack/assets/javascripts/react_app/components/HostDetails/DetailsCard/PowerStatus/constants.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/DetailsCard/PowerStatus/constants.js
@@ -1,7 +1,7 @@
 import { translate as __ } from '../../../../common/I18n';
 
 export const POWER_REQURST_KEY = 'HOST_TOGGLE_POWER';
-export const POWER_REQUEST_OPTIONS = { key: POWER_REQURST_KEY };
+export const POWER_REQUEST_OPTIONS = { key: POWER_REQURST_KEY, params: { timeout: 30 } };
 export const BASE_POWER_STATES = { off: __('Off'), on: __('On') };
 export const BMC_POWER_STATES = { soft: __('Reboot'), cycle: __('Reset') };
 export const SUPPORTED_POWER_STATES = {
