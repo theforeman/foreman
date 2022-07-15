theforeman/foreman

commit 1cf4147a06420842d3818f4edb6e22892f40a939

Fixes #34526 - Switch Rails version to 6.1
Changes overview:
- Set default Rails version to 6.1
- Required dynfow >= 1.6.5
- Cleanup deprecation warnings in config/as_deprecation_whitelist.yaml
- ActiveSupport::ParameterFilter instead of ActionDispatch::Http::ParameterFilter
- model.errors.attribute_names instead of model.errors.keys
- Fixed issue with iterating over the model.errors
- use .add for adding error messages instead of <<
- 'where.not' use NAND instead of NOR [0]

[0] https://github.com/rails/rails/pull/36029

Gemfile
@@ -51,7 +51,7 @@ gem 'deacon', '~> 1.0'
 gem 'webpack-rails', '~> 0.9.8'
 gem 'mail', '~> 2.7'
 gem 'sshkey', '~> 1.9'
-gem 'dynflow', '>= 1.4.4', '< 2.0.0'
+gem 'dynflow', '>= 1.6.5', '< 2.0.0'
 gem 'daemons'
 gem 'bcrypt', '~> 3.1'
 gem 'get_process_mem'

app/controllers/concerns/foreman/controller/filter_parameters.rb
@@ -21,7 +21,7 @@ module Foreman::Controller::FilterParameters
   private
 
   def filter
-    ActionDispatch::Http::ParameterFilter.new(self.class.filter_parameters_options)
+    ActiveSupport::ParameterFilter.new(self.class.filter_parameters_options)
   end
 
   def needs_filtering?(params)

app/graphql/mutations/base_mutation.rb
@@ -74,10 +74,10 @@ module Mutations
     end
 
     def map_errors_to_path(resource)
-      resource.errors.map do |attribute, message|
+      resource.errors.map do |error|
         {
-          path: ['attributes', attribute.to_s.camelize(:lower)],
-          message: message,
+          path: ['attributes', error.attribute.to_s.camelize(:lower)],
+          message: error.message,
         }
       end
     end

app/helpers/hosts_helper.rb
@@ -297,7 +297,7 @@ module HostsHelper
 
   # we ignore interfaces.conflict because they are always registered in host errors as well
   def conflict_objects(errors)
-    errors.keys.map(&:to_s).select { |key| key =~ /conflict$/ && key != 'interfaces.conflict' }.map(&:to_sym)
+    errors.attribute_names.map(&:to_s).select { |key| key =~ /conflict$/ && key != 'interfaces.conflict' }.map(&:to_sym)
   end
 
   def has_conflicts?(errors)

app/models/compute_resources/foreman/model/ovirt.rb
@@ -172,13 +172,13 @@ module Foreman::Model
     rescue => e
       case e.message
         when /404/
-          errors[:url] << e.message
+          errors.add(:url, e.message)
         when /302/
-          errors[:url] << _('HTTPS URL is required for API access')
+          errors.add(:url, _('HTTPS URL is required for API access'))
         when /401/
-          errors[:user] << e.message
+          errors.add(:user, e.message)
         else
-          errors[:base] << e.message
+          errors.add(:base, e.message)
       end
     end
 

app/models/user.rb
@@ -560,9 +560,9 @@ class User < ApplicationRecord
     # If an invalid data returned from an authentication source for any attribute(s) then set its value as nil
     saved_attrs = {}
     unless user.valid?
-      user.errors.each do |attr|
-        saved_attrs[attr] = user[attr]
-        user[attr] = nil
+      user.errors.each do |error|
+        saved_attrs[error.attribute] = user[error.attribute]
+        user[error.attribute] = nil
       end
     end
 

app/validators/association_exists_validator.rb
@@ -1,7 +1,7 @@
 class AssociationExistsValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     if record.send("#{attribute}_id".to_sym).present? && value.nil?
-      record.errors[attribute] << (options[:message] || _("with given ID not found"))
+      record.errors.add(attribute, (options[:message] || _("with given ID not found")))
     end
   end
 end

app/validators/bookmark_controller_validator.rb
@@ -1,7 +1,7 @@
 class BookmarkControllerValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     unless self.class.valid_controllers_list.include?(value)
-      record.errors[attribute] << _("%{value} is not a valid controller") % {:value => value }
+      record.errors.add(attribute, _("%{value} is not a valid controller") % {:value => value })
     end
   end
 

commit cdb0d32231acc31f2b1c0dff89c46d117e728866

Refs #34526 - Drop Rails 6.0 support code
When Foreman switched to Rails 6.1, Rails 6.0 compatibility was dropped.
This makes it fail hard when it is attempted to be used. it also removes
a stray backslash.

Fixes: 1cf4147a06420842d3818f4edb6e22892f40a939
Fixes: f95e36928306637843fdfb39ed5ffe1d5f5af782

Gemfile
@@ -6,12 +6,10 @@ require_relative 'config/boot_settings'
 source 'https://rubygems.org'
 
 gem 'rails', case SETTINGS[:rails]
-             when '6.0'
-               '~> 6.0.4.7'
              when '6.1'
                '~> 6.1.6.1'
              else
-               raise "Unsupported Ruby on Rails version configured in settings.yaml: #{SETTINGS[:rails]}"\
+               raise "Unsupported Ruby on Rails version configured in settings.yaml: #{SETTINGS[:rails]}"
              end
 
 gem 'rest-client', '>= 2.0.0', '< 3', :require => 'rest_client'

config/initializers/routing_hash_for.rb
@@ -2,64 +2,35 @@ module ActionDispatch
   module Routing
     class RouteSet
       class NamedRouteCollection
-        if Gem::Version.new(SETTINGS[:rails]) >= Gem::Version.new('6.1')
-          def define_url_helper(mod, name, helper, url_strategy)
-            mod.define_method(name) do |*args|
-              last = args.last
-              options = \
-                case last
-                when Hash
-                  args.pop
-                when ActionController::Parameters
-                  args.pop.to_h
-                end
-              helper.call(self, name, args, options, url_strategy)
-            end
-
-            # because we heavily rely on the removed hash_for method in routes, we must add this monkey patch.
-            mod.define_method("hash_for_#{name}") do |*args|
-              inner_options = \
-                case args.last
-                when Hash
-                  args.pop
-                when ActionController::Parameters
-                  args.pop.to_h
-                end
-              opts = helper.instance_variable_get(:@options)
-              helper.send(:handle_positional_args,
-                {},
-                inner_options || {},
-                args,
-                opts.merge(:use_route => helper.route_name),
-                helper.instance_variable_get(:@segment_keys))
-            end
+        def define_url_helper(mod, name, helper, url_strategy)
+          mod.define_method(name) do |*args|
+            last = args.last
+            options = \
+              case last
+              when Hash
+                args.pop
+              when ActionController::Parameters
+                args.pop.to_h
+              end
+            helper.call(self, name, args, options, url_strategy)
           end
-        else
-          def define_url_helper(mod, route, name, opts, route_key, url_strategy)
-            helper = UrlHelper.create(route, opts, route_key, url_strategy)
-            mod.define_method(name) do |*args|
-              last = args.last
-              options = \
-                case last
-                when Hash
-                  args.pop
-                when ActionController::Parameters
-                  args.pop.to_h
-                end
-              helper.call self, args, options
-            end
 
-            # because we heavily rely on the removed hash_for method in routes, we must add this monkey patch.
-            mod.define_method("hash_for_#{name}") do |*args|
-              inner_options = nil
-              inner_options = args.pop if args.last.is_a? Hash
-              helper.send(:handle_positional_args,
-                {},
-                inner_options || {},
-                args,
-                opts.merge(:use_route => route_key),
-                route.segment_keys.uniq)
-            end
+          # because we heavily rely on the removed hash_for method in routes, we must add this monkey patch.
+          mod.define_method("hash_for_#{name}") do |*args|
+            inner_options = \
+              case args.last
+              when Hash
+                args.pop
+              when ActionController::Parameters
+                args.pop.to_h
+              end
+            opts = helper.instance_variable_get(:@options)
+            helper.send(:handle_positional_args,
+              {},
+              inner_options || {},
+              args,
+              opts.merge(:use_route => helper.route_name),
+              helper.instance_variable_get(:@segment_keys))
           end
         end
       end