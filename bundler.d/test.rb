theforeman/foreman

commit a439e1e92e9f98f3dbc8ed2f204bd53cc861565e

Fixes #35146 - Lock rack-test < 2.0.0 (#9288)

bundler.d/test.rb
@@ -5,8 +5,11 @@ group :test do
   gem 'minitest-reporters', '~> 1.4', :require => false
   gem 'minitest-retry', '~> 0.0', :require => false
   gem 'minitest-spec-rails', '~> 6.0'
+  # Lock rack-test until some of below deps are fixed
+  gem 'rack-test', '< 2'
   gem 'capybara', '~> 3.33', :require => false
   gem 'show_me_the_cookies', '~> 5.0', :require => false
+  # End of possible broken deps
   gem 'database_cleaner', '~> 1.3', :require => false
   gem 'launchy', '~> 2.4'
   gem 'facterdb', '~> 1.7'

commit 4ce7d199e935f0c8866ea9023304705befb300e4

Fixes #35371 - Update show_me_the_cookies to 6.x
This version brings compatibility with rack-test 2.x.

Fixes: a439e1e92e9f98f3dbc8ed2f204bd53cc861565e

bundler.d/test.rb
@@ -5,11 +5,8 @@ group :test do
   gem 'minitest-reporters', '~> 1.4', :require => false
   gem 'minitest-retry', '~> 0.0', :require => false
   gem 'minitest-spec-rails', '~> 6.0'
-  # Lock rack-test until some of below deps are fixed
-  gem 'rack-test', '< 2'
   gem 'capybara', '~> 3.33', :require => false
-  gem 'show_me_the_cookies', '~> 5.0', :require => false
-  # End of possible broken deps
+  gem 'show_me_the_cookies', '~> 6.0', :require => false
   gem 'database_cleaner', '~> 1.3', :require => false
   gem 'launchy', '~> 2.4'
   gem 'facterdb', '~> 1.7'