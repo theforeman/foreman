a36fc46ee655c9c1d902b335eb6aff478069e9d6
Leos Stejskal <lstejska@redhat.com>
2022-08-18 09:19:02 +0200

    Fixes #35365 - Deprecation warning for oVirt integration
    Show warning on following pages:
    * Create compute resource
    * View compute resource
    * Create host

--- app/helpers/compute_resources_helper.rb
+++ app/helpers/compute_resources_helper.rb
@@ -116,4 +116,17 @@ module ComputeResourcesHelper
       { :label => _("HTTP Proxy") }
     )
   end
+
+  def show_rhev_deprecation?
+    Foreman::Plugin.all.map(&:name).include?('foreman_theme_satellite')
+  end
+
+  def rhev_deprecation_alert
+    header = _('RHEV integration is deprecated.')
+    text = _('On August 31, 2022, Red Hat Virtualization entered the Maintenance Support Phase. The Maintenance Support Phase runs until August 31, 2024, followed by the Extended Life Phase, which runs until August 31, 2026.')
+    link = link_to(_('More information'), 'https://access.redhat.com/announcements/6960518', target: '_blank', rel: 'noopener')
+    alert = "<strong>#{header}</strong><p>#{text}&nbsp;#{link}</p>"
+
+    content_tag(:div, alert.html_safe, class: 'alert alert-warning')
+  end
 end
