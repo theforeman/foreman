5b7bfd255565f920c74e26f53cac277772c729bd
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2022-08-25 11:55:48 +0200

    Refs #35428 - Simplify i18n.fallbacks handling
    This reverts commit 11bfc142561f1c69d2b8b5abc66df943a66b1672. Since
    Rails 6.0 the value true is interpreted as `[I18n.default_locale]`
    ([Rails commit]). ActiveSupport 6.1 also pulls in at least version 1.6
    so the specific requirement is also not needed.
    
    [Rails commit]: https://github.com/rails/rails/commit/66614f6b67402ec526a699760d1c21c586834da8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Foreman::Application.configure do |app|
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = [I18n.default_locale]
+  config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
