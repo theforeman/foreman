theforeman/foreman

commit 2c252511913090d3defe58cbc863af5c5920811d

Fixes #34997 - Fix expand/collapse all behavior in Details tab (#9416)

webpack/assets/javascripts/react_app/components/HostDetails/Templates/CardItem/CardTemplate/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import {
   Card,
   CardActions,
@@ -13,36 +13,38 @@ import {
   FlexItem,
 } from '@patternfly/react-core';
 
-import { useLocalStorage } from '../../../../../common/hooks/Storage';
-import { useDidUpdateEffect } from '../../../../../common/hooks/Common';
+import { CardExpansionContext } from '../../../Tabs/Details';
 
 const CardTemplate = ({
   header,
   children,
   expandable,
-  isExpandedGlobal,
   dropdownItems,
   overrideGridProps,
   overrideDropdownProps,
   masonryLayout,
 }) => {
-  const [dropdownVisibility, setDropdownVisibility] = useState(false);
-  const [isExpanded, setExpanded] = useLocalStorage(
-    `${header} card expanded`,
-    true
+  const { cardExpandStates, dispatch, registerCard } = useContext(
+    CardExpansionContext
   );
+  const cardId = header;
+  const [dropdownVisibility, setDropdownVisibility] = useState(false);
+  const isExpanded = expandable && cardExpandStates[`${cardId}`] === true;
   const onDropdownToggle = isOpen => setDropdownVisibility(isOpen);
-  const onExpandCallback = () => setExpanded(prevState => !prevState);
+  useEffect(() => {
+    if (expandable) registerCard(cardId);
+  }, [cardId, registerCard, expandable]);
+  const onExpandCallback = () =>
+    dispatch({
+      type: isExpanded ? 'collapse' : 'expand',
+      key: `${cardId}`,
+    });
   const onDropdownSelect = event => {
     setDropdownVisibility(prevState => !prevState);
     // https://github.com/eslint/eslint/issues/12822
     // eslint-disable-next-line no-unused-expressions
     overrideDropdownProps?.onSelect?.(event);
   };
-  useDidUpdateEffect(
-    () => isExpandedGlobal !== undefined && setExpanded(isExpandedGlobal),
-    [isExpandedGlobal]
-  );
   const CardContainer = masonryLayout ? FlexItem : GridItem;
   const gridWidthProps = masonryLayout
     ? {}
@@ -93,7 +95,6 @@ const CardTemplate = ({
 
 CardTemplate.propTypes = {
   header: PropTypes.node.isRequired,
-  isExpandedGlobal: PropTypes.bool,
   children: PropTypes.node,
   overrideGridProps: PropTypes.object,
   dropdownItems: PropTypes.arrayOf(PropTypes.node),
@@ -108,7 +109,6 @@ CardTemplate.defaultProps = {
   dropdownItems: undefined,
   overrideDropdownProps: {},
   expandable: false,
-  isExpandedGlobal: false,
   masonryLayout: false,
 };
 

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/OperatingSystem/index.js
@@ -15,19 +15,14 @@ import DefaultLoaderEmptyState from '../../../../DetailsCard/DefaultLoaderEmptyS
 import { STATUS } from '../../../../../../constants';
 import RelativeDateTime from '../../../../../common/dates/RelativeDateTime';
 
-const OperatingSystemCard = ({ status, isExpandedGlobal, hostDetails }) => {
+const OperatingSystemCard = ({ status, hostDetails }) => {
   const {
     architecture_name: architectureName,
     operatingsystem_name: osName,
     reported_data: { boot_time: bootTime } = {},
   } = hostDetails;
   return (
-    <CardTemplate
-      header={__('Operating system')}
-      expandable
-      masonryLayout
-      isExpandedGlobal={isExpandedGlobal}
-    >
+    <CardTemplate header={__('Operating system')} expandable masonryLayout>
       <DescriptionList isCompact isHorizontal>
         <DescriptionListGroup>
           <DescriptionListTerm>{__('Architecture')}</DescriptionListTerm>
@@ -89,7 +84,6 @@ const OperatingSystemCard = ({ status, isExpandedGlobal, hostDetails }) => {
 
 OperatingSystemCard.propTypes = {
   status: PropTypes.string,
-  isExpandedGlobal: PropTypes.bool,
   hostDetails: PropTypes.shape({
     architecture_name: PropTypes.string,
     operatingsystem_name: PropTypes.string,
@@ -99,7 +93,6 @@ OperatingSystemCard.propTypes = {
 
 OperatingSystemCard.defaultProps = {
   status: STATUS.PENDING,
-  isExpandedGlobal: false,
   hostDetails: {
     architecture_name: undefined,
     operatingsystem_name: undefined,

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/SystemProperties/index.js
@@ -15,7 +15,7 @@ import SkeletonLoader from '../../../../../common/SkeletonLoader';
 import DefaultLoaderEmptyState from '../../../../DetailsCard/DefaultLoaderEmptyState';
 import { STATUS } from '../../../../../../constants';
 
-const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
+const SystemPropertiesCard = ({ status, hostDetails }) => {
   const {
     name,
     uuid,
@@ -33,7 +33,6 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
       header={__('System properties')}
       expandable
       masonryLayout
-      isExpandedGlobal={isExpandedGlobal}
     >
       <DescriptionList isCompact>
         <DescriptionListGroup>
@@ -154,7 +153,6 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
 
 SystemPropertiesCard.propTypes = {
   status: PropTypes.string,
-  isExpandedGlobal: PropTypes.bool,
   hostDetails: PropTypes.shape({
     hostgroup_name: PropTypes.string,
     model_name: PropTypes.string,
@@ -172,7 +170,6 @@ SystemPropertiesCard.propTypes = {
 
 SystemPropertiesCard.defaultProps = {
   status: STATUS.PENDING,
-  isExpandedGlobal: false,
   hostDetails: {
     name: undefined,
     model_name: undefined,

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/index.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useCallback } from 'react';
 import { Flex, FlexItem, Button } from '@patternfly/react-core';
 import { registerCoreCards } from './CardRegistry';
 import Slot from '../../../common/Slot';
@@ -7,6 +7,48 @@ import { STATUS } from '../../../../constants';
 import '../Overview/styles.css';
 import './styles.css';
 import { translate as __ } from '../../../../common/I18n';
+
+export const CardExpansionContext = React.createContext({});
+
+const cardExpansionReducer = (state, action) => {
+  // A React reducer, not a Redux one!
+  switch (action.type) {
+    case 'expand':
+      return {
+        ...state,
+        [action.key]: true,
+      };
+    case 'collapse':
+      return {
+        ...state,
+        [action.key]: false,
+      };
+    case 'add':
+      if (state[action.key] === undefined) {
+        return {
+          ...state,
+          [action.key]: true,
+        };
+      }
+      return state;
+    case 'expandAll': {
+      const expandedState = {};
+      Object.keys(state).forEach(key => {
+        expandedState[key] = true;
+      });
+      return expandedState;
+    }
+    case 'collapseAll': {
+      const collapsedState = {};
+      Object.keys(state).forEach(key => {
+        collapsedState[key] = false;
+      });
+      return collapsedState;
+    }
+    default:
+      throw new Error(`invalid card expansion type: ${action.type}`);
+  }
+};
 
 const DetailsTab = ({ response, status, hostName }) => {
   useEffect(() => {
@@ -16,7 +58,38 @@ const DetailsTab = ({ response, status, hostName }) => {
     registerCoreCards();
     return () => document.body.classList.remove('pf-gray-background');
   }, []);
-  const [isExpandedGlobal, setExpandedGlobal] = useState(true);
+
+  const [cardExpandStates, dispatch] = useReducer(cardExpansionReducer, {});
+  const areAllCardsExpanded = Object.values(cardExpandStates).every(
+    value => value === true
+  );
+
+  const expandAllCards = () => dispatch({ type: 'expandAll' });
+
+  const collapseAllCards = () => dispatch({ type: 'collapseAll' });
+
+  // On mount, get values from localStorage and set them in state
+  const initializeCardFromLocalStorage = useCallback(key => {
+    const value = localStorage.getItem(`${key} card expanded`);
+    if (value !== null && value !== undefined) {
+      dispatch({ type: value === 'true' ? 'expand' : 'collapse', key });
+    } else {
+      dispatch({ type: 'add', key });
+    }
+  }, []);
+
+  // On unmount, save the values to local storage
+  // eslint-disable-next-line arrow-body-style
+  useEffect(() => {
+    return () =>
+      Object.entries(cardExpandStates).forEach(([key, value]) =>
+        localStorage.setItem(`${key} card expanded`, value)
+      );
+  });
+
+  const buttonText = areAllCardsExpanded
+    ? __('Collapse all cards')
+    : __('Expand all cards');
 
   return (
     <div className="host-details-tab-item details-tab">
@@ -24,10 +97,10 @@ const DetailsTab = ({ response, status, hostName }) => {
         <FlexItem align={{ default: 'alignRight' }}>
           <Button
             ouiaId="expand-button"
-            onClick={() => setExpandedGlobal(prev => !prev)}
+            onClick={areAllCardsExpanded ? collapseAllCards : expandAllCards}
             variant="link"
           >
-            {__('Expand/Collapse all')}
+            {buttonText}
           </Button>
         </FlexItem>
       </Flex>
@@ -36,14 +109,21 @@ const DetailsTab = ({ response, status, hostName }) => {
         flexWrap={{ default: 'wrap' }}
         className="details-tab-flex-container"
       >
-        <Slot
-          hostDetails={response}
-          status={status}
-          hostName={hostName}
-          id="host-tab-details-cards"
-          isExpandedGlobal={isExpandedGlobal}
-          multi
-        />
+        <CardExpansionContext.Provider
+          value={{
+            cardExpandStates,
+            dispatch,
+            registerCard: initializeCardFromLocalStorage,
+          }}
+        >
+          <Slot
+            hostDetails={response}
+            status={status}
+            hostName={hostName}
+            id="host-tab-details-cards"
+            multi
+          />
+        </CardExpansionContext.Provider>
       </Flex>
     </div>
   );

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/CardRegistry.js
@@ -4,8 +4,8 @@ import Properties from '../Details/Cards/SystemProperties';
 import OperatingSystem from '../Details/Cards/OperatingSystem';
 
 const cards = [
-  { key: '[core]-properties-card', Component: Properties, weight: 4000 },
-  { key: '[core]-os-card', Component: OperatingSystem, weight: 3000 },
+  { key: '[core] System properties', Component: Properties, weight: 4000 },
+  { key: '[core] Operating systems', Component: OperatingSystem, weight: 3000 },
 ];
 
 export const registerCoreCards = () => {

commit 18c97690449f5eabcea1f7516bc247f1254b8404

Refs #34997 - prevent CardTemplate from crashing
At the moment, the `CardExpansionContext` which was introduced in #9416
fails on plugins that don't use that functionality yet.
Added more conditions in order to prevent it from crashing entire tabs in the new host details page.

webpack/assets/javascripts/react_app/components/HostDetails/Templates/CardItem/CardTemplate/index.js
@@ -29,10 +29,10 @@ const CardTemplate = ({
   );
   const cardId = header;
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
-  const isExpanded = expandable && cardExpandStates[`${cardId}`] === true;
+  const isExpanded = expandable && cardExpandStates?.[`${cardId}`] === true;
   const onDropdownToggle = isOpen => setDropdownVisibility(isOpen);
   useEffect(() => {
-    if (expandable) registerCard(cardId);
+    if (expandable && registerCard) registerCard(cardId);
   }, [cardId, registerCard, expandable]);
   const onExpandCallback = () =>
     dispatch({