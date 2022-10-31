theforeman/foreman

commit 0ab14da142c9539b0f8de34c32d929d2aa5de6d4

Refs #35414 - Fix sd_notify for multiple orchestrators

extras/dynflow-sidekiq.rb
@@ -1,3 +1,5 @@
+require "sidekiq/sd_notify"
+
 rails_root = Dir.pwd
 
 app_file = File.expand_path('./config/application', rails_root)
@@ -25,5 +27,17 @@ end
   Sidekiq.options[:dynflow_world] = world
 end
 
+# To be able to ensure ordering, dynflow requires that there is only one
+# orchestrator active at the same time.
+# This is enforced by orchestrators contending a lock in redis, calls to
+# dynflow.initialize! block until the lock is acquired by the current process.
+# To play nice with sd_notify, we need to mark the process as ready before it
+# attempts to acquire the lock.
+if Sidekiq.options[:dynflow_executor]
+  Sidekiq::SdNotify.ready
+  Sidekiq::SdNotify.status('orchestrator in passive mode')
+end
 ::Rails.application.dynflow.initialize!
-Rails.logger.info("Everything ready for world: #{::Rails.application.dynflow.world.id}")
+msg = "Everything ready for world: #{::Rails.application.dynflow.world.id}"
+Rails.logger.info(msg)
+Sidekiq::SdNotify.status(msg)

commit a73afca51f7e660fc4bc3624f3af323ec5c280c6

Refs #35414 - Log an early message on dynflow startup
When the service is marked as ready, the verification starts. When
puppet-foreman configures the service using journald logging it checks
the journal. However, the application can take a while to really start
while it's already signaled to systemd it has started up. This means
verification fails.

By logging a message before sending ready we're guaranteed that there is
something to verify.

Fixes: 0ab14da142c9539b0f8de34c32d929d2aa5de6d4

extras/dynflow-sidekiq.rb
@@ -34,8 +34,10 @@ end
 # To play nice with sd_notify, we need to mark the process as ready before it
 # attempts to acquire the lock.
 if Sidekiq.options[:dynflow_executor]
+  msg = 'orchestrator in passive mode'
+  Rails.logger.info(msg)
+  Sidekiq::SdNotify.status(msg)
   Sidekiq::SdNotify.ready
-  Sidekiq::SdNotify.status('orchestrator in passive mode')
 end
 ::Rails.application.dynflow.initialize!
 msg = "Everything ready for world: #{::Rails.application.dynflow.world.id}"