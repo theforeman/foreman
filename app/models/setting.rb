theforeman/foreman

commit 662c4a3a3a1749846f5fc6c114dd3363c7a3bcf1

Fixes #34838 - Add errors when setting values are invalid (#9203)

app/models/setting.rb
@@ -140,7 +140,6 @@ class Setting < ApplicationRecord
 
       if boolean.nil?
         invalid_value_error _("must be boolean")
-        return false
       end
 
       self.value = boolean
@@ -150,7 +149,6 @@ class Setting < ApplicationRecord
         self.value = val.to_i
       else
         invalid_value_error _("must be integer")
-        return false
       end
 
     when "array"
@@ -159,11 +157,9 @@ class Setting < ApplicationRecord
           self.value = YAML.load(val.gsub(/(\,)(\S)/, "\\1 \\2"))
         rescue => e
           invalid_value_error e.to_s
-          return false
         end
       else
         invalid_value_error _("must be an array")
-        return false
       end
 
     when "string", nil
@@ -171,11 +167,14 @@ class Setting < ApplicationRecord
       self.value = NOT_STRIPPED.include?(name) ? val : val.to_s.strip
 
     when "hash"
-      raise Foreman::Exception, "parsing hash from string is not supported"
+      raise Foreman::SettingValueException, N_("parsing hash from string is not supported")
 
     else
-      raise Foreman::Exception.new(N_("parsing settings type '%s' from string is not defined"), settings_type)
+      raise Foreman::SettingValueException.new(N_("parsing settings type '%s' from string is not defined"), settings_type)
 
+    end
+    if errors.present?
+      raise Foreman::SettingValueException.new(N_("error parsing value for setting '%s': %s"), name, errors.full_messages.join(", "))
     end
     true
   end

app/controllers/api/v2/settings_controller.rb
@@ -53,6 +53,8 @@ module Api
         end
         @setting = Foreman.settings.set_user_value(@setting.name, value)
         process_response @setting.save
+      rescue Foreman::SettingValueException => e
+        render_error :custom_error, :locals => { :message => e.message }, :status => :unprocessable_entity
       end
 
       def resource_scope(_options = {})

app/services/setting_registry.rb
@@ -90,7 +90,7 @@ class SettingRegistry
     when db_record.settings_type
       db_record.value = value
     else
-      raise ::Foreman::Exception.new(N_('expected a value of type %s'), @setting.settings_type)
+      raise ::Foreman::SettingValueException.new(N_('expected a value of type %s'), @setting.settings_type)
     end
     db_record
   end

lib/foreman/exception.rb
@@ -97,6 +97,9 @@ module Foreman
   class PermissionMissingException < Foreman::Exception
   end
 
+  class SettingValueException < Foreman::Exception
+  end
+
   class LdapException < Foreman::WrappedException
   end
 

test/models/setting_test.rb
@@ -23,7 +23,7 @@ class SettingTest < ActiveSupport::TestCase
   end
 
   test 'should not strip setting value when parsing if we do not want to' do
-    setting = Setting.create(:name => 'not_stripped_test', :value => 'whatever', :setting_type => 'string')
+    setting = Setting.create(:name => 'not_stripped_test', :default => '', :description => 'not_stripped_test', :value => 'whatever', :setting_type => 'string')
     trailing_space_val = 'Local '
     setting.parse_string_value(trailing_space_val)
     assert_equal setting.value, trailing_space_val
@@ -423,8 +423,9 @@ class SettingTest < ActiveSupport::TestCase
 
   def check_parsed_value_failure(settings_type, string_value)
     setting = Setting.new(:name => "foo", :default => "default", :settings_type => settings_type)
-    setting.parse_string_value(string_value)
-
+    assert_raises Foreman::SettingValueException do
+      setting.parse_string_value(string_value)
+    end
     assert_equal "default", setting.value
     assert setting.errors[:value].join(";").include?("is invalid")
   end

webpack/assets/javascripts/react_app/components/SettingUpdateModal/components/SettingForm/SettingForm.js
@@ -25,9 +25,12 @@ const SettingForm = ({
       url: SETTING_UPDATE_PATH.replace(':id', setting.id),
       values: submitValues,
       item: 'Settings',
-      message: __('Setting was successfully updated.'),
+      message: __('Setting updated.'),
       method: 'put',
       successCallback: setModalClosed,
+      errorToast: error =>
+        `${__('Error updating setting')}: ${error.response?.data?.error
+          ?.message ?? error.message}`,
       actions,
     });
   };

commit 36f2892adfa19b84fbb52d1fb017e10922075f97

Fixes #35708 - Avoid multiple %s placeholders in i18n
Fixes: 662c4a3a3a1749846f5fc6c114dd3363c7a3bcf1

app/models/setting.rb
@@ -161,7 +161,8 @@ class Setting < ApplicationRecord
 
     end
     if errors.present?
-      raise Foreman::SettingValueException.new(N_("Error parsing value for setting '%s': %s"), name, errors.full_messages.join(", "))
+      raise Foreman::SettingValueException.new(N_("Error parsing value for setting '%(name)s': %(error)s"),
+        { 'name' => name, 'error' => errors.full_messages.join(", ") })
     end
     true
   end