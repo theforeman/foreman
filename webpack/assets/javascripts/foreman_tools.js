theforeman/foreman

commit 16b27a08a2e632c5a9c8cbf8eb8c84cf45ec2770

Fixes #30062 - DataTable isnt fully translated

webpack/assets/javascripts/foreman_tools.js
@@ -6,7 +6,7 @@
 /* eslint-disable jquery/no-class */
 
 import $ from 'jquery';
-import { translate as __ } from './react_app/common/I18n';
+import { sprintf, translate as __ } from './react_app/common/I18n';
 
 import { showLoading, hideLoading } from './foreman_navigation';
 
@@ -30,12 +30,37 @@ export function iconText(name, innerText, iconClass) {
 }
 
 export function activateDatatables() {
+  const language = {
+    searchPlaceholder: __('Filter...'),
+    emptyTable: __('No data available in table'),
+    info: sprintf(
+      __('Showing %s to %s of %s entries'),
+      '_START_',
+      '_END_',
+      '_TOTAL_'
+    ),
+    infoEmpty: __('Showing 0 to 0 of 0 entries'),
+    infoFiltered: sprintf(__('(filtered from %s total entries)'), '_MAX_'),
+    lengthMenu: sprintf(__('Show %s entries'), '_MENU_'),
+    loadingRecords: __('Loading...'),
+    processing: __('Processing...'),
+    search: __('Search:'),
+    zeroRecords: __('No matching records found'),
+    paginate: {
+      first: __('First'),
+      last: __('Last'),
+      next: __('Next'),
+      previous: __('Previous'),
+    },
+    aria: {
+      sortAscending: __(': activate to sort column ascending'),
+      sortDescending: __(': activate to sort column descending'),
+    },
+  };
   $('[data-table=inline]')
     .not('.dataTable')
     .DataTable({
-      language: {
-        searchPlaceholder: __('Filter...'),
-      },
+      language,
       dom: "<'row'<'col-md-6'f>r>t<'row'<'col-md-6'i><'col-md-6'p>>",
     });
 
@@ -45,9 +70,7 @@ export function activateDatatables() {
       const url = el.getAttribute('data-source');
 
       $(el).DataTable({
-        language: {
-          searchPlaceholder: __('Filter...'),
-        },
+        language,
         processing: true,
         serverSide: true,
         ordering: false,

webpack/assets/javascripts/foreman_tools.test.js
@@ -42,6 +42,25 @@ describe('activateDatatables', () => {
       ajax: $('[data-table=server]').data('source'),
       language: {
         searchPlaceholder: 'Filter...',
+        emptyTable: 'No data available in table',
+        info: 'Showing %s to %s of %s entries',
+        infoEmpty: 'Showing 0 to 0 of 0 entries',
+        infoFiltered: '(filtered from %s total entries)',
+        lengthMenu: 'Show %s entries',
+        loadingRecords: 'Loading...',
+        processing: 'Processing...',
+        search: 'Search:',
+        zeroRecords: 'No matching records found',
+        paginate: {
+          first: 'First',
+          last: 'Last',
+          next: 'Next',
+          previous: 'Previous',
+        },
+        aria: {
+          sortAscending: ': activate to sort column ascending',
+          sortDescending: ': activate to sort column descending',
+        },
       },
       dom: "<'row'<'col-md-6'f>r>t<'row'<'col-md-6'><'col-md-6'p>>",
     });
@@ -84,4 +103,3 @@ describe('initTypeAheadSelect', () => {
     expect($('.select2-chosen').text()).toEqual('testoption');
   });
 });
-

commit 3de9f429ddb6cf7d9a2c08f72831485e657927e6

Refs #35708 - Avoid multiple %s placeholders in i18n
Fixes: 16b27a08a2e632c5a9c8cbf8eb8c84cf45ec2770

webpack/assets/javascripts/foreman_tools.js
@@ -39,12 +39,11 @@ export function activateDatatables() {
   const language = {
     searchPlaceholder: __('Filter...'),
     emptyTable: __('No data available in table'),
-    info: sprintf(
-      __('Showing %s to %s of %s entries'),
-      '_START_',
-      '_END_',
-      '_TOTAL_'
-    ),
+    info: sprintf(__('Showing %(start)s to %(end)s of %(total)s entries'), {
+      start: '_START_',
+      end: '_END_',
+      total: '_TOTAL_',
+    }),
     infoEmpty: __('Showing 0 to 0 of 0 entries'),
     infoFiltered: sprintf(__('(filtered from %s total entries)'), '_MAX_'),
     lengthMenu: sprintf(__('Show %s entries'), '_MENU_'),