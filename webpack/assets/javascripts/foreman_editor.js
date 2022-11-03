theforeman/foreman

commit 0b167ac6bdd7be65ce632049306bc442a27dda3d

Fixes #25335 - Create a React/Redux Editor

webpack/assets/javascripts/foreman_editor.js
@@ -1,373 +1,20 @@
-import $ from 'jquery';
-import * as ace from 'brace';
+import API from './react_app/API';
+import store from './react_app/redux';
 
-import 'brace/mode/ruby';
-import 'brace/mode/json';
-import 'brace/mode/sh';
-import 'brace/mode/xml';
-import 'brace/mode/yaml';
-import 'brace/mode/diff';
-import 'brace/theme/twilight';
-import 'brace/theme/clouds';
-import 'brace/keybinding/vim';
-import 'brace/keybinding/emacs';
-import 'brace/ext/searchbox';
-import { sprintf, translate as __ } from './react_app/common/I18n';
+import * as editorActions from './react_app/components/Editor/EditorActions';
+import { translate as __ } from './react_app/common/I18n';
 
-import { initTypeAheadSelect } from './foreman_tools';
-
-let Editor;
-
-$(document).on('ContentLoad', onEditorLoad);
-
-$(document).on('click', '#editor_submit', () => {
-  if ($('.diffMode').exists()) {
-    setEditMode($('.editor_source'));
-  }
-});
-
-$(document).on('change', '.editor_file_source', e => {
-  if ($('.editor_file_source').val() !== '') {
-    editorFileSource(e);
-  }
-});
-
-$(document).on('change', '#keybinding', () => {
-  setKeybinding();
-});
-
-$(document).on('change', '#mode', () => {
-  setMode();
-});
-
-function onEditorLoad() {
-  const editorSource = $('.editor_source');
-  if (editorSource.exists()) {
-    createEditor();
-    if ($('.diffMode').exists()) {
-      setDiffMode(editorSource);
-    } else {
-      setEditMode(editorSource);
-    }
-
-    initTypeAheadSelect($('#preview_host_id'));
-  }
-}
-
-function setKeybinding() {
-  const keybindings = [
-    null, // Null = use "default" keymapping
-    'ace/keyboard/vim',
-    'ace/keyboard/emacs',
-  ];
-
-  Editor.setKeyboardHandler(keybindings[$('#keybinding')[0].selectedIndex]);
-}
-
-function setMode(mode) {
-  const session = Editor.getSession();
-  const modes = [
-    'ace/mode/text',
-    'ace/mode/json',
-    'ace/mode/ruby',
-    'ace/mode/html_ruby',
-    'ace/mode/sh',
-    'ace/mode/xml',
-    'ace/mode/yaml',
-  ];
-
-  if (mode) {
-    if (modes.indexOf(mode) >= 0) {
-      $('#mode').val(mode.replace('ace/mode/', ''));
-    }
-  } else {
-    // eslint-disable-next-line no-param-reassign
-    mode = modes[$('#mode')[0].selectedIndex];
-  }
-
-  session.setMode(mode);
-}
-
-export function showImporter() {
-  $('.editor_file_source').click();
-}
-
-/* eslint-disable max-statements, no-alert */
-function editorFileSource(evt) {
-  if (window.File && window.FileList && window.FileReader) {
-    if (
-      !window.confirm(
-        __('You are about to override the editor content, are you sure?')
-      )
-    ) {
-      $('.editor_file_source').val('');
-      return;
-    }
-
-    const { files } = evt.target; // files is a FileList object
-
-    for (const f of files) {
-      const reader = new FileReader();
-      // Closure to capture the file information.
-
-      // eslint-disable-next-line
-      reader.onloadend = function(evt) {
-        if (evt.target.readyState === FileReader.DONE) {
-          // DONE == 2
-          $('#new').val(evt.target.result);
-          setEditMode($('.editor_source'));
-        }
-      };
-      // Read in the file as text.
-      reader.readAsText(f);
-      $('.editor_file_source').val('');
-      $('.navbar-editor input[type="radio"]').blur();
-      $('.navbar-editor #option1').click();
-    }
-  } else {
-    // Browser can't read the file content,
-    // the file will be uploaded to the server on form submit.
-    // SetEditor to read only mode
-    Editor.setTheme('ace/theme/clouds');
-    Editor.setReadOnly(true);
-  }
-}
-
-export function snippetChanged(item) {
-  const checked = $(item).is(':checked');
-
-  $('#kind_selector').toggle(!checked);
-  $('#snippet_message').toggle(checked);
-  $('#association').toggle(!checked);
-  if (checked) {
-    $('#ptable_os_family').val('');
-    $('#ptable_os_family').trigger('change');
-  }
-}
-
-function createEditor() {
-  const editorId = `editor-${Math.random()}`;
-  const $editorContainer = $('.editor-container');
-  const $editorSource = $editorContainer.find('.editor_source');
-
-  $editorContainer.append(`<div id="${editorId}" class="editor"></div>`);
-  $editorSource.hide();
-
-  Editor = ace.edit(editorId);
-  Editor.$blockScrolling = Infinity;
-  Editor.setShowPrintMargin(false);
-  Editor.renderer.setShowGutter(false);
-  setMode('ace/mode/ruby');
-  $(document).on('resize', editorId, () => {
-    Editor.resize();
-  });
-  if ($editorSource.is(':disabled')) {
-    Editor.setReadOnly(true);
-  }
-  if ($editorSource.hasClass('masked-input')) {
-    $editorContainer.find('.ace_content').addClass('masked-input');
-  }
-}
-
-export function setPreview() {
-  if ($('.editor_source').hasClass('diffMode')) {
-    return;
-  }
-  $('#preview_host_selector').hide();
-  if ($('.editor_source').hasClass('renderMode')) {
-    // coming from renderMode, don't store code
-    $('.editor_source').removeClass('renderMode');
-  } else {
-    $('#new').val(Editor.getSession().getValue());
-  }
-  $('.editor_source').addClass('diffMode');
-  setDiffMode($('.editor_source'));
-}
-
-export function setCode() {
-  $('#preview_host_selector').hide();
-  $('.editor_source').removeClass('diffMode renderMode');
-  setEditMode($('.editor_source'));
-}
-
-export function setRender() {
-  if ($('.editor_source').hasClass('renderMode')) {
-    return;
-  }
-  $('#preview_host_selector').show();
-  if ($('.editor_source').hasClass('diffMode')) {
-    // coming from diffMode, don't store code
-    $('.editor_source').removeClass('diffMode');
-  } else {
-    $('#new').val(Editor.getSession().getValue());
-  }
-  $('.editor_source').addClass('renderMode');
-  setRenderMode();
-}
-
-function setEditMode(item) {
-  if (Editor === undefined) {
-    return;
-  }
-  Editor.setTheme('ace/theme/twilight');
-  if (!item.is(':disabled')) {
-    Editor.setReadOnly(false);
-  }
-
-  setMode('ace/mode/ruby');
-
-  const session = Editor.getSession();
-
-  session.setValue($('#new').val());
-  session.on('change', () => {
-    item.text(session.getValue());
-  });
-}
-
-function setDiffMode(item) {
-  Editor.setTheme('ace/theme/clouds');
-  Editor.setReadOnly(true);
-  const session = Editor.getSession();
-
-  session.setMode('ace/mode/diff');
-  const JsDiff = require('diff'); // eslint-disable-line global-require
-
-  let patch = JsDiff.createPatch(
-    item.attr('data-file-name'),
-    $('#old').val(),
-    $('#new').val()
-  );
-
-  patch = patch.replace(/^(.*\n){0,4}/, '');
-  if (patch.length === 0) {
-    patch = __('No changes');
-  }
-
-  $(session).off('change');
-  session.setValue(patch);
-}
-
-function setRenderMode() {
-  Editor.setTheme('ace/theme/twilight');
-  Editor.setReadOnly(true);
-  const session = Editor.getSession();
-
-  session.setMode('ace/mode/text');
-  $(session).off('change');
-  getRenderedTemplate();
-}
-
-export function getRenderedTemplate() {
-  const session = Editor.getSession();
-  const hostId = $('#preview_host_id').select2('val');
-  const url = $('.editor_source').data('render-path');
-  const template = $('#new').val();
-  const params = {
-    template,
-  };
-
-  if (hostId != null) {
-    /* eslint-disable camelcase */
-    params.preview_host_id = hostId;
-  }
-
-  session.setValue(__('Rendering the template, please wait...'));
-  $.post(url, params, response => {
-    $('div#preview_error').hide();
-    $('div#preview_error span.text').html('');
-    session.setValue(response);
-  }).fail(response => {
-    $('div#preview_error span.text').text(response.responseText);
-    $('div#preview_error').show();
-    session.setValue(
-      __(
-        'There was an error during rendering, return to the Code tab to edit the template.'
-      )
-    );
-  });
-}
-
-export function submitCode() {
-  if ($('.editor_source').is('.diffMode,.renderMode')) {
-    setCode();
-  }
-}
-
-/* eslint-disable max-len */
-export function revertTemplate(item) {
+export const revertTemplate = ({ dataset: { version, url } }) => {
   if (
-    !window.confirm(
-      __(
-        'You are about to override the editor content with a previous version, are you sure?'
-      )
-    )
+    window.confirm(__('Are you sure you would like to revert the Template?'))
   ) {
-    return;
+    API.get(url, {}, { version })
+      .then(res => {
+        document.getElementById('primary_tab').click();
+        store.dispatch(editorActions.changeEditorValue(res.data));
+      })
+      .catch(res => {
+        alert(__(`Revert Failed, ${res}`));
+      });
   }
-
-  const version = $(item).attr('data-version');
-  const url = $(item).attr('data-url');
-
-  $.ajax({
-    type: 'get',
-    url,
-    data: `version=${version}`,
-    complete(res) {
-      $('#primary_tab').click();
-      if ($('#new').length) {
-        $('#new').val(res.responseText);
-        setEditMode($('.editor_source'));
-      }
-      const time = $(item)
-        .closest('div.row')
-        .find('h6 span')
-        .attr('data-original-title');
-
-      $('#provisioning_template_audit_comment').text(
-        sprintf(__('Revert to revision from: %s'), time)
-      );
-    },
-  });
-}
-
-export function enterFullscreen(element, relativeTo) {
-  let $element = $(element);
-
-  if (relativeTo) {
-    $element = $(relativeTo).find(element);
-  }
-
-  $element.children().removeClass('hidden');
-  $element
-    .data('origin', $element.parent())
-    .data('position', $(window).scrollTop())
-    .addClass('fullscreen')
-    .appendTo($('.container-pf-nav-pf-vertical'))
-    .resize();
-  $('.btn-fullscreen').addClass('hidden');
-  $('.btn-exit-fullscreen').removeClass('hidden');
-
-  $('#content').addClass('hidden');
-  $(document).on('keyup', e => {
-    if (e.keyCode === 27) {
-      // esc
-      exitFullscreen();
-    }
-  });
-  Editor.resize(true);
-}
-
-export function exitFullscreen() {
-  const element = $('.fullscreen');
-
-  $('#content').removeClass('hidden');
-  element
-    .removeClass('fullscreen')
-    .prependTo(element.data('origin'))
-    .resize();
-
-  $('.btn-exit-fullscreen').addClass('hidden');
-  $('.btn-fullscreen').removeClass('hidden');
-  $(window).scrollTop(element.data('position'));
-  Editor.resize(true);
-}
+};

webpack/assets/javascripts/react_app/components/Editor/EditorActions.js
@@ -1,0 +1,272 @@
+import debounce from 'lodash/debounce';
+import API from '../../API';
+import { translate as __ } from '../../common/I18n';
+
+import {
+  EDITOR_CHANGE_DIFF_VIEW,
+  EDITOR_CHANGE_SETTING,
+  EDITOR_CHANGE_TAB,
+  EDITOR_CHANGE_VALUE,
+  EDITOR_DISMISS_ERROR,
+  EDITOR_SHOW_ERROR,
+  EDITOR_EXEC_PREVIEW,
+  EDITOR_SHOW_LOADING,
+  EDITOR_HIDE_LOADING,
+  EDITOR_IMPORT_FILE,
+  EDITOR_INITIALIZE,
+  EDITOR_MODAL_TOGGLE,
+  EDITOR_REVERT_CHANGES,
+  EDITOR_TOGGLE_MASK,
+  EDITOR_TOGGLE_RENDER_VIEW,
+  EDITOR_HOSTS_URL,
+  EDITOR_HOST_SELECT_TOGGLE,
+  EDITOR_HOST_SELECT_CLEAR,
+  EDITOR_FETCH_HOST_PENDING,
+  EDITOR_FETCH_HOST_RESOLVED,
+  EDITOR_HOST_SELECT_RESET,
+  EDITOR_HOST_ARR,
+  EDITOR_HOST_FILTERED_ARR,
+} from './EditorConstants';
+
+import {
+  selectTemplateClass,
+  selectValue,
+  selectShowError,
+  selectIsSelectOpen,
+  selectHosts,
+} from './EditorSelectors';
+
+export const initializeEditor = initializeData => dispatch => {
+  const {
+    template,
+    locked,
+    type,
+    templateClass,
+    readOnly,
+    isMasked,
+    selectedView,
+    isRendering,
+    previewResult,
+    showError,
+  } = initializeData;
+
+  const initialState = {};
+  // initialize after changing editors
+  initialState.selectedHost = { id: '', name: '' };
+  initialState.hosts = [];
+  initialState.isSearchingHosts = false;
+  initialState.value = template || '';
+  initialState.templateClass = templateClass;
+  if (readOnly !== locked) {
+    if (locked === true) initialState.readOnly = true;
+    else initialState.readOnly = false;
+  }
+  if (isMasked && type === 'templates') initialState.isMasked = false;
+  if (selectedView !== 'input') initialState.selectedView = 'input';
+  if (isRendering) initialState.isRendering = false;
+  if (previewResult !== '') initialState.previewResult = '';
+  if (showError) initialState.showError = false;
+  dispatch({
+    type: EDITOR_INITIALIZE,
+    payload: initialState,
+  });
+};
+
+export const importFile = e => dispatch => {
+  const reader = new FileReader();
+  reader.onloadstart = () => dispatch({ type: EDITOR_SHOW_LOADING });
+  reader.onloadend = () => dispatch({ type: EDITOR_HIDE_LOADING });
+  reader.onload = event => {
+    dispatch({
+      type: EDITOR_IMPORT_FILE,
+      payload: {
+        value: event.target.result,
+      },
+    });
+  };
+  reader.readAsText(e.target.files[0]);
+};
+
+export const revertChanges = template => dispatch => {
+  dispatch({
+    type: EDITOR_REVERT_CHANGES,
+    payload: {
+      value: template || '',
+      isRendering: false,
+    },
+  });
+};
+
+export const previewTemplate = ({ host, renderPath }) => (
+  dispatch,
+  getState
+) => {
+  const { id, name } = host;
+  if (selectIsSelectOpen(getState()))
+    dispatch({ type: EDITOR_HOST_SELECT_TOGGLE });
+  const templateValue = selectValue(getState());
+  const isErrorShown = selectShowError(getState());
+
+  const params = {
+    template: templateValue,
+    /* eslint-disable camelcase */
+    preview_host_id: id,
+  };
+  dispatch({ type: EDITOR_SHOW_LOADING });
+  API.post(renderPath, params)
+    .then(response => {
+      if (isErrorShown) dispatch(dismissErrorToast());
+      dispatch({ type: EDITOR_HIDE_LOADING });
+      dispatch({
+        type: EDITOR_EXEC_PREVIEW,
+        payload: {
+          renderedEditorValue: templateValue,
+          selectedHost: {
+            id: id.toString(),
+            name,
+          },
+          previewResult: response.data,
+          isSearchingHosts: false,
+        },
+      });
+    })
+    .catch(error => {
+      dispatch({ type: EDITOR_HIDE_LOADING });
+      dispatch({
+        type: EDITOR_SHOW_ERROR,
+        payload: {
+          showError: true,
+          errorText: __(error.response.data),
+          previewResult: __('Error during rendering, Return to Editor tab.'),
+          selectedHost: {
+            id: id.toString(),
+            name,
+          },
+        },
+      });
+    });
+};
+
+// fetch & debounced fetch
+const fetchHosts = (
+  query = '',
+  array = EDITOR_HOST_ARR,
+  url = EDITOR_HOSTS_URL
+) => (dispatch, getState) =>
+  createHostAPIRequest(query, array, url, dispatch, getState);
+
+const debouncedFetchHosts = (
+  query = '',
+  array = EDITOR_HOST_ARR,
+  url = EDITOR_HOSTS_URL
+) => (dispatch, getState) =>
+  debouncedCreateHostAPIRequest(query, array, url, dispatch, getState);
+
+// API & debounced API
+const createHostAPIRequest = async (query, array, url, dispatch, getState) => {
+  const onResultsSuccess = response =>
+    dispatch({
+      type: EDITOR_FETCH_HOST_RESOLVED,
+      payload: { [array]: response.data },
+    });
+
+  const onResultsError = response =>
+    dispatch({
+      type: EDITOR_SHOW_ERROR,
+      payload: {
+        showError: true,
+        errorText: __(`Host Fetch ${response}`),
+        previewResult: __('Error during rendering, Return to Editor tab.'),
+      },
+    });
+
+  try {
+    const response = await API.get(
+      url,
+      {},
+      { q: query, scope: selectTemplateClass(getState()) }
+    );
+    return onResultsSuccess(response);
+  } catch (error) {
+    return onResultsError(error);
+  }
+};
+const debouncedCreateHostAPIRequest = debounce(createHostAPIRequest, 250);
+
+export const onHostSearch = e => (dispatch, getState) => {
+  if (e.target.value === '')
+    return dispatch({ type: EDITOR_HOST_SELECT_RESET });
+
+  const payload = {
+    isFetchingHosts: true,
+    searchQuery: e.target.value,
+    isSearchingHosts: true,
+  };
+
+  dispatch({ type: EDITOR_FETCH_HOST_PENDING, payload });
+  return dispatch(
+    debouncedFetchHosts(e.target.value, EDITOR_HOST_FILTERED_ARR)
+  );
+};
+
+export const fetchAndPreview = renderPath => async (dispatch, getState) => {
+  dispatch({ type: EDITOR_SHOW_LOADING });
+  await dispatch(fetchHosts());
+  const hosts = selectHosts(getState());
+  if (hosts.length > 0)
+    dispatch(previewTemplate({ host: hosts[0], renderPath }));
+  else dispatch({ type: EDITOR_HIDE_LOADING });
+};
+
+export const toggleModal = () => ({
+  type: EDITOR_MODAL_TOGGLE,
+});
+
+export const changeDiffViewType = viewType => dispatch => {
+  dispatch({
+    type: EDITOR_CHANGE_DIFF_VIEW,
+    payload: viewType,
+  });
+};
+
+export const changeEditorValue = value => dispatch => {
+  dispatch({
+    type: EDITOR_CHANGE_VALUE,
+    payload: value,
+  });
+};
+
+export const dismissErrorToast = () => dispatch => {
+  dispatch({
+    type: EDITOR_DISMISS_ERROR,
+    payload: { showError: false, errorText: '' },
+  });
+};
+
+export const changeTab = selectedView => dispatch => {
+  dispatch({
+    type: EDITOR_CHANGE_TAB,
+    payload: selectedView,
+  });
+};
+
+export const toggleMaskValue = () => ({
+  type: EDITOR_TOGGLE_MASK,
+});
+
+export const changeSetting = newSetting => dispatch => {
+  dispatch({
+    type: EDITOR_CHANGE_SETTING,
+    payload: newSetting,
+  });
+};
+
+export const toggleRenderView = isRendering => ({
+  type: EDITOR_TOGGLE_RENDER_VIEW,
+});
+
+export const onSearchClear = () => ({ type: EDITOR_HOST_SELECT_CLEAR });
+
+export const onHostSelectToggle = () => ({
+  type: EDITOR_HOST_SELECT_TOGGLE,
+});

app/assets/stylesheets/base.scss
@@ -193,10 +193,6 @@ select {
   color: #4d5258 !important;
 }
 
-.ace_editor {
-  border: 2px solid #eee;
-}
-
 .audit-content {
   border-bottom: 1px solid #ccc;
   margin-bottom: 9px;

app/assets/stylesheets/editor.scss
@@ -1,41 +1,0 @@
-.editor.ace_editor {
-  position: relative;
-  height: 360px;
-  width: 100%;
-
-  .ace_invisible {
-    color: #575757;
-  }
-}
-
-.navbar-full-width {
-  width: 100%;
-}
-
-.navbar-nav.navbar-right.navbar-editor {
-  margin-right: 0;
-
-  li {
-    padding-right: 5px;
-  }
-}
-
-.editor-container.fullscreen {
-  height: 82vh !important;
-  .navbar-editor {
-    margin: 0;
-  }
-
-  .clearfix {
-    display: none;
-  }
-
-  .editor {
-    height: 100%;
-  }
-}
-
-span#preview_host_selector a.select2-choice {
-  height: 22px;
-  line-height: 1.2;
-}

app/helpers/editor_helper.rb
@@ -1,0 +1,5 @@
+module EditorHelper
+  def template_name_attribute(template)
+    "#{template.to_s.underscore}[template]"
+  end
+end

app/views/editor/_toolbar.html.erb
@@ -1,58 +1,0 @@
-<div class="navbar navbar-form navbar-full-width navbar-editor">
-
-    <div class="btn-group" data-toggle="buttons" >
-      <label class="btn btn-default btn-sm active" onclick="tfm.editor.setCode()">
-        <input type="radio" id="option1" ><%= _("Input") %>
-      </label>
-      <label class="btn btn-default btn-sm" onclick="tfm.editor.setPreview()">
-        <input type="radio" id="option2" ><%= _("Diff") %>
-      </label>
-      <% if show_preview %>
-        <label class="btn btn-default btn-sm" onclick="tfm.editor.setRender()">
-          <input type="radio" id="option3" ><%= _("Preview") %>
-        </label>
-      <% end %>
-    </div>
-
-    <% if show_import %>
-      <a class="btn btn-default btn-sm" onclick="tfm.editor.showImporter()">
-        <%= _("Import") %>
-      </a>
-    <% end %>
-
-    <% if show_preview %>
-      <span id="preview_host_selector" style="display:none">
-        <%= text_field_tag :preview_host_id, nil,
-            :'data-url' => preview_host_collection_hosts_path,
-            :'data-scope' => controller.send(:model_of_controller),
-            :name => '',
-            :onchange => 'tfm.editor.getRenderedTemplate()' %>
-      </span>
-    <% end %>
-
-    <a class="btn btn-default btn-sm btn-fullscreen" href="#" onclick="tfm.editor.enterFullscreen('.editor-container');">
-      <%= _('Fullscreen') %>
-    </a>
-
-    <a class="btn btn-default btn-sm btn-exit-fullscreen hidden" href="#" onclick="tfm.editor.exitFullscreen();">
-      <%= _('Exit Fullscreen') %>
-    </a>
-
-    <ul class="nav navbar-nav navbar-right navbar-editor">
-      <li>
-        <select id="mode" class="form-control input-sm without_select2">
-          <%= content_tag(:optgroup, options_for_select(['text', 'json', 'ruby', 'erb', 'sh', 'xml', 'yaml']), :label => _('Syntax Highlighting')) %>
-        </select>
-      </li>
-
-      <li>
-        <select id="keybinding" class="form-control input-sm without_select2">
-          <%= content_tag(:optgroup, options_for_select([_('Default'), 'Vim', 'Emacs']), :label => _('Key Binding')) %>
-        </select>
-      </li>
-    </ul>
-
-    <% if show_import %>
-      <%= file_field_tag :import_file, :class => "hidden editor_file_source", :id => 'import_file' %>
-    <% end %>
-</div>

app/views/provisioning_templates/_custom_tabs.html.erb
@@ -1,5 +1,5 @@
 <div class="tab-pane" id="template_type">
-  <%= checkbox_f f, :snippet, :onchange => "tfm.editor.snippetChanged(this)", :label=>_('Snippet'), :disabled => @template.locked? %>
+  <%= checkbox_f f, :snippet, :onchange => "tfm.templateInputs.snippetChanged(this)", :label=>_('Snippet'), :disabled => @template.locked? %>
 
   <div id="kind_selector" <%= display? @template.snippet %>>
     <%= select_f f, :template_kind_id, TemplateKind.all, :id, :to_s, {:include_blank => true, :selected => @template_kind_id},

app/views/ptables/_custom_tabs.html.erb
@@ -1,5 +1,5 @@
 <div class="clearfix">
-  <%= checkbox_f f, :snippet, :onchange => "tfm.editor.snippetChanged(this)", :label=>_('Snippet'), :disabled => @template.locked? %>
+  <%= checkbox_f f, :snippet, :onchange => "tfm.templateInputs.snippetChanged(this)", :label=>_('Snippet'), :disabled => @template.locked? %>
   <%= snippet_message(@template) %>
   <div id="association" <%= display? @template.snippet %> >
     <%= select_f f, :os_family, Operatingsystem.families_as_collection, :value, :name, { :include_blank => _("Choose a family")  }, { :label => _("Operating System Family") } %>

commit 6c476d09ca7c1e9b3faf603bf7668819802151db

Refs #35708 - Avoid string interpolation with translations
Fixes: 0b167ac6bdd7be65ce632049306bc442a27dda3d

webpack/assets/javascripts/foreman_editor.js
@@ -2,7 +2,7 @@ import { API } from './react_app/redux/API';
 import store from './react_app/redux';
 
 import * as editorActions from './react_app/components/Editor/EditorActions';
-import { translate as __ } from './react_app/common/I18n';
+import { sprintf, translate as __ } from './react_app/common/I18n';
 import { openConfirmModal } from './foreman_tools';
 
 export const revertTemplate = ({ dataset: { version, url } }) => {
@@ -15,7 +15,7 @@ export const revertTemplate = ({ dataset: { version, url } }) => {
         document.getElementById('primary_tab').click();
         store.dispatch(editorActions.changeEditorValue(response.data));
       } catch (err) {
-        alert(__(`Revert Failed, ${err}`));
+        alert(sprintf(__('Revert Failed, %s'), err));
       }
     },
   });

webpack/assets/javascripts/react_app/components/Editor/EditorActions.js
@@ -1,6 +1,6 @@
 import { debounce, toString } from 'lodash';
 import { API } from '../../redux/API';
-import { translate as __ } from '../../common/I18n';
+import { sprintf, translate as __ } from '../../common/I18n';
 
 import {
   EDITOR_CHANGE_DIFF_VIEW,
@@ -183,7 +183,7 @@ const createHostAPIRequest = async (query, array, url, dispatch, getState) => {
       type: EDITOR_SHOW_ERROR,
       payload: {
         showError: true,
-        errorText: __(`Host Fetch ${response}`),
+        errorText: sprintf(__('Host Fetch %s'), response),
         previewResult: __('Error during rendering, Return to Editor tab.'),
       },
     });