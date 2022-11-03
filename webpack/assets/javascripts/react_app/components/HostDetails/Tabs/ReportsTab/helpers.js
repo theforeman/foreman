theforeman/foreman

commit 616c0d3b8638613124a51671ba807f0f8930d743

Fixes #34782 - Add reports tab in host page (#9190)
* Fixes #34782 - Add reports tab in host page

* Refs #34782 - use Foreman EmptyState

also added proper error message display

* Refs #34782 - use origin_image_path

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/ReportsTab/helpers.js
@@ -1,0 +1,188 @@
+/* eslint-disable camelcase */
+/* eslint-disable react/prop-types */
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  KebabToggle,
+  FlexItem,
+  Flex,
+} from '@patternfly/react-core';
+import {
+  ExclamationCircleIcon,
+  SyncAltIcon,
+  CheckCircleIcon,
+  PendingIcon,
+  AngleDoubleRightIcon,
+} from '@patternfly/react-icons';
+import { openConfirmModal } from '../../../ConfirmModal';
+import { APIActions } from '../../../../redux/API';
+import { translate as __ } from '../../../../common/I18n';
+import RelativeDateTime from '../../../common/dates/RelativeDateTime';
+
+const statusMapper = {
+  failed: amount => (
+    <>
+      <ExclamationCircleIcon color="var(--pf-global--palette--red-100)" />{' '}
+      {amount}
+    </>
+  ),
+  failed_restarts: amount => (
+    <>
+      <SyncAltIcon color="var(--pf-global--palette--red-100)" /> {amount}
+    </>
+  ),
+  restarted: amount => (
+    <>
+      <SyncAltIcon color="var(--pf-global--palette--orange-300)" /> {amount}
+    </>
+  ),
+  applied: amount => (
+    <>
+      <CheckCircleIcon color="var(--pf-global--success-color--100)" /> {amount}
+    </>
+  ),
+  skipped: amount => (
+    <>
+      <AngleDoubleRightIcon /> {amount}
+    </>
+  ),
+  pending: amount => (
+    <>
+      <PendingIcon /> {amount}
+    </>
+  ),
+};
+
+export const statusFormatter = (statusName, response) => {
+  const amount = response[statusName];
+  if (!amount) return '--';
+  return statusMapper[statusName](amount);
+};
+
+export const originFormatter = ({ origin: { src, label } }) =>
+  src ? (
+    <>
+      <img
+        alt={`${label}-icon`}
+        src={src}
+        style={{ width: '15px', marginBottom: '3px' }}
+      />{' '}
+      {label}
+    </>
+  ) : (
+    '--'
+  );
+
+export const ActionFormatter = ({ id, can_delete }, fetchReports) => {
+  const [isOpen, setOpen] = useState(false);
+  const dispatch = useDispatch();
+  const dispatchConfirm = () => {
+    dispatch(
+      openConfirmModal({
+        isWarning: true,
+        title: __('Delete report?'),
+        confirmButtonText: __('Delete report'),
+        onConfirm: () =>
+          dispatch(
+            APIActions.delete({
+              url: `/api/v2/config_reports/${id}`,
+              key: `report-${id}-DELETE`,
+              successToast: success => __('Report was successfully deleted'),
+              errorToast: error =>
+                __(`There was some issue deleting the report: ${error}`),
+              handleSuccess: fetchReports,
+            })
+          ),
+        message: __(
+          'Are you sure you want to delete this report? This action is irreversible.'
+        ),
+      })
+    );
+  };
+  const dropdownItems = [
+    <DropdownItem
+      key="action"
+      component="button"
+      onClick={dispatchConfirm}
+      disabled={!can_delete}
+    >
+      {__('Delete')}
+    </DropdownItem>,
+  ];
+  return (
+    <Flex>
+      <FlexItem align={{ default: 'alignRight' }}>
+        <Dropdown
+          onSelect={v => setOpen(!v)}
+          toggle={<KebabToggle onToggle={setOpen} id="toggle-action" />}
+          isOpen={isOpen}
+          isPlain
+          dropdownItems={dropdownItems}
+        />
+      </FlexItem>
+    </Flex>
+  );
+};
+
+export const reportToShowFormatter = ({ reported_at, can_view, id }) => (
+  <Button
+    variant="link"
+    component="a"
+    isInline
+    isDisabled={!can_view}
+    href={`/config_reports/${id}`}
+  >
+    <RelativeDateTime date={reported_at} />
+  </Button>
+);
+
+export const getColumns = fetchReports => [
+  {
+    title: __('Reported at'),
+    formatter: reportToShowFormatter,
+    width: 20,
+  },
+  {
+    title: __('Failed'),
+    formatter: response => statusFormatter('failed', response),
+    width: 10,
+  },
+  {
+    title: __('Failed restarts'),
+    formatter: response => statusFormatter('failed_restarts', response),
+    width: 10,
+  },
+  {
+    title: __('Restarted'),
+    formatter: response => statusFormatter('restarted', response),
+    width: 10,
+  },
+  {
+    title: __('Applied'),
+    formatter: response => statusFormatter('applied', response),
+    width: 10,
+  },
+  {
+    title: __('Skipped'),
+    formatter: response => statusFormatter('skipped', response),
+    width: 10,
+  },
+  {
+    title: __('Pending'),
+    formatter: response => statusFormatter('pending', response),
+    width: 10,
+  },
+  {
+    title: __('Origin'),
+    formatter: originFormatter,
+    width: 10,
+  },
+  {
+    title: null,
+    formatter: data => ActionFormatter(data, fetchReports),
+    width: 10,
+  },
+];

.eslintrc
@@ -13,6 +13,7 @@
         "skipWords": [
           "2xl",
           "4xl",
+          "Ansible",
           "Autocompletion",
           "Borderless",
           "IPv4",

app/controllers/config_reports_controller.rb
@@ -6,6 +6,25 @@ class ConfigReportsController < ApplicationController
 
   def index
     respond_to do |format|
+      format.json do
+        reports = resource_base_search_and_page(:host)
+        render json: {
+          itemCount: reports.count,
+          reports: reports.map do |r|
+            r.attributes.except('metrics', 'created_at', 'updated_at', 'status', 'type').merge(
+              can_delete: can_delete?(r),
+              can_view: can_view?(r),
+              origin: origin_image_path(r),
+              applied: r.applied,
+              restarted: r.restarted,
+              failed: r.failed,
+              failed_restarts: r.failed_restarts,
+              skipped: r.skipped,
+              pending: r.pending
+            )
+          end,
+        }, status: :ok
+      end
       format.html do
         @host = resource_finder(Host.authorized(:view_hosts), params[:host_id]) if params[:host_id]
         @config_reports = resource_base_search_and_page(:host)
@@ -48,5 +67,21 @@ class ConfigReportsController < ApplicationController
 
   def resource_base
     super.my_reports
+  end
+
+  def can_delete?(report)
+    authorized_for(auth_object: report, authorizer: authorizer, permission: "destroy_#{controller_permission}")
+  end
+
+  def can_view?(report)
+    authorized_for(auth_object: report, authorizer: authorizer, permission: "view_#{controller_permission}")
+  end
+
+  def origin_image_path(report)
+    @origin_image_paths ||= {
+      Ansible: helpers.image_path('Ansible.png'),
+      Puppet: helpers.image_path('Puppet.png'),
+    }
+    { src: @origin_image_paths[:"#{report.origin}"], label: report.origin }
   end
 end

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/ReportsTab/ReportsTable.js
@@ -1,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  TableComposable,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+} from '@patternfly/react-table';
+import { Spinner } from '@patternfly/react-core';
+import { SearchIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
+import { STATUS } from '../../../../constants';
+import EmptyState from '../../../common/EmptyState';
+import { translate as __ } from '../../../../common/I18n';
+import { getColumns } from './helpers';
+
+const ReportsTable = ({ reports, status, fetchReports, error }) => {
+  const columns = getColumns(fetchReports);
+  let tableBody = null;
+  let tableHead = null;
+  let emptyState = null;
+
+  if (status === STATUS.RESOLVED) {
+    if (reports.length) {
+      tableHead = (
+        <Thead>
+          <Tr>
+            {columns.map(({ width, title }, columnIndex) => (
+              <Th key={columnIndex} width={width}>
+                {title}
+              </Th>
+            ))}
+          </Tr>
+        </Thead>
+      );
+      tableBody = (
+        <Tbody>
+          {reports.map((row, rowIndex) => (
+            <Tr key={rowIndex}>
+              {columns.map(({ title, formatter }, cellIndex) => (
+                <Td key={`${rowIndex}_${cellIndex}`} dataLabel={title}>
+                  {formatter(row)}
+                </Td>
+              ))}
+            </Tr>
+          ))}
+        </Tbody>
+      );
+    } else {
+      emptyState = (
+        <EmptyState icon={<SearchIcon />} header={__('No results found')} />
+      );
+    }
+  } else if (status === STATUS.ERROR) {
+    emptyState = (
+      <EmptyState
+        icon={
+          <ExclamationCircleIcon color="var(--pf-global--palette--red-200)" />
+        }
+        header={__('Something went wrong')}
+        description={error}
+      />
+    );
+  } else if (status === STATUS.PENDING) {
+    emptyState = <EmptyState icon={<Spinner />} header={__('Loading')} />;
+  }
+
+  return (
+    <React.Fragment>
+      <TableComposable aria-label="Reports table" variant="compact">
+        {tableHead}
+        {tableBody}
+      </TableComposable>
+      {emptyState}
+    </React.Fragment>
+  );
+};
+
+ReportsTable.propTypes = {
+  reports: PropTypes.array,
+  status: PropTypes.string,
+  error: PropTypes.string,
+  fetchReports: PropTypes.func.isRequired,
+};
+
+ReportsTable.defaultProps = {
+  reports: [],
+  status: null,
+  error: null,
+};
+
+export default ReportsTable;

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/ReportsTab/index.js
@@ -1,0 +1,151 @@
+/* eslint-disable camelcase */
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Grid, GridItem } from '@patternfly/react-core';
+import URI from 'urijs';
+import SearchBar from '../../../SearchBar';
+import Pagination from '../../../Pagination';
+import { get } from '../../../../redux/API';
+import {
+  selectAPIStatus,
+  selectAPIResponse,
+  selectAPIErrorMessage,
+} from '../../../../redux/API/APISelectors';
+import { useForemanSettings } from '../../../../Root/Context/ForemanContext';
+import ReportsTable from './ReportsTable';
+import { getControllerSearchProps } from '../../../../constants';
+
+const ReportsTab = ({ hostName, origin }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const API_KEY = `get-reports-${hostName}`;
+  const { reports, itemCount } = useSelector(state =>
+    selectAPIResponse(state, API_KEY)
+  );
+  const { perPage: settingsPerPage = 20 } = useForemanSettings() || {};
+  const status = useSelector(state => selectAPIStatus(state, API_KEY));
+  const error = useSelector(state => selectAPIErrorMessage(state, API_KEY));
+
+  const fetchReports = useCallback(
+    ({ search: searchParam, per_page: perPageParam, page: pageParam } = {}) => {
+      if (!hostName) return;
+      const {
+        page: urlPage,
+        perPage: urlPerPage,
+        search: urlSearch,
+      } = getUrlParams();
+      const search = searchParam !== undefined ? searchParam : urlSearch;
+      const page = pageParam || urlPage;
+      const per_page = perPageParam || urlPerPage;
+      dispatch(
+        get({
+          key: API_KEY,
+          url: '/config_reports',
+          params: {
+            page,
+            per_page,
+            search: getServerQuery(search),
+          },
+        })
+      );
+      updateUrl({ page, per_page, search });
+    },
+    [API_KEY, dispatch, getServerQuery, getUrlParams, updateUrl, hostName]
+  );
+
+  useEffect(() => {
+    fetchReports();
+  }, [fetchReports, history.location]);
+
+  const onPaginationChange = ({ page, per_page }) => {
+    const { search } = getUrlParams();
+    updateUrl({ page, per_page, search });
+  };
+
+  const getServerQuery = useCallback(
+    search => {
+      const serverQuery = [`host = ${hostName}`];
+      if (origin) {
+        serverQuery.push(`origin = ${origin}`);
+      }
+      if (search) {
+        serverQuery.push(`(${search})`);
+      }
+      return serverQuery.join(' AND ');
+    },
+    [origin, hostName]
+  );
+
+  const getUrlParams = useCallback(() => {
+    const params = { page: 1, perPage: settingsPerPage, search: '' };
+    const urlSearch = history.location?.search;
+    const urlParams = urlSearch && new URLSearchParams(urlSearch);
+    if (urlParams) {
+      params.search = urlParams.get('search') || params.search;
+      params.page = Number(urlParams.get('page')) || params.page;
+      params.perPage = Number(urlParams.get('per_page')) || params.perPage;
+    }
+    return params;
+  }, [history.location, settingsPerPage]);
+
+  const updateUrl = useCallback(
+    ({ page, per_page, search = '' }) => {
+      const uri = new URI();
+      uri.search({ page, per_page, search });
+      history.push({ search: uri.search() });
+    },
+    [history]
+  );
+
+  return (
+    <Grid id="new_host_details_insights_tab" hasGutter style={{ padding: 24 }}>
+      <GridItem span={6}>
+        <SearchBar
+          data={{
+            ...getControllerSearchProps('/config_reports'),
+            controller: 'config_reports',
+          }}
+          onSearch={search => fetchReports({ search, page: 1 })}
+          onBookmarkClick={search => fetchReports({ search, page: 1 })}
+          initialQuery={getUrlParams().search}
+        />
+      </GridItem>
+      <GridItem span={6}>
+        <Pagination
+          variant="top"
+          itemCount={itemCount}
+          onChange={onPaginationChange}
+        />
+      </GridItem>
+      <GridItem>
+        <ReportsTable
+          reports={reports}
+          status={status}
+          error={error}
+          fetchReports={fetchReports}
+        />
+      </GridItem>
+      <GridItem>
+        <Pagination
+          variant="bottom"
+          itemCount={itemCount}
+          onChange={onPaginationChange}
+        />
+      </GridItem>
+    </Grid>
+  );
+};
+
+ReportsTab.propTypes = {
+  hostName: PropTypes.string,
+  origin: PropTypes.string,
+};
+
+ReportsTab.defaultProps = {
+  origin: null,
+  hostName: null,
+};
+
+export default ReportsTab;

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/index.js
@@ -4,6 +4,7 @@ import { addGlobalFill } from '../../common/Fill/GlobalFill';
 import { DEFAULT_TAB, TABS_SLOT_ID } from '../consts';
 import OverviewTab from './Overview';
 import DetailTab from './Details';
+import ReportsTab from './ReportsTab';
 
 export const registerCoreTabs = ({ except = [] }) => {
   addGlobalFill(
@@ -21,6 +22,15 @@ export const registerCoreTabs = ({ except = [] }) => {
     {
       title: __('Details'),
       hideTab: () => except.includes('host-details-detail-tab'),
+    }
+  );
+  addGlobalFill(
+    TABS_SLOT_ID,
+    'Reports',
+    <ReportsTab key="host-details-reports-tab" />,
+    477,
+    {
+      title: __('Reports'),
     }
   );
 };

webpack/assets/javascripts/react_app/components/common/EmptyState/EmptyStatePropTypes.js
@@ -18,8 +18,7 @@ export const emptyStatePatternPropTypes = {
     }),
     PropTypes.node,
   ]),
-  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node])
-    .isRequired,
+  description: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
   action: PropTypes.node,
   secondaryActions: PropTypes.node,
 };

commit 58c821dca18e6606648181a6644cf5da3cc5796a

Refs #35708 - Avoid string interpolation with translations
Fixes: 616c0d3b8638613124a51671ba807f0f8930d743

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/ReportsTab/helpers.js
@@ -19,7 +19,7 @@ import {
 } from '@patternfly/react-icons';
 import { openConfirmModal } from '../../../ConfirmModal';
 import { APIActions } from '../../../../redux/API';
-import { translate as __ } from '../../../../common/I18n';
+import { sprintf, translate as __ } from '../../../../common/I18n';
 import RelativeDateTime from '../../../common/dates/RelativeDateTime';
 
 const statusMapper = {
@@ -92,7 +92,10 @@ export const ActionFormatter = ({ id, can_delete }, fetchReports) => {
               key: `report-${id}-DELETE`,
               successToast: success => __('Report was successfully deleted'),
               errorToast: error =>
-                __(`There was some issue deleting the report: ${error}`),
+                sprintf(
+                  __('There was some issue deleting the report: %s'),
+                  error
+                ),
               handleSuccess: fetchReports,
             })
           ),