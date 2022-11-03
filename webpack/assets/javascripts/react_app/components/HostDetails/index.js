theforeman/foreman

commit 1e06a71164535fc1192472481ff87e22f19f19ac

Fixes #30984 - translate host relative date creation

webpack/assets/javascripts/react_app/components/HostDetails/index.js
@@ -29,10 +29,12 @@ import { selectIsCollapsed } from '../Layout/LayoutSelectors';
 import ActionsBar from './ActionsBar';
 import Slot from '../common/Slot';
 import { registerCoreTabs } from './Tabs';
-import { translate as __ } from '../../common/I18n';
+import { sprintf, translate as __ } from '../../common/I18n';
 import HostGlobalStatus from './Status/GlobalStatus';
 
 import './HostDetails.scss';
+import SkeletonLoader from '../common/SkeletonLoader';
+import { STATUS } from '../../constants';
 
 const HostDetails = ({
   match: {
@@ -121,26 +123,23 @@ const HostDetails = ({
               <ActionsBar hostName={response.name} />
             </GridItem>
           </Grid>
-          <Text style={{ fontStyle: 'italic' }} component={TextVariants.p}>
-            {/* TODO: extracting text and remove timeago usage in favor i18n */}
-            {response.name ? (
-              <span>
-                created{' '}
-                <RelativeDateTime
-                  date={response.created_at}
-                  defaultValue="N/A"
-                />{' '}
-                by {response.owner_name} (updated{' '}
-                <RelativeDateTime
-                  date={response.updated_at}
-                  defaultValue="N/A"
-                />
-                )
-              </span>
-            ) : (
-              <Skeleton width={400} />
+          <SkeletonLoader
+            skeletonProps={{ width: 400 }}
+            status={status || STATUS.PENDING}
+          >
+            {response && (
+              <Text component={TextVariants.span}>
+                <RelativeDateTime date={response.created_at} defaultValue="N/A">
+                  {date =>
+                    sprintf(__('Created %s by %s'), date, response.owner_name)
+                  }
+                </RelativeDateTime>{' '}
+                <RelativeDateTime date={response.updated_at} defaultValue="N/A">
+                  {date => sprintf(__('(updated %s)'), date)}
+                </RelativeDateTime>
+              </Text>
             )}
-          </Text>
+          </SkeletonLoader>
           <br />
         </div>
         <Tabs

webpack/assets/javascripts/react_app/components/common/dates/RelativeDateTime.js
@@ -4,7 +4,7 @@ import { FormattedRelative, intlShape } from 'react-intl';
 import { isoCompatibleDate } from '../../../common/helpers';
 
 const RelativeDateTime = (props, context) => {
-  const { date, defaultValue } = props;
+  const { date, defaultValue, children } = props;
   if (date) {
     const isoDate = isoCompatibleDate(date);
     const title = context.intl.formatDate(isoDate, {
@@ -18,7 +18,9 @@ const RelativeDateTime = (props, context) => {
     /* eslint-disable react/style-prop-object */
     return (
       <span title={title}>
-        <FormattedRelative value={isoDate} style="numeric" />
+        <FormattedRelative value={isoDate} style="numeric">
+          {children}
+        </FormattedRelative>
       </span>
     );
     /* eslint-enable react/style-prop-object */
@@ -34,11 +36,13 @@ RelativeDateTime.contextTypes = {
 RelativeDateTime.propTypes = {
   date: PropTypes.any,
   defaultValue: PropTypes.node,
+  children: PropTypes.func,
 };
 
 RelativeDateTime.defaultProps = {
   date: null,
   defaultValue: '',
+  children: undefined,
 };
 
 export default RelativeDateTime;

commit ad8c93d0ad63f42d9ed0496a4ce88cee85c2bb72

Refs #35708 - Avoid multiple %s placeholders in i18n
Fixes: 1e06a71164535fc1192472481ff87e22f19f19ac

webpack/assets/javascripts/react_app/components/HostDetails/index.js
@@ -217,7 +217,10 @@ const HostDetails = ({
               <Text ouiaId="date-text" component={TextVariants.span}>
                 <RelativeDateTime date={response.created_at} defaultValue="N/A">
                   {date =>
-                    sprintf(__('Created %s by %s'), date, response.owner_name)
+                    sprintf(__('Created %(date)s by %(owner)s'), {
+                      date,
+                      owner: response.owner_name,
+                    })
                   }
                 </RelativeDateTime>{' '}
                 <RelativeDateTime date={response.updated_at} defaultValue="N/A">