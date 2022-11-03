theforeman/foreman

commit 6730cf2a5b440021373a5687dcc2adacfd61bd1d

Fixes #25888 - move components from Katello to Foreman
- Add the 'ModalProgressBar' and 'Settings' components to Foreman
- Add some of the helper functions to Foreman's helper

webpack/assets/javascripts/react_app/components/common/ModalProgressBar/ModalProgressBar.js
@@ -1,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Modal, ProgressBar } from 'patternfly-react';
+import { sprintf, translate as __ } from '../../../common/I18n';
+import './ModalProgressBar.scss';
+
+const ModalProgressBar = ({ show, container, title, progress }) => (
+  <Modal id="modal-progress-bar" show={show} container={container}>
+    <Modal.Header>
+      <Modal.Title>{title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <ProgressBar
+        active
+        now={progress}
+        label={sprintf(__(`${progress}%% Complete`))}
+      />
+    </Modal.Body>
+  </Modal>
+);
+
+ModalProgressBar.propTypes = {
+  show: PropTypes.bool.isRequired,
+  container: PropTypes.shape({}),
+  title: PropTypes.string,
+  progress: PropTypes.number,
+};
+
+ModalProgressBar.defaultProps = {
+  container: document.body,
+  title: null,
+  progress: 0,
+};
+
+export default ModalProgressBar;

webpack/assets/javascripts/react_app/common/helpers.js
@@ -128,6 +128,15 @@ const propsToCase = (casingFn, errorMsg, ob) => {
   }, {});
 };
 
+/**
+ * Check if a string is a positive integer
+ * @param {String} value - the string
+ */
+export const stringIsPositiveNumber = value => {
+  const reg = new RegExp('^[0-9]+$');
+  return reg.test(value);
+};
+
 export default {
   isoCompatibleDate,
   bindMethods,
@@ -140,4 +149,5 @@ export default {
   translateArray,
   propsToCamelCase,
   propsToSnakeCase,
+  stringIsPositiveNumber,
 };

webpack/assets/javascripts/react_app/common/helpers.test.js
@@ -5,6 +5,7 @@ import {
   propsToSnakeCase,
   propsToCamelCase,
   removeLastSlashFromPath,
+  stringIsPositiveNumber,
 } from './helpers';
 
 describe('isoCompatibleDate', () => {
@@ -56,5 +57,17 @@ describe('removeLastSlashFromPath', () => {
   });
   it('should not change the path', () => {
     expect(removeLastSlashFromPath(pathWithoutSlash)).toBe('example.com');
+  });
+});
+
+describe('stringIsPositiveNumber', () => {
+  it('should return true on positive number', () => {
+    expect(stringIsPositiveNumber('1')).toBeTruthy();
+  });
+  it('should return false on negative number', () => {
+    expect(stringIsPositiveNumber('-1')).toBeFalsy();
+  });
+  it('should return false on a word', () => {
+    expect(stringIsPositiveNumber('number')).toBeFalsy();
   });
 });

webpack/assets/javascripts/react_app/common/keyCodes.js
@@ -1,0 +1,8 @@
+export const KEYCODES = {
+  ENTER: 13,
+  FWD_SLASH: 47,
+  BACK_SLASH: 46,
+  ESC: 27,
+  TAB_KEY: 9,
+};
+export default KEYCODES;

webpack/assets/javascripts/react_app/common/urlHelpers.js
@@ -1,0 +1,15 @@
+/**
+ * Build a url from given controller, action and id
+ * @param {String} controller - the controller
+ * @param {String} action - the action
+ */
+export const urlBuilder = (controller, action, id = undefined) =>
+  `/${controller}/${id ? `${id}/` : ''}${action}`;
+
+/**
+ * Build a url with search query
+ * @param {String} base - the base url
+ * @param {String} searchQuery - the search query
+ */
+export const urlWithSearch = (base, searchQuery) =>
+  `/${base}?search=${searchQuery}`;

webpack/assets/javascripts/react_app/common/urlHelpers.test.js
@@ -1,0 +1,23 @@
+import { urlBuilder, urlWithSearch } from './urlHelpers';
+
+describe('urlBuilder', () => {
+  const controller = 'testController';
+  const action = 'testAction';
+  it('builds url with id', () => {
+    expect(urlBuilder(controller, action, 'testID')).toBe(
+      '/testController/testID/testAction'
+    );
+  });
+
+  it('builds url without id', () => {
+    expect(urlBuilder(controller, action)).toBe('/testController/testAction');
+  });
+});
+
+describe('urlWithSearch', () => {
+  const base = 'testBase';
+  const query = 'query=test';
+  it('builds url with search', () => {
+    expect(urlWithSearch(base, query)).toBe('/testBase?search=query=test');
+  });
+});

webpack/assets/javascripts/react_app/components/AutoComplete/AutoComplete.js
@@ -10,7 +10,8 @@ import AutoCompleteError from './components/AutoCompleteError';
 import AutoCompleteAux from './components/AutoCompleteAux';
 import AutoCompleteFocusShortcut from './components/AutoCompleteFocusShortcut';
 import { STATUS } from '../../constants';
-import { TRIGGERS, KEYCODES } from './AutoCompleteConstants';
+import { TRIGGERS } from './AutoCompleteConstants';
+import { KEYCODES } from '../../common/keyCodes';
 import { translate as __ } from '../../common/I18n';
 import './auto-complete.scss';
 

webpack/assets/javascripts/react_app/components/AutoComplete/AutoCompleteConstants.js
@@ -3,12 +3,6 @@ export const AUTO_COMPLETE_REQUEST = 'AUTO_COMPLETE_REQUEST';
 export const AUTO_COMPLETE_SUCCESS = 'AUTO_COMPLETE_SUCCESS';
 export const AUTO_COMPLETE_FAILURE = 'AUTO_COMPLETE_FAILURE';
 export const AUTO_COMPLETE_RESET = 'AUTO_COMPLETE_RESET';
-export const KEYCODES = {
-  ENTER: 13,
-  FWD_SLASH: 47,
-  BACK_SLASH: 46,
-  ESC: 27,
-};
 export const TRIGGERS = {
   INPUT_FOCUS: 'INPUT_FOCUS',
   INPUT_CHANGE: 'INPUT_CHANGE',

commit d307314602c72b3c14795af092fc3fd22fd9ab22

Refs #35708 - Avoid string interpolation with translations
This was using sprintf incorrectly, which meant the string wasn't
translated.

Fixes: 6730cf2a5b440021373a5687dcc2adacfd61bd1d

webpack/assets/javascripts/react_app/components/common/ModalProgressBar/ModalProgressBar.js
@@ -18,7 +18,7 @@ const ModalProgressBar = ({ show, container, title, progress }) => (
       <ProgressBar
         active
         now={progress}
-        label={sprintf(__(`${progress}%% Complete`))}
+        label={sprintf(__('%s%% Complete'), progress)}
       />
     </Modal.Body>
   </Modal>