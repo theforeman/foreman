df2bbe1f128898e1272e8241d6686c919a4d6e96
Evgeni Golov <evgeni@golov.de>
2022-11-04 08:25:31 +0100

    Fixes #35717 - use caller_locations in deprecation warnings
    Rails prefers to have it that way for better reporting

--- app/services/foreman/deprecation.rb
+++ app/services/foreman/deprecation.rb
@@ -3,7 +3,7 @@ module Foreman
     # deadline_version - is the version the deprecation is going to be deleted, the format must be a major release e.g "1.8"
     def self.deprecation_warning(foreman_version_deadline, info)
       check_version_format foreman_version_deadline
-      ActiveSupport::Deprecation.warn("You are using a deprecated behavior, it will be removed in version #{foreman_version_deadline}, #{info}", caller(2))
+      ActiveSupport::Deprecation.warn("You are using a deprecated behavior, it will be removed in version #{foreman_version_deadline}, #{info}", caller_locations(2))
     end
 
     def self.check_version_format(foreman_version_deadline)
