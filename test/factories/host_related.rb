theforeman/foreman

commit 641c3f4a9bb5401a8ac06e70b925918125193957

Fixes #31370 - multi-host stub template snapshots (#8149)
Our snapshot template render test and generator only supports one host stub (ipv4 single NIC redhat OS). In order to test more ERB codepaths, we need a way to create snapshots against multiple host stubs (e.g. IPv6, dualstack, static IPv4 or IPv6, debian family). In order to do that, template metadata can be extended to define list of host stubs to create and test snapshots against.

Snapshot files are now named ProvisioningTemplate/PXEGrub/Kickstart_default_PXEGrub.host4and6.snap.txt where host4and6 is the stub name, this implements the following for starters:

* host - the original simple host
* host6 - IPv6 provisioning NIC
* host4and6 - dualstack provisioning NIC
* host4static - single stack IPv4 with static network credentials
* host6static - single stack IPv4 with static network credentials

app/services/foreman/template_snapshot_service.rb
@@ -6,13 +6,30 @@ module Foreman
       new.templates
     end
 
-    def self.host
-      new.host
+    def self.host4dhcp
+      new.host4dhcp
     end
 
-    def self.render_template(template)
+    def self.host6dhcp
+      new.host6dhcp
+    end
+
+    def self.host4and6dhcp
+      new.host4and6dhcp
+    end
+
+    def self.host4static
+      new.host4static
+    end
+
+    def self.host6static
+      new.host6static
+    end
+
+    def self.render_template(template, host_name = :host4dhcp)
+      host_stub = send(host_name.to_sym)
       source = Foreman::Renderer::Source::Snapshot.new(template)
-      scope = Foreman::Renderer.get_scope(host: host, source: source)
+      scope = Foreman::Renderer.get_scope(host: host_stub, source: source)
       Foreman::Renderer.render(source, scope)
     end
 
@@ -20,29 +37,7 @@ module Foreman
       files.map { |path| Foreman::Renderer::Source::Snapshot.load_file(path) }
     end
 
-    def host
-      interface = FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
-                                   mac: '00-f0-54-1a-7e-e0',
-                                   ip: '127.0.0.1')
-      domain = FactoryBot.build(:domain, name: 'snapshotdomain.com')
-      subnet = FactoryBot.build(:subnet_ipv4, name: 'one', network: interface.ip)
-      architecture = FactoryBot.build(:architecture, name: 'x86_64')
-      medium = FactoryBot.build(:medium, name: 'CentOS mirror')
-      ptable = FactoryBot.build(:ptable, name: 'ptable')
-      operatingsystem = FactoryBot.build(:operatingsystem, name: 'Redhat',
-                                         major: 7,
-                                         architectures: [architecture],
-                                         media: [medium],
-                                         ptables: [ptable])
-      host = FactoryBot.build(:host, :managed, hostname: 'snapshothost',
-                       domain: domain,
-                       subnet: subnet,
-                       architecture: architecture,
-                       medium: medium,
-                       ptable: ptable,
-                       operatingsystem: operatingsystem,
-                       interfaces: [interface])
-
+    def define_host_params(host)
       host_params = {
         "enable-epel" => "true",
         "package_upgrade" => "true",
@@ -61,6 +56,66 @@ module Foreman
         host_params[name]
       end
       host
+    end
+
+    def ipv4_interface
+      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
+        mac: '00-f0-54-1a-7e-e0',
+        ip: '192.168.42.42')
+    end
+
+    def ipv6_interface
+      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
+        mac: '00-f0-54-1a-7e-e0',
+        ip: '2001:db8:42::42')
+    end
+
+    def ipv46_interface
+      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
+        mac: '00-f0-54-1a-7e-e0',
+        ip: '192.168.42.42',
+        ip6: '2001:db8:42::42')
+    end
+
+    def host4dhcp
+      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+        name: 'snapshot-ipv4-dhcp-el7',
+        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
+        interfaces: [ipv4_interface])
+      define_host_params(host)
+    end
+
+    def host4static
+      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+        name: 'snapshot-ipv4-static-el7',
+        subnet: FactoryBot.build(:subnet_ipv4_static_for_snapshots),
+        interfaces: [ipv4_interface])
+      define_host_params(host)
+    end
+
+    def host6dhcp
+      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+        name: 'snapshot-ipv6-dhcp-el7',
+        subnet: FactoryBot.build(:subnet_ipv6_dhcp_for_snapshots),
+        interfaces: [ipv6_interface])
+      define_host_params(host)
+    end
+
+    def host6static
+      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+        name: 'snapshot-ipv6-static-el7',
+        subnet: FactoryBot.build(:subnet_ipv6_static_for_snapshots),
+        interfaces: [ipv6_interface])
+      define_host_params(host)
+    end
+
+    def host4and6dhcp
+      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+        name: 'snapshot-ipv4-6-dhcp-el7',
+        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
+        subnet6: FactoryBot.build(:subnet_ipv6_dhcp_for_snapshots),
+        interfaces: [ipv46_interface])
+      define_host_params(host)
     end
 
     private

app/views/unattended/provisioning_templates/snippet/kickstart_kernel_options.erb
@@ -33,6 +33,7 @@ snippet: true
 
   # networking credentials
   raise("Dual-stack provisioning not supported") if subnet4 && subnet6
+  options.push("dualstack!") if subnet4 && subnet6
   if subnet4 && subnet4.dhcp_boot_mode?
     options.push("ip=dhcp") if rhel_compatible && major >= 7
   elsif subnet4 && !subnet4.dhcp_boot_mode?
@@ -50,7 +51,7 @@ snippet: true
         options.push("ip=#{iface.ip}::#{subnet4.gateway}:#{subnet4.mask}:#{hostname}:#{iface.identifier}:none")
       end
     end
-  elsif subnet6.dhcp_boot_mode? && rhel_compatible && major >= 7
+  elsif subnet6 && subnet6.dhcp_boot_mode? && rhel_compatible && major >= 7
     if host_param_true?("use-slaac")
       options.push("ip=auto6")
     else

test/factories/host_related.rb
@@ -320,6 +320,24 @@ FactoryBot.define do
       end
     end
 
+    factory :host_for_snapshots_ipv4_dhcp_el7 do
+      name { 'snapshot-ipv4-dhcp-el7' }
+      hostname { name }
+      managed { true }
+      operatingsystem { FactoryBot.build(:for_snapshots_centos_7_0) }
+      domain { FactoryBot.build(:domain_for_snapshots) }
+      subnet { FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots) }
+      pxe_loader { "PXELinux BIOS" }
+      architecture { operatingsystem.try(:architectures).try(:first) }
+      medium { operatingsystem.try(:media).try(:first) }
+      ptable { operatingsystem.try(:ptables).try(:first) }
+      root_pass { '$1$rtd8Ub7R$5Ohzuy8WXlkaK9cA2T1wb0' }
+      environment { FactoryBot.build(:environment, :for_snapshots_test) }
+      certname { name }
+      puppet_proxy { FactoryBot.build(:puppet_smart_proxy, name: 'puppet-proxy', url: 'http://localhost:8448') }
+      puppet_ca_proxy { FactoryBot.build(:puppet_ca_smart_proxy, name: 'puppetca-proxy', url: 'http://localhost:8448') }
+    end
+
     trait :with_dhcp_orchestration do
       managed
       compute_resource do

test/factories/subnet.rb
@@ -8,6 +8,7 @@ FactoryBot.define do
     ipam { "None" }
     organizations { [Organization.find_by_name('Organization 1')] }
     locations { [Location.find_by_name('Location 1')] }
+    boot_mode { :DHCP }
 
     # Skip the Subnet.after_validation hook that validates against External IPAM API
     after(:build) do |subnet|
@@ -80,6 +81,67 @@ FactoryBot.define do
       mask { Array.new(4, 'ffff').join(':') + '::' }
 
       factory :subnet_ipv6_with_domains, :traits => [:with_domains]
+    end
+
+    trait :proxies_for_snapshots do
+      # ability to build more than one smart proxies with the same url or name for snapshot testing
+      association :tftp, :ignore_validations, :factory => :template_smart_proxy, :name => "snapshot-proxy-tftp", :url => "http://localhost:8001"
+      association :httpboot, :ignore_validations, :factory => :template_smart_proxy, :name => "snapshot-proxy-httpboot", :url => "http://localhost:8002"
+      association :dhcp, :ignore_validations, :factory => :dhcp_smart_proxy, :name => "snapshot-proxy-dhcp", :url => "http://localhost:8003"
+      association :dns, :ignore_validations, :factory => :dns_smart_proxy, :name => "snapshot-proxy-dns", :url => "http://localhost:8004"
+      association :bmc, :ignore_validations, :factory => :bmc_smart_proxy, :name => "snapshot-proxy-bmc", :url => "http://localhost:8005"
+    end
+
+    factory :subnet_ipv4_dhcp_for_snapshots, :class => Subnet::Ipv4 do
+      network { '192.168.42.0' }
+      mask { '255.255.255.0' }
+      name { 'snapshot-ipv4-dhcp' }
+      gateway { '192.168.42.1' }
+      dns_primary { '192.168.42.2' }
+      dns_secondary { '192.168.42.3' }
+      mtu { '1142' }
+      boot_mode { :DHCP }
+      domains { [FactoryBot.build(:domain_for_snapshots)] }
+      proxies_for_snapshots
+    end
+
+    factory :subnet_ipv4_static_for_snapshots, :class => Subnet::Ipv4 do
+      network { '192.168.42.0' }
+      mask { '255.255.255.0' }
+      name { 'snapshot-ipv4-static' }
+      gateway { '192.168.42.1' }
+      dns_primary { '192.168.42.2' }
+      dns_secondary { '192.168.42.3' }
+      mtu { '1242' }
+      boot_mode { :Static }
+      domains { [FactoryBot.build(:domain_for_snapshots)] }
+      proxies_for_snapshots
+    end
+
+    factory :subnet_ipv6_dhcp_for_snapshots, :class => Subnet::Ipv6 do
+      network { '2001:db8:42::' }
+      mask { 'ffff:ffff:ffff::' }
+      name { 'snapshot-ipv6-dhcp' }
+      gateway { '2001:db8:42::1' }
+      dns_primary { '2001:db8:42::8' }
+      dns_secondary { '2001:db8:42::4' }
+      mtu { '1342' }
+      boot_mode { :DHCP }
+      domains { [FactoryBot.build(:domain_for_snapshots)] }
+      proxies_for_snapshots
+    end
+
+    factory :subnet_ipv6_static_for_snapshots, :class => Subnet::Ipv6 do
+      network { '2001:db8:42::' }
+      mask { 'ffff:ffff:ffff::' }
+      name { 'snapshot-ipv6-static' }
+      gateway { '2001:db8:42::1' }
+      dns_primary { '2001:db8:42::8' }
+      dns_secondary { '2001:db8:42::4' }
+      mtu { '1442' }
+      boot_mode { :Static }
+      domains { [FactoryBot.build(:domain_for_snapshots)] }
+      proxies_for_snapshots
     end
   end
 end

commit 1c18ddfb53d76a0bb7f5d4e4a4ab671a78060090

Fixes #35779 - Correct generated provision snapshots
Previously it had incorrect data where IPv6 addresses were set in IPv4
subnets. This generated incorrect snapshots.

It uses traits to reduce duplication.

Fixes: 641c3f4a9bb5401a8ac06e70b925918125193957

app/services/foreman/template_snapshot_service.rb
@@ -6,44 +6,8 @@ module Foreman
       new.templates
     end
 
-    def self.host4dhcp
-      new.host4dhcp
-    end
-
-    def self.host6dhcp
-      new.host6dhcp
-    end
-
-    def self.host4and6dhcp
-      new.host4and6dhcp
-    end
-
-    def self.host4static
-      new.host4static
-    end
-
-    def self.host6static
-      new.host6static
-    end
-
-    def self.debian4dhcp
-      new.debian4dhcp
-    end
-
-    def self.ubuntu4dhcp
-      new.ubuntu4dhcp
-    end
-
-    def self.ubuntu_autoinst4dhcp
-      new.ubuntu_autoinst4dhcp
-    end
-
-    def self.rhel9_dhcp
-      new.rhel9_dhcp
-    end
-
     def self.render_template(template, host_name = :host4dhcp)
-      host_stub = send(host_name.to_sym)
+      host_stub = new.send(host_name.to_sym)
       source = Foreman::Renderer::Source::Snapshot.new(template)
       scope = Foreman::Renderer.get_scope(host: host_stub, source: source)
       Foreman::Renderer.render(source, scope)
@@ -55,6 +19,7 @@ module Foreman
 
     def define_host_params(host)
       host_params = {
+        "dual_stack_provision_fallback" => "IPv6",
         "enable-epel" => "true",
         "package_upgrade" => "true",
         "ansible_tower_provisioning" => "true",
@@ -81,95 +46,66 @@ module Foreman
       host
     end
 
-    def ipv4_interface
-      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
-        mac: '00-f0-54-1a-7e-e0',
-        ip: '192.168.42.42')
-    end
-
-    def ipv6_interface
-      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
-        mac: '00-f0-54-1a-7e-e0',
-        ip: '2001:db8:42::42')
-    end
-
-    def ipv46_interface
-      FactoryBot.build(:nic_primary_and_provision, identifier: 'eth0',
-        mac: '00-f0-54-1a-7e-e0',
-        ip: '192.168.42.42',
-        ip6: '2001:db8:42::42')
-    end
-
     def host4dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+      host = FactoryBot.build(:host_for_snapshots, :with_c7,
         name: 'snapshot-ipv4-dhcp-el7',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp)])
       define_host_params(host)
     end
 
     def host4static
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+      host = FactoryBot.build(:host_for_snapshots, :with_c7,
         name: 'snapshot-ipv4-static-el7',
-        subnet: FactoryBot.build(:subnet_ipv4_static_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_static)])
       define_host_params(host)
     end
 
     def host6dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+      host = FactoryBot.build(:host_for_snapshots, :with_c7,
         name: 'snapshot-ipv6-dhcp-el7',
-        subnet: FactoryBot.build(:subnet_ipv6_dhcp_for_snapshots),
-        interfaces: [ipv6_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v6_dhcp)])
       define_host_params(host)
     end
 
     def host6static
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+      host = FactoryBot.build(:host_for_snapshots, :with_c7,
         name: 'snapshot-ipv6-static-el7',
-        subnet: FactoryBot.build(:subnet_ipv6_static_for_snapshots),
-        interfaces: [ipv6_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v6_static)])
       define_host_params(host)
     end
 
     def host4and6dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_el7,
+      host = FactoryBot.build(:host_for_snapshots, :with_c7,
         name: 'snapshot-ipv4-6-dhcp-el7',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        subnet6: FactoryBot.build(:subnet_ipv6_dhcp_for_snapshots),
-        interfaces: [ipv46_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp, :with_v6_dhcp)])
       define_host_params(host)
     end
 
     def debian4dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_deb10,
+      host = FactoryBot.build(:host_for_snapshots, :with_deb10,
         name: 'snapshot-ipv4-dhcp-deb10',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp)])
       define_host_params(host)
     end
 
     def ubuntu4dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_ubuntu18,
+      host = FactoryBot.build(:host_for_snapshots, :with_ubuntu18,
         name: 'snapshot-ipv4-dhcp-ubuntu18',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp)])
       define_host_params(host)
     end
 
     def ubuntu_autoinst4dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_ubuntu20,
+      host = FactoryBot.build(:host_for_snapshots, :with_ubuntu20,
         name: 'snapshot-ipv4-dhcp-ubuntu20',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp)])
       define_host_params(host)
     end
 
     def rhel9_dhcp
-      host = FactoryBot.build(:host_for_snapshots_ipv4_dhcp_rhel9,
+      host = FactoryBot.build(:host_for_snapshots, :with_rhel9,
         name: 'snapshot-ipv4-dhcp-rhel9',
-        subnet: FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots),
-        interfaces: [ipv4_interface])
+        interfaces: [FactoryBot.build(:nic_for_snapshots, :with_v4_dhcp)])
       define_host_params(host)
     end
 

app/views/unattended/provisioning_templates/snippet/kickstart_kernel_options.erb
@@ -59,6 +59,8 @@ description: |
     elsif dual_stack_fallback == 'IPv6'
       subnet4 = false
     else
+      # Dracut does not support dual stack
+      # https://bugzilla.redhat.com/show_bug.cgi?id=1008076
       raise("Dual-stack provisioning not supported, set parameter 'dual_stack_provision_fallback'")
     end
   end

test/factories/host_related.rb
@@ -32,6 +32,8 @@ FactoryBot.define do
       os_family { 'Debian' }
     end
 
+    factory :ptable_ubuntu_autoinstall, traits: [:ubuntu_autoinstall]
+
     trait :suse do
       sequence(:name) { |n| "suse default#{n}" }
       layout { "<partitioning  config:type=\"list\">\n  <drive>\n    <device>/dev/hda</device>\n    <use>all</use>\n  </drive>\n</partitioning>" }
@@ -108,6 +110,33 @@ FactoryBot.define do
     provision { true }
     sequence(:mac) { |n| "00:53:67:ab:" + n.to_s(16).rjust(4, '0').insert(2, ':') }
     sequence(:ip) { |n| IPAddr.new(n, Socket::AF_INET).to_s }
+  end
+
+  trait :with_v4_dhcp do
+    ip { '192.168.42.42' }
+    subnet factory: :subnet_ipv4_dhcp_for_snapshots
+  end
+
+  trait :with_v4_static do
+    ip { '192.168.42.42' }
+    subnet factory: :subnet_ipv4_static_for_snapshots
+  end
+
+  trait :with_v6_dhcp do
+    ip6 { '2001:db8:42::42' }
+    subnet6 factory: :subnet_ipv6_dhcp_for_snapshots
+  end
+
+  trait :with_v6_static do
+    ip6 { '2001:db8:42::42' }
+    subnet6 factory: :subnet_ipv6_static_for_snapshots
+  end
+
+  factory :nic_for_snapshots, parent: :nic_managed, class: Nic::Managed do
+    primary { true }
+    provision { true }
+    identifier { 'eth0' }
+    mac { '00-f0-54-1a-7e-e0' }
   end
 
   factory :model do
@@ -294,12 +323,11 @@ FactoryBot.define do
       end
     end
 
-    factory :host_for_snapshots_base do
+    factory :host_for_snapshots do
       name { 'snapshot-ipv4-dhcp-el7' }
       hostname { name }
       managed { true }
-      domain { FactoryBot.build(:domain_for_snapshots) }
-      subnet { FactoryBot.build(:subnet_ipv4_dhcp_for_snapshots) }
+      domain factory: :domain_for_snapshots
       pxe_loader { "PXELinux BIOS" }
       architecture { operatingsystem.try(:architectures).try(:first) }
       medium { operatingsystem.try(:media).try(:first) }
@@ -307,25 +335,29 @@ FactoryBot.define do
       root_pass { '$1$rtd8Ub7R$5Ohzuy8WXlkaK9cA2T1wb0' }
       certname { name }
 
-      factory :host_for_snapshots_ipv4_dhcp_el7 do
-        operatingsystem { FactoryBot.build(:for_snapshots_centos_7_0) }
+      trait :with_c7 do
+        name { 'snapshot-c7' }
+        operatingsystem factory: :for_snapshots_centos_7_0
       end
 
-      factory :host_for_snapshots_ipv4_dhcp_deb10 do
-        operatingsystem { FactoryBot.build(:for_snapshots_debian_10) }
+      trait :with_deb10 do
+        name { 'snapshot-d10' }
+        operatingsystem factory: :for_snapshots_debian_10
       end
 
-      factory :host_for_snapshots_ipv4_dhcp_ubuntu18 do
-        operatingsystem { FactoryBot.build(:for_snapshots_ubuntu_18) }
+      trait :with_ubuntu18 do
+        name { 'snapshot-u18' }
+        operatingsystem factory: :for_snapshots_ubuntu_18
       end
 
-      factory :host_for_snapshots_ipv4_dhcp_ubuntu20 do
-        ptable { FactoryBot.build(:ptable, :ubuntu_autoinstall) }
-        operatingsystem { FactoryBot.build(:for_snapshots_ubuntu_20) }
+      trait :with_ubuntu20 do
+        name { 'snapshot-u20' }
+        ptable factory: :ptable_ubuntu_autoinstall
+        operatingsystem factory: :for_snapshots_ubuntu_20
       end
 
-      factory :host_for_snapshots_ipv4_dhcp_rhel9 do
-        operatingsystem { FactoryBot.build(:for_snapshots_rhel9) }
+      trait :with_rhel9 do
+        operatingsystem factory: :for_snapshots_rhel9
       end
     end
 

test/factories/subnet.rb
@@ -92,56 +92,53 @@ FactoryBot.define do
       association :bmc, :ignore_validations, :factory => :bmc_smart_proxy, :name => "snapshot-proxy-bmc", :url => "http://localhost:8005"
     end
 
-    factory :subnet_ipv4_dhcp_for_snapshots, :class => Subnet::Ipv4 do
+    trait :snapshot do
+      domains { [FactoryBot.build(:domain_for_snapshots)] }
+      proxies_for_snapshots
+    end
+
+    trait :ipv4 do
       network { '192.168.42.0' }
       mask { '255.255.255.0' }
+      gateway { '192.168.42.1' }
+      dns_primary { '192.168.42.2' }
+      dns_secondary { '192.168.42.3' }
+    end
+
+    trait :ipv6 do
+      network { '2001:db8:42::' }
+      mask { 'ffff:ffff:ffff::' }
+      gateway { '2001:db8:42::1' }
+      dns_primary { '2001:db8:42::8' }
+      dns_secondary { '2001:db8:42::4' }
+    end
+
+    trait :dhcp do
+      boot_mode { :DHCP }
+    end
+
+    trait :static do
+      boot_mode { :Static }
+    end
+
+    factory :subnet_ipv4_dhcp_for_snapshots, class: Subnet::Ipv4, traits: [:snapshot, :ipv4, :dhcp] do
       name { 'snapshot-ipv4-dhcp' }
-      gateway { '192.168.42.1' }
-      dns_primary { '192.168.42.2' }
-      dns_secondary { '192.168.42.3' }
-      mtu { '1142' }
-      boot_mode { :DHCP }
-      domains { [FactoryBot.build(:domain_for_snapshots)] }
-      proxies_for_snapshots
-    end
-
-    factory :subnet_ipv4_static_for_snapshots, :class => Subnet::Ipv4 do
-      network { '192.168.42.0' }
-      mask { '255.255.255.0' }
-      name { 'snapshot-ipv4-static' }
-      gateway { '192.168.42.1' }
-      dns_primary { '192.168.42.2' }
-      dns_secondary { '192.168.42.3' }
-      mtu { '1242' }
-      boot_mode { :Static }
-      domains { [FactoryBot.build(:domain_for_snapshots)] }
-      proxies_for_snapshots
-    end
-
-    factory :subnet_ipv6_dhcp_for_snapshots, :class => Subnet::Ipv6 do
-      network { '2001:db8:42::' }
-      mask { 'ffff:ffff:ffff::' }
-      name { 'snapshot-ipv6-dhcp' }
-      gateway { '2001:db8:42::1' }
-      dns_primary { '2001:db8:42::8' }
-      dns_secondary { '2001:db8:42::4' }
       mtu { '1342' }
-      boot_mode { :DHCP }
-      domains { [FactoryBot.build(:domain_for_snapshots)] }
-      proxies_for_snapshots
     end
 
-    factory :subnet_ipv6_static_for_snapshots, :class => Subnet::Ipv6 do
-      network { '2001:db8:42::' }
-      mask { 'ffff:ffff:ffff::' }
-      name { 'snapshot-ipv6-static' }
-      gateway { '2001:db8:42::1' }
-      dns_primary { '2001:db8:42::8' }
-      dns_secondary { '2001:db8:42::4' }
+    factory :subnet_ipv4_static_for_snapshots, class: Subnet::Ipv4, traits: [:snapshot, :ipv4, :static] do
+      name { 'snapshot-ipv4-static' }
       mtu { '1442' }
-      boot_mode { :Static }
-      domains { [FactoryBot.build(:domain_for_snapshots)] }
-      proxies_for_snapshots
+    end
+
+    factory :subnet_ipv6_dhcp_for_snapshots, class: Subnet::Ipv6, traits: [:snapshot, :ipv6, :dhcp] do
+      name { 'snapshot-ipv6-dhcp' }
+      mtu { '1342' }
+    end
+
+    factory :subnet_ipv6_static_for_snapshots, class: Subnet::Ipv6, traits: [:snapshot, :ipv6, :static] do
+      name { 'snapshot-ipv6-static' }
+      mtu { '1442' }
     end
   end
 end