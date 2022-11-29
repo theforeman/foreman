theforeman/foreman

commit ec35c4d82e493574da0b55e0d52dd24dbc6b638f

Fixes #31080 - Add UI for managing Personal Access Tokens (#7806)

webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/PersonalAccessTokensActions.js
@@ -1,0 +1,53 @@
+import URI from 'urijs';
+import { API } from '../../../redux/API';
+import { addToast } from '../../../redux/actions/toasts';
+import { ajaxRequestAction } from '../../../redux/actions/common';
+import { translate as __ } from '../../../common/I18n';
+import {
+  PERSONAL_ACCESS_TOKEN_CLEAR,
+  PERSONAL_ACCESS_TOKENS_REQUEST,
+  PERSONAL_ACCESS_TOKENS_SUCCESS,
+  PERSONAL_ACCESS_TOKENS_FAILURE,
+} from './PersonalAccessTokensConstants';
+
+export const getPersonalAccessTokens = ({ url }) => dispatch => {
+  const uri = new URI(url);
+  // eslint-disable-next-line camelcase
+  uri.setSearch({ per_page: 9999 });
+
+  ajaxRequestAction({
+    dispatch,
+    url: uri,
+    requestAction: PERSONAL_ACCESS_TOKENS_REQUEST,
+    successAction: PERSONAL_ACCESS_TOKENS_SUCCESS,
+    failedAction: PERSONAL_ACCESS_TOKENS_FAILURE,
+  });
+};
+
+export const revokePersonalAccessToken = ({ url, id }) => async dispatch => {
+  try {
+    await API.delete(`${url}/${id}`);
+    dispatch(getPersonalAccessTokens({ url }));
+    dispatch(
+      addToast({
+        type: 'success',
+        message: __('Token was successfully revoked.'),
+      })
+    );
+  } catch (error) {
+    /* eslint-disable no-console */
+    console.log(error);
+    dispatch(
+      addToast({
+        type: 'error',
+        message: __('Could not revoke Token: ') + error,
+      })
+    );
+  }
+};
+
+export const clearNewPersonalAccessToken = () => dispatch =>
+  dispatch({
+    type: PERSONAL_ACCESS_TOKEN_CLEAR,
+    payload: {},
+  });

webpack/assets/javascripts/react_app/components/common/ClipboardCopy/index.js
@@ -1,0 +1,57 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Button, OverlayTrigger, Tooltip } from 'patternfly-react';
+import UUID from 'uuid/v1';
+import { translate as __ } from '../../../common/I18n';
+import './clipboard-copy.scss';
+
+const ClipboardCopy = ({
+  text: defaultText,
+  successMessage,
+  buttonText,
+  textareaProps,
+  buttonProps,
+}) => {
+  const [text, setText] = useState(defaultText);
+
+  return (
+    <div className="clipboard-copy">
+      <textarea
+        defaultValue={text}
+        onChange={({ target: { value } }) => setText(value)}
+        {...textareaProps}
+      />
+      <OverlayTrigger
+        overlay={<Tooltip id={UUID()}>{successMessage}</Tooltip>}
+        placement="right"
+        trigger={['click']}
+        rootClose
+      >
+        <Button
+          onClick={() => navigator.clipboard.writeText(text)}
+          bsStyle="default"
+          {...buttonProps}
+        >
+          {buttonText}
+        </Button>
+      </OverlayTrigger>
+    </div>
+  );
+};
+
+ClipboardCopy.propTypes = {
+  text: PropTypes.string.isRequired,
+  buttonText: PropTypes.string,
+  successMessage: PropTypes.string,
+  textareaProps: PropTypes.object,
+  buttonProps: PropTypes.object,
+};
+
+ClipboardCopy.defaultProps = {
+  buttonText: __('Copy to clipboard'),
+  successMessage: __('Copied!'),
+  textareaProps: {},
+  buttonProps: {},
+};
+
+export default ClipboardCopy;

webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/PersonalAccessTokensList/index.js
@@ -1,0 +1,53 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import PersonalAccessToken from './PersonalAccessToken';
+import { translate as __ } from '../../../../common/I18n';
+import { noop } from '../../../../common/helpers';
+
+const PersonalAccessTokensList = ({
+  title,
+  tokens,
+  revocable,
+  revokePersonalAccessToken,
+}) => (
+  <Fragment>
+    <h3>{`${title} (${tokens.length})`}</h3>
+    {tokens.length > 0 && (
+      <table className="table table-bordered table-striped table-hover table-fixed">
+        <thead>
+          <tr>
+            <th>{__('Name')}</th>
+            <th>{__('Created')}</th>
+            <th>{revocable ? __('Expires') : __('Expired')}</th>
+            <th>{__('Last Used')}</th>
+            <th>{__('Actions')}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tokens.map(token => (
+            <PersonalAccessToken
+              key={token.id}
+              {...token}
+              revokePersonalAccessToken={revokePersonalAccessToken}
+            />
+          ))}
+        </tbody>
+      </table>
+    )}
+  </Fragment>
+);
+
+PersonalAccessTokensList.propTypes = {
+  tokens: PropTypes.array.isRequired,
+  title: PropTypes.string,
+  revokePersonalAccessToken: PropTypes.func,
+  revocable: PropTypes.bool,
+};
+
+PersonalAccessTokensList.defaultProps = {
+  revokePersonalAccessToken: noop,
+  title: __('Personal Access Tokens'),
+  revocable: false,
+};
+
+export default PersonalAccessTokensList;

webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/index.js
@@ -1,0 +1,6 @@
+import PersonalAccessTokens from './PersonalAccessTokens';
+import reducer from './PersonalAccessTokensReducer';
+
+export const reducers = { personalAccessTokens: reducer };
+
+export default PersonalAccessTokens;

webpack/assets/javascripts/react_app/redux/reducers/index.js
@@ -21,6 +21,7 @@ import { reducers as modalReducers } from '../../components/ForemanModal';
 import { reducers as apiReducer } from '../API';
 import { reducers as modelsPageReducers } from '../../routes/Models/ModelsPage';
 import { reducers as settingRecordsReducers } from '../../components/SettingRecords';
+import { reducers as personalAccessTokensReducers } from '../../components/users/PersonalAccessTokens';
 
 export function combineReducersAsync(asyncReducers) {
   return combineReducers({
@@ -39,6 +40,7 @@ export function combineReducersAsync(asyncReducers) {
     ...factChartReducers,
     ...typeAheadSelectReducers,
     ...settingRecordsReducers,
+    ...personalAccessTokensReducers,
 
     router: connectRouter(history),
     // Pages

app/models/personal_access_token.rb
@@ -24,7 +24,10 @@ class PersonalAccessToken < ApplicationRecord
       active.find_by(user: user, token: hash_token(user, token, :sha1))
     return false unless token
 
-    token.update(last_used_at: Time.current.utc)
+    User.as_anonymous_admin do
+      token.update(last_used_at: Time.current.utc)
+    end
+
     true
   end
 

app/views/api/v2/personal_access_tokens/base.json.rabl
@@ -1,3 +1,3 @@
 object @personal_access_token
 
-attributes :id, :name, :expires_at, :active?, :user_id, :last_used_at
+attributes :id, :name, :expires_at, :active?, :user_id, :last_used_at, :revoked?

app/views/personal_access_tokens/_personal_access_tokens_tab.html.erb
@@ -1,0 +1,6 @@
+<div class="tab-pane" id="personal_access_tokens">
+  <%= react_component('PersonalAccessTokens', {
+    url: api_user_personal_access_tokens_path(user_id: @user),
+    canCreate: authorized_for(controller: 'api/v2/personal_access_tokens', auth_action: :create)
+  }) %>
+</div>

commit fd7af1a6fd57620822a692b5edf7f51c418bb02d

Fixes #35802 - Use per_page=all in the UI
The client code doesn't implement pagination so you actually want to
retrieve everything, rather than some magic high number.

Fixes: ec35c4d82e493574da0b55e0d52dd24dbc6b638f
Fixes: 1c02010e9d334c7449ada31c2292e2348a686777

webpack/assets/javascripts/react_app/components/HostStatuses/index.js
@@ -15,7 +15,7 @@ import Head from '../Head/index';
 import './HostStatuses.scss';
 
 const HostStatuses = () => {
-  const url = foremanUrl('/api/v2/host_statuses?per_page=99');
+  const url = foremanUrl('/api/v2/host_statuses?per_page=all');
   const { status = STATUS.PENDING } = useAPI('get', url, API_OPTIONS);
 
   const Skeleton = () => (

webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/PersonalAccessTokensActions.js
@@ -13,7 +13,7 @@ import {
 export const getPersonalAccessTokens = ({ url }) => dispatch => {
   const uri = new URI(url);
   // eslint-disable-next-line camelcase
-  uri.setSearch({ per_page: 9999 });
+  uri.setSearch({ per_page: 'all' });
 
   ajaxRequestAction({
     dispatch,