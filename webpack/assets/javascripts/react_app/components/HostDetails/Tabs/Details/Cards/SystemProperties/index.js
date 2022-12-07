theforeman/foreman

commit 4e7ac8c11cb1f936fc6103bd837492e152ccfe65

Fixes #34901 - System properties card updates

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/SystemProperties/index.js
@@ -13,9 +13,7 @@ import CardTemplate from '../../../../Templates/CardItem/CardTemplate';
 import Slot from '../../../../../common/Slot';
 import SkeletonLoader from '../../../../../common/SkeletonLoader';
 import DefaultLoaderEmptyState from '../../../../DetailsCard/DefaultLoaderEmptyState';
-import LongDateTime from '../../../../../common/dates/LongDateTime';
 import { STATUS } from '../../../../../../constants';
-import RelativeDateTime from '../../../../../common/dates/RelativeDateTime';
 
 const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
   const {
@@ -28,9 +26,6 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
     domain_name: domain,
     hostgroup_name: hostgroupName,
     owner_type: ownerType,
-    created_at: createdAt,
-    updated_at: updateAt,
-    reported_data: { boot_time: bootTime } = {},
   } = hostDetails;
   return (
     <CardTemplate
@@ -60,6 +55,29 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
             </SkeletonLoader>
           </DescriptionListDescription>
         </DescriptionListGroup>
+        <Slot
+          id="host-details-tab-properties-1"
+          multi
+          hostDetails={hostDetails}
+        />
+        <DescriptionListGroup>
+          <DescriptionListTerm>{__('BIOS UUID')}</DescriptionListTerm>
+          <DescriptionListDescription>
+            <SkeletonLoader
+              status={status}
+              emptyState={<DefaultLoaderEmptyState />}
+            >
+              {uuid && (
+                <ClipboardCopy isBlock variant="inline-compact">
+                  {uuid}
+                </ClipboardCopy>
+              )}
+            </SkeletonLoader>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+      </DescriptionList>
+      <Divider className="padded-divider" />
+      <DescriptionList isCompact isHorizontal>
         <DescriptionListGroup>
           <DescriptionListTerm>{__('Domain')}</DescriptionListTerm>
           <DescriptionListDescription>
@@ -75,34 +93,7 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
             </SkeletonLoader>
           </DescriptionListDescription>
         </DescriptionListGroup>
-        <DescriptionListGroup>
-          <DescriptionListTerm>{__('UUID')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            <SkeletonLoader
-              status={status}
-              emptyState={<DefaultLoaderEmptyState />}
-            >
-              {uuid && (
-                <ClipboardCopy isBlock variant="inline-compact">
-                  {uuid}
-                </ClipboardCopy>
-              )}
-            </SkeletonLoader>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-        <Slot
-          id="host-details-tab-properties-1"
-          multi
-          hostDetails={hostDetails}
-        />
-      </DescriptionList>
-      <Divider className="padded-divider" />
-      <DescriptionList
-        isCompact
-        columnModifier={{
-          default: '2Col',
-        }}
-      >
+
         <DescriptionListGroup>
           <DescriptionListTerm>{__('Model')}</DescriptionListTerm>
           <DescriptionListDescription>
@@ -114,6 +105,11 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
             </SkeletonLoader>
           </DescriptionListDescription>
         </DescriptionListGroup>
+        <Slot
+          id="host-details-tab-properties-2"
+          multi
+          hostDetails={hostDetails}
+        />
         <DescriptionListGroup>
           <DescriptionListTerm>{__('Host group')}</DescriptionListTerm>
           <DescriptionListDescription>
@@ -125,13 +121,8 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
             </SkeletonLoader>
           </DescriptionListDescription>
         </DescriptionListGroup>
-        <Slot
-          id="host-details-tab-properties-2"
-          multi
-          hostDetails={hostDetails}
-        />
         <DescriptionListGroup>
-          <DescriptionListTerm>{__('Owner')}</DescriptionListTerm>
+          <DescriptionListTerm>{__('Host owner')}</DescriptionListTerm>
           <DescriptionListDescription>
             <SkeletonLoader
               status={status}
@@ -146,45 +137,14 @@ const SystemPropertiesCard = ({ status, isExpandedGlobal, hostDetails }) => {
           <DescriptionListDescription>{ownerType}</DescriptionListDescription>
         </DescriptionListGroup>
         <DescriptionListGroup>
-          <DescriptionListTerm>{__('Location')}</DescriptionListTerm>
-          <DescriptionListDescription>{location}</DescriptionListDescription>
-        </DescriptionListGroup>
-        <DescriptionListGroup>
           <DescriptionListTerm>{__('Organization')}</DescriptionListTerm>
           <DescriptionListDescription>
             {organization}
           </DescriptionListDescription>
         </DescriptionListGroup>
-      </DescriptionList>
-      <Divider className="padded-divider" />
-      <DescriptionList isCompact>
         <DescriptionListGroup>
-          <DescriptionListTerm>{__('Created at')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            {createdAt && <LongDateTime date={createdAt} />}
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-        <DescriptionListGroup>
-          <DescriptionListTerm>{__('Updated at')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            <SkeletonLoader
-              status={status}
-              emptyState={<DefaultLoaderEmptyState />}
-            >
-              {updateAt && <LongDateTime date={updateAt} />}
-            </SkeletonLoader>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-        <DescriptionListGroup>
-          <DescriptionListTerm>{__('Boot time')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            <SkeletonLoader
-              status={status}
-              emptyState={<DefaultLoaderEmptyState />}
-            >
-              {bootTime && <RelativeDateTime date={bootTime} />}
-            </SkeletonLoader>
-          </DescriptionListDescription>
+          <DescriptionListTerm>{__('Location')}</DescriptionListTerm>
+          <DescriptionListDescription>{location}</DescriptionListDescription>
         </DescriptionListGroup>
       </DescriptionList>
     </CardTemplate>

commit 42914522b5e0e84ceb7990df58fa421d5f4bf029

Fixes #35836 - Drop "BIOS" UUID field
The host.uuid property is actually the virtualization UUID (for managed
hosts). This has nothing to do with the BIOS and is only confusing.

The UUID should also be considered an internal property and users
shouldn't be bothered by this. Instead, they should see a card showing
the virtual machine's details.

Fixes: 4e7ac8c11cb1f936fc6103bd837492e152ccfe65

webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/SystemProperties/index.js
@@ -18,7 +18,6 @@ import { STATUS } from '../../../../../../constants';
 const SystemPropertiesCard = ({ status, hostDetails }) => {
   const {
     name,
-    uuid,
     model_name: model,
     location_name: location,
     organization_name: organization,
@@ -60,21 +59,6 @@ const SystemPropertiesCard = ({ status, hostDetails }) => {
           multi
           hostDetails={hostDetails}
         />
-        <DescriptionListGroup>
-          <DescriptionListTerm>{__('BIOS UUID')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            <SkeletonLoader
-              status={status}
-              emptyState={<DefaultLoaderEmptyState />}
-            >
-              {uuid && (
-                <ClipboardCopy isBlock variant="inline-compact">
-                  {uuid}
-                </ClipboardCopy>
-              )}
-            </SkeletonLoader>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
       </DescriptionList>
       <Divider className="padded-divider" />
       <DescriptionList isCompact isHorizontal>
@@ -166,7 +150,6 @@ SystemPropertiesCard.propTypes = {
     owner_name: PropTypes.string,
     owner_type: PropTypes.string,
     name: PropTypes.string,
-    uuid: PropTypes.string,
     domain_name: PropTypes.string,
     created_at: PropTypes.string,
     updated_at: PropTypes.string,
@@ -183,7 +166,6 @@ SystemPropertiesCard.defaultProps = {
     hostgroup_name: undefined,
     owner_type: undefined,
     owner_name: undefined,
-    uuid: undefined,
     domain_name: undefined,
     created_at: undefined,
     updated_at: undefined,