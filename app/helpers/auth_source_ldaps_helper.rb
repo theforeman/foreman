0a70fbd0b07a54e5b6305b0420f0d834751c8832
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2023-01-19 12:31:20 +0100

    Fixes #36321 - Extract exact value from example strings
    This unifies the strings to translate, which means less work for
    translators.

--- app/helpers/auth_source_ldaps_helper.rb
+++ app/helpers/auth_source_ldaps_helper.rb
@@ -6,25 +6,25 @@ module AuthSourceLdapsHelper
 
   def account_help_data
     @account_help_data ||= {
-      'active_directory' => _("Example value is <code>DOMAIN\\foreman</code>"),
-      'free_ipa' => _("Example value is <code>uid=foreman,cn=users,cn=accounts,dc=example,dc=com</code>"),
-      'posix' => _("Example value is <code>uid=foreman,dc=example,dc=com</code>"),
+      'active_directory' => _("Example value is <code>%s</code>") % 'DOMAIN\Foreman',
+      'free_ipa' => _("Example value is <code>%s</code>") % 'uid=foreman,cn=users,cn=accounts,dc=example,dc=com',
+      'posix' => _("Example value is <code>%s</code>") % 'uid=foreman,dc=example,dc=com',
     }
   end
 
   def base_dn_help_data
     @base_dn_help_data ||= {
-      'active_directory' => _("Example value is <code>CN=Users,DC=example,DC=COM</code>"),
-      'free_ipa' => _("Example value is <code>cn=users,cn=accounts,dc=example,dc=com</code>"),
-      'posix' => _("Example value is <code>dc=example,dc=com</code>"),
+      'active_directory' => _("Example value is <code>%s</code>") % 'CN=Users,DC=example,DC=COM',
+      'free_ipa' => _("Example value is <code>%s</code>") % 'cn=users,cn=accounts,dc=example,dc=com',
+      'posix' => _("Example value is <code>%s</code>") % 'dc=example,dc=com',
     }
   end
 
   def groups_base_dn_help_data
     @groups_base_dn_help_data ||= {
-      'active_directory' => _("Example value is <code>CN=Users,DC=example,DC=com</code>"),
+      'active_directory' => _("Example value is <code>%s</code>") % 'CN=Users,DC=example,DC=com',
       'free_ipa' => _("Example value is <code>cn=groups,cn=accounts,dc=example,dc=com</code> or <code>cn=ng,cn=compat,dc=example,dc=com</code> if you use netgroups"),
-      'posix' => _("Example value is <code>dc=example,dc=com</code>"),
+      'posix' => _("Example value is <code>%s</code>") % 'dc=example,dc=com',
     }
   end
 end
