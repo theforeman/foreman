b7f865dd5cd49020fa544fa488ea5567781879e9
Markus Bucher <bucher@atix.de>
2023-01-20 22:12:29 +0100

    Fixes #35977 - html_escape host-comment in index

--- config/initializers/foreman_register.rb
+++ config/initializers/foreman_register.rb
@@ -24,7 +24,7 @@ Pagelets::Manager.with_key 'hosts/_list' do |ctx|
     add_pagelet :hosts_table_column_header, key: :last_report, label: _('Last report'), sortable: true, default_sort: 'DESC', width: '10%', class: common_th_class
     add_pagelet :hosts_table_column_content, key: :last_report, class: common_td_class, callback: ->(host) { last_report_column(host) }
     add_pagelet :hosts_table_column_header, key: :comment, label: _('Comment'), sortable: true, width: '7%', class: common_th_class
-    add_pagelet :hosts_table_column_content, key: :comment, class: common_th_class + ' ca', attr_callbacks: { title: ->(host) { host.comment&.truncate(255) } }, callback: ->(host) { icon_text('comment', '') unless host.comment.empty? }
+    add_pagelet :hosts_table_column_content, key: :comment, class: common_th_class + ' ca', attr_callbacks: { title: ->(host) { h host.comment&.truncate(255) } }, callback: ->(host) { icon_text('comment', '') unless host.comment.empty? }
   end
   ctx.with_profile :network_data, _('Network'), default: false do
     common_th_class = 'hidden-tablet hidden-xs'
