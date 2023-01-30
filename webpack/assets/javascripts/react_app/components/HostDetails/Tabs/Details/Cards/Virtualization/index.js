598c98edbe20f5a067e251ec61ee71d206d5fd42
Lucy Fu <lufu@redhat.com>
2023-01-30 10:51:58 -0500

    Refs #35649 - improve the error handling for Virtualization card

--- webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/Virtualization/index.js
+++ webpack/assets/javascripts/react_app/components/HostDetails/Tabs/Details/Cards/Virtualization/index.js
@@ -9,6 +9,8 @@ import {
 import { translate as __ } from '../../../../../../common/I18n';
 import { foremanUrl } from '../../../../../../common/helpers';
 import { useAPI } from '../../../../../../common/hooks/API/APIHooks';
+import EmptyState from '../../../../../common/EmptyState';
+import ErrorBoundary from '../../../../../common/ErrorBoundary';
 import CardTemplate from '../../../../Templates/CardItem/CardTemplate';
 import VirtVmware from './VirtVmware';
 import VirtOvirt from './VirtOvirt';
@@ -38,21 +40,31 @@ const VirtualizationCard = ({ hostDetails }) => {
   };
   const VirtCardDetails = components[provider];
 
+  const errorFallback = () => (
+    <EmptyState
+      icon={<div />}
+      header={__('Something went wrong')}
+      description={__('There was an error loading this content.')}
+    />
+  );
+
   return (
     <CardTemplate header={__('Virtualization')} expandable masonryLayout>
-      <DescriptionList isCompact isHorizontal>
-        {VirtCardDetails && <VirtCardDetails vm={vm} />}
-        <DescriptionListGroup>
-          <DescriptionListTerm>{__('Running on')}</DescriptionListTerm>
-          <DescriptionListDescription>
-            <a
-              href={`/compute_resources/${computeResourceId}-${computeResourceName}`}
-            >
-              {computeResourceName}
-            </a>
-          </DescriptionListDescription>
-        </DescriptionListGroup>
-      </DescriptionList>
+      <ErrorBoundary fallback={errorFallback}>
+        <DescriptionList isCompact isHorizontal>
+          {VirtCardDetails && <VirtCardDetails vm={vm} />}
+          <DescriptionListGroup>
+            <DescriptionListTerm>{__('Running on')}</DescriptionListTerm>
+            <DescriptionListDescription>
+              <a
+                href={`/compute_resources/${computeResourceId}-${computeResourceName}`}
+              >
+                {computeResourceName}
+              </a>
+            </DescriptionListDescription>
+          </DescriptionListGroup>
+        </DescriptionList>
+      </ErrorBoundary>
     </CardTemplate>
   );
 };
