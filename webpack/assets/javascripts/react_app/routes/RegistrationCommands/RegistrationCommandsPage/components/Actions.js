47e8a04eb1ec2452cc14fddb02102ea0cefa4d18
Girija Soni <gisoni@redhat.com>
2023-04-19 12:22:26 +0530

    Fixes #36312 - Remove toast for register host validation

--- webpack/assets/javascripts/react_app/routes/RegistrationCommands/RegistrationCommandsPage/components/Actions.js
+++ webpack/assets/javascripts/react_app/routes/RegistrationCommands/RegistrationCommandsPage/components/Actions.js
@@ -2,28 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import { Alert, ActionGroup, Button, FormGroup } from '@patternfly/react-core';
+import { ActionGroup, Button } from '@patternfly/react-core';
 
-import { sprintf, translate as __ } from '../../../../common/I18n';
+import { translate as __ } from '../../../../common/I18n';
 import { foremanUrl } from '../../../../../foreman_tools';
 
 const Actions = ({ isLoading, isGenerating, handleSubmit, invalidFields }) => (
   <>
-    <FormGroup fieldId="actions_help" className="pf-u-pt-xl">
-      {invalidFields.length === 1 && (
-        <Alert
-          variant="warning"
-          title={sprintf('Invalid field: %s', invalidFields[0])}
-        />
-      )}
-      {invalidFields.length > 1 && (
-        <Alert
-          variant="warning"
-          title={sprintf('Invalid fields: %s', invalidFields.join(', '))}
-        />
-      )}
-    </FormGroup>
-    <ActionGroup>
+    <ActionGroup style={{ marginTop: '50px' }}>
       <Button
         variant="primary"
         id="generate_btn"
