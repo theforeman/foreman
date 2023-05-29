b6878a041d0f4abc6c53b0dd8a60e1017eafbf25
Adam Ruzicka <aruzicka@redhat.com>
2023-05-29 15:31:06 +0000

    Fixes #36449 - Make /api/v2 public

--- app/controllers/api/v2/home_controller.rb
+++ app/controllers/api/v2/home_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V2
     class HomeController < V2::BaseController
-      before_action :require_admin, :only => [:index]
       layout false
 
       api :GET, "/", N_("Show available API links")
