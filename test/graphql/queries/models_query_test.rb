cb88310aafc92e704238fc70ded56ee3b173fad9
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2023-06-15 17:54:38 +0200

    Fixes #36509 - Fix totalCount handling for GraphQL queries with first parameter

--- test/graphql/queries/models_query_test.rb
+++ test/graphql/queries/models_query_test.rb
@@ -2,10 +2,12 @@ require 'test_helper'
 
 module Queries
   class ModelsQueryTest < GraphQLQueryTestCase
+    let(:page_size) { 10 }
+
     let(:query) do
       <<-GRAPHQL
       query {
-        models {
+        models(first: #{page_size}) {
           totalCount
           pageInfo {
             startCursor
@@ -27,17 +29,16 @@ module Queries
     let(:data) { result['data']['models'] }
 
     setup do
-      FactoryBot.create_list(:model, 2)
+      FactoryBot.create_list(:model, 20)
     end
 
     test 'fetching models attributes' do
       assert_empty result['errors']
 
-      expected_count = Model.count
+      expected_total_count = Model.count
 
-      assert_not_equal 0, expected_count
-      assert_equal expected_count, data['totalCount']
-      assert_equal expected_count, data['edges'].count
+      assert_equal expected_total_count, data['totalCount']
+      assert_equal page_size, data['edges'].count
     end
   end
 end
