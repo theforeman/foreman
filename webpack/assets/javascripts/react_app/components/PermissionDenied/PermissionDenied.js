5313801d8c210c913c854c33fce948ae91abd382
MariaAga <mariaaga@redhat.com>
2023-06-29 14:58:28 +0200

    Fixes #36551 - Update PermissionDenied component

--- webpack/assets/javascripts/react_app/components/PermissionDenied/PermissionDenied.js
+++ webpack/assets/javascripts/react_app/components/PermissionDenied/PermissionDenied.js
@@ -1,72 +1,47 @@
 import React from 'react';
-import { Button, Icon } from 'patternfly-react';
 import PropTypes from 'prop-types';
 import { translate as __ } from '../../common/I18n';
+import { EmptyStatePattern as EmptyState } from '../common/EmptyState';
 
-/**
- * A component that explains to the user about the permissions needed to perform an operation.
- * @param {Object} props - The props object.
- * @param {string[]} [props.missingPermissions=['unknown']] - An array of strings representing the missing permissions.
- * @param {Object} [props.texts={}] - An object containing text strings for the component.
- * @param {string} [props.texts.notAuthorizedMsg='You are not authorized to perform this action.'] - The message to display when the user is not authorized to perform the action.
- * @param {string} [props.texts.permissionDeniedMsg='Permission denied'] - The message to display when the user does not have the required permissions.
- * @param {string} [props.texts.pleaseRequestMsg='Please request one of the required permissions listed below from a Foreman administrator:'] - The message to display instructing the user to request the required permissions.
- * @param {string} [props.backHref='/'] - The URL to navigate to when the user clicks the back button.
- * @returns {ReactNode} The rendered `PermissionDenied` component.
- */
+const PermissionDenied = ({ missingPermissions }) => {
+  const description = (
+    <span>
+      {__('You are not authorized to perform this action.')}
+      <br />
+      {__(
+        'Please request one of the required permissions listed below from a Foreman administrator:'
+      )}
+      <br />
+    </span>
+  );
 
-const PermissionDenied = ({ missingPermissions, texts, backHref }) => {
-  const { notAuthorizedMsg, permissionDeniedMsg, pleaseRequestMsg } = texts;
+  const doc = (
+    <ul className="list-unstyled">
+      {missingPermissions.map(permission => (
+        <li key={permission}>
+          <strong>{permission}</strong>
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
-    <div className="blank-slate-pf">
-      <div className="blank-slate-pf-icon">
-        <Icon name="lock" color="#9c9c9c" size="2x" title="lock" />
-      </div>
-      {notAuthorizedMsg}
-
-      <h1>{permissionDeniedMsg}</h1>
-      <p>
-        {notAuthorizedMsg}
-        <br />
-        {pleaseRequestMsg}
-        <br />
-      </p>
-      <ul className="list-unstyled">
-        {missingPermissions.map(permission => (
-          <li key={permission}>
-            <strong>{permission}</strong>
-          </li>
-        ))}
-      </ul>
-      <p>
-        <a href={backHref}>
-          <Button ouiaId="back-button">{__('Back')}</Button>
-        </a>
-      </p>
-    </div>
+    <EmptyState
+      iconType="fa"
+      icon="lock"
+      header={__('Permission Denied')}
+      description={description}
+      documentation={doc}
+    />
   );
 };
 
 PermissionDenied.propTypes = {
-  missingPermissions: PropTypes.node,
-  texts: PropTypes.shape({
-    notAuthorizedMsg: PropTypes.string,
-    permissionDeniedMsg: PropTypes.string,
-    pleaseRequestMsg: PropTypes.string,
-  }),
-  backHref: PropTypes.string,
+  missingPermissions: PropTypes.array,
 };
 
 PermissionDenied.defaultProps = {
   missingPermissions: ['unknown'],
-  texts: {
-    notAuthorizedMsg: __('You are not authorized to perform this action.'),
-    permissionDeniedMsg: __('Permission denied'),
-    pleaseRequestMsg: __(
-      'Please request one of the required permissions listed below from a Foreman administrator:'
-    ),
-  },
-  backHref: '/',
 };
 
 export default PermissionDenied;
