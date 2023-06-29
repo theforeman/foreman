6883b5a4c63719a32569efb12446c3bd377e18fe
MariaAga <mariaaga@redhat.com>
2023-06-29 14:58:28 +0200

    Fixes #36551 - Update PermissionDenied component

--- webpack/assets/javascripts/react_app/components/PermissionDenied/PermissionDenied.test.js
+++ webpack/assets/javascripts/react_app/components/PermissionDenied/PermissionDenied.test.js
@@ -1,20 +1,28 @@
-import PermissionDenied from '.';
-import { testComponentSnapshotsWithFixtures } from '../../common/testHelpers';
-
-const fixtures = {
-  'should render with default props': {},
-  'should render with custom props': {
-    backHref: '/home/dashboard',
-    texts: {
-      notAuthorizedMsg: 'notAuthorizedMsg',
-      pleaseRequestMsg: 'pleaseRequestMsg',
-      permissionDeniedMsg: 'permissionDeniedMsg',
-    },
-  },
-};
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import PermissionDenied from './PermissionDenied';
 
 describe('PermissionDenied', () => {
-  describe('rendering', () => {
-    testComponentSnapshotsWithFixtures(PermissionDenied, fixtures);
+  it('renders the header and description', () => {
+    const missingPermissions = ['permission1', 'permission2'];
+    render(<PermissionDenied missingPermissions={missingPermissions} />);
+
+    expect(screen.getAllByText('Permission Denied')).toHaveLength(1);
+    expect(screen.getAllByText(/You are not authorized to perform this action./)).toHaveLength(1);
+    expect(screen.getAllByText(/Please request one of the required permissions listed below from a Foreman administrator:/)).toHaveLength(1);
+  });
+
+  it('renders the missing permissions', () => {
+    const missingPermissions = ['permission1', 'permission2'];
+    render(<PermissionDenied missingPermissions={missingPermissions} />);
+
+    expect(screen.getAllByText('permission1')).toHaveLength(1);
+    expect(screen.getAllByText('permission2')).toHaveLength(1);
+  });
+
+  it('renders the default missing permission', () => {
+    render(<PermissionDenied />);
+
+    expect(screen.getAllByText('unknown')).toHaveLength(1);
   });
 });
