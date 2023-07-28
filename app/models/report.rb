theforeman/foreman

commit c9b82a573f64fe54e61d55f079adeb1df3d0cae8

Fixes #36219 - use YAML.safe_load instead of YAML.load
as explained in this rubocop rule https://www.rubydoc.info/gems/rubocop/RuboCop/Cop/Security/YAMLLoad

app/models/report.rb
@@ -62,7 +62,7 @@ class Report < ApplicationRecord
   # extracts serialized metrics and keep them as a hash_with_indifferent_access
   def metrics
     return {} if self[:metrics].nil?
-    YAML.load(read_metrics).with_indifferent_access
+    YAML.safe_load(read_metrics).with_indifferent_access
   end
 
   # serialize metrics as YAML

app/models/compute_resources/foreman/model/vmware.rb
@@ -804,7 +804,7 @@ module Foreman::Model
     end
 
     def valid_cloudinit_for_customspec?(cloudinit)
-      parsed = YAML.load(cloudinit)
+      parsed = YAML.safe_load(cloudinit)
       return false if parsed.nil?
       return true if parsed.is_a?(Hash)
       raise Foreman::Exception.new('The user-data template must be a hash in YAML format for VM customization to work.')

app/models/lookup_keys/lookup_key.rb
@@ -177,7 +177,7 @@ class LookupKey < ApplicationRecord
     begin
       JSON.load value
     rescue
-      YAML.load value
+      YAML.safe_load(value, permitted_classes: [Symbol])
     end
   end
 

app/models/setting.rb
@@ -105,7 +105,7 @@ class Setting < ApplicationRecord
   def value
     v = self[:value]
     v = decrypt_field(v)
-    v.nil? ? default : YAML.load(v)
+    v.nil? ? default : YAML.safe_load(v, permitted_classes: [Symbol, Pathname])
   end
   alias_method :value_before_type_cast, :value
 
@@ -130,7 +130,7 @@ class Setting < ApplicationRecord
     when "array"
       if val =~ /\A\[.*\]\Z/
         begin
-          self.value = YAML.load(val.gsub(/(\,)(\S)/, "\\1 \\2"))
+          self.value = YAML.safe_load(val.gsub(/(\,)(\S)/, "\\1 \\2"))
         rescue => e
           invalid_value_error e.to_s
         end

app/services/foreman/importer_puppetclass.rb
@@ -29,7 +29,7 @@ class Foreman::ImporterPuppetclass
         begin
           return "json" if JSON.load value
         rescue
-          return "yaml" if YAML.load value
+          return "yaml" if YAML.safe_load value
         end
       end
       "string"

app/services/foreman/parameters/caster.rb
@@ -107,13 +107,13 @@ module Foreman
       end
 
       def cast_yaml
-        YAML.load value
+        YAML.safe_load(value, permitted_classes: [Symbol])
       end
 
       def load_yaml_or_json
         return value unless value.is_a? String
         begin
-          YAML.load value
+          YAML.safe_load(value, permitted_classes: [Symbol])
         rescue Psych::SyntaxError
           JSON.load value
         end

lib/tasks/parameters.rake
@@ -24,7 +24,7 @@ namespace :parameters do
   task :cast_key_types_and_values => :environment do
     def override_key_type_and_value(param)
       key_type_name = 'string'
-      value = YAML.load param.value
+      value = YAML.safe_load param.value
       key_type_name = value.is_a?(Hash) ? 'yaml' : find_key_type(value)
 
       # Avoid updating parameter with true/false when param.value

lib/tasks/puppet.rake
@@ -15,7 +15,7 @@ namespace :puppet do
         name = yaml.match(/.*\/(.*).yaml/)[1]
         puts "Importing #{name}"
         puppet_facts = File.read(yaml)
-        facts_stripped_of_class_names = YAML.load(puppet_facts.gsub(/\!ruby\/object.*$/, ''))
+        facts_stripped_of_class_names = YAML.safe_load(puppet_facts.gsub(/\!ruby\/object.*$/, ''))
         User.as_anonymous_admin do
           host = Host::Managed.import_host(facts_stripped_of_class_names['name'], 'puppet')
           HostFactImporter.new(host).import_facts(facts_stripped_of_class_names['values'])
@@ -46,7 +46,7 @@ namespace :puppet do
 
         Host.find_each do |host|
           $stdout.print "processing #{host.name} "
-          nodeinfo = YAML.load `#{script} #{host.name}`
+          nodeinfo = YAML.safe_load `#{script} #{host.name}`
           if nodeinfo.is_a?(Hash)
             $stdout.puts "DONE" if host.importNode nodeinfo
           else

commit dfe1c25fd01dec434520ce8b0ece5f824eafb6bc

Refs #36219 - allow loading HashWithIndifferentAccess from YAML
Fixes: c9b82a573f64fe54e61d55f079adeb1df3d0cae8

app/models/report.rb
@@ -62,7 +62,7 @@ class Report < ApplicationRecord
   # extracts serialized metrics and keep them as a hash_with_indifferent_access
   def metrics
     return {} if self[:metrics].nil?
-    YAML.safe_load(read_metrics).with_indifferent_access
+    YAML.safe_load(read_metrics, permitted_classes: [ActiveSupport::HashWithIndifferentAccess]).with_indifferent_access
   end
 
   # serialize metrics as YAML