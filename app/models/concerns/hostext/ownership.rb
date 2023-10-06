theforeman/foreman

commit 535d232934a572d83092a98822712d193f316a78

Fixes #14013 - add a setting for default owner of new hosts

app/models/concerns/hostext/ownership.rb
@@ -1,0 +1,69 @@
+module Hostext
+  module Ownership
+    extend ActiveSupport::Concern
+
+    included do
+      OWNER_TYPES = %w(User Usergroup)
+      belongs_to :owner, :polymorphic => true
+
+      before_validation :set_default_user
+      validates :owner_type, :inclusion =>
+                               {:in => OWNER_TYPES,
+                               :allow_blank => true,
+                               :message     => (_("Owner type needs to be one of the following: %s") % OWNER_TYPES.join(', '))}
+      validate :validate_owner
+      validate :owner_taxonomies_match, :if => Proc.new { |host| host.owner.is_a?(User) }
+    end
+
+    # method to return the correct owner list for host edit owner select dropbox
+    def is_owned_by
+      owner.id_and_type if owner
+    end
+
+    # virtual attributes which sets the owner based on the user selection
+    # supports a simple user, or a usergroup
+    # selection parameter is expected to be an ActiveRecord id_and_type method (see Foreman's AR extentions).
+    def is_owned_by=(selection)
+      owner = OwnerClassifier.new(selection).user_or_usergroup
+      self.owner = owner
+    end
+
+    def owner_suggestion
+      owner_id_and_type = Setting[:host_owner]
+      owner = OwnerClassifier.new(owner_id_and_type).user_or_usergroup
+      self.owner || owner || User.current
+    end
+
+    private
+
+    def owner_taxonomies_match
+      return true if self.owner.admin?
+
+      if Organization.organizations_enabled && self.organization_id && !self.owner.my_organizations.where(:id => self.organization_id).exists?
+        errors.add :is_owned_by, _("does not belong into host's organization")
+      end
+      if Location.locations_enabled && self.location_id && !self.owner.my_locations.where(:id => self.location_id).exists?
+        errors.add :is_owned_by, _("does not belong into host's location")
+      end
+    end
+
+    def set_default_user
+      return if self.owner_type.present? && (!OWNER_TYPES.include?(self.owner_type) || self.owner.nil?)
+      self.owner = owner_suggestion
+    end
+
+    def validate_owner
+      return true if self.owner_type.nil? && self.owner.nil?
+
+      add_owner_error if self.owner_type.present? && self.owner.nil?
+    end
+
+    def add_owner_error
+      if self.owner_id.present?
+        errors.add(:owner, (_('There is no owner with id %d and type %s') % [self.owner_id, self.owner_type]))
+      else
+        errors.add(:owner, _('If owner type is specified, owner must be specified too.'))
+      end
+    end
+  end
+end

app/helpers/application_helper.rb
@@ -382,6 +382,23 @@ module ApplicationHelper
     prev_controller_url
   end
 
+  # creates a data set for editable select-optgroup. each element in the 'groups' array is a hash represents a group with its children.
+  # e.g - {:name => _("Users"), :class => 'user', :scope => 'visible', :value_method => 'id_and_type', :text_method => 'login'}
+  # :name -> group's name, :scope -> scoped method (e.g 'all' or another predefined scope),
+  # :value_method -> value in params, and text_method -> the shown text in the select element.
+
+  def editable_select_optgroup(groups, options = {})
+    select = []
+    select.push(nil => options[:include_blank]) if options[:include_blank].present?
+    groups.each do |group|
+      klass = group[:class].classify.constantize
+      scope = group[:scope]
+      children = Hash[klass.send(scope).map {|obj| [obj.send(group[:value_method]), obj.send(group[:text_method])]}]
+      select.push(:text => group[:name], :children => children)
+    end
+    select
+  end
+
   private
 
   def edit_inline(object, property, options = {})

app/models/host/base.rb
@@ -5,11 +5,11 @@ module Host
     include Parameterizable::ByName
     include DestroyFlag
     include InterfaceCloning
+    include Hostext::Ownership
 
     self.table_name = :hosts
     extend FriendlyId
     friendly_id :name
-    OWNER_TYPES = %w(User Usergroup)
 
     validates_lengths_from_database
     belongs_to :model, :name_accessor => 'hardware_model_name'
@@ -29,17 +29,11 @@ module Host
     belongs_to :organization
 
     alias_attribute :hostname, :name
-    before_validation :set_default_user
 
     validates :name, :presence   => true, :uniqueness => true, :format => {:with => Net::Validations::HOST_REGEXP}
-    validates :owner_type, :inclusion => { :in          => OWNER_TYPES,
-                                           :allow_blank => true,
-                                           :message     => (_("Owner type needs to be one of the following: %s") % OWNER_TYPES.join(', ')) }
-    validate :validate_owner
     validate :host_has_required_interfaces
     validate :uniq_interfaces_identifiers
     validate :build_managed_only
-    validate :owner_taxonomies_match, :if => Proc.new { |host| host.owner.is_a?(User) }
 
     include PxeLoaderSuggestion
 
@@ -318,22 +312,6 @@ module Host
 
     private
 
-    def owner_taxonomies_match
-      return true if self.owner.admin?
-
-      if Organization.organizations_enabled && self.organization_id && !self.owner.my_organizations.where(:id => self.organization_id).exists?
-        errors.add :is_owned_by, _("does not belong into host's organization")
-      end
-      if Location.locations_enabled && self.location_id && !self.owner.my_locations.where(:id => self.location_id).exists?
-        errors.add :is_owned_by, _("does not belong into host's location")
-      end
-    end
-
-    def set_default_user
-      self.owner ||= User.current if self.owner_type.nil? && self.owner_id.nil?
-      true
-    end
-
     def build_values_for_primary_interface!(values_for_primary_interface, args)
       new_attrs = args.shift
       unless new_attrs.nil?
@@ -531,20 +509,6 @@ module Host
         root_pass == hostgroup.try(:read_attribute, :root_pass)
       else
         true
-      end
-    end
-
-    def validate_owner
-      return true if self.owner_type.nil? && self.owner.nil?
-
-      add_owner_error if self.owner_type.present? && self.owner.nil?
-    end
-
-    def add_owner_error
-      if self.owner_id.present?
-        errors.add(:owner, (_('There is no owner with id %d and type %s') % [self.owner_id, self.owner_type]))
-      else
-        errors.add(:owner, _('If owner type is specified, owner must be specified too.'))
       end
     end
   end

app/models/host/managed.rb
@@ -219,22 +219,8 @@ class Host::Managed < Host::Base
     self.name <=> other.name
   end
 
-  # method to return the correct owner list for host edit owner select dropbox
-  def is_owned_by
-    owner.id_and_type if owner
-  end
-
   def self.model_name
     ActiveModel::Name.new(Host)
-  end
-
-  # virtual attributes which sets the owner based on the user selection
-  # supports a simple user, or a usergroup
-  # selection parameter is expected to be an ActiveRecord id_and_type method (see Foreman's AR extentions).
-  def is_owned_by=(selection)
-    oid = User.find(selection.to_i) if selection =~ (/-Users\Z/)
-    oid = Usergroup.find(selection.to_i) if selection =~ (/-Usergroups\Z/)
-    self.owner = oid
   end
 
   def clear_reports

app/models/setting.rb
@@ -10,7 +10,7 @@ class Setting < ActiveRecord::Base
   TYPES= %w{ integer boolean hash array string }
   FROZEN_ATTRS = %w{ name category full_name }
   NONZERO_ATTRS = %w{ puppet_interval idle_timeout entries_per_page max_trend outofsync_interval }
-  BLANK_ATTRS = %w{ trusted_puppetmaster_hosts login_delegation_logout_url authorize_login_delegation_auth_source_user_autocreate root_pass default_location default_organization websockets_ssl_key websockets_ssl_cert oauth_consumer_key oauth_consumer_secret login_text
+  BLANK_ATTRS = %w{ host_owner trusted_puppetmaster_hosts login_delegation_logout_url authorize_login_delegation_auth_source_user_autocreate root_pass default_location default_organization websockets_ssl_key websockets_ssl_cert oauth_consumer_key oauth_consumer_secret login_text
                     smtp_address smtp_domain smtp_user_name smtp_password smtp_openssl_verify_mode smtp_authentication sendmail_arguments sendmail_location }
   ARRAY_HOSTNAMES = %w{ trusted_puppetmaster_hosts }
   URI_ATTRS = %w{ foreman_url unattended_url }
@@ -42,6 +42,7 @@ class Setting < ActiveRecord::Base
 
   validates :value, :url_schema => ['http', 'https'], :if => Proc.new { |s| URI_BLANK_ATTRS.include?(s.name) && s.value.present? }
 
+  validate :validate_host_owner, :if => Proc.new {|s| s.name == "host_owner" }
   validates :value, :format => { :with => Resolv::AddressRegex }, :if => Proc.new { |s| IP_ATTRS.include? s.name }
   validates :value, :regexp => true, :if => Proc.new { |s| REGEXP_ATTRS.include? s.name }
   validates :value, :array_type => true, :if => Proc.new { |s| s.settings_type == "array" }
@@ -268,7 +269,10 @@ class Setting < ActiveRecord::Base
   def self.set(name, description, default, full_name = nil, value = nil, options = {})
     if options.has_key? :collection
       SettingsHelper.module_eval do
-        define_method("#{name}_collection".to_sym){ options[:collection].call }
+        define_method("#{name}_collection".to_sym) do
+          collection = options[:collection].call
+          collection.is_a?(Hash) ? collection : editable_select_optgroup(collection, :include_blank => options[:include_blank])
+        end
       end
     end
     options[:encrypted] ||= false
@@ -286,6 +290,13 @@ class Setting < ActiveRecord::Base
   # End methods for loading default settings
 
   private
+
+  def validate_host_owner
+    owner_type_and_id = self.value
+    return if owner_type_and_id.blank?
+    owner = OwnerClassifier.new(owner_type_and_id).user_or_usergroup
+    errors.add(:value, _("Host owner is invalid")) if owner.nil?
+  end
 
   def invalid_value_error(error)
     errors.add(:value, _("is invalid: %s") % error)

app/models/setting/provisioning.rb
@@ -12,8 +12,11 @@ class Setting::Provisioning < Setting
   def self.default_settings
     fqdn = Facter.value(:fqdn) || SETTINGS[:fqdn]
     unattended_url = "http://#{fqdn}"
+    select = [{:name => _("Users"), :class => 'user', :scope => 'visible', :value_method => 'id_and_type', :text_method => 'login'},
+              {:name => _("Usergroup"), :class => 'usergroup', :scope => 'visible', :value_method => 'id_and_type', :text_method => 'name'}]
 
     [
+      self.set('host_owner', N_("Default owner on provisioned hosts, if empty Foreman will use current user"), nil, N_('Host owner'), nil, {:collection => Proc.new { select }, :include_blank => _("Select an owner")}),
       self.set('root_pass', N_("Default encrypted root password on provisioned hosts"), nil, N_('Root password')),
       self.set('unattended_url', N_("URL hosts will retrieve templates from during build (normally http as many installers don't support https)"), unattended_url, N_('Unattended URL')),
       self.set('safemode_render', N_("Enable safe mode config templates rendering (recommended)"), true, N_('Safemode rendering')),

app/services/owner_classifier.rb
@@ -1,0 +1,10 @@
+class OwnerClassifier
+  def initialize(id_and_type)
+    @id_and_type = id_and_type
+  end
+
+  def user_or_usergroup
+    return nil unless @id_and_type =~ /^\d+-(Users|Usergroups)$/
+    @id_and_type.include?('Users') ? User.find_by_id(@id_and_type.to_i) : Usergroup.find_by_id(@id_and_type.to_i)
+  end
+end

app/views/hosts/_form.html.erb
@@ -114,7 +114,7 @@
         <% if SETTINGS[:login] %>
           <%= selectable_f f, :is_owned_by, option_groups_from_collection_for_select(
             [User.authorized(:view_users), Usergroup.authorized(:view_usergroups)], :visible, :table_name, :id_and_type, :select_title,
-            @host.is_owned_by || User.current.id_and_type), { :include_blank => _("select an owner") }, { :label => _("Owned By") }
+            @host.owner_suggestion.id_and_type), { :include_blank => _("Select an owner") }, { :label => _("Owned By") }
           %>
         <% end %>
         <%= checkbox_f f, :enabled,

commit b11015dc89e8c2464b6badcba1eb0ca19e66235d

Refs #36575 - Avoid i18n during startup in owner_type validation
Fixes: 535d232934a5 ("Fixes #14013 - add a setting for default owner of new hosts")

app/models/concerns/hostext/ownership.rb
@@ -10,7 +10,7 @@ module Hostext
       validates :owner_type, :inclusion =>
                                {:in => OWNER_TYPES,
                                :allow_blank => true,
-                               :message     => (_("Owner type needs to be one of the following: %s") % OWNER_TYPES.join(', '))}
+                               :message     => (N_("Owner type needs to be one of the following: %s") % OWNER_TYPES.join(', '))}
       validate :validate_owner
       validate :owner_taxonomies_match, :if => proc { |host| host.owner.is_a?(User) }
     end