theforeman/foreman

commit 9d3eace428f2fc3d53415ff19d29b0ae1c42920a

Fixes #10359 - API doc does mention org/loc in create/update

app/controllers/concerns/api/v2/taxonomies_controller.rb
@@ -33,6 +33,9 @@ module Api::V2::TaxonomiesController
       param :subnet_ids, Array, N_("Subnet IDs"), :required => false
       param :parent_id, :number, :desc => N_('Parent ID'), :required => false
       param :ignore_types, Array, N_("List of resources types that will be automatically associated"), :required => false
+      resource_name = (param_name == :location) ? 'organization' : 'location'
+      resource_ids = "#{resource_name}_ids".to_sym
+      param resource_ids, Array, N_("Associated %{resource} IDs") % { resource: _(resource_name) }, :required => false
     end
   end
 

commit f80a318577dc7732b3a50e68cf955b0e7bafcd37

Refs #36575 - Correct i18n usage in taxonomies apipie
String interpolation in translated strings should mostly be avoided.
This rewrites it to have explicit definitions for both locations and
organizations.

Fixes: 9d3eace428f2fc3d53415ff19d29b0ae1c42920a ("Fixes #10359 - API doc does mention org/loc in create/update")

app/controllers/concerns/api/v2/taxonomies_controller.rb
@@ -31,9 +31,11 @@ module Api::V2::TaxonomiesController
       param :subnet_ids, Array, N_("Subnet IDs"), :required => false
       param :parent_id, :number, :desc => N_('Parent ID'), :required => false
       param :ignore_types, Array, N_("List of resources types that will be automatically associated"), :required => false
-      resource_name = (param_name == :location) ? 'organization' : 'location'
-      resource_ids = "#{resource_name}_ids".to_sym
-      param resource_ids, Array, N_("Associated %{resource} IDs") % { resource: _(resource_name) }, :required => false
+      if param_name == :location
+        param :organization_ids, Array, N_("Associated organization IDs"), :required => false
+      else
+        param :location_ids, Array, N_("Associated location IDs"), :required => false
+      end
     end
   end
 