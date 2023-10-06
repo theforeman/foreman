theforeman/foreman

commit 9c30b63eb1d2e799f857abf1f56a43fdc650d759

fixes #6067 - UI inconsistency around root password limit 8 characters

app/models/hostgroup.rb
@@ -12,6 +12,7 @@ class Hostgroup < ActiveRecord::Base
   has_many :user_hostgroups, :dependent => :destroy
   has_many :users, :through => :user_hostgroups
   validates :name, :format => { :with => /\A(\S+\s?)+\Z/, :message => N_("can't be blank or contain trailing white spaces.")}
+  validates :root_pass, :allow_blank => true, :length => {:minimum => 8, :message => _('should be 8 characters or more')}
   has_many :group_parameters, :dependent => :destroy, :foreign_key => :reference_id
   accepts_nested_attributes_for :group_parameters, :reject_if => lambda { |a| a[:value].blank? }, :allow_destroy => true
   has_many_hosts

app/views/hostgroups/_form.html.erb
@@ -51,7 +51,7 @@
 
     <div class="tab-pane" id="os">
       <%= render "common/os_selection/initial", :item=> @hostgroup %>
-      <%= password_f f, :root_pass %>
+      <%= password_f f, :root_pass, :help_inline => _("Password should be 8 characters or more")%>
     </div>
 
     <div class="tab-pane" id="params">

commit bd709db94a9875dfd6c40fd47548eaa97b971a0d

Refs #36575 - Avoid i18n during startup in hg root_pass
Fixes: 9c30b63eb1d2 ("fixes #6067 - UI inconsistency around root password limit 8 characters")

app/models/hostgroup.rb
@@ -23,7 +23,7 @@ class Hostgroup < ApplicationRecord
 
   validates_lengths_from_database :except => [:name]
   before_destroy EnsureNotUsedBy.new(:hosts)
-  validates :root_pass, :allow_blank => true, :length => {:minimum => 8, :message => _('should be 8 characters or more')}
+  validates :root_pass, :allow_blank => true, :length => {:minimum => 8, :message => N_('should be 8 characters or more')}
   has_many :group_parameters, :dependent => :destroy, :foreign_key => :reference_id, :inverse_of => :hostgroup
   accepts_nested_attributes_for :group_parameters, :allow_destroy => true
   include ParameterValidators