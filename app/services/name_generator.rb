theforeman/foreman

commit 0351034151a70be5bac559065c58056801bb3b22

Fixes #13768 - random and MAC-based name generator

app/services/name_generator.rb
@@ -1,0 +1,53 @@
+class NameGenerator
+  GENERATOR_TYPES = {
+    'Off' => _('Off'),
+    'Random-based' => _('Random-based'),
+    'MAC-based' => _('MAC-based')
+  }.freeze
+
+  def self.random_based?
+    Setting[:name_generator_type] =~ /^Random/i
+  end
+  delegate :random_based?, :to => :class
+
+  def self.mac_based?
+    Setting[:name_generator_type] =~ /^MAC/i
+  end
+  delegate :mac_based?, :to => :class
+
+  def initialize
+    @mac_generator = Deacon::MacGenerator.new
+    @random_generator = Deacon::RandomGenerator.new
+  end
+
+  def next_mac_name(mac)
+    if mac_based? && mac
+      @mac_generator.generate(mac).join('-').downcase
+    else
+      ''
+    end
+  end
+
+  def next_random_name
+    if random_based?
+      self.register, firstname, lastname = @random_generator.generate(self.register)
+      [firstname, lastname].join('-').downcase
+    else
+      ''
+    end
+  end
+
+  def register
+    index = Rails.cache.fetch("name_generator_register")
+    unless index
+      index = self.register = Deacon::RandomGenerator.random_initial_seed
+      Rails.logger.debug "Generated new randomized name generator register: #{index}"
+    end
+    index
+  end
+
+  def register=(value)
+    Rails.cache.write("name_generator_register", value)
+    value
+  end
+end

.rubocop_todo.yml
@@ -69,7 +69,7 @@ Metrics/MethodLength:
 # Offense count: 21
 # Configuration parameters: CountComments.
 Metrics/ModuleLength:
-  Max: 411
+  Max: 450
 
 # Offense count: 5
 # Configuration parameters: CountKeywordArgs.

Gemfile
@@ -33,6 +33,7 @@ gem 'sprockets-rails', '>= 2.3.3', '< 3'
 gem 'responders', '~> 2.0'
 gem 'roadie-rails', '~> 1.1'
 gem 'x-editable-rails', '~> 1.5.5'
+gem 'deacon', '~> 1.0'
 
 Dir["#{File.dirname(FOREMAN_GEMFILE)}/bundler.d/*.rb"].each do |bundle|
   self.instance_eval(Bundler.read_file(bundle))

app/assets/javascripts/host_edit.js
@@ -769,3 +769,16 @@ function selectedSubnetHasIPAM() {
   if (subnet_id == '') return true;
   return subnets[subnet_id]['ipam'];
 };
+
+function randomizeName() {
+  $.ajax({
+    type: "GET",
+    url: "/hosts/random_name",
+    success: function(response, status, xhr) {
+      var element = $('#host_name');
+      element.val(response.name);
+      element.focus();
+      element.select();
+    }
+  });
+ }

app/assets/javascripts/host_edit_interfaces.js
@@ -1,4 +1,7 @@
-
+$(document).ready(function() {
+  $('#host_name').select();
+  $('#host_name').focus();
+})
 
 function remove_interface(interface_id) {
   $('#interface'+interface_id).remove();
@@ -321,3 +324,21 @@ function update_fqdn() {
 
   $('#hostFQDN').text(name);
 }
+
+$(document).on('change', '.interface_mac', function (event) {
+  if (event.target.id == $('#interfaceModal').find('.interface_mac').attr('id')) {
+    var interface = $('#interfaceModal').find('.interface_mac');
+    var mac = interface.val();
+    var baseurl = interface.attr('data-url');
+    $.ajax({
+      type: "GET",
+      url: baseurl + '?mac=' + mac,
+      success: function(response, status, xhr) {
+        if ($('#host_name').val() == '')
+          $('#host_name').val(response.name);
+        if ($('#interfaceModal').find('.interface_name').val() == '')
+          $('#interfaceModal').find('.interface_name').val(response.name);
+      }
+    });
+  }
+});

app/controllers/hosts_controller.rb
@@ -127,6 +127,12 @@ class HostsController < ApplicationController
   end
 
   # form AJAX methods
+  def random_name
+    render :json => { :name => NameGenerator.new.next_random_name }
+  rescue ActionView::Template::Error => exception
+    process_ajax_error exception, 'generate random name'
+  end
+
   def compute_resource_selected
     return not_found unless (params[:host] && (id=params[:host][:compute_resource_id]))
     Taxonomy.as_taxonomy @organization, @location do
@@ -704,7 +710,8 @@ class HostsController < ApplicationController
           'select_multiple_owner', 'update_multiple_owner',
           'select_multiple_power_state', 'update_multiple_power_state',
           'select_multiple_puppet_proxy', 'update_multiple_puppet_proxy',
-          'select_multiple_puppet_ca_proxy', 'update_multiple_puppet_ca_proxy'
+          'select_multiple_puppet_ca_proxy', 'update_multiple_puppet_ca_proxy',
+          'random_name'
         :edit
       when 'multiple_destroy', 'submit_multiple_destroy'
         :destroy

app/controllers/interfaces_controller.rb
@@ -23,4 +23,19 @@ class InterfacesController < ApplicationController
 
     render 'nic/new'
   end
+
+  def random_name
+    render :json => { :name => NameGenerator.new.next_mac_name(params[:mac]) }
+  rescue ActionView::Template::Error => exception
+    process_ajax_error exception, 'generate random name'
+  end
+
+  def action_permission
+    case params[:action]
+      when 'random_name'
+        :edit
+      else
+        super
+    end
+  end
 end

app/helpers/form_helper.rb
@@ -228,8 +228,7 @@ module FormHelper
         options = {}
         options[:class] = "btn btn-#{overwrite ? 'danger' : 'primary'} remove_form_templates"
         options.merge! :'data-id' => form_to_submit_id(f) unless options.has_key?(:'data-id')
-        link_to(_("Cancel"), args[:cancel_path], :class => "btn btn-default") + " " +
-            f.submit(text, options)
+        link_to(_("Cancel"), args[:cancel_path], :class => "btn btn-default") + " " + f.submit(text, options)
       end
     end
   end

commit 5f817fb5dc89f7da841dbe03237c4c49984d38db

Refs #36575 - Avoid translations during startup in name gen
Fixes: 0351034151a7 ("Fixes #13768 - random and MAC-based name generator")

app/services/name_generator.rb
@@ -1,8 +1,8 @@
 class NameGenerator
   GENERATOR_TYPES = {
-    'Off' => _('Off'),
-    'Random-based' => _('Random-based'),
-    'MAC-based' => _('MAC-based'),
+    'Off' => N_('Off'),
+    'Random-based' => N_('Random-based'),
+    'MAC-based' => N_('MAC-based'),
   }.freeze
 
   def self.random_based?