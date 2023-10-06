theforeman/foreman

commit b1b46b6a17b21c8aec566ef846ab70a8b2aa931c

Fixes #35274 - Make columns on host index page selectable (#9319)

config/initializers/foreman_register.rb
@@ -4,3 +4,24 @@ Pagelets::Manager.with_key 'hosts/show' do |mgr|
     partial: 'hosts/init_config_tab',
     priority: 100
 end
+
+Foreman::SelectableColumns::Storage.define(:hosts) do
+  category :general, default: true do
+    column :name, th: { label: _('Name'), sortable: true, width: '25%' },
+                  td: { class: 'ellipsis', callback: ->(host) { name_column(host) } }
+    column :os_title, th: { label: _('Operating system'), sortable: true, width: '17%', class: 'hidden-xs' },
+                      td: { class: 'hidden-xs ellipsis', callback: ->(host) { (icon(host.operatingsystem, size: "16x16") + " #{host.operatingsystem.to_label}").html_safe if host.operatingsystem } }
+    column :model, th: { label: _('Model'), sortable: true, width: '10%', class: common_th_class },
+                   td: { class: common_td_class, callback: ->(host) { host.compute_resource_or_model } }
+    column :owner, th: { label: _('Owner'), sortable: true, width: '8%', class: common_th_class },
+                   td: { class: common_td_class, callback: ->(host) { host_owner_column(host) } }
+    column :hostgroup, th: { label: _('Host group'), sortable: true, width: '15%', class: common_th_class },
+                       td: { class: common_th_class, callback: ->(host) { label_with_link host.hostgroup, 23, @hostgroup_authorizer } }
+    column :last_report, th: { label: _('Last report'), sortable: true, default_sort: 'DESC', width: '10%', class: common_th_class },
+                         td: { class: common_td_class, callback: ->(host) { last_report_column(host) } }
+    column :comment, th: { label: _('Comment'), sortable: true, width: '7%', class: common_th_class },
+                     td: { class: common_th_class + ' ca',
+                           attr_callbacks: { title: ->(host) { host.comment&.truncate(255) } },
+                           callback: ->(host) { icon_text('comment', '') unless host.comment.empty? } }
+  end
+end

app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
   before_action :session_expiry, :update_activity_time, :unless => proc { |c| c.remote_user_provided? || c.api_request? }
   before_action :set_taxonomy, :require_mail, :check_empty_taxonomy
   before_action :authorize
-  before_action :welcome, :only => :index, :unless => :api_request?
+  before_action :welcome, :find_selected_columns, :only => :index, :unless => :api_request?
   prepend_before_action :allow_webpack, if: -> { Rails.configuration.webpack.dev_server.enabled }
   around_action :set_timezone
 

app/controllers/concerns/application_shared.rb
@@ -140,4 +140,8 @@ module ApplicationShared
     end
     determined_taxonomy
   end
+
+  def find_selected_columns
+    @selected_columns = Foreman::SelectableColumns::Storage.selected_by(User.current, controller_name)
+  end
 end

app/controllers/hosts_controller.rb
@@ -33,6 +33,8 @@ class HostsController < ApplicationController
   before_action :set_host_type, :only => [:update]
   before_action :find_multiple, :only => MULTIPLE_ACTIONS
   before_action :validate_power_action, :only => :update_multiple_power_state
+  # index action is already included in ApplicationController
+  before_action(:only => SEARCHABLE_ACTIONS.without('index')) { find_selected_columns }
 
   helper :hosts, :reports, :interfaces
 

app/helpers/selectable_columns_helper.rb
@@ -1,0 +1,52 @@
+module SelectableColumnsHelper
+  def render_selected_column_ths
+    result = ""
+    @selected_columns.each do |column|
+      result += render(
+        'common/selectable_column_th',
+        attributes: attributes(column[:th]),
+        th_content: th_content(column),
+        callback: column[:th][:callback]
+      )
+    end
+    result.html_safe
+  end
+
+  def render_selected_column_tds(record)
+    result = ""
+    @selected_columns.each do |column|
+      result += render(
+        'common/selectable_column_td',
+        attributes: attributes(column[:td]),
+        attr_callbacks: column[:td][:attr_callbacks],
+        subject: record,
+        callback: column[:td][:callback]
+      )
+    end
+    result.html_safe
+  end
+
+  def attr_from_callbacks(callbacks, subject)
+    return unless callbacks
+
+    callbacks.map { |(k, v)| "#{k}=\"#{instance_exec(subject, &v)}\"" }
+             .join(' ')
+             .html_safe
+  end
+
+  private
+
+  def attributes(th_or_td)
+    th_or_td.except(:callback, :sortable, :default_sort, :attr_callbacks, :label)
+            .map { |(k, v)| "#{k}=\"#{v}\"" }
+            .join(' ')
+            .html_safe
+  end
+
+  def th_content(col)
+    return if col[:th][:callback]
+    return col[:th][:label] unless col[:th][:sortable]
+
+    sort col[:key], as: col[:th][:label], default: col[:th][:default_sort] || 'ASC'
+  end
+end

app/registries/foreman/plugin.rb
@@ -329,6 +329,10 @@ module Foreman #:nodoc:
       SettingManager.define(id, &block)
     end
 
+    def selectable_columns(table, &block)
+      Foreman::SelectableColumns::Storage.register(table, &block)
+    end
+
     def security_block(name, &block)
       @security_block = name
       instance_eval(&block)

app/registries/foreman/selectable_columns/category.rb
@@ -1,0 +1,63 @@
+module Foreman
+  module SelectableColumns
+    class Category < Array
+      attr_reader :id, :label
+
+      def initialize(id, label, table, default: false)
+        @id = id
+        @label = label
+        @table = table
+        @default = default
+        @columns_to_use = HashWithIndifferentAccess.new
+        super(0)
+      end
+
+      def column(key, th:, td:)
+        self << { key: key.to_s, th: th, td: td }.with_indifferent_access
+      end
+
+      def use_column(key, from:)
+        return if from.to_s == @id
+
+        @columns_to_use[from] ||= []
+        @columns_to_use[from] << key.to_s
+      end
+
+      def common_th_class
+        'hidden-tablet hidden-xs'
+      end
+
+      def common_td_class
+        common_th_class + ' ellipsis'
+      end
+
+      # This instance can be updated after it was created by DSL, but
+      #   only at the initialization time.
+      # This method is only for actual runtime usage,
+      #   after the Storage is fully defined/updated.
+      # Thus, saving once computed result.
+      def keys
+        @keys ||= columns.map { |c| c[:key] }.sort
+      end
+
+      def default?
+        @default
+      end
+
+      def columns
+        self + foreign_columns
+      end
+
+      private
+
+      # This is meant for actual runtime usage,
+      #  after the categories are fully defined.
+      # Thus, saving once computed result.
+      def foreign_columns
+        @foreign_columns ||= @columns_to_use.keys.map do |category_id|
+          @table.find { |cat| cat.id == category_id }&.select { |col| @columns_to_use[category_id].include?(col[:key]) }
+        end.flatten.compact
+      end
+    end
+  end
+end

app/registries/foreman/selectable_columns/storage.rb
@@ -1,0 +1,57 @@
+module Foreman
+  module SelectableColumns
+    class Storage
+      include Singleton
+
+      class << self
+        def tables
+          @tables ||= ActiveSupport::HashWithIndifferentAccess.new
+        end
+
+        def define(name, &block)
+          return Foreman::Logging.logger('app').warn _('Table %s is already defined, ignoring.') % name if tables[name]
+
+          table = SelectableColumns::Table.new(name)
+          table.instance_eval(&block)
+          tables[name] = table
+        end
+
+        def register(name, &block)
+          return Foreman::Logging.logger('app').warn _('Table %s is not defined, ignoring.') % name unless tables[name]
+
+          tables[name].instance_eval(&block)
+        end
+
+        # This is for UI data mostly
+        def defined_for(table)
+          return Foreman::Logging.logger('app').warn _('Table %s is not defined, ignoring.') % table unless tables[table]
+
+          tables[table].map do |category|
+            {
+              id: category.id,
+              name: category.label,
+              columns: category.columns.map { |c| { id: c[:key], name: c[:th][:label] } },
+            }
+          end
+        end
+
+        def selected_by(user, table)
+          return unless tables[table]
+
+          selected_keys = user.table_preferences.find_by(name: table)&.columns&.sort
+          result = if selected_keys
+                     tables[table].select { |category| (category.keys & selected_keys).any? }
+                                  .map(&:columns)
+                                  .flatten
+                                  .select { |col| selected_keys.include?(col[:key]) }
+                   else
+                     tables[table].select { |category| category.default? }
+                                  .map(&:columns)
+                                  .flatten
+                   end
+          result.uniq
+        end
+      end
+    end
+  end
+end

commit 920f1d196117c5c45adb0c9cffe02db01980845f

Refs #36575 - Avoid translating labels during startup
Since b1b46b6a17b21c8aec566ef846ab70a8b2aa931c the labels are translated
during initialization. This is because it uses () instead of N(). The
latter only marks a string for translation. Doing this leads to infinite
recursion while starting up when updating to fast_gettext 2.1+. It can
also lead to other issues where a column can be translated multiple
times.

Fixes: b1b46b6a17b2 ("Fixes #35274 - Make columns on host index page selectable (#9319)")

config/initializers/foreman_register.rb
@@ -6,56 +6,56 @@ Pagelets::Manager.with_key 'hosts/show' do |mgr|
 end
 
 Pagelets::Manager.with_key 'hosts/_list' do |ctx|
-  ctx.with_profile :general, _('General'), default: true do
+  ctx.with_profile :general, N_('General'), default: true do
     common_class = 'hidden-tablet hidden-xs ellipsis'
-    add_pagelet :hosts_table_column_header, key: :power_status, label: _('Power'), sortable: false, width: '80px', class: 'ca hidden-xs',
+    add_pagelet :hosts_table_column_header, key: :power_status, label: N_('Power'), sortable: false, width: '80px', class: 'ca hidden-xs',
                 export_data: CsvExporter::ExportDefinition.new(:power_status, callback: ->(host) { PowerManager::PowerStatus.safe_power_state(host)[:state] })
     add_pagelet :hosts_table_column_content, key: :power_status, class: 'ca hidden-xs', callback: ->(host) { react_component('PowerStatus', id: host.id, url: power_api_host_path(host)) }
-    add_pagelet :hosts_table_column_header, key: :name, label: _('Name'), sortable: true, width: '24%', locked: true
+    add_pagelet :hosts_table_column_header, key: :name, label: N_('Name'), sortable: true, width: '24%', locked: true
     add_pagelet :hosts_table_column_content, key: :name, callback: ->(host) { name_column(host) }, locked: true, class: 'ellipsis'
-    add_pagelet :hosts_table_column_header, key: :os_title, label: _('OS'), sortable: true, width: '17%', attr_callbacks: { title: ->(host) { _('Operating system') } }, export_key: 'operatingsystem', class: common_class
+    add_pagelet :hosts_table_column_header, key: :os_title, label: N_('OS'), sortable: true, width: '17%', attr_callbacks: { title: ->(host) { _('Operating system') } }, export_key: 'operatingsystem', class: common_class
     add_pagelet :hosts_table_column_content, key: :os_title, callback: ->(host) { (icon(host.operatingsystem, size: "16x16") + " #{host.operatingsystem.to_label}").html_safe if host.operatingsystem }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :owner, label: _('Owner'), sortable: true, width: '8%', class: common_class
+    add_pagelet :hosts_table_column_header, key: :owner, label: N_('Owner'), sortable: true, width: '8%', class: common_class
     add_pagelet :hosts_table_column_content, key: :owner, callback: ->(host) { host_owner_column(host) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :hostgroup, label: _('Host group'), sortable: true, width: '15%', class: common_class
+    add_pagelet :hosts_table_column_header, key: :hostgroup, label: N_('Host group'), sortable: true, width: '15%', class: common_class
     add_pagelet :hosts_table_column_content, key: :hostgroup, callback: ->(host) { label_with_link host.hostgroup, 23, @hostgroup_authorizer }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :boot_time, label: _('Boot time'), sortable: true, width: '10%', export_key: 'reported_data.boot_time', class: common_class
+    add_pagelet :hosts_table_column_header, key: :boot_time, label: N_('Boot time'), sortable: true, width: '10%', export_key: 'reported_data.boot_time', class: common_class
     add_pagelet :hosts_table_column_content, key: :boot_time, callback: ->(host) { date_time_unless_empty(host.reported_data&.boot_time) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :last_report, label: _('Last report'), sortable: true, default_sort: 'DESC', width: '10%', class: common_class
+    add_pagelet :hosts_table_column_header, key: :last_report, label: N_('Last report'), sortable: true, default_sort: 'DESC', width: '10%', class: common_class
     add_pagelet :hosts_table_column_content, key: :last_report, callback: ->(host) { last_report_column(host) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :comment, label: _('Comment'), sortable: true, width: '7%', class: common_class
+    add_pagelet :hosts_table_column_header, key: :comment, label: N_('Comment'), sortable: true, width: '7%', class: common_class
     add_pagelet :hosts_table_column_content, key: :comment, class: common_class + ' ca', attr_callbacks: { title: ->(host) { h host.comment&.truncate(255) } }, callback: ->(host) { icon_text('comment', '') unless host.comment.empty? }
   end
-  ctx.with_profile :network_data, _('Network'), default: false do
+  ctx.with_profile :network_data, N_('Network'), default: false do
     common_class = 'hidden-tablet hidden-xs ellipsis'
-    add_pagelet :hosts_table_column_header, key: :ip, label: _('IPv4'), sortable: true, width: '10%', attr_callbacks: { title: ->(host) { _('IPv4 address') } }, priority: 200, class: common_class
+    add_pagelet :hosts_table_column_header, key: :ip, label: N_('IPv4'), sortable: true, width: '10%', attr_callbacks: { title: ->(host) { _('IPv4 address') } }, priority: 200, class: common_class
     add_pagelet :hosts_table_column_content, key: :ip, callback: ->(host) { host.ip }, priority: 200, class: common_class
-    add_pagelet :hosts_table_column_header, key: :ip6, label: _('IPv6'), sortable: true, width: '13%', attr_callbacks: { title: ->(host) { _('IPv6 address') } }, priority: 200, class: common_class
+    add_pagelet :hosts_table_column_header, key: :ip6, label: N_('IPv6'), sortable: true, width: '13%', attr_callbacks: { title: ->(host) { _('IPv6 address') } }, priority: 200, class: common_class
     add_pagelet :hosts_table_column_content, key: :ip6, callback: ->(host) { host.ip6 }, priority: 200, class: common_class
-    add_pagelet :hosts_table_column_header, key: :mac, label: _('MAC'), sortable: true, width: '10%', attr_callbacks: { title: ->(host) { _('MAC address') } }, priority: 200, class: common_class
+    add_pagelet :hosts_table_column_header, key: :mac, label: N_('MAC'), sortable: true, width: '10%', attr_callbacks: { title: ->(host) { _('MAC address') } }, priority: 200, class: common_class
     add_pagelet :hosts_table_column_content, key: :mac, callback: ->(host) { host.mac }, priority: 200, class: common_class
   end
-  ctx.with_profile :reported_data, _('Reported data'), default: false do
+  ctx.with_profile :reported_data, N_('Reported data'), default: false do
     common_class = 'hidden-tablet hidden-xs ellipsis'
-    add_pagelet :hosts_table_column_header, key: :model, label: _('Model'), sortable: true, width: '10%', export_key: 'compute_resource_or_model', class: common_class
+    add_pagelet :hosts_table_column_header, key: :model, label: N_('Model'), sortable: true, width: '10%', export_key: 'compute_resource_or_model', class: common_class
     add_pagelet :hosts_table_column_content, key: :model, callback: ->(host) { host.compute_resource_or_model }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :sockets, label: _('Sockets'), width: '5%', export_key: 'reported_data.sockets', class: common_class
+    add_pagelet :hosts_table_column_header, key: :sockets, label: N_('Sockets'), width: '5%', export_key: 'reported_data.sockets', class: common_class
     add_pagelet :hosts_table_column_content, key: :sockets, callback: ->(host) { host.reported_data&.sockets }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :cores, label: _('Cores'), width: '5%', export_key: 'reported_data.cores', class: common_class
+    add_pagelet :hosts_table_column_header, key: :cores, label: N_('Cores'), width: '5%', export_key: 'reported_data.cores', class: common_class
     add_pagelet :hosts_table_column_content, key: :cores, callback: ->(host) { host.reported_data&.cores }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :ram, label: _('RAM'), width: '5%', export_key: 'reported_data.ram', class: common_class
+    add_pagelet :hosts_table_column_header, key: :ram, label: N_('RAM'), width: '5%', export_key: 'reported_data.ram', class: common_class
     add_pagelet :hosts_table_column_content, key: :ram, callback: ->(host) { humanize_bytes(host.reported_data&.ram, from: :mega) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :virtual, label: _('Virtual'), width: '5%', class: common_class
+    add_pagelet :hosts_table_column_header, key: :virtual, label: N_('Virtual'), width: '5%', class: common_class
     add_pagelet :hosts_table_column_content, key: :virtual, callback: ->(host) { virtual?(host) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :disks_total, label: _('Disks space'), width: '8%', attr_callbacks: { title: ->(host) { _('Disks total space') } }, export_key: 'reported_data.disks_total', class: common_class
+    add_pagelet :hosts_table_column_header, key: :disks_total, label: N_('Disks space'), width: '8%', attr_callbacks: { title: ->(host) { _('Disks total space') } }, export_key: 'reported_data.disks_total', class: common_class
     add_pagelet :hosts_table_column_content, key: :disks_total, callback: ->(host) { humanize_bytes(host.reported_data&.disks_total) }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :kernel_version, label: _('Kernel version'), width: '12%', export_key: 'reported_data.kernel_version', class: common_class
+    add_pagelet :hosts_table_column_header, key: :kernel_version, label: N_('Kernel version'), width: '12%', export_key: 'reported_data.kernel_version', class: common_class
     add_pagelet :hosts_table_column_content, key: :kernel_version, callback: ->(host) { host.reported_data&.kernel_version }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :bios_vendor, label: _('BIOS vendor'), width: '8%', export_key: 'reported_data.bios_vendor', class: common_class
+    add_pagelet :hosts_table_column_header, key: :bios_vendor, label: N_('BIOS vendor'), width: '8%', export_key: 'reported_data.bios_vendor', class: common_class
     add_pagelet :hosts_table_column_content, key: :bios_vendor, callback: ->(host) { host.reported_data&.bios_vendor }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :bios_release_date, label: _('BIOS release date'), width: '10%', export_key: 'reported_data.bios_release_date', class: common_class
+    add_pagelet :hosts_table_column_header, key: :bios_release_date, label: N_('BIOS release date'), width: '10%', export_key: 'reported_data.bios_release_date', class: common_class
     add_pagelet :hosts_table_column_content, key: :bios_release_date, callback: ->(host) { host.reported_data&.bios_release_date }, class: common_class
-    add_pagelet :hosts_table_column_header, key: :bios_version, label: _('BIOS version'), width: '12%', export_key: 'reported_data.bios_version', class: common_class
+    add_pagelet :hosts_table_column_header, key: :bios_version, label: N_('BIOS version'), width: '12%', export_key: 'reported_data.bios_version', class: common_class
     add_pagelet :hosts_table_column_content, key: :bios_version, callback: ->(host) { host.reported_data&.bios_version }, class: common_class
   end
 end