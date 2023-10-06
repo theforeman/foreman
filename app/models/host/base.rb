theforeman/foreman

commit 060d2004d78676998fc55ba03eab7f0ffcde2859

Fixes #20119 - Be more descriptive when validating hostname
Signed-off-by: Boaz Shuster <ripcurld.github@gmail.com>

app/models/host/base.rb
@@ -31,7 +31,7 @@ module Host
 
     alias_attribute :hostname, :name
 
-    validates :name, :presence   => true, :uniqueness => true, :format => {:with => Net::Validations::HOST_REGEXP}
+    validates :name, :presence   => true, :uniqueness => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => _(Net::Validations::HOST_REGEXP_ERR_MSG)}
     validate :host_has_required_interfaces
     validate :uniq_interfaces_identifiers
     validate :build_managed_only

app/models/nic/interface.rb
@@ -11,7 +11,7 @@ module Nic
     before_validation :copy_hostname_from_host, :if => Proc.new { |nic| nic.primary? && nic.hostname.blank? }
     before_validation :normalize_name
 
-    validates :name, :allow_nil => true, :allow_blank => true, :format => {:with => Net::Validations::HOST_REGEXP}
+    validates :name, :allow_nil => true, :allow_blank => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => _(Net::Validations::HOST_REGEXP_ERR_MSG)}
 
     validate :name_uniqueness, :if => Proc.new { |i| i.name.present? }
 

lib/net/validations.rb
@@ -9,6 +9,8 @@ module Net
     HOST_REGEXP ||= /\A(([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\.)*([a-z0-9]|[a-z0-9][a-z0-9\-]*[a-z0-9])\z/
     MASK_REGEXP ||= /\A((255.){3}(0|128|192|224|240|248|252|254))|((255.){2}(0|128|192|224|240|248|252|254).0)|(255.(0|128|192|224|240|248|252|254)(.0){2})|((128|192|224|240|248|252|254)(.0){3})\z/
 
+    HOST_REGEXP_ERR_MSG = N_("hostname can contain only lowercase letters, numbers, dashes and dots")
+
     class Error < RuntimeError
     end
 

commit 0339beadeb22f431fd91ba6dca0e2d0b5d401a7b

Refs #36575 - Avoid i18n during startup in host name validation
Fixes: 060d2004d786 ("Fixes #20119 - Be more descriptive when validating hostname")

app/models/host/base.rb
@@ -37,7 +37,7 @@ module Host
 
     alias_attribute :hostname, :name
 
-    validates :name, :presence => true, :uniqueness => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => _(Net::Validations::HOST_REGEXP_ERR_MSG)}
+    validates :name, :presence => true, :uniqueness => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => N_(Net::Validations::HOST_REGEXP_ERR_MSG)}
     validate :host_has_required_interfaces
     validate :uniq_interfaces_identifiers
 

app/models/nic/interface.rb
@@ -11,7 +11,7 @@ module Nic
     before_validation :copy_hostname_from_host, :if => proc { |nic| nic.primary? && nic.hostname.blank? }
     before_validation :normalize_name
 
-    validates :name, :allow_nil => true, :allow_blank => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => _(Net::Validations::HOST_REGEXP_ERR_MSG)}
+    validates :name, :allow_nil => true, :allow_blank => true, :format => {:with => Net::Validations::HOST_REGEXP, :message => N_(Net::Validations::HOST_REGEXP_ERR_MSG)}
 
     validate :name_uniqueness, :if => proc { |i| i.name.present? }
 