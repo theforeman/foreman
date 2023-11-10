09c29e57a45cdd8b9025063126e6be328e724c77
Oleh Fedorenko <ofedoren@redhat.com>
2023-11-10 17:53:06 +0000

    Refs #36849 - Fix Ruby 3 kwargs handling

--- db/migrate/20150525081931_remove_duplicate_tokens.rb
+++ db/migrate/20150525081931_remove_duplicate_tokens.rb
@@ -1,15 +1,15 @@
 class RemoveDuplicateTokens < ActiveRecord::Migration[4.2]
   def up
-    remove_foreign_key :tokens, :column => :host_id if foreign_key_exists?(:tokens, { :name => "tokens_host_id_fk" })
+    remove_foreign_key :tokens, :column => :host_id if foreign_key_exists?(:tokens, name: "tokens_host_id_fk")
     remove_index :tokens, :host_id if index_exists? :tokens, :host_id
     add_index :tokens, :host_id, :unique => true
-    add_foreign_key :tokens, :hosts, :name => "tokens_host_id_fk" unless foreign_key_exists?(:tokens, { :name => "tokens_host_id_fk" })
+    add_foreign_key :tokens, :hosts, :name => "tokens_host_id_fk" unless foreign_key_exists?(:tokens, name: "tokens_host_id_fk")
   end
 
   def down
-    remove_foreign_key :tokens, :column => :host_id if foreign_key_exists?(:tokens, { :name => "tokens_host_id_fk" })
+    remove_foreign_key :tokens, :column => :host_id if foreign_key_exists?(:tokens, name: "tokens_host_id_fk")
     remove_index :tokens, :host_id if index_exists? :tokens, :host_id
     add_index :tokens, :host_id
-    add_foreign_key :tokens, :hosts, :name => "tokens_host_id_fk" unless foreign_key_exists?(:tokens, { :name => "tokens_host_id_fk" })
+    add_foreign_key :tokens, :hosts, :name => "tokens_host_id_fk" unless foreign_key_exists?(:tokens, name: "tokens_host_id_fk")
   end
 end
