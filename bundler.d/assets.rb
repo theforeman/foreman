54fac5e22f7148307c0dc25f8cfb49d8e3aeef5f
Eric D. Helms <ericdhelms@gmail.com>
2023-11-21 16:45:53 -0500

    Fixes #36936 - Switch to terser

--- bundler.d/assets.rb
+++ bundler.d/assets.rb
@@ -4,7 +4,7 @@ group :assets do
   gem 'gettext_i18n_rails_js', '~> 1.4'
   gem 'po_to_json', '~> 1.1'
   gem 'execjs', '>= 1.4.0', '< 3.0'
-  gem 'uglifier', '>= 1.0.3'
+  gem 'terser', '~> 1.1'
   gem 'sass-rails', '~> 6.0'
   # this one is a dependecy for x-editable-rails
   gem 'coffee-rails', '~> 5.0.0'
