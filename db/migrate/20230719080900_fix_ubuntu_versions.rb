d314942f17235274bee07ac1e98a5c8384671faa
Markus Reisner <reisner@atix.de>
2023-12-19 14:42:01 +0100

    Refs #36547 - Fix parsing of Ubuntu version in fact parsers

--- db/migrate/20230719080900_fix_ubuntu_versions.rb
+++ db/migrate/20230719080900_fix_ubuntu_versions.rb
@@ -3,10 +3,11 @@ class FixUbuntuVersions < ActiveRecord::Migration[6.0]
     # For every Ubuntu operating system with a set minor version we
     #   * Check if there is already an operating system present with the same major and minor version combined in the major field
     #   * If not, we update the version
-    Operatingsystem.where(name: 'Ubuntu').where('major ~* ?', '^\d*$').where.not(minor: [nil, '']).each do |os|
-      unless Operatingsystem.find_by(name: 'Ubuntu', major: "#{os.major}.#{os.minor}", minor: [nil, ''])
-        os.update(major: "#{os.major}.#{os.minor}", minor: nil)
-      end
-    end
+    Operatingsystem.where(name: 'Ubuntu')
+      .where('operatingsystems.major ~* ?', '^\d*$')
+      .where.not(minor: [nil, ''])
+      .where('o3.id IS NULL')
+      .joins("left outer join operatingsystems o3 on o3.major like concat(operatingsystems.major, '.', operatingsystems.minor)")
+      .update_all('major = concat(operatingsystems.major, \'.\', operatingsystems.minor), minor = \'\'')
   end
 end
