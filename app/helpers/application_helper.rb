theforeman/foreman

commit b566ea8ab3c56f79edb0ed9a1caa3c28038af961

Fixes #32848 - Support linking to docs.theforeman.org
An effort is under way to use docs.theforeman.org as the primary
documentation source. It is already the official documentation source
for Katello, but until now it wasn't easily possible to (correctly) link
to it.

The documentation is built in 3 flavors:

* Katello (index-katello.html)
* Debian/Ubuntu (index-deb.html)
* Enterprise Linux (index-el.html)

app/helpers/application_helper.rb
@@ -305,8 +305,8 @@ module ApplicationHelper
     editable(object, property, {:type => type, :title => title, :value => value, :class => klass, :source => select_values, :url => update_url, :placeholder => placeholder}.compact)
   end
 
-  def documentation_url(section = "", options = {})
-    main_app.external_link_url(options.merge(type: 'manual', params: { section: section }))
+  def documentation_url(section = nil, type: 'manual', **options)
+    main_app.external_link_url(type: type, section: section, params: options)
   end
 
   def spinner(text = '', options = {})

app/controllers/links_controller.rb
@@ -25,7 +25,16 @@ class LinksController < ApplicationController
       'https://projects.theforeman.org/projects/foreman/issues'
     when 'vmrc'
       'https://www.vmware.com/go/download-vmrc'
+    when 'docs'
+      params.require(:section)
+      guide, chapter, flavor = params.permit(:section, :chapter, :flavor).values_at(:section, :chapter, :flavor)
+      flavor ||= self.class.new_docs_flavor
+      docs_url(guide: guide, chapter: chapter, flavor: flavor)
     end
+  end
+
+  def self.new_docs_flavor
+    Foreman::Plugin.installed?('katello') ? 'katello' : SETTINGS[:docs_os_flavor]
   end
 
   private
@@ -47,6 +56,14 @@ class LinksController < ApplicationController
   def documentation_url(section = "", options = {})
     root_url = options[:root_url] || foreman_org_path("manuals/#{SETTINGS[:version].short}/index.html#")
     root_url + (section || '')
+  end
+
+  # For new documentation at docs.theforeman.org
+  def docs_url(guide:, flavor:, chapter: nil)
+    version = SETTINGS[:version].tag == 'develop' ? 'nightly' : SETTINGS[:version].short
+    chapter_hash = "##{chapter}" if chapter
+
+    "https://docs.theforeman.org/#{version}/#{guide}/index-#{flavor}.html#{chapter_hash}"
   end
 
   def plugin_documentation_url

config/settings.rb
@@ -10,6 +10,11 @@ SETTINGS[:version] = Foreman::Version.new
 # Load settings from env variables
 SETTINGS.deep_merge!(Foreman::EnvSettingsLoader.new.to_h)
 
+# foreman-documentation builds different flavors for Debian and Enterprise
+# Linux. It also builds for Katello, but we can't detect that here so the key
+# is docs_os_flavor instead of docs_flavor.
+SETTINGS[:docs_os_flavor] ||= File.exist?('/etc/debian_version') ? 'foreman-deb' : 'foreman-el'
+
 # Force setting to true until all code using it is removed
 [:locations_enabled, :organizations_enabled, :unattended].each do |setting|
   SETTINGS[setting] = true

test/controllers/links_controller_test.rb
@@ -70,5 +70,50 @@ class LinksControllerTest < ActionController::TestCase
       assert_redirected_to /15.0/
       assert_redirected_to /Usage/
     end
+
+    test 'new docs on nightly' do
+      get :show, params: {
+        type: 'docs',
+        section: 'TestSection',
+        chapter: 'TestChapter',
+      }
+
+      assert_redirected_to %r{https://docs\.theforeman\.org/nightly/TestSection/index-(foreman-(deb|el)|katello)\.html#TestChapter}
+    end
+
+    test 'new docs on a stable release' do
+      with_temporary_settings(version: Foreman::Version.new('3.9.1')) do
+        get :show, params: {
+          type: 'docs',
+          section: 'TestSection',
+          chapter: 'TestChapter',
+        }
+
+        assert_redirected_to %r{https://docs\.theforeman\.org/3\.9/TestSection/index-(foreman-(deb|el)|katello)\.html#TestChapter}
+      end
+    end
+
+    describe '#new_docs_flavor' do
+      test 'on Enterprise Linux' do
+        Foreman::Plugin.stubs(:installed?).with('katello').returns(false)
+        with_temporary_settings(docs_os_flavor: 'foreman-el') do
+          assert_equal(LinksController.new_docs_flavor, 'foreman-el')
+        end
+      end
+
+      test 'on Debian' do
+        Foreman::Plugin.stubs(:installed?).with('katello').returns(false)
+        with_temporary_settings(docs_os_flavor: 'foreman-deb') do
+          assert_equal(LinksController.new_docs_flavor, 'foreman-deb')
+        end
+      end
+
+      test 'on Enterprise Linux with Katello' do
+        Foreman::Plugin.stubs(:installed?).with('katello').returns(true)
+        with_temporary_settings(docs_os_flavor: 'foreman-el') do
+          assert_equal(LinksController.new_docs_flavor, 'katello')
+        end
+      end
+    end
   end
 end

test/helpers/application_helper_test.rb
@@ -32,12 +32,32 @@ class ApplicationHelperTest < ActionView::TestCase
       assert_match /my_plugin/, url
     end
 
+    test '#documentation_url and new docs page' do
+      url = documentation_url('TestSection', { type: 'plugin_manual', name: 'foreman_my_plugin', version: '1.2' })
+
+      assert_match %r{links/plugin_manual/TestSection\?name=foreman_my_plugin&version=1\.2}, url
+    end
+
+    test '#documentation_url and new docs page' do
+      url = documentation_url('TestSection', { type: 'docs', chapter: 'test_chapter' })
+
+      assert_match %r{links/docs/TestSection\?chapter=test_chapter}, url
+    end
+
     test '#documentation_button forwards options to #documentation_url' do
       expects(:icon_text).returns('http://nowhere.com')
       expects(:link_to).returns('<a>test</a>'.html_safe)
       expects(:documentation_url).with('2.2PluginSection', { :root_url => 'http://www.theforeman.org/my_plugin/v0.1/index.html#' })
 
       documentation_button '2.2PluginSection', :root_url => 'http://www.theforeman.org/my_plugin/v0.1/index.html#'
+    end
+
+    test '#documentation_button forwards plugin manual options to #documentation_url' do
+      expects(:icon_text).returns('http://nowhere.com')
+      expects(:link_to).returns('<a>test</a>'.html_safe)
+      expects(:documentation_url).with('2.2PluginSection', { type: 'plugin_manual', name: 'foreman_my_plugin', version: '1.2' })
+
+      documentation_button '2.2PluginSection', type: 'plugin_manual', name: 'foreman_my_plugin', version: '1.2'
     end
   end
 

test/test_helper.rb
@@ -188,6 +188,17 @@ class ActionController::TestCase
   def disable_webpack
     Webpack::Rails::Manifest.stubs(:asset_paths).returns([])
   end
+
+  def with_temporary_settings(**kwargs)
+    old_settings = SETTINGS.slice(*kwargs.keys)
+    begin
+      SETTINGS.update(kwargs)
+
+      yield
+    ensure
+      SETTINGS.update(old_settings)
+    end
+  end
 end
 
 class GraphQLQueryTestCase < ActiveSupport::TestCase

webpack/assets/javascripts/react_app/common/helpers.js
@@ -158,10 +158,21 @@ export const stringIsPositiveNumber = value => {
 
 /**
  * Get manual url based on version
- * @param {String} section - section id for foreman documetation
+ * @param {String} section - section id for foreman documentation
  */
 export const getManualURL = section => foremanUrl(`/links/manual/${section}`);
 export const getWikiURL = section => foremanUrl(`/links/wiki/${section}`);
+
+/**
+ * Get the documentation URL
+ * @param {String} guide - The guide containing the documentation
+ * @param {String} chapter - Optional chapter within the guide
+ * @returns {String}
+ */
+export const getDocsURL = (guide, chapter = null) => {
+  const url = foremanUrl(`/links/docs/${guide}`);
+  return chapter ? `{url}?chapter={encodeURIComponent(chapter)}` : url;
+};
 
 /**
  * Transform the Date object to date string accepted in the server

webpack/assets/javascripts/react_app/routes/RegistrationCommands/RegistrationCommandsPage/RegistrationCommandsPageConstants.js
@@ -1,10 +1,0 @@
-import URI from 'urijs';
-import { foremanUrl } from '../../../../foreman_tools';
-
-export const docUrl = (foremanVersion) => {
-  const rootUrl = `https://docs.theforeman.org/${foremanVersion}/`
-  const section = 'Managing_Hosts/index-foreman-el.html#registering-a-host_managing-hosts'
-
-  const url = new URI({path: '/links/manual', query: { root_url: rootUrl, section: section }});
-  return foremanUrl(url.href());
-}

commit 002d179f1f65425815c35123c5297ad89dc73c2c

Refs #36849 - Correct kwargs handling in documentation_button
Fixes: b566ea8ab3c5 ("Fixes #32848 - Support linking to docs.theforeman.org")

app/helpers/application_helper.rb
@@ -291,7 +291,7 @@ module ApplicationHelper
   end
 
   def documentation_button(section = "", options = {})
-    url = documentation_url section, options
+    url = documentation_url(section, **options)
     link_to(icon_text('help', _('Documentation'), :kind => 'pficon'),
       url, :rel => 'external noopener noreferrer', :class => 'btn btn-default btn-docs', :target => '_blank')
   end