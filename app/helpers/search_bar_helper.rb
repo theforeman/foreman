7bde1688ca5b030d393529d630e04c987cccc1ef
archanaserver <ak31960466@gmail.com>
2024-01-04 17:21:12 +0530

    Fix rubocop Layout/FirstHashElementIndentation

--- app/helpers/search_bar_helper.rb
+++ app/helpers/search_bar_helper.rb
@@ -7,14 +7,14 @@ module SearchBarHelper
     }
     url = send("auto_complete_search_#{auto_complete_controller_name}_path")
     react_component("SearchBar", data: {
-                      controller: auto_complete_controller_name,
-                      autocomplete: {
-                        id: 'searchBar',
-                        searchQuery: params[:search],
-                        url: url,
-                        useKeyShortcuts: true,
-                      },
-                      bookmarks: bookmarks,
-                    })
+      controller: auto_complete_controller_name,
+      autocomplete: {
+        id: 'searchBar',
+        searchQuery: params[:search],
+        url: url,
+        useKeyShortcuts: true,
+      },
+      bookmarks: bookmarks,
+    })
   end
 end
