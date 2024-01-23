0e5433c8f6a8f64eb5728ac47fd1e8230bbc4568
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-01-23 19:34:25 +0100

    Fixes #37092 - Use minitest_reporters_github in GHA
    This is a specialized reporter to provide GitHub annotations on failure.
    These annotations can be seen in the changes files tab.

--- test/test_report_helper.rb
+++ test/test_report_helper.rb
@@ -1,7 +1,16 @@
 require 'minitest/reporters'
 
-junit_reporter = Minitest::Reporters::JUnitReporter.new('jenkins/reports/unit/')
-meantime_reporter = Minitest::Reporters::MeanTimeReporter.new(previous_runs_filename: Rails.root.join('tmp', 'foreman_minitest_reporters_previous_run'),
-                                                              report_filename: Rails.root.join('tmp', 'foreman_minitest_reporters_report'))
+if ENV['GITHUB_ACTIONS'] == 'true'
+  require 'minitest_reporters_github'
+  reporters = [MinitestReportersGithub.new]
+else
+  reporters = [
+    Minitest::Reporters::JUnitReporter.new('jenkins/reports/unit/'),
+    Minitest::Reporters::MeanTimeReporter.new(
+      previous_runs_filename: Rails.root.join('tmp', 'foreman_minitest_reporters_previous_run'),
+      report_filename: Rails.root.join('tmp', 'foreman_minitest_reporters_report')
+    ),
+  ]
+end
 
-Minitest::Reporters.use! [junit_reporter, meantime_reporter]
+Minitest::Reporters.use! reporters
