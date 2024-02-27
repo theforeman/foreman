bb76e866d5db6e3ca3c1bf0c0a17d99a4ed9c173
MariaAga <mariaaga@redhat.com>
2024-02-27 11:33:02 +0100

    Fixes #37134 - upgrade to node 18 and npm 8

--- webpack/assets/javascripts/react_app/components/common/dates/LongDateTime.test.js
+++ webpack/assets/javascripts/react_app/components/common/dates/LongDateTime.test.js
@@ -18,7 +18,59 @@ describe('LongDateTime', () => {
 
     intl.ready.then(() => {
       wrapper.update();
-      expect(wrapper.find('LongDateTime')).toMatchSnapshot();
+      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={false}
+            showRelativeTimeTooltip={false}
+          >
+            <span>
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017, 11:54 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      } else {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={false}
+            showRelativeTimeTooltip={false}
+          >
+            <span>
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017 at 11:54 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      }
     });
   });
 
@@ -33,7 +85,63 @@ describe('LongDateTime', () => {
 
     intl.ready.then(() => {
       wrapper.update();
-      expect(wrapper.find('LongDateTime')).toMatchSnapshot();
+      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={false}
+            showRelativeTimeTooltip={true}
+          >
+            <span
+              title="15 days ago"
+            >
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017, 11:54 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      } else {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={false}
+            showRelativeTimeTooltip={true}
+          >
+            <span
+              title="15 days ago"
+            >
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017 at 11:54 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      }
     });
   });
 
@@ -44,7 +152,61 @@ describe('LongDateTime', () => {
 
     intl.ready.then(() => {
       wrapper.update();
-      expect(wrapper.find('LongDateTime')).toMatchSnapshot();
+      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={true}
+            showRelativeTimeTooltip={false}
+          >
+            <span>
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                second="2-digit"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017, 11:54:55 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      } else {
+        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
+          `
+          <LongDateTime
+            date={2017-10-13T11:54:55.000Z}
+            defaultValue="Default value"
+            seconds={true}
+            showRelativeTimeTooltip={false}
+          >
+            <span>
+              <FormattedDate
+                day="2-digit"
+                hour="2-digit"
+                minute="2-digit"
+                month="long"
+                second="2-digit"
+                value={2017-10-13T11:54:55.000Z}
+                year="numeric"
+              >
+                <span>
+                  October 13, 2017 at 11:54:55 AM
+                </span>
+              </FormattedDate>
+            </span>
+          </LongDateTime>
+        `
+        );
+      }
     });
   });
 
@@ -55,7 +217,18 @@ describe('LongDateTime', () => {
 
     intl.ready.then(() => {
       wrapper.update();
-      expect(wrapper.find('LongDateTime')).toMatchSnapshot();
+      expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(`
+        <LongDateTime
+          date={null}
+          defaultValue="Default value"
+          seconds={false}
+          showRelativeTimeTooltip={false}
+        >
+          <span>
+            Default value
+          </span>
+        </LongDateTime>
+      `);
     });
   });
 });
