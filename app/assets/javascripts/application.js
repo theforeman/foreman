theforeman/foreman

commit d91a0e4d219560000c5cc9c145650bab2039ba3b

fixes #14178 - introduce patternfly toast notifications
- replaces old jnoitfy with react implementation.
- Adds a notification store to handle events from
rails flash + legacy javascript Notice function and expose
an Action API to add notifications.

app/assets/javascripts/application.js
@@ -23,10 +23,6 @@ $(document).on("page:fetch", tfm.tools.showSpinner)
 
 $(document).on("page:change", tfm.tools.hideSpinner)
 
-$(window).bind('beforeunload', function() {
-  $(".jnotify-container").remove();
-});
-
 $(function() {
   $(document).trigger('ContentLoad');
 });
@@ -37,22 +33,6 @@ function onContentLoad(){
     $.each(uninitialized_autocompletes, function(i, input) {$(input).scopedSearch({'delay': 250})});
     $('.ui-helper-hidden-accessible').remove();
   }
-
-  $('.flash.error').each(function(index, item) {
-     if ($('.alert.alert-danger.base').length == 0) {
-       if ($('#host-conflicts-modal').length == 0) {
-         notify(item, 'danger');
-       }
-     }
-   });
-
-   $('.flash.warning').each(function(index, item) {
-     notify(item, 'warning');
-   });
-
-   $('.flash.notice').each(function(index, item) {
-     notify(item, 'success');
-   });
 
   if ($("input[focus_on_load=true]").length > 0) {
     $("input[focus_on_load]").first().focus();
@@ -375,16 +355,9 @@ function spinner_placeholder(text){
   return "<div class='spinner-placeholder'><p class='spinner-label'>" + text + "</p><div id='Loading' class='spinner spinner-md spinner-inline'> </div></div>";
 }
 
-function notify(item, type) {
-  var icon = typeToIcon(type);
-  var options = { classMessage: "foreman-alert",
-                  classBackground: "",
-                  sticky: (type != 'success'),
-                  type: type };
-  $.jnotify("</div>" + icon + $(item).text(), options);
-  // jnotify does not support multiple classes passed via classMessage so added via jquery.
-  $('.foreman-alert').addClass("alert alert-" + type);
-  $(item).remove();
+function notify(message, type) {
+  tfm.tools.deprecate('notify', 'tfm.toastNotifications.notify');
+  tfm.toastNotifications.notify({message: message, type: type});
 }
 
 function typeToIcon(type) {

LICENSE
@@ -28,7 +28,6 @@ The following files and directories are exceptions:
 * vendor/assets/fonts/ is (c) 2011-2016, Red Hat Inc. under SIL Open Font License v1.1 or LGPL v2.1 licenses.
 * vendor/assets/javascripts/noVNC/ is (c) 1999 AT&T Laboratories Cambridge, (c) 1996 Widget Workshop, Inc., (c) 1996 Jef Poskanzer <jef@acme.com>, (c) 2011 Joel Martin, (c) Hiroshi Ichikawa <http://gimite.net/en/>, (c) Martijn Pieters <mj@digicool.com>, Samuel Sieb <samuel@sieb.net> under (MPL 1.1 or GPL v2+ or LGPL v2.1+) and LGPL v3 and LGPL v2+ and MIT and Revised BSD licenses.
 * vendor/assets/javascripts/jquery.flot.axislabels.js is (c) 2010 Xuan Luo under MIT license.
-* vendor/assets/javascripts/jquery.jnotify.js is (c) 2010 Giva, Inc. under ASL 2.0 license.
 
 All rights reserved.
 

app/assets/javascripts/dashboard.js
@@ -35,12 +35,12 @@ function remove_widget(item){
             type: 'DELETE',
             url: $(item).data('url'),
             success: function(){
-                $.jnotify(__("Widget removed from dashboard."), 'success', false);
+                tfm.toastNotifications.notify(__("Widget removed from dashboard."), 'success');
                 gridster.remove_widget(widget);
                 window.location.reload();
             },
             error: function(){
-                $.jnotify(__("Error removing widget from dashboard."), 'error', true);
+                tfm.toastNotifications.notify(__("Error removing widget from dashboard."), 'error');
             },
         });
     }
@@ -52,11 +52,11 @@ function add_widget(name){
         url: 'widgets',
         data: {'name': name},
         success: function(){
-            $.jnotify(__("Widget added to dashboard."), 'success', false);
+            tfm.toastNotifications.notify(__("Widget added to dashboard."), 'success');
             window.location.reload();
         },
         error: function(){
-            $.jnotify(__("Error adding widget to dashboard."), 'error', true);
+            tfm.toastNotifications.notify(__("Error adding widget to dashboard."), 'error');
         },
         dataType: 'json'
     });
@@ -69,10 +69,10 @@ function save_position(path){
         url: path,
         data: {'widgets': positions},
         success: function(){
-            $.jnotify(__("Widget positions successfully saved."), 'success', false);
+            tfm.toastNotifications.notify(__("Widget positions successfully saved."), 'success');
         },
         error: function(){
-            $.jnotify(__("Failed to save widget positions."), 'error', true);
+            tfm.toastNotifications.notify(__("Failed to save widget positions."), 'error');
         },
         dataType: 'json'
     });

app/assets/stylesheets/application.scss
@@ -5,7 +5,6 @@
 @import "jquery.ui.autocomplete";
 @import "jquery.ui.spinner";
 @import "jquery.ui.spinner_custom";
-@import "../../../vendor/assets/stylesheets/vendor";
 @import "scoped_search";
 @import "patternfly-sprockets";
 @import "patternfly";

app/assets/stylesheets/patternfly_and_overrides.scss
@@ -49,10 +49,6 @@ textarea.form-control{
   position: relative;
 }
 
-.jnotify-close{
-  color: black !important;
-}
-
 #login {
   max-width: 100%;
 }

app/helpers/application_helper.rb
@@ -510,6 +510,12 @@ module ApplicationHelper
     params[:host] && params[:host][:hostgroup_id] && params[:host][field]
   end
 
+  def notifications
+    content_tag :div, :id => 'notifications', :'data-flash' => flash.to_json.html_safe do
+      mount_react_component('ToastNotifications', '#notifications')
+    end
+  end
+
   def current_url_params(permitted: [])
     params.permit(permitted + [:locale, :search])
   end

app/helpers/reactjs_helper.rb
@@ -1,5 +1,5 @@
 module ReactjsHelper
-  def mount_react_component(name, selector, data)
+  def mount_react_component(name, selector, data = [])
     javascript_tag defer: 'defer' do
       "$(tfm.reactMounter.mount('#{name}', '#{selector}', #{data}));".html_safe
     end

app/views/layouts/_application_content.html.erb
@@ -1,7 +1,5 @@
 <div id="main">
-  <%= content_tag('div', flash[:error],   :class => 'flash hide error')   if flash[:error] %>
-  <%= content_tag('div', flash[:warning], :class => 'flash hide warning') if flash[:warning] %>
-  <%= content_tag('div', flash[:notice],  :class => 'flash hide notice')  if flash[:notice] %>
+  <%= notifications %>
   <div id="content" class="container">
     <% unless @page_header.blank? %>
       <div class="row form-group">
@@ -21,4 +19,3 @@
     <%= yield %>
   </div>
 </div>
-

commit 1a6c42e36cf4ccab71a6bb48945102e104d6db2c

Fixes #37387 - Drop unused typeToIcon function
Has been unused since d91a0e4d219560000c5cc9c145650bab2039ba3b.

Fixes: d91a0e4d2195 ("fixes #14178 - introduce patternfly toast notifications")

app/assets/javascripts/application.js
@@ -328,21 +328,6 @@ function spinner_placeholder(text) {
   );
 }
 
-function typeToIcon(type) {
-  switch (type) {
-    case 'success':
-      return tfm.tools.iconText('ok', __('Success') + ': ', 'pficon');
-    case 'warning':
-      return tfm.tools.iconText(
-        'warning-triangle-o',
-        __('Warning') + ': ',
-        'pficon'
-      );
-    case 'danger':
-      return tfm.tools.iconText('error-circle-o', __('Error') + ': ', 'pficon');
-  }
-}
-
 function setPowerState(item, status) {
   var power_actions = $('#power_actions'),
     loading_power_state = $('#loading_power_state');