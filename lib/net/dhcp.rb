4766d1b635f14cb95d7bfa6943264df9914df971
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-02 14:21:58 +0000

    Refs #29991 - Move net into app/lib

--- lib/net/dhcp.rb
+++
@@ -1,7 +0,0 @@
-module Net
-  module DHCP
-    autoload :Record, "net/dhcp/record.rb"
-    autoload :SparcRecord, "net/dhcp/sparc_record.rb"
-    autoload :ZTPRecord, "net/dhcp/ztp_record.rb"
-  end
-end
