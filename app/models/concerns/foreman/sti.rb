ccee69a78ae4b4e18cb75e968f077e79d5198418
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-02 14:22:13 +0000

    Refs #29991 - Preload STI differently

--- app/models/concerns/foreman/sti.rb
+++ app/models/concerns/foreman/sti.rb
@@ -18,33 +18,6 @@ module Foreman
 
         super
       end
-
-      def descendants
-        preload_sti unless preloaded
-        super
-      end
-
-      # Constantizes all types present in the database. There might be more on
-      # disk, but that does not matter in practice as far as the STI API is
-      # concerned.
-      #
-      # Assumes store_full_sti_class is true, the default.
-      def preload_sti
-        return [] unless base_class.connected? && base_class.table_exists?
-        types_in_db = base_class
-                          .unscoped
-                          .select(inheritance_column)
-                          .distinct
-                          .pluck(inheritance_column)
-                          .compact
-
-        types_in_db.each do |type|
-          logger.debug("Preloading STI type #{type}")
-          type.constantize
-        end
-
-        self.preloaded = true
-      end
     end
 
     def save(*args, **kwargs)
