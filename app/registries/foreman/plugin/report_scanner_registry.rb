269c05aa7922f5e93f7bb2b252be558229be4ead
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-10 14:02:48 +0000

    Refs #29991 - Fix ReportScanner warning with plugins in mind

--- app/registries/foreman/plugin/report_scanner_registry.rb
+++ app/registries/foreman/plugin/report_scanner_registry.rb
@@ -1,15 +1,10 @@
 module Foreman
   class Plugin
     class ReportScannerRegistry
-      DEFAULT_REPORT_SCANNERS = [
-        ::ReportScanner::PuppetReportScanner,
-      ].freeze
-
       attr_accessor :report_scanners
 
       def initialize
         @report_scanners = []
-        register_default_scanner
       end
 
       def report_scanners
@@ -22,14 +17,6 @@ module Foreman
 
       def unregister_report_scanner(scanner)
         @report_scanners -= [scanner]
-      end
-
-      private
-
-      def register_default_scanner
-        DEFAULT_REPORT_SCANNERS.each do |default_scanner|
-          register_report_scanner default_scanner
-        end
       end
     end
   end
