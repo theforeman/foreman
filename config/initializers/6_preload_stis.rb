75274956fa7f1b09dc08cb06169c0647e680414b
Oleh Fedorenko <ofedoren@redhat.com>
2024-05-10 14:05:37 +0000

    Refs #29991 - Apply review suggestions

--- config/initializers/6_preload_stis.rb
+++ config/initializers/6_preload_stis.rb
@@ -3,5 +3,5 @@ Rails.application.config.to_prepare do
   # E.g. Base.register_type(BMC)
   # Some constants that use such classes may be defined before all the related classes/models are loaded and registered
   # E.g. InterfaceTypeMapper::ALLOWED_TYPE_NAMES
-  Rails.autoloaders.main.eager_load_dir("#{Rails.root}/app/models/nic")
+  Rails.autoloaders.main.eager_load_dir(Rails.root.join('app', 'models', 'nic'))
 end
