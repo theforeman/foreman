c5de5f7ca0040e59e95ab6e68fd0b2b82c525073
Adam J. Richardson <fatman.uk@gmail.com>
2024-07-08 23:07:10 +0100

    Add model for Void Linux

---
+++ app/models/operatingsystems/voidlinux.rb
@@ -0,0 +1,20 @@
+class Voidlinux < Operatingsystem
+  PXEFILES = {:kernel => "linux", :initrd => "initrd"}
+
+  # Simple output of the media url
+  def mediumpath(medium_provider)
+    medium_provider.medium_uri.to_s
+  end
+
+  def pxe_type
+    "aif"
+  end
+
+  def pxedir(medium_provider = nil)
+    "boot/$arch/loader"
+  end
+
+  def display_family
+    "Void Linux"
+  end
+end
