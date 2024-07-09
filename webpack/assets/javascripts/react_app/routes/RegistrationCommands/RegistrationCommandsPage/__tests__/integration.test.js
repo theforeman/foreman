5ac8bba5f2eb7cd6244f998a8c807322c0982371
Nadja Heitmann <nadjah@atix.de>
2024-07-09 20:23:54 +0000

    Fixes #37640 - Apply Loc/Org filter to Host Registration form

--- webpack/assets/javascripts/react_app/routes/RegistrationCommands/RegistrationCommandsPage/__tests__/integration.test.js
+++ webpack/assets/javascripts/react_app/routes/RegistrationCommands/RegistrationCommandsPage/__tests__/integration.test.js
@@ -8,8 +8,16 @@ import RegistrationCommandsPage from '../index'
 import { APIMiddleware } from '../../../../redux/API';
 import apiReducer from '../../../../redux/API/APIReducer';
 import { spySelector } from './fixtures'
+import ForemanContext from '../../../../Root/Context/ForemanContext';
 
 jest.mock('../../../../components/common/Slot', () => () => (<></>));
+jest
+  .spyOn(ForemanContext, 'useForemanOrganization')
+  .mockReturnValue({ id: 3, title: 'ACME' });
+jest
+  .spyOn(ForemanContext, 'useForemanLocation')
+  .mockReturnValue({ id: 4, title: 'munich' });
+
 
 spySelector(selectors);
 
@@ -28,6 +36,16 @@ describe('RegistrationCommandsPage integration', () => {
 
     expect(submitBtn.hasClass('pf-m-disabled')).toBe(false);
     expect(commandField.length).toBe(0);
+
+    // check that only current Org and Loc are selectable
+    const organizationSelectOptions = component.find('#reg_organization').find('FormSelectOption');
+    expect(organizationSelectOptions.length).toBe(2);
+    expect(organizationSelectOptions.findWhere((n) => n.prop('value') === 1).length).toBe(0);
+    expect(organizationSelectOptions.findWhere((n) => n.prop('value') === 3).length).toBe(2);
+    const locationSelectOptions = component.find('#reg_location').find('FormSelectOption');
+    expect(locationSelectOptions.length).toBe(2);
+    expect(locationSelectOptions.findWhere((n) => n.prop('value') === 2).length).toBe(0);
+    expect(locationSelectOptions.findWhere((n) => n.prop('value') === 4).length).toBe(2);
 
     submitBtn.simulate('click');
     integrationTestHelper.takeStoreAndLastActionSnapshot('generated command');
