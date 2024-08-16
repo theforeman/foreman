theforeman/foreman

commit d690fa772475adc612ca11f6bd3a0209a8133126

Fixes #36547 - Fix parsing of Ubuntu version in fact parsers

app/services/puppet_fact_parser.rb
@@ -5,9 +5,14 @@ class PuppetFactParser < FactParser
     orel = os_release.dup
 
     if orel.present?
-      major, minor = orel.split('.', 2)
-      major = major.to_s.gsub(/\D/, '')
-      minor = minor.to_s.gsub(/[^\d\.]/, '')
+      if os_name =~ /ubuntu/i
+        major = os_major_version
+        minor = os_minor_version
+      else
+        major, minor = orel.split('.', 2)
+        major = major.to_s.gsub(/\D/, '')
+        minor = minor.to_s.gsub(/[^\d\.]/, '')
+      end
       args = {:name => os_name, :major => major, :minor => minor}
       os = Operatingsystem.find_or_initialize_by(args)
       if os_name[/debian|ubuntu/i] || os.family == 'Debian'
@@ -225,6 +230,14 @@ class PuppetFactParser < FactParser
   rescue StandardError => e
     logger.error { "Failed to read the OS name: #{e}" }
     raise(::Foreman::Exception.new("invalid facts, missing operating system value"))
+  end
+
+  def os_major_version
+    facts.dig(:os, :release, :major)
+  end
+
+  def os_minor_version
+    facts.dig(:os, :release, :minor)
   end
 
   def os_release

app/models/operatingsystem.rb
@@ -169,12 +169,24 @@ class Operatingsystem < ApplicationRecord
   def self.find_by_to_label(str)
     os = find_by_description(str.to_s)
     return os if os
-    a = str.split(" ")
-    b = a[1].split('.') if a[1]
-    cond = {:name => a[0]}
-    cond[:major] = b[0] if b && b[0]
-    cond[:minor] = b[1] if b && b[1]
+    name, version = str.split(" ")
+    cond = {:name => name}
+    if version
+      (major, minor) = os_major_minor_from_version_str(name, version)
+      cond[:major] = major if major
+      cond[:minor] = minor if minor
+    end
     find_by(cond)
+  end
+
+  def self.os_major_minor_from_version_str(os_name, version_str)
+    if os_name == 'Ubuntu'
+      x, y, minor = version_str.split('.', 3)
+      major = "#{x}.#{y}"
+    else
+      major, minor = version_str.split('.')
+    end
+    [major, minor]
   end
 
   # Implemented only in the OSs subclasses where it makes sense

app/services/foreman_ansible/operating_system_parser.rb
@@ -68,8 +68,12 @@ module ForemanAnsible
     end
 
     def os_minor
-      _, minor = os_release&.split('.', 2) ||
+      if os_name == 'Ubuntu'
+        _, _, minor = os_release&.split('.', 3)
+      else
+        _, minor = os_release&.split('.', 2) ||
           (facts[:version].split('R') if os_name == 'junos')
+      end
       # Until Foreman supports os.minor as something that's not a number,
       # we should remove the extra dots in the version. E.g:
       # '6.1.7601.65536' becomes '6.1.760165536'

app/services/foreman_chef/fact_parser.rb
@@ -9,6 +9,8 @@ module ForemanChef
       # if we have no release information we can't assign OS properly (e.g. missing redhat-lsb)
       if release.nil?
         major, minor = 1, nil
+      elsif os_name == 'Ubuntu'
+        major, minor = release, nil
       else
         major, minor = release.split('.')
       end

app/services/katello/rhsm_fact_parser.rb
@@ -52,7 +52,7 @@ module Katello
       return nil if name.nil? || version.nil?
 
       os_name = distribution_to_puppet_os(name)
-      major, minor = version.split('.')
+      (major, minor) = ::Operatingsystem.os_major_minor_from_version_str(os_name, version)
       unless facts['ignore_os']
         os_attributes = {:major => major, :minor => minor || '', :name => os_name}
 

db/migrate/20230719080900_fix_ubuntu_versions.rb
@@ -1,0 +1,19 @@
+class FixUbuntuVersions < ActiveRecord::Migration[6.0]
+  def up
+    # For every Ubuntu operating system with a major version and a minor version in the format "<digits>(.<digits>)",
+    #   * We check if there is already an operating system present with a major version of
+    #     "<major>.<first_place_of_minor>" and a minor version of "<second_place_of_minor_or_empty>".
+    #   * If not, we update the major version of the operating system to "<major>.<first_place_of_minor>" and the minor
+    #     version to "<second_place_of_minor_or_empty>".
+    Operatingsystem.where(name: 'Ubuntu')
+      .where('operatingsystems.major ~* ?', '^\d*$')
+      .where('operatingsystems.minor ~* ?', '^\d\d(\.\d)?$')
+      .where('o2.id IS NULL')
+      .joins("left outer join operatingsystems o2 " \
+              "on o2.name = operatingsystems.name " \
+              "and o2.major = concat(operatingsystems.major, '.', split_part(operatingsystems.minor, '.', 1)) " \
+              "and o2.minor = split_part(operatingsystems.minor, '.', 2)")
+      .update_all("major = concat(operatingsystems.major, '.', split_part(operatingsystems.minor, '.', 1)), " \
+                  "minor = split_part(operatingsystems.minor, '.', 2)")
+  end
+end

test/factories/operatingsystem.rb
@@ -108,6 +108,24 @@ FactoryBot.define do
       title { 'Ubuntu Utopic' }
     end
 
+    factory :ubuntu22_04, class: Debian do
+      sequence(:name) { 'Ubuntu' }
+      major { '22.04' }
+      minor { '' }
+      type { 'Debian' }
+      release_name { 'jammy' }
+      title { 'Ubuntu Jammy' }
+    end
+
+    factory :ubuntu22_04_3, class: Debian do
+      sequence(:name) { 'Ubuntu' }
+      major { '22.04' }
+      minor { '3' }
+      type { 'Debian' }
+      release_name { 'jammy' }
+      title { 'Ubuntu Jammy' }
+    end
+
     factory :debian7_0, class: Debian do
       sequence(:name) { 'Debian' }
       major { '7' }

test/fixtures/operatingsystems.yml
@@ -20,8 +20,7 @@ redhat:
 
 ubuntu1010:
   name: Ubuntu
-  major: 10
-  minor: 10
+  major: '10.10'
   release_name: rn10
   type: Debian
   media: ubuntu

commit f03bc503b993197ef770285a30b080dcf29bc44d

Refs #36547 - explicitly stringify major/minor for Ubuntu
We stringify these anyway on save, but here it's also used for
searching, and that fails to find the existing OS when searching with
minor=nil.

When the existing OS is not found, a new one is created, but that fails
to save as the name/major/minor combination already exists and that
breaks fact uploads.

Fixes: d690fa772475adc612ca11f6bd3a0209a8133126

app/services/puppet_fact_parser.rb
@@ -6,8 +6,8 @@ class PuppetFactParser < FactParser
 
     if orel.present?
       if os_name =~ /ubuntu/i
-        major = os_major_version
-        minor = os_minor_version
+        major = os_major_version.to_s
+        minor = os_minor_version.to_s
       else
         major, minor = orel.split('.', 2)
         major = major.to_s.gsub(/\D/, '')