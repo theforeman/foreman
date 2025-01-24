198ba3a0a29a7122592f3d0a9f35ea820c2fea09
Oleh Fedorenko <ofedoren@redhat.com>
2025-01-24 13:45:12 +0000

    Fixes #38159 - Cache classes for apipie:cache task

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Foreman::Application.configure do
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = false
+  config.cache_classes = Foreman.in_rake?('apipie:cache')
 
   # Eager load is set for all environments
   config.eager_load = true
