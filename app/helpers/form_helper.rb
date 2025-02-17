aa916b0c9711f419e967cc83c2634f90aef6a44b
Adam Ruzicka <aruzicka@redhat.com>
2025-02-17 09:38:11 +0100

    Fixes #38217 - Set autocomplete attribute on name inputs
    to prevent a password manager from suggesting saved credentials in
    various name fields.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -2,7 +2,10 @@ module FormHelper
   def text_f(f, attr, options = {})
     field(f, attr, options) do
       addClass options, "form-control"
-      options[:focus_on_load] = true if options[:focus_on_load].nil? && attr.to_s == 'name'
+      if attr.to_s == 'name'
+        options[:focus_on_load] ||= true
+        options[:autocomplete] ||= "section-#{f.object_name} name"
+      end
       f.text_field attr, options
     end
   end
