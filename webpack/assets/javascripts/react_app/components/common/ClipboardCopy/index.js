2d6d57f37047ce0de68dfda4500a15b8464d53f6
Lukas Jezek <lukas-jezek@seznam.cz>
2025-02-26 11:08:25 +0000

    Fixes #38242 - erb template, common/ClipboardCopy migrated to pf5

--- webpack/assets/javascripts/react_app/components/common/ClipboardCopy/index.js
+++ webpack/assets/javascripts/react_app/components/common/ClipboardCopy/index.js
@@ -1,74 +1,36 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Icon } from 'patternfly-react';
-import { Tooltip, TooltipPosition } from '@patternfly/react-core';
+import {
+  ClipboardCopy as PFClipboardCopy,
+  ClipboardCopyVariant as PFClipboardCopyVariant,
+} from '@patternfly/react-core';
 import { translate as __ } from '../../../common/I18n';
-import './clipboard-copy.scss';
 
-const ClipboardCopy = ({
-  text: defaultText,
-  successMessage,
-  buttonText,
-  textareaProps,
-  buttonProps,
-  hideTextarea,
-  withCopyIcon,
-}) => {
-  const [text, setText] = useState(defaultText);
-
-  const clipboardClass = hideTextarea === false ? 'clipboard-copy' : '';
-  const iconMarginLeft = buttonText === '' ? '0px' : '5px';
+const ClipboardCopy = ({ text: defaultText, textareaProps }) => {
+  const [text] = useState(defaultText);
 
   return (
-    <div className={clipboardClass}>
-      {hideTextarea === false ? (
-        <textarea
-          defaultValue={text}
-          onChange={({ target: { value } }) => setText(value)}
-          {...textareaProps}
-        />
-      ) : null}
-      <Tooltip
-        content={successMessage}
-        position={TooltipPosition.right}
-        trigger="click"
-      >
-        <Button
-          onClick={() => navigator.clipboard.writeText(text)}
-          bsStyle="default"
-          {...buttonProps}
-        >
-          {buttonText}
-          {withCopyIcon && (
-            <Icon
-              style={{ marginLeft: iconMarginLeft }}
-              type="fa"
-              name="copy"
-            />
-          )}
-        </Button>
-      </Tooltip>
-    </div>
+    <PFClipboardCopy
+      hoverTip={__('Copy to clipboard')}
+      clickTip={__('Copied to clipboard')}
+      isBlock
+      variant={PFClipboardCopyVariant.expansion}
+      isExpanded
+      isReadOnly={textareaProps.readOnly ?? false}
+      className={textareaProps.className ?? ''}
+    >
+      {`${text}`}
+    </PFClipboardCopy>
   );
 };
 
 ClipboardCopy.propTypes = {
   text: PropTypes.string.isRequired,
-  buttonText: PropTypes.string,
-  successMessage: PropTypes.string,
   textareaProps: PropTypes.object,
-  buttonProps: PropTypes.object,
-  hideTextarea: PropTypes.bool,
-  withCopyIcon: PropTypes.bool,
 };
 
 ClipboardCopy.defaultProps = {
-  buttonText: __('Copy to clipboard'),
-  successMessage: __('Copied!'),
   textareaProps: {},
-  buttonProps: {},
-  hideTextarea: false,
-  withCopyIcon: false,
 };
 
 export default ClipboardCopy;
