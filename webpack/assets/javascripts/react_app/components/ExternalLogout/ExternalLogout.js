c9c7e73bd13119ed57bdd5d7504d51ed7bd3b904
Lukas Jezek <lukas-jezek@seznam.cz>
2025-03-05 08:58:33 +0000

    Fixes #38257 - ExternalLogout updated to pf5

--- webpack/assets/javascripts/react_app/components/ExternalLogout/ExternalLogout.js
+++ webpack/assets/javascripts/react_app/components/ExternalLogout/ExternalLogout.js
@@ -1,43 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Grid } from 'patternfly-react';
+import { Button, Grid, GridItem } from '@patternfly/react-core';
 import { translate as __ } from '../../common/I18n';
 import './externalLogout.scss';
 
-const ExternalLogout = ({
-  logoSrc,
-  version,
-  caption,
-  submitLink,
-  backgroundUrl,
-}) => {
+const ExternalLogout = ({ logoSrc, caption, submitLink, backgroundUrl }) => {
   const style = backgroundUrl
     ? { backgroundImage: `url(${backgroundUrl})` }
     : {};
   return (
     <div className="external-logout" style={style}>
       <Grid>
-        <Grid.Row>
-          <Grid.Col sm={8} smOffset={2} md={6} mdOffset={3}>
-            <header className="login-pf-page-header">
-              <img className="brand" src={logoSrc} alt="logo" />
-              <div className="login-pf-caption">
-                <h1 id="title">{__('Welcome')}</h1>
-                {caption && <p id="login_text">{caption}</p>}
-              </div>
-            </header>
-            <Button
-              type="submit"
-              bsStyle="primary"
-              bsSize="large"
-              block
-              className="login-pf-submit-button"
-              href={submitLink}
-            >
-              {__('Click to log in again')}
-            </Button>
-          </Grid.Col>
-        </Grid.Row>
+        <GridItem span={12} sm={8} smOffset={2} md={6} mdOffset={3}>
+          <header className="login-pf-page-header">
+            <img className="brand" src={logoSrc} alt="logo" />
+            <div className="login-pf-caption">
+              <h1 id="title">{__('Welcome')}</h1>
+              {caption && <p id="login_text">{caption}</p>}
+            </div>
+          </header>
+          <Button
+            ouiaId="login-pf-submit-button"
+            type="submit"
+            variant="primary"
+            size="lg"
+            isBlock
+            className="login-pf-submit-button"
+            href={submitLink}
+            component="a"
+          >
+            {__('Click to log in again')}
+          </Button>
+        </GridItem>
       </Grid>
     </div>
   );
@@ -47,7 +41,6 @@ ExternalLogout.propTypes = {
   backgroundUrl: PropTypes.string,
   caption: PropTypes.string,
   logoSrc: PropTypes.string,
-  version: PropTypes.string,
   submitLink: PropTypes.string.isRequired,
 };
 
@@ -55,7 +48,6 @@ ExternalLogout.defaultProps = {
   backgroundUrl: null,
   caption: null,
   logoSrc: null,
-  version: null,
 };
 
 export default ExternalLogout;
