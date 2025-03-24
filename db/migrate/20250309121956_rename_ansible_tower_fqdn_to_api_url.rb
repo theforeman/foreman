e48c8decba237164440135f94b433c6fdf1a2761
nofaralfasi <nofaralfasi@gmail.com>
2025-03-24 19:01:27 +0200

    Fixes #38319 - Fix Ansible Tower FQDN migration
    Updated the migration to bypass validations and callbacks for reliable record updates.

--- db/migrate/20250309121956_rename_ansible_tower_fqdn_to_api_url.rb
+++ db/migrate/20250309121956_rename_ansible_tower_fqdn_to_api_url.rb
@@ -1,7 +1,8 @@
 class RenameAnsibleTowerFqdnToApiURL < ActiveRecord::Migration[7.0]
   def up
     Parameter.where(name: 'ansible_tower_fqdn').find_each do |parameter|
-      parameter.update(
+      # Using update_columns bypasses ActiveRecord validations, callbacks, and audit tracking.
+      parameter.update_columns(
         name: 'ansible_tower_api_url',
         value: "https://#{parameter.value}/api/controller/v2"
       )
@@ -10,7 +11,8 @@ class RenameAnsibleTowerFqdnToApiURL < ActiveRecord::Migration[7.0]
 
   def down
     Parameter.where(name: 'ansible_tower_api_url').find_each do |parameter|
-      parameter.update(
+      # Using update_columns bypasses ActiveRecord validations, callbacks, and audit tracking.
+      parameter.update_columns(
         name: 'ansible_tower_fqdn',
         value: URI(parameter.value).host
       )
