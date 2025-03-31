ba001172db31b71b1df761fe9e565ec711ffad3b
Jeremy Lenz <jlenz@redhat.com>
2025-03-31 15:04:09 -0400

    Fixes #38333 - correct the TreeView prop name for checkboxes

--- webpack/assets/javascripts/react_app/components/ColumnSelector/ColumnSelector.js
+++ webpack/assets/javascripts/react_app/components/ColumnSelector/ColumnSelector.js
@@ -187,7 +187,7 @@ const ColumnSelector = props => {
             </Button>,
           ]}
         >
-          <TreeView data={selectedColumns} onCheck={onCheck} hasChecks />
+          <TreeView data={selectedColumns} onCheck={onCheck} hasCheckboxes />
         </Modal>
       </div>
     </div>
