20e434cd0015925d54b1b100f7b579cd9f1fda44
Lukas Jezek <lukas-jezek@seznam.cz>
2025-04-04 16:00:58 +0000

    Fixes #38242 - replace ClipboardCopy with PF5

--- webpack/assets/javascripts/react_app/components/common/ClipboardCopy/index.js
+++ webpack/assets/javascripts/react_app/components/common/ClipboardCopy/index.js
@@ -1,74 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Icon } from 'patternfly-react';
-import { Tooltip, TooltipPosition } from '@patternfly/react-core';
+import {
+  ClipboardCopy as PFClipboardCopy,
+  ClipboardCopyVariant as PFClipboardCopyVariant,
+} from '@patternfly/react-core';
 import { translate as __ } from '../../../common/I18n';
-import './clipboard-copy.scss';
 
-const ClipboardCopy = ({
-  text: defaultText,
-  successMessage,
-  buttonText,
-  textareaProps,
-  buttonProps,
-  hideTextarea,
-  withCopyIcon,
-}) => {
-  const [text, setText] = useState(defaultText);
-
-  const clipboardClass = hideTextarea === false ? 'clipboard-copy' : '';
-  const iconMarginLeft = buttonText === '' ? '0px' : '5px';
-
-  return (
-    <div className={clipboardClass}>
-      {hideTextarea === false ? (
-        <textarea
-          defaultValue={text}
-          onChange={({ target: { value } }) => setText(value)}
-          {...textareaProps}
-        />
-      ) : null}
-      <Tooltip
-        content={successMessage}
-        position={TooltipPosition.right}
-        trigger="click"
-      >
-        <Button
-          onClick={() => navigator.clipboard.writeText(text)}
-          bsStyle="default"
-          {...buttonProps}
-        >
-          {buttonText}
-          {withCopyIcon && (
-            <Icon
-              style={{ marginLeft: iconMarginLeft }}
-              type="fa"
-              name="copy"
-            />
-          )}
-        </Button>
-      </Tooltip>
-    </div>
-  );
-};
+const ClipboardCopy = ({ text: defaultText, textareaProps }) => (
+  <PFClipboardCopy
+    hoverTip={__('Copy to clipboard')}
+    clickTip={__('Copied to clipboard')}
+    isBlock
+    variant={PFClipboardCopyVariant.expansion}
+    isExpanded
+    isReadOnly={textareaProps.readOnly ?? false}
+    className={textareaProps.className ?? null}
+  >
+    {defaultText}
+  </PFClipboardCopy>
+);
 
 ClipboardCopy.propTypes = {
   text: PropTypes.string.isRequired,
-  buttonText: PropTypes.string,
-  successMessage: PropTypes.string,
   textareaProps: PropTypes.object,
-  buttonProps: PropTypes.object,
-  hideTextarea: PropTypes.bool,
-  withCopyIcon: PropTypes.bool,
 };
 
 ClipboardCopy.defaultProps = {
-  buttonText: __('Copy to clipboard'),
-  successMessage: __('Copied!'),
   textareaProps: {},
-  buttonProps: {},
-  hideTextarea: false,
-  withCopyIcon: false,
 };
 
 export default ClipboardCopy;
--- webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/NewPersonalAccessToken.js
+++ webpack/assets/javascripts/react_app/components/users/PersonalAccessTokens/NewPersonalAccessToken.js
@@ -1,8 +1,9 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { Alert } from 'patternfly-react';
-import ClipboardCopy from '../../common/ClipboardCopy';
+import { ClipboardCopy } from '@patternfly/react-core';
 import { translate as __ } from '../../../common/I18n';
+import './personalAccessToken.scss';
 
 const NewTokenInfo = ({ newPersonalAccessToken, onDismiss }) => (
   <Fragment>
@@ -10,9 +11,14 @@ const NewTokenInfo = ({ newPersonalAccessToken, onDismiss }) => (
       <Alert type="success" onDismiss={onDismiss}>
         <strong>{__('Your New Personal Access Token')}</strong>
         <ClipboardCopy
-          text={newPersonalAccessToken}
-          textareaProps={{ readOnly: true, className: 'col-md-6', rows: '1' }}
-        />
+          isReadOnly
+          hoverTip={__('Copy to clipboard')}
+          clickTip={__('Copied to clipboard')}
+          variant="inline-compact"
+          id="personal-token-clipboard"
+        >
+          {newPersonalAccessToken}
+        </ClipboardCopy>
         {__(
           'Make sure to copy your new personal access token now. You won’t be able to see it again!'
         )}
