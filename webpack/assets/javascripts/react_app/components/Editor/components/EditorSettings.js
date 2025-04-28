1aff9ff724d7f3372d28b0e593022eb6c4b8b63b
Lukas Jezek <lukas-jezek@seznam.cz>
2025-04-28 18:11:44 +0000

    Fixes #38352 - Editor settings popover not showing

--- webpack/assets/javascripts/react_app/components/Editor/components/EditorSettings.js
+++ webpack/assets/javascripts/react_app/components/Editor/components/EditorSettings.js
@@ -21,105 +21,109 @@ const EditorSettings = ({
   autocompletion,
   liveAutocompletion,
 }) => (
-  <Popover
-    id="cog-popover"
-    position={PopoverPosition.bottom}
-    enableFlip={false}
-    hasAutoWidth
-    headerContent={__('Settings')}
-    bodyContent={
-      <div>
-        <div className="cog-popover-dropdown">
-          <div className="cog-popover-dropdown-title">{__('Syntax')}</div>
-          <Dropdown disabled={selectedView === 'preview'} id="mode-dropdown">
-            <Dropdown.Toggle>{mode}</Dropdown.Toggle>
-            <Dropdown.Menu id="settings-dropdown">
-              {modes.map((aceMode, i) => (
-                <MenuItem
-                  key={i}
-                  onClick={() => changeSetting({ mode: aceMode })}
-                >
-                  {aceMode}
-                </MenuItem>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className="cog-popover-dropdown">
-          <div className="cog-popover-dropdown-title">{__('Keybind')}</div>
-          <Dropdown
-            disabled={selectedView === 'preview'}
-            id="keybindings-dropdown"
-          >
-            <Dropdown.Toggle>{keyBinding}</Dropdown.Toggle>
-            <Dropdown.Menu id="settings-dropdown">
-              {keyBindings.map((keyBind, i) => (
-                <MenuItem
-                  key={i}
-                  onClick={() => changeSetting({ keyBinding: keyBind })}
-                >
-                  {keyBind}
-                </MenuItem>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className="cog-popover-dropdown">
-          <div className="cog-popover-dropdown-title">{__('Theme')}</div>
-          <Dropdown id="themes-dropdown">
-            <Dropdown.Toggle>{theme}</Dropdown.Toggle>
-            <Dropdown.Menu id="settings-dropdown">
-              {themes.map((themeKey, i) => (
-                <MenuItem
-                  key={i}
-                  onClick={() => changeSetting({ theme: themeKey })}
-                >
-                  {themeKey}
-                </MenuItem>
-              ))}
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className="cog-popover-dropdown">
-          <div className="cog-popover-dropdown-title">
-            {__('Autocompletion')}
+  <>
+    <Popover
+      id="cog-popover"
+      position={PopoverPosition.bottom}
+      enableFlip={false}
+      hasAutoWidth
+      headerContent={__('Settings')}
+      bodyContent={
+        <div>
+          <div className="cog-popover-dropdown">
+            <div className="cog-popover-dropdown-title">{__('Syntax')}</div>
+            <Dropdown disabled={selectedView === 'preview'} id="mode-dropdown">
+              <Dropdown.Toggle>{mode}</Dropdown.Toggle>
+              <Dropdown.Menu id="settings-dropdown">
+                {modes.map((aceMode, i) => (
+                  <MenuItem
+                    key={i}
+                    onClick={() => changeSetting({ mode: aceMode })}
+                  >
+                    {aceMode}
+                  </MenuItem>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
           </div>
-          <div className="dropdown btn-group">
-            <input
-              id="autocompletion-checkbox"
-              name="autocompletion"
-              type="checkbox"
-              checked={autocompletion}
-              onChange={e => changeSetting({ autocompletion: !autocompletion })}
-            />
+          <div className="cog-popover-dropdown">
+            <div className="cog-popover-dropdown-title">{__('Keybind')}</div>
+            <Dropdown
+              disabled={selectedView === 'preview'}
+              id="keybindings-dropdown"
+            >
+              <Dropdown.Toggle>{keyBinding}</Dropdown.Toggle>
+              <Dropdown.Menu id="settings-dropdown">
+                {keyBindings.map((keyBind, i) => (
+                  <MenuItem
+                    key={i}
+                    onClick={() => changeSetting({ keyBinding: keyBind })}
+                  >
+                    {keyBind}
+                  </MenuItem>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <div className="cog-popover-dropdown">
+            <div className="cog-popover-dropdown-title">{__('Theme')}</div>
+            <Dropdown id="themes-dropdown">
+              <Dropdown.Toggle>{theme}</Dropdown.Toggle>
+              <Dropdown.Menu id="settings-dropdown">
+                {themes.map((themeKey, i) => (
+                  <MenuItem
+                    key={i}
+                    onClick={() => changeSetting({ theme: themeKey })}
+                  >
+                    {themeKey}
+                  </MenuItem>
+                ))}
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+          <div className="cog-popover-dropdown">
+            <div className="cog-popover-dropdown-title">
+              {__('Autocompletion')}
+            </div>
+            <div className="dropdown btn-group">
+              <input
+                id="autocompletion-checkbox"
+                name="autocompletion"
+                type="checkbox"
+                checked={autocompletion}
+                onChange={e =>
+                  changeSetting({ autocompletion: !autocompletion })
+                }
+              />
+            </div>
+          </div>
+          <div className="cog-popover-dropdown">
+            <div className="cog-popover-dropdown-title">
+              {__('Live Autocompletion')}
+            </div>
+            <div className="dropdown btn-group">
+              <input
+                id="live-autocompletion-checkbox"
+                name="liveAutocompletion"
+                type="checkbox"
+                checked={liveAutocompletion}
+                disabled={!autocompletion}
+                onChange={e =>
+                  changeSetting({ liveAutocompletion: !liveAutocompletion })
+                }
+              />
+            </div>
           </div>
         </div>
-        <div className="cog-popover-dropdown">
-          <div className="cog-popover-dropdown-title">
-            {__('Live Autocompletion')}
-          </div>
-          <div className="dropdown btn-group">
-            <input
-              id="live-autocompletion-checkbox"
-              name="liveAutocompletion"
-              type="checkbox"
-              checked={liveAutocompletion}
-              disabled={!autocompletion}
-              onChange={e =>
-                changeSetting({ liveAutocompletion: !liveAutocompletion })
-              }
-            />
-          </div>
-        </div>
-      </div>
-    }
-  >
+      }
+      triggerRef={() => document.getElementById('cog-btn')}
+    />
     <Tooltip content={__('Settings')} position={TooltipPosition.top}>
       <Button className="editor-button" id="cog-btn" bsStyle="link">
         <Icon size="lg" name="cog" />
       </Button>
     </Tooltip>
-  </Popover>
+  </>
 );
 
 EditorSettings.propTypes = {
