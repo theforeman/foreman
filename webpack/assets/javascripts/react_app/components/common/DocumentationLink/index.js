5e97fc0fe2f63141c46238e1133edb18dd1cfac9
Lukas Jezek <lukas-jezek@seznam.cz>
2025-06-04 16:06:26 +0000

    Fixes #38464 - Deprecate common/DocumentationLink

--- webpack/assets/javascripts/react_app/components/common/DocumentationLink/index.js
+++ webpack/assets/javascripts/react_app/components/common/DocumentationLink/index.js
@@ -1,22 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { MenuItem, Icon } from 'patternfly-react';
-import { newWindowOnClick } from '../../../common/helpers';
-import { translate as __ } from '../../../../react_app/common/I18n';
+import { deprecate } from '../../../common/DeprecationService';
 
-const DocumentationLink = ({ href, children }) => (
-  <MenuItem key="documentationUrl" onClick={newWindowOnClick(href)}>
-    <Icon type="fa" name="question-circle" /> {children}
-  </MenuItem>
-);
-
-DocumentationLink.propTypes = {
-  href: PropTypes.string.isRequired,
-  children: PropTypes.node,
-};
-
-DocumentationLink.defaultProps = {
-  children: __('Documentation'),
-};
-
-export default DocumentationLink;
+export const DocumentationLink = () =>
+  deprecate('DocumentationLink', 'components/PF4/DocumentationLink', '3.18');
