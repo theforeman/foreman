131207a5b69b2f91e337e80339936b866023b955
Macleykun <wesley.de.vree@gmail.com>
2025-07-18 10:50:52 +0200

    Revert "Fixes #38554 - Host registration on Debian based systems fail because dpkg options are improperly formatted"
    This reverts commit 208a99ab4b73878468d823d834edb9513d26a9f7.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,20 +35,31 @@ $(document).on('click', 'a[disabled="disabled"]', function(event) {
   return handleDisabledClick(event, this);
 });
 
-const removeSelect2TitlesTooltips = function() {
-  // select2 tooltips dont update and dont close properly
-  $('.select2-selection span').attr('title', '');
-  $('.select2-selection__rendered').attr('title', '');
-  $('.select2-selection__clear').attr('title', '');
-  $(document).on(
-    'select2:select select2:unselecting',
-    '.select2-hidden-accessible',
-    function() {
-      $('.select2-selection span').attr('title', '');
-      $('.select2-selection__rendered').attr('title', '');
-      $('.select2-selection__clear').attr('title', '');
-    }
+const autoUpdateSelect2Titles = function() {
+  const targetNodes = document.querySelectorAll(
+    '.select2-selection__rendered'
   );
+
+  const config = { attributes: true, attributeFilter: ['title'] };
+
+  const callback = function(mutationsList) {
+    for (let mutation of mutationsList) {
+      if (
+        mutation.type === 'attributes' &&
+        mutation.attributeName === 'title'
+      ) {
+        mutation.target.setAttribute(
+          'data-original-title',
+          mutation.target.getAttribute('title')
+        );
+      }
+    }
+  };
+
+  targetNodes.forEach(targetNode => {
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  });
 };
 
 function onContentLoad() {
@@ -107,7 +118,33 @@ function onContentLoad() {
         .remove();
     });
 
-  removeSelect2TitlesTooltips();
+  const hideSelect2ClearTooltip = function() {
+    $(document).on('blur', '.select2-selection__clear', function() {
+      $('.tooltip').tooltip('hide');
+    });
+
+    const targetNode = document.querySelector('body');
+    const config = { attributes: false, childList: true, subtree: true };
+    const callback = function(mutationsList) {
+      for (let mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          const node = Array.from(mutation.removedNodes).find(
+            node =>
+              node.classList &&
+              node.classList.contains('select2-selection__clear')
+          );
+          if (node) {
+            $('.tooltip').tooltip('hide');
+          }
+        }
+      }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+  };
+
+  hideSelect2ClearTooltip();
+  autoUpdateSelect2Titles();
 }
 
 function preserve_selected_options(elem) {
@@ -375,7 +412,7 @@ function reloadOnAjaxComplete(element) {
   activate_select2(':root');
   tfm.advancedFields.initAdvancedFields();
   tfm.templateInputs.initTypeChanges();
-  removeSelect2TitlesTooltips();
+  autoUpdateSelect2Titles();
 }
 
 function set_fullscreen(element) {
