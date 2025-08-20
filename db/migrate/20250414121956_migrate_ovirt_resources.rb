4a831db2a2479803ac2f109527947dab1164f124
Frank Wall <fw@moov.de>
2025-08-20 13:59:26 +0200

    Refs #38266 - Skip removal of oVirt data if plugin is installed

--- db/migrate/20250414121956_migrate_ovirt_resources.rb
+++ db/migrate/20250414121956_migrate_ovirt_resources.rb
@@ -1,9 +1,19 @@
 class MigrateOvirtResources < ActiveRecord::Migration[7.0]
   def up
-    ComputeResourceCleaner.new(klass: 'Foreman::Model::Ovirt').run!
+    unless plugin_present?('foreman_ovirt')
+      ComputeResourceCleaner.new(klass: 'Foreman::Model::Ovirt').run!
+    end
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration, "Cannot restore migrated Ovirt data."
+    unless plugin_present?('foreman_ovirt')
+      raise ActiveRecord::IrreversibleMigration, "Cannot restore migrated Ovirt data."
+    end
+  end
+
+  private
+
+  def plugin_present?(name)
+    Foreman::Plugin.find(name).present?
   end
 end
