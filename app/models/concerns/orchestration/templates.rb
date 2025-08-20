93b8a05d4ffdb57589135bb22376c2c283ad50cf
Stejskal Leos <github@stejskalleos.cz>
2025-08-20 11:11:31 +0000

    refs #38422 - Rename methods in Orchestration::Templates
    Original method names collide with the Orchestration::SshProvision module,
    causing an unexpected behavior when the script execution exits with a non-zero status.
    
    refs 5295d856daf912b311c3f668d866795a6ca5c513

--- app/models/concerns/orchestration/templates.rb
+++ app/models/concerns/orchestration/templates.rb
@@ -9,20 +9,20 @@ module Orchestration::Templates
   def queue_render_checks
     return if skip_orchestration?
     return unless managed?
-    return unless template
+    return unless template_to_render
 
     logger.debug "Scheduling render checks of template for #{self}"
 
-    queue.create name: _("Check renderability of template '%{name}'.") % { name: template.name },
+    queue.create name: _("Check renderability of template '%{name}'.") % { name: template_to_render.name },
       priority: 1, action: [self, :set_renderability]
   end
 
   def set_renderability
-    template.render(host: self)
+    template_to_render.render(host: self)
     true
   rescue => e
-    Foreman::Logging.exception("Error while rendering '#{template.name}' template", e)
-    failure _("Failed to render template '%{t}', error: %{e}") % { t: template.name, e: e }
+    Foreman::Logging.exception("Error while rendering '#{template_to_render.name}' template", e)
+    failure _("Failed to render template '%{t}', error: %{e}") % { t: template_to_render.name, e: e }
   end
 
   def del_renderability
@@ -31,16 +31,14 @@ module Orchestration::Templates
 
   private
 
-  def template
-    @template ||= provisioning_template(kind: kind)
-  end
+  def template_to_render
+    kind = case provision_method
+      when 'build'
+        'provision'
+      when 'image'
+        template_kinds('image').first&.name
+           end
 
-  def kind
-    case provision_method
-    when 'build'
-      'provision'
-    when 'image'
-      template_kinds('image').first&.name
-    end
+    @template_to_render ||= provisioning_template(kind: kind)
   end
 end
