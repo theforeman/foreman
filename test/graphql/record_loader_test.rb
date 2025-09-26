d20ef2ee7083a9b7cb0aba1bfd4b745f055ea1f2
Adam Ruzicka <aruzicka@redhat.com>
2025-09-26 16:57:03 +0200

    Fixes #38913 - GraphQL taxonomy scoping
    (cherry picked from commit 13cafa0391b45c074c2b8473efe20c8ca2efc56d)

--- test/graphql/record_loader_test.rb
+++ test/graphql/record_loader_test.rb
@@ -38,4 +38,83 @@ class RecordLoaderTest < ActiveSupport::TestCase
       assert_nil object
     end
   end
+
+  context 'with taxonomy scoping' do
+    setup do
+      @organization1 = FactoryBot.create(:organization)
+      @organization2 = FactoryBot.create(:organization)
+      @location1 = FactoryBot.create(:location)
+      @location2 = FactoryBot.create(:location)
+    end
+
+    context 'for organizations' do
+      let(:user) do
+        user = setup_user('view', 'organizations')
+        user.organizations = [@organization1]
+        user.save!
+        user
+      end
+
+      test 'loads organization within user scope' do
+        User.current = user
+        org = GraphQL::Batch.batch do
+          RecordLoader.for(Organization).load(@organization1.id).then
+        end
+        assert_equal @organization1, org
+      end
+
+      test 'does not load organization outside user scope' do
+        User.current = user
+        org = GraphQL::Batch.batch do
+          RecordLoader.for(Organization).load(@organization2.id).then
+        end
+        assert_nil org
+      end
+
+      test 'admin loads all organizations' do
+        User.current = FactoryBot.create(:user, :admin)
+        [@organization1, @organization2].each do |org|
+          loaded_org = GraphQL::Batch.batch do
+            RecordLoader.for(Organization).load(org.id).then
+          end
+          assert_equal org, loaded_org
+        end
+      end
+    end
+
+    context 'for locations' do
+      let(:user) do
+        user = setup_user('view', 'locations')
+        user.locations = [@location1]
+        user.save!
+        user
+      end
+
+      test 'loads location within user scope' do
+        User.current = user
+        loc = GraphQL::Batch.batch do
+          RecordLoader.for(Location).load(@location1.id).then
+        end
+        assert_equal @location1, loc
+      end
+
+      test 'does not load location outside user scope' do
+        User.current = user
+        loc = GraphQL::Batch.batch do
+          RecordLoader.for(Location).load(@location2.id).then
+        end
+        assert_nil loc
+      end
+
+      test 'admin loads all locations' do
+        User.current = FactoryBot.create(:user, :admin)
+        [@location1, @location2].each do |loc|
+          loaded_loc = GraphQL::Batch.batch do
+            RecordLoader.for(Location).load(loc.id).then
+          end
+          assert_equal loc, loaded_loc
+        end
+      end
+    end
+  end
 end
