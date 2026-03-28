15ad940b1d46ff69bc9d9187b7ee3e2a34b74a1b
Jakub Duchek <jakduch@seznam.cz>
2026-03-28 20:33:24 +0100

    Fixes #31239 - Validate root password length even outside build mode
    When a managed host is not in build mode, the root_pass validation
    was completely skipped, allowing short passwords like "123" to be
    saved. Split the validation so that minimum length is always checked
    when root_pass is present, while presence is only required in build
    mode. This preserves the ability to have a blank root password
    outside build mode (needed for Katello) while preventing weak
    passwords from being accepted.

--- app/models/host/managed.rb
+++ app/models/host/managed.rb
@@ -317,7 +317,8 @@ class Host::Managed < Host::Base
 
   validates :architecture_id, :presence => true, :if => proc { |host| host.managed }
   validates :root_pass, :length => {:minimum => 8, :message => N_('should be 8 characters or more')},
-                        :presence => {:message => N_('should not be blank - consider setting a global or host group default')},
+                        :if => proc { |host| host.managed && !host.image_build? && host.root_pass.present? }
+  validates :root_pass, :presence => {:message => N_('should not be blank - consider setting a global or host group default')},
                         :if => proc { |host| host.managed && !host.image_build? && build? }
   validates :ptable_id, :presence => {:message => N_("can't be blank unless a custom partition has been defined")},
                         :if => proc { |host| host.managed && host.disk.empty? && !Foreman.in_rake? && !host.image_build? && host.build? }
