theforeman/foreman

commit 47dca75e8c530937a771df0f39f5a1190e75577f

Fixes #38916 - Update EditorHostSelect to pf5
Use the shared PF5 AutocompleteInput for the template editor preview host
field.

Drop patternfly-react-extensions and its webpack vendor bundle and SCSS
imports now that the editor no longer uses the extensions Select
component.

Co-authored-by: Lukas Jezek <ljezek@redhat.com>

webpack/assets/javascripts/react_app/components/Editor/components/EditorNavbar.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+/* eslint-disable max-lines */
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Nav, Spinner, Alert, Button } from 'patternfly-react';
 import { translate as __ } from '../../../common/I18n';
+import AutocompleteInput from '../../common/AutocompleteInput/AutocompleteInput';
 import EditorRadioButton from './EditorRadioButton';
 import EditorOptions from './EditorOptions';
-import EditorHostSelect from './EditorHostSelect';
 import EditorSafemodeCheckbox from './EditorSafemodeCheckbox';
 
 const EditorNavbar = ({
@@ -68,6 +69,47 @@ const EditorNavbar = ({
   };
   const selectedRenderPath = safemode ? safemodeRenderPath : renderPath;
 
+  const hostOptions = useMemo(() => {
+    const pool = isSearchingHosts ? filteredHosts : hosts;
+    const mapped = Array.from(pool, h => ({
+      value: h.id,
+      label: h.name,
+    }));
+    if (selectedHost?.id !== '' && selectedHost?.id != null) {
+      const idStr = String(selectedHost.id);
+      if (!mapped.some(o => String(o.value) === idStr)) {
+        return [
+          { value: selectedHost.id, label: selectedHost.name },
+          ...mapped,
+        ];
+      }
+    }
+    return mapped;
+  }, [hosts, filteredHosts, selectedHost, isSearchingHosts]);
+
+  const resolveHostById = hostId => {
+    const strId = String(hostId);
+    if (String(selectedHost.id) === strId) {
+      previewTemplate({
+        host: selectedHost,
+        renderPath: selectedRenderPath,
+        templateKindId,
+      });
+      return;
+    }
+    const primary = isSearchingHosts ? filteredHosts : hosts;
+    let host = primary.find(h => String(h.id) === strId);
+    if (!host) host = hosts.find(h => String(h.id) === strId);
+    if (!host) host = filteredHosts.find(h => String(h.id) === strId);
+    if (host) {
+      previewTemplate({
+        host,
+        renderPath: selectedRenderPath,
+        templateKindId,
+      });
+    }
+  };
+
   return (
     <div className="navbar navbar-form navbar-full-width navbar-editor">
       <Nav className="nav nav-tabs nav-tabs-pf nav-tabs-pf-secondary">
@@ -94,7 +136,7 @@ const EditorNavbar = ({
           }}
         />
         {showPreview && (
-          <React.Fragment>
+          <>
             <EditorRadioButton
               stateView={selectedView}
               btnView="preview"
@@ -112,27 +154,29 @@ const EditorNavbar = ({
                 }
               }}
             />
-            {showHostSelector && (
-              <EditorHostSelect
-                show={selectedView === 'preview'}
-                open={isSelectOpen}
-                selectedItem={selectedHost}
-                placeholder={__('Select Host...')}
-                isLoading={isFetchingHosts}
-                onChange={host =>
-                  previewTemplate({
-                    host,
-                    renderPath: selectedRenderPath,
-                    templateKindId,
-                  })
-                }
-                searchQuery={searchQuery}
-                onToggle={onHostSelectToggle}
-                onSearchChange={onHostSearch}
-                onSearchClear={onSearchClear}
-                options={isSearchingHosts ? filteredHosts : hosts}
-                key="hostsSelect"
-              />
+            {showHostSelector && selectedView === 'preview' && (
+              <>
+                <AutocompleteInput
+                  name="editor-preview-host"
+                  placeholder={__('Filter Host...')}
+                  selected={
+                    selectedHost.id === '' || selectedHost.id == null
+                      ? ''
+                      : selectedHost.id
+                  }
+                  options={hostOptions}
+                  onChange={inputValue =>
+                    onHostSearch({ target: { value: inputValue } })
+                  }
+                  onBlur={() => onHostSearch({ target: { value: '' } })}
+                  onSelect={resolveHostById}
+                />
+                {isFetchingHosts && (
+                  <div id="editor-host-fetch-spinner">
+                    <Spinner size="sm" loading />
+                  </div>
+                )}
+              </>
             )}
             <EditorSafemodeCheckbox
               show={selectedView === 'preview'}
@@ -166,7 +210,7 @@ const EditorNavbar = ({
                 <Spinner size="sm" loading />
               </div>
             )}
-          </React.Fragment>
+          </>
         )}
       </Nav>
       <EditorOptions

webpack/assets/javascripts/react_app/components/common/AutocompleteInput/AutocompleteInput.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-lines */
 
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Select,
@@ -27,12 +27,16 @@ export const AutocompleteInputComponent = ({
   validationMsg,
   isDisabled,
   fieldId,
+  onBlur,
 }) => {
   if (validationStatus === 'error') validationStatus = 'danger';
   const NO_RESULTS = __('No matches found');
-  const noOptions = [
-    { value: '', isAriaDisabled: true, disabled: true, label: NO_RESULTS },
-  ];
+  const noOptions = useMemo(
+    () => [
+      { value: '', isAriaDisabled: true, disabled: true, label: NO_RESULTS },
+    ],
+    [NO_RESULTS]
+  );
 
   const displayOptions = options.length < 1 ? noOptions : options;
 
@@ -49,6 +53,10 @@ export const AutocompleteInputComponent = ({
   const [activeItemId, setActiveItemId] = useState(null);
   const textInputRef = useRef(null);
   const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    setInputValue(displayValue);
+  }, [displayValue]);
 
   useEffect(() => {
     const input = textInputRef.current;
@@ -73,14 +81,10 @@ export const AutocompleteInputComponent = ({
           },
         ];
       }
-      if (!isOpen) {
-        setIsOpen(true);
-      }
+      setIsOpen(true);
     }
     setSelectOptions(newSelectOptions);
-
-    /* eslint-disable react-hooks/exhaustive-deps */
-  }, [filterValue]);
+  }, [filterValue, displayOptions, NO_RESULTS, options]);
 
   const createItemId = value => `select-typeahead-${value}`;
   const setActiveAndFocusedItem = itemIndex => {
@@ -103,6 +107,8 @@ export const AutocompleteInputComponent = ({
       closeMenu();
     }
     setInputValue(displayValue);
+    setFilterValue('');
+    onBlur(displayValue);
   };
 
   const onInputClick = () => {
@@ -311,6 +317,7 @@ AutocompleteInputComponent.propTypes = {
   validationMsg: PropTypes.string,
   isDisabled: PropTypes.bool,
   fieldId: PropTypes.string,
+  onBlur: PropTypes.func,
 };
 
 AutocompleteInputComponent.defaultProps = {
@@ -323,6 +330,7 @@ AutocompleteInputComponent.defaultProps = {
   onChange: () => {},
   isDisabled: false,
   fieldId: undefined,
+  onBlur: () => {},
 };
 
 export default AutocompleteInputComponent;

webpack/assets/javascripts/react_app/components/common/AutocompleteInput/__tests__/AutocompleteInput.test.js
@@ -185,6 +185,7 @@ describe('AutocompleteInput RTL Tests', () => {
       const input = screen.getByRole('combobox');
       expect(input).toHaveValue('true');
     });
+
   });
 
   describe('Accessibility', () => {

config/webpack.vendor.js
@@ -40,7 +40,6 @@ module.exports = [
    * Patternfly related
    */
   'patternfly-react',
-  'patternfly-react-extensions',
   '@patternfly/react-core',
   '@patternfly/react-icons',
   '@patternfly/react-table',

webpack/assets/javascripts/react_app/common/scss/vendor-core.scss
@@ -12,7 +12,6 @@
 
 // patternfly v3
 @import '~patternfly-react/dist/sass/_patternfly-react.scss';
-@import '~patternfly-react-extensions/dist/sass/_select.scss';
 @import '~patternfly/dist/sass/patternfly/_loading-state';
 
 // patternfly v5

webpack/assets/javascripts/react_app/components/Editor/components/EditorHostSelect.js
@@ -1,87 +1,0 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { Select } from 'patternfly-react-extensions';
-import { translate as __ } from '../../../common/I18n';
-import './editorhostselect.scss';
-
-class EditorHostSelect extends Component {
-  componentDidMount() {
-    document.addEventListener('mousedown', this.handleClickOutside);
-  }
-
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleClickOutside);
-  }
-
-  setWrapperRef = node => {
-    this.selectRef = node;
-  };
-
-  handleClickOutside = event => {
-    if (this.selectRef && !this.selectRef.contains(event.target)) {
-      const { open, onToggle } = this.props;
-      if (open) onToggle();
-    }
-  };
-
-  onKey = event => {
-    if (event.keyCode === 27) {
-      const { open, onToggle } = this.props;
-      if (open) onToggle();
-    }
-  };
-
-  render() {
-    const {
-      show,
-      isLoading,
-      onChange,
-      onSearchChange,
-      onSearchClear,
-      onToggle,
-      open,
-      options,
-      searchQuery,
-      selectedItem,
-    } = this.props;
-    return (
-      <div
-        ref={this.setWrapperRef}
-        id="editor-select-container"
-        className={show ? '' : 'hidden'}
-      >
-        <Select
-          options={options}
-          placeholder={__('Filter Host...')}
-          open={open}
-          onToggle={onToggle}
-          searchValue={searchQuery}
-          onSearchChange={onSearchChange}
-          onSearchClear={onSearchClear}
-          onKeyDown={this.onKey}
-          onItemClick={onChange}
-          selectedItem={selectedItem}
-          isLoading={isLoading}
-        />
-      </div>
-    );
-  }
-}
-
-EditorHostSelect.propTypes = {
-  show: PropTypes.bool.isRequired,
-  isLoading: PropTypes.bool.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSearchChange: PropTypes.func.isRequired,
-  onSearchClear: PropTypes.func.isRequired,
-  onToggle: PropTypes.func.isRequired,
-  open: PropTypes.bool.isRequired,
-  options: PropTypes.array.isRequired,
-  searchQuery: PropTypes.string.isRequired,
-  selectedItem: PropTypes.shape({
-    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-    name: PropTypes.string,
-  }).isRequired,
-};
-
-export default EditorHostSelect;

webpack/assets/javascripts/react_app/components/Editor/components/__tests__/EditorHostSelect.test.js
@@ -1,37 +1,0 @@
-import { testComponentSnapshotsWithFixtures } from '../../../../common/testHelpers';
-import { noop } from '../../../../common/helpers';
-
-import EditorHostSelect from '../EditorHostSelect';
-import { editorOptions } from '../../Editor.fixtures';
-
-const fixtures = {
-  'renders EditorHostSelect': {
-    show: true,
-    open: true,
-    selectedItem: { id: '1', name: 'one' },
-    onToggle: noop,
-    searchQuery: '',
-    onSearchChange: noop,
-    onSearchClear: noop,
-    onChange: noop,
-    isLoading: false,
-    options: editorOptions.hosts,
-  },
-  'renders EditorHostSelect loading': {
-    show: true,
-    open: true,
-    onToggle: noop,
-    selectedItem: { id: '1', name: 'one' },
-    searchQuery: '',
-    onSearchChange: noop,
-    onSearchClear: noop,
-    onChange: noop,
-    isLoading: true,
-    options: editorOptions.hosts,
-  },
-};
-
-describe('EditorHostSelect', () => {
-  describe('should render', () =>
-    testComponentSnapshotsWithFixtures(EditorHostSelect, fixtures));
-});

webpack/assets/javascripts/react_app/components/Editor/components/__tests__/__snapshots__/EditorHostSelect.test.js.snap
@@ -1,85 +1,0 @@
-// Jest Snapshot v1, https://goo.gl/fbAQLP
-
-exports[`EditorHostSelect should render renders EditorHostSelect 1`] = `
-<div
-  className=""
-  id="editor-select-container"
->
-  <Select
-    disabled={false}
-    emptyText="No Results"
-    focusOnMount={true}
-    isLoading={false}
-    isSearchable={true}
-    loadingText="Loading..."
-    onItemClick={[Function]}
-    onKeyDown={[Function]}
-    onSearchChange={[Function]}
-    onSearchClear={[Function]}
-    onToggle={[Function]}
-    open={true}
-    options={
-      Array [
-        Object {
-          "id": "1",
-          "name": "host1",
-        },
-        Object {
-          "id": "2",
-          "name": "host2",
-        },
-      ]
-    }
-    placeholder="Filter Host..."
-    searchValue=""
-    selectedItem={
-      Object {
-        "id": "1",
-        "name": "one",
-      }
-    }
-  />
-</div>
-`;
-
-exports[`EditorHostSelect should render renders EditorHostSelect loading 1`] = `
-<div
-  className=""
-  id="editor-select-container"
->
-  <Select
-    disabled={false}
-    emptyText="No Results"
-    focusOnMount={true}
-    isLoading={true}
-    isSearchable={true}
-    loadingText="Loading..."
-    onItemClick={[Function]}
-    onKeyDown={[Function]}
-    onSearchChange={[Function]}
-    onSearchClear={[Function]}
-    onToggle={[Function]}
-    open={true}
-    options={
-      Array [
-        Object {
-          "id": "1",
-          "name": "host1",
-        },
-        Object {
-          "id": "2",
-          "name": "host2",
-        },
-      ]
-    }
-    placeholder="Filter Host..."
-    searchValue=""
-    selectedItem={
-      Object {
-        "id": "1",
-        "name": "one",
-      }
-    }
-  />
-</div>
-`;

commit e3c58a545cd1eba23db7575ff10877e0e375ef94

Fixes #39311 - Allow selection clearance for AutocompleteInput
Fixes: 47dca75e8c530937a771df0f39f5a1190e75577f

- Explicitly check selected for empty values
- Drop custom onKeyDown logic
- Add tests
- Fix linter failures
- Fix onBlur stale value

webpack/assets/javascripts/react_app/components/Editor/components/EditorNavbar.js
@@ -167,6 +167,7 @@ const EditorNavbar = ({
                   }
                   onBlur={() => onHostSearch({ target: { value: '' } })}
                   onSelect={resolveHostById}
+                  allowClear={false}
                 />
                 {isFetchingHosts && (
                   <div id="editor-host-fetch-spinner">

webpack/assets/javascripts/react_app/components/common/AutocompleteInput/AutocompleteInput.js
@@ -28,6 +28,7 @@ export const AutocompleteInputComponent = ({
   isDisabled,
   fieldId,
   onBlur,
+  allowClear,
 }) => {
   if (validationStatus === 'error') validationStatus = 'danger';
   const NO_RESULTS = __('No matches found');
@@ -49,13 +50,12 @@ export const AutocompleteInputComponent = ({
   const [inputValue, setInputValue] = useState(displayValue);
   const [filterValue, setFilterValue] = useState('');
   const [selectOptions, setSelectOptions] = useState(displayOptions);
-  const [focusedItemIndex, setFocusedItemIndex] = useState(null);
-  const [activeItemId, setActiveItemId] = useState(null);
   const textInputRef = useRef(null);
-  const wrapperRef = useRef(null);
+  const inputValueRef = useRef(inputValue);
 
   useEffect(() => {
     setInputValue(displayValue);
+    inputValueRef.current = displayValue;
   }, [displayValue]);
 
   useEffect(() => {
@@ -86,29 +86,26 @@ export const AutocompleteInputComponent = ({
     setSelectOptions(newSelectOptions);
   }, [filterValue, displayOptions, NO_RESULTS, options]);
 
-  const createItemId = value => `select-typeahead-${value}`;
-  const setActiveAndFocusedItem = itemIndex => {
-    setFocusedItemIndex(itemIndex);
-    const focusedItem = selectOptions[itemIndex];
-    setActiveItemId(createItemId(focusedItem.value));
-  };
-  const resetActiveAndFocusedItem = () => {
-    setFocusedItemIndex(null);
-    setActiveItemId(null);
-  };
   const closeMenu = () => {
     setIsOpen(false);
-    resetActiveAndFocusedItem();
   };
 
-  const handleBlurCapture = e => {
-    const next = e.relatedTarget;
-    if (!wrapperRef.current?.contains(next)) {
-      closeMenu();
-    }
-    setInputValue(displayValue);
+  const onClose = () => {
+    closeMenu();
     setFilterValue('');
-    onBlur(displayValue);
+    if (
+      allowClear &&
+      !inputValueRef.current &&
+      selected != null &&
+      selected !== ''
+    ) {
+      onSelect('');
+      onBlur('');
+    } else {
+      setInputValue(displayValue);
+      inputValueRef.current = displayValue;
+      onBlur(displayValue);
+    }
   };
 
   const onInputClick = () => {
@@ -120,6 +117,7 @@ export const AutocompleteInputComponent = ({
   };
   const selectOption = (value, content) => {
     setInputValue(String(content));
+    inputValueRef.current = String(content);
     setFilterValue('');
     onSelect(value);
     closeMenu();
@@ -134,71 +132,10 @@ export const AutocompleteInputComponent = ({
   const onTextInputChange = (_event, value) => {
     onChange(value);
     setInputValue(value);
+    inputValueRef.current = value;
     setFilterValue(value);
-    resetActiveAndFocusedItem();
-  };
-  const handleMenuArrowKeys = key => {
-    let indexToFocus = 0;
-    if (!isOpen) {
-      setIsOpen(true);
-    }
-    if (selectOptions.every(option => option.isDisabled)) {
-      return;
-    }
-    if (key === 'ArrowUp') {
-      if (focusedItemIndex === null || focusedItemIndex === 0) {
-        indexToFocus = selectOptions.length - 1;
-      } else {
-        indexToFocus = focusedItemIndex - 1;
-      }
-      while (selectOptions[indexToFocus].isDisabled) {
-        indexToFocus--;
-        if (indexToFocus === -1) {
-          indexToFocus = selectOptions.length - 1;
-        }
-      }
-    }
-    if (key === 'ArrowDown') {
-      if (
-        focusedItemIndex === null ||
-        focusedItemIndex === selectOptions.length - 1
-      ) {
-        indexToFocus = 0;
-      } else {
-        indexToFocus = focusedItemIndex + 1;
-      }
-      while (selectOptions[indexToFocus].isDisabled) {
-        indexToFocus++;
-        if (indexToFocus === selectOptions.length) {
-          indexToFocus = 0;
-        }
-      }
-    }
-    setActiveAndFocusedItem(indexToFocus);
-  };
-  const onInputKeyDown = event => {
-    const focusedItem =
-      focusedItemIndex !== null ? selectOptions[focusedItemIndex] : null;
-    // eslint-disable-next-line default-case
-    switch (event.key) {
-      case 'Enter':
-        if (
-          isOpen &&
-          focusedItem &&
-          focusedItem.value !== NO_RESULTS &&
-          !focusedItem.isAriaDisabled
-        ) {
-          selectOption(focusedItem.value, focusedItem.label);
-        }
-        if (!isOpen) {
-          setIsOpen(true);
-        }
-        break;
-      case 'ArrowUp':
-      case 'ArrowDown':
-        event.preventDefault();
-        handleMenuArrowKeys(event.key);
-        break;
+    if (allowClear && !value && selected != null && selected !== '') {
+      onSelect('');
     }
   };
   const onToggleClick = () => {
@@ -224,14 +161,10 @@ export const AutocompleteInputComponent = ({
           value={inputValue}
           onClick={onInputClick}
           onChange={onTextInputChange}
-          onKeyDown={onInputKeyDown}
           inputId={fieldId ?? `id-${name}`}
           id={`typeahead-select-input-${name}`}
           innerRef={textInputRef}
           placeholder={placeholder}
-          {...(activeItemId && {
-            'aria-activedescendant': activeItemId,
-          })}
           role="combobox"
           isExpanded={isOpen}
           aria-controls={`id-${name}`}
@@ -250,11 +183,7 @@ export const AutocompleteInputComponent = ({
     </MenuToggle>
   );
   return (
-    <div
-      ref={wrapperRef}
-      onBlurCapture={handleBlurCapture}
-      className="autocomplete-input-wrapper"
-    >
+    <div className="autocomplete-input-wrapper">
       <Select
         name={name}
         ouiaId={`autocomplete-select-${name}`}
@@ -263,20 +192,18 @@ export const AutocompleteInputComponent = ({
         selected={selected}
         onSelect={onSelectLocal}
         onOpenChange={nextIsOpen => {
-          if (!nextIsOpen) closeMenu();
+          if (!nextIsOpen) onClose();
           else setIsOpen(true);
         }}
         toggle={toggle}
         shouldFocusFirstItemOnOpen={false}
       >
         <SelectList id="select-typeahead-listbox">
-          {selectOptions.map((option, index) => (
+          {selectOptions.map(option => (
             <SelectOption
               key={option.value || option.label}
-              isFocused={focusedItemIndex === index}
               className={option.className}
               isDisabled={option.disabled || false}
-              id={createItemId(option.value)}
               {...option}
               ref={null}
             >
@@ -318,6 +245,7 @@ AutocompleteInputComponent.propTypes = {
   isDisabled: PropTypes.bool,
   fieldId: PropTypes.string,
   onBlur: PropTypes.func,
+  allowClear: PropTypes.bool,
 };
 
 AutocompleteInputComponent.defaultProps = {
@@ -331,6 +259,7 @@ AutocompleteInputComponent.defaultProps = {
   isDisabled: false,
   fieldId: undefined,
   onBlur: () => {},
+  allowClear: true,
 };
 
 export default AutocompleteInputComponent;

webpack/assets/javascripts/react_app/components/common/AutocompleteInput/__tests__/AutocompleteInput.test.js
@@ -124,14 +124,32 @@ describe('AutocompleteInput RTL Tests', () => {
         expect(screen.queryByRole('listbox')).not.toBeInTheDocument();
       });
     });
-  });
 
-  describe('Keyboard Navigation', () => {
-    test('opens dropdown on Enter key', async () => {
+    test('selects option after typing to filter', async () => {
       render(<AutocompleteInput {...defaultProps} />);
 
       const input = screen.getByRole('combobox');
-      fireEvent.keyDown(input, { key: 'Enter' });
+      fireEvent.change(input, { target: { value: 'Option 2' } });
+
+      await waitFor(() => {
+        expect(screen.getByText('Option 2')).toBeInTheDocument();
+      });
+
+      fireEvent.click(screen.getByText('Option 2'));
+
+      await waitFor(() => {
+        expect(setSelected).toHaveBeenCalledWith('option2');
+      });
+      expect(input).toHaveValue('Option 2');
+    });
+  });
+
+  describe('Keyboard Navigation', () => {
+    test('opens dropdown on click', async () => {
+      render(<AutocompleteInput {...defaultProps} />);
+
+      const input = screen.getByRole('combobox');
+      fireEvent.click(input);
 
       await waitFor(() => {
         expect(screen.getByRole('listbox')).toBeInTheDocument();
@@ -149,6 +167,153 @@ describe('AutocompleteInput RTL Tests', () => {
         expect(screen.getByText('Option 1')).toHaveClass(
           'pf-v5-c-menu__item-text'
         );
+      });
+    });
+  });
+
+  describe('allowClear', () => {
+    test('clears selection when input is emptied with allowClear=true (default)', async () => {
+      const onSelect = jest.fn();
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          selected="option1"
+          onSelect={onSelect}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      expect(input).toHaveValue('Option 1');
+
+      fireEvent.change(input, { target: { value: '' } });
+
+      expect(onSelect).toHaveBeenCalledWith('');
+    });
+
+    test('does not clear selection when input is emptied with allowClear=false', async () => {
+      const onSelect = jest.fn();
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          selected="option1"
+          onSelect={onSelect}
+          allowClear={false}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      expect(input).toHaveValue('Option 1');
+
+      fireEvent.change(input, { target: { value: '' } });
+
+      expect(onSelect).not.toHaveBeenCalled();
+    });
+
+    test('clears selection for numeric selected value with allowClear=true', async () => {
+      const numericOptions = [
+        { value: 0, label: 'Zero' },
+        { value: 1, label: 'One' },
+      ];
+      const onSelect = jest.fn();
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          options={numericOptions}
+          selected={0}
+          onSelect={onSelect}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      expect(input).toHaveValue('Zero');
+
+      fireEvent.change(input, { target: { value: '' } });
+
+      expect(onSelect).toHaveBeenCalledWith('');
+    });
+
+    test('does not call onSelect when selected is already empty', () => {
+      const onSelect = jest.fn();
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          selected=""
+          onSelect={onSelect}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      fireEvent.change(input, { target: { value: '' } });
+
+      expect(onSelect).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('onClose behavior', () => {
+    test('calls onBlur on close', async () => {
+      const onBlur = jest.fn();
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          selected="option1"
+          onBlur={onBlur}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      fireEvent.click(input);
+
+      await waitFor(() => {
+        expect(screen.getByRole('listbox')).toBeInTheDocument();
+      });
+
+      fireEvent.keyDown(input, { key: 'Escape' });
+
+      await waitFor(() => {
+        expect(onBlur).toHaveBeenCalledWith('Option 1');
+      });
+    });
+
+    test('restores input value on close when allowClear=false', async () => {
+      render(
+        <AutocompleteInput
+          {...defaultProps}
+          selected="option1"
+          allowClear={false}
+        />
+      );
+
+      const input = screen.getByRole('combobox');
+      expect(input).toHaveValue('Option 1');
+
+      fireEvent.change(input, { target: { value: 'typed text' } });
+      expect(input).toHaveValue('typed text');
+
+      fireEvent.keyDown(input, { key: 'Escape' });
+
+      await waitFor(() => {
+        expect(input).toHaveValue('Option 1');
+      });
+    });
+
+    test('resets filter on close', async () => {
+      render(<AutocompleteInput {...defaultProps} />);
+
+      const input = screen.getByRole('combobox');
+      fireEvent.change(input, { target: { value: 'Option 1' } });
+
+      await waitFor(() => {
+        expect(screen.queryByText('Option 2')).not.toBeInTheDocument();
+      });
+
+      fireEvent.keyDown(input, { key: 'Escape' });
+
+      fireEvent.click(input);
+
+      await waitFor(() => {
+        expect(screen.getByText('Option 1')).toBeInTheDocument();
+        expect(screen.getByText('Option 2')).toBeInTheDocument();
+        expect(screen.getByText('Option 3')).toBeInTheDocument();
       });
     });
   });
@@ -184,21 +349,6 @@ describe('AutocompleteInput RTL Tests', () => {
 
       const input = screen.getByRole('combobox');
       expect(input).toHaveValue('true');
-    });
-
-  });
-
-  describe('Accessibility', () => {
-    test('has proper ARIA attributes when focused', async () => {
-      render(<AutocompleteInput {...defaultProps} />);
-
-      const input = screen.getByRole('combobox');
-      fireEvent.click(input);
-
-      await waitFor(() => {
-        fireEvent.keyDown(input, { key: 'ArrowDown' });
-        expect(input).toHaveAttribute('aria-activedescendant');
-      });
     });
   });
 });