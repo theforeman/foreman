87c22e16341b13800cb89f71167581bc70425909
Oleh Fedorenko <ofedoren@redhat.com>
2026-06-03 11:03:56 +0000

    Refs #39361 - Drop old code

--- app/models/concerns/scoped_search_extensions.rb
+++ app/models/concerns/scoped_search_extensions.rb
@@ -13,12 +13,8 @@ module ScopedSearchExtensions
     end
 
     def cast_facts(table, key, operator, value)
-      is_int = value.to_s.match?(/\A[-+]?\d+\z/) || value.is_a?(Integer)
-      is_pg = ActiveRecord::Base.connection.adapter_name.downcase.starts_with? 'postgresql'
-      # Once Postgresql 8 support is removed (used in CentOS 6), this could be replaced to only keep the first form (working well with PG 9)
-      if (is_int && !is_pg)
-        casted = "CAST(#{table}.value AS DECIMAL) #{operator} #{value}"
-      elsif (is_int && is_pg && operator !~ /LIKE/i)
+      is_int = value.to_s.match?(/\A[-+]?\d+\z/)
+      if is_int && operator !~ /LIKE/i
         casted = "#{table}.value ~ E'^\\\\d+$' AND CAST(#{table}.value AS DECIMAL) #{operator} #{value}"
       else
         # Escape string formatting with %, as conditions will be re-sanitized through scoped_search
