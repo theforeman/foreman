317397a216b6b30e0feaddb6a261a12f91ed179d
Titani Labaj <tlabaj@redhat.com>
2026-06-24 09:18:59 -0400

    Fixes #39450 - Update ISO Date Time Enzyme tests to RTL

--- webpack/assets/javascripts/react_app/components/common/dates/IsoDate.test.js
+++ webpack/assets/javascripts/react_app/components/common/dates/IsoDate.test.js
@@ -1,35 +1,51 @@
-/* eslint-disable promise/prefer-await-to-then */
-// Configure Enzyme
-import { mount } from 'enzyme';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import IsoDate from './IsoDate';
 import { i18nProviderWrapperFactory } from '../../../common/i18nProviderWrapperFactory';
-import { intl } from '../../../common/I18n';
 
-describe('Date', () => {
+describe('IsoDate', () => {
   const date = new Date('2017-10-13 00:54:55 -1100');
   const now = new Date('2017-10-28 00:00:00 -1100');
-  const IntlDate = i18nProviderWrapperFactory(now, 'UTC')(IsoDate);
+  const IntlIsoDate = i18nProviderWrapperFactory(now, 'UTC')(IsoDate);
 
-  it('formats date', () => {
-    const wrapper = mount(
-      <IntlDate date={date} defaultValue="Default value" />
-    );
+  test('formats date as MM/DD/YYYY', async () => {
+    render(<IntlIsoDate date={date} defaultValue="Default value" />);
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('IsoDate')).toMatchSnapshot();
-    });
+    expect(await screen.findByText('10/13/2017')).toBeInTheDocument();
   });
 
-  it('renders default value', () => {
-    const wrapper = mount(
-      <IntlDate date={null} defaultValue="Default value" />
+  test('shows relative time in the title attribute', async () => {
+    render(<IntlIsoDate date={date} defaultValue="Default value" />);
+
+    expect(await screen.findByTitle('15 days ago')).toBeInTheDocument();
+  });
+
+  test('formats non-ISO date strings from the API', async () => {
+    render(
+      <IntlIsoDate
+        date="2017-10-13 00:54:55 -1100"
+        defaultValue="Default value"
+      />
     );
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('IsoDate')).toMatchSnapshot();
+    expect(await screen.findByText('10/13/2017')).toBeInTheDocument();
+    expect(await screen.findByTitle('15 days ago')).toBeInTheDocument();
+  });
+
+  test('renders default value when date is null', async () => {
+    render(<IntlIsoDate date={null} defaultValue="Default value" />);
+
+    expect(await screen.findByText('Default value')).toBeInTheDocument();
+  });
+
+  test('renders empty string when date is null and no default value is provided', async () => {
+    const { container } = render(<IntlIsoDate date={null} />);
+
+    await screen.findByText((content, element) => {
+      return element.tagName === 'SPAN' && content === '';
     });
+
+    expect(container.querySelector('span')).toBeEmptyDOMElement();
   });
 });
