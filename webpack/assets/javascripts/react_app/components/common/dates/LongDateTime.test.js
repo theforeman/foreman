bd63b2c60ad93b282da69647e670b9f9a4087444
Titani Labaj <tlabaj@redhat.com>
2026-06-24 09:41:31 -0400

    Fixes #39460 - Update Long Date Time Enzyme tests to RTL

--- webpack/assets/javascripts/react_app/components/common/dates/LongDateTime.test.js
+++ webpack/assets/javascripts/react_app/components/common/dates/LongDateTime.test.js
@@ -1,234 +1,90 @@
-/* eslint-disable promise/prefer-await-to-then */
-// Configure Enzyme
-import { mount } from 'enzyme';
 import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import LongDateTime from './LongDateTime';
 import { i18nProviderWrapperFactory } from '../../../common/i18nProviderWrapperFactory';
-import { intl } from '../../../common/I18n';
 
 describe('LongDateTime', () => {
   const date = new Date('2017-10-13 00:54:55 -1100');
   const now = new Date('2017-10-28 00:00:00 -1100');
-  const IntlDate = i18nProviderWrapperFactory(now, 'UTC')(LongDateTime);
+  const IntlLongDateTime = i18nProviderWrapperFactory(now, 'UTC')(LongDateTime);
 
-  it('formats date', () => {
-    const wrapper = mount(
-      <IntlDate date={date} defaultValue="Default value" />
+  const renderLongDateTime = props =>
+    render(<IntlLongDateTime {...props} />);
+
+  const longDateTimePattern = /October 13, 2017[, at]+ 11:54(:\d{2})? AM/;
+
+  it('renders a formatted long date and time without seconds', async () => {
+    renderLongDateTime({ date, defaultValue: 'Default value' });
+
+    await waitFor(() => {
+      expect(screen.getByText(longDateTimePattern)).toBeInTheDocument();
+    });
+    expect(screen.getByText(longDateTimePattern).textContent).not.toMatch(
+      /11:54:\d{2} AM/
     );
+  });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={false}
-            showRelativeTimeTooltip={false}
-          >
-            <span>
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017, 11:54 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      } else {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={false}
-            showRelativeTimeTooltip={false}
-          >
-            <span>
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017 at 11:54 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      }
+  it('renders a formatted long date and time with seconds when enabled', async () => {
+    renderLongDateTime({ date, seconds: true, defaultValue: 'Default value' });
+
+    await waitFor(() => {
+      expect(
+        screen.getByText(/October 13, 2017[, at]+ 11:54:55 AM/)
+      ).toBeInTheDocument();
     });
   });
 
-  it('formats date with relative tooltip', () => {
-    const wrapper = mount(
-      <IntlDate
-        date={date}
-        defaultValue="Default value"
-        showRelativeTimeTooltip
-      />
-    );
+  it('shows a relative time tooltip when showRelativeTimeTooltip is enabled', async () => {
+    renderLongDateTime({
+      date,
+      defaultValue: 'Default value',
+      showRelativeTimeTooltip: true,
+    });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={false}
-            showRelativeTimeTooltip={true}
-          >
-            <span
-              title="15 days ago"
-            >
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017, 11:54 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      } else {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={false}
-            showRelativeTimeTooltip={true}
-          >
-            <span
-              title="15 days ago"
-            >
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017 at 11:54 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      }
+    await waitFor(() => {
+      expect(screen.getByTitle('15 days ago')).toBeInTheDocument();
+    });
+    expect(screen.getByTitle('15 days ago')).toHaveTextContent(
+      longDateTimePattern
+    );
+  });
+
+  it('does not show a relative time tooltip when showRelativeTimeTooltip is disabled', async () => {
+    renderLongDateTime({ date, defaultValue: 'Default value' });
+
+    await waitFor(() => {
+      expect(screen.getByText(longDateTimePattern)).toBeInTheDocument();
+    });
+    expect(screen.queryByTitle('15 days ago')).not.toBeInTheDocument();
+  });
+
+  it('renders the default value when date is null', async () => {
+    renderLongDateTime({ date: null, defaultValue: 'Default value' });
+
+    await waitFor(() => {
+      expect(screen.getByText('Default value')).toBeInTheDocument();
     });
   });
 
-  it('formats date with seconds', () => {
-    const wrapper = mount(
-      <IntlDate date={date} seconds defaultValue="Default value" />
-    );
+  it('renders an empty string when date is null and no default value is provided', async () => {
+    const { container } = renderLongDateTime({ date: null });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      if (process.version.startsWith('v14') || process.version.startsWith('v16')) {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={true}
-            showRelativeTimeTooltip={false}
-          >
-            <span>
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                second="2-digit"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017, 11:54:55 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      } else {
-        expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(
-          `
-          <LongDateTime
-            date={2017-10-13T11:54:55.000Z}
-            defaultValue="Default value"
-            seconds={true}
-            showRelativeTimeTooltip={false}
-          >
-            <span>
-              <FormattedDate
-                day="2-digit"
-                hour="2-digit"
-                minute="2-digit"
-                month="long"
-                second="2-digit"
-                value={2017-10-13T11:54:55.000Z}
-                year="numeric"
-              >
-                <span>
-                  October 13, 2017 at 11:54:55 AM
-                </span>
-              </FormattedDate>
-            </span>
-          </LongDateTime>
-        `
-        );
-      }
+    await waitFor(() => {
+      expect(container.querySelector('span')).toBeInTheDocument();
     });
+    expect(container.querySelector('span')).toHaveTextContent('');
   });
 
-  it('renders default value', () => {
-    const wrapper = mount(
-      <IntlDate date={null} defaultValue="Default value" />
-    );
+  it('formats a non-ISO date string', async () => {
+    renderLongDateTime({
+      date: '2017-10-13 00:54:55 -1100',
+      defaultValue: 'Default value',
+    });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('LongDateTime')).toMatchInlineSnapshot(`
-        <LongDateTime
-          date={null}
-          defaultValue="Default value"
-          seconds={false}
-          showRelativeTimeTooltip={false}
-        >
-          <span>
-            Default value
-          </span>
-        </LongDateTime>
-      `);
+    await waitFor(() => {
+      expect(screen.getByText(longDateTimePattern)).toBeInTheDocument();
     });
   });
 });
