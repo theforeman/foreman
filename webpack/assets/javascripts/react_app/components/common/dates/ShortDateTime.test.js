b29e3cbc5ec3aaa678f0ea72364766a5b0d9d4a9
Titani Labaj <tlabaj@redhat.com>
2026-06-24 10:28:14 -0400

    Fixes #39435 - Refactor Short Date Time Enzyme tests to RTL

--- webpack/assets/javascripts/react_app/components/common/dates/ShortDateTime.test.js
+++ webpack/assets/javascripts/react_app/components/common/dates/ShortDateTime.test.js
@@ -1,61 +1,80 @@
-/* eslint-disable promise/prefer-await-to-then */
-// Configure Enzyme
-import { mount } from 'enzyme';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import ShortDateTime from './ShortDateTime';
 import { i18nProviderWrapperFactory } from '../../../common/i18nProviderWrapperFactory';
-import { intl } from '../../../common/I18n';
 
 describe('ShortDateTime', () => {
   const date = new Date('2017-10-13 00:54:55 -1100');
   const now = new Date('2017-10-28 00:00:00 -1100');
-  const IntlDate = i18nProviderWrapperFactory(now, 'UTC')(ShortDateTime);
 
-  it('formats date', () => {
-    const wrapper = mount(
-      <IntlDate date={date} defaultValue="Default value" />
+  const renderShortDateTime = (props = {}) => {
+    const IntlShortDateTime = i18nProviderWrapperFactory(now, 'UTC')(
+      ShortDateTime
     );
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('ShortDateTime')).toMatchSnapshot();
+    return render(
+      <IntlShortDateTime defaultValue="Default value" {...props} />
+    );
+  };
+
+  describe('when a date is provided', () => {
+    it('renders the date in short month, day, and time format', async () => {
+      renderShortDateTime({ date });
+
+      expect(await screen.findByText('Oct 13, 11:54 AM')).toBeInTheDocument();
+    });
+
+    it('formats non-ISO date strings', async () => {
+      renderShortDateTime({ date: '2017-10-13 00:54:55 -1100' });
+
+      expect(await screen.findByText('Oct 13, 11:54 AM')).toBeInTheDocument();
+    });
+
+    it('includes seconds in the formatted time when seconds is enabled', async () => {
+      renderShortDateTime({ date, seconds: true });
+
+      expect(
+        await screen.findByText('Oct 13, 11:54:55 AM')
+      ).toBeInTheDocument();
+    });
+
+    it('sets the title to relative time when showRelativeTimeTooltip is enabled', async () => {
+      renderShortDateTime({ date, showRelativeTimeTooltip: true });
+
+      await screen.findByText('Oct 13, 11:54 AM');
+      expect(screen.getByTitle('15 days ago')).toBeInTheDocument();
+    });
+
+    it('does not set a title attribute when showRelativeTimeTooltip is disabled', async () => {
+      renderShortDateTime({ date });
+
+      expect(
+        await screen.findByText('Oct 13, 11:54 AM')
+      ).not.toHaveAttribute('title');
     });
   });
 
-  it('formats date with relative tooltip', () => {
-    const wrapper = mount(
-      <IntlDate
-        date={date}
-        defaultValue="Default value"
-        showRelativeTimeTooltip
-      />
-    );
+  describe('when no date is provided', () => {
+    it('renders the default value when date is null', async () => {
+      renderShortDateTime({ date: null });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('ShortDateTime')).toMatchSnapshot();
+      expect(await screen.findByText('Default value')).toBeInTheDocument();
     });
-  });
 
-  it('formats date with seconds', () => {
-    const wrapper = mount(
-      <IntlDate date={date} seconds defaultValue="Default value" />
-    );
+    it('renders the default value when date is undefined', async () => {
+      renderShortDateTime({ date: undefined });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('ShortDateTime')).toMatchSnapshot();
+      expect(await screen.findByText('Default value')).toBeInTheDocument();
     });
-  });
 
-  it('renders default value', () => {
-    const wrapper = mount(
-      <IntlDate date={null} defaultValue="Default value" />
-    );
+    it('renders an empty string when date is null and no default value is given', async () => {
+      renderShortDateTime({
+        date: null,
+        defaultValue: '',
+      });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('ShortDateTime')).toMatchSnapshot();
+      await screen.findByText('', { selector: 'span' });
     });
   });
 });
