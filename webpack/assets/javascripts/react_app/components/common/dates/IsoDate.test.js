81d8d67f8b27be8716421c0d2f394c0d2395b70d
Konstantinos Familonidis <kfamilon@redhat.com>
2026-06-28 13:06:07 +0300

    Fixes #39489: Replace enzyme test in IsoDate with RTL

--- webpack/assets/javascripts/react_app/components/common/dates/IsoDate.test.js
+++ webpack/assets/javascripts/react_app/components/common/dates/IsoDate.test.js
@@ -1,35 +1,35 @@
-/* eslint-disable promise/prefer-await-to-then */
-// Configure Enzyme
-import { mount } from 'enzyme';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import IsoDate from './IsoDate';
 import { i18nProviderWrapperFactory } from '../../../common/i18nProviderWrapperFactory';
-import { intl } from '../../../common/I18n';
 
-describe('Date', () => {
+describe('IsoDate', () => {
   const date = new Date('2017-10-13 00:54:55 -1100');
   const now = new Date('2017-10-28 00:00:00 -1100');
-  const IntlDate = i18nProviderWrapperFactory(now, 'UTC')(IsoDate);
 
-  it('formats date', () => {
-    const wrapper = mount(
-      <IntlDate date={date} defaultValue="Default value" />
-    );
+  const renderIsoDate = (props = {}) => {
+    const IntlIsoDate = i18nProviderWrapperFactory(now, 'UTC')(IsoDate);
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('IsoDate')).toMatchSnapshot();
-    });
+    return render(<IntlIsoDate defaultValue="Default value" {...props} />);
+  };
+
+  it('formats the date as month/day/year', async () => {
+    renderIsoDate({ date });
+
+    expect(await screen.findByText('10/13/2017')).toBeInTheDocument();
   });
 
-  it('renders default value', () => {
-    const wrapper = mount(
-      <IntlDate date={null} defaultValue="Default value" />
-    );
+  it('sets the title to the relative time', async () => {
+    renderIsoDate({ date });
 
-    intl.ready.then(() => {
-      wrapper.update();
-      expect(wrapper.find('IsoDate')).toMatchSnapshot();
-    });
+    await screen.findByText('10/13/2017');
+    expect(screen.getByTitle('15 days ago')).toBeInTheDocument();
+  });
+
+  it('renders the default value when no date is provided', async () => {
+    renderIsoDate({ date: null });
+
+    expect(await screen.findByText('Default value')).toBeInTheDocument();
   });
 });
