777c514352707e09f60144031795169b208f7bfc
Konstantinos Familonidis <kfamilon@redhat.com>
2026-06-28 13:10:13 +0300

    Fixes #39488: Replace enzyme test in MessageBox with RTL

--- webpack/assets/javascripts/react_app/components/common/MessageBox/MessageBox.test.js
+++ webpack/assets/javascripts/react_app/components/common/MessageBox/MessageBox.test.js
@@ -1,36 +1,45 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
 import MessageBox from './index';
 
 jest.unmock('./index');
 
-const testMessageBoxSnapshot = props => {
-  const wrapper = shallow(<MessageBox {...props} />);
-
-  expect(wrapper).toMatchSnapshot();
-};
-
 describe('MessageBox', () => {
   describe('the message', () => {
-    it('displays this is some text', () =>
-      testMessageBoxSnapshot({ msg: 'this is some text', icontype: 'info' }));
+    it('displays this is some text', () => {
+      render(<MessageBox msg="this is some text" icontype="info" />);
 
-    it('displays This is another message', () =>
-      testMessageBoxSnapshot({
-        msg: 'This is another message',
-        icontype: 'warning',
-      }));
+      expect(screen.getByText('this is some text')).toBeInTheDocument();
+    });
+
+    it('displays This is another message', () => {
+      render(<MessageBox msg="This is another message" icontype="warning" />);
+
+      expect(screen.getByText('This is another message')).toBeInTheDocument();
+    });
   });
 
   describe('the icon', () => {
-    it('has pficon and pficon-info classes', () =>
-      testMessageBoxSnapshot({ msg: 'this is some text', icontype: 'info' }));
+    it('has pficon and pficon-info classes', () => {
+      const { container } = render(
+        <MessageBox msg="this is some text" icontype="info" />
+      );
 
-    it('has pficon and pficon-info classes with error icon', () =>
-      testMessageBoxSnapshot({
-        msg: 'this is some text',
-        icontype: 'error-circle-o',
-      }));
+      expect(
+        container.querySelector('.message-box-icon')
+      ).toHaveClass('pficon', 'pficon-info');
+    });
+
+    it('has pficon and pficon-info classes with error icon', () => {
+      const { container } = render(
+        <MessageBox msg="this is some text" icontype="error-circle-o" />
+      );
+
+      expect(
+        container.querySelector('.message-box-icon')
+      ).toHaveClass('pficon', 'pficon-error-circle-o');
+    });
   });
 });
