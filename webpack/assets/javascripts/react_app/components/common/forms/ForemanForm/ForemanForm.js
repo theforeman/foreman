6ecbb239eee86ad99b913ec74bba7c2b1611f64b
Lukas Jezek <lukas-jezek@seznam.cz>
2026-07-01 17:28:29 +0000

    Fixes #39485 - deprecate ForemanForm

--- webpack/assets/javascripts/react_app/components/common/forms/ForemanForm/ForemanForm.js
+++ webpack/assets/javascripts/react_app/components/common/forms/ForemanForm/ForemanForm.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Formik } from 'formik';
 import PropTypes from 'prop-types';
 import Form from '../Form';
 import { translate as __ } from '../../../../common/I18n';
+import { deprecate } from '../../../../common/DeprecationService';
 
 export const isInitialValid = ({ validationSchema, initialValues }) =>
   !validationSchema ? true : validationSchema.isValidSync(initialValues);
@@ -14,37 +15,47 @@ const ForemanForm = ({
   validationSchema,
   enableReinitialize,
   onCancel,
-}) => (
-  <Formik
-    onSubmit={onSubmit}
-    initialValues={initialValues}
-    validationSchema={validationSchema}
-    isInitialValid={isInitialValid}
-    enableReinitialize={enableReinitialize}
-  >
-    {formProps => {
-      const disabled = formProps.isSubmitting || !formProps.isValid;
-      const submissionError = formProps.errors._error;
+}) => {
+  useEffect(() => {
+    deprecate(
+      `forms/ForemanForm/ForemanForm`,
+      'Form from @patternfly/react-core',
+      '5.1'
+    );
+  }, []);
 
-      return (
-        <Form
-          onSubmit={formProps.handleSubmit}
-          onCancel={onCancel}
-          disabled={disabled}
-          error={submissionError}
-          errorTitle={
-            submissionError?.severity === 'danger'
-              ? __('Error! ')
-              : __('Warning! ')
-          }
-          submitting={formProps.isSubmitting}
-        >
-          {cloneChildren(children, { formProps, disabled })}
-        </Form>
-      );
-    }}
-  </Formik>
-);
+  return (
+    <Formik
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      isInitialValid={isInitialValid}
+      enableReinitialize={enableReinitialize}
+    >
+      {formProps => {
+        const disabled = formProps.isSubmitting || !formProps.isValid;
+        const submissionError = formProps.errors._error;
+
+        return (
+          <Form
+            onSubmit={formProps.handleSubmit}
+            onCancel={onCancel}
+            disabled={disabled}
+            error={submissionError}
+            errorTitle={
+              submissionError?.severity === 'danger'
+                ? __('Error! ')
+                : __('Warning! ')
+            }
+            submitting={formProps.isSubmitting}
+          >
+            {cloneChildren(children, { formProps, disabled })}
+          </Form>
+        );
+      }}
+    </Formik>
+  );
+};
 
 const cloneChildren = (children, childProps) => (
   <React.Fragment>
