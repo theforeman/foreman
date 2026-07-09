3dcd73f69d6c584ed912dda361da25495dc63912
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-07-09 07:37:54 -0400

    Fixes #39496 - update editor settings to pf5 and the tests to RTL

--- webpack/assets/javascripts/react_app/components/Editor/components/EditorSettings.js
+++ webpack/assets/javascripts/react_app/components/Editor/components/EditorSettings.js
@@ -1,15 +1,84 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Dropdown, MenuItem, Button } from 'patternfly-react';
 import { CogIcon } from '@patternfly/react-icons';
 import {
+  Button,
+  Checkbox,
+  FormGroup,
   Icon,
+  MenuToggle,
   Popover,
   PopoverPosition,
+  Select,
+  SelectList,
+  SelectOption,
   Tooltip,
   TooltipPosition,
 } from '@patternfly/react-core';
 import { translate as __ } from '../../../common/I18n';
+
+const SettingsSelect = ({
+  id,
+  label,
+  disabled,
+  options,
+  selected,
+  onSelect,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <FormGroup label={label} fieldId={id} isInline>
+      <Select
+        id={id}
+        ouiaId={id}
+        isOpen={isOpen}
+        selected={selected}
+        onSelect={(_event, selection) => {
+          onSelect(selection);
+          setIsOpen(false);
+        }}
+        onOpenChange={setIsOpen}
+        toggle={toggleRef => (
+          <MenuToggle
+            ref={toggleRef}
+            onClick={() => {
+              if (!disabled) {
+                setIsOpen(!isOpen);
+              }
+            }}
+            isExpanded={isOpen}
+            isDisabled={disabled}
+            isFullWidth
+          >
+            {selected}
+          </MenuToggle>
+        )}
+      >
+        <SelectList id="settings-dropdown">
+          {options.map(option => (
+            <SelectOption key={option} value={option}>
+              {option}
+            </SelectOption>
+          ))}
+        </SelectList>
+      </Select>
+    </FormGroup>
+  );
+};
+
+SettingsSelect.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  selected: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+SettingsSelect.defaultProps = {
+  disabled: false,
+};
 
 const EditorSettings = ({
   selectedView,
@@ -32,96 +101,58 @@ const EditorSettings = ({
       headerContent={__('Settings')}
       bodyContent={
         <div>
-          <div className="cog-popover-dropdown">
-            <div className="cog-popover-dropdown-title">{__('Syntax')}</div>
-            <Dropdown disabled={selectedView === 'preview'} id="mode-dropdown">
-              <Dropdown.Toggle>{mode}</Dropdown.Toggle>
-              <Dropdown.Menu id="settings-dropdown">
-                {modes.map((aceMode, i) => (
-                  <MenuItem
-                    key={i}
-                    onClick={() => changeSetting({ mode: aceMode })}
-                  >
-                    {aceMode}
-                  </MenuItem>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-          <div className="cog-popover-dropdown">
-            <div className="cog-popover-dropdown-title">{__('Keybind')}</div>
-            <Dropdown
-              disabled={selectedView === 'preview'}
-              id="keybindings-dropdown"
-            >
-              <Dropdown.Toggle>{keyBinding}</Dropdown.Toggle>
-              <Dropdown.Menu id="settings-dropdown">
-                {keyBindings.map((keyBind, i) => (
-                  <MenuItem
-                    key={i}
-                    onClick={() => changeSetting({ keyBinding: keyBind })}
-                  >
-                    {keyBind}
-                  </MenuItem>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-          <div className="cog-popover-dropdown">
-            <div className="cog-popover-dropdown-title">{__('Theme')}</div>
-            <Dropdown id="themes-dropdown">
-              <Dropdown.Toggle>{theme}</Dropdown.Toggle>
-              <Dropdown.Menu id="settings-dropdown">
-                {themes.map((themeKey, i) => (
-                  <MenuItem
-                    key={i}
-                    onClick={() => changeSetting({ theme: themeKey })}
-                  >
-                    {themeKey}
-                  </MenuItem>
-                ))}
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-          <div className="cog-popover-dropdown">
-            <div className="cog-popover-dropdown-title">
-              {__('Autocompletion')}
-            </div>
-            <div className="dropdown btn-group">
-              <input
-                id="autocompletion-checkbox"
-                name="autocompletion"
-                type="checkbox"
-                checked={autocompletion}
-                onChange={e =>
-                  changeSetting({ autocompletion: !autocompletion })
-                }
-              />
-            </div>
-          </div>
-          <div className="cog-popover-dropdown">
-            <div className="cog-popover-dropdown-title">
-              {__('Live Autocompletion')}
-            </div>
-            <div className="dropdown btn-group">
-              <input
-                id="live-autocompletion-checkbox"
-                name="liveAutocompletion"
-                type="checkbox"
-                checked={liveAutocompletion}
-                disabled={!autocompletion}
-                onChange={e =>
-                  changeSetting({ liveAutocompletion: !liveAutocompletion })
-                }
-              />
-            </div>
-          </div>
+          <SettingsSelect
+            id="mode-dropdown"
+            label={__('Syntax')}
+            disabled={selectedView === 'preview'}
+            options={modes}
+            selected={mode}
+            onSelect={selection => changeSetting({ mode: selection })}
+          />
+          <SettingsSelect
+            id="keybindings-dropdown"
+            label={__('Keybind')}
+            disabled={selectedView === 'preview'}
+            options={keyBindings}
+            selected={keyBinding}
+            onSelect={selection => changeSetting({ keyBinding: selection })}
+          />
+          <SettingsSelect
+            id="themes-dropdown"
+            label={__('Theme')}
+            options={themes}
+            selected={theme}
+            onSelect={selection => changeSetting({ theme: selection })}
+          />
+          <Checkbox
+            id="autocompletion-checkbox"
+            ouiaId="autocompletion-checkbox"
+            label={__('Autocompletion')}
+            isChecked={autocompletion}
+            onChange={() => changeSetting({ autocompletion: !autocompletion })}
+          />
+          <Checkbox
+            id="live-autocompletion-checkbox"
+            ouiaId="live-autocompletion-checkbox"
+            label={__('Live Autocompletion')}
+            isChecked={liveAutocompletion}
+            isDisabled={!autocompletion}
+            onChange={() =>
+              changeSetting({ liveAutocompletion: !liveAutocompletion })
+            }
+          />
         </div>
       }
       triggerRef={() => document.getElementById('cog-btn')}
     />
     <Tooltip content={__('Settings')} position={TooltipPosition.top}>
-      <Button className="editor-button" id="cog-btn" bsStyle="link">
+      <Button
+        variant="plain"
+        className="editor-button"
+        id="cog-btn"
+        ouiaId="cog-btn"
+        aria-label={__('Settings')}
+      >
         <Icon size="md">
           <CogIcon />
         </Icon>
