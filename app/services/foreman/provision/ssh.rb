923a46ca5ac85f9c46258d8b4177ba119263bf47
Lukas Zapletal <lzap+git@redhat.com>
2026-08-18 14:59:18 +0200

    Fixes #39645 - Replace Fog::SSH/SCP with system SSH

--- app/services/foreman/provision/ssh.rb
+++ app/services/foreman/provision/ssh.rb
@@ -1,113 +1,121 @@
-require 'timeout'
+require 'open3'
 
-class Foreman::Provision::Ssh
-  attr_reader :template, :uuid, :results, :address, :username, :options
+module Foreman
+  module Provision
+    # Simple SSH client implemented via OpenSSH ssh and sshpass commands.
+    class Ssh
+      def initialize(address, username = "root", options = {})
+        @username = username
+        @address  = address
+        @template = options.delete(:template) || (raise ::Foreman::Exception.new(N_('must provide a template')))
+        @uuid     = options.delete(:uuid) || "#{@address}-#{@username}"
+        @options  = options
 
-  def initialize(address, username = "root", options = { })
-    @username = username
-    @address  = address
-    @template = options.delete(:template) || raise("must provide a template")
-    @uuid     = options.delete(:uuid)     || "#{address}-#{username}"
-    @options  = defaults.merge(options)
-
-    initiate_connection!
-  end
-
-  def deploy!
-    logger.debug "about to upload #{template} to remote system at #{remote_script}"
-    scp.upload(template, remote_script)
-    logger.debug "about to execute #{command}"
-    @results = ssh.run(command)
-    log_stdout
-    log_stderr
-    success?
-  end
-
-  private
-
-  def success?
-    return true if results.empty?
-    results.map(&:status).compact == [0]
-  end
-
-  def log_stdout
-    results.each do |r|
-      r.stdout.split("\n").each { |l| logger.debug l }
-    end
-  end
-
-  def log_stderr
-    results.each do |r|
-      r.stderr.split("\n").each { |l| logger.warn l }
-    end
-  end
-
-  def remote_script
-    "bootstrap-#{uuid}"
-  end
-
-  def command_prefix
-    (username == "root") ? "" : "sudo "
-  end
-
-  def command
-    # Use the users home to store the provision script since we can't reliably
-    # tell if other locations are writeable or executable by the user.
-    main_execution = "(chmod 0701 ./#{remote_script} && #{command_prefix} ./#{remote_script} ; echo $? >#{remote_script}.status) 2>&1"
-    "#{command_prefix} sh -c '#{main_execution} | tee #{remote_script}.log; exit $(cat #{remote_script}.status)'"
-  end
-
-  def defaults
-    {
-      :keys_only    => true,
-      :config       => false,
-      :auth_methods => %w(publickey),
-      :compression  => true,
-      :logger       => logger,
-    }
-  end
-
-  def logger
-    Rails.logger
-  end
-
-  def initiate_connection!
-    Timeout.timeout(Setting[:ssh_timeout].to_i) do
-      Timeout.timeout(8) do
-        ssh.run('pwd')
+        check_auth_options
       end
-    rescue Errno::ECONNREFUSED
-      logger.debug "Connection refused for #{address}, retrying"
-      sleep(2)
-      retry
-    rescue Errno::EHOSTUNREACH
-      logger.debug "Host unreachable for #{address}, retrying"
-      sleep(2)
-      retry
-    rescue Net::SSH::Disconnect
-      logger.debug "Host dropping connections for #{address}, retrying"
-      sleep(2)
-      retry
-    rescue Net::SSH::ConnectionTimeout
-      logger.debug "Host timed out for #{address}, retrying"
-      sleep(2)
-      retry
-    rescue Net::SSH::AuthenticationFailed
-      logger.debug "Auth failed for #{username} at #{address}, retrying"
-      sleep(2)
-      retry
-    rescue Timeout::Error
-      retry
-    rescue => e
-      Foreman::Logging.exception("SSH error", e)
+
+      def deploy!
+        logger.info "Executing provisioning script on #{@address}"
+
+        _stdout, _stderr, status = run_ssh(
+          "#{command_prefix}sh",
+          stdin_data: File.binread(@template)
+        )
+
+        status.success?
+      end
+
+      # Wait for maximum of ssh_timeout setting for a client to complete full connection.
+      def ping
+        deadline = Process.clock_gettime(Process::CLOCK_MONOTONIC) + Setting[:ssh_timeout].to_i
+
+        logger.info "Making a ping SSH connection to #{@address}"
+
+        loop do
+          _stdout, _stderr, status = run_ssh("true")
+
+          if status.success?
+            return self
+          end
+
+          if Process.clock_gettime(Process::CLOCK_MONOTONIC) >= deadline
+            raise ::Foreman::Exception.new(
+              N_("Failed to connect to %{address} after %{timeout} seconds"),
+              {:address => @address, :timeout => Setting[:ssh_timeout]}
+            )
+          end
+
+          logger.debug "SSH connection to #{@address} failed, retrying..."
+          sleep 2
+        end
+      end
+
+      private
+
+      def run_ssh(remote_command, stdin_data: nil)
+        with_tmp_key do |key_file|
+          cmd = build_command(remote_command, key_file)
+          opts = {}
+          opts[:stdin_data] = stdin_data if stdin_data
+          stdout, stderr, status = Open3.capture3(*cmd, **opts)
+          logger.debug(stdout) if stdout.present?
+          stderr.each_line { |l| logger.warn(l.chomp) } if stderr.present?
+          [stdout, stderr, status]
+        end
+      end
+
+      def build_command(remote_command, key_file)
+        cmd = []
+        if @options[:password].present?
+          cmd.push({"SSHPASS" => @options[:password]}, "sshpass", "-e")
+        end
+        cmd.push("ssh", *ssh_options(key_file), "#{@username}@#{@address}", remote_command)
+        cmd
+      end
+
+      def ssh_options(key_file)
+        opts = %w[
+          -oStrictHostKeyChecking=no
+          -oUserKnownHostsFile=/dev/null
+          -oConnectTimeout=4
+          -oCompression=yes
+          -oBatchMode=yes
+        ]
+
+        opts.push("-oIdentityFile=#{key_file.path}") if key_file
+
+        methods = []
+        methods << "publickey" if @options[:key_data].present?
+        methods << "password" if @options[:password].present?
+        opts.push("-oPreferredAuthentications=#{methods.join(',')}")
+
+        opts
+      end
+
+      def command_prefix
+        @username == "root" ? "" : "sudo "
+      end
+
+      def with_tmp_key
+        return yield(nil) if @options[:key_data].blank?
+
+        Tempfile.create("foreman-ssh-#{@uuid}") do |f|
+          f.write(@options[:key_data])
+          yield f
+        end
+      end
+
+      def check_auth_options
+        return if @options[:key_data].present? || @options[:password].present?
+
+        raise Foreman::Exception.new(
+          N_("Missing SSH credentials: provide key_data or password")
+        )
+      end
+
+      def logger
+        Rails.logger
+      end
     end
-  end
-
-  def ssh
-    Fog::SSH.new(address, username, options.merge(:timeout => 4))
-  end
-
-  def scp
-    Fog::SCP.new(address, username, options)
   end
 end
