179d3e691b71e0a6321e961588e07dd516259ef1
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-08-19 04:42:52 -0400

    Fixes #39644 - replace enzyme tests with RTL tests for APIRequest

--- webpack/assets/javascripts/react_app/redux/API/__tests__/APIRequest.test.js
+++ webpack/assets/javascripts/react_app/redux/API/__tests__/APIRequest.test.js
@@ -1,124 +1,159 @@
 import { API } from '../';
-import IntegrationTestHelper from '../../../common/IntegrationTestHelper';
 import { action, key, postActionWithCallback } from '../APIFixtures';
 import { apiRequest } from '../APIRequest';
 
 const data = { results: [1] };
 jest.mock('../');
 
+const createStore = () => ({
+  dispatch: jest.fn(),
+  getState: jest.fn(() => ({
+    intervals: { [key]: 1 },
+    API: {
+      INITIAL_RESOURCE: { response: { results: [2] } },
+    },
+  })),
+});
+
 describe('API get', () => {
-  const store = {
-    dispatch: jest.fn(),
-    getState: jest.fn(() => ({
-      intervals: { [key]: 1 },
-      API: {
-        INITIAL_RESOURCE: { response: { results: [2] } },
-      },
-    })),
-  };
+  let store;
   beforeEach(() => {
-    store.dispatch = jest.fn();
+    store = createStore();
+    jest.clearAllMocks();
   });
 
   it('should dispatch request and success actions on resolve', async () => {
     const apiSuccessResponse = { data };
-    API.get.mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          resolve(apiSuccessResponse);
-        })
-    );
+    API.get.mockResolvedValue(apiSuccessResponse);
+
     const modifiedAction = { ...action };
     modifiedAction.payload.handleSuccess = jest.fn();
-    apiRequest(modifiedAction, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(modifiedAction.payload.handleSuccess.mock.calls).toMatchSnapshot();
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
+
+    await apiRequest(modifiedAction, store);
+
+    expect(store.dispatch).toHaveBeenCalledTimes(2);
+    expect(store.dispatch).toHaveBeenNthCalledWith(1, {
+      type: 'SOME_KEY_REQUEST',
+      key,
+      payload: { id: 2, url: 'some/url' },
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(2, {
+      type: 'SOME_KEY_SUCCESS',
+      key,
+      payload: { id: 2, url: 'some/url' },
+      response: data,
+    });
+    expect(modifiedAction.payload.handleSuccess).toHaveBeenCalledWith(
+      apiSuccessResponse,
+      expect.any(Function)
+    );
   });
 
   it('should dispatch request and failure actions on reject', async () => {
     const apiError = new Error('bad request');
-    API.get.mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          reject(apiError);
-        })
-    );
-    const modifiedAction = { ...action };
-    modifiedAction.payload.handleError = jest.fn();
-    apiRequest(modifiedAction, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(modifiedAction.payload.handleError.mock.calls).toMatchSnapshot();
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
-  });
+    API.get.mockRejectedValue(apiError);
 
-  it('should dispatch stop interval on API error', async () => {
-    const apiError = new Error('bad request');
-    API.get.mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          reject(apiError);
-        })
-    );
     const modifiedAction = { ...action };
     modifiedAction.payload.handleError = jest.fn();
-    apiRequest(modifiedAction, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(modifiedAction.payload.handleError.mock.calls).toMatchSnapshot();
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
+
+    await apiRequest(modifiedAction, store);
+
+    expect(store.dispatch).toHaveBeenCalledTimes(2);
+    expect(store.dispatch).toHaveBeenNthCalledWith(1, {
+      type: 'SOME_KEY_REQUEST',
+      key,
+      payload: { id: 2, url: 'some/url' },
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(2, {
+      type: 'SOME_KEY_FAILURE',
+      key,
+      payload: { id: 2, url: 'some/url' },
+      response: apiError,
+    });
+    expect(modifiedAction.payload.handleError).toHaveBeenCalledWith(
+      apiError,
+      expect.any(Function)
+    );
   });
 
   it('should dispatch a success toast notification on API resolve', async () => {
     const apiSuccessResponse = { data };
-    API.get.mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          resolve(apiSuccessResponse);
-        })
-    );
+    API.get.mockResolvedValue(apiSuccessResponse);
+
     const modifiedAction = { ...action };
     modifiedAction.payload.successToast = jest.fn(
       () => 'Your API request was successful!'
     );
-    apiRequest(modifiedAction, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(modifiedAction.payload.successToast).toHaveBeenLastCalledWith(
+
+    await apiRequest(modifiedAction, store);
+
+    expect(modifiedAction.payload.successToast).toHaveBeenCalledWith(
       apiSuccessResponse
     );
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
+    expect(store.dispatch).toHaveBeenCalledTimes(3);
+    expect(store.dispatch).toHaveBeenNthCalledWith(3, {
+      type: 'toasts/addToast',
+      payload: {
+        key: 'SOME_KEY_SUCCESS',
+        toast: {
+          key: 'SOME_KEY_SUCCESS',
+          message: 'Your API request was successful!',
+          type: 'success',
+        },
+      },
+    });
   });
 
   it('should dispatch an error toast notification on API failure', async () => {
     const apiError = new Error('bad request');
-    API.get.mockImplementation(
-      () =>
-        new Promise((resolve, reject) => {
-          reject(apiError);
-        })
-    );
+    API.get.mockRejectedValue(apiError);
+
     const modifiedAction = { ...action };
     modifiedAction.payload.errorToast = jest.fn(
       error =>
         `Oh no! Something went wrong, server returned the error: ${error.message}`
     );
-    apiRequest(modifiedAction, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(modifiedAction.payload.errorToast).toHaveBeenLastCalledWith(
-      apiError
-    );
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
+
+    await apiRequest(modifiedAction, store);
+
+    expect(modifiedAction.payload.errorToast).toHaveBeenCalledWith(apiError);
+    expect(store.dispatch).toHaveBeenCalledTimes(3);
+    expect(store.dispatch).toHaveBeenNthCalledWith(3, {
+      type: 'toasts/addToast',
+      payload: {
+        key: 'SOME_KEY_FAILURE',
+        toast: {
+          key: 'SOME_KEY_FAILURE',
+          message:
+            'Oh no! Something went wrong, server returned the error: bad request',
+          type: 'danger',
+        },
+      },
+    });
   });
 
   it('should dispatch an update if an updateData callback exists', async () => {
     const apiSuccessResponse = { data };
-    API.post.mockImplementation(
-      () =>
-        new Promise(resolve => {
-          resolve(apiSuccessResponse);
-        })
-    );
-    apiRequest(postActionWithCallback, store);
-    await IntegrationTestHelper.flushAllPromises();
-    expect(store.dispatch.mock.calls).toMatchSnapshot();
+    API.post.mockResolvedValue(apiSuccessResponse);
+
+    await apiRequest(postActionWithCallback, store);
+
+    expect(store.dispatch).toHaveBeenCalledTimes(3);
+    expect(store.dispatch).toHaveBeenNthCalledWith(1, {
+      type: 'SOME_KEY_REQUEST',
+      key: 'INITIAL_RESOURCE',
+      payload: { id: 2, url: 'some/url' },
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(2, {
+      type: 'SOME_KEY_SUCCESS',
+      key: 'INITIAL_RESOURCE',
+      payload: { id: 2, url: 'some/url' },
+      response: data,
+    });
+    expect(store.dispatch).toHaveBeenNthCalledWith(3, {
+      type: 'SOME_KEY_UPDATE',
+      key: 'INITIAL_RESOURCE',
+      payload: { results: [3] },
+    });
   });
 });
