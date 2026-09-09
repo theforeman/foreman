15c7b2093570d5627ec16726c05fb1d2335a7382
Andrei Lakatos <andreilakatos.dev@gmail.com>
2026-09-09 07:14:27 -0400

    Fixes #39745 - remove ForemanModal and its uses inside the repo

--- webpack/assets/javascripts/react_app/components/ForemanModal/ForemanModalContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react';
-import forceSingleton from '../../common/forceSingleton';
-
-// creating context in a separate file to avoid circular imports
-export default forceSingleton('ForemanModalContext', () => createContext(null));
