a21273a13820103c2f569a4d5446806dfff3dae0
Eric D. Helms <ericdhelms@gmail.com>
2026-09-21 09:33:31 -0400

    Refs #39737: Require Puma 8 for systemd service support

--- bundler.d/service.rb
+++ bundler.d/service.rb
@@ -1,5 +1,1 @@
-gem 'puma', '>= 5.1', '< 9', groups: [:test, :service], require: false
-group :service do
-  # Puma has a soft dependency on this
-  gem 'sd_notify', '~> 0.1.0'
-end
+gem 'puma', '>= 8.0', '< 9', groups: [:test, :service], require: false
